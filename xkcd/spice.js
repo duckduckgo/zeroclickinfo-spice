18635f494a80bcee76626da0a379088cff6bb000
Duck Duck Go <duckduckgo@gmail.com>
2011-08-09 15:36:13 -0400

    Edits to make XKCD ready to go live

--- xkcd/spice.js
+++ xkcd/spice.js
@@ -1,22 +1,27 @@
 /*
-  assuming the function will named xkcd for now
+  nr is the prefix for this function space.
 */
-function xkcd(xk) {
+function nrxk(xk) {
   // console.log(xk);
 
   // validity check
-  if (xk['img'] && xk['alt']) {
+    if (xk['img'] && xk['alt']) {
     // snippet shown in 0-click
-    snippet = '<img src="' + xk['img'] + '" title="' + xk['alt'] + '" />';
-    items = new Array();
-    items[0] = new Array();
-    items[0]['a'] = snippet;
+	snippet = '<img src="' + xk['img'] + '" title="' + xk['alt'] + '" />';
+	items = new Array();
+	items[0] = new Array();
+	items[0]['a'] = snippet;
 
-    items[0]['h'] = '';
+	items[0]['h'] = xk['title'];
 
     // Source name and url for the More at X link.
-    items[0]['s'] = 'XKCD';
-    items[0]['u'] = 'http://xkcd.com/';
-    nxkcd(items);
-  }
+	items[0]['s'] = 'XKCD';
+	items[0]['u'] = 'http://xkcd.com/';
+
+	// Force no compression.
+	items[0]['f'] = 1;
+
+	// The rendering function is nra.
+	nra(items);
+    }
 }
