4962e540fdab312b4797c252150e4227dd71fdb5
Duck Duck Go <duckduckgo@gmail.com>
2011-08-11 09:26:25 -0400

    Fixed image size and added prev link for XKCD

--- xkcd/spice.js
+++ xkcd/spice.js
@@ -4,24 +4,42 @@
 function nrxk(xk) {
   // console.log(xk);
 
+    var img,snippet,link,div;
+
   // validity check
-    if (xk['img'] && xk['alt']) {
-    // snippet shown in 0-click
-	snippet = '<img src="' + xk['img'] + '" title="' + xk['alt'] + '" />';
-	items = new Array();
-	items[0] = new Array();
-	items[0]['a'] = snippet;
+   if (xk['img'] && xk['alt']) {
 
-	items[0]['h'] = xk['title'];
+       // snippet shown in 0-click
+       snippet = d.createElement('span');
 
-    // Source name and url for the More at X link.
-	items[0]['s'] = 'XKCD';
-	items[0]['u'] = 'http://xkcd.com/';
+       if (nur) img = nur('',xk['alt'],xk['img']);
+       if (img) {
+	   YAHOO.util.Dom.addClass(img,'cizb');
+	   div = d.createElement('div');
+	   div.appendChild(img);
+	   snippet.appendChild(div);
 
-	// Force no compression.
-	items[0]['f'] = 1;
-
-	// The rendering function is nra.
-	nra(items);
-    }
+	   link = d.createElement('a');
+	   link.href = '/?q=xkcd ' + (parseInt(xk['num']) - 1)
+	   link.appendChild(d.createTextNode('Prev'));
+	   snippet.appendChild(link);
+	   snippet.appendChild(d.createTextNode('\u00a0 \u00a0'));
+       }
+       
+       items = new Array();
+       items[0] = new Array();
+       items[0]['a'] = snippet;
+       
+       items[0]['h'] = xk['safe_title'];
+       
+       // Source name and url for the More at X link.
+       items[0]['s'] = 'XKCD';
+       items[0]['u'] = 'http://xkcd.com/' + xk['num'];
+       
+       // Force no compression.
+       items[0]['f'] = 1;
+       
+       // The rendering function is nra.
+       nra(items);
+   }
 }
