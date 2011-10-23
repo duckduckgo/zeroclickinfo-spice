ee78176745ffee890439384ba3bea4689c0f447e
eet-fuk <dutch109@gmail.com>
2011-10-23 12:18:13 +0200

    Added initial Flash version detection code

---
+++ flash_version/spice.js
@@ -0,0 +1,16 @@
+function get_flash_version() 
+{
+  var zc_data = []
+	zc_data[0] = []
+
+  zc_data[0]['h'] = "Your Flash Player version";  
+  
+  if(YAHOO.util.FlashDetect.installed) {
+    zc_data[0]['a'] = "Flash version: " + YAHOO.util.FlashDetect.major + "." + YAHOO.util.FlashDetect.minor + "." + YAHOO.util.FlashDetect.revision;
+  }
+  else {
+    zc_data[0]['a'] = "Flash is no installed";
+  }
+
+  nra(zc_data);
+}
