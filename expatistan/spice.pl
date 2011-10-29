42f11edb3d683e22c3e257f0b54c450556befec2
Gabriel Weinberg <yegg@caerus.duckduckgo.com>
2011-10-29 11:02:28 -0400

    attempt to integrate expatistan

--- expatistan/spice.pl
+++ expatistan/spice.pl
@@ -1,7 +1,6 @@
-if ($q_check_lc =~ /cost of living/i) {
-    $call_extf = qq(/js/nrexp102.js);
+if ($q_check_lc =~ /cost of living/) {
+    $call_extf = qq(/js/nrexp.js);
     if ($1) {
-			$call_ext = qq(/iexp/$1);
+	$call_ext = qq(/iexp/$1);
     }
-    $is_kill_pre_results = 1;
 }
