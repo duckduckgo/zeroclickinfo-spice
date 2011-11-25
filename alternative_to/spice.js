38fb59bd9cbde7eca048f1c9beed978dd5f8eaed
mr.Shu <mrshux@gmail.com>
2011-11-25 04:47:33 +0000

    first version of alternativeto.net spice

--- alternative_to/spice.js
+++ alternative_to/spice.js
@@ -1,23 +1,48 @@
+d = document;
 function nrat(alts) 
 {
-  console.log(alts);
+  // console.log(alts);
+  var out, tmp;
+  out = "";
   
   // validity check
   if (alts['Items']) {
 
-    var items = new Array();
-    var i = 0;
-
-    for (item in alts['Items']) {
+    for (i in alts['Items']) {
+      var item = alts['Items'][i];
       
-      items[i] = new Array();
-      items[i]['a'] = alts['Items'][i]['Name'];
+      wrapper = d.createElement("div");
+      box = d.createElement("div");
 
-      i++;
+      link = d.createElement("a");
+      link.href = item['Url'];
+      link.innerHTML = item['Name'];
+
+      img = d.createElement("img");
+      img.src = item['IconUrl'];
+      
+      box.appendChild(img);
+      box.appendChild(d.createElement("br"));
+      box.appendChild(link);
+      
+      YAHOO.util.Dom.setStyle(box, "text-align", "center");
+
+      YAHOO.util.Dom.setStyle(box, "float", "left");
+      YAHOO.util.Dom.setStyle(box, "margin", "10px");
+      YAHOO.util.Dom.setStyle(box, "width", "60px");
+
+      wrapper.appendChild(box);
+
+      out += wrapper.innerHTML;
+
     }
-
     
-    console.log(items[0]['a']);
+    items = new Array();
+    items[0] = new Array();
+    items[0]['a'] = out;
+    items[0]['h'] = '';
+    items[0]['s'] = 'AlternativeTo';
+    items[0]['u'] = alts['Url'];
     nra(items);
   }
 }
