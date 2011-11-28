7cb7eb687ba0f54ea415cd8357b8bbf2c01387ea
mr.Shu <mrshux@gmail.com>
2011-11-28 16:27:26 +0000

    Better regex for alternativeTo
    The regex should now be able to process queries like:
    
    -> free alternative to firefox for mac
    -> opensource omnigraffle
    -> TextMate for windows
    -> alternative to gmail

--- alternative_to/spice.pl
+++ alternative_to/spice.pl
@@ -1,8 +1,26 @@
-if ($q_check =~ /^alternatives? to ([\w\-]+)$/) {
-  #$call_extf = qq(/js/nrxk102.js);
-  if ($1) {
-    $call_ext = qq(/iat/software/$1);
-  }
-  $is_kill_pre_results = 1;
+if ($q_check_lc =~ /^(?:(free|opensource|commercial))?\s*(?:alternatives?\s*to\s*)?([\w\-]+)(?:\sfor\s(.*))?$/i) {
+    #$call_extf = qq(/js/nrat.js);
+    if ($1 and $4) {
+        # license and platform specified - queries like:
+        # -> free alternative to firefox for mac
+        # -> opensource matlab for linux
+        $call_ext = qq(/iat/software/$3/?platform=$4&license=$1);
+    } elsif ($1) {
+        # lincense secified only:
+        # -> free nod32
+        # -> opensource alternative to omnigraffle
+        $call_ext = qq(/iat/software/$3/?license=$1);
+    } elsif ($4) {
+        # platform specified:
+        # -> TextMate for windows
+        # -> alternative to vim for linux
+        $call_ext = qq(/iat/software/$3/?platform=$4);
+    } elsif($2) {
+        # license and platform not specified
+        # in this case we need to match 'alternative(s) to':
+        # -> alternative to firefox
+        $call_ext = qq(/iat/software/$3);
+    }   
+    $is_kill_pre_results = 1;
 }
 
