6af7ca3816c411c7db40152de4f859db835880cc
Wiriana Awiputra <rianawi@gmail.com>
2012-03-07 14:16:08 +0700

    added h, s, u info

--- lnm/spice.js
+++ lnm/spice.js
@@ -1,24 +1,18 @@
 function nrlnm(songs) {
-  var snippet = '';
+  var snippet = '',
+      heading = '';
 
-  // For debugging.
-  console.log(songs);
+  // console.log(songs);
 
-  // Validity check.
   if (songs.length > 0) {
 
-    // Snippet that gets shown in the 0-click box.
     s = songs[0];
-    snippet  = '<p>' + s.artist_name + ' - ' + s.title + ' Lyrics' + '</p>';
-    snippet += '<p>' + s.snippet.replace(/(\r\n){2}/g, ' // ').replace(/(\r\n)/g, ' / ').replace(/\s?\.{3}/, '') + '</p>';
+    heading = s.artist_name + ' - ' + s.title + ' (Lyrics)';
+    snippet = s.snippet.replace(/(\r\n){2}/g, ' // ').replace(/(\r\n)/g, ' / ').replace(/\s?\.{3}/, '');
 
-    items = new Array();
-    items[0] = new Array();
+    items = [[]];
+    items[0]['h'] = heading;
     items[0]['a'] = snippet;
-
-    items[0]['h'] = '';
-
-    // Source name and url for the More at X link.
     items[0]['s'] = 'LYRICSnMUSIC';
     items[0]['u'] = s.url;
 
