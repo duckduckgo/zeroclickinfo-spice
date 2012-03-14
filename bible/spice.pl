7ba26ed667e8ec23ab3224b057e211c0f8705efe
MithrandirAgain <mithrandiragain@lavabit.com>
2012-03-13 22:23:50 -0700

    Fixed bible bug (couldn't use numbers before chapter name.)

--- bible/spice.pl
+++ bible/spice.pl
@@ -1,4 +1,4 @@
-if ($q_check_lc =~ /^bible (\w+)$/) {
+if ($q_check_lc =~ /^bible(\s*)(\d*)(\s+)(\w+)(\s+)(\d+)(:+)(\d+)$/) {
     if ($1) {
     $call_extf = qq(/js/nrbi.js);
     $call_ext = qq(/ibi/$1);
