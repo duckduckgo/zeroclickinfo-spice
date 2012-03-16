2cda08c22550fdb99a3e64c03c740442f6c1008c
MithrandirAgain <mithrandiragain@lavabit.com>
2012-03-15 18:25:06 -0700

    Small regex error fix and 'bible' is not required anymore.

--- bible/spice.pl
+++ bible/spice.pl
@@ -1,7 +1,9 @@
-if ($q_check_lc =~ /^bible(\s*)(\d*)(\s+)(\w+)(\s+)(\d+)(:+)(\d+)$/) {
+#$q_check_lc = "1 someone 1:23";
+if ($q_check_lc =~ /^(bible\s)?(\d*)(\s*)(\w+)(\s+)(\d+)(:+)(\d+)$/) {
     if ($1) {
     $call_extf = qq(/js/nrbi.js);
     $call_ext = qq(/ibi/$1);
     }
     $is_kill_pre_results = 1;
+    #print "it works\n";
 }
