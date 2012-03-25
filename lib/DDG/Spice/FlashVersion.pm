2999bf4b5b9d662baad7813f4ba395519e826dd4
Hunter Lang <hunterlang@comcast.net>
2012-03-25 11:27:16 -0300

    fixed $_

--- lib/DDG/Spice/FlashVersion.pm
+++ lib/DDG/Spice/FlashVersion.pm
@@ -4,7 +4,7 @@ use DDG::Spice;
 triggers startend => "flash";
 
 handle query_lc => sub {
-    if ($q_check_lc eq 'flash version') {
+    if ($_ eq 'flash version') {
 	return qq(/js/nrfl.js);
 #	$is_kill_pre_results = 1;
     }
