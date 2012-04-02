64608c8d977798a2787f9b06fd22ff4b38e1bc49
hunterlang <hunterlang@comcast.net>
2012-04-02 17:58:18 -0400

    modify alternativeTo to match internal updates

--- lib/DDG/Spice/AlternativeTo.pm
+++ lib/DDG/Spice/AlternativeTo.pm
@@ -7,26 +7,26 @@ triggers any => "alternative","alternatives";
 
 handle query_lc => sub {
     if ($_ =~ /^(?:(free|opensource|commercial))?\s*(?:alternatives?\s*to\s*)?([\w\-]+)(?:\sfor\s(.*))?$/i) {
-        if ($1 and $4) {
+        if ($1 and $3) {
             # license and platform specified - queries like:
-            # -> free alternative to firefox for mac
+            # -> free alternative to firefox for mac
             # -> opensource matlab for linux
-            return qq(/iat/software/$3/?platform=$4&license=$1);
-        } elsif ($1) {
+            return qq(/iat/$2/platform=$3/license=$1);
+        } elsif ($1 and $2) {
             # lincense secified only:
-            # -> free nod32
+            # -> free nod32
             # -> opensource alternative to omnigraffle
-            return qq(/iat/software/$3/?license=$1);
-        } elsif ($4) {
+            return qq(/iat/$2/license=$1/);
+        } elsif ($3) {
             # platform specified:
             # -> TextMate for windows
             # -> alternative to vim for linux
-            return qq(/iat/software/$3/?platform=$4);
+            return qq(/iat/$2/platform=$3/);
         } elsif($2) {
             # license and platform not specified
             # in this case we need to match 'alternative(s) to':
             # -> alternative to firefox
-            return qq(/iat/software/$3);
+            return qq(/iat/$2/);
         }
     }
     return;
--- share/spice/alternative_to/spice.js
+++ share/spice/alternative_to/spice.js
@@ -1,49 +1,54 @@
-d = document;
 function nrat(alts) 
 {
-  // console.log(alts);
-  var out, tmp;
-  out = "";
+    var out, tmp, div, div2, link, img, item;
+    out = '';
   
-  // validity check
-  if (alts['Items']) {
+    // validity check
+    if (alts['Items']) {
+	for (i in alts['Items']) {
+	    item = alts['Items'][i];
 
-    for (i in alts['Items']) {
-      var item = alts['Items'][i];
+	    div = d.createElement("div");
+	    div2 = d.createElement("div");
+
+	    link = d.createElement("a");
+	    link.href = item['Url'];
+	    if (item['Name'].length >= 10) {
+		item['Name'] = item['Name'].substring(0,8) + "...";
+	    }
+	    img = d.createElement('img');
+	    img.src = '/iu/?u='+item['IconUrl'];
+	    YAHOO.util.Dom.setStyle(img, "margin", '0 auto 0 auto');
+	    YAHOO.util.Dom.setStyle(div,'margin-bottom', '10px');
+	    YAHOO.util.Dom.setStyle(div,'text-align', 'center');
+	    link.appendChild(img);
+	    div.appendChild(link);
+
+	    link = d.createElement('a');
+	    link.href = item['Url'];
+	    link.innerHTML = item['Name'];
+	    div.appendChild(link);
+	    div.appendChild(d.createElement('br'));
       
-      wrapper = d.createElement("div");
-      box = d.createElement("div");
-
-      link = d.createElement("a");
-      link.href = item['Url'];
-      link.innerHTML = item['Name'];
-
-      img = d.createElement("img");
-      img.src = item['IconUrl'];
-      
-      box.appendChild(img);
-      box.appendChild(d.createElement("br"));
-      box.appendChild(link);
-      
-      YAHOO.util.Dom.setStyle(box, "text-align", "center");
-
-      YAHOO.util.Dom.setStyle(box, "float", "left");
-      YAHOO.util.Dom.setStyle(box, "margin", "10px");
-      YAHOO.util.Dom.setStyle(box, "width", "60px");
-
-      wrapper.appendChild(box);
-
-      out += wrapper.innerHTML;
-
+	    YAHOO.util.Dom.addClass(div, 'inline highlight_zero_click1 highlight_zero_click_wrapper');
+	    YAHOO.util.Dom.setStyle(div, "float", "left");
+	    YAHOO.util.Dom.setStyle(div, "margin", "10px 20px 10px 0px");
+	    YAHOO.util.Dom.setStyle(div, "padding", "5px");
+	    YAHOO.util.Dom.setStyle(div, "max-width", "80px");
+	    
+	    div2.appendChild(div);
+	    out += div2.innerHTML;
+	}
     }
-    
-    items = new Array();
-    items[0] = new Array();
-    items[0]['a'] = out;
-    items[0]['h'] = '';
-    items[0]['s'] = 'AlternativeTo';
-    items[0]['u'] = alts['Url'];
-    nra(items);
-  }
+    if (out) {
+	// @yegg likes it w/out the newline :)
+	// out += '<div class="clear"></div>';
+	items = new Array();
+	items[0] = new Array();
+	items[0]['a'] = out;
+	items[0]['h'] = '';
+	items[0]['s'] = 'AlternativeTo';
+	items[0]['u'] = alts['Url'];
+	nra(items,1,1);
+    }
 }
-
