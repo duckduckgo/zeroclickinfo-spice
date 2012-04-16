fbc91d0f7209b2844274f1eea8cb6f72905b549d
Zaahir Moolla <moollaza@gmail.com>
2012-04-16 00:33:41 -0400

    Rotten Tomatoes Spice Plugin v1

--- lib/DDG/Spice/Xkcd.pm
+++ lib/DDG/Spice/Xkcd.pm
@@ -5,13 +5,13 @@ use DDG::Spice;
 triggers startend => "xkcd";
 
 handle query_lc => sub {
-    if ($_ eq 'xkcd' || $_ =~ /^xkcd (\d+)$/) {
-	if ($1) {
-	    $return = qq(/ixk/$1);
-	} else {
-	    $return = qq(/ixk);
-	}
+  if ($_ eq 'xkcd' || $_ =~ /^xkcd (\d+)$/) {
+    if ($1) {
+        $return = qq(/ixk/$1);
+    } else {
+        $return = qq(/ixk);
     }
+  }
 };
 
 1;
