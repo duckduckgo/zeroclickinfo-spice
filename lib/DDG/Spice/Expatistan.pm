9fe7522f7a7673b025fd92d383e1dc8fff03702b
hunterlang <hunterlang@comcast.net>
2012-04-22 15:16:14 -0400

    revert to old url

--- lib/DDG/Spice/Expatistan.pm
+++ lib/DDG/Spice/Expatistan.pm
@@ -10,10 +10,8 @@ sub nginx_conf {
     $nginx_conf = <<"__END_OF_CONF__";
 
 location ^~ /js/spice/expatistan/ {
-    echo_before_body 'ddg_spice_expatistan(';
-    rewrite ^/js/spice/expatistan/(.*) /api/?q=\$1&api_key=$api_key break;
+    rewrite ^/js/spice/expatistan/(.*) /api/spice?q=\$1&api_key=$api_key break;
     proxy_pass http://www.expatistan.com/;
-    echo_after_body ');';
 }
 
 __END_OF_CONF__
