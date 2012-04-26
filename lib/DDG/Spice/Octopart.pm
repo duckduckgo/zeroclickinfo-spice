d27d844c0c7342b6fadb637afb2b2d8f083844c6
bnewbold <bryan@octopart.com>
2012-04-26 12:43:24 -0400

    tweak octopart display; use apikey

--- lib/DDG/Spice/Octopart.pm
+++ lib/DDG/Spice/Octopart.pm
@@ -4,9 +4,19 @@ use DDG::Spice;
 
 triggers any => "datasheet";
 
-# no API key; will saturate ratelimiter unless host IPs are whitelisted
 # see: http://octopart.com/api/documentation
-spice to => 'https://www.octopart.com/api/v2/parts/search?callback={{callback}}&limit=3&optimize.hide_offers=1&optimize.hide_specs=1&q=$1'
+sub nginx_conf {
+    my $api_key = $ENV{DDG_SPICE_OCTOPART_APIKEY}; 
+    return unless defined $api_key;
+    $nginx_conf = <<"__END_OF_CONF__";
+
+location ^~ /js/spice/octopart/ {
+    rewrite ^/js/spice/octopart/(.*) /api/v2/parts/search?callback=ddg_spice_octopart&apikey=$api_key&limit=3&optimize.hide_offers=1&optimize.hide_specs=1&q=$1 break;
+    proxy_pass https://www.octopart.com/;
+}
+
+__END_OF_CONF__
+}
 
 handle query_lc => sub {
     if ($_ =~ /^\s*(?:(datasheet))?\s*([\w\-]+)(?:\s(datasheet))?\s*$/i) {
--- share/spice/octopart/spice.js
+++ share/spice/octopart/spice.js
@@ -1,3 +1,4 @@
+
 function ddg_spice_octopart(resp) {
     
     //console.log(resp);
@@ -16,36 +17,45 @@ function ddg_spice_octopart(resp) {
 
             // header is manufacturer name followed by MPN
             heading = "<b>" + part['manufacturer']['displayname'] + ' ' + part['mpn'] + "</b>";
+            
+            // Plain English sentence lead in [removed, use header instead]
+            //content += "The " + part['mpn'] + " is produced by " + part['manufacturer']['displayname'] + ". ";
 
-            // abstract is octopart-generated short description (up to ~160 characters)
+            // abstract is octopart-generated short description (up to ~160
+            // characters)
             if(part['short_description']) {
-                content += part['short_description'] + '<br><br>';
+                content += " Specs: <i>" + part['short_description'] + "</i>. ";
             }
+
+            // show market price and availability status [andres: edit this]
+            if(part['avg_price'].length > 0) {
+                if(part['avg_price'][0] < 0.01) {
+                    content += "Price below $0.01/each; ";
+                } else {
+                    content += "Price around $" + part['avg_price'][0].toFixed(2) + "/each; ";
+                }
+            } else {
+                content += "Price unknown; ";
+            }
+            content += part['market_status'].split(/:\ /)[1] + " ";
 
             // add top datasheet link if available
             if(part['datasheets'].length > 0) {
                 // replace this PDF favicon with a DDG-local one?
-                content += "<a href=\"" + part['datasheets'][0]['url'] + "\"><img src=\"http://n1.octostatic.com/o3/partsearch/partsearch/images/content/pdf_small.jpg\" style=\"float: left; margin-right: 3px;\"></img> Datasheet</a> | ";
+                content += "[<a href=\"" + part['datasheets'][0]['url'] + "\"><img src=\"http://n1.octostatic.com/o3/partsearch/partsearch/images/content/pdf_small.jpg\" style=\"display: inline; margin-right: 3px; height: 12px;\"></img>Datasheet</a>] ";
             } else {
-                content += "No Datasheet | "
+                content += "[No Datasheet] ";
             }
-
-            // show market price and availability status
-            if(part['avg_price'].length > 0) {
-                content += "Market Price $" + part['avg_price'][0].toFixed(2) + " | ";
-            } else {
-                content += "Market unknown | ";
-            }
-            content += part['market_status'].split(/:\ /)[1]
 
             // if we have a direct manufacturer URL use that, or else a link to
             // their homepage, or else nothing
             if(part['hyperlinks']['manufacturer']) {
-                content += "<br>More info from <a href=\"" + part['hyperlinks']['manufacturer'] + "\">" + part['manufacturer']['displayname'] + "</a>";
+                content += "[<a href=\"" + part['hyperlinks']['manufacturer'] + "\">Manufacturer</a>] ";
             } else if(part['manufacturer']['homepage_url']) {
-                content += "<br><a href=\"" + part['manufacturer']['homepage_url'] + "\">" + part['manufacturer']['displayname'] + " Homepage</a>";
+                content += "[<a href=\"" + part['manufacturer']['homepage_url'] + "\">Manufacturer</a>] ";
             }
 
+            // attach info
             items[i]['h'] = heading;
             items[i]['a'] = content;
 
