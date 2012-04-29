d3daa394e2f6e77babc09dbdfb57e93e3d7ae75e
mr.Shu <mrshux@gmail.com>
2012-04-29 20:25:48 +0200

    added also 'is X down?' option

--- lib/DDG/Spice/IsItUp.pm
+++ lib/DDG/Spice/IsItUp.pm
@@ -4,7 +4,7 @@ use DDG::Spice;
 
 spice is_cached => 0;
 
-triggers query_lc => qr/^(:?is\s)?([a-z0-9\-\.]+\.[a-z]{2,4})(:?\sup\s*(:?\?)?)?$/;
+triggers query_lc => qr/^(:?is\s)?([a-z0-9\-\.]+\.[a-z]{2,4})(:?\s(up|down)\s*(:?\?)?)?$/;
 
 spice to => 'http://isitup.org/$1.json';
 
