0d31ace98157800758e209befa10bde0a5ac9aee
mr.Shu <mrshux@gmail.com>
2012-04-29 20:24:06 +0200

    better response

--- share/spice/is_it_up/spice.js
+++ share/spice/is_it_up/spice.js
@@ -1,9 +1,23 @@
 function ddg_spice_is_it_up(res) 
 {
     var out = '';
+    
+    var responses = new Array();
+    responses[1] = "1xx_Informational";
+    responses[2] = "2xx_Success";
+    responses[3] = "3xx_Redirection";
+    responses[4] = "4xx_Client_Error";
+    responses[5] = "5xx_Server_Error";
+
     if (res['status_code'] === 1) {
         out = "<a href='http://" + res['domain'] +"'>" + res['domain'] + "</a> is up. <br />";
-        out += "It took " + res['response_time'] + " seconds to get " + res['response_code'] + " response form " + res['response_ip'];
+        out += "It took " + res['response_time'] + " seconds to get ";
+
+        out += "<a href='http://en.wikipedia.org/wiki/List_of_HTTP_status_codes#" +
+                responses[Math.floor(res['response_code'] / 100)] + 
+                "'>" + res['response_code'] + "</a>";
+
+        out += " response form " + res['response_ip'];
     } else {
         out = "<a href='http://" + res['domain'] +"'>" + res['domain'] + "</a> seems to be down.";
     } 
