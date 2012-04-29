cb80faf407232435b14049ebced92dd423b5112b
mr.Shu <mrshux@gmail.com>
2012-04-29 19:48:13 +0200

    added IsItUp

---
+++ lib/DDG/Spice/IsItUp.pm
@@ -0,0 +1,20 @@
+package DDG::Spice::IsItUp;
+
+use DDG::Spice;
+
+spice is_cached => 0;
+
+triggers query_lc => qr/^(:?is\s)?([a-z0-9\-\.]+\.[a-z]{2,4})(:?\sup\s*(:?\?)?)?$/;
+
+spice to => 'http://isitup.org/$1.json';
+
+handle matches => sub {
+    if ($1 && $2 && $3) {
+        return $2;
+    } elsif ($2 && $3) {
+        return $2;
+    }
+    return;
+};
+
+1;
