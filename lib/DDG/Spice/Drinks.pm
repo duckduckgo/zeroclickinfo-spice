03e1c9a069dd4a6b1a17d7a5407c1d7c4debe1e5
Scott Gusler <sdgusler@gmail.com>
2012-05-02 19:14:39 -0400

    compacted regex a bit

--- lib/DDG/Spice/Drinks.pm
+++ lib/DDG/Spice/Drinks.pm
@@ -7,25 +7,10 @@ triggers any => "drink", "drink mix", "drink recipe", "mixed drink", "drink ingr
 spice to => 'http://www.drinkproject.com/api/?type=json&name=$1&callback=ddg_spice_drinks';
 
 handle query_lc => sub {
-    if ($_ =~ /^drink\s+([0-9a-z #]+)$/i) {
-            return $1 if $1;
+    if (/^(mixed\s+)*drink(\s+recipe)*\s+([0-9a-z #]+)$) {
+            return $3 if $3;
     }
-    if ($_ =~ /^([0-9a-z #]+)\s+drink mix$/i) {
-            return $1 if $1;
-    }
-    if ($_ =~ /^([0-9a-z #]+)\s+mixed drink$/i) {
-            return $1 if $1;
-    }
-    if ($_ =~ /^([0-9a-z #]+)\s+drink recipe$/i) {
-            return $1 if $1;
-    }
-    if ($_ =~ /^([0-9a-z #]+)\s+drink ingredients$/i) {
-            return $1 if $1;
-    }
-    if ($_ =~ /^mixed drink\s([0-9a-z #]+)$/i) {
-            return $1 if $1;
-    }
-    if ($_ =~ /^drink recipe\s+([0-9a-z #]+)$/i) {
+    if (/^([0-9a-z #]+)\s+(drink|mixed) (drink|mix|recipe|ingredients)$) {
             return $1 if $1;
     }
     return;
