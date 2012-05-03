d4b0386b4c83494446bb1c2df6f04a98e5e3eec8
mr.Shu <mrshux@gmail.com>
2012-05-03 18:30:33 +0200

    added '.com' assumed feature

--- lib/DDG/Spice/IsItUp.pm
+++ lib/DDG/Spice/IsItUp.pm
@@ -4,15 +4,19 @@ use DDG::Spice;
 
 spice is_cached => 0;
 
-triggers query_lc => qr/^(:?is\s)?([a-z0-9\-\.]+\.[a-z]{2,4})(:?\s(up|down|working)\s*(:?\?)?)?$/;
+triggers query_lc => qr/^(:?is\s)?([a-z0-9\-\.]+(:?\.[a-z]{2,4})?)(:?\s(up|down|working)\s*(:?\?)?)?$/;
 
 spice to => 'http://isitup.org/$1.json?callback={{callback}}';
 
 handle matches => sub {
-    if ($1 && $2 && $3) {
+    if ($1 && $2 && $4) {
         return $2;
-    } elsif ($2 && $3) {
-        return $2;
+    } elsif ($2 && $4) {
+        if ($2 =~ /.com$/) {
+            return $2;
+        } else {
+            return $2 . '.com';
+        }
     }
     return;
 };
