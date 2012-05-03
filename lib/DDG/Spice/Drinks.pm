8e11ef60897cedf79d7ad78007464bbfab007eca
Scott Gusler <sdgusler@gmail.com>
2012-05-02 20:40:05 -0400

    updated regex

--- lib/DDG/Spice/Drinks.pm
+++ lib/DDG/Spice/Drinks.pm
@@ -7,7 +7,7 @@ triggers any => "drink", "mix", "recipe", "ingredients";
 spice to => 'http://www.drinkproject.com/api/?type=json&name=$1&callback=ddg_spice_drinks';
 
 handle query_lc => sub {
-    if (/^((((making)+|(how\sto\s(make|mix)+)+)+(\s(a|an|the))*)|(mixed\s+)*drink(\s+(recipe|mix))*)+\s+([0-9a-z #]+)$/) {
+    if (/^((((making)+|(how\sto\s(make|mix)+)+)+(\s(a|an|the)*)*)|(mixed\s+)*drink(\s+(recipe|mix))*)+\s+([0-9a-z #]+)$/) {
             return $12 if $12;
     }
     if (/^([0-9a-z #]+)\s+(drink|mixed)\s(drink|mix|recipe|ingredients)$/) {
