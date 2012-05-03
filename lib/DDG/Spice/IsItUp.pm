0a14a38b7ed9ab8cd682cd5211f005c10bad22a4
mr.Shu <mrshux@gmail.com>
2012-05-03 18:02:20 +0200

    added callback

--- lib/DDG/Spice/IsItUp.pm
+++ lib/DDG/Spice/IsItUp.pm
@@ -6,7 +6,7 @@ spice is_cached => 0;
 
 triggers query_lc => qr/^(:?is\s)?([a-z0-9\-\.]+\.[a-z]{2,4})(:?\s(up|down|working)\s*(:?\?)?)?$/;
 
-spice to => 'http://isitup.org/$1.json';
+spice to => 'http://isitup.org/$1.json?callback={{callback}}';
 
 handle matches => sub {
     if ($1 && $2 && $3) {
