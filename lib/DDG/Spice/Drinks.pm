dc6b9b7d1900c929a642fa5c3c79a9bf2845341f
Scott Gusler <sdgusler@gmail.com>
2012-05-02 20:33:43 -0400

    added some slashes so it actually works

--- lib/DDG/Spice/Drinks.pm
+++ lib/DDG/Spice/Drinks.pm
@@ -7,10 +7,10 @@ triggers any => "drink", "mix", "recipe", "ingredients";
 spice to => 'http://www.drinkproject.com/api/?type=json&name=$1&callback=ddg_spice_drinks';
 
 handle query_lc => sub {
-    if (/^((((making)+|(how\sto\s(make|mix)+)+)+(\s(a|an|the))*)|(mixed\s+)*drink(\s+(recipe|mix))*)+\s+([0-9a-z #]+)$) {
+    if (/^((((making)+|(how\sto\s(make|mix)+)+)+(\s(a|an|the))*)|(mixed\s+)*drink(\s+(recipe|mix))*)+\s+([0-9a-z #]+)$/) {
             return $12 if $12;
     }
-    if (/^([0-9a-z #]+)\s+(drink|mixed)\s(drink|mix|recipe|ingredients)$) {
+    if (/^([0-9a-z #]+)\s+(drink|mixed)\s(drink|mix|recipe|ingredients)$/) {
             return $1 if $1;
     }
     return;
