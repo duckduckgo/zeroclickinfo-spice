380d21d734877e0522a9ac52a51c3e1e64ee2323
MithrandirAgain <mithrandiragain@lavabit.com>
2012-05-03 16:49:15 -0700

    removed reddit link function

--- lib/DDG/Spice/Reddit.pm
+++ lib/DDG/Spice/Reddit.pm
@@ -4,7 +4,7 @@ use DDG::Spice;
 
 triggers start => '/r/', 'r/', 'reddit', 'subreddit';
 
-spice to => "http://www.reddit.com/r/$1/about.json?jsonp=ddg_spice_reddit", "http://www.reddit.com/r/$1/.json?jsonp=link_parse&limit=1";
+spice to => "http://www.reddit.com/r/$1/about.json?jsonp=ddg_spice_reddit";
 
 handle query_lc => sub {
     if ($_ =~ /^(reddit|subreddit)\s+(\w+)$/i || $_ =~ /^(\/?r\/)+(\w+)$/) {
--- share/spice/reddit/spice.js
+++ share/spice/reddit/spice.js
@@ -31,43 +31,9 @@ function ddg_spice_reddit(re) {
         items[0]["s"] = "Reddit";
         items[0]["u"] = "http://www.reddit.com" + url;
         items[0]['i'] = "https://icons.duckduckgo.com/i/www.reddit.com.ico";
-
-        //console.log(content);
+	
+	//console.log(content);
+	nra(items);       
     }
         
-};
-
-function link_parse(li) {
-    if (typeof items == 'undefined') {
-        return; // The subreddit is NSFW.
-    }
-    if (li['data']['children'][0]) {
-        //console.log(root["title"]);
-        var root = li['data']['children'][0].data;
-        var content, title, url, permalink, score, author, created, date;
-        var one_day = 1000*60*60*24; // One day in ms.
-        var today = new Date();
-        title = root['title'];
-        url = root['url'];
-        permalink = root['permalink'];
-        score = root['score'];
-        author = root['author'];
-        created = new Date(root['created_utc']*1000);
-        date = Math.floor((today.getTime()-created.getTime())/(one_day));
-        date = (date === 0) ? Math.floor((today.getTime()-created.getTime())/(one_day/24)) + " hours ago" : date + " days ago";
-        if (root['over_18'] === true) {
-            title += " (NSFW)";
-        }
-
-        content = "<div class='subreddit_link'><i>Popular Link:</i> ";
-        content += "<a href='"+url+"'>"+title+"</a>"+" by <a href='http://www.reddit.com/user/"+author+"'>\
-"+author+"</a> posted "+date+" with "+score+" karma. ";
-        content += "(<a href='http://www.reddit.com"+permalink+"'>Discuss</a>)</div>";
-
-        items[0]["a"] += content;
-
-    }
-
-    nra(items);
-
 };
