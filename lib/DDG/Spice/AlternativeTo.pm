b82ab96f2080dabd85ff41d29547eefc00dec986
hunterlang <hunterlang@comcast.net>
2012-05-03 22:00:16 -0400

    fix the fix

--- lib/DDG/Spice/AlternativeTo.pm
+++ lib/DDG/Spice/AlternativeTo.pm
@@ -5,8 +5,8 @@ use DDG::Spice;
 triggers start => "free","opensource","commercial";
 triggers any => "alternative","alternatives";
 
-spice from => '([^/]+)/(?:([^/]+)/(?:([^/]+)|)|)';
-spice to => 'http://api.alternativeto.net/software/$license/?$2&$platform&count=6&callback={{callback}}';
+spice from => '([^/]+)/?(?:([^/]+)/?(?:([^/]+)|)|)';
+spice to => 'http://api.alternativeto.net/software/$1/?platform=$2&license=$3&count=6&callback={{callback}}';
 
 my %alternatives = (
     'google' => 'googlecom',
@@ -22,22 +22,22 @@ handle query_lc => sub {
         $prog =~ s/^\s+//g;
         $prog =~ s/\s+/-/g;
         $prog = $alternatives{$prog} if exists $alternatives{$prog};
-
-        if ($license && $prog) {
+	
+        if ($license && $prog && $platform) {
             # license and platform specified - queries like:
             # -> free alternative to firefox for mac
             # -> opensource matlab for linux
-            return $prog, "platform=".$platform, "license=".$license;
+            return $prog, $platform, $license;
         } elsif ($license && $prog) {
             # lincense secified only:
             # -> free nod32
             # -> opensource alternative to omnigraffle
-            return $prog, "license=".$license;
-        } elsif ($platform) {
+            return $prog, $license;
+        } elsif ($platform && $prog) {
             # platform specified:
             # -> TextMate for windows
             # -> alternative to vim for linux
-            return $prog, "platform=".$platform;
+            return $prog, $platform;
         } elsif($prog) {
             # license and platform not specified
             # in this case we need to match 'alternative(s) to':
