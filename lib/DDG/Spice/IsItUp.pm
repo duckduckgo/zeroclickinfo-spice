c7bb91e0129be4a5fde0d00c60b29ce4d54002b8
hunterlang <hunterlang@comcast.net>
2012-05-04 15:59:23 -0400

    minor regex/perl simplification for isitup

--- lib/DDG/Spice/IsItUp.pm
+++ lib/DDG/Spice/IsItUp.pm
@@ -4,21 +4,14 @@ use DDG::Spice;
 
 spice is_cached => 0;
 
-triggers query_lc => qr/^(:?is\s)?([a-z0-9\-\.]+(:?\.[a-z]{2,4})?)(:?\s(up|down|working)\s*(:?\?)?)?$/;
+triggers query_lc => qr/^(?:is\s|)([0-9a-z\-]+)(?:(\.[a-z]{2,4})|)\s(?:up|down|working)/;
 
 spice to => 'http://isitup.org/$1.json?callback={{callback}}';
 
 handle matches => sub {
-    if ($1 && $2 && $4) {
-        return $2;
-    } elsif ($2 && $4) {
-        if ($2 =~ /.com$/) {
-            return $2;
-        } else {
-            return $2 . '.com';
-        }
-    }
+    return $2 ? $1.$2 : $1.'.com';
     return;
 };
 
 1;
+
