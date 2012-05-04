946434b1cfb7d54f58b8467877675ae46b9e645a
root <root@main.drinkproject.com>
2012-05-03 22:41:26 -0400

    updated triggers & regex

--- lib/DDG/Spice/Drinks.pm
+++ lib/DDG/Spice/Drinks.pm
@@ -2,12 +2,13 @@ package DDG::Spice::Drinks;
 
 use DDG::Spice;
 
-triggers any => "drink", "mix", "recipe", "ingredients";
+triggers any => "drink", "make", "mix", "recipe", "ingredients";
+triggers start => "mixing", "making";
 
 spice to => 'http://www.drinkproject.com/api/?type=json&name=$1&callback=ddg_spice_drinks';
 
 handle query_lc => sub {
-    if (/^((((making)+|(how\sto\s(make|mix)+)+)+(\s(a|an|the)*)*)|(mixed\s+)*drink(\s+(recipe|mix))*)+\s+([0-9a-z #]+)$/) {
+    if (/^((((making|mixing)+|(how\sto\s(make|mix)+)+)+(\s(a|an|the)*)*)|(mixed\s+)*drink(\s+(recipe|mix))*)+\s+([0-9a-z #]+)$/) {
             return $12 if $12;
     }
     if (/^([0-9a-z #]+)\s+(drink|mixed)\s(drink|mix|recipe|ingredients)$/) {
