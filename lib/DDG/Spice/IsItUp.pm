83236969e7019c32ab1145b3869c2f28d366f29a
hunterlang <hunterlang@comcast.net>
2012-05-07 15:23:58 -0400

    fix false positives for isitup

--- lib/DDG/Spice/IsItUp.pm
+++ lib/DDG/Spice/IsItUp.pm
@@ -4,14 +4,12 @@ use DDG::Spice;
 
 spice is_cached => 0;
 
-# false positive: scrounges up
-# in response forcing is, but may want to only check for is if no domain extension
-triggers query_lc => qr/^(?:is\s)([0-9a-z\-]+)(?:(\.[a-z]{2,4})|)\s(?:up|down|working)/;
+triggers query_lc => qr/^((?:is\s|))([0-9a-z\-]+)(?:(\.[a-z]{2,4})|)\s(?:up|down|working)/;
 
 spice to => 'http://isitup.org/$1.json?callback={{callback}}';
 
 handle matches => sub {
-    return $2 ? $1.$2 : $1.'.com';
+    return $1 ? ($3 ? $2.$3 : $2.'.com') : $2;
     return;
 };
 
