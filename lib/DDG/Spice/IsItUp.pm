b2bb937105ac4664009674500299bbaf0f9224a6
hunterlang <hunterlang@comcast.net>
2012-05-07 15:36:17 -0400

    this would be easier without ternaries, but now i'm committed

--- lib/DDG/Spice/IsItUp.pm
+++ lib/DDG/Spice/IsItUp.pm
@@ -9,7 +9,7 @@ triggers query_lc => qr/^((?:is\s|))([0-9a-z\-]+)(?:(\.[a-z]{2,4})|)\s(?:up|down
 spice to => 'http://isitup.org/$1.json?callback={{callback}}';
 
 handle matches => sub {
-    return $1 ? ($3 ? $2.$3 : $2.'.com') : $2;
+    return $1 ? ($3 ? $2.$3 : $2.'.com') : ($3 ? $2.$3 : $2);
     return;
 };
 
