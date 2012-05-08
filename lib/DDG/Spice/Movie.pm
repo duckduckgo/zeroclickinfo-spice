53427f79cd9285f25168ced50d6931ebfe9bd237
moollaza <moollaza@gmail.com>
2012-05-08 15:43:09 -0400

    fixed a few small typos

--- lib/DDG/Spice/Movie.pm
+++ lib/DDG/Spice/Movie.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Movie;
 
 use DDG::Spice;
 
-spice to => 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=ccw2b5ce8dsy7sb3x2qxmn3x&q=$1&page_limit=1&page=1&callback={{callback}}'
+spice to => 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=ccw2b5ce8dsy7sb3x2qxmn3x&q=$1&page_limit=1&page=1&callback={{callback}}';
 
 triggers startend => "movie";
 
