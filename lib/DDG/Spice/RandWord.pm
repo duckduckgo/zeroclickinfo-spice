ceed6d40e167bfdb03c90c226bace5870596333d
hunterlang <hunterlang@comcast.net>
2012-05-12 11:55:36 -0400

    update spice to use DDG::Rewrite params

--- lib/DDG/Spice/RandWord.pm
+++ lib/DDG/Spice/RandWord.pm
@@ -1,21 +1,12 @@
 package DDG::Spice::RandWord;
+
 use DDG::Spice;
 
 spice is_cached => 0;
 
-sub nginx_conf {
-    my $api_key = $ENV{DDG_SPICE_RANDWORD_APIKEY}; 
-    return unless defined $api_key;
-    return <<"__END_OF_CONF__";
-
-location ^~ /js/spice/rand_word/ {
-    rewrite ^/js/spice/rand_word/(?:([0-9]+)\-([0-9]+)|) /v4/words.json/randomWord?minLength=\$1&maxLength=\$2&api_key=$api_key&callback=ddg_spice_rand_word break;
-    proxy_pass http://api.wordnik.com/;
-    proxy_cache_valid 418 1d;
-}
-
-__END_OF_CONF__
-}
+spice from => '(?:([0-9]+)\-([0-9]+)|)';
+spice to => 'http://api.wordnik.com/v4/words.json/randomWord?minLength=$1&maxLength=$2&api_key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
+spice proxy_cache_valid => "418 1d";
 
 triggers any => "random", "word";
 
