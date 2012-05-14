092c1d422eb85899d642eacc1c4fac36593acece
Zachary Owen <dev@scrptr.com>
2012-05-13 20:43:38 -0400

    created new branch synonyms

--- share/spice/synonyms/spice.js
+++ share/spice/synonyms/spice.js
@@ -14,7 +14,7 @@ function ddg_spice_synonyms(synonyms) {
       for(var i=0;i< synonyms.verb.syn.length; i++) {
         content += synonyms.verb.syn[i] + ", ";
       }
-      content = content.substr(0, content.length -2);
+      content = content.substr(0, content.length - 2);
     }
 
     var heading = "Synonyms";// of " + word;
@@ -22,6 +22,14 @@ function ddg_spice_synonyms(synonyms) {
     /* There was no way to include an extra parameter in the callback
      * function -- so I can't display the search term. BigHugeAPI does
      * not currently return search request. 
+     * STANDS4 API -- only returns xml, less than ideal
+     * Wiktionary/Wikimedia -- would have taken 2 requests or could have
+     * been a fathead (but this plugin was listed as spice by ddg admin)
+     * thesaurus.altervista.org -- didnt return request info either, also
+     * seemed slow
+     * dictionary.com -- would have taken up to 30 days to get an api key
+     * and very little ifnromation about requests/day and cost
+     * 
      */
 
     items = [[]];
