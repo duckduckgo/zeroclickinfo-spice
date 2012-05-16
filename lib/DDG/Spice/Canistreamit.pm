9c6d29b002285ac9e84ea9e0fe2aa44b34ff3c4b
CanIStreamIt <canistreamit@gmail.com>
2012-05-15 19:07:20 -0700

    attribution.

--- lib/DDG/Spice/Canistreamit.pm
+++ lib/DDG/Spice/Canistreamit.pm
@@ -2,6 +2,10 @@ package DDG::Spice::Canistreamit;
 
 use DDG::Spice;
 
+attribution twitter = 'CanIStreamIt'
+attribution website => ['http://canistream.it','CanIStream.It']
+attribution email => ['canistreamit@gmail.com','CanIStream.It']
+
 spice to => 'http://www.canistream.it/ddg/query/$1?callback={{callback}}';
 
 triggers any => "stream", "watch", "streaming";
