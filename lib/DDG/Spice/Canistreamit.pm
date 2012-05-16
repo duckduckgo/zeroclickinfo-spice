a66e62acac242ee7662726b8e72c4ca0724d5245
Michael Smith <crazedpsyc@duckduckgo.com>
2012-05-15 19:54:52 -0600

    oops, that did not make sense

--- lib/DDG/Spice/Canistreamit.pm
+++ lib/DDG/Spice/Canistreamit.pm
@@ -7,8 +7,7 @@ spice to => 'http://www.canistream.it/ddg/query/$1?callback={{callback}}';
 triggers any => "stream", "watch", "streaming";
 
 handle remainder => sub {
-    s/^(?:can\s*i?|how\s*to|where\s*(?:to|can\s+i))\s*(.+)$/$1/i;
-    s/\?$//;
+    return unless /^(?:can\s*i?|how\s*to|where\s*(?:to|can\s+i))\s*(.+)\??$/i;
     return $_;
 };
 
