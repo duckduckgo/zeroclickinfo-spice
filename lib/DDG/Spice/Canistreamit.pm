81768d4bb4bf11fa920ad2f9e0cb4d90c2ee4712
CanIStreamIt <canistreamit@gmail.com>
2012-05-15 19:13:34 -0700

    Fixed attribution.

--- lib/DDG/Spice/Canistreamit.pm
+++ lib/DDG/Spice/Canistreamit.pm
@@ -2,9 +2,9 @@ package DDG::Spice::Canistreamit;
 
 use DDG::Spice;
 
-attribution twitter = 'CanIStreamIt'
-attribution website => ['http://canistream.it','CanIStream.It']
-attribution email => ['canistreamit@gmail.com','CanIStream.It']
+attribution twitter => 'CanIStreamIt',
+            web => ['http://canistream.it','CanIStream.It'],
+            email => ['canistreamit@gmail.com','CanIStream.It'];
 
 spice to => 'http://www.canistream.it/ddg/query/$1?callback={{callback}}';
 
