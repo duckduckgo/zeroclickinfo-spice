15c7ad0efd81b1dd4c6d7db5c33d684916505459
Zachary Owen <dev@scrptr.com>
2012-05-21 20:39:19 -0400

    created abstract function to handle all forms. is it me or is it dry in here?

--- lib/DDG/Spice/Synonyms.pm
+++ lib/DDG/Spice/Synonyms.pm
@@ -6,10 +6,10 @@ use DDG::Spice;
 my $api_key = $ENV{"BHUGE_KEY"};
 spice to => 'http://words.bighugelabs.com/api/2/${api_key}/$1/json?callback=ddg_spice_synonyms';
 
-triggers startend => "synonyms", "synonym";
+triggers startend => "synonyms", "synonym", "antonyms", "antonym", "related", "similar";
 
 handle remainder => sub {
-    return $_;
+  return $_;
 }
 
 1;
