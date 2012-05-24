be12f8d9f379c82fd430ea752e12753a5dd7e727
Zachary Owen <dev@scrptr.com>
2012-05-23 21:30:06 -0400

    forgot to rename abstract function..sorry getty

--- share/spice/synonyms/spice.js
+++ share/spice/synonyms/spice.js
@@ -1,23 +1,22 @@
 function ddg_spice_antonym(antonyms) {
-  ddg_spice_abstract(antonyms, 'ant', 'Antonyms');
+  ddg_spice_varinym(antonyms, 'ant', 'Antonyms');
 }
 function ddg_spice_related(related) {
-  ddg_spice_abstract(related, 'rel', 'Related');
+  ddg_spice_varinym(related, 'rel', 'Related');
 }
 function ddg_spice_similar(similar) {
-  ddg_spice_abstract(similar, 'sim', 'Similar');
+  ddg_spice_varinym(similar, 'sim', 'Similar');
 }
 function ddg_spice_synonym(synonyms) {
-  ddg_spice_abstract(synonyms, 'syn', 'Synonyms');
+  ddg_spice_varinym(synonyms, 'syn', 'Synonyms');
 }
 
-function ddg_spice_abstract(json, mode, heading) {
+function ddg_spice_varinym(json, mode, heading) {
   mode = encodeURI(mode);
   var code = "if(json) {var content='';if(json.noun) { if(json.noun." + mode + "){ " + "content+=get_content(json.noun." + mode + ", 'Nouns');}}if(json.verb){ if(json.verb." + mode + "){ " + "content+=get_content(json.verb." + mode + ", 'Verbs');}}if(json.adjective) { if(json.adjective." + mode + " ){ " + "content+=get_content(json.adjective." + mode + ", 'Adjectives');}}if(content.length > 0){build_items(content, heading);}}";
   eval(code);
 }
 function get_content(json, heading) {
-  //var content = "<strong style='font-weight:bold;'>" + heading + "</strong>: ";
   var content = "<b>" + heading + "</b>: ";
   for(var i=0;i<json.length;i++) {
     content += json[i] + ", ";
