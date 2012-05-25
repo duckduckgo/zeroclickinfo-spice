304438c5710ecce24a69be380fbd5a6675507270
Jag Talon <talon.jag@gmail.com>
2012-05-24 23:38:05 -0400

    DDG can now show the bio of Google+ users

--- share/spice/google_plus/spice.js
+++ share/spice/google_plus/spice.js
@@ -1,8 +1,10 @@
 function ddg_spice_google_plus(google) {
 	console.log(google);
-	var out, tmp, div, div2, link, img, item;
-	out = '';
-	if(google['items'].length > 0) {
+
+	var out = '';
+	if(google.kind === "plus#peopleFeed") {
+		var tmp, div, div2, link, img, item;
+
 		//Sometimes, the API returns a lot of results even if we
 		//asked for only five. (e.g. coke)
 		if(google['items'].length > 5) {
@@ -16,7 +18,7 @@ function ddg_spice_google_plus(google) {
 		    div2 = d.createElement("div");
 
 		    link = d.createElement("a");
-		    link.href = item.url;
+		    link.href = '/?q=google%2B+' + item.id;
 		    
 		    if (item.displayName.length >= 10) {
 				item.displayName = item.displayName.substring(0,8) + "...";
@@ -31,7 +33,7 @@ function ddg_spice_google_plus(google) {
 		    div.appendChild(link);
 
 		    link = d.createElement('a');
-		    link.href = item.url;
+		    link.href = '/?q=google%2B+' + item.id;
 		    link.innerHTML = item.displayName;
 		    div.appendChild(link);
 		    div.appendChild(d.createElement('br'));
@@ -47,14 +49,51 @@ function ddg_spice_google_plus(google) {
 		    out += div2.innerHTML;
 		}
 
-		items = new Array();
+		var items = new Array();
 		items[0] = new Array();
 		items[0]['a'] = out;
 		items[0]['h'] = '';
 		items[0]['s'] = 'Google+';
-		items[0]['u'] = 'http://plus.google.com';
 		items[0]['f'] = 1;
-		//items[0]['i'] = 'http://upload.wikimedia.org/wikipedia/commons/9/95/Google%2B_logo.png';
+		items[0]['u'] = 'http://plus.google.com';
+		nra(items,1,1);
+	} else if(google.kind === "plus#person") {
+		//Check if the user has a tagline
+		//Tagline or the about me page can be displayed here.
+		if (google.tagline) {
+			out += '<div class="google_profile"><i>Tagline:</i> ' + google.tagline + '</div>';
+		}
+
+		//Check for organizations
+		if(google.organizations) {
+			var orgs = '';
+			for(var i=0;i < google.organizations.length && i < 2;i++) {
+				orgs += google.organizations[i].name + 
+					(google.organizations[i].title ? ', ' + google.organizations[i].title : '') 
+					+ '<br>';
+			}
+			out += '<div class="google_orgs"><i>Organizations:</i><br>' + orgs + '</div>';
+		} 
+		//Check if the person has links to show.
+		if(google.urls) {
+			var links = '';
+			if(google.urls.length > 2) {
+				google.urls.length -= 2;
+				for(var i=0;i < google.urls.length && i < 2;i++) {
+					links += '<a href="' + google.urls[i].value + '">' + google.urls[i].value + 
+						'</a><br>';
+				}
+				out += '<div class="google_links"><i>Links:</i><br>' + links + '</div>';
+			}
+		}
+		var items = new Array();
+		items[0] = new Array();
+		items[0]['a'] = out += '<div></div>';
+		items[0]['h'] = google.displayName;
+		items[0]['s'] = google.displayName;
+		items[0]['u'] = google.url;
+		items[0]['f'] = 1;
+		items[0]['i'] =	google.image.url;
 		nra(items);
 	}
 }
