f8dd3c24bfa6982b778c49c0218e6db7f41e32ed
Jag Talon <talon.jag@gmail.com>
2012-05-24 23:38:05 -0400

    DDG can now show the bio of Google+ users

--- lib/DDG/Spice/GooglePlus.pm
+++ lib/DDG/Spice/GooglePlus.pm
@@ -1,15 +1,20 @@
 package DDG::Spice::GooglePlus;
-# ABSTRACT: Give the Google+ users matching the search item.
+# ABSTRACT: Search for Google+ users and return their bio.
 
 use DDG::Spice;
 
-spice to => 'https://www.googleapis.com/plus/v1/people?query=$1&maxResults=5&key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
+spice to => 'https://www.googleapis.com/plus/v1/people/$1?query=$2&fields=$3&key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
+spice from => '(?:([^/]*)/([^/]*)/([^/]*)|)';
 
 triggers startend => 'google+';
 
 handle remainder => sub {
-	return $_ if $_;
+	my $query = $_;
+	if($query =~ /(\d+)$/) {
+		return $1, '', 'birthday,currentLocation,displayName,emails,etag,gender,hasApp,id,image,kind,languagesSpoken,name,nickname,objectType,organizations,placesLived,relationshipStatus,tagline,url,urls';
+	} elsif($query =~ /((?:[a-zA-Z]|\s)+)$/) {
+		return '', $1, 'items,kind,nextPageToken,selfLink,title,totalItems';
+	}
 	return;
 };
-
 1;
