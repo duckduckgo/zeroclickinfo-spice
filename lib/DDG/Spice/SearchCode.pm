daa06714d221bb281966671360f3e896ea16b4cb
Ben Boyter <bboyte01@gmail.com>
2012-05-26 08:11:48 +0200

    Moved over to keyword triggers

--- lib/DDG/Spice/SearchCode.pm
+++ lib/DDG/Spice/SearchCode.pm
@@ -2,13 +2,15 @@ package DDG::Spice::SearchCode;
 
 use DDG::Spice;
 
-triggers query_lc => qr/(?:apache2\s*directive|apache|apache2|apple\s*ios|ios|brainfuck|clojure|cobol|emacs|fossil|ftp\s*code|git|hello\s*world|hresult|http\s*code|java|jquery|linux\s*command|linux\s*kernel\s*error|mercurial|hg|mysql\s*error|mysql\s*function|nt\s*statu\s|osx|perl|perl5|perl5\s*var|perl\s*var|php|python|smarty|snippet|sql\s*server\s*function|svn|underscore\.js|underscore|vb6|win32\s*error|windows\s*command) (.*)/;
+triggers start => "apache2 directive","apache","apache2","apple ios","ios","brainfuck","clojure","cobol","emacs","fossil","ftp code","git","hello world","hresult","http code","java","jquery","linux command","linux kernel error","mercurial","hg","mysql error","mysql function","nt status","osx","perl","perl5","perl5 var","perl var","php","python","smarty","snippet","sql server function","svn","underscore.js","underscore","vb6","win32 error","windows command";
 
 spice to => 'http://searchco.de/api/jsonp_search_III/$1?callback={{callback}}';
 
-handle matches => sub {
-    my ($match) = @_;
-    return $match if $match;
+handle query_raw => sub {
+    # check if there is anything to search for
+    if ($_ =~ qr/apache2\s*directive|apache|apache2|apple\s*ios|ios|brainfuck|clojure|cobol|emacs|fossil|ftp\s*code|git|hello\s*world|hresult|http\s*code|java|jquery|linux\s*command|linux\s*kernel\s*error|mercurial|hg|mysql\s*error|mysql\s*function|nt\s*status|osx|perl|perl5|perl5\s*var|perl\s*var|php|python|smarty|snippet|sql\s*server\s*function|svn|underscore\.js|underscore|vb6|win32\s*error|windows\s*command .+/i) {
+        return $_;
+    }
     return;
 };
 
