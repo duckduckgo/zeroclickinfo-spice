26afdc458b3ac0db4dd3b66801ee1bf1a6c12c1d
MithrandirAgain <mithrandiragain@lavabit.com>
2012-05-26 22:48:09 -0700

    reddit: bug fix and clean up

--- lib/DDG/Spice/Reddit.pm
+++ lib/DDG/Spice/Reddit.pm
@@ -2,14 +2,12 @@ package DDG::Spice::Reddit;
 
 use DDG::Spice;
 
-triggers start => '/r/', 'r/', 'reddit', 'subreddit';
+triggers query_lc => qr#^(?:reddit|subreddit|/r/|r/)\s*(?:/r/|r/)*(\w+)|subreddit\s+(\w+)#i;
+spice to => 'http://www.reddit.com/r/$1/about.json?jsonp=ddg_spice_reddit';
 
-spice to => "http://www.reddit.com/r/$1/about.json?jsonp=ddg_spice_reddit";
-
-handle query_lc => sub {
-    if ($_ =~ /^(reddit|subreddit)\s+(\w+)$/i || $_ =~ /^(\/?r\/)+(\w+)$/) {
-        return $1 if $1;
-}
+handle matches => sub {
+    my ($rname) = @_;
+    return $rname if $rname;
     return;
 };
 
