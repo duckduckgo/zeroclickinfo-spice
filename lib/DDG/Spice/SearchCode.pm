3ac24e840f25246904da8d5066d2027a9430f3e6
moollaza <moollaza@gmail.com>
2012-05-29 16:27:53 -0400

    Mostly finished SearchCode spice plugin:   --SearchCode.pm:     --> changed triggers to list of words     --> modified regex check to make sure there is text before or after trigger (allowing startend triggers to work)   --Spice.js:     --> cleaned up the code a little, no major changes

--- lib/DDG/Spice/SearchCode.pm
+++ lib/DDG/Spice/SearchCode.pm
@@ -2,13 +2,14 @@ package DDG::Spice::SearchCode;
 
 use DDG::Spice;
 
-triggers query_lc => qr/apache2\s*directive|apache|apache2|apple\s*ios|ios|brainfuck|clojure|cobol|emacs|fossil|ftp\s*code|git|hello\s*world|hresult|http\s*code|java|jquery|linux\s*command|linux\s*kernel\s*error|mercurial|hg|mysql\s*error|mysql\s*function|nt\s*status|osx|perl|perl5|perl5\s*var|perl\s*var|php|python|smarty|snippet|sql\s*server\s*function|svn|underscore\.js|underscore|vb6|win32\s*error|windows\s*command/;
+triggers startend => "apache2 directive","apache","apache2","apple ios","ios","brainfuck","clojure","cobol","emacs","fossil","ftp code","git","hello world","hresult","http code","java","jquery","linux command","linux kernel error","mercurial","hg","mysql error","mysql function","nt status","osx","perl","perl5","perl5 var","perl var","php","python","smarty","snippet","sql server function","svn","underscore.js","underscore","vb6","win32 error","windows command";
 
 spice to => 'http://searchco.de/api/jsonp_search_III/$1?callback={{callback}}';
 
 handle query_raw => sub {
     # check if there is anything to search for
-    if ($_ =~ qr/apache2\s*directive|apache|apache2|apple\s*ios|ios|brainfuck|clojure|cobol|emacs|fossil|ftp\s*code|git|hello\s*world|hresult|http\s*code|java|jquery|linux\s*command|linux\s*kernel\s*error|mercurial|hg|mysql\s*error|mysql\s*function|nt\s*status|osx|perl|perl5|perl5\s*var|perl\s*var|php|python|smarty|snippet|sql\s*server\s*function|svn|underscore\.js|underscore|vb6|win32\s*error|windows\s*command .+/i) {
+    my $words = 'apache2\s*directive|apache|apache2|apple\s*ios|ios|brainfuck|clojure|cobol|emacs|fossil|ftp\s*code|git|hello\s*world|hresult|http\s*code|java|jquery|linux\s*command|linux\s*kernel\s*error|mercurial|hg|mysql\s*error|mysql\s*function|nt\s*status|osx|perl|perl5|perl5\s*var|perl\s*var|php|python|smarty|snippet|sql\s*server\s*function|svn|underscore\.js|underscore|vb6|win32\s*error|windows\s*command';
+    if ($_ =~ qr/(\w+\s*$words|$words\s*\w+)/i) {
         return $_;
     }
     return;
--- share/spice/search_code/spice.js
+++ share/spice/search_code/spice.js
@@ -1,50 +1,50 @@
 function ddg_spice_search_code(data) {
+    if(data.results.length > 0) {
 
-	var snippet = []; // To store the results
-	var searchterm; // holds the search term
-	var resultscount = 1; // Number of results to display
-	var result; // holds the main result
+        var searchterm; // holds the search term
+        var result; // holds the main result
 
-	if(data.results.length > 0) {
-		result = data.results[0];
-		searchterm = data.query;
-	
-		var div = d.createElement('div');
-		var div2 = d.createElement('div');
-		YAHOO.util.Dom.addClass(div2,'zero_click_searchcode');
-			
-		div2.innerHTML = search_codeFormatZeroClick(result);
+        result = data.results[0];
+        searchterm = data.query;
+    
+        var div = d.createElement('div');
+        var div2 = d.createElement('div');
+        YAHOO.util.Dom.addClass(div2,'zero_click_searchcode');
+            
+        div2.innerHTML = search_codeFormatZeroClick(result);
 
-		div.appendChild(div2);
-			
-		snippet[0] = Array();
-		snippet[0]['a'] = div.innerHTML;
-		snippet[0]['h'] = search_codeFormatName(result);
-		snippet[0]['s'] = 'search[code]';
-		snippet[0]['u'] = 'http://searchco.de/?q='+encodeURIComponent(searchterm);
-			
-		// DDG rendering function is nra.
-		nra(snippet);
-	}
+        div.appendChild(div2);
+        
+        items = [[]];
+        items[0]['a'] = div.innerHTML;
+        items[0]['h'] = search_codeFormatName(result);
+        items[0]['s'] = 'search[code]';
+        items[0]['u'] = 'http://searchco.de/?q='+encodeURIComponent(searchterm);
+            
+        // DDG rendering function is nra.
+        nra(items);
+    }
 }
 
 function search_codeFormatName(result) {
-	if(result.namespace != '') {
-		return result.name + ' (' + result.namespace + ')';
-	}
-	return result.name;
+    if(result.namespace != '') {
+        return result.name + ' (' + result.namespace + ')';
+    } else {
+        return result.name;
+    }   
 }
 
 function search_codeFormatZeroClick(result) {
-	if(result.synopsis != '') {
-		return '<pre><code>' + search_codeStrip(result.synopsis) + '</code></pre>' + result.description + '<br>[<a href="'+result.url+'">Reference</a>]';
-	}
-	return result.description + '<br>[<a href="'+result.url+'">Reference</a>]';
+    if(result.synopsis != '') {
+        return '<pre><code>' + result.synopsis + '</code></pre>' + result.description + '<br>[<a href="'+result.url+'">Reference</a>]';
+    } else {
+        return result.description + '<br>[<a href="'+result.url+'">Reference</a>]';
+    }   
 }
 
 // This uses the browser to strip HTML, possibly YUI2 has better way to do this
 function search_codeStrip(html) {
-	var tmp = document.createElement("div");
-	tmp.innerHTML = html;
-	return tmp.textContent||tmp.innerText;
+    var tmp = document.createElement("div");
+    tmp.innerHTML = html;
+    return tmp.textContent||tmp.innerText;
 }
