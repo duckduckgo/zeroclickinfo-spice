5aab5f737693f0fa4f2c6d7200cd44878f2ce6ea
Jag Talon <talon.jag@gmail.com>
2012-05-29 20:09:39 -0400

    Used aboutMe instead of tagline

--- lib/DDG/Spice/GooglePlus.pm
+++ lib/DDG/Spice/GooglePlus.pm
@@ -3,17 +3,17 @@ package DDG::Spice::GooglePlus;
 
 use DDG::Spice;
 
-spice to => 'https://www.googleapis.com/plus/v1/people/$1?query=$2&fields=$3&key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
-spice from => '(?:([^/]*)/([^/]*)/([^/]*)|)';
+spice to => 'https://www.googleapis.com/plus/v1/people/$1?query=$2&key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
+spice from => '(?:([^/]*)/([^/]*)|)';
 
 triggers startend => 'google+';
 
 handle remainder => sub {
 	my $query = $_;
 	if($query =~ /(\d+)$/) {
-		return $1, '', 'birthday,currentLocation,displayName,emails,etag,gender,hasApp,id,image,kind,languagesSpoken,name,nickname,objectType,organizations,placesLived,relationshipStatus,tagline,url,urls';
+		return $1, ''; 
 	} elsif($query =~ /((?:[a-zA-Z]|\s)+)$/) {
-		return '', $1, 'items,kind,nextPageToken,selfLink,title,totalItems';
+		return '', $1; 
 	}
 	return;
 };
