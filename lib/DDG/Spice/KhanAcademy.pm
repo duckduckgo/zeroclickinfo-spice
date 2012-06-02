6a2645da605e23bd341e8b252ba8a0f7407c3035
Arlo Breault <arlolra@gmail.com>
2012-06-01 23:13:27 -0700

    some khan academy spice

---
+++ lib/DDG/Spice/KhanAcademy.pm
@@ -0,0 +1,21 @@
+package DDG::Spice::KhanAcademy;
+
+use DDG::Spice;
+
+spice to => join '&',
+    'https://gdata.youtube.com/feeds/api/videos?',
+    'author=khanacademy',
+    'max-results=1',
+    'alt=json-in-script',
+    'callback={{callback}}',
+    'v=2',
+    'q=$1';
+
+triggers query_lc => qr/^khan(\s+academy)?\s+(.*)/;
+
+handle matches => sub {
+    return $2 if $2 and $2 ne 'academy';
+    return;   
+};
+
+1;
