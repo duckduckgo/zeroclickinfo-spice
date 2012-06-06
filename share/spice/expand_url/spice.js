dcb4fe78b6007b8912c1fb19da79cd8d5507e02b
mrshu <mrshux@gmail.com>
2012-06-06 23:51:44 +0300

    Ignoring the same URL.

--- share/spice/expand_url/spice.js
+++ share/spice/expand_url/spice.js
@@ -1,6 +1,10 @@
 function ddg_spice_expand_url(res)
 {
     var out = '';
+    query = query.replace(/\s/g, "");
+
+    if (query == res["long-url"])
+        return;
     
     var query = DDG.get_query().replace(/expand\s*/, "");
 
