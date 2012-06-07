5b064570eb9325b954375a1f1b4fee3fc7e4610d
Dylan Lloyd <dylan@dylansserver.com>
2012-06-07 15:11:17 -0400

    simplified RedditSubSearch regex

--- lib/DDG/Spice/RedditSubSearch.pm
+++ lib/DDG/Spice/RedditSubSearch.pm
@@ -2,13 +2,11 @@ package DDG::Spice::RedditSubSearch;
 
 use DDG::Spice;
 
-triggers query_lc => qr#^(?:subreddit|/r/|r/)\s*(?:/r/|r/)*(\w+)\s*$#i;
+triggers query_lc => qr#^(?:subreddit|/?r/)\s*(\w+)$#i;
 spice to => 'http://www.reddit.com/r/$1/about.json?jsonp=ddg_spice_reddit';
 
 handle matches => sub {
-    my ($rname) = @_;
-    return $rname if $rname;
-    return;
+    return $_[0]
 };
 
 1;
