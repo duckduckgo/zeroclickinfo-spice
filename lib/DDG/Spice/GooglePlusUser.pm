2e2cca6cc8779c83e624faf5735246b69c438d3d
Jag Talon <talon.jag@gmail.com>
2012-06-06 20:07:37 -0400

    Saved space and separated the plugins.

---
+++ lib/DDG/Spice/GooglePlusUser.pm
@@ -0,0 +1,17 @@
+package DDG::Spice::GooglePlusUser;
+# ABSTRACT: Search for Google+ users and return their bio.
+
+use DDG::Spice;
+
+spice to => 'https://www.googleapis.com/plus/v1/people/$1?key={{ENV{DDG_SPICE_GOOGLE_PLUS_APIKEY}}}&callback={{callback}}';
+
+triggers startend => 'g+ id';
+
+handle remainder => sub {
+	my $query = $_;
+	if($query =~ /(\d+)$/){
+		return $query;
+	}
+	return;
+};
+1;
--- share/spice/google_plus/spice.js
+++ share/spice/google_plus/spice.js
@@ -1,6 +1,5 @@
 function ddg_spice_google_plus(google) {
 	console.log(google);
-
 	var out = '';
 	if(google.kind === "plus#peopleFeed") {
 		var tmp, div, div2, link, img, item, limit;
@@ -20,11 +19,11 @@ function ddg_spice_google_plus(google) {
 		    div2 = d.createElement("div");
 
 		    link = d.createElement("a");
-		    link.href = '/?q=google%2B+' + item.id;
+		    link.href = '/?q=g%2B+id+' + item.id;
 		    
-		    if (item.displayName.length >= 10) {
-				item.displayName = item.displayName.substring(0,8) + "...";
-			}
+		 //    if (item.displayName.length >= 10) {
+			// 	item.displayName = item.displayName.substring(0,8) + "...";
+			// }
 
 		    img = d.createElement('img');
 		    img.src = item.image.url;
@@ -35,7 +34,7 @@ function ddg_spice_google_plus(google) {
 		    div.appendChild(link);
 
 		    link = d.createElement('a');
-		    link.href = '/?q=google%2B+' + item.id;
+		    link.href = '/?q=g%2B+id+' + item.id;
 		    link.innerHTML = item.displayName;
 		    div.appendChild(link);
 		    div.appendChild(d.createElement('br'));
@@ -61,43 +60,5 @@ function ddg_spice_google_plus(google) {
 		items[0]['f'] = 1;
 		items[0]['u'] = 'http://plus.google.com/s/' + query;
 		nra(items,1,1);
-	} else if(google.kind === "plus#person") {
-		//Check if the user has a tagline
-		//Tagline or the about me page can be displayed here.
-		if (google.aboutMe) {
-			out += '<div class="google_profile"><i>Introduction:</i> ' + google.aboutMe + '</div>';
-		}
-
-		//Check for organizations
-		if(google.organizations) {
-			var orgs = '';
-			for(var i=0;i < google.organizations.length && i < 2;i++) {
-				orgs += google.organizations[i].name + 
-					(google.organizations[i].title ? ', ' + google.organizations[i].title : '') 
-					+ '<br>';
-			}
-			out += '<div class="google_orgs"><i>Organizations:</i><br>' + orgs + '</div>';
-		} 
-		//Check if the person has links to show.
-		if(google.urls) {
-			var links = '';
-			if(google.urls.length > 2) {
-				google.urls.length -= 2;
-				for(var i=0;i < google.urls.length && i < 2;i++) {
-					links += '<a href="' + google.urls[i].value + '">' + google.urls[i].value + 
-						'</a><br>';
-				}
-				out += '<div class="google_links"><i>Links:</i><br>' + links + '</div>';
-			}
-		}
-		var items = new Array();
-		items[0] = new Array();
-		items[0]['a'] = out += '<div></div>';
-		items[0]['h'] = google.displayName;
-		items[0]['s'] = 'Google+';
-		items[0]['u'] = google.url;
-		items[0]['f'] = 1;
-		items[0]['i'] =	google.image.url;
-		nra(items);
 	}
 }
