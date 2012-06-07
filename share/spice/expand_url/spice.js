34b03095f74a0339bb3f81e2344849ce2df8eb57
moollaza <moollaza@gmail.com>
2012-06-07 17:16:02 -0400

    Finished ExpandURL Spice, triggeres on 'exapnd' or url from recognized shortening service

--- share/spice/expand_url/spice.js
+++ share/spice/expand_url/spice.js
@@ -1,23 +1,20 @@
 function ddg_spice_expand_url(res)
 {
-    var out = '';
-    query = query.replace(/\s/g, "");
+    if (res["long-url"]){
 
-    if (query == res["long-url"])
-        return;
-    
-    var query = DDG.get_query().replace(/expand\s*/, "");
+        var query = DDG.get_query().replace(/expand\s*/, "");
 
-    if (res["long-url"])    
-        out += '<b>Long url is: </b> <a href="' + res['long-url'] + '">' + res['long-url'] + '</a><br>';
-    else
-        out += res['messages'][0]['message'];        
+        if (res["long-url"] != query){
 
-    items = new Array();
-    items[0] = new Array();
+            var out ='Expanded url: <a href="' 
+                    + res['long-url'] + '">' 
+                    + res['long-url'] + '</a><br>';
 
-    items[0]['a'] = out;
-    items[0]['s'] = 'LongURL';
-    items[0]['u'] = 'http://longurl.org/expand?url=' + query;
-    nra(items);
+            items = [[]];
+            items[0]['a'] = out;
+            items[0]['s'] = 'LongURL';
+            items[0]['u'] = 'http://longurl.org/expand?url=' + query;
+            nra(items);
+        }
+    }
 }
