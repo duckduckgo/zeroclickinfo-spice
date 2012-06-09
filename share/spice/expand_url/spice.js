08869a564d70b887b7cf9836bfe8e1812de26154
yegg <yegg@duckduckgo.com>
2012-06-09 19:59:49 -0400

    Display changes for ExpandURL.

--- share/spice/expand_url/spice.js
+++ share/spice/expand_url/spice.js
@@ -8,10 +8,11 @@ function ddg_spice_expand_url(res)
 
             var out ='Expanded url: <a href="' 
                     + res['long-url'] + '">' 
-                    + res['long-url'] + '</a><br>';
+                    + res['long-url'] + '</a>';
 
             items = [[]];
             items[0]['a'] = out;
+            items[0]['h'] = query + ' (Short URL)';
             items[0]['s'] = 'LongURL';
             items[0]['u'] = 'http://longurl.org/expand?url=' + query;
             nra(items);
