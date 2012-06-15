cb7c3e617e3cc50df96df55acb230110083d7f9c
Jag Talon <talon.jag@gmail.com>
2012-06-14 22:58:07 -0400

    Lists more jobs

--- share/spice/github_jobs/spice.js
+++ share/spice/github_jobs/spice.js
@@ -1,26 +1,34 @@
 function ddg_spice_github_jobs(github) {
 	if(github.length > 0) {
 		console.log(github);
-		var out = '', length;
-		if(github.length > 5) {
-			length = 5;
-		} else {
-			length = github.length;
-		}
-		out += '<div style="jobs">';
-		out += '<ul>';
-		for(var i = 0;i < length;i++) {
-			out += '<li><a href="' + github[i].url + '">' + github[i].title + 
+		var out = '', more = '';
+		out += '<div style="jobs"><ul>';
+		more += out;
+		for(var i = 0;i < github.length;i++) {
+			var bullet = '<li><a href="' + github[i].url + '">' + github[i].title + 
 					'</a> (' + github[i].company + ') in ' + github[i].location + '</li>';
-			//out += '<br>';
+			if(i < 5) {
+				out += bullet;
+			} else {
+				more += bullet;
+			}
 		}
 		out += '</ul>';
-		var items = [[]];
+		more += '</ul>';
+		var items = [[],[]];
 		items[0]['a'] = out;
 		items[0]['h'] = 'Github Jobs';
 		items[0]['s'] = 'Github';
 		items[0]['u'] = 'https://jobs.github.com/';
 		items[0]['f'] = 1;
+
+		if(github.length >= 5) {
+			items[1]['a'] = more;
+			items[1]['t'] = 'More jobs';
+			items[1]['s'] = 'Github';
+			items[1]['u'] = 'https://jobs.github.com/';
+			items[1]['f'] = 1;
+		}
 		nra(items);
 	}
 }
