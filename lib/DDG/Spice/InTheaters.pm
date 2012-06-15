52579557bb75c527f5e4fe0931aab6818e94b485
Jag Talon <talon.jag@gmail.com>
2012-06-14 22:40:24 -0400

    Listed more movies

--- lib/DDG/Spice/InTheaters.pm
+++ lib/DDG/Spice/InTheaters.pm
@@ -3,7 +3,7 @@ package DDG::Spice::InTheaters;
 
 use DDG::Spice;
 
-spice to => 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=ccw2b5ce8dsy7sb3x2qxmn3x&page_limit=5&callback={{callback}}';
+spice to => 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=ccw2b5ce8dsy7sb3x2qxmn3x&callback={{callback}}';
 
 triggers query_lc => qr/(?:\s*(?:i\s+|we\s+)?(?:want\s+|need\s+|deserve\s+)?(?:to\s+)?(?:watch\s*|see\s*)(?:something|(?:a\s+)?movies?)\s*)|(?:movies\s*)(?:(?:in\s+)?theaters?)?(?:(?:now\s+|already\s+)?showing)?\s*$/i;
 
--- share/spice/in_theaters/spice.js
+++ share/spice/in_theaters/spice.js
@@ -1,26 +1,33 @@
 function ddg_spice_in_theaters(rotten) {
 	if(rotten.movies.length > 0) {
 		console.log(rotten);
-		var out = '', length;
-		if(rotten.movies.length > 5) {
-			length = 5;
-		} else {
-			length = rotten.movies.length;
+		var out = '', length, more = '';
+		out += '<div style="movies"><ul>';
+		more += out;
+		for(var i = 0;i < rotten.movies.length;i++) {
+			var bullet = '<li><a href="' + rotten.movies[i].links.alternate + '">' + rotten.movies[i].title + 
+					'</a> (' + rotten.movies[i].mpaa_rating + ') (' + rotten.movies[i].ratings.critics_rating + 
+					')</li>';
+			if(i < 5) {
+				out += bullet;
+			} else {
+				more += bullet;
+			}
 		}
-		out += '<div style="movies">';
-		out += '<ul>';
-		for(var i = 0;i < length;i++) {
-			out += '<li><a href="' + rotten.movies[i].links.alternate + '">' + rotten.movies[i].title + 
-					'</a> (' + rotten.movies[i].mpaa_rating + ') <i>Critic\'s Rating: ' + rotten.movies[i].ratings.critics_score + 
-					'</i></li>';
-		}
-		out += '</ul>';
-		var items = [[]];
+		out += '</ul></div>';
+		more += '</ul></div>';
+		var items = [[],[]];
 		items[0]['a'] = out;
 		items[0]['h'] = 'In Theaters';
 		items[0]['s'] = 'Rotten Tomatoes';
 		items[0]['u'] = 'http://www.rottentomatoes.com/movie/in-theaters/';
 		items[0]['f'] = 1;
+
+		items[1]['a'] = more;
+		items[1]['t'] = 'Show more movies';
+		items[1]['s'] = 'Rotten Tomatoes';
+		items[1]['u'] = 'http://www.rottentomatoes.com/movie/in-theaters/';		
+		items[1]['f'] = 1;
 		nra(items);
 	}
 }
