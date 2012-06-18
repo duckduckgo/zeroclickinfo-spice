30f1f41557e93a2fb0bf5b1b790fe8b42ec6beec
Dylan Lloyd <dylan@dylansserver.com>
2012-06-18 12:52:20 -0400

    don't clobber query github jobs

--- lib/DDG/Spice/Github.pm
+++ lib/DDG/Spice/Github.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Github;
 
 use DDG::Spice;
 
-triggers query_lc => qr#^github\s+(.+)$#i;
+triggers query_lc => qr#^github\s+(?!jobs?)(.+)$#i;
 spice to => 'http://github.com/api/v2/json/repos/search/$1';
 spice wrap_jsonp_callback => 1;
 
