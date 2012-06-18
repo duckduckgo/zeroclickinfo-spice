a503cb00631d0dbcbe6832ceb1ab1710f80a6829
Jag Talon <talon.jag@gmail.com>
2012-06-18 18:53:39 -0400

    Checked for unrated movies

--- share/spice/in_theaters/spice.js
+++ share/spice/in_theaters/spice.js
@@ -5,9 +5,16 @@ function ddg_spice_in_theaters(rotten) {
 		out += '<div style="movies"><ul>';
 		more += out;
 		for(var i = 0;i < rotten.movies.length;i++) {
+			var rating = rotten.movies[i].ratings.critics_rating;
+			var score = rotten.movies[i].ratings.critics_score;
+			var joined = '';
+			if(rating) {
+				joined = rating + ' - ' +  score + '%';
+			} else {
+				joined = 'not reviewed yet';
+			}
 			var bullet = '<li><a href="' + rotten.movies[i].links.alternate + '">' + rotten.movies[i].title + 
-					'</a> (' + rotten.movies[i].mpaa_rating + ') <i>' + rotten.movies[i].ratings.critics_rating + 
-					' - ' + rotten.movies[i].ratings.critics_score  + '%</i></li>';
+					'</a> (' + rotten.movies[i].mpaa_rating + ') <i>' + joined + '</i></li>';
 			if(i < 5) {
 				out += bullet;
 			} else {
@@ -21,6 +28,7 @@ function ddg_spice_in_theaters(rotten) {
 		items[0]['h'] = 'Currently In Theaters';
 		items[0]['s'] = 'Rotten Tomatoes';
 		items[0]['u'] = 'http://www.rottentomatoes.com/movie/in-theaters/';
+		items[0]['force_big_header'] = true;
 		items[0]['f'] = 1;
 
 		items[1]['a'] = more;
