0728cd70206f2faf4a31fba8efe8557859e44d8f
moollaza <moollaza@gmail.com>
2012-06-18 15:16:04 -0400

    modified CodeSearch to trigger when there are at least 2 characters after the relevant language term (prevents results for searches like 'example iOS 6'

--- lib/DDG/Spice/CodeSearch.pm
+++ lib/DDG/Spice/CodeSearch.pm
@@ -8,7 +8,7 @@ spice to => 'http://searchco.de/api/jsonp_codesearch_I/?q=$1&callback={{callback
 
 handle remainder => sub {
     my $words = 'actionscript|ada|asp|asp\.net|assembly|awk|bc|bourneagainshell|bourneshell|c|cshell|c\/c\+\+header|c\#|c\+\+|cmake|cobol|coldfusion|css|cython|d|dal|dart|dosbatch|dtd|erlang|expect|fortran77|fortran90|fortran95|go|groovy|haskell|html|idl|java|javascript|jsp|kermit|kornshell|lex|lisp|lua|m4|make|matlab|modula3|msbuildscripts|mumps|mxml|nantscripts|objectivec|objectivec\+\+|ocaml|octave|oracleforms|oraclereports|pascal|patrancommandlanguage|perl|php|python|rexx|ruby|rubyhtml|scala|sed|skill|smarty|softbridgebasic|sql|sqldata|tcl\/tk|teamcenterdef|teamcentermet|teamcentermth|vhdl|vimscript|visualbasic|xaml|xml|xsd|xslt|yacc|yaml';
-    if ($_ =~ qr/(\w+\s*$words|$words\s*\w+)/i) {
+    if ($_ =~ qr/(\w\w+\s*$words|$words\s*\w\w+)/i) {
         return "lang:".$_;
     }
     return;
