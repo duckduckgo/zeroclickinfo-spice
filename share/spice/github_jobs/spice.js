61a1f869d13ae0217a1f77d933652444737c7d28
Dylan Lloyd <dylan@dylansserver.com>
2012-06-19 17:36:55 -0400

    rm'd 2nd nra in github

--- share/spice/github_jobs/spice.js
+++ share/spice/github_jobs/spice.js
@@ -5,35 +5,20 @@ function ddg_spice_github_jobs(github) {
 		var description = query.replace(re, "$1");
 		var location = query.replace(re, "$2");
 		var moreat = "https://jobs.github.com/positions?description=" +  encodeURIComponent(description) + "&location=" +  encodeURIComponent(location);
-		var out = '', more = '';
+		var out = ''
 		out += '<div style="jobs"><ul>';
-		more += out;
 		for(var i = 0;i < github.length;i++) {
-			var bullet = '<li><a href="' + github[i].url + '">' + github[i].title + 
-					'</a> in ' + github[i].location + ' (' + github[i].company + ')</li>';
-			if(i < 5) {
-				out += bullet;
-			} else {
-				more += bullet;
-			}
+			out += '<li><a href="' + github[i].url + '">' + github[i].title 
+                +  '</a> in ' + github[i].location + ' (' + github[i].company + ')</li>';
 		}
 		out += '</ul>';
-		more += '</ul>';
-		var items = [[],[]];
+		var items = [[]];
 		items[0]['a'] = out;
 		items[0]['h'] = query + ' (Github Jobs)';
 		items[0]['s'] = 'Github';
 		items[0]['u'] = moreat;
-		items[0]['f'] = 1;
 		items[0]['force_big_header'] = true;
 
-		if(github.length > 5) {
-			items[1]['a'] = more;
-			items[1]['t'] = 'More jobs';
-			items[1]['s'] = 'Github';
-			items[1]['u'] =  moreat;
-			items[1]['f'] = 1;
-		}
 		nra(items);
 	}
 }
