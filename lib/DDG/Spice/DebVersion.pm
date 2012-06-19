7ae93e52d6b23a0a174c640d8a6af2adcc6cb509
Simon <simonft@riseup.net>
2012-06-19 18:46:52 -0500

    Broaden trigger cases

--- lib/DDG/Spice/DebVersion.pm
+++ lib/DDG/Spice/DebVersion.pm
@@ -1,18 +1,19 @@
 package DDG::Spice::DebVersion;
 
 use DDG::Spice;
+use strict;
+use warnings;
 
-triggers any => "debian version";
+triggers any => "debian version","debian versions";
 
 spice from => '([^/]+)/?(?:([^/]+)/?(?:([^/]+)|)|)';
 spice to => 'http://qa.debian.org/madison.php?package=$1&table=debian&a=&c=&s=&text=on#';
 
-zci is_cached => 1;
 
 spice wrap_string_callback => 1;
 
 handle query_lc => sub {
-    if (/^debian version (\S*)$/i) {
+    if (/^\s*debian\s*versions?\s*(\S+)\s*$/i) {
         my $DebPackage = $1;
         $DebPackage =~ s/\s+$//g;
         $DebPackage =~ s/^\s+//g;
