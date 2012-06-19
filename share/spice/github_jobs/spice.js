2b508cf4c95d0874fd143d0fcc6e73e3245bb406
Jag Talon <talon.jag@gmail.com>
2012-06-19 15:24:28 -0400

    Better more at ... link

--- share/spice/github_jobs/spice.js
+++ share/spice/github_jobs/spice.js
@@ -1,6 +1,8 @@
 function ddg_spice_github_jobs(github) {
 	if(github.length > 0) {
 		var query = DDG.get_query();
+		var re = /(?:\s*(?:i\s+|we\s+)?(?:need|want|deserve|seek|get)\s+(?:an?\s+)?)?(?:(.+)\s+)(?:jobs?|work|employment)(?:\s+(?:in\s+)?(.+))?/;
+		var moreat = query.replace(re, "https://jobs.github.com/positions?description=$1&location=$2");
 		console.log(github);
 		var out = '', more = '';
 		out += '<div style="jobs"><ul>';
@@ -20,7 +22,7 @@ function ddg_spice_github_jobs(github) {
 		items[0]['a'] = out;
 		items[0]['h'] = query + ' (Github Jobs)';
 		items[0]['s'] = 'Github';
-		items[0]['u'] = 'https://jobs.github.com/';
+		items[0]['u'] = moreat;
 		items[0]['f'] = 1;
 		items[0]['force_big_header'] = true;
 
@@ -28,7 +30,7 @@ function ddg_spice_github_jobs(github) {
 			items[1]['a'] = more;
 			items[1]['t'] = 'More jobs';
 			items[1]['s'] = 'Github';
-			items[1]['u'] = 'https://jobs.github.com/';
+			items[1]['u'] = moreat;
 			items[1]['f'] = 1;
 		}
 		nra(items);
