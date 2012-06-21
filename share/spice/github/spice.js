122f8a242b366d04ede0472e5e1b4b0243e94b1d
Dylan Lloyd <dylan@dylansserver.com>
2012-06-20 20:22:30 -0400

    added last update to github single

--- share/spice/github/spice.js
+++ share/spice/github/spice.js
@@ -14,15 +14,26 @@ function ddg_spice_github(re) {
     var more = "http://www.github.com/search?q=" + encodeURI(query);
     if (re.length == 1) {
         re = re[0];
-        console.log(re);
+        last_pushed = Math.floor((new Date() - new Date(re.pushed) ) / (1000*60*60*24));
+        var years_ago = Math.floor(last_pushed / 365);
+        if (years_ago >= 1) {
+            last_pushed = years_ago + " year" + (years_ago == 1 ? "" : "s") + " ago";
+        } else if (last_pushed == 0) {
+            last_pushed = "today";
+        } else if (last_pushed == 1) {
+            last_pushed = "yesterday";
+        } else {
+            last_pushed = last_pushed + " day" + (last_pushed == 1 ? "" : "s") + " ago";
+        }
         var url = re.homepage.replace(/^(?!https?:\/\/)/, "http://");
         content =  "<i>Description</i>: " + re.description + "<br>"
                 +  "<i>Author</i>: " + re.owner + "<br>"
                 +  "<i>Homepage</i>: " + "<a href='"
                 +  url + "'>" + re.homepage.replace(/^https?:\/\/|\/$/gi, '')
-                +  "</a><br>" + "<i>Activity</i>: " + re.watchers + " watching, "
+                +  "</a><br>" + "<i>Activity</i>: last updated " + last_pushed
+                +  ", " + re.watchers + " watching, "
                 +  re.forks + "<a href='"
-                +  re.url + "/network'> forks</a>, "
+                +  re.url + "/network'> forks</a> and "
                 +  re.open_issues + "<a href='" + re.url
                 +  "/issues'> issues</a>"
                 +  "<br>";
