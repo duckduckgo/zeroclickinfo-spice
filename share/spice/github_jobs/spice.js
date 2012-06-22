f9666e9675279d3eeb6c167cfc8d50611b56a7e4
Jag Talon <talon.jag@gmail.com>
2012-06-22 09:33:56 -0400

    Added a second array

--- share/spice/github_jobs/spice.js
+++ share/spice/github_jobs/spice.js
@@ -4,21 +4,38 @@ function ddg_spice_github_jobs(github) {
 		var re = /(?:\s*(?:i\s+|we\s+)?(?:need|want|deserve|seek|get)\s+(?:an?\s+)?)?(?:(.+)\s+)(?:jobs?|work|employment)(?:\s+(?:in\s+)?(.+))?/;
 		var description = query.replace(re, "$1");
 		var location = query.replace(re, "$2");
-		var moreat = "https://jobs.github.com/positions?description=" +  encodeURIComponent(description) + "&location=" +  encodeURIComponent(location);
-		var out = ''
+		var more_at = "https://jobs.github.com/positions?description=" +  encodeURIComponent(description) + "&location=" +  encodeURIComponent(location);
+		var out = '', more = '';
 		out += '<div style="jobs"><ul>';
+		more += out;
 		for(var i = 0;i < github.length;i++) {
-			out += '<li><a href="' + github[i].url + '">' + github[i].title 
+            if(i > 5) {
+            	more += '<li><a href="' + github[i].url + '">' + github[i].title 
                 +  '</a> in ' + github[i].location + ' (' + github[i].company + ')</li>';
+            } else {
+            	out += '<li><a href="' + github[i].url + '">' + github[i].title 
+                +  '</a> in ' + github[i].location + ' (' + github[i].company + ')</li>';
+            }
 		}
 		out += '</ul>';
-		var items = [[]];
+		more += '</ul>';
+		var items = [[],[]];
 		items[0]['a'] = out;
 		items[0]['h'] = query + ' (GitHub Jobs)';
 		items[0]['s'] = 'GitHub';
-		items[0]['u'] = moreat;
+		items[0]['u'] = more_at;
 		items[0]['force_big_header'] = true;
+		items[0]['f'] = 1;
 
+		//Second array
+		if(github.length > 5) {
+			items[1]['a'] = more;
+			items[1]['t'] = '+More jobs';
+			items[1]['s'] = 'GitHub';
+			items[1]['u'] = more_at;
+			items[1]['force_big_header'] = true;
+			items[1]['f'] = 1;
+		}
 		nra(items);
 	}
 }
