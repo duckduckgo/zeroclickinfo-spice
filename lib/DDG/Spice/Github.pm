9e761132ff72ce8d8d6d8291816c46e9feda5682
Dylan Lloyd <dylan@dylansserver.com>
2012-06-22 21:30:12 -0400

    added abstract and attribution to github search

--- lib/DDG/Spice/Github.pm
+++ lib/DDG/Spice/Github.pm
@@ -1,9 +1,13 @@
 package DDG::Spice::Github;
+# ABSTRACT: Search for information about GitHub repositories
 
 use DDG::Spice;
 
 triggers any => "github";
 spice to => 'https://api.github.com/legacy/repos/search/$1?callback={{callback}}';
+
+attribution web => ['http://dylansserver.com','Dylan Lloyd'],
+            email => ['dylan@dylansserver.com','Dylan Lloyd'];
 
 handle query_lc => sub {
     if (/^github\s+(?!jobs?)(.+)$/) {
