bad98da987f230b0a085b4494d169a6c696f91b8
Dylan Lloyd <dylan@dylansserver.com>
2012-06-22 21:25:53 -0400

    github search now word triggered

--- lib/DDG/Spice/Github.pm
+++ lib/DDG/Spice/Github.pm
@@ -2,11 +2,14 @@ package DDG::Spice::Github;
 
 use DDG::Spice;
 
-triggers query_lc => qr#^github\s+(?!jobs?)(.+)$#i;
+triggers any => "github";
 spice to => 'https://api.github.com/legacy/repos/search/$1?callback={{callback}}';
 
-handle matches => sub {
-    return $_[0]
+handle query_lc => sub {
+    if (/^github\s+(?!jobs?)(.+)$/) {
+        return $1;
+    }
+    return;
 };
 
 1;
