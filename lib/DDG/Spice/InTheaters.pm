d784d43e1fce99ea412177efec09df2820508dee
Jag Talon <talon.jag@gmail.com>
2012-06-28 22:33:53 -0400

    Fixed regex

--- lib/DDG/Spice/InTheaters.pm
+++ lib/DDG/Spice/InTheaters.pm
@@ -3,15 +3,15 @@ package DDG::Spice::InTheaters;
 
 use DDG::Spice;
 
+my $rating = '(?:g\s*|pg\s*|r\s*)?';
 spice to => 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/$1.json?apikey=ccw2b5ce8dsy7sb3x2qxmn3x&callback={{callback}}';
-triggers any => "movies", "movie", "theaters", "watch", "something", "showing", "watch";
+triggers query_lc => qr/\s*(?:movies?\s*)?opening\s*($rating)(?:soon\s*)?(?:movies?|in\s*theaters)?\s*|\s*(?:\s*(?:i\s*|we\s*)?(?:want\s*|need\s*|deserve\s*)?(?:to\s*)?(?:watch\s*|see\s*)(?:something|(?:an?\s*)?($rating)movies?)\s*)|\s*($rating)(?:movies\s*|current(?:ly)?\s*)(?:(?:in\s*)?(?:theaters?|theatres?))?($rating)(?:(?:now\s*|already\s*)?showing)?\s*|\s*what\s*(?:to\s*|can\s*I\s*)?watch\??\s*$/i;
 
 handle query_lc => sub {
-	my $rating = '(g\s+|pg\s+|r\s+)?';
-	if(/\s*opening\s+$rating(?:movies?|in\s+theaters)\s*$/) {
+	if(/\s*($rating)(?:movies?\s*)?opening\s*($rating)(?:soon\s*)?($rating)(?:movies?|in\s*theaters)?\s*$/i) {
 		return "opening";
 	}
-	if(/\s*(?:\s*(?:i\s+|we\s+)?(?:want\s+|need\s+|deserve\s+)?(?:to\s+)?(?:watch\s*|see\s*)(?:something|(?:an?\s+$rating)?movies?)\s*)|\s*$rating(?:movies\s*|current(?:ly)?\s*)$rating(?:movies\s*)?(?:(?:in\s+)?(?:theaters?|theatres?))?(?:(?:now\s+|already\s+)?showing)?\s*|\s*what\s+(?:to\s+|can\s+I\s+)?watch\??\s*$/) {
+	if(/\s*(?:\s*(?:i\s*|we\s*)?(?:want\s*|need\s*|deserve\s*)?(?:to\s*)?(?:watch\s*|see\s*)(?:something|(?:an?\s*)?($rating)movies?)\s*)|\s*($rating)(?:movies\s*|current(?:ly)?\s*)(?:(?:in\s*)?(?:theaters?|theatres?))?($rating)(?:(?:now\s*|already\s*)?showing)?\s*|\s*what\s*(?:to\s*|can\s*I\s*)?watch\??\s*$/i) {
 		return "in_theaters";
 	} 
 	return;
