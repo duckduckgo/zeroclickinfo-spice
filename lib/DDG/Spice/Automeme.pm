b970d6dde43238fdf844f5852a2601b9725c2c8f
moollaza <moollaza@gmail.com>
2012-07-02 14:10:07 -0400

    added Automeme spice - made minor modifications

--- lib/DDG/Spice/Automeme.pm
+++ lib/DDG/Spice/Automeme.pm
@@ -4,19 +4,14 @@ package DDG::Spice::Automeme;
 
 use DDG::Spice;
 
-spice to =>
-    'http://api.automeme.net/html.json?lines=1$1&callback={{callback}}';
+spice to => 'http://api.automeme.net/html.json?lines=1&callback={{callback}}';
 spice wrap_jsonp_callback => 1;
 spice proxy_cache_valid   => "418 1d";
 
-triggers any => 'automeme', 'meme';
+triggers any => 'automeme';
 
 handle remainder => sub {
-
-   # If you liked Automeme before it was cool, use the vocab=hipster parameter
-   # for some unique hipster words.
-    /hipster/i and return '&vocab=hipster';
-    return q{};
+	return '';
 };
 
 1;
