7a8f6bd9aeb00553cd353e41c92b833aa66459a4
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2012-07-06 14:59:05 +0100

    Zanran styling changed to look better (based on alternatives to)

--- share/spice/zanran/spice.js
+++ share/spice/zanran/spice.js
@@ -14,66 +14,58 @@ function ddg_spice_zanran(zanran_results) {
 
   var results = zanran_results.results;
   
-  var content = d.createElement('div');
-  var table = d.createElement('table');
-  var tbody = d.createElement('tbody');
-  var row1 = d.createElement('tr');
-  var row2 = d.createElement('tr');
-
-  content.appendChild(table);
-  table.appendChild(tbody);
-  tbody.appendChild(row1);
-  tbody.appendChild(row2);
+  var out = "";
 
   for(var i=0; i<results.length; i++) {
-    var cell1 = d.createElement('td');
-    var cell2 = d.createElement('td');
+    var div = d.createElement("div");
+    var idiv = d.createElement("div");
+    var link = d.createElement("a");
+    link.href = results[i].preview_url;
+    link.title = results[i].title;
 
-    var link1 = d.createElement('a');
-    var link2 = d.createElement('a');
-    var img  = d.createElement('img');
-    var title = d.createElement('div');
-
-    YAHOO.util.Dom.setStyle(cell1, 'text-align', 'center');
-    YAHOO.util.Dom.setStyle(cell1, 'vertical-align', 'bottom');
-
-    YAHOO.util.Dom.setStyle(cell2, 'vertical-align', 'top');
-    YAHOO.util.Dom.setStyle(cell2, 'width', '137px');
-    YAHOO.util.Dom.setStyle(cell2, 'background', '#e7fdc7');
-    YAHOO.util.Dom.setStyle(cell2, 'border', '1px solid black');
-    YAHOO.util.Dom.setStyle(cell2, 'margin', '2px');
-    YAHOO.util.Dom.setStyle(cell2, 'padding', '2px');
-    YAHOO.util.Dom.setStyle(cell2, 'font-size', '80%');
+    var img = d.createElement('img');
+    img.src = results[i].preview_image;
     
-    link1.href = results[i].preview_url;
-    link1.title = results[i].title;
-
+    link.appendChild(img);
+    idiv.appendChild(link);
+    div.appendChild(idiv);
+    
+    var link2 = d.createElement("a");
     link2.href = results[i].preview_url;
     link2.title = results[i].title;
+    link2.appendChild(d.createTextNode(results[i].short_title));
     
-    img.src = results[i].preview_image;
-    YAHOO.util.Dom.setStyle(img, 'display', 'inline');
+    div.appendChild(link2);
+    div.appendChild(d.createElement('br'));
+    
+    YAHOO.util.Dom.addClass(div, 'inline highlight_zero_click1 highlight_zero_click_wrapper');
+    YAHOO.util.Dom.setStyle(div, "float", "left");
+    YAHOO.util.Dom.setStyle(div, "max-width", "137px");
+    YAHOO.util.Dom.setStyle(div, "vertical-align", "bottom");
+    YAHOO.util.Dom.setStyle(div, "text-align", "center");
+
+    YAHOO.util.Dom.setStyle(idiv, 'height', '200px');
+    YAHOO.util.Dom.setStyle(idiv, "position", "relative");
+
+    YAHOO.util.Dom.setStyle(img, "position", "absolute");
+    YAHOO.util.Dom.setStyle(img, "bottom", "0px");
+    YAHOO.util.Dom.setStyle(img, "margin", '0 auto 0 auto');
     YAHOO.util.Dom.setStyle(img, 'max-width', '137px');
-    YAHOO.util.Dom.setStyle(img, 'max-height', '200px');
-
-	  title.appendChild(d.createTextNode(results[i].short_title));
-
-	  row1.appendChild(cell1);
-	  cell1.appendChild(link1);
-	  link1.appendChild(img);
-
-	  row2.appendChild(cell2);
-	  cell2.appendChild(link2)
-	  link2.appendChild(title);
+    YAHOO.util.Dom.setStyle(img, 'max-height', '194px'); // A4 ratio
+    
+    var container_div = d.createElement("div");
+    container_div.appendChild(div);
+    out += container_div.innerHTML;
   }
+  out += '<div class="clear"></div>';
   
   items = [[]];
-  items[0]['a'] = content;
-  items[0]['h'] = 'Data & Statistics';
+  items[0]['a'] = out;
+  items[0]['h'] = '<h1>Data & Statistics</h1>';
   items[0]['s'] = 'Zanran';
   items[0]['u'] = zanran_results.more;
   items[0]['f'] = 1;
   // items[0]['i'] = 'http://zanran.com/favicon.ico';
 
-  nra(items);
+  nra(items,1,1);
 };
