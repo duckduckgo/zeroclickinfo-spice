72b07d37a40cc12acec5c82e61513b31091f2544
Kevin Schaul <kevin.schaul@gmail.com>
2012-07-05 21:01:03 -0700

    More matches for regex
    Includes "trending on twitter"

--- lib/DDG/Spice/Trends.pm
+++ lib/DDG/Spice/Trends.pm
@@ -8,7 +8,7 @@ attribution web => ['http://kevinschaul.com','Kevin Schaul'],
 
 spice to => 'http://query.yahooapis.com/v1/public/yql?q=select%20woeid%20from%20geo.places%20where%20text%3D\'$1\'&format=json&callback={{callback}}';
 
-triggers query_lc => qr/^(?:twitter\s+)?(?:trending|trends|trend)\s*(?:twitter)?\s*(?:topics|topic)?\s*(?:twitter)?\s*(?:in)?\s*(.*)?/;
+triggers query_lc => qr/^(?:twitter\s+)?(?:trending|trends|trend)\s*(?:on)?\s*(?:twitter)?\s*(?:topics|topic)?\s*(?:twitter)?\s*(?:in)?\s*(.*)?/;
 
 handle matches => sub {
     my ($uname) = @_;
