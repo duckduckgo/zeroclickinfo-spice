ac8729b488f8ed0d2f621a1ebf10e5769a468d0d
Kevin Schaul <kevin.schaul@gmail.com>
2012-07-06 22:45:26 -0700

    Add state codes lookup object, force_big_header
    Uses state codes lookup to provide better title
    ex: "Members of the Washington Senate" vs "Members of WA Senate"
    
    Use html list to display items

--- share/spice/congress/spice.js
+++ share/spice/congress/spice.js
@@ -1,8 +1,64 @@
 function ddg_spice_congress(data) {
+
+    var stateCodes = {
+        "AL": "Alabama",
+        "AK": "Alaska",
+        "AZ": "Arizona",
+        "AR": "Arkansas",
+        "CA": "California",
+        "CO": "Colorado",
+        "CT": "Connecticut",
+        "DE": "Delaware",
+        "FL": "Florida",
+        "GA": "Georgia",
+        "HI": "Hawaii",
+        "ID": "Idaho",
+        "IL": "Illinois",
+        "IN": "Indiana",
+        "IA": "Iowa",
+        "KS": "Kansas",
+        "KY": "Kentucky",
+        "LA": "Louisiana",
+        "ME": "Maine",
+        "MD": "Maryland",
+        "MA": "Massachusetts",
+        "MI": "Michigan",
+        "MN": "Minnesota",
+        "MS": "Mississippi",
+        "MO": "Missouri",
+        "MT": "Montana",
+        "NE": "Nebraska",
+        "NV": "Nevada",
+        "NH": "New Hampshire",
+        "NJ": "New Jersey",
+        "NM": "New Mexico",
+        "NY": "New York",
+        "NC": "North Carolina",
+        "ND": "North Dakota",
+        "OH": "Ohio",
+        "OK": "Oklahoma",
+        "OR": "Oregon",
+        "PW": "Palau",
+        "PA": "Pennsylvania",
+        "PR": "Puerto Rico",
+        "RI": "Rhode Island",
+        "SC": "South Carolina",
+        "SD": "South Dakota",
+        "TN": "Tennessee",
+        "TX": "Texas",
+        "UT": "Utah",
+        "VT": "Vermont",
+        "VA": "Virginia",
+        "WA": "Washington",
+        "WV": "West Virginia",
+        "WI": "Wisconsin",
+        "WY": "Wyoming"
+    };
+    
     if (data.status == "OK" && data.results.length > 0) {
         var result = data.results[0];
         var members = result.members;
-        var snippet = "";
+        var snippet = "<ul>";
         for (var i = 0; i < members.length; i++) {
             var member = members[i];
             var name = "";
@@ -16,7 +72,7 @@ function ddg_spice_congress(data) {
             if (member.twitter_account) {
                 twitter = "@" + member.twitter_account;
             }
-            snippet += "<a href=\""
+            snippet += "<li><a href=\""
                 + member.times_topics_url + "\">"
                 + "(" + member.party + ") "
                 + name
@@ -25,10 +81,11 @@ function ddg_spice_congress(data) {
                 snippet += " &mdash; <i><a href=\"http://www.twitter.com/"
                     + member.twitter_account + "\">" + twitter + "</a></i>" 
             }
-            snippet += "<br />";
+            snippet += "</li>";
         }
+        snippet += "</ul>";
 
-        var title = "Members of " + result.state + " " + result.chamber;
+        var title = "Members of the " + stateCodes[result.state] + " " + result.chamber;
         var source = "The New York Times";
         var url = "http://topics.nytimes.com/top/reference/timestopics/"
             + "organizations/c/congress/index.html";
@@ -40,6 +97,7 @@ function ddg_spice_congress(data) {
         items[0]['h'] = title;
         items[0]['s'] = source;
         items[0]['u'] = url;
+        items[0]['force_big_header'] = true;
         nra(items);
     }
 }    
