a7f983ee599c5cede32e87e2e4c3100660846c69
kogalex <kogalex@gmail.com>
2012-07-10 12:56:17 +0300

    adding js

---
+++ share/spice/pipl/spice.js
@@ -0,0 +1,9 @@
+function ddg_spice_aur(results) {
+        items = new Array();
+        items[0] = new Array();
+        items[0]['a'] = out;
+        items[0]['h'] = res['Name'];
+        items[0]['s'] = 'Archlinux User Repository';
+        items[0]['u'] = 'https://aur.archlinux.org/packages.php?ID=' + res['ID'];
+        nra(items);
+}
