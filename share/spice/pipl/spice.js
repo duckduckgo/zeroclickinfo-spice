de5ea6fac6591667b248fc63322b560084de3424
kogalex <kogalex@gmail.com>
2012-07-10 18:10:11 +0300

    fixing urls

--- share/spice/pipl/spice.js
+++ share/spice/pipl/spice.js
@@ -21,7 +21,7 @@ function ddg_spice_pipl(results) {
 			links = results['suggestions'];
 			content += "<ul>";
 			for (var i=0; i<links.length; i++)
-				content += '<li><a href=http://www.pipl.com/search/"' + links[i]['url'] + '">' + links[i]['caption'] +'</a></li>';
+				content += '<li><a href="' + links[i]['url'] + '">' + links[i]['caption'] +'</a></li>';
 			
 			content += "</ul>";
 		}
