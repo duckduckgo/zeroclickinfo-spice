45d2695deed2764146bf765d3a70dd4d7758b276
kogalex <kogalex@gmail.com>
2012-07-10 15:55:37 +0300

    tets

--- share/spice/pipl/spice.js
+++ share/spice/pipl/spice.js
@@ -1,4 +1,16 @@
 function ddg_spice_pipl(results) {
+		
+	items = [[]];
+    items[0]['a'] = '<span>some content<BR><BR>cont</span>';
+    items[0]['h'] = 'Kevin Kelber(Person)';
+    items[0]['s'] = 'Pipl';
+    items[0]['u'] = 'http://twitter.com/';
+    items[0]['i'] = 'http://a1.ec-images.myspacecdn.com/profile01/146/af7f9c694f21402f9f8f027635c738e8/t.jpg';
+
+//    console.log(tweets[0].user.profile_image_url);
+
+    nra(items);
+    /*
         items = new Array();
         items[0] = new Array();
         items[0]['a'] = 'Kevin Scott Kelber, 52 years old, Rancho Cordova, CA, US, Sa ...';
@@ -7,4 +19,5 @@ function ddg_spice_pipl(results) {
         items[0]['u'] = 'http://www.pipl.com';
         items[0]['i'] = 'http://a1.ec-images.myspacecdn.com/profile01/146/af7f9c694f21402f9f8f027635c738e8/t.jpg';
         nra(items);
+        */
 }
