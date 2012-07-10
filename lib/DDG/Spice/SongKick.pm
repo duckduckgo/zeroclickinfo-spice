ad08e4f71648367ffd551ed947aca4f162019980
moollaza <moollaza@gmail.com>
2012-07-10 11:13:47 -0400

    first attempt at songkick

--- lib/DDG/Spice/SongKick.pm
+++ lib/DDG/Spice/SongKick.pm
@@ -5,27 +5,32 @@ use feature "switch";
 
 triggers any => "songkick";
 
-my $base_url = 'http://api.songkick.com/api/3.0/$1.json?query=$1&apikey={{ENV{DDG_SPICE_BIGHUGE_APIKEY}}}&jsoncallback=={{callback}}';
+my $base_url = 'http://api.songkick.com/api/3.0/###TEMPLATE###.json?query=$1&apikey={{ENV{DDG_SPICE_BIGHUGE_APIKEY}}}&jsoncallback=={{callback}}';
 
 handle query_lc => sub {
     my $query = $_;
+    my $query_type;
 
     given ($query)    {
-        when (/artist/) { $1 = '/search/artists'}
-        when (/venue/) { $1 = '/search/venues'}
-        when (/location/) { $1 = '/search/locations'}
-        when (/event/) { $1 = get_params($query)}
+        when (/artist/) { $query_type = '/search/artists'}
+        when (/venue/) { $query_type = '/search/venues'}
+        when (/location/) { $query_type = '/search/locations'}
+        when (/event/) { $query_type = get_params($query)}
     }
 
-    return;
+    $base_url =~ s/###TEMPLATE###/$query_type/;
+
+    spice to => $base_url;
+
+    return 'yahoo'; 
 };
 
-spice to => $base_url
-
 sub get_params {
-    given ($_){
-        when
-    }
-}
+    # given ($_){
+    #     when
+    # }
+
+    return '/thisTest/';
+};
 
 1;
