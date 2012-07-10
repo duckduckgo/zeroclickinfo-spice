d944c2ff5861b96f1647f40b7299b5307f11de29
kogalex <kogalex@gmail.com>
2012-07-10 14:55:35 +0300

    changing to ec2

--- lib/DDG/Spice/Pipl.pm
+++ lib/DDG/Spice/Pipl.pm
@@ -4,7 +4,7 @@ use DDG::Spice;
 
 triggers query => qr/^([A-Za-z]+\s[A-Za-z]+)$/;
 
-spice to => 'http://v2.pipl.com/test.json?$1';
+spice to => 'http://ec2-23-22-204-83.compute-1.amazonaws.com/test.json?$1';
 spice wrap_jsonp_callback => 1;
 
 handle matches => sub {
