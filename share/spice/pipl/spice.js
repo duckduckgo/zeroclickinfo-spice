14ce5b831cec9d676f4d7bea6f0b0657a85fbc18
kogalex <kogalex@gmail.com>
2012-07-10 15:45:22 +0300

    testing spice/pipl/spice.js

--- share/spice/pipl/spice.js
+++ share/spice/pipl/spice.js
@@ -1,9 +1,10 @@
 function ddg_spice_pipl(results) {
         items = new Array();
         items[0] = new Array();
-        items[0]['a'] = 'a';
-        items[0]['h'] = 'h';
-        items[0]['s'] = 's';
-        items[0]['u'] = 'https://url';
+        items[0]['a'] = 'Kevin Scott Kelber, 52 years old, Rancho Cordova, CA, US, Sa ...';
+        items[0]['h'] = 'Kevin Kelber (Person)';
+        items[0]['s'] = 'Pipl';
+        items[0]['u'] = 'http://www.pipl.com';
+        items[0]['i'] = 'http://t18.thumb.pipl.com/cgi-bin/fdt.fcgi?hg=78&wd=78&th=1&favicon=1&dsid=568&fd=www.myspace.com&def=1&rem=0&url=http://a1.ec-images.myspacecdn.com/profile01/146/af7f9c694f21402f9f8f027635c738e8/t.jpg';
         nra(items);
 }
