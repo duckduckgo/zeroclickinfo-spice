2496cecada45858c4ec49a473add2c1351240c33
kogalex <kogalex@gmail.com>
2012-07-12 16:36:06 +0300

    adding is this you header

--- share/spice/pipl/spice.js
+++ share/spice/pipl/spice.js
@@ -1,3 +1,5 @@
+CONST_PIPL_PRIVACY_URL = 'http://pipl.com/privacy_ddg/'
+
 
 function ddg_spice_pipl(results) {
 	if (!results['error']) {
@@ -8,27 +10,51 @@ function ddg_spice_pipl(results) {
 	    items[0]["force_big_header"] = true;
 	    var d = document;
 	    var content = d.createElement('span');
-	    var div, ul;
+	    var div, ul, is_this_you;
 	    
 	    
 		if (!(typeof results['profile'] === "undefined")) {
+			// single profile
+			
+			//create tagline
 			div = d.createElement('div');
 			div.appendChild(d.createTextNode(results['profile']['tagline']));
+			YAHOO.util.Dom.addClass(div, 'tagline');
 			content.appendChild(div);
 			
 			links = results['profile']['links'];
 			ul = createUnorderedList(links, 'results')
 			content.appendChild(ul);
 			
+			// profile image
 		    items[0]['i'] = results['profile']['image'];
 		} else {
+			// suggestions
 			links = results['suggestions'];
 			ul = createUnorderedList(links, 'suggestions')
 			content.appendChild(ul);
 		}
+		
+		// clear everything before the attribution
+		div = d.createElement('div');
+		YAHOO.util.Dom.addClass(ul, 'clear');
+		content.appendChild(div);
+		
 		items[0]['a'] = content;
 
 		nra(items);
+		
+
+
+		//create "is this you? header"
+		var spice_header_element = YAHOO.util.Dom.get('zero_click_header');
+		if (spice_header_element) {
+			is_this_you = d.createElement('a');
+			is_this_you.href = CONST_PIPL_PRIVACY_URL;
+			is_this_you.appendChild(d.createTextNode('Is this you? click here to learn more about privacy');
+			YAHOO.util.Dom.addClass(is_this_you, 'pipl-privacy-link');
+		}
+				
 	}
 }
 
@@ -39,6 +65,7 @@ function createUnorderedList(links, ulClass) {
 	for (var i=0; i<links.length; i++) {
 		li = d.createElement('li');
 		link = d.createElement('a');
+		link.href = links[i]['url'];
 		if (!(typeof links[i]['icon'] === "undefined")) {
 			if (nur) img = nur('',links[i]['caption'],links[i]['icon']);
 			if (img) link.appendChild(img);
