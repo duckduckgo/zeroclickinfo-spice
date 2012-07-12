248958ae12abc99d870a2101ecdb646faedc7ce4
kogalex <kogalex@gmail.com>
2012-07-12 16:03:49 +0300

    addig css + changing html to Dom element

--- share/spice/pipl/spice.js
+++ share/spice/pipl/spice.js
@@ -1,3 +1,4 @@
+
 function ddg_spice_pipl(results) {
 	if (!results['error']) {
 		var items = [[]];
@@ -5,28 +6,46 @@ function ddg_spice_pipl(results) {
 	    items[0]['s'] = 'Pipl';
 	    items[0]['u'] = results['more_url'];
 	    items[0]["force_big_header"] = true;
-	    var content = "";
+	    var d = document;
+	    var content = d.createElement('span');
+	    var div, ul;
+	    
 	    
 		if (!(typeof results['profile'] === "undefined")) {
-			content += "<div>" + results['profile']['tagline'] + "</div>";
-			links = results['profile']['links'];
-			content += "<ul>";
-			for (var i=0; i<links.length; i++)
-				content += '<li><a href="' + links[i]['url'] + '">' + links[i]['caption'] +'</a></li>';
+			div = d.createElement('div');
+			div.appendChild(d.createTextNode(results['profile']['tagline']));
+			content.appendChild(div);
 			
-			content += "</ul>";
-		    
+			links = results['profile']['links'];
+			ul = createUnorderedList(links, 'results')
+			content.appendChild(ul);
+			
 		    items[0]['i'] = results['profile']['image'];
 		} else {
 			links = results['suggestions'];
-			content += "<ul>";
-			for (var i=0; i<links.length; i++)
-				content += '<li><a href="' + links[i]['url'] + '">' + links[i]['caption'] +'</a></li>';
-			
-			content += "</ul>";
+			ul = createUnorderedList(links, 'suggestions')
+			content.appendChild(ul);
 		}
 		items[0]['a'] = content;
 
 		nra(items);
 	}
+}
+
+function createUnorderedList(links, ulClass) {
+	var ul, li, a, img, d = document;
+	ul = d.createElement('ul');
+	YAHOO.util.Dom.addClass(ul, ulClass);
+	for (var i=0; i<links.length; i++) {
+		li = d.createElement('li');
+		link = d.createElement('a');
+		if (!(typeof links[i]['icon'] === "undefined")) {
+			if (nur) img = nur('',links[i]['caption'],links[i]['icon']);
+			if (img) link.appendChild(img);
+		}
+		link.appendChild(d.createTextNode(links[i]['caption']));
+		li.appendChild(link);
+		ul.appendChild(li);
+	}
+	return ul;
 }
