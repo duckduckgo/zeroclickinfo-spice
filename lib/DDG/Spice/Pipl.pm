b0eda38b5a9b2d09c073870c19a3177aecbfc78e
kogalex <kogalex@gmail.com>
2012-07-22 13:03:37 +0300

    making regex a little bit more selective

--- lib/DDG/Spice/Pipl.pm
+++ lib/DDG/Spice/Pipl.pm
@@ -4,7 +4,7 @@ use DDG::Spice;
 
 #triggers query => qr/^([A-Za-z]+\s[A-Za-z]+)$/;
 
-triggers query => qr/^(.+)$/;
+triggers query => qr/^(\w+\s\w+\s?\w*)|([\d\s\-\+\.()]{6,20})|([a-zA-Z0-9._%\-+]+@[a-zA-Z0-9._%\-]+\.[a-zA-Z]{2,6})$/;
 
 spice to => 'http://75.126.80.72/duckduckapi/?q=$1&callback={{callback}}';
 spice wrap_jsonp_callback => 1;
