e0ca91beb94e9828a12688d684e514136fe2dabb
moollaza <moollaza@gmail.com>
2012-07-27 12:33:40 -0400

    redid Hayoo layout in spice.js - ready for deploy

--- lib/DDG/Spice/Hayoo.pm
+++ lib/DDG/Spice/Hayoo.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Hayoo;
 
 use DDG::Spice;
 
-triggers start => "hayoo";
+triggers start => "hayoo", "haskell api";
 
 spice to => 'http://holumbus.fh-wedel.de/hayoo/hayoo.json?query=$1';
 spice wrap_jsonp_callback => 1;
--- share/spice/hayoo/spice.js
+++ share/spice/hayoo/spice.js
@@ -1,44 +1,28 @@
-// Hayoo returns separate results for definitions, modules, packages, and other search queries
-// we will just show "functions" results for now (which means everything that isn't a module or a package)
-// we also won't show any "did you mean..." stuff, because it's not generally that helpful
 function ddg_spice_hayoo(res) {
-  var query = DDG.get_query();
-  query = query.replace(/^hayoo\s+/i, '');
-
+ 
   if (res['hits'] > 0) {
-    var items = new Array();
-    var item = new Array();
+    var query = DDG.get_query().replace(/\s*hayoo\s*/i, '');
 
-    // the individual function's json
-    var info = res['functions'][0];
+    var item = res['functions'][0];
 
-    // build some nice output
-    var snippet = document.createElement('span');
-    snippet.innerHTML += '<code>' + info['name'] + ' :: ' + info['signature'] + '</code><br>'; // answer
-    if (info['description'] != '') {
-      // Hayoo uses scripts to fill empty <a> tags
-      var description = info['description'].replace(/<a>/g,'');
+    var snippet = '<pre><code>' + item.name + ' :: ' + item.signature + '</code></pre>'
+                + 'Package: ' + item.package + '<br>' + 'Module: ' + item.module + '<br>';
+    
+    if (item.description != '') {
+      var description = item.description.replace(/<a>/g,'');
       description = description.replace(/<\/a>/g,'');
 
-      snippet.innerHTML += description;
+      snippet += description;
     }
 
-    // build item
-    item['a'] = snippet;
+    var items = [[]];
+    items[0]['a'] = snippet;
+    items[0]['h'] = 'Hayoo! (' + query + ')';
+    items[0]['s'] = 'Hackage';
+    items[0]['u'] = item.uri;
+    items[0]['f'] = 1;
+    items[0]['force_big_header'] = 1;
 
-    item['h'] = 'Hayoo - <b>' + info['module'] + '</b>.' + info['name']; // title
-
-    // build a link back to Hayoo, rather than Hackage - it's polite, and users rarely want exactly one result when searching by type
-    item['s'] = 'Hayoo'; // source
-    item['u'] = 'http://holumbus.fh-wedel.de/hayoo/hayoo.html?query=' + query; // url
-
-    item['t'] = info['module'];
-
-    // shove it in the array and print
-    items[0] = item;
-    nra(items, 1);
-  } else {
-    // no hits, no zero-click info
+    nra(items);
   }
 }
-
