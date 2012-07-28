62bd06be372319bfd684b9fd1d281de7abfc7bf1
MithrandirAgain <mithrandiragain@lavabit.com>
2012-07-28 01:42:56 -0700

    torrent: included dht into regex and fixed javascript replace

--- lib/DDG/Spice/Torrent.pm
+++ lib/DDG/Spice/Torrent.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 attribution twitter => 'mithrandiragain',
             web => ['http://www.atomitware.tk/mith','MithrandirAgain'];
 
-triggers query_lc => qr/\burn:btih:([A-F\d]{40})\b|(?:bittorrents?|torrents?|bt|magnet)\s+(.*)/i;
+triggers query_lc => qr/\burn:btih:([A-F\d]{40})\b|dht:\/\/([A-F\d]{40})(?:\.dht)*|(?:bittorrents?|torrents?|bt|magnet)\s+(.*)/i;
 
 spice wrap_jsonp_callback => 1;
 spice to => 'http://isohunt.com/js/json.php?ihq=$1&rows=4&sort=seeds';
--- share/spice/torrent/spice.js
+++ share/spice/torrent/spice.js
@@ -12,7 +12,7 @@ function ddg_spice_torrent(to) {
         var query, title, url, items, content;
         query = to.title.replace("isoHunt > All > ", "");
         title = query+" (Torrents)";
-        query = query.replace(" ", "+");
+        query = query.replace(/\s/g, "+");
         url = "http://isohunt.com/torrents/?ihq="+query;
         items = to.items.list;
         content = "";
@@ -20,12 +20,12 @@ function ddg_spice_torrent(to) {
         // Order isn't guaranteed with foreach loops
         // so use a plain for loop.
         for (var i = 0; i < items.length; i++) {
-            torrentTitle = search_codeStrip(items[i].title);
+            torrentTitle = search_codeStrip(to.items.list[i].title);
             torrentTitle = (torrentTitle.length > 50) ? torrentTitle.slice(0, 50)+"..." : torrentTitle;
-            torrentUrl = items[i].link;
+            torrentUrl = to.items.list[i].link;
             torrentUrl = torrentUrl.replace(query, "");
-            torrentSize = items[i].size;
-            torrentSeeds = (items[i].Seeds == 1) ? items[i].Seeds+" seed" : items[i].Seeds+" seeds";
+            torrentSize = to.items.list[i].size;
+            torrentSeeds = (to.items.list[i].Seeds == 1) ? to.items.list[i].Seeds+" seed" : to.items.list[i].Seeds+" seeds";
             content += "<div class='torrent"
                         +i+"'><a href ='"
                         +torrentUrl+"'>"
