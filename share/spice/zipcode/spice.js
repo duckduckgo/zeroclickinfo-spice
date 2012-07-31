0442b9e6c8b5d8d6618a13d3dca413bd50382b28
moollaza <moollaza@gmail.com>
2012-07-31 10:52:12 -0400

    finished Zipcode spice, reworked layout, and some js logic. Added some css

--- share/spice/zipcode/spice.js
+++ share/spice/zipcode/spice.js
@@ -1,33 +1,33 @@
+var loc_string = "";
+
 function ddg_spice_zipcode(result) {
     var snippet = '';
-
     if (result['places']['place']) {
     	var places = result['places']['place'];
         var place = places[0];
        
-//      build up a place name from whatever components are available in the result
-        var loc_string = "";
+        //build up a place name from whatever components are available in the result
         var location_types = ['locality2', 'locality1', 'admin2', 'admin1'];
         for (i in location_types) {
         	if (place[location_types[i]]) loc_string += place[location_types[i]] + ', ';
         }
         if (place['country']) loc_string += place['country'];
        
-        snippet = format_snippet(place, places);    	   
+        snippet = format_snippet(place, places);	   
         items = new Array();
         items[0] = new Array();
         items[0]['a'] = snippet;
-        items[0]['h'] = place['name'] + ' is a ' + place['postal attrs']['type'] + ' in ' + loc_string;
-        items[0]['s'] = 'Yahoo GeoPlanet Web Service';
-        items[0]['u'] = 'http://where.yahooapis.com/v1/place/' + place['woeid'] + '?appid={{ENV{DDG_SPICE_ZIPCODE_APIKEY}}}';
-        console.log(items);
-        nra(items);
+        items[0]['h'] = loc_string;
+        items[0]['s'] = 'MapQuest';
+        items[0]['u'] = 'http://mapq.st/map?q=' + loc_string;
+        items[0]['f'] = 1;
+        items[0]['force_big_header'] = 1;
+        nra(items, 0, 1);
     }
 }
 
-
 function format_snippet(place, places){	
-//	construct the bounding box for the postal code
+    //construct the bounding box for the postal code
 	var coords = place['boundingBox'];
 	var box = [[coords['southWest']['latitude'], coords['southWest']['longitude']],
 	           [coords['northEast']['latitude'], coords['southWest']['longitude']], 
@@ -38,31 +38,37 @@ function format_snippet(place, places){
 	var coord_string = box.join(','); // change to '|' separator for Google Maps
 	var center = [place['centroid']['latitude'], place['centroid']['longitude']].join()
 
-//  bigbox sets a slightly larger frame for MapQuest's 'bestfit' parameter; not needed for Google
+    //bigbox sets a slightly larger frame for MapQuest's 'bestfit' parameter; not needed for Google
 	var boxpad = (coords['northEast']['latitude'] - coords['southWest']['latitude']) * .25
 	var bigbox = [[coords['northEast']['latitude']+boxpad, coords['southWest']['longitude']],  
 		          [coords['southWest']['latitude']-boxpad, coords['northEast']['longitude']]].join();
 
 
-	var snippet, div;	
-	snippet = d.createElement('span');
-    div = d.createElement('div');
-    div.innerHTML = 'Approximate area:';
-//  comment out mapquest call, uncomment google, and change separator in coord_string above to '|' to use google's static map 
-//  div.innerHTML += '<br><img src="http://maps.googleapis.com/maps/api/staticmap?size=400x150&sensor=false&path=fillcolor:0xAA000033|color:0xff0000ff|weight:0|' + coord_string + '"></img>';
-    div.innerHTML += '<br><img src="http://open.mapquestapi.com/staticmap/v4/getmap?size=400,150&scalebar=false&bestfit=' + bigbox + '&polygon=fill:0x30ff0000|color:0x20ff0000|width:1|' + coord_string + '"></img>';
+	var div = d.createElement('div');
+    YAHOO.util.Dom.setStyle(div, "margin-bottom", "5px");
+    div.innerHTML = '<span>' + place['postal attrs']['type'] + " " + place['name'] + ' approximate area:</span>';
+    div.innerHTML += '<br>';
+    var img = d.createElement("img");
+    img.id = "map";
+    img.src = 'http://www.duckduckgo.com/imq2/?size=623,150&scalebar=false&bestfit=' + bigbox + '&polygon=fill:0x30ff0000|color:0x20ff0000|width:1|' + coord_string;
+    var img_link = d.createElement("a");
+    img_link.href = 'http://mapq.st/map?q=' + loc_string;
+    img_link.appendChild(img);
+    div.appendChild(img_link);
 
-//  construct a list of other possible hits if available; generates new DDG inputs with country codes in parentheses to force the right code to the top of the results
+    //construct a list of other possible hits if available; generates new DDG inputs with country codes in parentheses to force the right code to the top of the results
     if (places.length > 1) {
-        div.innerHTML += 'Similar postal codes: ';
-    	for (i=1;i<places.length;i++){
+    	var similar = '';
+        for (i=1;i<places.length;i++){
     		if (places[i]['name'] == place['name']){
-    			div.innerHTML += '<a href = "/?q=postcode+' + encodeURI(places[i]['name']) + '%20(' + places[i]['country attrs']['code']+ ')">' + places[i]['name'] + "</a> (" + places[i]['country'] + ')  ';	
-    		}	
+            	similar += '<a href = "http://www.duckduckgo.com/?q=postcode+' + encodeURI(places[i]['name']) + '%20(' + places[i]['country attrs']['code']+ ')">' + places[i]['name'] + "</a> (" + places[i]['country'] + ')  ';	
+    		}
     	}
+        if (similar != ''){
+            div.innerHTML += 'Similar postal codes: ';
+            div.innerHTML += similar;
+        }
     }
 
-    snippet.appendChild(div);
-
-    return snippet;
+    return div;
 }
