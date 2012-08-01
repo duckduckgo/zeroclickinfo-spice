46d3ef990eff0efc6a9464ca8d9309f47d3b270b
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-01 21:55:25 +0200

    add RandNums spice plugin

---
+++ lib/DDG/Spice/RandNums.pm
@@ -0,0 +1,22 @@
+package DDG::Spice::RandNums;
+
+use DDG::Spice;
+
+spice to   => 'http://www.random.org/integers/?num=10&min=$1&max=$2&col=10&base=10&format=plain&rnd=new';
+spice from => '([0-9]+)\/([0-9]+)';
+
+spice is_cached => 0;
+spice wrap_string_callback => 1;
+
+triggers query_lc => qr/^random nums ?(([0-9]+)\-([0-9]+))?$/;
+
+handle matches => sub {
+	my (undef, $a, $b) = @_;
+
+	my $min = $a ? $a : 1;
+	my $max = $b ? $b : 100;
+
+	return ($min, $max)
+};
+
+1;
