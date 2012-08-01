db09c726ba5d5d945cdcf2d775a029f924a267c8
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-02 00:06:12 +0200

    handle errors in Translate plugin

--- share/spice/translate/spice.js
+++ share/spice/translate/spice.js
@@ -1,13 +1,17 @@
 function ddg_spice_translate(ir) {
 	var items = new Array();
-	var ts    = ir.term0.PrincipalTranslations;
 
 	items[0] = new Array();
-
-	items[0]['a'] = ts[0].FirstTranslation.term;
 	items[0]['s'] = 'Wordreference.com';
 	items[0]['u'] = 'http://wordreference.com/';
-	items[0]['f'] = 1;
+
+	if (ir["Error"]) {
+		items[0]['a'] = ir["Note"];
+	} else {
+		var ts = ir.term0.PrincipalTranslations;
+
+		items[0]['a'] = ts[0].FirstTranslation.term;
+	}
 
 	nra(items);
 }
