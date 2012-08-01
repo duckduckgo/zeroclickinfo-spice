1807a9f13048450da0121e4458a86c568c735cd0
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-01 21:55:25 +0200

    add RandNums spice plugin

---
+++ share/spice/rand_nums/spice.js
@@ -0,0 +1,11 @@
+function ddg_spice_rand_nums(ir) {
+	items = new Array();
+	items[0] = new Array();
+
+	items[0]['a'] = ir;
+	items[0]['h'] = '';
+	items[0]['s'] = 'Random.org';
+	items[0]['u'] = 'http://random.org/';
+
+	nra(items);
+}
