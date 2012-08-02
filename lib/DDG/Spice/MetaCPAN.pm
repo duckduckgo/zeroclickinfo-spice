06bb9e96cc56061e2190e5fec9f7591a4b46065a
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-02 16:16:26 +0200

    add attribution for MetaCPAN plugin

--- lib/DDG/Spice/MetaCPAN.pm
+++ lib/DDG/Spice/MetaCPAN.pm
@@ -2,6 +2,9 @@ package DDG::Spice::MetaCPAN;
 
 use DDG::Spice;
 
+attribution github  => ['https://github.com/AlexBio', 'AlexBio'  ],
+            web     => ['http://ghedini.me', 'Alessandro Ghedini'];
+
 spice to   => 'http://api.metacpan.org/v0/module/$1?callback={{callback}}';
 
 triggers query => qr/^cpan ([A-Za-z0-9\:]+)$/;
