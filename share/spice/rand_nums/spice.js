c559c91ec97b472c274a81ae790a3331a61df46e
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-05 14:07:33 +0200

    improve More at link in RandNums

--- share/spice/rand_nums/spice.js
+++ share/spice/rand_nums/spice.js
@@ -2,10 +2,22 @@ function ddg_spice_rand_nums(ir) {
 	items = new Array();
 	items[0] = new Array();
 
+	query  = DDG.get_query();
+	regexp = /^random (numbers|nums)(?: ([0-9]+)\-([0-9]+)|)$/i;
+	match  = query.match(regexp);
+	min    = match[2];
+	max    = match[3];
+
+	if (min == undefined)
+		min = 0;
+
+	if (max == undefined)
+		max = 100;
+
 	items[0]['a'] = ir;
 	items[0]['h'] = '';
 	items[0]['s'] = 'Random.org';
-	items[0]['u'] = 'http://random.org/';
+	items[0]['u'] = 'http://www.random.org/integers/?num=100&min=' + min + '&max=' + max + '&col=5&base=10&format=html&rnd=new';
 
 	nra(items);
 }
