e8e5b464eb99a541017bfd245040b9ec8cc3cbcd
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-06 15:38:07 +0200

    improve shown results in Translate callback

--- share/spice/translate/spice.js
+++ share/spice/translate/spice.js
@@ -1,17 +1,29 @@
 function ddg_spice_translate(ir) {
-	var items = new Array();
+	items = new Array();
+
+	query  = DDG.get_query();
+	regexp = /^translate ([a-z]+) from ([a-z]+) to ([a-z]+)$/;
+	match  = query.match(regexp);
+	word   = match[1];
+	from   = match[2];
+	to     = match[3];
 
 	items[0] = new Array();
+	/* items[0]["h"] = 'lol'; */
 	items[0]['s'] = 'Wordreference.com';
-	items[0]['u'] = 'http://wordreference.com/';
+	items[0]['u'] = 'http://wordreference.com/' + from + to + '/' + word;
 
-	if (ir["Error"]) {
-		items[0]['a'] = ir["Note"];
-	} else {
-		var ts = ir.term0.PrincipalTranslations;
+	if (ir["Error"])
+		return;
 
-		items[0]['a'] = ts[0].FirstTranslation.term;
-	}
+	ts = ir.term0.PrincipalTranslations;
+
+	text = ts[0].FirstTranslation.term;
+
+	if (ts[0].SecondTranslation != undefined)
+		text += ' (' + ts[0].SecondTranslation.term + ')';
+
+	items[0]['a'] = text;
 
 	nra(items);
 }
