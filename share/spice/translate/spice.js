6ca6cd5f2347e87ed26929fb9e440035d2c35570
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-06 21:06:23 +0200

    improve output format in Translate callback

--- share/spice/translate/spice.js
+++ share/spice/translate/spice.js
@@ -9,21 +9,43 @@ function ddg_spice_translate(ir) {
 	to     = match[3];
 
 	items[0] = new Array();
-	/* items[0]["h"] = 'lol'; */
+	items[0]["h"] = 'Translations for <i>' + word + '</i>';
 	items[0]['s'] = 'Wordreference.com';
 	items[0]['u'] = 'http://wordreference.com/' + from + to + '/' + word;
+	items[0]["force_big_header"] = true;
 
 	if (ir["Error"])
 		return;
 
-	ts = ir.term0.PrincipalTranslations;
+	text = '<ul>';
 
-	text = ts[0].FirstTranslation.term;
+	text += format_translations(ir.term0.PrincipalTranslations);
 
-	if (ts[0].SecondTranslation != undefined)
-		text += ' (' + ts[0].SecondTranslation.term + ')';
+	if (ir.term0.AdditionalTranslations)
+		text += format_translations(ir.term0.AdditionalTranslations);
+
+	text += '</ul>';
 
 	items[0]['a'] = text;
 
 	nra(items);
+}
+
+function format_translations(ts) {
+	text = '';
+
+	for (i in ts) {
+		text += format_translation(ts[i].FirstTranslation);
+
+		if (ts[i].SecondTranslation != undefined)
+			text += format_translation( ts[i].SecondTranslation);
+	}
+
+	return text;
+}
+
+function format_translation(t) {
+	text = '<li><i>' + t.term + '</i> (' + t.POS + ')</li>';
+
+	return text;
 }
