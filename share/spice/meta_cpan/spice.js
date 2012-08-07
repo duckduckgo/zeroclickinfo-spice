9d89abbafd7d6cdb5a3ba6c766925ffbd7627999
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-07 18:36:31 +0200

    show long description too in MetaCPAN results

--- share/spice/meta_cpan/spice.js
+++ share/spice/meta_cpan/spice.js
@@ -1,22 +1,35 @@
 function ddg_spice_meta_cpan(ir) {
 	items = new Array();
-	items[0] = new Array();
-
-	items[0]['s'] = 'MetaCPAN';
-	items[0]['h'] = '';
 
 	if (ir["message"])
 		return;
 
-	text = ir["documentation"];
-	abst = ir["abstract"];
+	name = ir["documentation"];
+	desc = ir["description"];
 
-	if (ir["abstract"]) {
-		text += ' - ' + ir["abstract"];
+	head = 'MetaCPAN (' + name + ')';
+
+	items[0] = new Array();
+	items[0]['h'] = head;
+	items[0]['s'] = 'MetaCPAN';
+	items[0]['u'] = 'http://metacpan.org/module/' + name;
+	items[0]["force_big_header"] = true;
+
+	text = '';
+
+	if (desc) {
+		short_desc = '';
+		max_len = 260;
+
+		if (desc.length > max_len)
+			short_desc = desc.substring(0, max_len) + '...';
+		else
+			short_desc = desc;
+
+		text += '<p>' + short_desc + '<p>';
 	}
 
 	items[0]['a'] = text;
-	items[0]['u'] = 'http://metacpan.org/module/' + ir["documentation"];
 
 	nra(items);
 }
