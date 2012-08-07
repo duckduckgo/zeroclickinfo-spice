02fd4f27a556b3f895b17e5a87eceaee9bfd825d
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-07 12:23:50 +0200

    allow negative numbers in RandNums queries

--- lib/DDG/Spice/RandNums.pm
+++ lib/DDG/Spice/RandNums.pm
@@ -9,13 +9,13 @@ attribution github  => ['https://github.com/AlexBio', 'AlexBio'  ],
             web     => ['http://ghedini.me', 'Alessandro Ghedini'];
 
 spice to   => 'http://www.random.org/integers/?num=10&min=$1&max=$2&col=10&base=10&format=plain&rnd=new';
-spice from => '([0-9]+)\/([0-9]+)';
+spice from => '(\-?[0-9]+)\/(\-?[0-9]+)';
 
 spice is_cached => 0;
 spice proxy_cache_valid => "418 1d";
 spice wrap_string_callback => 1;
 
-triggers query_lc => qr/^(rand|random) (numbers|nums)(?: ([0-9]+)\-([0-9]+)|)$/;
+triggers query_lc => qr/^(rand|random) (numbers|nums)(?: (\-?[0-9]+)\-(\-?[0-9]+)|)$/;
 
 handle matches => sub {
 	my (undef, undef, $a, $b) = @_;
