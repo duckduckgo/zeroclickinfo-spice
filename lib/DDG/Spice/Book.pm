e089b19f6c96cc4833a29c6ee89568d46b743060
Mohit Aggarwal <mohitagg@gmail.com>
2012-08-06 19:01:48 -0700

    first draft of snippet

--- lib/DDG/Spice/Book.pm
+++ lib/DDG/Spice/Book.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Book;
 
 use DDG::Spice;
 
-spice to => 'http://127.0.0.1:3000/newbooks/api.json?q=$1&callback={{callback}}';
+spice to => 'http://127.0.0.1:3000/newbooks/api.json?api_key=duckduckgo&q=$1&callback={{callback}}';
 
 triggers startend => "readometer","book", "book review", "book reviews","critics","critic rating","critic reviews", "review", "reviews", "idreambooks", "rating";
 
--- share/spice/book/spice.js
+++ share/spice/book/spice.js
@@ -1,15 +1,63 @@
-function ddg_spice_book(response) {
-    // var snippet = 'test';
-    // ir['status']= 'OK';
-    //System.out.println("Hello World!");
-    // if (ir['status'] == 'OK') {
-       // snippet = ir['abstract'];
+// function ddg_spice_book(ir) {
+//     // var snippet = 'test';
+//     // ir['status']= 'OK';
+//     //System.out.println("Hello World!");
+//     if (ir['total_results'] > 0) {
+//        var result = ir[book]
+//        var  snippet = result.title + 'is rated '+ result.rating + '% by ' + result.review_count + ' critics.' ;
+
+
+
+//        items = new Array();
+//        items[0] = new Array();
+//        items[0]['a'] = snippet;
+//        items[0]['h'] = header;
+//        items[0]['s'] = 'idreambooks';
+//        items[0]['u'] = result.url;
+//        items[0]['i'] = result.image;
+//        nra(items);
+//     }
+// }
+
+
+function ddg_spice_book(ir) {
+
+    if (ir['total_results'] > 0) {
+
+      // Making an array of results
+      var book = ir['book'];
+      
+      // snippet
+      var  snippet = book.title + 'is rated '+ book.rating + '% by ' + book.review_count + ' critics.' ;
+
+      // Make title for header
+      var title = book.title;
+      var author = book.author;
+      var header = title;
+      if (author.length > 0) header += ' by ' + author;
+      var header = header.substring(0,49);
+      if (title.length > 50) header += '...';
+
        items = new Array();
        items[0] = new Array();
-       items[0]['a'] = response['snippet'];
-       items[0]['h'] = 'Readometer';
+       items[0]['a'] = snippet;
+       items[0]['h'] = header;
+
+       // Source name and url for the More at X link.
        items[0]['s'] = 'idreambooks';
-       items[0]['u'] = 'http://idreambooks.com';
+       items[0]['u'] = book.url;
+       
+       // Options
+       items[0]['force_big_header'] = true;
+       items[0]['force_space_after'] = true;
+
+       // Thumbnail url
+       //items[0]['i'] = book.image;
+       items[0]['i'] = 'https://d21k36m3c0ezbf.cloudfront.net/uploads/asset/image/293/passport_9781594202292.jpg';
+       //items[0]['i'] = 'http://localhost:3000//uploads/asset/image/111/thumb_9781594202292.jpg';
+
+       // The rendering function is nra.
        nra(items);
-    // }
+     }
+
 }
