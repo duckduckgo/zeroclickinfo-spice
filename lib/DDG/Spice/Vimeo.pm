fd7ea89c9c2efd86b758c146856460eff09a4b93
Ado Matejov <a.matejov@centrum.sk>
2012-08-10 19:04:06 +0200

    initial commit for vimeo plugin

---
+++ lib/DDG/Spice/Vimeo.pm
@@ -0,0 +1,19 @@
+package DDG::Spice::Vimeo;
+# ABSTRACT: Embeds videos from vimeo
+
+use DDG::Spice;
+
+spice to => 'http://vimeo.com/api/oembed.json?url=$1&maxwidth=300&maxheight=300&callback={{callback}}';
+
+triggers query_lc => qr/^vimeo\s(.*)|^https?:\/\/vimeo.com\/[0-9]+/;
+
+handle matches => sub {
+    if (/^http:\/\/vimeo.com\/[0-9]+/) {
+        return $_;
+    } elsif ($1) {
+        return $1;
+    }    
+    return;
+};
+
+1;
---
+++ share/spice/vimeo/spice.js
@@ -0,0 +1,20 @@
+function ddg_spice_vimeo(res)
+{
+    var out = '';
+
+    out += 'By <a href="' + res["author_url"] +'">' 
+        +           res["author_name"]
+        +      '</a><br />'
+        +  '<i>' + res["description"] + '</i><br />';
+
+    //embed video in iframe
+
+    out += res["html"];
+
+
+    items = [[]];
+    items[0]['a'] = out;
+    items[0]['h'] = 'Vimeo (' + res["title"] + ')';
+    
+    nra(items);
+}
