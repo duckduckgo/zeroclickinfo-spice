8adb39a2345bd5dfc9cb17014522bda44de652be
Ado Matejov <a.matejov@centrum.sk>
2012-08-11 21:41:44 +0200

    added api key

--- lib/DDG/Spice/Vimeo/Search.pm
+++ lib/DDG/Spice/Vimeo/Search.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Vimeo::Search;
 
 use DDG::Spice;
 
-spice to => '';
+spice to => 'http://vimeo.com/api/rest/v2?format=json&method=vimeo.videos.search&per_page=7&summary_response=1&oauth_consumer_key=e1f6213a20a462b20d5c7fa4f1217eb22490091e&query=$1&callback={{callback}}';
 
 triggers startend => 'vimeo';
 
