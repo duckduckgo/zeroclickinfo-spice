70184051aa01756b1eadb8122b4ad6f7575629a2
Mohit Aggarwal <mohitagg@gmail.com>
2012-08-12 05:48:08 -0500

    potty trained and dressed up. Ready for interview

--- lib/DDG/Spice/Book.pm
+++ lib/DDG/Spice/Book.pm
@@ -2,9 +2,9 @@ package DDG::Spice::Book;
 
 use DDG::Spice;
 
-spice to => 'http://127.0.0.1:3000/newbooks/api.json?api_key=duckduckgo&q=$1&callback={{callback}}';
+spice to => 'http://localhost:3000/newbooks/api.json?api_key=duckduckgo&q=$1&callback={{callback}}';
 
-triggers startend => "readometer","book", "book review", "book reviews","critics","critic rating","critic reviews", "review", "reviews", "idreambooks", "rating";
+triggers startend => "readometer","book", "books", "book review", "book reviews","critics","critic","critic rating","critic reviews", "review", "reviews", "idreambooks", "rating";
 
 handle remainder => sub {
     return $_;
--- share/spice/book/spice.js
+++ share/spice/book/spice.js
@@ -1,61 +1,72 @@
-// function ddg_spice_book(ir) {
-//     // var snippet = 'test';
-//     // ir['status']= 'OK';
-//     //System.out.println("Hello World!");
-//     if (ir['total_results'] > 0) {
-//        var result = ir[book]
-//        var  snippet = result.title + 'is rated '+ result.rating + '% by ' + result.review_count + ' critics.' ;
-
-
-
-//        items = new Array();
-//        items[0] = new Array();
-//        items[0]['a'] = snippet;
-//        items[0]['h'] = header;
-//        items[0]['s'] = 'idreambooks';
-//        items[0]['u'] = result.url;
-//        items[0]['i'] = result.image;
-//        nra(items);
-//     }
-// }
-
-
 function ddg_spice_book(ir) {
 
     if (ir['total_results'] > 0) {
 
       // Making an array of results
       var book = ir['book'];
-      
-      // snippet
-      var  snippet = book.title + 'is rated '+ book.rating + '% by ' + book.review_count + ' critics.' ;
 
-      // Make title for header
       var title = book.title;
       var author = book.author;
+      var date = book.release_date;
+      if (date.length > 0) var year = date.substring(0,4);
+      var pages = book.pages;
+
+     
+      // header
       var header = title;
-      if (author.length > 0) header += ' by ' + author;
-      var header = header.substring(0,49);
-      if (title.length > 50) header += '...';
+      if (author) header += ' by ' + author;
+      if (year.length == 4) header += ' (' + year + ')'
+      var header_short = header.substring(0,59);
+      if (header.length > 60) header_short += ' ...';
+   
+      // genre and page info on book
+      var bio = book.genre + ' book';
+      if (pages) bio += ', ' + pages + ' pages';
+
+      // rating overview
+      var  rating  = book.title + ' (' + bio + ') '  +' is rated '
+                   + "<a href=" 
+                        + book.link + "#bookreviews>" +  book.rating + 
+                        '% by ' + book.review_count + ' critics. '
+                   + "</a>";
+
+      //synopsis
+      var synopsis = book.synopsis;
+      var synopsis_short = "<i>Synopsis: </i>" + synopsis.substring(0,199);
+      if (synopsis.length > 200) synopsis_short += ' ...';
+
+      //snippet
+      var snippet = book.review_sample.snippet;
+      var source = book.review_sample.source;
+      var snippet_short = snippet.substring(0,139);
+      if (snippet.length > 140 ) snippet_short += ' ...';
+      var review = ' <i> "' + snippet_short + '"</i>';
+      if (source) review += '<b> - ' + source + '</b>' ;
+
+      //Summary
+      var summary = "<div align='justify';>" + rating + synopsis_short + '</div>' + '<center>' + review + '</center>';
+    
 
        items = new Array();
        items[0] = new Array();
-       items[0]['a'] = snippet;
-       items[0]['h'] = header;
+       items[0]['a'] = summary;
+       items[0]['h'] = header_short;
 
        // Source name and url for the More at X link.
        items[0]['s'] = 'idreambooks';
-       items[0]['u'] = book.url;
+       items[0]['u'] = book.link;
        
        // Options
        items[0]['force_big_header'] = true;
        items[0]['force_space_after'] = true;
+       items[0]['f'] = 1;
 
-       // Thumbnail url
-       //items[0]['i'] = book.image;
+       // Thumbnail url //works with public images only 
+       items[0]['i'] = book.image;
        items[0]['i'] = 'https://d21k36m3c0ezbf.cloudfront.net/uploads/asset/image/293/passport_9781594202292.jpg';
-       //items[0]['i'] = 'http://localhost:3000//uploads/asset/image/111/thumb_9781594202292.jpg';
+      
 
+       
        // The rendering function is nra.
        nra(items);
      }
