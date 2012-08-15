6a6640d6b17059fa0f8e93eb5c7f1e173d06ec8d
Ado Matejov <a.matejov@centrum.sk>
2012-08-15 15:58:17 +0200

    event listener

--- share/spice/vimeo/video/spice.js
+++ share/spice/vimeo/video/spice.js
@@ -1,7 +1,7 @@
 function ddg_spice_vimeo_video(res)
 {
     var out = '';
-    var div, divtitle, divres, img, img2, whole, divby;
+    var div, divtitle, divres, img, img2, whole, divby, embed;
     var vimeo_url = 'http://vimeo.com/';
     
     
@@ -12,11 +12,16 @@ function ddg_spice_vimeo_video(res)
     divres = d.createElement("div");
     img = d.createElement("img");
     img2 = d.createElement("img");
+    embed = d.createElement("div");
+
+    embed.innerHTML = res["html"];
+	YAHOO.util.Dom.setStyle(embed, "display", "none");
 
     //div for thumbnail
 	YAHOO.util.Dom.setStyle(div, "margin-right", "10px");
 	YAHOO.util.Dom.setStyle(div, "clear", "left");
 	YAHOO.util.Dom.setStyle(div, "float", "left");
+	YAHOO.util.Dom.setStyle(div, "display", "block");
 	//YAHOO.util.Dom.setStyle(div, "overflow", "hidden");
 
     //add thumbnail and play button	
@@ -33,19 +38,23 @@ function ddg_spice_vimeo_video(res)
     div.appendChild(img2);
 	YAHOO.util.Dom.addClass(div, 'thumbnail');
 	YAHOO.util.Dom.addClass(div, 'inline');
-    
 
     divtitle.innerHTML = res["title"];
     divby.innerHTML = '<i>Author:</i> <a href="' + res["author_url"] +'">' + res["author_name"] + '</a>';
 
-
     whole.appendChild(div);
     whole.appendChild(divtitle);
     whole.appendChild(divby);
+    whole.appendChild(embed);
 
     divres.appendChild(whole);
 
     out = divres.innerHTML;
+
+    YAHOO.util.Event.addListener(div, "click", function (e) {
+        YAHOO.util.Dom.setStyle(div, "display", "none");
+	    YAHOO.util.Dom.setStyle(embed, "display", "block");
+    });
 
     items = [[]];
     items[0]['a'] = out;
