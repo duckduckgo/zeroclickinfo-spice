115a2e42debd8c583685058962d088e0325d8c68
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-27 12:19:33 +0200

    make 'from' optional in Wordreference plugin

--- lib/DDG/Spice/Wordreference.pm
+++ lib/DDG/Spice/Wordreference.pm
@@ -6,18 +6,41 @@ attribution github  => ['https://github.com/ghedo', 'ghedo'      ],
             web     => ['http://ghedini.me', 'Alessandro Ghedini'];
 
 my $dicts = 'ar|zh|cz|en|fr|gr|it|ja|ko|pl|pt|ro|es|tr';
+my $share = 'share/spice/wordreference';
 
 spice to   => 'http://api.wordreference.com/0.8/{{ENV{DDG_SPICE_TRANSLATE_APIKEY}}}/json/$1/$2?callback={{callback}}';
 spice from => '([a-z]+)\/([a-z]+)';
 
-triggers query_lc => qr/^translate ([a-z]+) from ($dicts) to ($dicts)$/;
+triggers query_lc => qr/^translate ([a-z]+)(?: from ($dicts))? to ($dicts)$/;
 
 handle matches => sub {
 	my ($word, $from, $to) = @_;
+
+	$from = $from ? $from : which_lang($word);
 
 	my $dict = $from.$to;
 
 	return ($dict, $word)
 };
+
+sub which_lang {
+	my ($word) = @_;
+
+	return 'en' if is_lang($word, 'english');
+	return 'it' if is_lang($word, 'italian');
+	return 'fr' if is_lang($word, 'french' );
+}
+
+sub is_lang {
+	my ($word, $lang) = @_;
+
+	my $out = 0;
+
+	open my $list, '<', $share.'/words/'.$lang;
+	$out = 1 if grep { chomp $_; /^$word$/ } <$list>;
+	close $list;
+
+	return $out;
+}
 
 1;
--- t/Wordreference.t
+++ t/Wordreference.t
@@ -16,4 +16,34 @@ ddg_spice_test(
 	),
 );
 
+ddg_spice_test(
+	[ qw(DDG::Spice::Wordreference) ],
+	'translate hello to it' => test_spice(
+		'/js/spice/wordreference/enit/hello',
+		call_type => 'include',
+		caller => 'DDG::Spice::Wordreference',
+		is_cached => 1
+	),
+);
+
+ddg_spice_test(
+	[ qw(DDG::Spice::Wordreference) ],
+	'translate ciao to en' => test_spice(
+		'/js/spice/wordreference/iten/ciao',
+		call_type => 'include',
+		caller => 'DDG::Spice::Wordreference',
+		is_cached => 1
+	),
+);
+
+ddg_spice_test(
+	[ qw(DDG::Spice::Wordreference) ],
+	'translate bonjour to en' => test_spice(
+		'/js/spice/wordreference/fren/bonjour',
+		call_type => 'include',
+		caller => 'DDG::Spice::Wordreference',
+		is_cached => 1
+	),
+);
+
 done_testing;
