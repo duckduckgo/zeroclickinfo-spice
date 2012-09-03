30b478698cabf981e61b12ac6164f62ea54965b3
Jag Talon <talon.jag@gmail.com>
2012-09-03 09:19:22 -0400

    Added other trigger words namely:
    'similar', 'band', 'bands', 'musician', 'musicians', 'player', 'artist',
    'artists', 'performer', 'performers', 'singer', 'singers', 'rapper', 'dj',
    'rappers', 'vocalist', 'vocalists', 'djs', 'songster', 'songsters'

--- lib/DDG/Spice/LastfmArtist.pm
+++ lib/DDG/Spice/LastfmArtist.pm
@@ -6,23 +6,24 @@ use DDG::Spice;
 spice to => 'http://ws.audioscrobbler.com/2.0/?format=json&method=artist.getinfo&artist=$1&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}_$2';
 spice from => '(?:([^/]*)/([^/]*)|)';
 
-triggers any => 'similar', 'band', 'bands', 'musician', 'musicians', 'player', 'artist', 'artists', 'performer', 'performers', 'singer', 'singers';
+triggers any => 'similar', 'band', 'bands', 'musician', 'musicians', 'player', 'artist', 'artists', 'performer', 'performers', 'singer', 'singers', 'rapper', 'dj', 'rappers', 'vocalist', 'vocalists', 'djs', 'songster', 'songsters';
 
 handle query_lc => sub {
+    my $synonyms = "bands?|musicians?|players?|artists?|performers?|singers?|rappers?|djs?|vocalists?|songsters?";
+
     #Queries like "bands similar to incubus" or "artists similar ben folds"
-    if(m{(?:bands?|musicians?|players?|artists?|performers?|singers?)\s+similar\s+(?:to\s+)?(\w+(?:\s+\w+)*)}) {
+    if(m{(?:$synonyms)\s+similar\s+(?:to\s+)?(\w+(?:\s+\w+)*)}) {
         return $1, 'similar';
     } 
     #Queries like "similar bands to incubus" or "similar artists ben folds"
-    if(m{similar\s+(?:bands?|musicians?|players?|artists?|performers?|singers?)\s+(?:to\s+)?(\w+(?:\s+\w+)*)}) {
+    if(m{similar\s+(?:$synonyms)\s+(?:to\s+)?(\w+(?:\s+\w+)*)}) {
         return $1, 'similar';
     } 
     #Queries like "30 seconds to mars similar bands" or "ben folds similar musicians"
-    if(m{(\w+(?:\s+\w+)*)\s+similar\s+(?:bands?|musicians?|players?|artists?|performers?|singers?)}) {
+    if(m{(\w+(?:\s+\w+)*)\s+similar\s+(?:$synonyms)}) {
         return $1, 'similar';
     }
     #Queries like "weezer band"
-    my $synonyms = "bands?|musicians?|players?|artists?|performers?|singers?";
     if(m{(\w+(?:\s+\w+)*)\s+(?:$synonyms)}) {
         return $1, 'all';
     }
