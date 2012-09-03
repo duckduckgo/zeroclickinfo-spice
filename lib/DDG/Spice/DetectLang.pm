c6cd06e6e7b99c51878c08565da29fe6412f0dc0
Alessandro Ghedini <al3xbio@gmail.com>
2012-09-03 12:43:31 +0200

    add DetectLang plugin

---
+++ lib/DDG/Spice/DetectLang.pm
@@ -0,0 +1,18 @@
+package DDG::Spice::DetectLang;
+
+use DDG::Spice;
+
+attribution github  => ['https://github.com/ghedo', 'ghedo'      ],
+            web     => ['http://ghedini.me', 'Alessandro Ghedini'];
+
+spice to   => 'http://ws.detectlanguage.com/0.2/detect?q=$1&key={{ENV{DDG_SPICE_DETECTLANGUAGE_APIKEY}}}';
+spice wrap_jsonp_callback => 1;
+
+triggers startend => 'detect language';
+
+handle remainder => sub {
+	my ($str) = @_;
+	return $str
+};
+
+1;
