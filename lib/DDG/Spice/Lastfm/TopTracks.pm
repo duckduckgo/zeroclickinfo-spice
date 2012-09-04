68e421646347227a7ffb601d73193ff28acb88df
Jag Talon <talon.jag@gmail.com>
2012-09-04 12:35:15 -0400

    Reverted to old version.

--- lib/DDG/Spice/Lastfm/TopTracks.pm
+++ lib/DDG/Spice/Lastfm/TopTracks.pm
@@ -1,18 +1,19 @@
-package DDG::Spice::Lastfm::TopTracks;
+package DDG::Spice::LastfmTopTracks;
 # ABSTRACT: Top 10 songs in a country. Defaults to USA.
 
 use DDG::Spice;
 
 spice to => 'http://ws.audioscrobbler.com/2.0/?format=json&method=geo.gettoptracks&country=$1&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}';
-triggers start => 'top', 'popular', 'popular songs in the', 'popular songs in', 'popular songs', 'popular tracks', 'top tracks', 'top songs';
 
-handle remainder => sub {
-	if($_) {
-		return $_;
-	} else {
-		return "United States";
-	}
-	return;
+triggers query_lc => qr/(?:tops?|popular)\s+(?:10\s)?(?:tracks?|charts?|songs?|musics?)(?:\s+in\s+(?:the\s+)?)?(.*)/;
+
+handle query_lc => sub {
+    if($1) {
+        return $1;
+    } else {
+        return "United States";
+    }
+    return;
 };
 
 1;
