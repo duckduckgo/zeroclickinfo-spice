0083075f9e80544469d0300a9070785f708a5a10
kogalex <kogalex@gmail.com>
2012-09-11 12:21:48 +0300

    fixing key issue

--- lib/DDG/Spice/Pipl.pm
+++ lib/DDG/Spice/Pipl.pm
@@ -4,7 +4,7 @@ use DDG::Spice;
 
 triggers query => qr/^(\w+\s\w+\s?\w*)|([\d\s\-\+\.()]{6,20})|([a-zA-Z0-9._%\-+]+@[a-zA-Z0-9._%\-]+\.[a-zA-Z]{2,6})$/;
 
-spice to => 'http://75.126.80.72/partnerapi/?q=$1&api-key={{ENV{DDG_SPICE_PIPL_APIKEY}}}&callback={{callback}}';
+spice to => 'http://75.126.80.72/partnerapi/?q=$1&key={{ENV{DDG_SPICE_PIPL_APIKEY}}}&callback={{callback}}';
 
 handle matches => sub {
     return @_;
