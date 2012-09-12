c5cf7609f28ebcd294c01f28c20eaf14d9adde50
Alessandro Ghedini <al3xbio@gmail.com>
2012-09-12 12:27:47 +0200

    do not use v5.10 is Wordreference

--- lib/DDG/Spice/Wordreference/Translate.pm
+++ lib/DDG/Spice/Wordreference/Translate.pm
@@ -1,7 +1,5 @@
 package DDG::Spice::Wordreference::Translate;
 
-use v5.10;
-
 use DDG::Spice;
 
 attribution github  => ['https://github.com/ghedo', 'ghedo'      ],
@@ -28,23 +26,24 @@ handle matches => sub {
 sub shorten_lang {
 	my ($lang) = @_;
 
-	given ($lang) {
-		when ('arabic')     { return 'ar'  }
-		when ('chinese')    { return 'zh'  }
-		when ('czech')      { return 'cz'  }
-		when ('english')    { return 'en'  }
-		when ('french')     { return 'fr'  }
-		when ('greek')      { return 'gr'  }
-		when ('italian')    { return 'it'  }
-		when ('japanese')   { return 'ja'  }
-		when ('korean')     { return 'ko'  }
-		when ('polish')     { return 'pl'  }
-		when ('portuguese') { return 'pt'  }
-		when ('romanian')   { return 'ro'  }
-		when ('spanish')    { return 'es'  }
-		when ('turkish')    { return 'tr'  }
-		default             { return $lang }
-	}
+	my %langs = {
+		'arabic'     => 'ar',
+		'chinese'    => 'zh',
+		'czech'      => 'cz',
+		'english'    => 'en',
+		'french'     => 'fr',
+		'greek'      => 'gr',
+		'italian'    => 'it',
+		'japanese'   => 'ja',
+		'korean'     => 'ko',
+		'polish'     => 'pl',
+		'portuguese' => 'pt',
+		'romanian'   => 'ro',
+		'spanish'    => 'es',
+		'turkish'    => 'tr'
+	};
+
+	return $langs{$lang} ? $langs{$lang} : $lang;
 }
 
 1;
