666e0d93ea078f9adb8d2874f3539f7a6f97fc73
Caine Tighe <caine@duckduckgo.com>
2012-09-13 11:56:17 -0400

    Twitter (hashtag): initial import...

---
+++ lib/DDG/Spice/Twitter/Hashtag.pm
@@ -0,0 +1,15 @@
+package DDG::Spice::Twitter::Hashtag;
+
+use DDG::Spice;
+
+spice to => 'http://twitter.com/search.json?q=%23$1&callback={{callback}}';
+
+triggers query_lc => qr/^(?:#|hashtag\s+)([^\s]+)$/;
+
+handle matches => sub {
+    my ( $tag ) = @_;
+    warn qq(TAG: $tag\n);
+    return $tag;
+};
+
+'im a little teapot';
