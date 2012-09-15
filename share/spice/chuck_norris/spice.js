f50baf3035b6c13f35f24f162394ea2d7ce1eba6
Jag Talon <talon.jag@gmail.com>
2012-09-15 13:05:21 -0400

    Moved the "More at ..." line in the ChuckNorris plugin down.

--- share/spice/chuck_norris/spice.js
+++ share/spice/chuck_norris/spice.js
@@ -7,7 +7,7 @@ function ddg_spice_chuck_norris(res){
 			items[0]['a'] = res['value']['joke'];
 			items[0]['s'] = 'Internet Chuck Norris Database';
 			items[0]['u'] = 'http://www.icndb.com/';
-			nra(items,4,1);
+			nra(items,2,1);
 		}
 	}
 
