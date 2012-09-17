bbcffd3cb164251a0221201214e2116993b34981
Ado Matejov <a.matejov@centrum.sk>
2012-09-17 18:57:53 +0200

    another trigger

--- lib/DDG/Spice/Vimeo/Video.pm
+++ lib/DDG/Spice/Vimeo/Video.pm
@@ -5,12 +5,15 @@ use DDG::Spice;
 
 spice to => 'http://vimeo.com/api/oembed.json?url=$1&callback={{callback}}';
 
-triggers query_lc => qr/^https?:\/\/vimeo.com\/[0-9]+/;
+triggers query_lc => qr/^https?:\/\/vimeo.com\/[0-9]+|^vimeo.com\/[0-9]+/;
 
 attribution github => ['https://github.com/adman','Adman'],
             twitter => ['http://twitter.com/adman_X','adman_X'];
 
 handle matches => sub {
+    if (/^vimeo.com\/[0-9]+/){
+        return 'http://' . $_ if defined $_;
+    }
     return $_ if defined $_; 
     return;
 };
