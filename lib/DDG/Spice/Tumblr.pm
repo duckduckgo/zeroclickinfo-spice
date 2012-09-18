940f2d4ff0e2615bf747b96e9199f9059effa5d8
Jag Talon <talon.jag@gmail.com>
2012-09-18 13:00:35 -0400

    Removed Tumblr API key from Tumblr.pm

--- lib/DDG/Spice/Tumblr.pm
+++ lib/DDG/Spice/Tumblr.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Tumblr;
 
 use DDG::Spice;
 
-spice to => 'http://api.tumblr.com/v2/tagged?tag=$1&feature_type=popular&api_key=HIpY8au5Gf4RU9uI1fHq81t4tCZZueG66smdPyoWnD9s3gkMZc&callback={{callback}}';
+spice to => 'http://api.tumblr.com/v2/tagged?tag=$1&feature_type=popular&api_key={{ENV{DDG_SPICE_TUMBLR_APIKEY}}}&callback={{callback}}';
 
 #triggers query_lc => qr/^(?:tumblr\s+)([^\s]+)$/;
 #triggers startend => "tumblr";
