69d38dc588e7f134c89298f95704b9d3a1565bb1
Jag Talon <talon.jag@gmail.com>
2012-09-18 17:58:13 -0400

    Escaped [] in ChuckNorris plugin.

--- lib/DDG/Spice/ChuckNorris.pm
+++ lib/DDG/Spice/ChuckNorris.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 spice proxy_cache_valid  => "418 1d";
 
 triggers start => 'chuck norris fact','chuck norris facts','chuck norris joke','chuck norris jokes';
-spice to => 'http://api.icndb.com/jokes/random?exclude=[explicit]?escape=javascript&callback={{callback}}';
+spice to => 'http://api.icndb.com/jokes/random?exclude=%5Bexplicit%5D?escape=javascript&callback={{callback}}';
 handle remainder => sub {
 	return @_ ;
 };
