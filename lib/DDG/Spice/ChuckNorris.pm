4082926babe64671759a081cc87e1a29cf1fddf1
Jag Talon <talon.jag@gmail.com>
2012-09-19 11:00:42 -0400

    Added back the is_unsafe property in ChuckNorris.

--- lib/DDG/Spice/ChuckNorris.pm
+++ lib/DDG/Spice/ChuckNorris.pm
@@ -3,9 +3,9 @@ package DDG::Spice::ChuckNorris;
 use DDG::Spice;
 
 spice proxy_cache_valid  => "418 1d";
-
+spice is_unsafe => 1;
 triggers start => 'chuck norris fact','chuck norris facts','chuck norris joke','chuck norris jokes';
-spice to => 'http://api.icndb.com/jokes/random?exclude=%5Bexplicit%5D?escape=javascript&callback={{callback}}';
+spice to => 'http://api.icndb.com/jokes/random?escape=javascript&callback={{callback}}';
 handle remainder => sub {
 	return @_ ;
 };
--- share/spice/chuck_norris/spice.js
+++ share/spice/chuck_norris/spice.js
@@ -1,10 +1,11 @@
 function ddg_spice_chuck_norris(res){
     if (res['type'] == 'success') {		
         items = [[]];
+        items[0]['i'] = 'https://icons.duckduckgo.com/i/21875478.jpg';
         items[0]['a'] = res['value']['joke'];
         items[0]['s'] = 'Internet Chuck Norris Database';
         items[0]['u'] = 'http://www.icndb.com/';
-        items[0]['force_space_after'] = true;
+        items[0]['force_no_icon'] = true;
         nra(items,1,1);
     }
 }
