cc8c6679b253a737727b0bf542751b09d1dc591d
Jag Talon <talon.jag@gmail.com>
2012-09-24 09:49:46 -0400

    Added Zaahir's code to update the plugin.

--- lib/DDG/Spice/Octopart.pm
+++ lib/DDG/Spice/Octopart.pm
@@ -4,27 +4,12 @@ use DDG::Spice;
 
 triggers any => "datasheet";
 
-# see: http://octopart.com/api/documentation
-sub nginx_conf {
-    my $api_key = $ENV{DDG_SPICE_OCTOPART_APIKEY}; 
-    return unless defined $api_key;
-    $nginx_conf = <<"__END_OF_CONF__";
+spice to => 'https://www.octopart.com/api/v2/parts/search?apikey={{ENV{DDG_SPICE_OCTOPART_APIKEY}}}&limit=3&optimize.hide_offers=1&optimize.hide_specs=1&q=$1&callback={{callback}}';
 
-location ^~ /js/spice/octopart/ {
-    rewrite ^/js/spice/octopart/(.*) /api/v2/parts/search?callback=ddg_spice_octopart&apikey=$api_key&limit=3&optimize.hide_offers=1&optimize.hide_specs=1&q=$1 break;
-    proxy_pass https://www.octopart.com/;
-}
-
-__END_OF_CONF__
-}
-
-handle query_lc => sub {
-    if ($_ =~ /^\s*(?:(datasheet))?\s*([\w\-]+)(?:\s(datasheet))?\s*$/i) {
-        if ($1 or $3) {
-            return $2;
-        }
+handle remainder => sub {
+    if ($_ =~ qr/([\w\-]+)/i) {
+        return $1;
     }
     return;
 };
-
 1;
