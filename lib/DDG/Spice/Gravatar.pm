3980ff0a5ba6ea202e263f749567cac2de3a431e
Jag Talon <talon.jag@gmail.com>
2012-09-25 15:46:55 -0400

    Finished the GravatarFinal endpoint. :birthday:

--- lib/DDG/Spice/Gravatar.pm
+++ lib/DDG/Spice/Gravatar.pm
@@ -6,7 +6,7 @@ use Digest::MD5 qw(md5_hex);
 
 triggers startend => "gravatar", "avatar of", "gravatar of";
 
-spice to => 'http://jagtalon.duckduckgo.com/js/spice/gravatar_final/$1';
+spice to => '/js/spice/gravatar_final/$1';
 
 attribution github => ['https://github.com/adman','Adman'],
             twitter => ['http://twitter.com/adman_X','adman_X'];
