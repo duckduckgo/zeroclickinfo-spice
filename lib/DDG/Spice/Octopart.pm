de2d552d6f83f8dfebf64d3b0f39492eaa9b1ef5
Jag Talon <talon.jag@gmail.com>
2012-09-26 17:09:57 -0400

    Added "specs" as a trigger.

--- lib/DDG/Spice/Octopart.pm
+++ lib/DDG/Spice/Octopart.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Octopart;
 
 use DDG::Spice;
 
-triggers any => "datasheet";
+triggers any => "datasheet", "specs";
 
 spice to => 'https://www.octopart.com/api/v2/parts/search?apikey={{ENV{DDG_SPICE_OCTOPART_APIKEY}}}&limit=3&optimize.hide_offers=1&optimize.hide_specs=1&q=$1&callback={{callback}}';
 
