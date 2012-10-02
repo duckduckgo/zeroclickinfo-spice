4ed25e79539fa367fa82aa099f5a8494acd7ee11
Jag Talon <talon.jag@gmail.com>
2012-10-02 13:28:24 -0400

    Added autocorrect parameters in the plugins.

--- lib/DDG/Spice/Lastfm/ArtistTracks.pm
+++ lib/DDG/Spice/Lastfm/ArtistTracks.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Lastfm::ArtistTracks;
 
 use DDG::Spice;
 
-spice to => 'http://ws.audioscrobbler.com/2.0/?limit=5&format=json&method=artist.gettoptracks&artist=$1&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}';
+spice to => 'http://ws.audioscrobbler.com/2.0/?limit=5&format=json&method=artist.gettoptracks&artist=$1&autocorrect=1&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}';
 
 #Queries like "songs by ben folds" and "ben folds songs"
 my $synonyms = "songs|tracks?|music";
--- lib/DDG/Spice/Lastfm/Song.pm
+++ lib/DDG/Spice/Lastfm/Song.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Lastfm::Song;
 
 use DDG::Spice;
 
-spice to => 'http://ws.audioscrobbler.com/2.0/?format=json&method=track.getinfo&track=$1&artist=$2&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}';
+spice to => 'http://ws.audioscrobbler.com/2.0/?format=json&method=track.getinfo&track=$1&artist=$2&autocorrect=1&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}';
 spice from => '(?:([^/]*)/([^/]*)|)';
 
 triggers query_lc => qr/^([^\s]+(?:\s+[^\s]+)*)\s+(?:tracks?|songs?|music)\s+(?:by|from)\s+([^\s]+(?:\s+[^\s]+)*)$/;
