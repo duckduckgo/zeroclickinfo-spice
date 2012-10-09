b82680c1287faea54511520724988ad9fc47ff46
Jag Talon <talon.jag@gmail.com>
2012-10-09 14:38:35 -0400

    Simplified regexp in Gravatar.

--- lib/DDG/Spice/Gravatar.pm
+++ lib/DDG/Spice/Gravatar.pm
@@ -12,8 +12,8 @@ attribution github => ['https://github.com/adman','Adman'],
             twitter => ['http://twitter.com/adman_X','adman_X'];
 
 handle remainder => sub {
-    s/^\s+//;
-    s/\s+$//;
+    #Remove whitespace.
+    s/^\s+|\s+$//;
     my $email_hash = md5_hex(lc $_);
     return $email_hash if defined $email_hash;
     return;
