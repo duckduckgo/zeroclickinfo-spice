56e54c5ffd337c59e08f2bd2d96e6a38e79c0a88
Dylan Lloyd <dylan@dylansserver.com>
2012-10-17 03:23:07 -0400

    new random xkcd feature, syntax, and a test for em

--- lib/DDG/Spice/Xkcd.pm
+++ lib/DDG/Spice/Xkcd.pm
@@ -7,14 +7,15 @@ triggers startend => "xkcd";
 spice to => 'http://dynamic.xkcd.com/api-0/jsonp/comic/$1';
 spice wrap_jsonp_callback => 1;
 
+spice is_cached => 0;
+
 handle query_lc => sub {
-    if ($_ eq 'xkcd' || $_ =~ /^xkcd (\d+)$/) {
-        if ($1) {
-            return $1;
-        } else {
-            return '';
-        }
+    s/ ?xkcd ?//;
+    if ($_ =~ /^(\d+|r(?:andom)?)$/) {
+        return int rand 1122 if $1 =~ /r/;
+        return $1;
     }
+    return '' if $_ eq '';
     return;
 };
 
