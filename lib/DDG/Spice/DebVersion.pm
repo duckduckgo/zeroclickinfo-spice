66476d2f808541e38d7b64ccf194e367683a22c0
Simon <simonft@riseup.net>
2012-10-17 18:39:32 -0500

    expand matches to allow "deb ver <package>"

--- lib/DDG/Spice/DebVersion.pm
+++ lib/DDG/Spice/DebVersion.pm
@@ -1,20 +1,21 @@
+# Get the version of a package in various debian repositories
 package DDG::Spice::DebVersion;
 
 use DDG::Spice;
 use strict;
 use warnings;
 
-triggers any => "debian version","debian versions";
+triggers any => "debian version","debian versions","deb ver";
 
 spice from => '([^/]+)/?(?:([^/]+)/?(?:([^/]+)|)|)';
 spice to => 'http://qa.debian.org/madison.php?package=$1&table=debian&a=&c=&s=&text=on#';
 
-
 spice wrap_string_callback => 1;
 
 handle query_lc => sub {
-    if (/^\s*debian\s*versions?\s*(\S+)\s*$/i) {
-        my $DebPackage = $1;
+    if (/^\s*deb(ian| )\s*ver(sion| )s?\s*(\S+)\s*$/i) {
+	print "\n\n\n$1\n\n\n";
+        my $DebPackage = $3;
         $DebPackage =~ s/\s+$//g;
         $DebPackage =~ s/^\s+//g;
         $DebPackage =~ s/\s+/-/g;
