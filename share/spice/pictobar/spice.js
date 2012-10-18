1c42e5ff189aa29f372fc8d583bdb7364d799519
Cinque McFarlane-Blake <cinque_mcfarlane-blake@brown.edu>
2012-10-18 15:32:50 -0400

    stripped down css file, commented javascript code more

--- share/spice/pictobar/spice.js
+++ share/spice/pictobar/spice.js
@@ -7,15 +7,20 @@ function ddg_spice_pictobar(pbr) {
         
         //generates stars
         
+        //numerical data into array
         var outPuts = new Array(pbr['attra'],pbr['friend'],pbr['open'],pbr['self'],pbr['thot']);
         var Image = new Array();
+        
+        //array of attribute names
         var attra = new Array('Attractiveness:','Friendliness:','Openness:','Selflessness:','Thoughtfulness:');
         var outString = new String('');
         var outputString = new String('');
         var orgLoop = new Array();
+        //loop to define rows for stars and attributes
         for (x=0; x<5; x++){
             orgLoop[x] = new Array();
             orgLoop[x].push(attra[x]);
+            //loop to create 5 stars for attributes based on numerical data array
             for ( i=1; i<6; i++){
                 if ( i < outPuts[x]+1 ){
                     orgLoop[x][i] = '<img src="http://www.pictobar.com/starfill.png" width="10" height="10" />';
@@ -24,10 +29,14 @@ function ddg_spice_pictobar(pbr) {
                     orgLoop[x][i] = '<img src="http://www.pictobar.com/starunfill.png" width="10" height="10" />';
                 }
             }
+            //add break beteween rows
             orgLoop[x].push('</br>');
+            //convert row to string in order to remove commas in row
             outString[x] = orgLoop[x].toString();
             outputString[x] = outString[x].replace(/,/g," ");
         }
+        //convert row to string in order to remove leftover commas in row
+
         outString = orgLoop.toString();
         outputString = outString.replace(/,/g," ");
         stars = d.write(outputString);
@@ -41,18 +50,16 @@ function ddg_spice_pictobar(pbr) {
         td = d.createElement('td');
         td.innerHTML = '<a href="'+pbr['proflink']+'"><p>'+pbr['fname']+' '+pbr['lname']+'</p></a><table class="alltable1"><tr><td><center><a href="'+pbr['proflink']+'"><img src="'+pbr['piclink']+'"  class="imagecirlce" /></a></center></td><td>'+stars+'</td></tr><tr><td><a href="'+pbr['proflink']+'">View Full Profile</a></br>Gender:'+pbr['gendr']+'</br>Total Votes: '+pbr['totvot']+'</td><td>Percent Introversion: '+pbr['intro']+'%</br>Percent Extroversion: '+pbr['extro']+'%</td></tr></table>';
         mid.appendChild(td);
-    
-        
-        
-        //body + css
+
+        //body 
         whole = mid;
         
         items = new Array();
         items[0] = new Array();
         items[0]['a'] = whole;
         items[0]['h'] = '';
-        items[0]['s'] = '';
-        items[0]['u'] = 'Pictobar';
+        items[0]['s'] = 'Pictobar';
+        items[0]['u'] = '';
         items[0]['i'] = 'http://www.pictobar.com/api/logo.png';
         nra(items);
     } 
