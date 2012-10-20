86f50824c0d3dcbd94619e173ba520f7037d1d6e
Cinque McFarlane-Blake <cinque_mcfarlane-blake@brown.edu>
2012-10-20 16:07:11 -0400

    fixed some styling, and displaying of data

--- share/spice/pictobar/spice.js
+++ share/spice/pictobar/spice.js
@@ -4,7 +4,6 @@ function ddg_spice_pictobar(pbr) {
     //check if name is there
     if( (pbr['fname'] && pbr['lname']) ){
 
-        
         //generates stars
         
         //numerical data into array
@@ -12,7 +11,7 @@ function ddg_spice_pictobar(pbr) {
         var Image = new Array();
         
         //array of attribute names
-        var attra = new Array('Attractiveness:','Friendliness:','Openness:','Selflessness:','Thoughtfulness:');
+        var attra = new Array('<tr><td>Attractiveness:</td>','<tr><td>Friendliness:</td>','<tr><td>Openness:</td>','<tr><td>Selflessness:</td>','<tr><td>Thoughtfulness:</td>');
         var outString = new String('');
         var outputString = new String('');
         var orgLoop = new Array();
@@ -23,23 +22,22 @@ function ddg_spice_pictobar(pbr) {
             //loop to create 5 stars for attributes based on numerical data array
             for ( i=1; i<6; i++){
                 if ( i < outPuts[x]+1 ){
-                    orgLoop[x][i] = '<img src="http://www.pictobar.com/starfill.png" width="10" height="10" />';
+                    orgLoop[x][i] = '<td><img src="http://www.pictobar.com/starfill.png" width="10" height="10" /></td>';
                 }
                 if ( i > outPuts[x]){
-                    orgLoop[x][i] = '<img src="http://www.pictobar.com/starunfill.png" width="10" height="10" />';
+                    orgLoop[x][i] = '<td><img src="http://www.pictobar.com/starunfill.png" width="10" height="10" /></td>';
                 }
             }
             //add break beteween rows
-            orgLoop[x].push('</br>');
+            orgLoop[x].push('</tr>');
             //convert row to string in order to remove commas in row
             outString[x] = orgLoop[x].toString();
             outputString[x] = outString[x].replace(/,/g," ");
         }
         //convert row to string in order to remove leftover commas in row
-
+        
         outString = orgLoop.toString();
         outputString = outString.replace(/,/g," ");
-        stars = d.write(outputString);
         
         //creates table where data is displayed
         
@@ -48,7 +46,7 @@ function ddg_spice_pictobar(pbr) {
         mid = d.createElement('table');
         mid.setAttribute('class','alltable');
         td = d.createElement('td');
-        td.innerHTML = '<a href="'+pbr['proflink']+'"><p>'+pbr['fname']+' '+pbr['lname']+'</p></a><table class="alltable1"><tr><td><center><a href="'+pbr['proflink']+'"><img src="'+pbr['piclink']+'"  class="imagecirlce" /></a></center></td><td>'+stars+'</td></tr><tr><td><a href="'+pbr['proflink']+'">View Full Profile</a></br>Gender:'+pbr['gendr']+'</br>Total Votes: '+pbr['totvot']+'</td><td>Percent Introversion: '+pbr['intro']+'%</br>Percent Extroversion: '+pbr['extro']+'%</td></tr></table>';
+        td.innerHTML = '<a href="'+pbr['proflink']+'"><p>'+pbr['fname']+' '+pbr['lname']+'</p></a><table class="alltable1"><tr><td><center><a href="'+pbr['proflink']+'"><img src="'+pbr['piclink']+'"  class="imagecirlce" /></a></center></td><td><center><table width="150" height="60">'+outputString+'</table></center></td></tr><tr><td><a href="'+pbr['proflink']+'">View Full Profile</a></br>Gender:'+pbr['gendr']+'</br>Total Votes: '+pbr['totvot']+'</td><td>Percent Introversion: '+pbr['intro']+'%</br>Percent Extroversion: '+pbr['extro']+'%</td></tr></table>';
         mid.appendChild(td);
 
         //body 
@@ -60,7 +58,7 @@ function ddg_spice_pictobar(pbr) {
         items[0]['h'] = '';
         items[0]['s'] = 'Pictobar';
         items[0]['u'] = '';
-        items[0]['i'] = 'http://www.pictobar.com/api/logo.png';
+        items[0]['i'] = 'http://i474.photobucket.com/albums/rr101/Pcfllintent/pbicon.jpg';
         nra(items);
     } 
 }
