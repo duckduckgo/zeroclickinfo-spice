0026845eb6ae1fcbaf3c2b91f1386e8c7444483e
Jag Talon <talon.jag@gmail.com>
2012-10-25 18:45:06 -0400

    Removed the display elements from Movies plugin.

--- share/spice/lastfm/artist/spice.js
+++ share/spice/lastfm/artist/spice.js
@@ -1,5 +1,4 @@
 function ddg_spice_lastfm_artist_all(lastfm) {
-    // console.log(lastfm);
     if(lastfm.artist && lastfm.artist.similar.artist) {
         similar = '<div style="similar"><i>Similar to: </i>';
         for(var i = 0;i < lastfm.artist.similar.artist.length;i++) {
@@ -10,8 +9,7 @@ function ddg_spice_lastfm_artist_all(lastfm) {
             }
         }
         similar += '.</div>';
-        var items = [];
-        items[0] = [];
+        var items = [[]];
         var rest = true;
 
         var albums = '<a href="/?q=albums+from+' + encodeURIComponent(lastfm.artist.name) + '">' + 
@@ -20,15 +18,8 @@ function ddg_spice_lastfm_artist_all(lastfm) {
                     'tracks</a> from ' + lastfm.artist.name + '.';            
         if(lastfm.artist.bio.summary) {
             items[1] = [];
-            var summary = lastfm.artist.bio.summary;
-            //Remove all the links
-            summary = summary.replace(/<.+?>/g, "");
-            //Trim
-            if(summary.length > 200) {
-                summary = '<span id="first" style="display: inline;">' + summary.slice(0, 200) + '</span> ' + 
-                '<a style="display: inline;" id="expand" href="javascript:;" onclick="DDG.toggle(\'ellipsis\', 1); DDG.toggle(\'first\', -1); DDG.toggle(\'expand\', -1);"><span style="color: rgb(119, 119, 119); font-size: 11px; ">More...<span></a>' + 
-                '<span id="ellipsis" style="display: none;">' + summary + '</span>';
-            }
+            var summary = _.str.stripTags(lastfm.artist.bio.summary);
+            summary = Coral.extractSentence(summary, 1, Coral.moreHTML);
             items[0]['a'] = summary + '<div style="clear:both;"></div>' + similar + '<i>See also:</i> ' + 
             albums + songs + '<div style="clear:both;"></div>';
         } else {
--- share/spice/movie/spice.js
+++ share/spice/movie/spice.js
@@ -1,121 +1,67 @@
-
-var SKIP_ARRAY = [];
-SKIP_ARRAY['movie'] = 1;
-SKIP_ARRAY['film'] = 1;
-
 function ddg_spice_movie(movie) {
-    var request = DDG.get_query().replace("movie",'').replace("film", "");
+    var request = DDG.get_query().replace("movie",'').replace("film", ""),
+        result,
+        blurb = "",
+        cast = "",
+        names = [],
+        poster = "",
+        header = "";
 
-    // validity check
-    if (movie['total'] > 0 && movie['movies']) {
+    // Verify things before accessing the object, amirite?
+    if(Coral.verify(movie['total'], movie['movies'])) {
+       // Get the movie titles and check which one is relevant.
+       if(movie['total'] > 1) {
+           result = movie.movies[0];
+           var stop = false;
+           result = _.find(movie.movies, function(aMovie) {
+                return DDG.isRelevant(aMovie.title, {movie: 1, film: 1});
+           });
+       } else {
+           result = movie.movies[0];
+       }
 
-        // more than one result
-        if (movie['total'] > 1) {
+       // Get the synopsis.
+       if(Coral.verify(result.synopsis)) {
+           blurb = result.synopsis;
+       } else if(Coral.verify(result.critics_consensus)) {
+           blurb = result.critics_consensus;
+       }
 
-            // Default to first result incase nothing is more relevant
-            result = movie["movies"][0];
+       // Get the cast.
+       _.each(result.abridged_cast, function(person) {
+           names.push(person.name);
+       });
+       cast = _.str.toSentenceSerial(names);
+       if(names.length > 1) cast = " starring " + cast;
 
-            // check which movie title is most relevant
-            for (var i = 0, aMovie; aMovie = movie.movies[i]; i++){
-                if (DDG.isRelevant(aMovie.title, SKIP_ARRAY)) {
-                    result = aMovie;
-                    break;
-                }
-            }             
-        } else {
-            result = movie["movies"][0];
-        }
+       // Get the poster.
+       poster = result.posters.thumbnail;
+       if (poster === 'http://images.rottentomatoescdn.com/images/redesign/poster_default.gif') poster = '';
 
-        // Create snippet to be shown
-        snippet = '';
+       // Get header.
+       header = result.title;
 
-        // Check presence of synopsis, and create element
-        if (result.synopsis) synopsis = result.synopsis.substring(0,135) + "...";
-        else if (result.critics_consensus && result.critics_consensus.length > 0) synopsis = result.critics_consensus.substring(0,135) + "...";
-        else synopsis = '';
-
-        var names = [];
-        // Loop through abridged cast members, add to cast element
-        for (var i=0; i < result.abridged_cast.length; i++){
-            var pre = '';
-            if ( i == result.abridged_cast.length - 1 && result.abridged_cast.length != 1 ) pre = 'and ';
-            var name = result.abridged_cast[i].name;
-            var url = 'http://www.rottentomatoes.com/celebrity/' + result.abridged_cast[i].id + '/';
-
-            names.push(pre+'<a onlcick="fl=1" href="'+url+'">'+name+'</a>');
-        }
-
-        var cast = '';
-        if (names.length > 1) cast = ', starring '+names.join(', ');
-
-        // check for default poster
-        var poster = result.posters.thumbnail;
-        if (poster === 'http://images.rottentomatoescdn.com/images/redesign/poster_default.gif') poster = '';
-
-        // Make title for header
-        var header = result.title.substring(0,49);
-        if (result.title.length > 50) header += '...';
-
-        //Movie Score
-        //var score = 'with an audience score of'+ result.audience_score +'%';
-
-        var rating, audience, critics, reaction, releaseDate, tomato;
-        var currentTime = new Date();
-
-        // Is a release date planned?
-        if (result.release_dates.theater) {
-            var rDate = result.release_dates.theater.split("-");
-            var opened = new Date(rDate[0], rDate[1]-1, rDate[2], 00, 00, 00); // Date uses month-1 notation
-            rating = (currentTime - opened < 0) ? "an upcoming " : "";
-            releaseDate = (rating) ? opened.toDateString().slice(4) : result.year;
-        }
-        else if (result.year > currentTime.getFullYear()) {
-            rating = "an upcoming ";
-            releaseDate = result.year;
-        }
-        reaction = (rating) ? " want to see)" : " approved)";
-        
-        // Who's reviewed it?
-        audience = (result.ratings.audience_score === -1) ? "" : result.ratings.audience_score+ "% audience"+reaction;
-        critics = (result.ratings.critics_score === -1) ? "" : result.ratings.critics_score+ "% critics, ";
-        critics += (audience) ? "" : reaction;
-
-        // Is it fresh or rotten?
-        tomato = (result.ratings.critics_rating) ? result.ratings.critics_rating+", " : "";
-
-        if (!rating){
-            releaseDate = result.year;
-            if (result.mpaa_rating === "R" || result.mpaa_rating === "NC-17" || result.mpaa_rating == "Unrated"){
-               rating = "an ";
-            } else {
-                rating = "a ";
-            }
-        }
-        rating += result.mpaa_rating;
-        
-        // Call nra function as per Spice Plugin Guidelines
-        items = [[]];
-        items[0]['a'] = result.title + ' ('+result.year+ ') is ' 
-                        +rating+ ' movie ('
-                        +tomato
-                        +critics
-                        +audience
-                        +cast + '. '
-                        +synopsis;
-
-        items[0]['h'] = header+" ("+releaseDate+")";
-
-        // Source name and url for the More at X link.
-        items[0]['s'] = 'Rotten Tomatoes';
-        items[0]['u'] = result.links.alternate;
-
-        // Force vertical expansion (no scrollbar)
-        // items[0]['f'] = 1;
-
-        // Thumbnail url
-        items[0]['i'] = poster; 
-
-        // The rendering function is nra.
-        nra(items);
-    }
+       // Display the thing
+       Coral.display({
+           type: "standard",
+           blurb: {
+               text: header + " is an X movie" + cast + ". " + blurb,
+               limit: "auto"
+           },
+           title: {
+               text: header,
+               limit: "auto",
+               big: true
+           },
+           image: {
+               url: poster,
+               height: "auto",
+               width: "auto"
+           },
+           source: {
+               text: 'Rotten Tomatoes',
+               url: result.links.alternate
+           }
+       });
+    } 
 }
