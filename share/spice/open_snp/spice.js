4154d0442358c48938d8a9def861fdf012fe0848
drsnuggles <philippbay@gmail.com>
2012-10-29 14:16:15 +1000

    Javascript works. Added small SNPedia-table, notes when Mendeley/PLOS is available

--- share/spice/open_snp/spice.js
+++ share/spice/open_snp/spice.js
@@ -1,10 +1,37 @@
 function ddg_spice_open_snp(ir) {
     // did the API return information?
-    items = new Array();
-    items[0] = new Array();
-    items[0]['a'] = ir['snp']['name'];
-    items[0]['h'] = 'SNP-annotation at openSNP.org';
-    items[0]['u'] = ir['source_url'];
-    items[0]['s'] = 'openSNP.org/snps/' + ir['snp']['name'];
-    nra(items);
+    var table;
+    if ( ir['snp'] ) {
+        var snp_name = ir['snp']['name'];
+        var snp_position = ir['snp']['position'];
+        var snp_chromosome = ir['snp']['chromosome'];
+        var snippet = 'Position: ';
+        snippet += snp_position + '<br/>';
+        snippet += 'Chromosome: ' + snp_chromosome + '<br/>';
+        // Do we have annotations?
+        if (ir['snp']['annotations']) {
+            var annotations = ir['snp']['annotations'];
+            if ( annotations['mendeley'] ){
+                snippet += '<br/>There is Mendeley-annotation available!<br/>';
+            }
+            if (annotations['snpedia']) {
+                snippet += '<br/>SNPedia has the following:<br/>';
+                for ( var i = 0; i < annotations['snpedia'].length; i++ ) {
+                    snippet += annotations['snpedia'][i]['url'].split('/')[4] + ': ' + annotations['snpedia'][i]['summary'] + '<br/>';
+                }
+            }
+            if (annotations['plos']) {
+                snippet += '<br/>There is PLOS-annotation available!<br/>';
+            }
+
+        }
+
+        items = new Array();
+        items[0] = new Array();
+        items[0]['a'] = snippet;
+        items[0]['h'] = 'Information on SNP <b>' + snp_name + '</b> at openSNP.org';
+        items[0]['s'] = 'openSNP.org'
+        items[0]['u'] = 'http://openSNP.org/snps/' + ir['snp']['name'];
+        nra(items);
+    }
 }
