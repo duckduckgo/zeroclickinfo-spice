db82e9bd4f3b8c3b753c7b5b363950ec1a4f72e7
drsnuggles <philippbay@gmail.com>
2012-10-30 10:40:56 +1000

    Everything now works nicely. To do: Proper addElements, no ugly string-concatenation.

--- share/spice/open_snp/spice.js
+++ share/spice/open_snp/spice.js
@@ -11,17 +11,21 @@ function ddg_spice_open_snp(ir) {
         // Do we have annotations?
         if (ir['snp']['annotations']) {
             var annotations = ir['snp']['annotations'];
+            var mendeley_papers = 0;
+            var plos_papers = 0;
             if ( annotations['mendeley'] ){
-                snippet += '<br/>There is Mendeley-annotation available!<br/>';
+                mendeley_papers = annotations['mendeley'].length;
             }
-            if (annotations['snpedia']) {
+            if ( annotations['plos'] ) {
+                plos_papers = annotations['plos'].length;
+            }
+            snippet += '<br/>There are ' + mendeley_papers + ' papers at Mendeley and ' + plos_papers + ' papers at PLOS available on this SNP.<br/>';
+            if ( annotations['snpedia'] ) {
                 snippet += '<br/>SNPedia has the following:<br/>';
                 for ( var i = 0; i < annotations['snpedia'].length; i++ ) {
                     snippet += annotations['snpedia'][i]['url'].split('/')[4] + ': ' + annotations['snpedia'][i]['summary'] + '<br/>';
                 }
-            }
-            if (annotations['plos']) {
-                snippet += '<br/>There is PLOS-annotation available!<br/>';
+                snippet += '<br/>';
             }
 
         }
@@ -31,7 +35,7 @@ function ddg_spice_open_snp(ir) {
         items[0]['a'] = snippet;
         items[0]['h'] = 'Information on SNP <b>' + snp_name + '</b> at openSNP.org';
         items[0]['s'] = 'openSNP.org'
-        items[0]['u'] = 'http://openSNP.org/snps/' + ir['snp']['name'];
+        items[0]['u'] = 'http://openSNP.org/snps/' + ir['snp']['name'] + '#papers';
         nra(items);
     }
 }
