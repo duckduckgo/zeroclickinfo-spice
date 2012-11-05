83d4d97d86426411e7d09957acc467b17ea3e8b8
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2012-11-05 20:32:57 +0100

    Split CSS to stylesheet, truncate long strings to avoid breaking layout

--- share/spice/maven/spice.js
+++ share/spice/maven/spice.js
@@ -1,17 +1,22 @@
+function truncate(str) {
+  if (str.length > 30) return str.substring(0,27) + '...'
+  return str
+}
+
 function ddg_spice_maven(resp) {
   if (resp['responseHeader']['status'] == 0 && resp['response']['numFound'] > 0) {
-    
-    content='<table width="100%" cellspacing="2">' +
-              '<tr style="font-weight: bold;">' + 
+
+    content='<table id="maven">' +
+              '<tr>' + 
                 '<th>groupId</th>' +
                 '<th>artifactId</th>' +
                 '<th>version</th>' +
               '</tr>'
     for (match in resp['response']['docs']) {
       content += '<tr>' +
-                   '<td><a href="http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22' + resp['response']['docs'][match]['g'] + '%22">' + resp['response']['docs'][match]['g'] + '</td>' + 
-                   '<td><a href="http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22' + resp['response']['docs'][match]['g'] + '%22%20AND%20a%3A%22' + resp['response']['docs'][match]['a'] + '%22">' + resp['response']['docs'][match]['a'] + '</td>' + 
-                   '<td><a href="http://search.maven.org/#artifactdetails%7C' + resp['response']['docs'][match]['g'] + '%7C' + resp['response']['docs'][match]['a'] + '%7C' + resp['response']['docs'][match]['latestVersion'] + '%7Cjar">' + resp['response']['docs'][match]['latestVersion'] + '</td>' + 
+                   '<td><a href="http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22' + resp['response']['docs'][match]['g'] + '%22">' + truncate(resp['response']['docs'][match]['g']) + '</a></td>' + 
+                   '<td><a href="http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22' + resp['response']['docs'][match]['g'] + '%22%20AND%20a%3A%22' + resp['response']['docs'][match]['a'] + '%22">' + truncate(resp['response']['docs'][match]['a']) + '</a></td>' + 
+                   '<td><a href="http://search.maven.org/#artifactdetails%7C' + resp['response']['docs'][match]['g'] + '%7C' + resp['response']['docs'][match]['a'] + '%7C' + resp['response']['docs'][match]['latestVersion'] + '%7Cjar">' + truncate(resp['response']['docs'][match]['latestVersion']) + '</a></td>' + 
                  '</tr>'
     }
     content+='</table>'
