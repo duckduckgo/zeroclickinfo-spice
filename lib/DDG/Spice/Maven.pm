d64982f53983d00e3935c8d439fb0f54a6198f38
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2012-11-05 20:03:26 +0100

    Remove unsafe flag from Maven spice

--- lib/DDG/Spice/Maven.pm
+++ lib/DDG/Spice/Maven.pm
@@ -11,7 +11,6 @@ triggers startend => "maven", "mvn";
 spice to => 'http://search.maven.org/solrsearch/select?q=$1&rows=5&wt=json&callback={{callback}}';
 spice wrap_jsonp_callback => 1;
 spice proxy_cache_valid   => "418 1d";
-spice is_unsafe => 1;
 
 handle remainder => sub {
     return $_;
