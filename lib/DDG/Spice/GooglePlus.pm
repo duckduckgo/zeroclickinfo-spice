774b60db88a280934caf062dd4d1bd579c67fa32
Jag Talon <talon.jag@gmail.com>
2012-11-06 17:47:41 -0500

    Added metadata to Google+.pm

--- lib/DDG/Spice/GooglePlus.pm
+++ lib/DDG/Spice/GooglePlus.pm
@@ -7,6 +7,18 @@ spice to => 'https://www.googleapis.com/plus/v1/people/$1?query=$2&key={{ENV{DDG
 spice from => '(.*?)-(.*)';
 spice proxy_ssl_session_reuse => "off";
 
+primary_example_queries  => "google+ duckduckgo";
+description "Find Google+ users";
+name "GooglePlus";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/GooglePlus.pm";
+topics => "everyday", "social";
+category => "ids";
+
+spice to => 'https://jobs.github.com/positions.json?description=$1&location=$2&callback={{callback}}';
+spice from => '(.*?)-(.*)';
+attribution github => ['https://github.com/jagtalon','jagtalon'],
+            twitter => ['http://twitter.com/juantalon','juantalon'];
+
 triggers startend => 'google+', 'google plus', 'g+', 'gplus', 'google+ user', 'g+ user', 
 'google plus user', 'google+ profile', 'g+ profile', 'gplus profile', 'gplus user', 'g plus profile',
 'g plus user';
