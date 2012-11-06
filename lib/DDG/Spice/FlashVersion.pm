e1fd729896f7e09f24b5e507b9a74758d0c5102d
Jag Talon <talon.jag@gmail.com>
2012-11-06 17:23:03 -0500

    Added metadata to FlashVersion.pm

--- lib/DDG/Spice/FlashVersion.pm
+++ lib/DDG/Spice/FlashVersion.pm
@@ -6,6 +6,16 @@ triggers startend => "flash";
 
 spice call_type => 'self';
 
+primary_example_queries  => "flash version";
+description "Shows the flash version";
+name "FlashVersion";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Expatistan.pm';
+topics => "everyday", "programming";
+category => "software";
+attribution github => ['https://github.com/Getty','Torsten Raudssus'],
+           twitter => ['https://twitter.com/raudssus','Torsten Raudssus'];
+status "disabled";
+
 handle query_lc => sub {
     return $_ eq 'flash version' ? call : ();
 };
