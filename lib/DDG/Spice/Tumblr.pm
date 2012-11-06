7ce6b3a4f4a8e2f7692f32aca072d1c80a9ed5a2
Jag Talon <talon.jag@gmail.com>
2012-11-06 17:57:22 -0500

    Added metadata to Tumblr.pm

--- lib/DDG/Spice/Tumblr.pm
+++ lib/DDG/Spice/Tumblr.pm
@@ -4,6 +4,16 @@ use DDG::Spice;
 
 spice to => 'http://api.tumblr.com/v2/tagged?tag=$1&feature_type=popular&api_key={{ENV{DDG_SPICE_TUMBLR_APIKEY}}}&callback={{callback}}';
 
+primary_example_queries  => "tumblr flowers";
+description "Tumblr pictures";
+name "Tumblr";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Tumblr.pm";
+topics => "everyday", "social";
+category => "entertainment";
+attribution github => ['https://github.com/nilnilnil','Caine Tighe'],
+            twitter => ['http://twitter.com/__nil','caine tighe'];
+status "disabled";
+
 #triggers query_lc => qr/^(?:tumblr\s+)([^\s]+)$/;
 #triggers startend => "tumblr";
 triggers query_lc => qr/^tumblr\s+(.+)$/;
