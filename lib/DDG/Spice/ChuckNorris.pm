b839ac8014a018de8a44d081b98424dc1c328a14
Jag Talon <talon.jag@gmail.com>
2012-11-06 15:48:49 -0500

    Added metadata to ChuckNorris and removed a few errors from Trends.

--- lib/DDG/Spice/ChuckNorris.pm
+++ lib/DDG/Spice/ChuckNorris.pm
@@ -2,6 +2,17 @@ package DDG::Spice::ChuckNorris;
 
 use DDG::Spice;
 
+primary_example_queries => "chuck norris facts";
+secondary_example_queries => "chuck norris jokes";
+description "Chuck Norris facts";
+name "ChuckNorris";
+source "Chuck Norris Database";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/ChuckNorris.pm";
+topics => "special interest";
+category => "random";
+attribution github => ['https://github.com/mr-mayank-gupta','Mayank Gupta'],
+           twitter => ['http://twitter.com/iammayankg','Mayank Gupta'];
+
 spice proxy_cache_valid  => "418 1d";
 spice is_unsafe => 1;
 triggers start => 'chuck norris fact','chuck norris facts','chuck norris joke','chuck norris jokes';
