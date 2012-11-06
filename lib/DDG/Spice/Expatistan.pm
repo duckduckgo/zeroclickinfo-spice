c66f5e7dbb5240ead5a35cfd0b4ca6479de29855
Jag Talon <talon.jag@gmail.com>
2012-11-06 17:18:23 -0500

    Added metadata to Expatisan.pm

--- lib/DDG/Spice/Expatistan.pm
+++ lib/DDG/Spice/Expatistan.pm
@@ -6,6 +6,16 @@ triggers query_lc => qr/cost of living/;
 
 spice to => 'http://www.expatistan.com/api/spice?q=$1&api_key={{ENV{DDG_SPICE_EXPATISTAN_APIKEY}}}';
 
+primary_example_queries => "cost of living in Philadelphia";
+description "See and compare costs of living via Expatistan";
+name "Expatisan";
+icon_url "/i/www.expatistan.com.ico";
+source "Expatisan";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Expatistan.pm";
+topics => "economy and finance";
+category => "facts";
+attribution github => ['https://github.com/hunterlang','Hunter Lang'];
+
 handle query_lc => sub {
     return $_;
 };
