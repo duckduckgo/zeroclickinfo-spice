be40bd35a64c9e716f6b9338a31578ada59a4e89
Jag Talon <talon.jag@gmail.com>
2012-11-07 12:23:23 -0500

    Added metadata to Octopart.pm

--- lib/DDG/Spice/Octopart.pm
+++ lib/DDG/Spice/Octopart.pm
@@ -2,6 +2,16 @@ package DDG::Spice::Octopart;
 
 use DDG::Spice;
 
+primary_example_queries "atmel datasheet";
+description "Searches parts from Octopart";
+name "Octopart";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Octopart.pm";
+icon_url "/i/octopart.com.ico";
+topics "special_interest", "geek";
+category "special";
+attribution github  => ['https://github.com/bnewbold', 'bnewbold'];
+status "enabled";
+
 triggers any => "datasheet", "specs";
 
 spice to => 'http://octopart.com/api/v2/parts/search?apikey={{ENV{DDG_SPICE_OCTOPART_APIKEY}}}&limit=3&optimize.hide_offers=1&optimize.hide_specs=1&q=$1&callback={{callback}}';
