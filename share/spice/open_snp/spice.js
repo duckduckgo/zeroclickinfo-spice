602d3ad70fd413f0513217659e8983fddf764cd3
drsnuggles <philippbay@gmail.com>
2012-11-14 19:45:35 +1000

    Got rid of some superfluous information. Merged PLOS + Mendeley.

--- share/spice/open_snp/spice.js
+++ share/spice/open_snp/spice.js
@@ -14,21 +14,22 @@ function ddg_spice_open_snp(ir) {
         if (ir['snp']['annotations']) {
             var annotations = ir['snp']['annotations'];
             // Mendeley-annotation
-            var mendeley_snippet = '<br/>';
+            var annotations_snippet = '<br/><br/>';
+            var mendeley_length = 0;
+            var plos_length = 0;
             if ( annotations['mendeley'] ){
                 mendeley_length = annotations['mendeley'].length;
                 var first_paper = annotations['mendeley'][0];
                 var time = first_paper['publication_year'];
-                mendeley_snippet += 'First paper:<br/>' + first_paper['author'] + '<i>et al.</i>, ' + time + ': <i>' + first_paper['title'] + '</i>. [<a href="' + first_paper['url'] + '">Link</a>]<br/>';
+                annotations_snippet += '<b>Mendeley:</b> ' + first_paper['author'] + ' <i>et al.</i>, ' + time + ': <i>' + '<a href="' +  first_paper['url'] + '">' + first_paper['title'] + '</a></i>. [<a href="http://www.mendeley.com/research-papers/search/?query=' + snp_name + '">' + (parseInt(mendeley_length)-1) + ' more papers at Mendeley</a>]<br/><br/>';
             }
 
             // PLOS-annotation
-            var plos_snippet = '<br/>';
             if ( annotations['plos'] ) {
                 plos_length = annotations['plos'].length;
                 var first_paper = annotations['plos'][0];
                 time = new Date(first_paper['publication_date']).getFullYear();
-                plos_snippet += 'First paper:<br/> ' + first_paper['author'] + ' <i>et al.</i>, ' + time +  ': <i>' + first_paper['title'] + '</i>. [<a href="' + first_paper['url'] + '">Link</a>]<br/>';
+                annotations_snippet += '<b>PLOS:</b> ' + first_paper['author'] + ' <i>et al.</i>, ' + time +  ': <i><a href="' + first_paper['url'] + '">'+ first_paper['title'] + '</i></a>. [<a href="http://www.plosone.org/search/advancedSearch.action?noSearchFlag=true&query=' + snp_name + '">'  + (parseInt(plos_length)-1) + ' more papers at PLOS</a>]<br/><br/>';
             }
 
             // SNPedia-annotation - add to main-snippet
@@ -42,23 +43,15 @@ function ddg_spice_open_snp(ir) {
             }
         }
 
-        items = [ [], [], [], [] ];
+        items = [ [], [] ];
         items[0]['a'] = snippet;
         items[0]['h'] = 'SNP <b>' + snp_name + '</b> at openSNP.org';
         items[0]['force_big_header'] = 1;
         items[0]['s'] = 'openSNP.org';
         items[0]['u'] = 'http://openSNP.org/snps/' + ir['snp']['name'];
-        if ( mendeley_snippet != '<br/>' ) {
-            items[1]['a'] = mendeley_snippet;
-            items[1]['t'] = 'Mendeley-papers: ' + mendeley_length;
-            items[1]['s'] = 'Mendeley';
-            items[1]['u'] = 'http://www.mendeley.com/research-papers/search/?query=' + snp_name;
-        }
-        if ( plos_snippet != '<br/>' ) {
-            items[2]['a'] = plos_snippet;
-            items[2]['t'] = 'PLOS-papers: ' + plos_length;
-            items[2]['s'] = 'PLOS';
-            items[2]['u'] = 'http://www.plosone.org/search/advancedSearch.action?noSearchFlag=true&query=' + snp_name;
+        if ( annotations_snippet != '<br/><br/>' ) {
+            items[1]['a'] = annotations_snippet;
+            items[1]['t'] = 'Further annotations';
         }
         nra(items);
     }
