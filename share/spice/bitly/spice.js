9af79e3789e0a47632625ebc67273cfd8f4070f5
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2012-11-19 23:50:30 -0500

    minor style tweaks, added bitly image.

--- share/spice/bitly/spice.js
+++ share/spice/bitly/spice.js
@@ -3,10 +3,12 @@ function ddg_spice_bitly (r) {
 		url = r['data']['url'];
 		items = new Array();
 		items[0] = new Array();
-		items[0]['a'] = '<p><span>Shortened URL: <a href="' + url + '" target="_blank">' + url + '</a></span></p>';
+		items[0]['a'] = '<span>Here is a shortened version of that URL: <a href="' + url + '" target="_blank">' + url + '</a></span><br />';
 		items[0]['h'] = 'Shortened URL (bitly)';
+		items[0]['i'] = 'http://i.imgur.com/SPtMJ.png';
 		items[0]['s'] = 'Bitly';
 		items[0]['u'] = 'http://bitly.com';
+		items[0]['force_big_header'] = true;
 		nra(items);
 	}
 }
