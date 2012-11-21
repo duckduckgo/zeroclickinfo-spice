956e93a60f4d083e019f1877c22c6d0cf3b22704
Przemysław Iskra <sparky@pld-linux.org>
2012-11-21 21:03:22 +0100

    Revert "Bootic: use strict_search on triggers other than 'bootic'"
    This reverts commit 39010d8f96155cd8e28cc603f353c603c99610b6.

--- lib/DDG/Spice/Bootic.pm
+++ lib/DDG/Spice/Bootic.pm
@@ -4,31 +4,11 @@ package DDG::Spice::Bootic;
 use DDG::Spice;
 
 triggers any => 'bootic';
-triggers startend => 'buy', 'purchase';
 
-spice from => '(.*)/([01])';
-spice to => 'http://www.bootic.com/cgi-bin/api/search/products?output=json&callback={{callback}}&pretty_name=1&limit=5&strict_search=$2&q=$1';
+spice to => 'http://www.bootic.com/cgi-bin/api/search/products?output=json&callback={{callback}}&pretty_name=1&limit=5&q=$1';
 
-handle query_lc => sub {
-	my $want_strict = 1;
-	my @args;
-	foreach my $word ( split /\s+/, $_ )
-	{
-		if ( $word eq 'bootic' )
-		{
-			$want_strict = 0;
-		}
-		elsif ( $word eq 'buy' or $word eq 'purchase' )
-		{
-			# nothing
-		}
-		else
-		{
-			push @args, $word;
-		}
-	}
-	my $args = join " ", @args;
-	return $args, $want_strict;
+handle remainder => sub {
+	return $_;
 };
 
 1;
