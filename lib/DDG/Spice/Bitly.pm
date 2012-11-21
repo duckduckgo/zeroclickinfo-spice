50b6508b65b5b7b5b8c71067f8007e1c0610f318
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2012-11-21 01:28:50 -0500

    Properly handle URIs w/o http:// or https://
    Removed: use URI::Escape as it is not used.

--- lib/DDG/Spice/Bitly.pm
+++ lib/DDG/Spice/Bitly.pm
@@ -1,15 +1,19 @@
 package DDG::Spice::Bitly;
 
 use DDG::Spice;
-use URI::Escape;
 
 spice to => 'http://api.bitly.com/v3/shorten?login=ddgspiceplugin&apiKey=R_2cbfeea1621c9f7795e8413356cfde46&longUrl=$1&callback={{callback}}';
 
 triggers start => 'bitly', 'bit.ly', 'shorten', 'shorten url', 'short url', 'url shorten';
 
 handle remainder => sub {
-	my ($longUri) = @_;
-	return $longUri if $longUri;
+	my ($longUri) = shift;
+	if ($longUri) {
+		if (!($longUri =~ /^https?:\/\//)) {
+			$longUri = 'http://' . $longUri;
+		}
+		return $longUri;
+	}
 	return;
 };
 
