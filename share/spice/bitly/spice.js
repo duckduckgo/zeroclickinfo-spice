2f3e27cf7d7ee78ff7d6c626c6a30cc8db6dacee
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2012-11-21 15:09:41 -0500

    Style tweaks.
    - Added a smaller image (with padding via spice.css).
    - Answer and 'More At' back on two lines.

--- share/spice/bitly/spice.js
+++ share/spice/bitly/spice.js
@@ -3,8 +3,9 @@ function ddg_spice_bitly (r) {
 		var url = r.data.url;
 		var items = [[]];
 		items[0] = {
-			a: '<span>Here is a shortened version of that URL: <input type="text" autofocus="true" value="' + url + '"/> :: </span>',
+			a: '<span>Here is a shortened version of that URL: <input type="text" autofocus="true" value="' + url + '"/></span><br />',
 			h: 'Shortened URL (bitly)',
+			i: 'http://i.imgur.com/xVpFr.png',
 			s: 'Bitly',
 			u: 'http://bitly.com',
 			force_big_header: true
