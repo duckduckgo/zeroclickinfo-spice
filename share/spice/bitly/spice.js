9f6e9b3228ff6de642883d6234a516fdd86ef20f
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2012-11-21 01:32:42 -0500

    Minor style tweaks (removed bitly image, zci on one line).

--- share/spice/bitly/spice.js
+++ share/spice/bitly/spice.js
@@ -3,9 +3,8 @@ function ddg_spice_bitly (r) {
 		url = r['data']['url'];
 		items = new Array();
 		items[0] = new Array();
-		items[0]['a'] = '<span>Here is a shortened version of that URL: <a href="' + url + '" target="_blank">' + url + '</a></span><br />';
+		items[0]['a'] = '<span>Here is a shortened version of that URL: <a href="' + url + '" target="_blank">' + url + '</a> :: </span>';
 		items[0]['h'] = 'Shortened URL (bitly)';
-		items[0]['i'] = 'http://i.imgur.com/SPtMJ.png';
 		items[0]['s'] = 'Bitly';
 		items[0]['u'] = 'http://bitly.com';
 		items[0]['force_big_header'] = true;
