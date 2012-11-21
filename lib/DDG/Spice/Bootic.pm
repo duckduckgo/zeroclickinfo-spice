4d29e8bcd5f9a304238cbc6cf1dd2c4a0b3e58a7
Przemysław Iskra <sparky@pld-linux.org>
2012-11-21 21:21:32 +0100

    Try smart search to return better results.

--- lib/DDG/Spice/Bootic.pm
+++ lib/DDG/Spice/Bootic.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 
 triggers any => 'bootic';
 
-spice to => 'http://www.bootic.com/cgi-bin/api/search/products?output=json&callback={{callback}}&pretty_name=1&limit=5&q=$1';
+spice to => 'http://www.bootic.com/cgi-bin/api/search/products?output=json&callback={{callback}}&pretty_name=1&limit=5&smart=1&q=$1';
 
 handle remainder => sub {
 	return $_;
