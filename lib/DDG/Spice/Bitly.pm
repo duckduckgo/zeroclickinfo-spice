dccddcfabedad163071564aba2b024b81a4e5afe
Jag Talon <talon.jag@gmail.com>
2012-11-26 10:35:16 -0500

    Changed the name of the variable.

--- lib/DDG/Spice/Bitly.pm
+++ lib/DDG/Spice/Bitly.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Bitly;
 
 use DDG::Spice;
 
-spice to => 'http://api.bitly.com/v3/shorten?login=ddgspiceplugin&apiKey={{ENV{DDG_SPICE_FORVO_APIKEY}}}&longUrl=$1&callback={{callback}}';
+spice to => 'http://api.bitly.com/v3/shorten?login=ddgspiceplugin&apiKey={{ENV{DDG_SPICE_BITLY_APIKEY}}}&longUrl=$1&callback={{callback}}';
 
 triggers start => 'bitly', 'bit.ly', 'shorten', 'shorten url', 'short url', 'url shorten';
 
