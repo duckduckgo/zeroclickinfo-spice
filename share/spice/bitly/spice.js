a908e725e23dde5509aa733b2230ed39bbd6f132
Jag Talon <talon.jag@gmail.com>
2012-11-26 10:13:51 -0500

    Removed attributes and added arguments to nra.
     - Changed nra(items) to nra(items, 1, 2) because the previous one
       doesn't allow people to click on the input element.
     - Changed autofocus="true" to simply autofocus.

--- share/spice/bitly/spice.js
+++ share/spice/bitly/spice.js
@@ -3,13 +3,13 @@ function ddg_spice_bitly (r) {
 		var url = r.data.url;
 		var items = [[]];
 		items[0] = {
-			a: '<span>Here is a shortened version of that URL: <input type="text" autofocus="true" value="' + url + '"/></span><br />',
-			h: 'Shortened URL (bitly)',
+			a: '<span>Here is a shortened version of that URL: <input type="text" autofocus value="' + url + '"/></span><br />',
+			h: 'Shortened URL (Bitly)',
 			i: 'http://i.imgur.com/xVpFr.png',
 			s: 'Bitly',
 			u: 'http://bitly.com',
 			force_big_header: true
 		};
-		nra(items);
+		nra(items, 1, 2);
 	}
 }
