3d5ef3d0d4085952fbc0442e8b6537ef899d1a9b
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2012-11-26 23:40:47 -0500

    Added metadata to Bitly plugin.

--- lib/DDG/Spice/Bitly.pm
+++ lib/DDG/Spice/Bitly.pm
@@ -1,6 +1,19 @@
 package DDG::Spice::Bitly;
+# ABSTRACT: Return a shortened version of a URL using the bitly API.
 
 use DDG::Spice;
+
+primary_example_queries "bitly http://www.example.com/long/url";
+secondary_example_queries "shorten http://www.example.com/long/url", "url shorten www.example.com/long/url";
+description "Shorten URLs using the bitly API";
+name "Bitly";
+icon_url "/i/bitly.com.ico";
+source "Bitly";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Bitly.pm";
+topics "social";
+category "computing_tools";
+attribution github => ['https://github.com/danjarvis','Dan Jarvis'],
+            twitter => ['http://twitter.com/danjarvis','danjarvis'];
 
 spice to => 'http://api.bitly.com/v3/shorten?login=ddgspiceplugin&apiKey={{ENV{DDG_SPICE_BITLY_APIKEY}}}&longUrl=$1&callback={{callback}}';
 
