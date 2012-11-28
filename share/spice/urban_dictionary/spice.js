0c0664a7f89d35c15fc64ddd3822efcb86ae0de2
Filippo Valsorda <filippo.valsorda@gmail.com>
2012-11-28 15:06:22 +0100

    UrbanDictionary plugin: js cleanup

--- share/spice/urban_dictionary/spice.js
+++ share/spice/urban_dictionary/spice.js
@@ -1,19 +1,17 @@
 function ddg_spice_urban_dictionary(res) 
 {
-	if (res.result_type == "exact" && res.list[0]) {
+	if (res && res.result_type === "exact" && res.list && res.list[0]) {
 		var def = res.list[0];
 
-		items = new Array();
-		items[0] = new Array();
+		var items = [[]];
 
-		items[0]['a'] = def.definition.split("\n")[0];
-		// items[0]['h'] = def.word + ' (Urban Dictionary definition)'; // Optimize for vertical space
-		items[0]['s'] = 'Urban Dictionary';
-
-		// Unsure if we should link to the specific definition
-		// I prefer the general definitions page
-		// items[0]['u'] = def.permalink; 
-		items[0]['u'] = 'http://www.urbandictionary.com/define.php?term=' + def.word; 
+		items[0] = {
+			a: def.definition.split("\n")[0],
+			//h: def.word + ' (Urban Dictionary definition)', // Optimize for vertical space
+			s: 'Urban Dictionary',
+			u: 'http://www.urbandictionary.com/define.php?term=' + def.word
+			//u: def.permalink // Alternatively, link to the specific shown definition
+		};
 
 		nra(items, 1); // Show the favicon and the "More at" link at the bottom
 	}
