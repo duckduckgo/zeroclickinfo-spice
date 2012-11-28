72d56957589537c29115f4d0bb270e1ccead146f
moollaza <moollaza@gmail.com>
2012-11-28 09:24:22 -0500

    small fix to Bootic.pm

--- lib/DDG/Spice/Bootic.pm
+++ lib/DDG/Spice/Bootic.pm
@@ -8,7 +8,7 @@ triggers any => 'bootic';
 spice to => 'http://www.bootic.com/cgi-bin/api/search/products?output=json&callback={{callback}}&pretty_name=1&limit=5&smart=1&q=$1';
 
 handle remainder => sub {
-	return $_;
+	return $_ if defined $_;
 };
 
 1;
