c2b6caf6ad123b4f3f1806f5fcbc97d64a875673
Cinque McFarlane-Blake <cinque_mcfarlane-blake@brown.edu>
2012-11-30 13:32:06 -0500

    Edited using jslint, declaring all variables if not global

--- share/spice/pictobar/spice.js
+++ share/spice/pictobar/spice.js
@@ -1,30 +1,26 @@
 function ddg_spice_pictobar(pbr) {
-    var whole,body,mid,stars,td;
-    
+    "use strict";
+    var whole, body, mid, td, x, i;
     //check if name is there
-    if( (pbr['fname'] && pbr['lname']) ){
-
+    if ((pbr.fname && pbr.lname)) {
         //generates stars
-        
         //numerical data into array
-        var outPuts = new Array(pbr['attra'],pbr['friend'],pbr['open'],pbr['self'],pbr['thot']);
-        var Image = new Array();
-        
+        var outPuts = [pbr.attra, pbr.friend, pbr.open, pbr.self, pbr.thot];
         //array of attribute names
-        var attra = new Array('<tr><td>Attractiveness:</td>','<tr><td>Friendliness:</td>','<tr><td>Openness:</td>','<tr><td>Selflessness:</td>','<tr><td>Thoughtfulness:</td>');
-        var outString = new String('');
-        var outputString = new String('');
-        var orgLoop = new Array();
+        var attra = ['<tr><td>Attractiveness:</td>', '<tr><td>Friendliness:</td>', '<tr><td>Openness:</td>', '<tr><td>Selflessness:</td>', '<tr><td>Thoughtfulness:</td>'];
+        var outString = '';
+        var outputString = '';
+        var orgLoop = [];
         //loop to define rows for stars and attributes
-        for (x=0; x<5; x++){
-            orgLoop[x] = new Array();
+        for (x = 0; x < 5; x += 1) {
+            orgLoop[x] = [];
             orgLoop[x].push(attra[x]);
             //loop to create 5 stars for attributes based on numerical data array
-            for ( i=1; i<6; i++){
-                if ( i < outPuts[x]+1 ){
+            for (i = 1; i < 6; i += 1) {
+                if (i < outPuts[x] + 1) {
                     orgLoop[x][i] = '<td><img src="http://www.pictobar.com/starfill.png" width="10" height="10" /></td>';
                 }
-                if ( i > outPuts[x]){
+                if (i > outPuts[x]) {
                     orgLoop[x][i] = '<td><img src="http://www.pictobar.com/starunfill.png" width="10" height="10" /></td>';
                 }
             }
@@ -32,34 +28,29 @@ function ddg_spice_pictobar(pbr) {
             orgLoop[x].push('</tr>');
             //convert row to string in order to remove commas in row
             outString[x] = orgLoop[x].toString();
-            outputString[x] = outString[x].replace(/,/g," ");
+            outputString[x] = outString[x].replace(/,/g, " ");
         }
         //convert row to string in order to remove leftover commas in row
-        
         outString = orgLoop.toString();
-        outputString = outString.replace(/,/g," ");
-        
+        outputString = outString.replace(/,/g, " ");
         //creates table where data is displayed
-        
         body = d.createElement('table');
-        body.setAttribute('class','all');
+        body.setAttribute('class', 'all');
         mid = d.createElement('table');
-        mid.setAttribute('class','alltable');
+        mid.setAttribute('class', 'alltable');
         td = d.createElement('td');
-        td.innerHTML = '<a href="'+pbr['proflink']+'"><p>'+pbr['fname']+' '+pbr['lname']+'</p></a><table class="alltable1"><tr><td><center><a href="'+pbr['proflink']+'"><img src="'+pbr['piclink']+'"  class="imagecirlce" /></a></center></td><td><center>Described by some as:</br>'+pbr['tags']+'</center></td><td><center><table width="150" height="60">'+outputString+'</table></center></td></tr><tr><td><a href="'+pbr['proflink']+'">View Full Profile</a></br>Gender:'+pbr['gendr']+'</br>Total Votes: '+pbr['totvot']+'</td><td>Percent Introversion: '+pbr['intro']+'%</br>Percent Extroversion: '+pbr['extro']+'%</td></tr></table>';
+        td.innerHTML = '<a href="' + pbr.proflink + '"><p>' + pbr.fname + ' ' + pbr.lname + '</p></a><table class="alltable1"><tr><td><center><a href="' + pbr.proflink + '"><img src="' + pbr.piclink + '"  class="imagecirlce" /></a></center></td><td><center>Described by some as:</br>' + pbr.tags + '</center></td><td><center><table width="150" height="60">' + outputString + '</table></center></td></tr><tr><td><a href="' + pbr.proflink + '">View Full Profile</a></br>Gender:' + pbr.gendr + '</br>Total Votes: ' + pbr.totvot + '</td><td>Percent Introversion: ' + pbr.intro + '%</br>Percent Extroversion: ' + pbr.extro + '%</td></tr></table>';
         mid.appendChild(td);
-
         //body 
         whole = mid;
-        
-        items = new Array();
-        items[0] = new Array();
-        items[0]['a'] = whole;
-        items[0]['h'] = '';
-        items[0]['s'] = 'Pictobar';
-        items[0]['u'] = '';
-        items[0]['i'] = 'http://i474.photobucket.com/albums/rr101/Pcfllintent/pbicon.jpg';
+        var items = [[]];
+        items[0] = {
+            a: whole,
+            h: '',
+            s: 'Pictobar',
+            u: '',
+            i: 'http://www.pictobar.com/images/pbicon.jpg'
+        };
         nra(items);
-    } 
+    }
 }
-
