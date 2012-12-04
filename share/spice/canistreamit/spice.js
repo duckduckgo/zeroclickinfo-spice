13684f1bf95ca8021d5cf96d49f3deaa1f2206ad
Jag Talon <talon.jag@gmail.com>
2012-12-04 13:45:10 -0500

    Removed unused variables and added a filter in the for-in loop.

--- share/spice/canistreamit/spice.js
+++ share/spice/canistreamit/spice.js
@@ -3,64 +3,59 @@ function ddg_spice_canistreamit(movies) {
     
     // Words to skip when isRelevant is called.
     if(movies && movies.length >= 1 && DDG.isRelevant(movies[0].title, {})) {
-        var result,img,snippet,link,div;
-        var items = new Array();
+        var result,
+            items = [],
+            hasOwn = Object.prototype.hasOwnProperty;
+
         for(var i = 0; i < movies.length; i++) {
             result = movies[i];
 
             // Make title for header
-            var header = 'Watch ' + result.title + " ("+result.year+")";
+            var header = 'Watch ' + result.title + " ("+result.year+")",
+                item = [],
+                content = "<div><i>Starring:</i> "+result.actors+".</div>",
+                streamingStr = "",
+                rentalStr = "",
+                streamingCount = 0,
+                rentalCount = 0,
+                service,
+                price;
 
-            var item = new Array();
-            var content = "<div><i>Starring:</i> "+result.actors+".</div>";
-            var streamingStr = "";
-            var rentalStr = "";
-            var streamingCount = 0;
-            var rentalCount = 0;
-            for(var subtype in result.affiliates)
-            {
-                var service = result.affiliates[subtype];
-                var price = parseFloat(service.price);
-                if(price > 0)
-                {
-                    rentalStr += service.friendlyName.replace("Rental","");
-                    rentalStr += "($"+service.price+")";
-                    rentalStr += ", ";
-                    rentalCount++;
-                }
-                else
-                {
-                    streamingStr += service.friendlyName;
-                    streamingStr += ", ";
-                    streamingCount++;
+            for(var subtype in result.affiliates) {
+                if(hasOwn.call(result.affiliates, subtype)) {
+                    service = result.affiliates[subtype];
+                    price = parseFloat(service.price);
+
+                    if(price > 0) {
+                        rentalStr += service.friendlyName.replace("Rental","");
+                        rentalStr += "($"+service.price+")";
+                        rentalStr += ", ";
+                        rentalCount++;
+                    } else {
+                        streamingStr += service.friendlyName;
+                        streamingStr += ", ";
+                        streamingCount++;
+                    }
                 }
             }
-            if(streamingCount > 0)
-            {
+            if(streamingCount > 0) {
               content += "<div><i>Streaming on</i> "+streamingStr.substring(0,streamingStr.length-2)+".</div>";
             }
-            if(rentalCount > 0)
-            {
+            if(rentalCount > 0) {
               content += "<div><i>Rent from </i> "+rentalStr.substring(0,rentalStr.length-2)+".</div>";
             }
 
-
-            item['a'] = content;
-
-            item['h'] = header;
-
-            // Source name and url for the More at X link.
-            item['s'] = 'CanIStream.It';
-            item['u'] = result.links.shortUrl;
-
-            // Force no compression.
-            item['f'] = 1;
-
-            // Thumbnail url
-            item['i'] = result.image;
-
+            item = {
+                a: content,
+                h: header,
+                // Source name and url for the More at X link.
+                s: 'CanIStream.It',
+                u: result.links.shortUrl,
+                // Force no compression.
+                f: true,
+                i: result.image
+            };
             items.push(item);
-
         }
         // The rendering function is nra.
         nra(items);
