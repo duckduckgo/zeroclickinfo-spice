1399fb31db380d365f54d3609c8342e14228fa23
Filippo Valsorda <filippo.valsorda@gmail.com>
2012-12-16 15:32:15 +0100

    UrbanDictionary: added metadata and reduced triggers

--- lib/DDG/Spice/UrbanDictionary.pm
+++ lib/DDG/Spice/UrbanDictionary.pm
@@ -3,13 +3,24 @@ package DDG::Spice::UrbanDictionary;
 
 use DDG::Spice;
 
-spice is_unsafe => 1;
-triggers startend => "ud", "urban", "urbandictionary", "urban dictionary", "meaning", "meaning of", "definition", "definition of", "acronym";
-spice to => 'http://api.urbandictionary.com/v0/define?term=$1&callback={{callback}}';
-
+primary_example_queries "urbandictionary ROTFL";
+secondary_example_queries "ud OMG", "ud ASD";
+description "Lookup UrbanDictionary definitions";
+name "UrbanDictionary";
+icon_url "/i/urbandictionary.com.ico";
+source "UrbanDictionary";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/UrbanDictionary.pm";
+topics "geek";
+category "language";
 attribution github => ['https://github.com/FiloSottile','FiloSottile'],
             web => ['http://pytux.it','Filippo Valsorda'],
             email => ['filippo.valsorda@gmail.com','Filippo Valsorda'];
+
+spice is_unsafe => 1;
+
+triggers startend => "ud", "urban", "urbandictionary", "urban dictionary";
+
+spice to => 'http://api.urbandictionary.com/v0/define?term=$1&callback={{callback}}';
 
 handle remainder => sub {
     my ($term) = @_;
