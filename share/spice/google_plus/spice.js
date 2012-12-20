68b7503863a4b33c9fc6a9e51d8aa1a9a0ad5123
Jag Talon <talon.jag@gmail.com>
2012-12-19 19:56:29 -0500

    Finished the code for Google+. Still missing documentation.

--- share/spice/google_plus/spice.js
+++ share/spice/google_plus/spice.js
@@ -14,21 +14,92 @@
 			re = /\s*(google\+|google\splus|g\+|gplus|google\+\suser|g\+\suser|google\splus\suser|google\+\sprofile|g\+\sprofile|gplus\sprofile|gplus\suser|g\splus\sprofile|g\splus\suser)\s*/;
 			query = DDG.get_query();
 			collection = {
-				limit: getLimit(google),
+				limit: getLimit(google.items.length, 5),
 				google: google,
 				query: query.replace(re, "")
+			};
+
+			display(collection);
+		} else if(google && google.kind === "plus#person") {
+			collection = {
+				about: getAbout(google),
+				organizations: getOrganizations(google),
+				placesLived: getPlaces(google),
+				links: getLinks(google),
+				google: google
 			};
 
 			display(collection);
 		}
 	};
 
+	function getLinks(google) {
+		var links = [], unique = [];
+		if(google.urls) {
+		    if(google.urls.length > 2) {
+		        google.urls.length -= 2;
+		        for(var i = 0; i < google.urls.length; i += 1) {
+		            if(unique.indexOf(google.urls[i].value) === -1) {
+		                unique.push(google.urls[i].value);
+		                var re = /(?:https?:\/\/)?(?:www\.)?([^\/]+)\/?.*/;
+		                var string =  google.urls[i].value.toLowerCase();
+		                string = string.replace(re, "$1");
+		                re = /\.(?:com|net|org)/;
+		                string = string.replace(re, "");
+		                links.push([google.urls[i].value, string]);
+		            }
+		        }
+		    }
+		}
+		return links;
+	}
+
+	function getPlaces(google) {
+		var places = [];
+		if(google.placesLived) {
+		    for(var i = 0, length = google.placesLived.length; i < length; i += 1) {
+		        if(google.placesLived[i].primary) {
+		            places.push(google.placesLived[i].value);
+		        }
+		    }
+		}
+		return places;
+	}
+
+	function getOrganizations(google) {
+		var orgs = '', length;
+		if(google.organizations) {
+			length = getLimit(google.organizations.length, 2);
+            for(var i = 0; i < length; i += 1) {
+                orgs += google.organizations[i].name + 
+                    (google.organizations[i].title ? ' (' + google.organizations[i].title + ')' : '');
+                if(i !== length - 1) {
+                    orgs += ', ';
+                }
+            }
+		}
+		return orgs;
+	}
+
+	function getAbout(google) {
+		var out = '';
+		if (google.tagline && google.tagline !== '<br>') {
+			out = google.tagline;
+		} else if(google.aboutMe && google.aboutMe !== '<br>') {
+			out = google.aboutMe.substring(0, 200);
+			if(google.aboutMe.length > 200) {
+			    out += '...';
+			}
+		}
+		return out;
+	}
+
 	// Limit the number of items displayed.
-	function getLimit(google) {
-		if(google.items.length > 5) {
-		    return 5;
+	function getLimit(compare, limit) {
+		if(compare > limit) {
+		    return limit;
 		} else {
-		    return google.items.length;
+		    return compare;
 		}
 	}
 
@@ -82,6 +153,45 @@
 			return out;
 		}
 
+		function aboutHTML(about) {
+			if(about !== '') {
+				return '<div class="google_profile"><i>Introduction: </i> ' + about + '</div>';
+			}
+			return '';
+		}
+
+		function organizationsHTML(organizations) {
+			if(organizations !== '') {
+				return '<div class="google_orgs"><i>Organizations: </i>' + organizations + '</div>';
+			}
+			return '';
+		}
+
+		function placesHTML(placesLived) {
+			var out = '';
+			for(var i = 0, length = placesLived.length; i < length; i += 1) {
+				out += '<div class="google_places"><i>Lives in: </i>' + placesLived[i] + '</div>';
+			}
+			return out;
+		}
+
+		function linksHTML(links) {
+			console.log(links);
+			var out = '';
+			if(links.length > 0) {
+				for(var i = 0, length = links.length; i < length; i += 1) {
+					out += '<a href="' + links[i][0] + '" title="' + links[i][0] + '">' +
+						links[i][1] + '</a>';
+					out += ', ';
+				}
+				if(out.substring(out.length-2, out.length) === ', ') {
+                    out = out.substring(0, out.length-3);
+                }
+				return '<div class="google_links"><i>Links: </i>' + out + '</div>'
+			}
+			return '';
+		}
+
 		var items = [[]];
 		if(collection.google.kind === "plus#peopleFeed") {
 			items[0] = {
@@ -90,8 +200,19 @@
 				s: 'Google+',
 				f: true,
 				u: 'http://plus.google.com/s/' + collection.query
-			}
-			nra(items,1,1);
+			};
+			nra(items, 1, 1);
+		} else {
+			items[0] = {
+				a: aboutHTML(collection.about) + organizationsHTML(collection.organizations) + 
+					placesHTML(collection.placesLived) + linksHTML(collection.links),
+				h: collection.google.displayName + ' (Google+)',
+				s: 'Google+',
+				u: collection.google.url,
+				f: true,
+				i: collection.google.image.url.substring(0, collection.google.image.url.length-6),
+			};
+			nra(items);
 		}
 	}
 }(this));
