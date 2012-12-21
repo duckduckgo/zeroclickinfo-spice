61f96f8fad073398d3438755e0ed7716a146effc
Jag Talon <talon.jag@gmail.com>
2012-12-21 14:49:30 -0500

    Change the regexp for triggering from [a-zA-Z] to [\w].
    This is for queries with non-latin characters e.g. google+ こん (I don't
    know what that means, but it works).

--- lib/DDG/Spice/GooglePlus.pm
+++ lib/DDG/Spice/GooglePlus.pm
@@ -25,7 +25,7 @@ handle remainder => sub {
 	if($query =~ /userid:(\d+)$/) {
 		return $1.'-'; 
 	}
-    if($query =~ /((?:[a-zA-Z]|\s)+)$/) {
+    if($query =~ /((?:[\w]|\s)+)$/) {
 		return '-'.$1;
 	}
 	return;
