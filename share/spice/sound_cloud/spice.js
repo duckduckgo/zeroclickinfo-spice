e8a68d2ede29132b7f4814bd979f5cf12a202e31
Jag Talon <talon.jag@gmail.com>
2012-12-27 16:02:40 -0500

    Added documentation to the Sound Cloud plugin.

--- share/spice/sound_cloud/spice.js
+++ share/spice/sound_cloud/spice.js
@@ -1,38 +1,42 @@
+// `ddg_spice_sound_cloud` is a callback function that gets
+// called when people search for movie titles. An example trigger
+// is "sc oppa spacejam style".
+
 function ddg_spice_sound_cloud(sc) {
     "use strict";
-    // Snippet shown in 0-click
+
     var snippet = d.createElement('span'), 
         iframe = d.createElement('iframe'), 
         res, 
         items = [[]];
 
-    // Validity check
+    // Check if the properties that we need are available.
+    // if it isn't, it's not going to display anything.
     if (sc && sc.length && sc[0].uri && sc[0].title && sc[0].user && sc[0].user.username && sc[0].permalink_url) {
         // Rename for readability
         res = sc[0];
 
-        // Create embedded soundcloud player
+        // Embed Sound Cloud's player in our plugin.
         iframe.setAttribute('width', '100%');
         iframe.setAttribute('height', '166');
         iframe.setAttribute('scrolling', 'no');
         iframe.setAttribute('frameborder', 'no');
         iframe.setAttribute('src', 'https://w.soundcloud.com/player/?url=' + encodeURI(res.uri));
-
-        // Append iframe to snippet
         snippet.appendChild(iframe);
 
         items[0] = {
-            // Set the header to the SoundCloud title
+            // Set the header to the Sound Cloud title.
             a: snippet,
-            h: res.title,
+            h: "Sound Cloud (" + res.title + ")",
             // Source name and url for the More at X link.
             s: "SoundCloud",
             u: res.permalink_url,
             // No compression.
-            f: true
+            f: true,
+            force_big_header: true
         };
 
-        // The rendering function is nra.
+        // The rendering function is `nra`.
         nra(items);
     }
 }
