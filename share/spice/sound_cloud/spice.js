113b33958f506a9af3bf08cbed0ddba14d36daaa
Jag Talon <talon.jag@gmail.com>
2013-01-07 16:36:27 -0500

    Sound Cloud now prevents the embedded player from loading.
    The embedded player from Sound Cloud will only load if it is clicked by
    the user. I was told that this will increase the responsiveness of the
    pag, and it also conforms with the current policy of DuckDuckGo.

--- share/spice/sound_cloud/spice.js
+++ share/spice/sound_cloud/spice.js
@@ -6,7 +6,6 @@ function ddg_spice_sound_cloud(sc) {
     "use strict";
 
     var snippet = d.createElement('span'), 
-        iframe = d.createElement('iframe'), 
         res, 
         items = [[]];
 
@@ -16,13 +15,7 @@ function ddg_spice_sound_cloud(sc) {
         // Rename for readability
         res = sc[0];
 
-        // Embed Sound Cloud's player in our plugin.
-        iframe.setAttribute('width', '100%');
-        iframe.setAttribute('height', '166');
-        iframe.setAttribute('scrolling', 'no');
-        iframe.setAttribute('frameborder', 'no');
-        iframe.setAttribute('src', 'https://w.soundcloud.com/player/?url=' + encodeURI(res.uri));
-        snippet.appendChild(iframe);
+        snippet.appendChild(initImage(res));
 
         items[0] = {
             a: snippet,
@@ -35,5 +28,48 @@ function ddg_spice_sound_cloud(sc) {
 
         // The rendering function is `nra`.
         nra(items, 1, 1);
+    }
+
+    function initImage(res) {
+        var image;
+        if(res.artwork_url) {
+            image = res.artwork_url;
+        } else if(res.user.avatar_url) {
+            image = res.user.avatar_url;
+        }
+        var outer_div = d.createElement('div');
+        
+        var img = d.createElement('img');
+        outer_div.setAttribute('id', 'soundcloud-play');
+        img.setAttribute('src', image);
+        img.setAttribute('style', 'clear: left; float: left; margin-left: 10px; margin-right: 10px; border: 1px solid rgb(255, 255, 255);');
+        img.addEventListener("click", hideImage, false);
+        outer_div.appendChild(img);
+        outer_div.appendChild(addLink(res.user.username, res.user.uri));
+        
+        return outer_div;
+    }
+
+    function addLink(value, href) {
+        var link = d.createElement('a');
+        link.setAttribute('href', href);
+        link.innerHTML = value;
+        return link;
+    }
+
+    function hideImage() { 
+        DDG.toggle('soundcloud-play', -1);
+        snippet.appendChild(soundcloud(res));
+    }
+
+    // Embed Sound Cloud's player in our plugin.
+    function soundcloud(res) {
+        var iframe = d.createElement('iframe');
+        iframe.setAttribute('width', '100%');
+        iframe.setAttribute('height', '166');
+        iframe.setAttribute('scrolling', 'no');
+        iframe.setAttribute('frameborder', 'no');
+        iframe.setAttribute('src', 'https://w.soundcloud.com/player/?url=' + encodeURI(res.uri));
+        return iframe;
     }
 }
