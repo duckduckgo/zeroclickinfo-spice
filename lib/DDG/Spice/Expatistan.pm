fa767f34fb391deec9f81849809e923835c87c13
jcr <james.rubino_g-mail.com>
2013-02-11 07:36:24 -0600

    DDG::Spice::Bitcoin - 0.0.0.1  converts currencies from mtgox: server listed updates every 15min

--- lib/DDG/Spice/Expatistan.pm
+++ lib/DDG/Spice/Expatistan.pm
@@ -4,11 +4,11 @@ use DDG::Spice;
 
 triggers query_lc => qr/cost of living/;
 
-spice to => 'http://www.expatistan.com/api/spice?q=$1&api_key={{ENV{DDG_SPICE_EXPATISTAN_APIKEY}}}';
+spice to => 'https://s3-eu-west-1.amazonaws.com/btc.convert/data.json?q={{data}}';
 
-primary_example_queries "cost of living in Philadelphia";
-description "See and compare costs of living via Expatistan";
-name "Expatisan";
+primary_example_queries "5.023 btc to usd";
+description "add bitcoin to currency exchange";
+name "BitcoinConverter";
 icon_url "/i/www.expatistan.com.ico";
 source "Expatisan";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Expatistan.pm";
