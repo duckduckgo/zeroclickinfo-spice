91f1d7678becb948c628a84bb10461307e316493
3pence <224221be@opayq.com>
2013-02-18 18:11:48 -0600

    core logic moved to client js

--- lib/DDG/Spice/Bitcoin/Rates.pm
+++ lib/DDG/Spice/Bitcoin/Rates.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 #triggers query_lc =>  "btc";
 triggers any => "usd to btc", "btc to usd",  "aud to btc", "btc to aud",  "chf to btc", "btc to chf",  "nok to btc", "btc to nok",  "rub to btc", "btc to rub",  "cny to btc", "btc to cny",  "jpy to btc", "btc to jpy",  "thb to btc", "btc to thb",  "sgd to btc", "btc to sgd",  "gbp to btc", "btc to gbp",  "nzd to btc", "btc to nzd",  "pln to btc", "btc to pln",  "eur to btc", "btc to eur",  "sek to btc", "btc to sek",  "dkk to btc", "btc to dkk",  "hkd to btc", "btc to hkd",  "cad to btc", "btc to cad";
 
-spice to => 'http://174.129.221.227/q?$1';
+spice to => 'https://s3-eu-west-1.amazonaws.com/btc.convert/data.json';
 spice wrap_jsonp_callback => 1;
 
 primary_example_queries "5.0234567 btc to usd";
@@ -13,7 +13,7 @@ secondary_example_queries "1.0 usd to btc";
 description "add bitcoin to currency exchange";
 name "BitcoinConvert";
 
-icon_url "/i/174.129.221.227.ico";
+icon_url "/i/174.129.229.21.ico";
 source "MtGox";
 
 spice proxy_cache_valid => "any 5m";
--- share/spice/bitcoin/rates/spice.js
+++ share/spice/bitcoin/rates/spice.js
@@ -1,25 +1,50 @@
 function ddg_spice_bitcoin_rates(data) {
-  var img,snippet,link,div;
+  var img,snippet,link,div, convert_to, initial, new_value, origin, res, convert_to, initial, new_value, origin, qr, res, divd, symb1, symb2;
+  var query = decodeURIComponent(rq);
 
-  // validity check
-  if (data['status'] == 200 && data['value']) {
+  qr = query.split(' ');
 
-    items = new Array();
-    items[0] = new Array();
-    items[0]['a'] = '<section>'+data['res']+'</section><br>';
+  initial = qr[0];
 
-    items[0]['h'] = 'BTC on DDG';
-    items[0]['i'] = data['img'];
+  origin = qr[1];
 
-     // Source name and url for the More at X link.
-     items[0]['s'] = 'BTC on DDG';
-     items[0]['u'] = 'http://174.129.221.227/';
-     
-     // Force no compression.
-     items[0]['force_no_fold'] = 1;
-     items[0]["force_big_header"] = true;
+  convert_to = qr[3];
+
+  if (origin === 'btc') {
+  new_value = initial * data[convert_to]['value'];
+  new_value = Math.round(new_value * Math.pow(10, 2)) / Math.pow(10, 2);
+  symb1 = data['thb']['hex'];
+  symb2 = data[convert_to]['hex'];
+  } else {
+  new_value = initial / data[origin]['value'];
+  new_value = Math.round(new_value * Math.pow(10, 8)) / Math.pow(10, 8);
+  symb1 = data[origin]['hex'];
+  symb2 = data['thb']['hex'];
+  }
+  
+  
+
+  res = '<section>'+symb1+' '+qr[0] + ' ' + origin + ' is equal to ' +symb2+' '+new_value + ' ' + convert_to+'</section><br>';
+
+    // validity check
+
+  items = new Array();
+  items[0] = new Array();
+  //items[0]['a'] = "Hello";
+  items[0]['a'] = res;
+
+  items[0]['h'] = 'Bitcoin Currency Conversion';
+  items[0]['i'] = data['qr'];
+
+   // Source name and url for the More at X link.
+   items[0]['s'] = 'BTC on DDG';
+   items[0]['u'] = 'http://174.129.229.21';
+   
+   // Force no compression.
+   items[0]['force_no_fold'] = 1;
+   items[0]["force_big_header"] = true;
      
      // The rendering function is nra.
-     nra(items);
-  }
+   nra(items);
+
 }
