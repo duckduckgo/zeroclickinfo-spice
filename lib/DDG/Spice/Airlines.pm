4126fba19d54172981b9c1b20ed84b87c9010f88
Jag Talon <talon.jag@gmail.com>
2013-02-26 18:03:58 -0500

    Changed tab to spaces.

--- lib/DDG/Spice/Airlines.pm
+++ lib/DDG/Spice/Airlines.pm
@@ -9,27 +9,27 @@ spice from => '(.*?)/(.*)';
 triggers query_lc => qr/^(\d+)\s*(.*?)(?:[ ]air.*|)$/i;
 
 handle query_lc => sub {
-	#block words unless they're the first word and only if separated by space (excludes TAP-Air)
-	# grammar - apostrophes specifically: 'chuck's regional charter'
-	# air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
-	my %airlines = ();
-	open(IN, "</usr/local/ddg/sources/flightstats/airlines.txt");
-	while (my $line = <IN>) {
-	  chomp($line);
-	  my @line = split(/,/, $line);
+    #block words unless they're the first word and only if separated by space (excludes TAP-Air)
+    # grammar - apostrophes specifically: 'chuck's regional charter'
+    # air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
+    my %airlines = ();
+    open(IN, "</usr/local/ddg/sources/flightstats/airlines.txt");
+    while (my $line = <IN>) {
+      chomp($line);
+      my @line = split(/,/, $line);
 
-	  $line[1] =~ s/\s+air.*$//i;
-	  $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
-	  $airlines{lc $line[0]} = $line[0]; #AA => AA
-	}
-	close(IN);
+      $line[1] =~ s/\s+air.*$//i;
+      $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
+      $airlines{lc $line[0]} = $line[0]; #AA => AA
+    }
+    close(IN);
 
-	if(exists $airlines{$2}) {
-		my $airline = $airlines{$2};
-		my $flightno = $1;
-		return $airline, $flightno;
-	}
-	return;
+    if(exists $airlines{$2}) {
+        my $airline = $airlines{$2};
+        my $flightno = $1;
+        return $airline, $flightno;
+    }
+    return;
 };
 
 1;
--- lib/DDG/Spice/Airlines2.pm
+++ lib/DDG/Spice/Airlines2.pm
@@ -9,38 +9,38 @@ spice from => '(.*?)/(.*)';
 triggers query_lc => qr/^(.*?)(?:[ ]air.*?)?\s*(\d+)$/i;
 
 handle query_lc => sub {
-	warn $2;
+    warn $2;
 
-	#block words unless they're the first word and only if separated by space (excludes TAP-Air)
-	# grammar - apostrophes specifically: 'chuck's regional charter'
-	# air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
-	my %airlines = ();
-	open(IN, "</usr/local/ddg/sources/flightstats/airlines.txt");
-	while (my $line = <IN>) {
-	  chomp($line);
-	  my @line = split(/,/, $line);
+    #block words unless they're the first word and only if separated by space (excludes TAP-Air)
+    # grammar - apostrophes specifically: 'chuck's regional charter'
+    # air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
+    my %airlines = ();
+    open(IN, "</usr/local/ddg/sources/flightstats/airlines.txt");
+    while (my $line = <IN>) {
+      chomp($line);
+      my @line = split(/,/, $line);
 
-	  $line[1] =~ s/\s+air.*$//i;
-	  $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
-	  $airlines{lc $line[0]} = $line[0]; #AA => AA
-	}
-	close(IN);
+      $line[1] =~ s/\s+air.*$//i;
+      $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
+      $airlines{lc $line[0]} = $line[0]; #AA => AA
+    }
+    close(IN);
     
     my %elements = ();
     open (IN, "</usr/local/ddg/sources/elements/symbols.txt");
-	while (my $l = <IN>) {
-		warn $l;
-		chomp $l;	      
-      	$elements{lcfirst($l)} = undef;
-	}
+    while (my $l = <IN>) {
+        warn $l;
+        chomp $l;         
+        $elements{lcfirst($l)} = undef;
+    }
     close(IN);
 
-	if(exists $airlines{$1} && !exists $elements{substr($1, 0, 2)}) {
-		my $airline = $airlines{$1};
-		my $flightno = $2;
-		return $airline, $flightno;
-	}
-	return;
+    if(exists $airlines{$1} && !exists $elements{substr($1, 0, 2)}) {
+        my $airline = $airlines{$1};
+        my $flightno = $2;
+        return $airline, $flightno;
+    }
+    return;
 };
 
 1;
