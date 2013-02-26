0fecd22ba613e12b974949304ba569eeed865094
Jag Talon <talon.jag@gmail.com>
2013-02-26 13:14:49 -0500

    Added "Is it snowing?" plugin.

---
+++ lib/DDG/Spice/Snow.pm
@@ -0,0 +1,41 @@
+package DDG::Spice::Snow;
+
+use DDG::Spice;
+
+spice to => 'http://isitsnowingyet.org/api/check/$1/key/e95fad09aa5091b7734d1a268b53cef5';
+
+triggers query_lc => qr/snow(?:ing)?/i;
+
+my %snow = map { $_ => undef } (
+	'is it going to snow',
+	'going to snow',
+	'going to snow today',
+	'is it snowing',
+    'is it snowing here',
+    'is it snowing now',
+    'is it going to snow here',
+	'is it snowing today',
+	'is it going to snow today',
+	'going to snow today',
+);
+
+
+handle query_lc => sub {
+	my $query = $_;
+	my $location = join(" ", $loc->city . ', ', $loc->region_name . ', ', $loc->country_name);
+
+	if(exists $snow{$query}) {
+		return $location;
+	} elsif($query =~ /^(?:is[ ]it[ ])?
+						(?:going[ ]to )?
+						snow(?:ing)?[ ]?
+						(?:(?:here|now)[ ]?)?
+						(?:in[ ](.*?))?
+						(?:[ ]today)?\??$/ix) {
+		$location = $1 || $location;
+		return $location;
+	}
+	return;
+};
+
+1;
