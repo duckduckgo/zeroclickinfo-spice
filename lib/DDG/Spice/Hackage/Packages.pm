25b67a64071ae3ab9af5bb01f2dadf8d8603483b
Jag Talon <talon.jag@gmail.com>
2013-02-27 14:48:25 -0500

    Renamed Hackage to PackageDetails and Packages.

---
+++ lib/DDG/Spice/Hackage/Packages.pm
@@ -0,0 +1,25 @@
+package DDG::Spice::Hackage::Packages;
+# ABSTRACT: Search for information about Hackage packages
+
+use DDG::Spice;
+
+triggers any => "hackage", "haskell";
+spice to => 'http://typeful.net/~tbot/hackage/latest-package-versions.jsonp';
+
+primary_example_queries "hackage containers";
+description "Haskell packages";
+name "Hackage";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Packages.pm";
+topics "programming";
+category "programming";
+attribution github => ["https://github.com/nomeata", "Joachim Breitner"],
+            web => ["http://www.joachim-breitner.de", "Joachim Breitner"],
+            email => ['mail@joachim-breitner.de', "Joachim Breitner"];
+
+
+handle remainder => sub {
+    return $_ if defined $_;
+    return;   
+};
+
+1;
---
+++ share/spice/hackage/packages/spice.js
@@ -0,0 +1,78 @@
+function hackagePackageVersionsCallback(packages) {
+    "use strict";
+
+    // Remove the trigger word.
+    var query = DDG.get_query().replace(/(^|\s)(hackage|haskell)($|\s)/, '');
+    // Convert multiple spaces into a single one.
+    query = query.replace(/[ ]+/, ' ');
+    // Remove trailing / leading spaces.
+    query = query.replace(/^\s+|\s+$/, '');
+    // Convert to lowercase.
+    query = query.toLowerCase();
+
+    var hasOwn = ({}).hasOwnProperty;
+    var all_packages = {};
+    
+    // Key gets the lowercase, and the value gets the unmodified string. 
+    for(var normal_case in packages) {
+        if(hasOwn.call(packages, normal_case)) {
+            all_packages[normal_case.toLowerCase()] = normal_case;
+        }
+    }
+
+    // If the query exists in the dictionary, call Haskell::PackageDetails.
+    if(all_packages[query.toLowerCase()]) {
+        nrj("/js/spice/hackage/package_details/" + all_packages[query.toLowerCase()]);
+    }
+};
+
+function hackageDataCallback(hackage) {
+    "use strict";
+
+    var pkgName = hackage.packageDescription.package.pkgName,
+        synopsis = hackage.packageDescription.synopsis,
+        author = hackage.packageDescription.author,
+        homepage = hackage.packageDescription.homepage,
+        license = hackage.packageDescription.license,
+        library = hackage.condLibrary,
+        executable = hackage.condExecutables;
+
+        var type;
+        if (library && (executable.length > 0)) {
+            type = "library and program";
+        } else if (executable.length > 0) {
+            type = "program";
+        } else if (library) {
+            type = "library";
+        } else {
+            type = "something";
+        }    
+
+        var items = [[]];
+        var url = "http://hackage.haskell.org/package/" + pkgName;
+
+        items[0] = {
+            h: "Hackage (" + pkgName + ")",
+            s: "Hackage",
+            u: url,
+            force_big_header: true
+        };
+
+        if(synopsis.match(/\.$/)) {
+            synopsis += "<br>";
+        } else {
+            synopsis += ".<br>";
+        }
+        items[0].a = synopsis;
+        if (author) {
+            items[0].a += "Author: " + author;
+        }
+        if(homepage) {
+            items[0].a += " (<a href='" + homepage + "'>Homepage</a>)" + "<br>";
+        }
+        if (license) {
+            items[0].a += "License: " + license + " ";
+        }
+        nra(items, 1, 1);
+}
+
