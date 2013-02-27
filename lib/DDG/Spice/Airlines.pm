bdf868d9287735d8922629e0bb727b9ecf68c035
Jag Talon <talon.jag@gmail.com>
2013-02-27 15:53:16 -0500

    Moved the file processing outside the function.

--- lib/DDG/Spice/Airlines.pm
+++ lib/DDG/Spice/Airlines.pm
@@ -8,22 +8,22 @@ spice from => '(.*?)/(.*)';
 
 triggers query_lc => qr/^(\d+)\s*(.*?)(?:[ ]air.*|)$/i;
 
+#block words unless they're the first word and only if separated by space (excludes TAP-Air)
+# grammar - apostrophes specifically: 'chuck's regional charter'
+# air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
+my %airlines = ();
+my @airlines_lines = share('airlines.txt')->slurp;
+
+foreach my $line (@airlines_lines) {
+  chomp($line);
+  my @line = split(/,/, $line);
+
+  $line[1] =~ s/\s+air.*$//i;
+  $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
+  $airlines{lc $line[0]} = $line[0]; #AA => AA
+}
+
 handle query_lc => sub {
-    #block words unless they're the first word and only if separated by space (excludes TAP-Air)
-    # grammar - apostrophes specifically: 'chuck's regional charter'
-    # air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
-    my %airlines = ();
-    my @lines = share('airlines.txt')->slurp;
-
-    foreach my $line (@lines) {
-      chomp($line);
-      my @line = split(/,/, $line);
-
-      $line[1] =~ s/\s+air.*$//i;
-      $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
-      $airlines{lc $line[0]} = $line[0]; #AA => AA
-    }
-
     if(exists $airlines{$2}) {
         my $airline = $airlines{$2};
         my $flightno = $1;
--- lib/DDG/Spice/Airlines2.pm
+++ lib/DDG/Spice/Airlines2.pm
@@ -8,28 +8,28 @@ spice from => '(.*?)/(.*)';
 
 triggers query_lc => qr/^(.*?)(?:[ ]air.*?)?\s*(\d+)$/i;
 
+#block words unless they're the first word and only if separated by space (excludes TAP-Air)
+# grammar - apostrophes specifically: 'chuck's regional charter'
+# air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
+my %airlines = ();
+my @airlines_lines = share('airlines.txt')->slurp;
+
+foreach my $line (@airlines_lines) {
+  chomp($line);
+  my @line = split(/,/, $line);
+  $line[1] =~ s/\s+air.*$//i;
+  $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
+  $airlines{lc $line[0]} = $line[0]; #AA => AA
+}
+
+my %elements = ();
+my @elements_lines = share('symbols.txt')->slurp;
+foreach my $line (@elements_lines) {
+    chomp $line;         
+    $elements{lcfirst($line)} = undef;
+}
+
 handle query_lc => sub {
-    #block words unless they're the first word and only if separated by space (excludes TAP-Air)
-    # grammar - apostrophes specifically: 'chuck's regional charter'
-    # air, express, airlines, airways, aviation, regional, service, cargo, transport, aircraft, ventures, charter, international, world 
-    my %airlines = ();
-    my @lines = share('airlines.txt')->slurp;
-
-    foreach my $line (@lines) {
-      chomp($line);
-      my @line = split(/,/, $line);
-      $line[1] =~ s/\s+air.*$//i;
-      $airlines{lc $line[1]} = $line[0]; #American (Airlines <- regex removed) => AA
-      $airlines{lc $line[0]} = $line[0]; #AA => AA
-    }
-    
-    my %elements = ();
-    @lines = share('symbols.txt')->slurp;
-    foreach my $line (@lines) {
-        chomp $line;         
-        $elements{lcfirst($line)} = undef;
-    }
-
     if(exists $airlines{$1} && !exists $elements{substr($1, 0, 2)}) {
         my $airline = $airlines{$1};
         my $flightno = $2;
