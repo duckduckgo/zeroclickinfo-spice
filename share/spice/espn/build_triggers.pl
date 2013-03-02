7c0f5323e3d5cfa402ce6f9b28bf077ceb948212
Dylan Lloyd <dylan@dylansserver.com>
2013-03-02 15:09:28 -0500

    fetch and add team name/ids to ESPN trigger hash

--- share/spice/espn/build_triggers.pl
+++ share/spice/espn/build_triggers.pl
@@ -16,9 +16,18 @@ my $json;
 
 my $response = decode_json $json;
 
-my @athletes = $response->{sports}[0]{leagues}[0]{athletes};
-
-map {
-  my $name = lc $_->{displayName};
-  print "  '$name' => $_->{id},\n"
-} @{$athletes[0]};
+if (exists $response->{sports}[0]{leagues}[0]{athletes}) {
+  my @athletes = $response->{sports}[0]{leagues}[0]{athletes};
+  map {
+    my $name = lc $_->{displayName};
+    print "  '$name' => $_->{id},\n"
+  } @{$athletes[0]};
+} elsif (exists $response->{sports}[0]{leagues}[0]{teams}){
+  my @teams = $response->{sports}[0]{leagues}[0]{teams};
+  map {
+    my $location = lc $_->{location};
+    my $name = lc $_->{name};
+    print "  '$location $name' => $_->{id},\n";
+    print "  'the $location $name' => $_->{id},\n";
+  } @{$teams[0]};
+}
