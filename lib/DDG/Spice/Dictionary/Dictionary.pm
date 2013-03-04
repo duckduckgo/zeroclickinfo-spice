37ce20d8a9bf092bcef59cd6b33e9d585dac4558
Jag Talon <talon.jag@gmail.com>
2013-03-04 15:19:53 -0500

    Enabled caching on Dictionary and DictionaryPronunciation. (DictionaryAudio cannot be cached.)

--- lib/DDG/Spice/Dictionary/Dictionary.pm
+++ lib/DDG/Spice/Dictionary/Dictionary.pm
@@ -4,8 +4,7 @@ use DDG::Spice;
 
 spice to => 'https://api.wordnik.com/v4/word.json/$1/definitions?includeRelated=true&useCanonical=false&includeTags=false&limit=3&api_key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
 triggers any => "define";
-spice is_cached => 0;
-spice proxy_cache_valid => "418 1d";
+spice is_cached => 1;
 
 handle remainder => sub {
     return $_ if $_;
--- lib/DDG/Spice/Dictionary/DictionaryPronunciation.pm
+++ lib/DDG/Spice/Dictionary/DictionaryPronunciation.pm
@@ -4,8 +4,7 @@ use DDG::Spice;
 
 spice to => 'http://api.wordnik.com/v4/word.json/$1/pronunciations?limit=1&useCanonical=false&api_key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
 triggers any => "///***never_trigger***///";
-spice is_cached => 0;
-spice proxy_cache_valid => "418 1d";
+spice is_cached => 1;
 
 handle remainder => sub {
     return $_ if $_;
