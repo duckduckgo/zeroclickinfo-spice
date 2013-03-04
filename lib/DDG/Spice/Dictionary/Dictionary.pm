cea6e7b81ff2ef4aa425671dd104b9497e212cf9
Jag Talon <talon.jag@gmail.com>
2013-03-04 15:35:19 -0500

    Added more trigger words to the Dictionary plugin.

--- lib/DDG/Spice/Dictionary/Dictionary.pm
+++ lib/DDG/Spice/Dictionary/Dictionary.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Dictionary::Dictionary;
 use DDG::Spice;
 
 spice to => 'https://api.wordnik.com/v4/word.json/$1/definitions?includeRelated=true&useCanonical=false&includeTags=false&limit=3&api_key={{ENV{DDG_SPICE_RANDWORD_APIKEY}}}&callback={{callback}}';
-triggers any => "define", "define:";
+triggers any => "define", "define:", "definition", "definition of", "definition of:";
 spice is_cached => 1;
 
 handle remainder => sub {
