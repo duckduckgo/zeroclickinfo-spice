6d1e0941eb33532bab74cdc95d74c476ab8c3c0d
Dylan Lloyd <dylan@dylansserver.com>
2013-03-20 19:38:08 -0400

    fix header, query parsing and code formatting

--- share/spice/rhymes/xspice.js
+++ share/spice/rhymes/xspice.js
@@ -1,8 +1,8 @@
 function ddg_spice_rhymes (api_result) {
-    var query = decodeURIComponent(DDG.get_query())
+    var query = DDG.get_query()
                 .replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
 
-	for (var i = 0; i < response.length; i++) {
+    for (var i = 0; i < response.length; i++) {
         word = response.splice(Math.random()*response.length, 1)[0];
         if (word.score == 300)
 	        words.push(word.word);
@@ -11,13 +11,13 @@ function ddg_spice_rhymes (api_result) {
 
     if (words.length == 0) return;
 
-	Spice.render({
-		data             : words,
-		header1          : "Rhymes",
-		source_url       : 'http://rhymebrain.com/en/What_rhymes_with_'
-                            + encodeURIComponent(query)
-		source_name      : 'RhymeBrain',
-		template_normal  : 'rhymes',
-		force_big_header : true
-	});
+    Spice.render({
+        data             : words,
+        header1          : "(Rhymes)" + query,
+        source_url       : 'http://rhymebrain.com/en/What_rhymes_with_'
+                            + encodeURIComponent(query),
+        source_name      : 'RhymeBrain',
+        template_normal  : 'rhymes',
+        force_big_header : true
+    });
 }
