51673652fa412ecd7737362bec77abb00156af45
Dylan Lloyd <dylan@dylansserver.com>
2013-03-20 19:32:31 -0400

    start making it do what it's supposed to do

--- share/spice/rhymes/xspice.js
+++ share/spice/rhymes/xspice.js
@@ -1,10 +1,23 @@
 function ddg_spice_rhymes (api_result) {
+    var query = decodeURIComponent(DDG.get_query())
+                .replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
+
+	for (var i = 0; i < response.length; i++) {
+        word = response.splice(Math.random()*response.length, 1)[0];
+        if (word.score == 300)
+	        words.push(word.word);
+        if (words.length == 30) break;
+	}
+
+    if (words.length == 0) return;
+
 	Spice.render({
-		data: 				api_result,
-		header1 : 			"Spice2 Rhymes",
-		source_url : 		'http://dylansserver.com',
-		source_name : 		'Rhymes',
-		template_normal : 	'rhymes',
-		force_big_header : 	true
+		data             : words,
+		header1          : "Rhymes",
+		source_url       : 'http://rhymebrain.com/en/What_rhymes_with_'
+                            + encodeURIComponent(query)
+		source_name      : 'RhymeBrain',
+		template_normal  : 'rhymes',
+		force_big_header : true
 	});
 }
