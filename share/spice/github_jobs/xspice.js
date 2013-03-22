72512fa161b9aae885b7f3c54576e8a2f7652aec
Dylan Lloyd <dylan@dylansserver.com>
2013-03-22 19:08:06 -0400

    fixed more minor mistakes

--- share/spice/github_jobs/xspice.js
+++ share/spice/github_jobs/xspice.js
@@ -1,5 +1,5 @@
 function ddg_spice_github_jobs(response) {
-    if (response.length > 0) return;
+    if (response.length == 0) return;
     var jobs = response;
     var query = DDG.get_query();
     var re = /(?:\s*(?:i\s+|we\s+)?(?:need|want|deserve|seek|get)\s+(?:an?\s+)?)?(?:(.+)\s+)(?:jobs?|work|employment)(?:\s+(?:in\s+)?(.+))?/;
@@ -9,8 +9,8 @@ function ddg_spice_github_jobs(response) {
         data             : { 'job' : jobs },
         header1          : query + " (GitHub Jobs)",
         source_url       : 'https://jobs.github.com/positions?description=' 
-                            + encodeURIComponent(description)
-                            + "&location=" +  encodeURIComponent(location),
+                            + encodeURIComponent(jobs['description'])
+                            + "&location=" +  encodeURIComponent(jobs['location']),
         source_name      : 'GitHub',
         template_normal  : 'github_jobs',
         force_big_header : true
