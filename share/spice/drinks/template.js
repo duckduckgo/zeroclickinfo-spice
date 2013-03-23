7ffdb95de1a38888213c6f151c9b42e199c16a32
Dylan Lloyd <dylan@dylansserver.com>
2013-03-22 23:40:52 -0400

    don't include semi-colons inside italics (drinks)

--- share/spice/drinks/template.js
+++ share/spice/drinks/template.js
@@ -11,13 +11,13 @@ function program1(depth0,data) {
   return escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0));
   }
 
-  buffer += "<i>Ingredients:</i> ";
+  buffer += "<i>Ingredients</i>: ";
   options = {hash:{
     'sep': ("; ")
   },inverse:self.noop,fn:self.program(1, program1, data),data:data};
   stack2 = ((stack1 = helpers.concat),stack1 ? stack1.call(depth0, depth0.ingredients, options) : helperMissing.call(depth0, "concat", depth0.ingredients, options));
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "<br>\n<i>Directions:</i> ";
+  buffer += "<br>\n<i>Directions</i>: ";
   if (stack2 = helpers.procedure) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
   else { stack2 = depth0.procedure; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
   buffer += escapeExpression(stack2)
