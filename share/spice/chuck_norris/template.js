5f296a6b0a7e4b7f440f2eb3d9ba01a95040e665
Dylan Lloyd <dylan@dylansserver.com>
2013-03-23 02:13:52 -0400

    ported chuck_norris to spice2

---
+++ share/spice/chuck_norris/template.js
@@ -0,0 +1,15 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['chuck_norris'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  if (stack1 = helpers.chuck) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.chuck; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\n";
+  return buffer;
+  });
+})();
---
+++ share/spice/chuck_norris/xspice.js
@@ -0,0 +1,12 @@
+function ddg_spice_chuck_norris(response) {
+    if (response.type != 'success') return;
+    Spice.render({
+        data             : { 'chuck' : response.value.joke },
+        source_url       : 'http://www.icndb.com',
+        source_name      : 'Internet Chuck Norris Database',
+        image            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/ChuckNorris200611292256.jpg/48px-ChuckNorris200611292256.jpg',
+        template_normal  : 'chuck_norris',
+        force_no_icon    : true
+    });
+}
+
