526a38294221f35db68aa5e50195087b8d9af524
Dylan Lloyd <dylan@dylansserver.com>
2013-03-23 02:55:54 -0400

    ported metacpan to spice2

---
+++ share/spice/meta_cpan/template.js
@@ -0,0 +1,37 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['metacpan'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  <i>Description</i>: ";
+  if (stack1 = helpers.description) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.description; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\n";
+  return buffer;
+  }
+
+  buffer += "<i>Abstract</i>: ";
+  if (stack1 = helpers['abstract']) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0['abstract']; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "<br>\n<i>Author</i>:  ";
+  if (stack1 = helpers.author) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.author; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "<br>\n<i>Version</i>: ";
+  if (stack1 = helpers.version) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.version; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "<br>\n";
+  stack1 = helpers['if'].call(depth0, depth0.description, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  });
+})();
---
+++ share/spice/meta_cpan/xspice.js
@@ -0,0 +1,27 @@
+function ddg_spice_meta_cpan(response) {
+	if (response["message"]) return;
+    
+    var pkg = [];
+
+	pkg.query       = DDG.get_query().replace(/\s*(metacpan|meta cpan|cpan)\s*/i, '');
+	pkg.name        = response.documentation ? response.documentation : "No documentation specified.";
+	pkg.author      = response.author ? response.author : "No author specified.";
+	pkg.abstract    = response.abstract ? response.abstract : "No abstract specified.";
+	pkg.version     = response.version ? response.version : "No version given.";
+	pkg.description = response.description ? response.description : "No description specified.";
+
+    if (pkg.description.length > 340)
+        pkg.description = pkg.description.substring(0, 340)
+                        + '...';
+
+    Spice.render({
+        data             : pkg,
+        header1          : pkg.query + " (MetaCPAN)",
+        source_url       : 'https://metacpan.org/search?q='
+                           + encodeURIComponent(name),
+        source_name      : 'MetaCPAN',
+        template_normal  : 'metacpan',
+        force_big_header : true,
+        force_no_fold    : true,
+    });
+}
