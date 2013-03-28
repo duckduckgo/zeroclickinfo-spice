f5bb957df40049763e1333e5f97aa117a1a6ed63
Jag Talon <talon.jag@gmail.com>
2013-03-28 13:33:46 -0400

    Added a new template for Forvo.
    - It's called forvo-fallback for now, but it's there because sometimes api_result.items would be an array, and sometimes api_result.items would be a hash. The built-in function "each" cannot iterate through the hash, so we make a new template to handle them.

--- share/spice/forvo/xspice.js
+++ share/spice/forvo/xspice.js
@@ -1,27 +1,45 @@
 var ddg_spice_forvo = function(api_result) {
-    var initializePlayer = function() {
-        // Display spice.
+    var isArray = function(obj) {
+        return toString.call(obj) === "[object Array]";
+    };
+
+    var render = function(template) {
         Spice.render({
             data             : api_result,
             force_big_header : true,
             header1          : "Pronunciations by Forvo",
             source_name      : "Forvo",
             source_url       : "http://www.forvo.com/search/",
-            template_normal  : "forvo"
+            template_normal  : template
         });
+    }
 
+    // Display spice.
+    if(isArray(api_result.items) && +api_result.attributes.total >= 3) {
+        render("forvo");
+    } else if(+api_result.attributes.total >= 3) {
+        render("forvo-fallback");
+    }
+
+    var initializePlayer = function() {
         // Initialize MediaElement.js
         $(document).ready(function() {
             $('audio').mediaelementplayer({
-                audioWidth: "51%",
-                audioHeight:30,
-                enableAutosize: true,
-                features: ['playpause','progress','current']
+                audioWidth      : "51%",
+                audioHeight     : 30,
+                enableAutosize  : true,
+                features        : ['playpause','progress','current']
             });
         });
     };
 
-    DDG.require("/forvo/mediaelement-and-player.min.js", {
-        success: initializePlayer
-    });
+    $.getScript("/forvo/mediaelement-and-player.min.js", initializePlayer);
 }
+
+Handlebars.registerHelper('list', function(items, options) {
+    var out = "";
+    for(var i = 0; i < items.length && i < 3; i += 1) {
+        out += options.fn(items[i]);
+    }
+    return out;
+});
