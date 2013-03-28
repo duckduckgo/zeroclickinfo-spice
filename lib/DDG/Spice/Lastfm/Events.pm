e0b0c9570b3745efcb95bc9f0c0099d81c2e7c49
Francisco Gomez <mail@frncscgmz.me>
2013-03-27 23:57:02 -0700

    Added references and removed some comments on Lastfm Events

--- lib/DDG/Spice/Lastfm/Events.pm
+++ lib/DDG/Spice/Lastfm/Events.pm
@@ -1,4 +1,5 @@
 package DDG::Spice::Lastfm::Events;
+# ABSTRACT: Search for music shows near the user location.
 
 use DDG::Spice;
 
@@ -8,11 +9,18 @@ spice from => '(\-?\d+\.\d+?)\/(\-?\d+\.\d+?)';
 
 triggers query_lc => qr/^(?:concerts?|music shows?|shows?|gigs)\s+(?:near?|near me)/;
 
-handle query_lc => sub {
-   #my $lat = $loc->latitude;
-   #my $lon = $loc->longitude;
-   #return ($lat, $lon);
+primary_example_queries "concerts near";
+secondary_example_queries "shows near", "music shows near me", "gigs near";
+description "Music show information";
+name "LastfmEvents";
+icon_url "/i/www.last.fm.ico";
+source "Last.fm";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Lastfm/Events.pm";
+topics "entertainment", "music";
+category "entertainment";
+attribution github => ['https://github.com/frncscgmz','frncscgmz'];
 
+handle query_lc => sub {
    return ($loc->latitude,$loc->longitude);
 };
 
