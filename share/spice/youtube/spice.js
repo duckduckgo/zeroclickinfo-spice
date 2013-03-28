3869efee28f86209ba9bb692d9f70c63f1b3bc8c
Jag Talon <talon.jag@gmail.com>
2013-03-28 09:40:45 -0400

    Use DDG.load instead of DDG.require on the YouTube plugin.
    - DDG.require was renamed to DDG.load.

--- share/spice/youtube/spice.js
+++ share/spice/youtube/spice.js
@@ -1,6 +1,6 @@
 function ddg_spice_youtube(api_result) {
     "use strict";
-    DDG.require("/js/nryt.js", {
+    DDG.load("/js/nryt.js", {
         success: function() {
             window.iqyt = 2;
             ddgyt.nryt(api_result);
