2db75a4272496341b4fc6a528bb5fcd47ac93289
Jag Talon <talon.jag@gmail.com>
2013-03-29 09:22:21 -0400

    Added comments to the new Forvo plugin.
    - I also removed spice.js since we already have the new one.

--- share/spice/forvo/xspice.js
+++ share/spice/forvo/xspice.js
@@ -1,28 +1,12 @@
 var ddg_spice_forvo = function(api_result) {
+
+    // From underscore.js. This function checks if obj is an array.
     var isArray = function(obj) {
         return toString.call(obj) === "[object Array]";
     };
 
-    var render = function(template) {
-        Spice.render({
-            data             : api_result,
-            force_big_header : true,
-            header1          : "Pronunciations by Forvo",
-            source_name      : "Forvo",
-            source_url       : "http://www.forvo.com/search/",
-            template_normal  : template
-        });
-    }
-
-    // Display spice.
-    if(isArray(api_result.items) && +api_result.attributes.total >= 3) {
-        render("forvo");
-    } else if(+api_result.attributes.total >= 3) {
-        render("forvo-fallback");
-    }
-
+    // This function initializes the audio player.
     var initializePlayer = function() {
-        // Initialize MediaElement.js
         $(document).ready(function() {
             $('audio').mediaelementplayer({
                 audioWidth      : "51%",
@@ -33,9 +17,29 @@ var ddg_spice_forvo = function(api_result) {
         });
     };
 
-    $.getScript("/forvo/mediaelement-and-player.min.js", initializePlayer);
+    // Display the data and load MediaElement.js.
+    var render = function(template) {
+        Spice.render({
+            data             : api_result,
+            force_big_header : true,
+            header1          : "Pronunciations by Forvo",
+            source_name      : "Forvo",
+            source_url       : "http://www.forvo.com/",
+            template_normal  : template
+        });
+
+        $.getScript("/forvo/mediaelement-and-player.min.js", initializePlayer);
+    }
+
+    // Check if api_result.items is an array or not.
+    if(isArray(api_result.items) && +api_result.attributes.total >= 3) {
+        render("forvo");
+    } else if(+api_result.attributes.total >= 3) {
+        render("forvo-fallback");
+    }
 }
 
+// Make sure we display only three items.
 Handlebars.registerHelper('list', function(items, options) {
     var out = "";
     for(var i = 0; i < items.length && i < 3; i += 1) {
