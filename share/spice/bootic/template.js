66334d42b179480a6031b21a8e13273d09cd83c3
moollaza <moollaza@gmail.com>
2013-03-29 15:52:23 -0400

    small updates to bootic, renamed spice.js to xspice.js

---
+++ share/spice/bootic/template.js
@@ -0,0 +1,29 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['bootic_items'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, stack2, options, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing;
+
+
+  buffer += "<li class=\"ddgc_item\"> "
+    + "\n    <a class=\"ddgc_detail_link\" href=\"http://bootic.com";
+  if (stack1 = helpers.url) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.url; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\" obj_index=\""
+    + escapeExpression(((stack1 = data.index),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">\n        <p><img src=\"";
+  if (stack2 = helpers.picture_url) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
+  else { stack2 = depth0.picture_url; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
+  buffer += escapeExpression(stack2)
+    + "\"></p>\n        <span>";
+  options = {hash:{
+    'maxlen': ("25")
+  },data:data};
+  stack2 = ((stack1 = helpers.condense),stack1 ? stack1.call(depth0, depth0.name, options) : helperMissing.call(depth0, "condense", depth0.name, options));
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+  buffer += "</span>\n    </a>\n</li>";
+  return buffer;
+  });
+})();
---
+++ share/spice/bootic/xspice.js
@@ -0,0 +1,45 @@
+function ddg_spice_bootic ( api_result ) {
+
+	// check for response
+	if ( !api_result.products ) return;
+
+	var query = (api_result.input_query ) ?
+		'?initial=1&q=' + encodeURIComponent( api_result.input_query ) :
+		'';
+
+	var array = [];
+	
+	$.each(api_result.products, function(key, value){
+		array.push(this);
+	});
+
+	api_result.products = array;
+
+	console.log("ARRAY IS: ", array);
+
+	Spice.render({
+		data: api_result,
+		source_name : 'Bootic',
+		source_url : 'http://www.bootic.com/?q=' + query,
+		header1 : api_result.input_query + ' ( Bootic )' ,
+		
+		template_frame: "carousel",
+		template_normal: "bootic_items",
+		carousel_css_id: "bootic",
+		carousel_items: api_result.products,
+		
+		force_no_fold : 1,
+		force_big_header : 1,
+	});
+}
+
+/*******************************
+  Handlebars helpers
+  *******************************/
+
+// forms the url for a bootic product image
+Handlebars.registerHelper ('picture_url', function() {
+	var pic_base_uri = 'http://static.bootic.com/_pictures/';
+	var pic_id = this.pictures[0];
+	return pic_base_uri + pic_id;
+});
