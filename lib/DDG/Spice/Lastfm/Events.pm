977fba0befbc60ff319a83522e8202bb75ec63cd
Jag Talon <talon.jag@gmail.com>
2013-03-31 07:59:11 -0400

    Changed the regexp of the Events plugin.
    The trigger words "near" and "near me" shouldn't be required.

--- lib/DDG/Spice/Lastfm/Events.pm
+++ lib/DDG/Spice/Lastfm/Events.pm
@@ -5,9 +5,9 @@ use DDG::Spice;
 
 spice to => 'http://ws.audioscrobbler.com/2.0/?format=json&method=geo.getevents&location=$1&api_key={{ENV{DDG_SPICE_LASTFM_APIKEY}}}&callback={{callback}}';
 
-triggers query_lc => qr/^(?:concerts?|music shows?|shows?|gigs)\s+(?:near?|near me)/;
+triggers query_lc => qr/^(?:concerts?|music shows?|shows?|gigs)(\s+(near|near me))?/;
 
-primary_example_queries "concerts near";
+primary_example_queries "concerts";
 secondary_example_queries "shows near", "music shows near me", "gigs near";
 description "Music show information";
 name "LastfmEvents";
