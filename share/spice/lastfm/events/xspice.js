8b42fc41eb45fe7698020265b2b6ae649b195e3d
Jag Talon <talon.jag@gmail.com>
2013-03-31 07:52:28 -0400

    Added additional comments to the Lastfm::Events plugin.

--- share/spice/lastfm/events/xspice.js
+++ share/spice/lastfm/events/xspice.js
@@ -1,8 +1,10 @@
-function ddg_spice_lastfm_events(api_result) {
+var ddg_spice_lastfm_events = function(api_result) {
+	// Check if we encountered an error.
 	if(api_result.error) {
 		return;
 	}
 
+	// Display the spice plugin.
 	Spice.render({
 	    data              : api_result,
 	    force_big_header  : true,
@@ -14,10 +16,12 @@ function ddg_spice_lastfm_events(api_result) {
 	});
 }
 
+// Shortens the date.
 Handlebars.registerHelper('date', function(text) {
     return text.substr(0, text.lastIndexOf(" "));
 });
 
+// Limits the number of displayed items to five.
 Handlebars.registerHelper('list', function(items, options) {
 	var out = "";
 	for(var i = 0; i < items.length && i < 5; i += 1) {
