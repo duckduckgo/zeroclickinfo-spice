fedfad369b01bc7e1fa256b72dbcca2f162fec6e
Jag Talon <talon.jag@gmail.com>
2013-04-01 14:17:01 -0400

    Linted the file and added comments.

--- share/spice/lastfm/events/xspice.js
+++ share/spice/lastfm/events/xspice.js
@@ -1,4 +1,6 @@
 var ddg_spice_lastfm_events = function(api_result) {
+    "use strict";
+
     // Check if we encountered an error.
     if(api_result.error) {
         return;
@@ -14,52 +16,60 @@ var ddg_spice_lastfm_events = function(api_result) {
         template_normal   : "events",
         template_small    : "events"
     });
-}
+};
 
 // Shortens the date.
-Handlebars.registerHelper('date', function(text) {
+Handlebars.registerHelper("date", function(text) {
+    "use strict";
+
     return text.substr(0, text.lastIndexOf(" "));
 });
 
 // Limits the number of displayed items to five.
-Handlebars.registerHelper('list', function(items, options) {
+Handlebars.registerHelper("list", function(items, options) {
+    "use strict";
+
     var out = "";
     for(var i = 0; i < items.length && i < 5; i += 1) {
-        out += options.fn(items[i])
+        out += options.fn(items[i]);
     }
     return out;
 });
 
 // Calculate the distance.
-Handlebars.registerHelper('distance', function(longitude, latitude, location) {
+Handlebars.registerHelper("distance", function(longitude, latitude, location) {
+    "use strict";
+
+    // Convert degrees to radians.
     var toRad = function(degrees) {
-        return degrees * Math.PI / 180
+        return degrees * Math.PI / 180;
     };
 
+    // The coordinates of the venue.
     var pointOne = {
         latitude: toRad(latitude),
         longitude: toRad(longitude)
     };
-    
-    var location = location.match(/\((.+),(.+)\)/);
-    
+
+    location = location.match(/\((.+),(.+)\)/);
+
+    // The coordinates of the user.
     var pointTwo = {
         latitude: toRad(location[2]),
         longitude: toRad(location[1])
     };
 
     // Radius of the Earth (from Wolfram Alpha).
-    var radius = "6367.5"; 
+    var radius = "6367.5";
 
     // Compute the Haversin function (from Wikipedia).
     var haversin = function(distance) {
         return Math.pow(Math.sin(distance) / 2, 2);
     };
 
-    var square = Math.sqrt(
-                            haversin(pointTwo.latitude - pointOne.latitude) + 
-                            Math.cos(pointOne.latitude) * Math.cos(pointTwo.latitude) * 
-                            haversin(pointTwo.longitude - pointOne.longitude));
+    var square = Math.sqrt(haversin(pointTwo.latitude - pointOne.latitude) +
+                           Math.cos(pointOne.latitude) * Math.cos(pointTwo.latitude) *
+                           haversin(pointTwo.longitude - pointOne.longitude));
 
     var distance = 2 * radius * Math.asin(square);
 
