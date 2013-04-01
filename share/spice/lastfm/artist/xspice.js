086233bfbc153a2484c36f77bc348b31ea84c0bd
Jag Talon <talon.jag@gmail.com>
2013-04-01 16:34:52 -0400

    Artist: We now have a coordinating conjunction in the list.
    The list that I am talking about is the similar artists list. The plugin will now show "Similar to: Beyoncé, Mary J. Blige, Mariah Carey, Brandy, and Destiny's Child" for Alicia Keys. I also added a bunch of comments for people to read.

--- share/spice/lastfm/artist/xspice.js
+++ share/spice/lastfm/artist/xspice.js
@@ -1,8 +1,12 @@
-function ddg_spice_lastfm_artist_all(api_result) {
+var ddg_spice_lastfm_artist_all = function(api_result) {
+    "use strict";
+
+    // Don't do anything if we find an error.
     if(api_result.error) {
         return;
     }
 
+    // Display the plugin.
     Spice.render({
         data             : api_result,
         force_big_header : true,
@@ -12,6 +16,7 @@ function ddg_spice_lastfm_artist_all(api_result) {
         template_normal  : "artist"
     });
 
+    // This minimizes and expands the text.
     $(document).ready(function() {
         $("#expand").click(function() {
             DDG.toggle('all', 1);
@@ -21,12 +26,16 @@ function ddg_spice_lastfm_artist_all(api_result) {
     });
 }
 
-
+// This helper removes the HTML tags, and it also shortens the text.
 Handlebars.registerHelper('snippet', function(text, method) {
+    "use strict";
+
+    // Remove the tags (thanks to Underscore.string).
     var stripTags = function(text) {
         return String(text).replace(/<\/?[^>]+>/g, '');
     };
 
+    // This shortens the text.
     if(method === "some") {
         return stripTags(text).slice(0, 200);
     } else {
@@ -34,16 +43,20 @@ Handlebars.registerHelper('snippet', function(text, method) {
     }
 });
 
-
+// This helper adds a comma between a list of links.
 Handlebars.registerHelper('list', function(items, options) {
-    var out = "";
+    "use strict";
+
+    var similar_artists = [];
 
     for(var i = 0; i < items.length; i += 1) {
-        out += options.fn(items[i]);
-        if(i !== items.length - 1) {
-            out += ", ";
-        }
+        similar_artists.push(options.fn(items[i]));
     }
 
-    return out;
+    // Only get the first n - 1 items because we want
+    // to add a coordinating conjunction before the last item.
+    var first = similar_artists.slice(0, similar_artists.length - 1);
+    first = first.join(", ");
+
+    return first + ", and " + similar_artists[similar_artists.length - 1] + ".";
 });
