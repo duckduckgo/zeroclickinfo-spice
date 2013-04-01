0fc6494e368f569f807f15b84ef4adf8db5b0bcc
Jag Talon <talon.jag@gmail.com>
2013-04-01 16:49:00 -0400

    Artist: Added a new template called similar.handlebars
    It only shows similar artists. Now we need to add error checking, too, just in case some of these things are missing.

--- share/spice/lastfm/artist/xspice.js
+++ share/spice/lastfm/artist/xspice.js
@@ -19,20 +19,20 @@ var ddg_spice_lastfm_artist_all = function(api_result) {
     // This minimizes and expands the text.
     $(document).ready(function() {
         $("#expand").click(function() {
-            DDG.toggle('all', 1);
-            DDG.toggle('some', -1); 
-            DDG.toggle('expand', -1);
-        })
+            DDG.toggle("all", 1);
+            DDG.toggle("some", -1);
+            DDG.toggle("expand", -1);
+        });
     });
-}
+};
 
 // This helper removes the HTML tags, and it also shortens the text.
-Handlebars.registerHelper('snippet', function(text, method) {
+Handlebars.registerHelper("snippet", function(text, method) {
     "use strict";
 
     // Remove the tags (thanks to Underscore.string).
     var stripTags = function(text) {
-        return String(text).replace(/<\/?[^>]+>/g, '');
+        return String(text).replace(/<\/?[^>]+>/g, "");
     };
 
     // This shortens the text.
@@ -44,7 +44,7 @@ Handlebars.registerHelper('snippet', function(text, method) {
 });
 
 // This helper adds a comma between a list of links.
-Handlebars.registerHelper('list', function(items, options) {
+Handlebars.registerHelper("list", function(items, options) {
     "use strict";
 
     var similar_artists = [];
@@ -60,3 +60,24 @@ Handlebars.registerHelper('list', function(items, options) {
 
     return first + ", and " + similar_artists[similar_artists.length - 1] + ".";
 });
+
+// This function calls similar.handlebars, and it only shows
+// the similar artists.
+var ddg_spice_lastfm_artist_similar = function(api_result) {
+    "use strict";
+
+    // Don't do anything if we find an error.
+    if(api_result.error) {
+        return;
+    }
+
+    // Display the plugin.
+    Spice.render({
+        data             : api_result,
+        force_big_header : true,
+        header1          : api_result.artist.name,
+        source_name      : "Last.fm",
+        source_url       : api_result.artist.url,
+        template_normal  : "similar"
+    });
+};
