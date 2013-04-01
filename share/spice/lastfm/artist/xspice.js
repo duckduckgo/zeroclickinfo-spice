a46bfe4d253a681ce3e7129760490dda95c4467f
Jag Talon <talon.jag@gmail.com>
2013-04-01 17:10:39 -0400

    Artist: Code for Spice.render is no longer duplicated.
    ddg_spice_lastfm_artist_all and ddg_spice_lastfm_artist_similar used to have similar functions. These functions are now moved to ddg_spice_lastfm to remove the redundant code.

--- share/spice/lastfm/artist/xspice.js
+++ share/spice/lastfm/artist/xspice.js
@@ -1,8 +1,33 @@
-var ddg_spice_lastfm_artist_all = function(api_result) {
+// This function checks for errors, and it's also the one that calls Spice.render.
+var ddg_spice_lastfm = function(api_result, template) {
     "use strict";
 
-    // Don't do anything if we find an error.
-    if(api_result.error) {
+    // These words should be skipped in the isRelevant function.
+    var skipArray = {
+        "similar": 1,
+        "band": 1,
+        "bands": 1,
+        "musician": 1,
+        "musicians": 1,
+        "player": 1,
+        "artist": 1,
+        "artists": 1,
+        "performer": 1,
+        "performers": 1,
+        "singer": 1,
+        "singers": 1,
+        "rapper": 1,
+        "dj": 1,
+        "rappers": 1,
+        "vocalist": 1,
+        "vocalists": 1,
+        "djs": 1,
+        "songster": 1,
+        "songsters": 1
+    };
+
+    // Don't do anything if we find an error, or if the result is irrelevant.
+    if(api_result.error && !DDG.isRelevant(api_result.artist.name, skip)) {
         return;
     }
 
@@ -13,10 +38,20 @@ var ddg_spice_lastfm_artist_all = function(api_result) {
         header1          : api_result.artist.name,
         source_name      : "Last.fm",
         source_url       : api_result.artist.url,
-        template_normal  : "artist"
+        template_normal  : template
     });
+};
+
+// This function calls artist.handlebars, and it shows everything about the artist.
+var ddg_spice_lastfm_artist_all = function(api_result) {
+    "use strict";
+    
+    // Display the plugin.   
+    ddg_spice_lastfm(api_result, "artist");
 
     // This minimizes and expands the text.
+    // This piece of code is only relevant to this function, and it's not needed
+    // when displaying similar artists.
     $(document).ready(function() {
         $("#expand").click(function() {
             DDG.toggle("all", 1);
@@ -24,6 +59,13 @@ var ddg_spice_lastfm_artist_all = function(api_result) {
             DDG.toggle("expand", -1);
         });
     });
+};
+
+// This function calls similar.handlebars, and it only shows the similar artists.
+var ddg_spice_lastfm_artist_similar = function(api_result) {
+    "use strict";
+
+    ddg_spice_lastfm(api_result, "similar");
 };
 
 // This helper removes the HTML tags, and it also shortens the text.
@@ -60,24 +102,3 @@ Handlebars.registerHelper("list", function(items, options) {
 
     return first + ", and " + similar_artists[similar_artists.length - 1] + ".";
 });
-
-// This function calls similar.handlebars, and it only shows
-// the similar artists.
-var ddg_spice_lastfm_artist_similar = function(api_result) {
-    "use strict";
-
-    // Don't do anything if we find an error.
-    if(api_result.error) {
-        return;
-    }
-
-    // Display the plugin.
-    Spice.render({
-        data             : api_result,
-        force_big_header : true,
-        header1          : api_result.artist.name,
-        source_name      : "Last.fm",
-        source_url       : api_result.artist.url,
-        template_normal  : "similar"
-    });
-};
