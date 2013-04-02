9ba9d2dde283da9bb57b5a406132c08a2836a3a0
Jag Talon <talon.jag@gmail.com>
2013-04-02 17:23:24 -0400

    Added videos to the trigger words.
    I also removed that embarrassing "yadayada" trigger.

--- lib/DDG/Spice/Youtube.pm
+++ lib/DDG/Spice/Youtube.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Youtube;
 use DDG::Spice;
 
 spice to => 'http://gdata.youtube.com/feeds/videos?max-results=5&start-index=1&alt=json-in-script&callback={{callback}}&vq=$1';
-triggers startend => "video", "yadayada";
+triggers startend => "video", "videos";
 
 handle remainder => sub {
     return $_ if $_;
