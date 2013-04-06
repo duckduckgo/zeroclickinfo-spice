3629e498981bc30605cd931948ee50ff5cac66a5
Jag Talon <talon.jag@gmail.com>
2013-04-06 19:44:11 -0400

    Bitly: Changed the "autofocus" to something better.
    The "autofocus" attribute is only HTML5, so we should trigger the "focus" and "select" events on the element instead. I also added bootstrap.js (only the Tooltip plugin).

--- share/spice/bitly/template.js
+++ share/spice/bitly/template.js
@@ -6,7 +6,7 @@ helpers = helpers || Handlebars.helpers; data = data || {};
   var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += "Here is a shortened version of that URL:\n<input type=\"text\" autofocus value=\"";
+  buffer += "Here is a shortened version of that URL:\n<input type=\"text\" id=\"bitly-url\" readonly=\"readonly\" value=\"";
   if (stack1 = helpers.url) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.url; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
--- share/spice/bitly/xspice.js
+++ share/spice/bitly/xspice.js
@@ -1,6 +1,7 @@
 function ddg_spice_bitly(response) {
-	if (!response || !response.status_code === 200
-            || !response.data || !response.data.url) return;
+	if (!response || !response.status_code === 200 || !response.data || !response.data.url) {
+        return;
+    }
 
     Spice.render({
         data             : { 'url' : response.data.url },
@@ -10,5 +11,20 @@ function ddg_spice_bitly(response) {
         source_name      : 'Bitly',
         template_normal  : 'bitly',
         force_big_header : true
+    });
+
+    $(document).ready(function() {
+        var url = $("#bitly-url");
+
+        // Load bootstrap.js. This shows the a tooltip that can guide the user.
+        $.getScript("/bootstrap/bootstrap.js", function() {
+            url.tooltip({
+                placement: "right",
+                trigger: "hover focus",
+                title: "Copy"
+            });
+            url.focus();
+            url.select();
+        });
     });
 }
