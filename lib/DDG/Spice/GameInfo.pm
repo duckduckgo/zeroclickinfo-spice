f0c5189a3162ab30c842c9d78d88d1b473775991
thomaspreece10 <thomaspreece10@gmail.com>
2013-04-07 09:56:45 -0400

    Fixed descriptions and spice URL

--- lib/DDG/Spice/GameInfo.pm
+++ lib/DDG/Spice/GameInfo.pm
@@ -2,14 +2,14 @@ package DDG::Spice::GameInfo;
 
 use DDG::Spice;
 triggers startend => "game";
-spice to => 'http://thefreegamesdb.com/API/DuckDuckGo2.php?Name=$1';
+spice to => 'http://thefreegamesdb.com/API/DuckDuckGo.php?Name=$1';
 spice wrap_jsonp_callback => 1;
 
 primary_example_queries "homesick game";
-description "See information about a free pc game";
+description "See information about a game";
 name "GameInfo";
 icon_url "/i/thefreegamesdb.com/favicon.ico";
-source "thefreegamesdb";
+source "thefreegamesdb.com & thegamesdb.net";
 topics "gaming";
 category "reference";
 attribution github => ['https://github.com/thomaspreece10','thomaspreece10'];
