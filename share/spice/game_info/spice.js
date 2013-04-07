952c918dacc3dbeb343cef450bfb04a1936cde9c
thomaspreece10 <thomaspreece10@gmail.com>
2013-04-07 12:10:08 -0400

    Changed header and content character cutoff limit

--- share/spice/game_info/spice.js
+++ share/spice/game_info/spice.js
@@ -2,18 +2,19 @@ function ddg_spice_game_info(ir) {
     var snippet;
     if (ir['status'] == 'OK') {
 
-	var about = ir['about'].substring(0, 199);
-        if (ir['about'].length > 200) {
+	var about = ir['about'].substring(0, 249);
+        if (ir['about'].length > 250) {
             about += '...';
         }
 
 	items = [[]];
 	items[0] = {
             a: about,
-            h: ir['name'],
+            h: ir['name'] + ' (Games)',
             s: ir['domain'],
             u: ir['link'],
-            i: ir['image']
+            i: ir['image'],
+	    force_big_header: 1
         };
         nra(items);
 
