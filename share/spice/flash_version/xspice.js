960071fd6fb5ec0a714a8b2a4f0809f18119d874
Jag Talon <talon.jag@gmail.com>
2013-04-08 14:09:23 -0400

    Flash Plugin: Added comments and a message if Flash is not available.

--- share/spice/flash_version/xspice.js
+++ share/spice/flash_version/xspice.js
@@ -1,16 +1,22 @@
+// TODO: Probably better if we display all the plugins similar to https://www.mozilla.org/en-US/plugincheck/.
+// We can get that from window.navigator.plugins.
 var ddg_spice_flash_version = function() {
-	if(!YAHOO.util.FlashDetect.installed) {
-		return;
-	} 
+    var context = {version: "You're using " + YAHOO.util.FlashDetect.raw};
 
-	Spice.render({
-	    data             : {version: YAHOO.util.FlashDetect.raw},
-	    header1          : 'Flash Version',
-	    source_name      : 'Adobe',
-	    source_url	     : 'https://get.adobe.com/flashplayer/',
-	    template_normal  : "flash",
-	    force_big_header : true
-	});	
+    // Check if Flash was detected.
+    if(!YAHOO.util.FlashDetect.installed) {
+        context.version = "You don't have Flash installed"
+    } 
+
+    // Display the plugin.
+    Spice.render({
+        data             : context,
+        header1          : 'Flash Version',
+        source_name      : 'Adobe',
+        source_url       : 'https://get.adobe.com/flashplayer/',
+        template_normal  : 'flash',
+        force_big_header : true
+    }); 
 };
 
 // Manually call the function.
