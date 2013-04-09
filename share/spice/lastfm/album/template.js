dd1a38be74d9c683343bd8532b94e78e81ab850b
Jag Talon <talon.jag@gmail.com>
2013-04-08 20:41:33 -0400

    Album: "Album" should be "Artist."

--- share/spice/lastfm/album/template.js
+++ share/spice/lastfm/album/template.js
@@ -9,8 +9,8 @@ function program1(depth0,data) {
   
   var buffer = "", stack1, stack2, options;
   buffer += "\n        <div>\n            <em>Artist:</em>\n            ";
-  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  if (stack1 = helpers.artist) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.artist; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
     + "\n        </div>\n        ";
   options = {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data};
