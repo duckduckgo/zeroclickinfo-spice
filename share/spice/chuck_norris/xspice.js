22c6f244eb494b67c808e6a0d87fe4202c81dfee
moollaza <moollaza@gmail.com>
2013-04-11 14:28:31 -0400

    small fix to Chuck Norris spice

--- share/spice/chuck_norris/xspice.js
+++ share/spice/chuck_norris/xspice.js
@@ -1,8 +1,8 @@
-function ddg_spice_chuck_norris(response) {
-    if (response.type != 'success') return;
+function ddg_spice_chuck_norris(api_result) {
+    if (api_result.type !== 'success') return;
 
     Spice.render({
-        data             : { 'chuck' : response.value.joke },
+        data             : api_result.value,
         source_url       : 'http://www.icndb.com',
         source_name      : 'Internet Chuck Norris Database',
         image            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/ChuckNorris200611292256.jpg/48px-ChuckNorris200611292256.jpg',
