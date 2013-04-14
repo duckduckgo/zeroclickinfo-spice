a5f7a0579a5f3f0c6c44e74f7346e7a32efb99d7
Dylan Lloyd <dylan@dylansserver.com>
2013-04-13 23:18:55 -0400

    separated player/team functions out w/ parse wrapper

--- share/spice/espn/fetch_ids.pl
+++ share/spice/espn/fetch_ids.pl
@@ -4,30 +4,49 @@ use strict;
 use warnings;
 
 use JSON;
+use LWP::Simple;
 use Data::Dumper;
 
-my $json;
-{
-  local $/;
-  open my $fh, '<', $ARGV[0];
-  $json = <$fh>;
-  close $fh;
+sub parse_players {
+    my $json = shift;
+    my @athletes = $json->{sports}[0]{leagues}[0]{athletes};
+    map { print lc "  '$_->{displayName}' => $_->{id},\n" } @{$athletes[0]};
+    return $json;
 }
 
-my $response = decode_json $json;
-
-if (exists $response->{sports}[0]{leagues}[0]{athletes}) {
-  my @athletes = $response->{sports}[0]{leagues}[0]{athletes};
-  map {
-    my $name = lc $_->{displayName};
-    print "  '$name' => $_->{id},\n"
-  } @{$athletes[0]};
-} elsif (exists $response->{sports}[0]{leagues}[0]{teams}){
-  my @teams = $response->{sports}[0]{leagues}[0]{teams};
-  map {
-    my $location = lc $_->{location};
-    my $name = lc $_->{name};
-    print "  '$location $name' => $_->{id},\n";
-    print "  'the $location $name' => $_->{id},\n";
-  } @{$teams[0]};
+sub parse_teams {
+    my $json = shift;
+    if (exists $json->{sports}[0]{leagues}[0]{teams}){
+      my @teams = $json->{sports}[0]{leagues}[0]{teams};
+      map {
+        print lc "  '$_->{location} $_->{name}' => $_->{id},\n";
+        print lc "  'the $_->{location} $_->{name}' => $_->{id},\n";
+      } @{$teams[0]};
+    }
+    return $json;
 }
+
+sub parse {
+    my ($resource, $sub) = @_;
+    my %subs = (
+        'players' => \&parse_players,
+        'teams'   => \&parse_players,
+    );
+    #print Dumper($subs{$sub}($resource));
+
+    my $json = $subs{$sub}(decode_json get $resource);
+
+    my $resultsCount  = $json->{resultsCount};
+    my $resultsLimit  = $json->{resultsLimit};
+    my $resultsOffset = 0;
+    
+    while (($resultsOffset += $resultsLimit) < $resultsCount) {
+        $subs{$sub}(decode_json get "$resource$resultsOffset");
+    }
+}
+
+my $offset = 0;
+
+my $api = "http://api.espn.com/v1/sports/basketball/nba/athletes?apikey=$ENV{DDG_SPICE_ESPN_APIKEY}&offset=";
+
+parse $api, 'players';
