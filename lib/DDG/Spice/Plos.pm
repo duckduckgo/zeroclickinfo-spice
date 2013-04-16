ace9fe5a2a472ce58b869a78eb982cbcd743a6c4
Bruno Vellutini <organelas@gmail.com>
2013-04-17 01:21:47 +0200

    First working prototype for DDG PLOS spice

---
+++ lib/DDG/Spice/Plos.pm
@@ -0,0 +1,19 @@
+package DDG::Spice::Plos;
+# ABSTRACT: Returns 5 best research articles from PLOS database.
+
+use DDG::Spice;
+
+triggers start => 'plos';
+
+spice to => 'http://api.plos.org/search?q="$1"&rows=5&wt=json&api_key={{ENV{DDG_SPICE_PLOS_APIKEY}}}';
+spice wrap_jsonp_callback => 1;
+
+spice is_cached => 0;
+
+handle remainder => sub {
+    return $_;
+    return '' if $_ eq '';
+    return;
+};
+
+1;
