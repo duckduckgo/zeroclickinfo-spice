670b2ed8b961fb8b9d8d0f5bb138657366d965c3
Jag Talon <talon.jag@gmail.com>
2013-04-16 12:18:23 -0400

    Forvo: New Forvo plugin based on SoundManager2.

--- share/spice/forvo/forvo.js
+++ share/spice/forvo/forvo.js
@@ -5,39 +5,56 @@ var ddg_spice_forvo = function(api_result) {
         return toString.call(obj) === "[object Array]";
     };
 
-    // This function initializes the audio player.
-    var initializePlayer = function() {
-        $(document).ready(function() {
-            $('audio').mediaelementplayer({
-                audioWidth      : "51%",
-                audioHeight     : 30,
-                enableAutosize  : true,
-                features        : ['playpause','progress','current']
+    Spice.render({
+        data             : api_result,
+        header1          : "Pronunciations (Forvo)",
+        source_url       : "http://www.forvo.com/",
+        source_name      : "Forvo",
+        template_normal  : "forvo",
+        force_big_header : true
+    });
+
+    $(".zero_click_snippet").attr("style", "margin-left: 0px !important; display: block;");
+
+    // Initialize SoundManager2.
+    window.SM2_DEFER = true;
+    var soundSetup = function() {
+        window.soundManager = new SoundManager();
+        soundManager.url = "/soundmanager2/swf/";
+        soundManager.flashVersion = 9;
+        soundManager.useFlashBlock = false;
+        soundManager.useHTML5Audio = false;
+        soundManager.beginDelayedInit();
+        soundManager.onready(function() {
+            // Prevent the user from clicking the link.
+            // This copies the attribute "data-stream" to "href."
+            $(".forvo-audio").each(function() {
+                var stream = $(this).attr("data-stream");
+                $(this).attr("href", stream);
             });
+            var pagePlayer = new PagePlayer();
+            pagePlayer.init(typeof PP_CONFIG !== 'undefined' ? PP_CONFIG : null);
         });
     };
 
-    // Display the data and load MediaElement.js.
-    var render = function(template) {
-        Spice.render({
-            data             : api_result,
-            force_big_header : true,
-            header1          : "Pronunciations by Forvo",
-            source_name      : "Forvo",
-            source_url       : "http://www.forvo.com/",
-            template_normal  : template
-        });
+    var ready = [false, false];
+    var checkReady = function(toggle) {
+        ready[toggle] = true;
+        if(ready[0] && ready[1]) {
+            soundSetup();
+        }
+    };
 
-        $.getScript("/forvo/mediaelement-and-player.min.js", initializePlayer);
-    }
+    // Load page-player.js. This JS file converts a list of MP3s into a playlist.
+    $.getScript("/soundmanager2/script/page-player-forvo.js", function() {
+        checkReady(0);
+    });
 
-    // Check if api_result.items is an array or not.
-    if(isArray(api_result.items) && +api_result.attributes.total >= 3) {
-        render("forvo");
-    } else if(+api_result.attributes.total >= 3) {
-        render("forvo-fallback");
-    }
-}
+    // Load SoundManager2. This JS file handles our audio.
+    $.getScript("/soundmanager2/script/soundmanager2-nodebug-jsmin.js", function() {
+        checkReady(1);
+    });
+};
 
 // Make sure we display only three items.
 Handlebars.registerHelper('list', function(items, options) {
@@ -46,4 +63,12 @@ Handlebars.registerHelper('list', function(items, options) {
         out += options.fn(items[i]);
     }
     return out;
+});
+
+Handlebars.registerHelper("sex", function(sex) {
+    if(sex === "m") {
+        return "Male";
+    } else {
+        return "Female";
+    }
 });
