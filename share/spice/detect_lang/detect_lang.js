4e0ede3c911457553984ca3fc6ca674900224d2b
Jag Talon <talon.jag@gmail.com>
2013-04-19 11:26:17 -0400

    Finished Detect Language.

--- share/spice/detect_lang/detect_lang.js
+++ share/spice/detect_lang/detect_lang.js
@@ -1,91 +1,20 @@
 var ddg_spice_detect_lang = function(api_result) {
     "use strict";
 
-    var langs = {
-        'af': 'Afrikaans',
-        'ar': 'Arabic',
-        'be': 'Belarusian',
-        'bg': 'Bulgarian',
-        'ca': 'Catalan',
-        'chr': 'Cherokee',
-        'cs': 'Czech',
-        'cy': 'Welsh',
-        'da': 'Danish',
-        'de': 'German',
-        'dv': 'Dhivehi',
-        'el': 'Greek',
-        'en': 'English',
-        'es': 'Spanish',
-        'et': 'Estonian',
-        'fa': 'Persian',
-        'fi': 'Finnish',
-        'fil': 'Tagalog',
-        'fr': 'French',
-        'ga': 'Irish',
-        'gu': 'Gujarati',
-        'he': 'Hebrew',
-        'hi': 'Hindi',
-        'hr': 'Croatian',
-        'hu': 'Hungarian',
-        'hy': 'Armenian',
-        'is': 'Icelandic',
-        'it': 'Italian',
-        'iu': 'Inuktitut',
-        'ja': 'Japanese',
-        'ka': 'Georgian',
-        'km': 'Khmer',
-        'kn': 'Kannada',
-        'ko': 'Korean',
-        'lo': 'Laothian',
-        'lt': 'Lithuanian',
-        'lv': 'Latvian',
-        'mk': 'Macedonian',
-        'ml': 'Malayalam',
-        'ms': 'Malay',
-        'nb': 'Norwegian',
-        'nl': 'Dutch',
-        'or': 'Oriya',
-        'pa': 'Punjabi',
-        'pl': 'Polish',
-        'pt': 'Portuguese',
-        'ro': 'Romanian',
-        'ru': 'Russian',
-        'si': 'Sinhalese',
-        'sk': 'Slovak',
-        'sl': 'Slovenian',
-        'sr': 'Serbian',
-        'sv': 'Swedish',
-        'sw': 'Swahili',
-        'syr': 'Syriac',
-        'ta': 'Tamil',
-        'te': 'Telugu',
-        'th': 'Thai',
-        'tr': 'Turkish',
-        'uk': 'Ukrainian',
-        'vi': 'Vietnamese',
-        'yi': 'Yiddish',
-        'zh': 'Chinese',
-        'zh-TW': 'Chineset'
-    };
+    // Check for any errors.
+    if(!api_result || !api_result.data || !api_result.data.detections || api_result.data.detections.length === 0) {
+        return;
+    }
 
-    var toPercent = function(confidence) {
-        return Math.round(confidence * 100) + "% sure";
-    };
+    var detections = api_result.data.detections;
 
-    var detections = api_result.data.detections, result;
-
-    if(detections[0].isReliable) {
-        result = "This text is definitely " + langs[detections[0].language] + " (" + toPercent(detections[0].confidence) + ").";
-    } else if(detections.length === 1) {
-        result = "This text is probably " + langs[detections[0].language] + " (" + toPercent(detections[0].confidence) + ").";
-    } else if(detections.length > 1) {
-        result = "This text is probably " + langs[detections[0].language] + " (" + toPercent(detections[0].confidence);
-        result += "), but it could also be "  + langs[detections[1].language] + " (" + toPercent(detections[1].confidence) + ").";
+    if(Handlebars.helpers.expandLang(detections[0].language) === "") {
+        return;
     }
 
     // Display the plug-in.
     Spice.render({
-        data             : {result: result},
+        data             : detections[0],
         header1          : "Language Detection (Detect Language)",
         source_url       : "http://detectlanguage.com/",
         source_name      : "Detect Language",
@@ -93,3 +22,105 @@ var ddg_spice_detect_lang = function(api_result) {
         force_big_header : true
     });
 };
+
+// Two letter (sometimes three) languages should be expanded.
+Handlebars.registerHelper("expandLang", function(language) {
+    "use strict";
+    var langs = {
+        af: "Afrikaans",
+        am: "Amharic",
+        ar: "Arabic",
+        az: "Azerbaijani",
+        be: "Belarusian",
+        bg: "Bulgarian",
+        bn: "Bengali",
+        bo: "Tibetan",
+        ca: "Catalan",
+        chr: "Cherokee",
+        cs: "Czech",
+        cy: "Welsh",
+        da: "Danish",
+        de: "German",
+        dv: "Dhivehi",
+        el: "Greek",
+        en: "English",
+        es: "Spanish",
+        et: "Estonian",
+        eu: "Basque",
+        fa: "Persian",
+        fi: "Finnish",
+        fil: "Tagalog",
+        fr: "French",
+        ga: "Irish",
+        gl: "Galician",
+        gu: "Gujarati",
+        he: "Hebrew",
+        hi: "Hindi",
+        hr: "Croatian",
+        ht: "Haitian Creole",
+        hu: "Hungarian",
+        hy: "Armenian",
+        id: "Indonesian",
+        is: "Icelandic",
+        it: "Italian",
+        iu: "Inuktitut",
+        ja: "Japanese",
+        ka: "Georgian",
+        km: "Khmer",
+        kn: "Kannada",
+        ko: "Korean",
+        lo: "Laothian",
+        lt: "Lithuanian",
+        lv: "Latvian",
+        mk: "Macedonian",
+        ml: "Malayalam",
+        ms: "Malay",
+        mt: "Maltese",
+        my: "Burmese",
+        nb: "Norwegian",
+        nl: "Dutch",
+        or: "Oriya",
+        pa: "Punjabi",
+        pl: "Polish",
+        pt: "Portuguese",
+        ro: "Romanian",
+        ru: "Russian",
+        si: "Sinhalese",
+        sk: "Slovak",
+        sl: "Slovenian",
+        sq: "Albanian",
+        sr: "Serbian",
+        sv: "Swedish",
+        sw: "Swahili",
+        syr: "Syriac",
+        ta: "Tamil",
+        te: "Telugu",
+        th: "Thai",
+        tr: "Turkish",
+        uk: "Ukrainian",
+        ur: "Urdu",
+        vi: "Vietnamese",
+        yi: "Yiddish",
+        zh: "Chinese"
+    };
+
+    if(!langs[language] || language === "xxx") {
+        return "";
+    } else {
+        return langs[language];
+    }
+});
+
+Handlebars.registerHelper("toPercent", function(confidence) {
+    "use strict";
+    return Math.round(confidence * 100) + "% sure";
+});
+
+Handlebars.registerHelper("confidence", function(isReliable) {
+    "use strict";
+    if(isReliable) {
+        return "definitely";
+    } else {
+        return "probably";
+    }
+});
