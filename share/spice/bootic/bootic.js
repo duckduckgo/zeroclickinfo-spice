b2e38584203423fc352c45d12399bc2505ce4d51
moollaza <moollaza@gmail.com>
2013-04-24 14:06:41 -0400

    fixed bootic image sizes, hardcoded to 60x80 via api call

--- share/spice/bootic/bootic.js
+++ share/spice/bootic/bootic.js
@@ -8,7 +8,7 @@ function ddg_spice_bootic ( api_result ) {
 		'';
 
 	var array = [];
-	
+
 	$.each(api_result.products, function(key, value){
 		array.push(this);
 	});
@@ -20,12 +20,12 @@ function ddg_spice_bootic ( api_result ) {
 		source_name : 'Bootic',
 		source_url : 'http://www.bootic.com/?q=' + query,
 		header1 : api_result.input_query + ' ( Bootic )' ,
-		
+
 		template_frame: "carousel",
 		template_normal: "bootic",
 		carousel_css_id: "bootic",
 		carousel_items: api_result.products,
-		
+
 		force_no_fold : 1,
 		force_big_header : 1,
 	});
@@ -37,7 +37,9 @@ function ddg_spice_bootic ( api_result ) {
 
 // forms the url for a bootic product image
 Handlebars.registerHelper ('picture_url', function() {
-	var pic_base_uri = 'http://static.bootic.com/_pictures/';
-	var pic_id = this.pictures[0];
-	return pic_base_uri + pic_id;
+	var pic_base_uri = 'http://static.bootic.com/_pictures/',
+		pic_id = this.pictures[0],
+		url = pic_id.replace(/\d+x\d+/, "60x80");
+
+	return pic_base_uri + url;
 });
