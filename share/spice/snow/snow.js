1d34df1a109199f4b798a16070cf5fe09f649d0c
Jag Talon <talon.jag@gmail.com>
2013-04-24 16:58:37 -0400

    Added a few comments to the Snow plug-in.

--- share/spice/snow/snow.js
+++ share/spice/snow/snow.js
@@ -8,10 +8,12 @@
 // is it snowing? - tells you if it's snowing in your current location.
 
 var nrio = function(api_result) {
+    // Check for errors.
     if(!api_result || api_result.error || !api_result.location) {
         return;
     }
 
+    // Get only the city.
     var getLocation = function() {
         if(api_result.location.indexOf(",") !== -1) {
             return api_result.location.substring(0, api_result.location.indexOf(","));
@@ -19,6 +21,7 @@ var nrio = function(api_result) {
         return api_result.location;
     };
 
+    // Display the spice plug-in.
     Spice.render({
         data              : api_result,
         header1           : "Is it snowing in " + getLocation() + "?",
