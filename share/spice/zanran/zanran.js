cb823cb68ec5cc2c16f5bf3cfeac604ca7c94f22
Jag Talon <talon.jag@gmail.com>
2013-04-25 16:49:29 -0400

    Remove checkLength from Zanran.

--- share/spice/zanran/zanran.js
+++ share/spice/zanran/zanran.js
@@ -41,10 +41,3 @@ Handlebars.registerHelper("each", function(context, options) {
 
     return result;
 });
-
-Handlebars.registerHelper("checkLength", function(context, options) {
-    if(context.length > 1) {
-        console.log(options.fn(context));
-        return options.fn(context);
-    }
-});
