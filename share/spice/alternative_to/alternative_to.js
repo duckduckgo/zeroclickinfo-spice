26f9f23d87a835e084989bb04e2cb92fd1bf11bb
Jag Talon <talon.jag@gmail.com>
2013-04-29 15:53:14 -0400

    AlternativeTo: Now works.

--- share/spice/alternative_to/alternative_to.js
+++ share/spice/alternative_to/alternative_to.js
@@ -5,6 +5,7 @@ function ddg_spice_alternative_to(api_result) {
         source_url     : api_result.Url,
         template_normal: "alternative_to",
         template_frame : "carousel",
+        carousel_template_detail: "alternative_to_details",
         carousel_css_id: "alternative_to",
         carousel_items : api_result.Items,
         force_no_fold  : 1
