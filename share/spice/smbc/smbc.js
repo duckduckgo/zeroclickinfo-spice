4bf7c54ed40cdb91ccec1145158a68f2f7ecafa3
Jag Talon <talon.jag@gmail.com>
2013-04-30 13:49:36 -0400

    SMBC now uses the {{#with}} block.

--- share/spice/smbc/smbc.js
+++ share/spice/smbc/smbc.js
@@ -5,7 +5,7 @@ function ddg_spice_smbc(api_result) {
 
     Spice.render({
         data             : api_result,
-        header1          : api_result.items[0].title,
+        header1          : api_result.items[0].title + " (SMBC)",
         source_url       : api_result.url,
         source_name      : 'SMBC',
         template_normal  : 'smbc',
