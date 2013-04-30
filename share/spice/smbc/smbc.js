c15c865dd72e739bebd29b3793b391d7287d5522
Jag Talon <talon.jag@gmail.com>
2013-04-30 13:29:05 -0400

    SMBC now points to the right URL.
    I guess the API changed or something.

--- share/spice/smbc/smbc.js
+++ share/spice/smbc/smbc.js
@@ -2,12 +2,12 @@ function ddg_spice_smbc(response) {
     var smbc       = {};
     smbc.img_url   = response.items[0].image_url;
     smbc.alt_text  = response.items[0].alt_text;
-    smbc.prev_url  = response.items[1].link;
+    smbc.prev_url  = response.items[1].url;
 
     Spice.render({
         data             : smbc,
         header1          : response.items[0].title,
-        source_url       : response.link,
+        source_url       : response.url,
         source_name      : 'SMBC',
         template_normal  : 'smbc',
         force_big_header : true
