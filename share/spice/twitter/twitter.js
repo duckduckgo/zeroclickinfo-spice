d04a0c1104a06205921aac45c560c77f5df227cd
moollaza <moollaza@gmail.com>
2013-05-01 01:18:04 -0400

    convert twitter trends to Spice2, removed old endpoints because we can't support them

---
+++ share/spice/twitter/twitter.js
@@ -0,0 +1,12 @@
+function ddg_spice_twitter_trends(api_result) {
+    if (!api_result[0]) return;
+
+		Spice.render({
+				data : api_result,
+				force_big_header : true,
+				source_name : "Twitter",
+				source_url : "http://twitter.com",
+				template_normal : "twitter_trends",
+				header1 : "Trending in " + api_result[0].locations[0].name + " (Twitter)"
+		});
+}
