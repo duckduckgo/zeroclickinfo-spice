e6dddabde823922d64de61d340398149262d4cf2
Ado Matejov <a.matejov@centrum.sk>
2013-05-01 12:11:39 +0200

    guidebox initial -- spice2

---
+++ lib/DDG/Spice/Guidebox/Lastshows.pm
@@ -0,0 +1,28 @@
+package DDG::Spice::Guidebox::Lastshows;
+
+use DDG::Spice;
+
+
+spice to => 'http://api.thetvapi.com/v1.2/json/NdlLbziCtRxn7zQQWIe80beqcthMlP/$1/all';
+
+spice wrap_jsonp_callback => 1;
+
+triggers any => "///***never_trigger***///";
+
+handle remainder => sub {
+    my %IDS = (
+        'NCIS' => 19274,
+        'Castle' => 23580,
+        'Grimm' => 36833,
+    );
+
+    my %choose = (
+        'NCIS' => 'series',
+        'Castle' => 'series',
+        'Grimm' => 'series',
+    );
+
+    return $choose{$_}, $IDS{$_};
+};
+
+1;
