f38d6db0d4f38eb1646bf1250c2fd0aee3569a32
Dylan Lloyd <dylan@dylansserver.com>
2013-05-12 19:01:02 -0400

    simplify munging w/ template & dont show empties
    Empty fields are now ignored. This still needs a sanity check in the
    case of all empty fields. Although if we use a real metacpan link rather
    than search for the more at link it might still be helpful.

--- share/spice/meta_cpan/meta_cpan.js
+++ share/spice/meta_cpan/meta_cpan.js
@@ -1,22 +1,15 @@
 function ddg_spice_meta_cpan(api_response) {
 	if (api_response["message"]) return;
     
-    var pkg = {};
+	query = DDG.get_query().replace(/\s*(metacpan|meta cpan|cpan)\s*/i, '');
 
-	pkg.query       = DDG.get_query().replace(/\s*(metacpan|meta cpan|cpan)\s*/i, '');
-	pkg.name        = api_response.documentation ? api_response.documentation : "No documentation specified.";
-	pkg.author      = api_response.author ? api_response.author : "No author specified.";
-	pkg.abstract    = api_response.abstract ? api_response.abstract : "No abstract specified.";
-	pkg.version     = api_response.version ? api_response.version : "No version given.";
-	pkg.description = api_response.description ? api_response.description : "No description specified.";
-
-    if (pkg.description.length > 340)
-        pkg.description = pkg.description.substring(0, 340)
+    if (api_response.description && api_response.description.length > 340)
+        api_response.description = api_response.description.substring(0, 340)
                         + '...';
 
     Spice.render({
-        data             : pkg,
-        header1          : pkg.query + " (MetaCPAN)",
+        data             : api_response,
+        header1          : query + " (MetaCPAN)",
         source_url       : 'https://metacpan.org/search?q='
                            + encodeURIComponent(name),
         source_name      : 'MetaCPAN',
