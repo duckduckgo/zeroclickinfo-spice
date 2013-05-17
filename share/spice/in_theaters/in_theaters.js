39377921da1435a556dae311ba8100ef972fbac6
Jag Talon <talon.jag@gmail.com>
2013-05-17 12:10:28 -0400

    InTheaters: Switch to the old version.
    We want the plugin to have a uniform look.

--- share/spice/in_theaters/in_theaters.js
+++ share/spice/in_theaters/in_theaters.js
@@ -37,58 +37,6 @@ var ddg_spice_in_theaters = function(api_result) {
         force_big_header : true
     });
 
-    // Get the size of the screen.
-    // If the screen is small, don't display the image.
-    var zero_click_image = $("#zero_click_image");
-    var checkWidth = function(width) {
-        if(width < 750) {
-            zero_click_image.addClass("hide");
-            $(".zero_click_snippet").attr("style", "margin-left: 0px !important; display: block;");
-        } else {
-            zero_click_image.removeClass("hide");
-            $(".zero_click_snippet").attr("style", "margin-left: 0px !important; display: block; width: 80%;");
-        }
-    };
-
-    // Monitor the size of the window.
-    checkWidth($(window).width());
-    $(window).resize(function() {
-        checkWidth($(window).width());
-    });
-
-    // This variable tracks the last movie that the user hovered on.
-    var last = 0;
-    // Remove all `img` tags first and start over.
-    // Why are we adding all the images instead of just updating the `src` attribute in $("#zero_click_image img")?
-    // Because if we do that, the browser will try to request for the image again.
-    zero_click_image.html("");
-    $(".movie").each(function(index) {
-        // We should show the first image and hide everything else by adding the class="hide" attribute.
-        if(index === 0) {
-            zero_click_image.append("<img style='height: 120px !important' id='Movie" + index + "' src='" + $(this).attr("data-image") + "'>");
-        } else {
-            zero_click_image.append("<img style='height: 120px !important' class='hide' id='Movie" + index + "' data-image='" + $(this).attr("data-image") + "'>");
-        }
-
-        // Show the image when we hover over the list of movies.
-        $(this).on("hover", function() {
-            // We wrap everything in a function to get hold of the index variable.
-            // Why? Because the computer is so fast that by the time we hover on a movie,
-            // index would point to the last element.
-            (function(index) {
-                var lastMovie = $("#Movie" + last);
-                var newMovie = $("#Movie" + index);
-                lastMovie.addClass("hide");
-                newMovie.removeClass("hide");
-
-                // Lazy loading. We should only load the image when the user hovered over it.
-                if(!newMovie.attr("src")) {
-                    newMovie.attr("src", newMovie.attr("data-image"));
-                }
-                last = index;
-            })(index);
-        });
-    });
 };
 
 // Convert minutes to hr. min. format.
