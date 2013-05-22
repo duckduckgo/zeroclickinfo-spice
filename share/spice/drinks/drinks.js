0e123e3cfa842b9a36641a25a60f15f2599c62ea
Jag Talon <talon.jag@gmail.com>
2013-05-22 16:47:03 -0400

    Drinks: Ingredients wasn't showing up.
    Turns out that the list of ingredients can either be an array or not. We use the helper function {{keys}} to iterate through the ingredients instead of {{each}} for it to work.

--- share/spice/drinks/drinks.js
+++ share/spice/drinks/drinks.js
@@ -1,14 +1,19 @@
 function ddg_spice_drinks(api_result) {
 
-    var drink = api_result[0];
-    
-    // validity check
-    if (!drink.name) return;
+    if(!api_result || api_result.length === 0 || !api_result[0].name) {
+        return;
+    }
+
+    if($.isArray(api_result[0].ingredients)) {
+        api_result[0].isArray = true;
+    } else {
+        api_result[0].isArray = false;
+    }
 
     Spice.render({
-        data             : drink,
-        header1          : drink.name + " (Drinks)",
-        source_url       : drink.url,
+        data             : api_result[0],
+        header1          : api_result[0].name + " (Drinks)",
+        source_url       : api_result[0].url,
         source_name      : 'The Drink Project',
         template_normal  : 'drinks',
         force_no_fold    : true
