36a1ad0bb973b0172a132c0dead1938011e199b4
Jag Talon <talon.jag@gmail.com>
2013-05-24 17:13:29 -0400

    Rhymes: Remove adult words.

--- share/spice/rhymes/rhymes.js
+++ share/spice/rhymes/rhymes.js
@@ -1,32 +1,34 @@
 function ddg_spice_rhymes ( api_result ) {
-	var query = DDG.get_query()
-				.replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
+    var query = DDG.get_query()
+                .replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
 
-	if ( !api_result.length ) return;
+    if (!api_result.length) {
+        return;
+    }
 
-	Spice.render({
-		data             : api_result,
-		header1          : query + " (Rhymes)",
-		source_url       : 'http://rhymebrain.com/en/What_rhymes_with_' +
-							encodeURIComponent(query),
-		source_name      : 'RhymeBrain',
-		template_normal  : 'rhymes',
-		force_big_header : true
-	});
+    Spice.render({
+        data             : api_result,
+        header1          : query + " (Rhymes)",
+        source_url       : 'http://rhymebrain.com/en/What_rhymes_with_' +
+                            encodeURIComponent(query),
+        source_name      : 'RhymeBrain',
+        template_normal  : 'rhymes',
+        force_big_header : true
+    });
 }
 
 // Randomly selects rhyming words from the
 // list returned by the RhymeBrain API
 Handlebars.registerHelper('selectRhymes', function(options) {
-	var words = [];
+    var words = [];
 
-	for (var i = 0; i < this.length; i++) {
-		var word = this.splice( Math.random() * this.length, 1)[0];
+    for (var i = 0; i < this.length && i < 15; i++) {
+        var word = this[i];
 
-		if (word.score == 300)
-			words.push(word);
-		if (words.length == 30) break;
-	}
+        if (word.score === 300 && !word.flags.match(/a/)) {
+            words.push(word);
+        }
+    }
 
-	return options.fn(words);
+    return options.fn(words);
 });
