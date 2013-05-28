1912326fcad9820d3d4bdc621d4741e4eaf8cb13
Jag Talon <talon.jag@gmail.com>
2013-05-27 20:16:06 -0400

    Google+: Increased maxResults to 12.

--- lib/DDG/Spice/GooglePlus.pm
+++ lib/DDG/Spice/GooglePlus.pm
@@ -3,7 +3,7 @@ package DDG::Spice::GooglePlus;
 
 use DDG::Spice;
 
-spice to => 'https://www.googleapis.com/plus/v1/people/?query=$1&key={{ENV{DDG_SPICE_GOOGLE_PLUS_APIKEY}}}&callback={{callback}}';
+spice to => 'https://www.googleapis.com/plus/v1/people/?query=$1&key={{ENV{DDG_SPICE_GOOGLE_PLUS_APIKEY}}}&callback={{callback}}&maxResults=12';
 spice proxy_ssl_session_reuse => "off";
 
 primary_example_queries "google+ duckduckgo";
