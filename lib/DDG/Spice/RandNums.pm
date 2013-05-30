9b138dc96696ca54f8a52aa17f5b3c612763c722
Dylan Lloyd <dylan@dylansserver.com>
2013-05-30 18:26:57 -0400

    refactored randnums, cleaned regex, added tests

--- lib/DDG/Spice/RandNums.pm
+++ lib/DDG/Spice/RandNums.pm
@@ -13,7 +13,6 @@ attribution github  => ['https://github.com/ghedo', 'ghedo'      ],
             web     => ['http://ghedini.me', 'Alessandro Ghedini'];
 status "disabled";
 
-
 spice to   => 'http://www.random.org/integers/?num=10&min=$1&max=$2&col=10&base=10&format=plain&rnd=new';
 spice from => '(\-?[0-9]+)\/(\-?[0-9]+)';
 
@@ -21,26 +20,19 @@ spice is_cached => 0;
 spice proxy_cache_valid => "418 1d";
 spice wrap_string_callback => 1;
 
-triggers query_lc => qr/^(rand|random) (numbers?|nums?)(?: (\-?[0-9]+)(?:\s*)(?:\-| )(?:\s*)(\-?[0-9]+)|)$/;
+triggers query_lc => qr/^(?:rand|random) (?:numbers?|nums?)(?: (\-?[0-9]+)\s*-?\s*(\-?[0-9]+))?$/;
 
 handle matches => sub {
-	my (undef, undef, $min, $max) = @_;
+    my ($min, $max) = @_;
 
-	if(!defined $min) {
-		$min = 1;
-	}
-	if(!defined $max) {
-		$max = 100;
-	}
+	$min = 1   unless defined $min;
+	$max = 100 unless defined $max;
 
 	$min = -1000000000 if $min < -1000000000;
 	$max =  1000000000 if $max >  1000000000;
 
-	if($min >= $max) {
-		return $max, $min;
-	} else {
-		return ($min, $max);
-	}
+	return $max, $min if $min >= $max;
+    return $min, $max;
 };
 
 1;
