1278153efa7f0838623ddcab1705f38ae5ef07a5
Jag Talon <talon.jag@gmail.com>
2013-05-31 15:15:45 -0400

    Aur: Removed undefined variables and added metadata.

--- lib/DDG/Spice/Aur.pm
+++ lib/DDG/Spice/Aur.pm
@@ -2,12 +2,19 @@ package DDG::Spice::Aur;
 
 use DDG::Spice;
 
+primary_example_queries "arch package 9base";
+secondary_example_queries "archlinux package epiphany"
+description "Github info";
+name "Github";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Aur.pm";
+topics "programming";
+category "programming";
 attribution twitter => 'crazedpsyc',
             cpan => 'CRZEDPSYC';
 
 spice to => 'https://aur.archlinux.org/rpc.php?type=search&arg=$1&callback={{callback}}';
 
-triggers any => "aur", "archlinux package", "arch package";
+triggers any => "aur", "archlinux package", "arch package", "arch linux package";
 
 handle remainder => sub {
     s/^for //;
--- share/spice/aur/aur.js
+++ share/spice/aur/aur.js
@@ -7,10 +7,12 @@ function ddg_spice_aur(response) {
     var pkgs = response.results;
     var pkg  = pkgs[0];
 
+    var query = DDG.get_query().replace(/(aur|archlinux package|arch package|arch linux package)/, "");
+
     Spice.render({
         data             : pkgs.length == 1 ? pkg : { 'package' : pkgs },
         header1          : pkg['Name'] + " (AUR)",
-        source_url       : 'https://aur.archlinux.org/packages.php?ID=' + response['ID'],
+        source_url       : 'https://aur.archlinux.org/packages/?O=0&K=' + query,
         source_name      : 'ArchLinux User Repository',
         template_normal  : pkgs.length == 1 ? 'single' : 'list',
         force_big_header : true
