5d74c3d41e7b0338b706df6bb9cd3612159126b9
Jag Talon <talon.jag@gmail.com>
2013-06-05 15:26:49 -0400

    Aur: Linted.

--- share/spice/aur/aur.js
+++ share/spice/aur/aur.js
@@ -1,8 +1,9 @@
 function ddg_spice_aur(response) {
-    if (response.type == 'error'
-            || !response.results
-            || response.results.length == 0)
+    "use strict";
+
+    if (response.type === 'error' || !response.results || response.results.length === 0) {
         return;
+    }
 
     var pkgs = response.results;
     var pkg  = pkgs[0];
@@ -10,11 +11,11 @@ function ddg_spice_aur(response) {
     var query = DDG.get_query().replace(/(aur|archlinux package|arch package|arch linux package)/, "");
 
     Spice.render({
-        data             : pkgs.length == 1 ? pkg : { 'package' : pkgs },
-        header1          : pkg['Name'] + " (AUR)",
+        data             : pkgs.length === 1 ? pkg : {'package' : pkgs},
+        header1          : pkg.Name + " (AUR)",
         source_url       : 'https://aur.archlinux.org/packages/?O=0&K=' + query,
         source_name      : 'ArchLinux User Repository',
-        template_normal  : pkgs.length == 1 ? 'single' : 'list',
+        template_normal  : pkgs.length === 1 ? 'single' : 'list',
         force_big_header : true
     });
 }
