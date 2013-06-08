7e8aca6b4051437f07f10bdd685179955f0723b2
moollaza <moollaza@gmail.com>
2013-06-08 14:04:32 -0400

    Translate Spice - added getTerms helper definition to translate_detect.js, also checks if alrady defined in translate_from_to.js

--- share/spice/translate/detect/translate_detect.js
+++ share/spice/translate/detect/translate_detect.js
@@ -18,3 +18,23 @@ function ddg_spice_translate_detect (api_result) {
         }
     }
 }
+
+
+// Helper to get only unique translations
+Handlebars.registerHelper('getTerms', function(input, options){
+    var translations = [];
+    var unique = {};
+
+    // iterate over each object and its keys
+    // in the "input" object
+    $.map(input, function(value, key) {
+        $.map(value, function(value2, key2) {
+
+            if (key2 !== "OriginalTerm" && value2.term && !unique[value2.term]){
+                unique[value2.term] = 1; //keep track of unique translations
+                translations.push( {word: value2.term} );
+            }
+        });
+    });
+    return options.fn(translations);
+});
--- share/spice/translate/from_to/translate_from_to.js
+++ share/spice/translate/from_to/translate_from_to.js
@@ -1,22 +1,23 @@
-/*******************************
-  Handlebars helpers
-  *******************************/
+// Check if helper already define
+// in the case translate_detect was called
+if (!Handlebars.helpers.getTerms) {
 
-// Helper to get only unique translations
-Handlebars.registerHelper('getTerms', function(input, options){
-    var translations = [];
-    var unique = {};
+    // Helper to get only unique translations
+    Handlebars.registerHelper('getTerms', function(input, options){
+        var translations = [];
+        var unique = {};
 
-    // iterate over each object and its keys
-    // in the "input" object
-    $.map(input, function(value, key) {
-        $.map(value, function(value2, key2) {
+        // iterate over each object and its keys
+        // in the "input" object
+        $.map(input, function(value, key) {
+            $.map(value, function(value2, key2) {
 
-            if (key2 != "OriginalTerm" && value2.term && !unique[value2.term]){
-                unique[value2.term] = 1; //keep track of unique translations
-                translations.push( {word: value2.term} );
-            }
+                if (key2 !== "OriginalTerm" && value2.term && !unique[value2.term]){
+                    unique[value2.term] = 1; //keep track of unique translations
+                    translations.push( {word: value2.term} );
+                }
+            });
         });
+        return options.fn(translations);
     });
-    return options.fn(translations);
-});
+}
