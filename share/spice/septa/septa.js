f08d9535bbafd6f2a5c21c1a7a9c6d0679c8b06e
Dylan Lloyd <dylan@dylansserver.com>
2013-06-09 21:35:14 -0400

    formatting changes

--- share/spice/septa/septa.js
+++ share/spice/septa/septa.js
@@ -1,13 +1,17 @@
 function ddg_spice_septa(api_result) {
-    console.log("SEPTA");
     console.log(api_result);
 
     Spice.render({
         data              : api_result,
-        header1           : 'SEPTA',
-        force_big_header  : true,
         source_name       : 'www.septa.org',
         source_url        : 'www.septa.org',
         template_normal   : 'septa'
     });
 };
+
+Handlebars.registerHelper ('delay', function(delay) {
+    if (delay == "On time") return "";
+    var parts = delay.split(" ");
+    var delay = parts[0] + " minute" + (parts[0] > 1 ? "s" : "") + " late";
+    return " (" + delay + ")";
+});
