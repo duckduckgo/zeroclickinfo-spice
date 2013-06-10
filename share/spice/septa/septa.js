ee49fe1780e92dabb64451a9c90af09a76dd334a
Dylan Lloyd <dylan@dylansserver.com>
2013-06-10 13:39:32 -0400

    parse query in js and use places in header

--- share/spice/septa/septa.js
+++ share/spice/septa/septa.js
@@ -1,11 +1,21 @@
 function ddg_spice_septa(api_result) {
     console.log(api_result);
+    var places = DDG.get_query().toLowerCase()
+        .replace(/\s+septa|septa\s+/, '')
+        .replace(/(next trains?|train times|train schedule)?( from)? /, '')
+        .replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase()
+                 + txt.substr(1).toLowerCase()
+        })
+        .split(" To ");
 
     Spice.render({
         data              : api_result,
         source_name       : 'SEPTA',
         source_url        : 'http://www.septa.org/schedules/',
         template_normal   : 'septa',
+        header1           : 'Trains from ' + places[0]
+                             + ' to ' + places[1] + ' (SEPTA)',
         force_no_icon     : true
     });
 };
