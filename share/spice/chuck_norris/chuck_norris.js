c9df22ea4e5934e01c12ec8903ae9b0df6fe25c1
Jag Talon <talon.jag@gmail.com>
2013-06-10 21:12:53 -0400

    ChuckNorris: Remove image.

--- share/spice/chuck_norris/chuck_norris.js
+++ share/spice/chuck_norris/chuck_norris.js
@@ -5,7 +5,6 @@ function ddg_spice_chuck_norris(api_result) {
         data             : api_result.value,
         source_url       : 'http://www.icndb.com',
         source_name      : 'Internet Chuck Norris Database',
-        image            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/ChuckNorris200611292256.jpg/48px-ChuckNorris200611292256.jpg',
         template_normal  : 'chuck_norris',
         force_no_icon    : true
     });
