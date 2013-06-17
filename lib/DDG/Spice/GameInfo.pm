73361450cdc18895bd833a43e21516c09c715173
moollaza <moollaza@gmail.com>
2013-06-17 17:04:51 -0400

    Gameinfo - small fix to perl handle, organized metadata

--- lib/DDG/Spice/GameInfo.pm
+++ lib/DDG/Spice/GameInfo.pm
@@ -1,11 +1,6 @@
 package DDG::Spice::GameInfo;
 
 use DDG::Spice;
-triggers startend => "game","games";
-spice to => 'http://thefreegamesdb.com/API/DuckDuckGo.php?Name=$1';
-spice wrap_jsonp_callback => 1;
-spice is_cached => 1;
-
 primary_example_queries "homesick game";
 description "See information about a game";
 name "GameInfo";
@@ -15,9 +10,14 @@ topics "gaming";
 category "reference";
 attribution github => ['https://github.com/thomaspreece10','thomaspreece10'];
 
+triggers startend => "game","games";
+spice to => 'http://thefreegamesdb.com/API/DuckDuckGo.php?Name=$1';
+spice wrap_jsonp_callback => 1;
+spice is_cached => 1;
 
 handle remainder => sub {
-    return $_;
+    return $_ if $_;
+    return;
 };
 
 
