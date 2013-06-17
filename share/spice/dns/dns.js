1421db4601d8b50e5dccba0bac5b85b99682f109
Dylan Lloyd <dylan@dylansserver.com>
2013-06-17 04:00:50 -0400

    ported DNS to spice2

---
+++ share/spice/dns/dns.js
@@ -0,0 +1,12 @@
+function ddg_spice_dns(api_result) {
+    if (!api_result.response.records > 0) return;
+
+    Spice.render({
+        data             : api_result,
+        header1          : api_result.query.domain + ' (ViewDNS)',
+        source_url       : 'http://www.viewdns.info/dnsrecord/?domain=' + api_result.query.domain,
+        source_name      : 'ViewDNS',
+        template_normal  : 'dns',
+        force_big_header : true
+    });
+}
