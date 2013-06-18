ae92ba4a1d1a140229713bc72ffcb78a39e8156b
Jag Talon <talon.jag@gmail.com>
2013-06-18 18:29:29 -0400

    MetaCPAN: Link to the pod that's associated with the module.

--- share/spice/meta_cpan/meta_cpan.js
+++ share/spice/meta_cpan/meta_cpan.js
@@ -2,15 +2,18 @@ function ddg_spice_meta_cpan(api_response) {
 	if (api_response["message"]) return;
     
 	query = DDG.get_query().replace(/\s*(metacpan|meta cpan|cpan)\s*/i, '');
+    var link = "search?q=" + encodeURIComponent(query);
+    if(api_response.module && api_response.module.length > 0 && api_response.module[0].associated_pod) {
+        link = "module/" + api_response.module[0].associated_pod;
+    }
 
     Spice.render({
         data             : api_response,
         header1          : query + " (MetaCPAN)",
-        source_url       : 'https://metacpan.org/search?q='
-                           + encodeURIComponent(name),
+        source_url       : 'https://metacpan.org/' + link,
         source_name      : 'MetaCPAN',
         template_normal  : 'meta_cpan',
         force_big_header : true,
-        force_no_fold    : true,
+        force_no_fold    : true
     });
 }
