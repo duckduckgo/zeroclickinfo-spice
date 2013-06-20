44e68b53bd71eccb21d3e99a25c710fae9eccf1c
Jag Talon <talon.jag@gmail.com>
2013-06-19 21:05:39 -0400

    YouTube: Bump nryt version to 117.
    nryt116.js called /iyt/{{rq}} is not what we want.

--- share/spice/youtube/youtube.js
+++ share/spice/youtube/youtube.js
@@ -1,8 +1,11 @@
 function ddg_spice_youtube (api_result) {
     "use strict";
 
-    $.getScript("/js/r/nryt116.js", function() {
-        window.iqyt = 2;
+    window.iqytid = "";
+    window.iqyt = 2;
+
+    $.getScript("/js/r/nryt117.js", function() {
         ddgyt.nryt(api_result);
     });
+    
 };
