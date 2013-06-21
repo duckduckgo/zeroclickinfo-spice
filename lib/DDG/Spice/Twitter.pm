fc6098a33656ccf45edc221ea5700cc0a17f3520
Jag Talon <talon.jag@gmail.com>
2013-06-21 17:09:22 -0400

    Twitter: Add a new parameter.

--- lib/DDG/Spice/Twitter.pm
+++ lib/DDG/Spice/Twitter.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Twitter;
 
 use DDG::Spice;
 
-spice to => 'https://jagtalon.duckduckgo.com/tw.js?user=$1&callback={{callback}}';
+spice to => 'https://jagtalon.duckduckgo.com/tw.js?user=$1&callback={{callback}}&current=1';
 spice is_cached => 1;
 
 triggers startend => "twitter";
