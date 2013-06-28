f3270d74e70243112bf68067932010123893f2b8
Dylan Lloyd <dylan@dylansserver.com>
2013-06-27 20:32:34 -0400

    ported hashme to spice2

---
+++ share/spice/hash_me/hash_me.js
@@ -0,0 +1,29 @@
+function ddg_spice_hash_me(api_result){
+    if (!api_result || !api_result.found
+            || !api_result.hashes) return;
+
+    var query = DDG.get_query().replace(/hashme/i, '');
+    Spice.render({
+        data             : api_result,
+        header1          : query + " (HashMe)",
+        source_url       : 'http://leakdb.abusix.com/?q='
+                            + encodeURIComponent(query),
+        source_name      : 'leakdb.abusix.com',
+        template_normal  : 'hash_me',
+        force_big_header : true,
+        force_no_fold    : true
+    });
+}
+
+Handlebars.registerHelper('list', function(hash, options) {
+    return Object.keys(hash)
+        .sort(function(a, b) {
+            return a == 'plaintext' ? -1 : 1
+        })
+        .reduce(function(previous, current) {
+            return previous + options.fn({
+                'type'  : current,
+                'value' : hash[current]
+            })
+        }, '');
+});
