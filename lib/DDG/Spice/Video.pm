3cfa717ee72745133ba3e4a85011f8f80780af1c
Jag Talon <talon.jag@gmail.com>
2013-07-02 17:00:32 -0400

    Video: Increase the number of videos to twelve.

--- lib/DDG/Spice/Video.pm
+++ lib/DDG/Spice/Video.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Video;
 
 use DDG::Spice;
 
-spice to => 'https://jagtalon.duckduckgo.com/video.js?search=$1&callback={{callback}}';
+spice to => 'https://jagtalon.duckduckgo.com/video.js?search=$1&limit=12&callback={{callback}}';
 triggers startend => "video";
 
 handle remainder => sub {
