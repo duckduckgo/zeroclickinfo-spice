a85185c2194b799ef83b0d28f9c575ef7369c14c
Jag Talon <talon.jag@gmail.com>
2013-07-02 15:04:49 -0400

    Video: Change the endpoint of the video plugin.

--- lib/DDG/Spice/Video.pm
+++ lib/DDG/Spice/Video.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Video;
 
 use DDG::Spice;
 
-spice to => 'https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&q=$1&type=video&videoType=any&key=AIzaSyCcg33a9CZJ8APUnbPW7tVdQPVgX5tty1o&callback={{callback}}';
+spice to => 'https://jagtalon.duckduckgo.com/video.js?search=$1&callback={{callback}}';
 triggers startend => "video";
 
 handle remainder => sub {
