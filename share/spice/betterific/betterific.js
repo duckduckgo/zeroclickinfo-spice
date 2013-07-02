0f2fedacd57db822a0e49f53f0c492983c1b5796
Brad Cater <bradcater-Aspire-5750>
2013-07-01 21:13:01 -0400

    Added separate betterif, tag, and user handlebars templates. Refactored the js to avoid so many special String constructions in the callback. Pushed the CSS closer to the DDG standard. Saved some vertical space. Hid all but the first object by default.

--- share/spice/betterific/betterific.js
+++ share/spice/betterific/betterific.js
@@ -1,77 +1,55 @@
+/* Taken from
+   https://gist.github.com/821904 */
+if (typeof(Number.prototype.number_with_delimiter) === 'undefined') {
+  Number.prototype.number_with_delimiter = function(delimiter) {
+      var number = this + '', delimiter = delimiter || ',';
+      var split = number.split('.');
+      split[0] = split[0].replace(
+          /(\d)(?=(\d\d\d)+(?!\d))/g,
+          '$1' + delimiter
+      );
+      return split.join('.');    
+  };
+}
+
 function ddg_spice_betterific(api_result) {
   "use strict";
   if (!api_result.betterifs || !api_result.tags || !api_result.users) {
     return;
   }
   var short_betterif_name_length = 50;
-  // We have to build URLs for each entity, and since Handlebars templates
-  // can't take multiple arguments, we have to build some text snippets.
-  var kinds = {
-    betterifs : function(o) {
-      o.url_params = {
-        id       : o.id,
-        name     : o.name,
-        username : o.user.username
-      };
-      o.upvotes_s = naive_pluralize_with_count('upvote', o.upvotes_count);
-      o.downvotes_s = naive_pluralize_with_count('downvote', o.downvotes_count);
-      if (o.name.length > short_betterif_name_length) {
-        o.shortened_name = o.name.slice(0, short_betterif_name_length) + '...';
-      }
-      return o;
-    },
-    tags      : function(o) {
-      o.url_params = {
-        id   : o.id,
-        name : o.name
-      };
-      o.betterifs_s = naive_pluralize_with_count('betterif', o.betterifs_count);
-      o.followers_s = naive_pluralize_with_count('follower', o.followers_count);
-      return o;
-    },
-    users     : function(o) {
-      o.url_params = {
-        id       : o.id,
-        username : o.username
-      };
-      o.betterifs_s = naive_pluralize_with_count('betterif', o.betterifs_count);
-      o.followers_s = naive_pluralize_with_count('follower', o.followers_count);
-      o.upvotes_given_s = naive_pluralize_with_count('upvote', o.upvotes_given_count) + ' given';
-      o.upvotes_received_s = naive_pluralize_with_count('upvote', o.upvotes_received_count) + ' received';
-      return o;
+  var betterif_shortened_name = function(o) {
+    if (o.name.length > short_betterif_name_length) {
+      o.shortened_name = o.name.slice(0, short_betterif_name_length) + '...';
     }
+    return o;
   };
+  var kinds = ['betterifs', 'tags', 'users'];
   var s = 0;
-  var cnt_more = 0;
-  for (var kind in kinds) {
-    if (kinds.hasOwnProperty(kind)) {
-      if (!api_result[kind] || !api_result[kind][kind]) {
-        return;
-      }
-      s += api_result[kind][kind].length;
-      if (api_result[kind][kind].length > 1) {
-        cnt_more += api_result[kind][kind].length - 1;
-      }
+  var kind;
+  for (var i=kinds.length-1; i>=0; i--) {
+    kind = kinds[i];
+    if (!api_result[kind] || !api_result[kind][kind]) {
+      return;
     }
+    s += api_result[kind][kind].length;
   }
   if (s == 0) {
     return;
   }
-  api_result.cnt_more = cnt_more;
+  // Since we show 1 entity by default, subtract 1 here.
+  api_result.cnt_more = s - 1;
   var obj_length, pp_fn;
-  for (var kind in kinds) {
-    if (kinds.hasOwnProperty(kind)) {
-      if ((obj_length = api_result[kind][kind].length) > 0) {
-        pp_fn = kinds[kind];
-        for (var j=obj_length; j>0; j--) {
-          if (!api_result[kind][kind][j-1].id) {
-            return;
-          }
-          api_result[kind][kind][j-1] = pp_fn(api_result[kind][kind][j-1]);
-          // We have to set up each betterif's User.
-          if (kind == 'betterifs') {
-            api_result[kind][kind][j-1].user = kinds.users(api_result[kind][kind][j-1].user);
-          }
+  for (var i=kinds.length-1; i>=0; i--) {
+    kind = kinds[i];
+    if ((obj_length = api_result[kind][kind].length) > 0) {
+      for (var j=obj_length; j>0; j--) {
+        if (!api_result[kind][kind][j-1].id) {
+          return;
+        }
+        if (kind == 'betterifs') {
+          // We have to set up each betterif's shortened_name.
+          api_result[kind][kind][j-1] = betterif_shortened_name(api_result[kind][kind][j-1]);
         }
       }
     }
@@ -97,19 +75,9 @@ function dashed_s(s) {
   return s.replace(/[^a-z0-9\-\s]/gi, '').replace(/\s+/g, '-');
 }
 
-function naive_pluralize(s, c) {
-  "use strict";
-  return (parseInt(c) == 1) ? s : (s + 's');
-}
-
-function naive_pluralize_with_count(s, c) {
-  "use strict";
-  return c + ' ' + naive_pluralize(s, c);
-}
-
 Handlebars.registerHelper('betterifUrl', function(params) {
   "use strict";
-  return 'http://betterific.com/' + dashed_s(params.username) + '/' + dashed_s(params.name) + '/' + params.id;
+  return 'http://betterific.com/' + dashed_s(params.user.username) + '/' + dashed_s(params.name) + '/' + params.id;
 });
 
 Handlebars.registerHelper('tagUrl', function(params) {
@@ -122,12 +90,19 @@ Handlebars.registerHelper('userUrl', function(params) {
   return 'http://betterific.com/innovator/' + dashed_s(params.username) + '/' + params.id;
 });
 
+Handlebars.registerHelper('pluralize', function(number, single, plural) {
+  "use strict";
+  return parseInt(number).number_with_delimiter() + ' ' + ((parseInt(number) == 1) ? single : plural);
+});
+
 // The More link (which also serves as the Less link) at the bottom of the
 // plugin should show (or hide) the extra content.
 $('body').on('click', '#spice_betterific_more', function() {
   $('#spice_betterific').find('.obj-wrapper').not(':first-child').slideToggle(200, function() {
     $('#spice_betterific').find('#spice_betterific_more').find('span').toggle();
   });
+  $('#spice_betterific').find('.left-img').toggle();
+  $('#spice_betterific').find('.kind-wrapper').not(':first-child').toggle();
 });
 // The More and Less links next to long betterif names should hide and show the
 // truncated part of the names.
