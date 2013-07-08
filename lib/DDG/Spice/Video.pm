8ab48cb4616b1657bdaf45aa1278a88b143eb091
Jag Talon <talon.jag@gmail.com>
2013-07-08 16:41:38 -0400

    Video: Now uses the normalized output.

--- lib/DDG/Spice/Video.pm
+++ lib/DDG/Spice/Video.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Video;
 
 use DDG::Spice;
 
-spice to => 'https://jagtalon.duckduckgo.com/video.js?search=$1&limit=12&callback={{callback}}';
+spice to => 'https://jagtalon.duckduckgo.com/video.js?q=$1&n=12&callback={{callback}}';
 triggers startend => "video";
 
 handle remainder => sub {
--- share/spice/video/video.js
+++ share/spice/video/video.js
@@ -8,7 +8,7 @@ function ddg_spice_video(api_result) {
         template_frame: "carousel",
         template_normal: "video",
         carousel_css_id: "video",
-        carousel_items: api_result.items,
+        carousel_items: api_result,
         force_no_fold : 1,
         carousel_template_detail: "video_detail",
         template_options: {
@@ -21,8 +21,8 @@ function ddg_spice_video(api_result) {
     $("#ddgc_detail").prependTo("#video");
 
     function resizeDetail() {
-        var $khan = $("#spice_video");
-        var width = $khan.width() - 14;    //need 7px of padding on each side
+        var $video = $("#spice_video");
+        var width = $video.width() - 14;    //need 7px of padding on each side
         var height = Math.floor(width * 0.5625) + 30;    //30px for player menu
         $("#ddgc_detail").height(height);
         console.log(width, height);
