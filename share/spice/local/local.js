8c82635f40a407d7db785b4c7bcb3b92a6c5e1d2
Dylan Lloyd <dylan@dylansserver.com>
2013-07-10 18:27:26 -0400

    refactor into nice fns & move calls into success

--- share/spice/local/local.js
+++ share/spice/local/local.js
@@ -8,24 +8,60 @@ function ddg_spice_local(api_response) {
     for (var i in api_response.businesses)
         api_response.businesses[i].index = i;
 
-    Spice.render({
-        //header1                  : ' (Local)',
-        source_url               : 'http://yelp.com/?q='
-                                    + encodeURIComponent(DDG.get_query()),
-        source_name              : 'places near you',
-        template_frame           : 'carousel',
-        template_normal          : 'local',
-        template_options         : { li_width : 400 },
-        carousel_css_id          : 'local',
-        carousel_template_detail : 'local_detail',
-        carousel_items           : api_response.businesses,
-        force_no_fold            : true,
-        data                     : api_response,
+    $.getScript("/dist/leaflet.js", function() {
+        Spice.render({
+            //header1                  : ' (Local)',
+            source_url               : 'http://yelp.com/?q='
+                                        + encodeURIComponent(DDG.get_query()),
+            source_name              : 'places near you',
+            template_frame           : 'carousel',
+            template_normal          : 'local',
+            template_options         : { li_width : 400 },
+            carousel_css_id          : 'local',
+            carousel_template_detail : 'local_detail',
+            carousel_items           : api_response.businesses,
+            force_no_fold            : true,
+            data                     : api_response,
+        });
+        render_map(api_response);
+        bind_navigation();
     });
+};
 
+function render_map(api_response) {
     $('#ddgc_detail').append($('<div>').attr('id', 'map'));
-    $.getScript("/dist/leaflet.js", function() { render_map(api_response) });
 
+    L.Icon.Default.imagePath = "/dist/images";
+    ddg_spice_local_map = L.map('map');
+
+    var attribution = 'Map data &copy; '
+        + '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+        + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,'
+        + 'Imagery &copy <a href="http://cloudmade.com">CloudMade</a>';
+
+    L.tileLayer(
+        'http://{s}.tile.cloudmade.com/2f62ad0b4ba046f2b907b67e2c866fa4'
+            + '/997/256/{z}/{x}/{y}.png',
+        { 'attribution' : attribution, 'maxZoom' : 18 }
+    ).addTo(ddg_spice_local_map);
+
+    var businesses = api_response.businesses;
+    for (var i in businesses) {
+        var location = [ businesses[i].location.coordinate.latitude,
+                         businesses[i].location.coordinate.longitude ]
+        if (i == 0) ddg_spice_local_map.setView(location, 13);
+        ddg_spice_local_markers.push(
+            L.marker(location, { 'title' : businesses[i].name, 'id' : i }
+            ).on('click', function(e) {
+                $('#ddgc_dots a')[e.target.options.id].click();
+            }).addTo(ddg_spice_local_map));
+    }
+    for (var i in ddg_spice_local_markers) 
+        ddg_spice_local_markers[i].options.zIndex =
+            $(ddg_spice_local_markers[i]._icon).css('z-index');
+};
+
+function bind_navigation() {
     $('#local .ddgc_item').off().click(function(e) { $('#ddgc_detail').show() });
 
     var dots = $('#ddgc_dots a').off();
@@ -61,36 +97,4 @@ function ddg_spice_local(api_response) {
                  && ddg_spice_local_current != 0)
             dots[--ddg_spice_local_current].click();
     });
-
-};
-
-function render_map(api_response) {
-    L.Icon.Default.imagePath = "/dist/images";
-    ddg_spice_local_map = L.map('map');
-
-    var attribution = 'Map data &copy; '
-        + '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
-        + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,'
-        + 'Imagery &copy <a href="http://cloudmade.com">CloudMade</a>';
-
-    L.tileLayer(
-        'http://{s}.tile.cloudmade.com/2f62ad0b4ba046f2b907b67e2c866fa4'
-            + '/997/256/{z}/{x}/{y}.png',
-        { 'attribution' : attribution, 'maxZoom' : 18 }
-    ).addTo(ddg_spice_local_map);
-
-    var businesses = api_response.businesses;
-    for (var i in businesses) {
-        var location = [ businesses[i].location.coordinate.latitude,
-                         businesses[i].location.coordinate.longitude ]
-        if (i == 0) ddg_spice_local_map.setView(location, 13);
-        ddg_spice_local_markers.push(
-            L.marker(location, { 'title' : businesses[i].name, 'id' : i }
-            ).on('click', function(e) {
-                $('#ddgc_dots a')[e.target.options.id].click();
-            }).addTo(ddg_spice_local_map));
-    }
-    for (var i in ddg_spice_local_markers) 
-        ddg_spice_local_markers[i].options.zIndex =
-            $(ddg_spice_local_markers[i]._icon).css('z-index');
-};
+}
