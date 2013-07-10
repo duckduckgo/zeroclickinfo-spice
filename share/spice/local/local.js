58905374e0e67b95d636f3dfa4bbd36d7fafb6cd
Dylan Lloyd <dylan@dylansserver.com>
2013-07-10 15:02:45 -0400

    li ids, map scroll & marker highlight

--- share/spice/local/local.js
+++ share/spice/local/local.js
@@ -1,7 +1,11 @@
+var ddg_spice_local_map;
+var ddg_spice_local_markers = [];
+
 function ddg_spice_local(api_response) {
-    console.log(api_response);
-    foo = api_response;
     if (!api_response || api_response.length == 0) return;
+
+    for (var i in api_response.businesses)
+        api_response.businesses[i].index = i;
 
     Spice.render({
         //header1                  : ' (Local)',
@@ -21,14 +25,28 @@ function ddg_spice_local(api_response) {
     $.getScript("/dist/leaflet.js", function() { render_map(api_response) });
 
     //$("#ddgc_detail").prependTo("#local");
-    $('#local .ddgc_item').off().click(function() { $('#ddgc_detail').show() });
+    $('#local .ddgc_item').off().click(function(e) {
+        $('#ddgc_detail').show()
+        for (var i in ddg_spice_local_markers) {
+            if (i == e.target.id) {
+                ddg_spice_local_markers[i]
+                    ._icon.src = '/dist/images/marker-icon-green.png';
+            } else {
+                ddg_spice_local_markers[i]
+                    ._icon.src = '/dist/images/marker-icon.png';
+            }
+        }
+        ddg_spice_local_map.setView(
+            ddg_spice_local_markers[e.target.id].getLatLng(), 13,
+                { 'animate' : true });
+    });
     //$('#ddgc_nav').hide();
     $('#ddgc_detail').append($('<div>').attr('id', 'map'));
 };
 
 function render_map(api_response) {
     L.Icon.Default.imagePath = "/dist/images";
-    var map = L.map('map');
+    ddg_spice_local_map = L.map('map');
 
     var attribution = 'Map data &copy; '
         + '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
@@ -39,21 +57,19 @@ function render_map(api_response) {
         'http://{s}.tile.cloudmade.com/2f62ad0b4ba046f2b907b67e2c866fa4'
             + '/997/256/{z}/{x}/{y}.png',
         { 'attribution' : attribution, 'maxZoom' : 18 }
-    ).addTo(map);
+    ).addTo(ddg_spice_local_map);
 
     var businesses = api_response.businesses;
     for (var i in businesses) {
-        if (i == 0) map.setView([ businesses[i].location.coordinate.latitude,
-                            businesses[i].location.coordinate.longitude ], 13);
-        L.marker([ businesses[i].location.coordinate.latitude,
-                businesses[i].location.coordinate.longitude ],
-                { 'title' : businesses[i].name, 'id' : i }
-                ).on('click', function(e) {
-                    $('#map .leaflet-marker-icon').attr(
-                        'src', '/dist/images/marker-icon.png');
-                    e.target._icon.src = '/dist/images/marker-icon-green.png';
-                }).addTo(map);
+        var location = [ businesses[i].location.coordinate.latitude,
+                         businesses[i].location.coordinate.longitude ]
+        if (i == 0) ddg_spice_local_map.setView(location, 13);
+        ddg_spice_local_markers.push(
+            L.marker(location, { 'title' : businesses[i].name, 'id' : i }
+            ).on('click', function(e) {
+                $('#map .leaflet-marker-icon').attr(
+                    'src', '/dist/images/marker-icon.png');
+                e.target._icon.src = '/dist/images/marker-icon-green.png';
+            }).addTo(ddg_spice_local_map));
     }
-
-
 };
