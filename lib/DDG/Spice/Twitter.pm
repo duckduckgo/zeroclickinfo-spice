827f8455b51157041d771b0c2c16314c126ef1fe
Jag Talon <talon.jag@gmail.com>
2013-07-11 21:15:55 -0400

    Twitter: Used a regexp instead of a trigger word.

--- lib/DDG/Spice/Twitter.pm
+++ lib/DDG/Spice/Twitter.pm
@@ -5,10 +5,15 @@ use DDG::Spice;
 spice to => 'https://jagtalon.duckduckgo.com/tw.js?user=$1&callback={{callback}}&current=1';
 spice is_cached => 1;
 
-triggers startend => "twitter";
+triggers query => qr/^twitter\s([a-z0-9_]+)$|^@([a-z0-9_]+)$/i;
 
-handle remainder => sub { 
-    return $_ if $_;
+handle matches => sub { 
+    if($1) {
+        return $1;
+    } elsif($2) {
+        return $2;
+    }
+
     return;
 };
 
