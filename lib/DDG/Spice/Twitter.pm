fd7a4edeb538b7ad0df6570379a89bb66d1d7862
Jag Talon <talon.jag@gmail.com>
2013-07-15 20:05:47 -0400

    Twitter: Move off jagtalon.duckduckgo.com

--- lib/DDG/Spice/Twitter.pm
+++ lib/DDG/Spice/Twitter.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Twitter;
 
 use DDG::Spice;
 
-spice to => 'https://jagtalon.duckduckgo.com/tw.js?user=$1&callback={{callback}}&current=1';
+spice to => 'https://duckduckgo.com/tw.js?user=$1&callback={{callback}}&current=1';
 spice is_cached => 1;
 
 triggers query => qr/^(?:twitter\s)?@([a-z0-9_]+)$|^twitter\s([a-z0-9_]+)$/i;
