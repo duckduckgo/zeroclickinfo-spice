ba345488c63089b91405a4c40f87369f47579ef3
Ado Matejov <a.matejov@centrum.sk>
2013-07-20 00:24:28 +0200

    reversed =~

--- lib/DDG/Spice/Guidebox/Getid.pm
+++ lib/DDG/Spice/Guidebox/Getid.pm
@@ -23,13 +23,13 @@ spice wrap_jsonp_callback => 1;
 handle remainder => sub {
     if ($loc->country_name eq "United States"){
         my $tmp = '';
-        if ($_ ~= qr/^watch ([\w\s])+ online$/ ){
+        if ($_ =~ qr/^watch ([\w\s])+ online$/ ){
             $tmp = $1;
-        } elsif ($_ ~= qr/^watch episodes of ([\w\s])+ online$/){
+        } elsif ($_ =~ qr/^watch episodes of ([\w\s])+ online$/){
             $tmp = $1;
-        } elsif ($_ ~= qr/^watch ([\w\s])+ series$/){
+        } elsif ($_ =~ qr/^watch ([\w\s])+ series$/){
             $tmp = $1;
-        } elsif ($_ ~= qr/^watch ([\w\s])+ tv series$/){
+        } elsif ($_ =~ qr/^watch ([\w\s])+ tv series$/){
             $tmp = $1;
         } else {
             $tmp = $_;
