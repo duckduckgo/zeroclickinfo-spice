40ba2cfcfd7de98712793b4e146bbec84cd0355c
Ado Matejov <a.matejov@centrum.sk>
2013-07-19 23:48:07 +0200

    regex

--- lib/DDG/Spice/Guidebox/Getid.pm
+++ lib/DDG/Spice/Guidebox/Getid.pm
@@ -3,9 +3,9 @@ package DDG::Spice::Guidebox::Getid;
 use DDG::Spice;
 
 primary_example_queries "guidebox Castle";
-description "Search for full episodes of all your favorite TV shows (USA only)";
+description "Search for full free episodes of all your favorite TV shows (USA only)";
 name "Guidebox";
-code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Lastshows.pm";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Guidebox/Getid.pm";
 icon_url "/i/www.guidebox.com.ico";
 topics "everyday", "entertainment", "social";
 category "entertainment";
@@ -13,7 +13,7 @@ attribution github => ['https://github.com/adman','Adman'],
             twitter => ['http://twitter.com/adman_X','adman_X'];
 
 triggers startend => "guidebox";
-triggers start => "watch","full episodes of", "full free episodes of", "free episodes of";
+triggers start => "watch","full episodes of", "full free episodes of", "free episodes of", "episodes of";
 triggers any => "full episodes", "watch free", "full free episodes", "free episodes", "episodes", "recent episodes";
 
 spice to => 'http://api-public.guidebox.com/v1.3/json/rKu5Jv4vtj0T0qOQAhSYr8sUQWcBNmpS/search/all/title/$1';
@@ -22,7 +22,15 @@ spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
     if ($loc->country_name eq "United States"){
-        return $_ if $_;
+        my $tmp = '';
+        if ($_ ~= qr/^watch ([\w\s])+ online$/ ){
+            $tmp = $1;
+        } elsif ($_ ~= qr/^watch episodes of ([\w\s])+ online$/){
+            $tmp = $1;
+        } else {
+            $tmp = $_;
+        }
+        return $tmp if $tmp;
     }
     return;
 };
