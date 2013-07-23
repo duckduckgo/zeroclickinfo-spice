344c6e41b789dde01bc4a24cef3a1ff64cda0499
moollaza <moollaza@gmail.com>
2013-07-23 13:57:32 -0400

    Guidebox Spice - Switched to use DDG's API Key

--- lib/DDG/Spice/Guidebox/Getid.pm
+++ lib/DDG/Spice/Guidebox/Getid.pm
@@ -16,7 +16,7 @@ triggers startend => "guidebox";
 triggers start => "watch","full episodes of", "full free episodes of", "free episodes of", "episodes of";
 triggers any => "full episodes", "watch free", "full free episodes", "free episodes", "episodes", "recent episodes";
 
-spice to => 'http://api-public.guidebox.com/v1.3/json/rKu5Jv4vtj0T0qOQAhSYr8sUQWcBNmpS/search/all/title/$1';
+spice to => 'http://api-public.guidebox.com/v1.3/json/{{ENV{DDG_SPICE_GUIDEBOX_APIKEY}}}/search/all/title/$1';
 
 spice wrap_jsonp_callback => 1;
 
