961115b8fa48b99ebc6306a5a5cdaa4cf08b96cd
Dylan Lloyd <dylan@dylansserver.com>
2013-07-28 19:11:31 -0400

    use explicit callback

--- lib/DDG/Spice/Local.pm
+++ lib/DDG/Spice/Local.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Local;
 
 use DDG::Spice;
 
-spice to => 'https://dylan.duckduckgo.com/local.js?q=$1';
+spice to => 'https://dylan.duckduckgo.com/local.js?q=$1&cb=ddg_spice_local';
 
 triggers any => (
     'near',
