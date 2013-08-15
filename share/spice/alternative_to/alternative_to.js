d119a1383bced0119b86d0df23a9f7f1483562dd
russell <russell@duckduckgo.com>
2013-08-15 15:26:15 -0400

    alternative_to: single item handler sets image and header

--- share/spice/alternative_to/alternative_to.js
+++ share/spice/alternative_to/alternative_to.js
@@ -15,7 +15,11 @@ function ddg_spice_alternative_to(api_result) {
             items                : api_result.Items,
             template_item        : "alternative_to",
             template_detail      : "alternative_to_details",
-            li_height            : 60
+            li_height            : 60,
+            single_item_handler  : function(obj) {              // gets called in the event of a single result
+                obj.header1 = api_result.Items[0].Name;         // set the header
+                obj.image_url = api_result.Items[0].IconUrl;    // set the image
+            }
         },
     });
 }
