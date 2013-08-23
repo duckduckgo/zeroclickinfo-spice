c22338fa3764f45d1c2fa4dbcd77d5b0f8fc7f14
Jag Talon <talon.jag@gmail.com>
2013-08-23 12:05:03 -0400

    Video: Add "videos" to the trigger words.

--- lib/DDG/Spice/Video.pm
+++ lib/DDG/Spice/Video.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Video;
 use DDG::Spice;
 
 spice to => 'https://jagtalon.duckduckgo.com/video.js?q=$1&n=20&callback={{callback}}';
-triggers startend => "video";
+triggers startend => "video", "videos";
 spice is_cached => 0;
 
 handle remainder => sub {
