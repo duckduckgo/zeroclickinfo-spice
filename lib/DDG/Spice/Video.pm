8df9bb40d23c838af82b912b46e2537978296bdd
Jag Talon <talon.jag@gmail.com>
2013-09-03 13:39:12 -0400

    Video: Do not trigger with "khan" or "khan academy".
    We should skip queries which are in the form of "khan <trigger words>" and "khan academy <trigger words>" because we want the Khan Academy plugin to show us results instead. This change only checks if the remainder is "khan" or "khan academy," and that rejects queries such as "khan academy videos", but not "wrath of khan videos".

--- lib/DDG/Spice/Video.pm
+++ lib/DDG/Spice/Video.pm
@@ -7,6 +7,15 @@ triggers startend => 'video', 'videos', 'vimeo', 'youtube';
 spice is_cached => 0;
 
 handle remainder => sub {
+    # We have to skip queries that are of the form "khan <trigger word>" and "khan academy <trigger word>".
+    # We're going to skip these forms because we want the Khan Academy plugin to trigger instead of this one.
+    my @skip_array = ("khan", "khan academy");
+    my $skip_qr = join("|", @skip_array);
+
+    if($_ =~ /^($skip_qr)$/) {
+	return;
+    }
+
     return $_ if $_;
     return;
 };
