d859109d2bee77658e80730e62a4b7edf047c2a4
Jag Talon <talon.jag@gmail.com>
2013-09-05 17:26:45 -0400

    Video: Remove "vimeo" trigger word for now.

--- lib/DDG/Spice/Video.pm
+++ lib/DDG/Spice/Video.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Video;
 use DDG::Spice;
 
 spice to => 'https://duckduckgo.com/v.js?q=$1&n=20&callback={{callback}}';
-triggers startend => 'video', 'videos', 'vimeo', 'youtube';
+triggers startend => 'video', 'videos', 'youtube';
 spice is_cached => 0;
 
 handle remainder => sub {
