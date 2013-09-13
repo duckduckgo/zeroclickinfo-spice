e9fe547bb1e9f60d6f5daf3db4da6ea5e3ca4968
Jag Talon <talon.jag@gmail.com>
2013-09-12 22:33:30 -0400

    News: Change the "More at ..." link when flipping through.

--- share/spice/news/news.js
+++ share/spice/news/news.js
@@ -30,4 +30,38 @@ function ddg_spice_news(api_result) {
 	force_big_header: true,
 	force_no_fold: true
     });
+
+    var change_more = function(obj) {
+	var image = Handlebars.helpers.favicon.call({source_url: obj.url, forces: {}});
+	var more_at_link = $(".zero_click_more_at_link");
+	more_at_link.attr("href", obj.url);
+        more_at_link.find("img").attr("src", image);
+        more_at_link.find("span").html("More at " + obj.source);
+    }
+    
+    var length = api_result.length;
+    var i = 0;
+    $("#preva").click(function() {
+	if(i !== 0) {
+	    change_more(api_result[--i]);
+	}
+    });
+
+    $("#nexta").click(function() {
+	if(i < length - 1) {
+	    change_more(api_result[++i]);
+	}
+    });
+
+    $("#ddgc_dots").click(function() {
+	var a = $(this).find("a");
+	if(a.length > 0) { 
+	    a.each(function(index) {
+		if($(this).hasClass("ddgc_selected")) {
+		    i = index;
+		    change_more(api_result[i]);
+		}
+	    });
+	}
+    });
 }
