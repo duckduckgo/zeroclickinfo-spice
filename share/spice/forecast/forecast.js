544d461196fd87cb1fac781517990f56dd1b3f67
Adam Grossman <thegrossman@gmail.com>
2013-09-18 16:33:11 -0400

    Converting the forecast plugin to a proper spice2 plugin, plus code cleanup.

--- share/spice/forecast/forecast.js
+++ share/spice/forecast/forecast.js
@@ -1,9 +1,32 @@
-var StaticSkycons = function() {
-  var OBJ = {}
+function ddg_spice_forecast(r) {
   
-  OBJ.play = OBJ.pause = function(){}
+  // Exit if we've got a bad forecast
+  if(!r || !r.hourly || !r.hourly.data || !r.daily || !r.daily.data)
+    return
   
-  OBJ.set = function(elem_id, type) {
+  // Render the container elements
+  Spice.render({
+    data             : {},
+    header1          : r.flags['ddg-location'] ? 'Weather for '+r.flags['ddg-location'] : 'Weather',
+    source_url       : 'http://forecast.io/#/f/'+r.latitude+','+r.longitude,
+    source_name      : 'Forecast.io',
+    template_normal  : 'forecast',
+    force_big_header : true,
+    force_no_fold    : true,
+  })
+  
+  // Set up some stuff we'll need
+  var $container = $('#spice_forecast'),
+      units = (r.flags && r.flags.units) || 'us',
+      unit_labels = {
+        'us': {speed: 'mph', temperature: 'F'},
+        'si': {speed: 'm/s', temperature: 'C'},
+        'ca': {speed: 'km/h', temperature: 'C'},
+        'uk': {speed: 'mph', temperature: 'C'},
+      }
+  
+  // Skycons (static version of these: http://darkskyapp.github.io/skycons/)
+  var set_skycons = function(elem_id, type) {
     var $elem = $('#'+elem_id),
         $img = $('<img />').attr('id', $elem.attr('id'))
                            .attr('class', $elem.attr('class'))
@@ -16,143 +39,45 @@ var StaticSkycons = function() {
     $elem.replaceWith($img)
   }
   
-  return OBJ
-}
-
-StaticSkycons.RAIN = 'rain'
-StaticSkycons.SNOW = 'snow'
-StaticSkycons.SLEET = 'sleet'
-StaticSkycons.WIND = 'wind'
-StaticSkycons.FOG = 'fog'
-StaticSkycons.CLOUDY = 'cloudy'
-StaticSkycons.PARTLY_CLOUDY_DAY = 'partly_cloudy_day'
-StaticSkycons.PARTLY_CLOUDY_NIGHT = 'partly_cloudy_night'
-StaticSkycons.CLEAR_DAY = 'clear_day'
-StaticSkycons.CLEAR_NIGHT = 'clear_night'
-
-
-
-var ForecastEmbed = function(opts) {
-  
-  var OBJ = {},
-      $container,
-      loading_skycons, skycons
-  
-  
-  var initialize = function() {
-    
-    // Create container
-    $container = $('\
-      <div id="forecast_embed" class="fe_container"> \
-        <div class="fe_title" style="display:none"> \
-          <span class="fe_location"> \
-            <span></span> \
-          </span> \
-           \
-          <span class="fe_forecast_link"> \
-            More at <a target="_blank" href="http://forecast.io">Forecast.io</a> \
-          </span> \
-        </div> \
-         \
-        <div class="fe_forecast"> \
-          <div class="fe_currently"> \
-            <canvas id="fe_current_icon" width="160" height="160" style="width:80px; height:80px"></canvas> \
-            <div class="fe_temp"></div> \
-            <div class="fe_summary"></div> \
-            <div class="fe_wind"></div> \
-          </div> \
-           \
-          <div class="fe_daily"></div> \
-          <div style="clear:left"></div> \
-        </div> \
-         \
-        <div class="fe_alert" style="display:none"></div> \
-         \
-        <div class="fe_loading" style="display:none"> \
-          <canvas id="fe_loading_icon" width="100" height="100" style="width:50px; height:50px"></canvas> \
-          Loading... \
-        </div> \
-      </div> \
-    ')
-    
-    OBJ.elem = $container
-    
-    // Setup skycons
-    if(opts.static_skycons)
-      window.Skycons = StaticSkycons
-    
-    loading_skycons = new Skycons({color: opts.text_color || "#333"})
-    skycons = new Skycons({color: opts.text_color || "#333"})
-    
-    // Set header
-    if(opts.hide_header) {
-      $container.find('.fe_title').remove()
-    } else {
-      $container.find('.fe_title .fe_location span').html(opts.title)
-      $container.find('.fe_title').show()
-    }
-    
-    // Set optional font
-    if(opts.ff_name && opts.ff_url) {
-      var s = document.createElement('style')
-      s.type = "text/css"
-      document.getElementsByTagName('head')[0].appendChild(s)
-      var rule = 'font-family: '+opts.ff_name+'; src: url('+opts.ff_url+');'
-      if(s.styleSheet)
-        s.styleSheet.cssText = "@font-face {" + rule + "}"
-      else
-        s.innerHTML = "@font-face {" + rule + "}"
-    }
-    
-    if(opts.font || opts.ff_name)
-      $('body').css('font-family', opts.font || opts.ff_name)
-    
-    // Set text color
-    if(opts.text_color) {
-      $container.css('color', opts.text_color)
-      $container.find('a').css('color', opts.text_color)
-      $container.find('.fe_title').css('border-color', opts.text_color)
-      $container.find('.fe_alert a').css('color', opts.text_color)
-    }
+  var skycon_type = function(icon) {
+    if(icon === 'rain')
+      return 'rain'
+    else if(icon === 'snow')
+      return 'snow'
+    else if(icon === 'sleet')
+      return 'sleet'
+    else if(icon === 'hail')
+      return 'sleet'
+    else if(icon === 'wind')
+      return 'wind'
+    else if(icon === 'fog')
+      return 'fog'
+    else if(icon === 'cloudy')
+      return 'cloudy'
+    else if(icon === 'partly-cloudy-day')
+      return 'partly_cloudy_day'
+    else if(icon === 'partly-cloudy-night')
+      return 'partly_cloudy_night'
+    else if(icon === 'clear-day')
+      return 'clear_day'
+    else if(icon === 'clear-night')
+      return 'clear_night'
+    else
+      return 'cloudy'
   }
   
+  // Convert a wind bearing in degrees to a string
   var wind_bearing_to_str = function(bearing) {
     var wind_i = Math.round(bearing / 45)
     return ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'][wind_i]
   }
   
-  var skycon_type = function(icon) {
-    if(icon === 'rain')
-      return Skycons.RAIN
-    else if(icon === 'snow')
-      return Skycons.SNOW
-    else if(icon === 'sleet')
-      return Skycons.SLEET
-    else if(icon === 'hail')
-      return Skycons.SLEET
-    else if(icon === 'wind')
-      return Skycons.WIND
-    else if(icon === 'fog')
-      return Skycons.FOG
-    else if(icon === 'cloudy')
-      return Skycons.CLOUDY
-    else if(icon === 'partly-cloudy-day')
-      return Skycons.PARTLY_CLOUDY_DAY
-    else if(icon === 'partly-cloudy-night')
-      return Skycons.PARTLY_CLOUDY_NIGHT
-    else if(icon === 'clear-day')
-      return Skycons.CLEAR_DAY
-    else if(icon === 'clear-night')
-      return Skycons.CLEAR_NIGHT
-    
-    return Skycons.CLOUDY
-  }
-  
+  // Build the current conditions
   var build_currently = function(f) {
     var now = new Date().getTime() / 1000,
         hourly = f.hourly.data,
         current_summary = f.currently.summary,
-        speed_units = ForecastEmbed.unit_labels[opts.units || 'us'].speed
+        speed_units = unit_labels[units].speed
     
     // If the next-hour summary is interesting enough, use that instead
     if(f.minutely && !f.minutely.summary.match(/ for the hour\.$/))
@@ -167,11 +92,11 @@ var ForecastEmbed = function(opts) {
       break
     }
     
-    var temp_str = '<span class="fe_temp_str">'+Math.round(f.currently.temperature)+'&deg;</span><span class="fe_temp_unit">'+ForecastEmbed.unit_labels[opts.units || 'us'].temperature+'</span>'
+    var temp_str = '<span class="fe_temp_str">'+Math.round(f.currently.temperature)+'&deg;</span><span class="fe_temp_unit">'+unit_labels[units].temperature+'</span>'
     if(temp_direction > 0)
-      temp_str += ' <span class="fe_dir">and rising.</span>'
+      temp_str += ' <span class="fe_dir">and rising</span>'
     else if(temp_direction < 0)
-      temp_str += ' <span class="fe_dir">and falling.</span>'
+      temp_str += ' <span class="fe_dir">and falling</span>'
     
     if(f.currently.apparentTemperature)
       temp_str += ' <span class="fe_feelslike">Feels like '+Math.round(f.currently.apparentTemperature)+'&deg;'+'</span>'
@@ -191,11 +116,11 @@ var ForecastEmbed = function(opts) {
     }
     
     setTimeout(function() {
-      skycons.set('fe_current_icon', skycon_type(f.currently.icon))
+      set_skycons('fe_current_icon', skycon_type(f.currently.icon))
     }, 0)
   }
   
-  
+  // Build the list of days
   var build_daily = function(f) {
     $daily_container = $container.find('.fe_daily')
     $daily_container.empty()
@@ -243,21 +168,17 @@ var ForecastEmbed = function(opts) {
       
       $day.find('.fe_temp_bar').css({
         'height': temp_height,
-        'top': temp_top,
-        'background-color': opts.color || '#333'
+        'top': temp_top
       })
       
-      if(typeof FlashCanvas != 'undefined')
-        FlashCanvas.initElement($day.find('canvas')[0])
-      
       $day.find('.fe_icon').attr('id', 'fe_day_icon'+i)
-      setTimeout(function(){skycons.set('fe_day_icon'+i, skycon_type(days[i].icon))}, 0)
+      setTimeout(function(){set_skycons('fe_day_icon'+i, skycon_type(days[i].icon))}, 0)
       
       $day.appendTo($daily_container)
     })(i)
   }
   
-  
+  // Build any weather alerts or warnings
   var build_alerts = function(f) {
     $alert = $container.find('.fe_alert').empty()
     
@@ -273,57 +194,8 @@ var ForecastEmbed = function(opts) {
     $alert.show()
   }
   
-  
-  OBJ.loading = function(show) {
-    if(show) {
-      loading_skycons.set('fe_loading_icon', Skycons.PARTLY_CLOUDY_DAY)
-      loading_skycons.play()
-      $container.find('.fe_loading').show()
-    } else {
-      $container.find('.fe_loading').hide()
-      loading_skycons.pause()
-    }
-  }
-  
-  OBJ.build = function(f) {
-    $container.find('.fe_title .fe_forecast_link a').attr('href', 'http://forecast.io/#/f/'+f.latitude+','+f.longitude)
-    build_currently(f)
-    build_daily(f)
-    build_alerts(f)
-    skycons.play()
-  }
-  
-  
-  initialize()
-  
-  return OBJ
-}
-
-ForecastEmbed.unit_labels = {
-  'us': {speed: 'mph', temperature: 'F'},
-  'si': {speed: 'm/s', temperature: 'C'},
-  'ca': {speed: 'km/h', temperature: 'C'},
-  'uk': {speed: 'mph', temperature: 'C'},
-}
-
-// -----------------------
-
-function ddg_spice_forecast(r) {
-  if(!r || !r.hourly || !r.hourly.data || !r.daily || !r.daily.data)
-    return;
-  
-  var items = new Array(),
-      units = r.flags && r.flags.units,
-      embed = new ForecastEmbed({static_skycons: true, hide_header: true, units: units || 'auto'});
-  
-  embed.build(r);
-  
-  items[0] = new Array();
-  items[0]['a'] = embed.elem[0];
-  items[0]['h'] = r.flags['ddg-location'] ? 'Weather for '+r.flags['ddg-location'] : 'Weather';
-  items[0]['s'] = 'Forecast.io';
-  items[0]['u'] = 'http://forecast.io/f/#'+r.latitude+','+r.longitude;
-  items[0]['force_no_fold'] = 1;
-  
-  nra(items);
+  // Go!
+  build_currently(r)
+  build_daily(r)
+  build_alerts(r)
 }
