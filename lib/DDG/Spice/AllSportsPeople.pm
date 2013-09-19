a98a071276b41f9e2f35e022a20841bf8c5d3851
wqr786 <waqar@4acetech.com>
2013-09-19 15:21:03 +0500

    added triggers and meta data for plugin

--- lib/DDG/Spice/AllSportsPeople.pm
+++ lib/DDG/Spice/AllSportsPeople.pm
@@ -2,10 +2,21 @@ package DDG::Spice::AllSportsPeople;
 
 use DDG::Spice;
 
-triggers any => 'allsportspeople';
+primary_example_queries "tiger woods";
+secondary_example_queries "biography tiger woods";
+description "All Sports People Database with huge amounts of sports personalities and teams from all around the globe";
+name "All Sports People Database";
+source "AllSportsPeople website";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/AllSportsPeople.pm";
+topics "everyday", "entertainment", "geek", "special_interest", "social";
+category "reference";
+attribution github => ['https://github.com/4AceTechnologies','wqr786'],
+web => 'http://www.allsportspeople.com/';
+
+my @triggers = share("triggers.txt")->slurp;
+triggers startend => @triggers;
 
 spice to => 'http://www.allsportspeople.com/api/$1/{{callback}}/';
-#spice to => 'http://4acetechnologies.com/projects/devshh/duckduckgo/task.php?name=$1&callback={{callback}}';
 
 handle remainder => sub {
     return $_ if $_;
