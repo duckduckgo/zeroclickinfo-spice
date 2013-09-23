414ae2446759aeb635b5f79236d28e931c813045
moollaza <moollaza@gmail.com>
2013-09-23 12:04:17 -0400

    KhanAcademy - Fixed indentation

--- share/spice/khan_academy/khan_academy.js
+++ share/spice/khan_academy/khan_academy.js
@@ -6,7 +6,7 @@ function ddg_spice_khan_academy ( api_result ) {
     var query = DDG.get_query().replace(/khan(\sacademy)?|videos?/g, "").trim();
     var header;
 
-    if(!query) {
+    if (!query) {
         header = "Khan Academy";
     } else {
         header = query + " (Khan Academy)";
@@ -26,59 +26,59 @@ function ddg_spice_khan_academy ( api_result ) {
             items           : api_result.feed.entry,
             template_detail : "khan_academy_detail",
             li_width : 120,
-	    single_item_handler: function(obj) {
-		// This disables autoplay.
-		obj.data.feed.entry[0].single = true;
-	    }
-        },
-	item_callback: function() {
-	    resizeDetail();
-	}
+            single_item_handler: function(obj) {
+                // This disables autoplay.
+                obj.data.feed.entry[0].single = true;
+                }
+            },
+        item_callback: function() {
+            resizeDetail();
+        }
     });
 
     function resizeDetail() {
         var $khan = $("#spice_khan_academy");
         var width = $khan.width() - 14;    //need 7px of padding on each side
         var height = Math.floor(width * 0.5625) + 30;    //30px for player menu
-	$("#spice_khan_academy #video-embed").width(width);
+        $("#spice_khan_academy #video-embed").width(width);
         $("#spice_khan_academy  #video-embed").height(height);
     };
 
     $(document).ready(function() {
         // Maintain 16/9 aspect ratio
-	resizeDetail();
+        resizeDetail();
         $(window).resize(resizeDetail);
     });
 }
 
-Handlebars.registerHelper("embedURL", function(single) {
+Handlebars.registerHelper ("embedURL", function(single) {
     var youtube_params = {
-	autoplay: 1,
-	wmode: "opaque",
-	iv_load_policy: 3,
-	autohide: 1,
-	version: 3
+        autoplay: 1,
+        wmode: "opaque",
+        iv_load_policy: 3,
+        autohide: 1,
+        version: 3
     };
 
     // Disable autoplay when single is not undefined.
-    if(single) {
-	youtube_params.autoplay = 0;
+    if (single) {
+        youtube_params.autoplay = 0;
     }
 
     // Concatenate all the parameters here.
     function parameters() {
-	var result = [];
-	for(o in youtube_params) {
-	    result.push(o + "=" + youtube_params[o]);
-	}
-	return result.join("&");
+        var result = [];
+        for(o in youtube_params) {
+            result.push(o + "=" + youtube_params[o]);
+        }
+        return result.join("&");
     }
 
     return parameters();
 });
 
 // forms the url for a khan_academy product image
-Handlebars.registerHelper('video_id', function() {
+Handlebars.registerHelper ('video_id', function() {
     var video_id = this.id.$t.split(":").pop();
     return video_id;
 });
