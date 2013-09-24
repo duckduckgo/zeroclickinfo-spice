c7924257063fb0cc315d42ccfe265118f4391cfe
Mohit Aggarwal <mohitagg@gmail.com>
2013-09-24 00:41:20 -0700

    change the idreambooks spice to the new front end

--- lib/DDG/Spice/Book.pm
+++ lib/DDG/Spice/Book.pm
@@ -2,9 +2,18 @@ package DDG::Spice::Book;
 
 use DDG::Spice;
 
-spice to => 'http://idreambooks.com/newbooks/api.json?api_key=duckduckgo&q=$1&callback={{callback}}';
+primary_example_queries "critic reviews moonwalking with einstein";
+description "Shows critic reviews of a book from publications like NY Times, NPR, Guardian.";
+name "iDreambooks";
 
-triggers startend => "idreambooks","critic rating", "critic reviews";
+spice to => 'http://idreambooks.com/newbooks/api.json?api_key=6ddd49633a8809eadb42a28f160bb25a1f24254d&q=$1';
+#spice to => 'http://localhost:3000/newbooks/api.json?api_key=&q=$1';
+
+triggers startend => "idreambooks", "critic reviews", "critic review", "book reviews", "book review";
+
+spice wrap_jsonp_callback => 1;
+
+spice is_cached => 0;
 
 handle remainder => sub {
     return $_;
