333a36d4fba3b079aef5264dda4f580ed016f9f0
Dylan Lloyd <dylan@dylansserver.com>
2013-09-30 06:17:08 -0400

    get all organizing bodies w/in all sports

--- share/spice/espn/fetch_sports.pl
+++ share/spice/espn/fetch_sports.pl
@@ -7,9 +7,17 @@ use JSON;
 use LWP::Simple;
 use Data::Dumper;
 
-my $api = 'http://api.espn.com/v1/sports?'
-        . "apikey=$ENV{DDG_SPICE_ESPN_APIKEY}";
+my ( $api, $key ) = (
+    'http://api.espn.com/v1/sports',
+    "$ENV{DDG_SPICE_ESPN_APIKEY}"
+);
 
-my $json = decode_json get $api;
+my ( $sports, $organizers ) #, $divisions ) =
+    decode_json get "$api?apikey=$key";
 
-map { print "$_->{name}\n" } @{$json->{sports}};
+map {
+    print "$_->{name} => (\n";
+    $organizers = decode_json get "$api/$_->{name}?apikey=$key";
+    map { print "\t$_->{abbreviation}\n" } @{$organizers->{sports}[0]{leagues}};
+    print ");\n";
+} @{$sports->{sports}};
