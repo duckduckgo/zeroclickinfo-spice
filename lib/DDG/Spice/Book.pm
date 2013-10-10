915b3bdf4bee96e3c9f719bfb50577a031f68f46
Mohit Aggarwal <mohitagg@gmail.com>
2013-10-09 23:25:26 -0700

    changed to two pane layout with more rating displayed

--- lib/DDG/Spice/Book.pm
+++ lib/DDG/Spice/Book.pm
@@ -2,19 +2,17 @@ package DDG::Spice::Book;
 
 use DDG::Spice;
 
-primary_example_queries "critic reviews moonwalking with einstein";
-description "Shows critic reviews of a book from publications like NY Times, NPR, Guardian.";
+primary_example_queries "book reviews moonwalking with einstein";
+description "Shows critic rating and a sample review of a book from publications like NY Times, NPR, Guardian.";
 name "iDreambooks";
 
-spice to => 'http://idreambooks.com/newbooks/api.json?api_key=6ddd49633a8809eadb42a28f160bb25a1f24254d&q=$1';
-#spice to => 'http://localhost:3000/newbooks/api.json?api_key=&q=$1';
-
+spice to => 'http://idreambooks.com/newbooks/api.json?api_key={{ENV{DDG_SPICE_BOOK_APIKEY}}}&q=$1';
 
 triggers startend => "idreambooks", "book reviews", "book review";
 
 spice wrap_jsonp_callback => 1;
 
-spice is_cached => 1;
+spice is_cached => 0;
 
 handle remainder => sub {
     return $_ if $_;
--- share/spice/book/book.js
+++ share/spice/book/book.js
@@ -1,26 +1,44 @@
 function ddg_spice_book(api_result) {
      
+    // return if no book is returned or if the returned book has no reviews
     if  (api_result == null || api_result.total_results < 1 || api_result.book.critic_reviews.length == 0) return
-    var header = "Critic Reviews of " + api_result.book.title;
-    if(api_result.book.author && (api_result.book.author.length > 0) ) {header += (" by " + api_result.book.author)};
-    
-    var data =  api_result.book.critic_reviews[0];
-    
-     
-
+      
+    // assign the book object to data
+    var data =  api_result.book;
    
-    var d = new Date(data.review_date);
-    if (d && !isNaN( d.getTime() ) ){
-        var m_names = ["Jan", "Feb", "Mar",  "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        var curr_date = d.getUTCDate();
-        var curr_month = d.getUTCMonth();
-        var curr_year = d.getUTCFullYear();
-        data.review_date = ( m_names[curr_month] + " " + curr_date + ", " + curr_year);
-    }else{
-        data.review_date = "";
+    // function to convert date from 2012-07-20 format to Jul 20, 2012
+    var prettyDate = function(date) {
+        var d = new Date(date);
+        if (d && !isNaN(d.getTime()) ){
+            var m_names = ["Jan", "Feb", "Mar",  "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            var curr_date = d.getUTCDate();
+            var curr_month = d.getUTCMonth();
+            var curr_year = d.getUTCFullYear();
+            return ( m_names[curr_month] + " " + curr_date + ", " + curr_year);
+        }else{
+            return null;
+        };
     };
-
     
+    // convert pulication date of reviews to pretty format
+    for (var i = 0; i < data.critic_reviews.length; i++) {
+        data.critic_reviews[i].review_date = prettyDate(data.critic_reviews[i].review_date);
+    }
+    
+    // convert book publication date to pretty format
+    data.release_date = prettyDate(data.release_date);
+
+    // get only the year of release date for header 
+    data.release_year = ( data.release_date || "" ).match(/\d{4}$/);
+
+    // pick a random critic review out of all the reviews returned
+    data.critic_review = data.critic_reviews[Math.floor(Math.random() * data.critic_reviews.length)];
+
+    var header = "Critic Reviews for " + api_result.book.title;
+    // add year of release to header
+    if (data.release_year){
+        header += " (" + data.release_year + ")"
+    }
 
     Spice.render({
          data              : data,
