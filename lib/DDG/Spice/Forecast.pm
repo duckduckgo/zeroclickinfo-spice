11069b94ef3a5209dc7e5ea2d5f7506961f91538
Caine Tighe <caine@duckduckgo.com>
2013-10-10 09:17:06 -0400

    DDG::Spice::Forecast: disable caching again since attribute behavior isn't explicitly defined yet.

--- lib/DDG/Spice/Forecast.pm
+++ lib/DDG/Spice/Forecast.pm
@@ -17,25 +17,20 @@ triggers any => @triggers;
 
 spice to => 'http://forecast.io/ddg?apikey={{ENV{DDG_SPICE_FORECAST_APIKEY}}}&q=$1&callback={{callback}}';
 
-# DDG cache by default and cache upstreams responses with return code 200 for 30 minutes
-spice is_cached => 1;
+# DDG cache off by default and cache API responses
+# with return code 200 for 30 minutes
+spice is_cached => 0;
 spice proxy_cache_valid   => "200 30m";
 
 handle query_lc => sub {
-    my $query = $_;
+    my $location;
 
-    # Don't cache generic queries due to
-    # variations in the users location.
-    if (grep {$query eq $_} @triggers) {
-	spice is_cached => 0;
-    }
-
-    my $location = '';
-    
+    # Capture user defined location if it exists.
     if (/^(?:what(?:'s| is) the |)(?:(?:weather|temp(?:erature|)) (?:fore?cast |report |today |tomm?orr?ow |this week |))+(?:in |for |at |)(.*)/) {
         $location = $1 unless ($1 =~ /fore?cast|report|weather|temp(erature)/);
     }
 
+    # Infer location if not explicitly in the query.
     $location = $loc->loc_str unless ($location);
 
     return $location;
