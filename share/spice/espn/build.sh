17948a4bc409357497e4f0c7cd52560daa5b482e
Dylan Lloyd <dylan@dylansserver.com>
2013-10-11 00:55:09 -0400

    move css to espn/ & add to build

--- share/spice/espn/build.sh
+++ share/spice/espn/build.sh
@@ -4,4 +4,8 @@ for dir in $(find -type d | tail -n+2);
 do
     echo "cp ./espn.js $dir/espn_${dir#./}.js";
     cp ./espn.js "$dir/espn_${dir#./}.js";
+
+    echo "cp ./espn.css $dir/espn_${dir#./}.css";
+    cp ./espn.css "$dir/espn_${dir#./}.css";
+
 done;
