dcf1cd5a940c71bcb7a08c22a0469e09fe0edd52
Caine Tighe <caine@duckduckgo.com>
2013-10-10 20:01:15 -0400

    Amazon: add ISBN trigger and extraction.

--- lib/DDG/Spice/Amazon.pm
+++ lib/DDG/Spice/Amazon.pm
@@ -4,9 +4,13 @@ use DDG::Spice;
 
 spice to => 'https://duckduckgo.com/m.js?q=$1&cb=ddg_spice_amazon';
 
-triggers any => 'amazon';
+triggers any => 'amazon', 'isbn';
 
 handle remainder => sub {
+    # ISBN extraction
+    $_ =~ qr/^(?:isbn |)(\d+)(?: isbn|)$/;
+    $_ = $1 if $1;
+
     return "$_" if $_;
 };
 
