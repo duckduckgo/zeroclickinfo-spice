3985b95e8c83ff573194be080c46458fe0ee67fc
Dylan Lloyd <dylan@dylansserver.com>
2013-10-11 05:22:58 -0400

    use shared js file instead of copying

--- share/spice/espn/build.sh
+++ share/spice/espn/build.sh
@@ -2,8 +2,8 @@
 
 for dir in $(find -type d | tail -n+2);
 do
-    echo "cp ./espn.js $dir/espn_${dir#./}.js";
-    cp ./espn.js "$dir/espn_${dir#./}.js";
+    echo "echo '' > $dir/espn_${dir#./}.js";
+    echo '' > "$dir/espn_${dir#./}.js";
 
     echo "cp ./espn.css $dir/espn_${dir#./}.css";
     cp ./espn.css "$dir/espn_${dir#./}.css";
