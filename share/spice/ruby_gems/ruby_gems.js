7c5b6885f91359ead70e2888fb17c06e86f86c8e
Zaahir Moolla <moollaza@fastmail.fm>
2013-10-11 15:23:57 -0400

    RubyGems - Converted to use list template

--- share/spice/ruby_gems/ruby_gems.js
+++ share/spice/ruby_gems/ruby_gems.js
@@ -1,15 +1,23 @@
-function ddg_spice_ruby_gems(response) {
-    if (response.length == 0)
-        return;
+function ddg_spice_ruby_gems(api_result) {
+    
+    if (api_result.length === 0) return;
 
-    var query = DDG.get_query();
+    var query = DDG.get_query()
     query = query.replace(/\s*(?:(?:ruby(?:gems?)?)|(?:gems?))\s*/i, '');
+
     Spice.render({
-        data             : response,
+        data             : api_result,
         header1          : query + " (RubyGems)",
         source_url       : 'http://rubygems.org/search?utf8=%E2%9C%93&query=' + encodeURIComponent(query),
         source_name      : 'RubyGems',
-        template_normal  : 'ruby_gems',
+        spice_name       : 'ruby_gems',
+        template_frame   : 'list',
+        template_options : {
+            items: api_result,
+            show: 5,
+            template_item: 'ruby_gems'
+        },
+        force_no_fold    : true,
         force_big_header : true
     });
 }
