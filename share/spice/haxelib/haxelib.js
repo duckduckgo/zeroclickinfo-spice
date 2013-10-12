8cee138e8b7fe2eeb1c78fafb2e36e9f56e325f5
Tom Bebbington <tophattedcoder@gmail.com>
2013-10-12 23:15:37 +0100

    Improved layout

--- share/spice/haxelib/haxelib.js
+++ share/spice/haxelib/haxelib.js
@@ -4,7 +4,7 @@ function ddg_spice_haxelib(response) {
     var query = DDG.get_query().replace(/(haxelib|haxe lib|haxe library)/, "");
     Spice.render({
         data             : response.info,
-        header1          : response.info.name + " (Haxelib) by " + response.info.owner,
+        header1          : response.info.name + " ("+response.info.curversion+") by " + response.info.owner,
         source_url       : 'http://lib.haxe.org/p/' + encodeURIComponent(response.info.name),
         source_name      : 'Haxelib',
         spice_name       : 'haxelib',
