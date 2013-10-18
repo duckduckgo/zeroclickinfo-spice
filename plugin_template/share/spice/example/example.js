b165347eca35d992a745f38259b94f327e552e8d
Supreet Sethi <supreet.sethi@gmail.com>
2013-10-18 17:33:55 +0530

    Spice plugin template which could be used with duckpan new command. Duckpan new command with generate a boilerplate code for plugin writers.

---
+++ plugin_template/share/spice/example/example.js
@@ -0,0 +1,6 @@
+function ddg_spice_$plugin_name (api_result) {
+    "use strict";
+    // Display the Spice plugin.
+    
+};
+
---
+++ plugin_template/t/Example.t
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use DDG::Test::Spice;
+
+
+done_testing;
