e158d669b79bf1015e166f20885bee4555b7983a
Tom Bebbington <tophattedcoder@gmail.com>
2013-10-26 19:19:07 +0100

    Revert "..."
    This reverts commit a555ef3429397e61ca57cf909f79d90ec4074967.

--- lib/DDG/Spice/GameInfo.pm
+++ lib/DDG/Spice/GameInfo.pm
@@ -1,17 +1,17 @@
 package DDG::Spice::GameInfo;
 
 use DDG::Spice;
-primary_example_queries "ocarina of time game";
+primary_example_queries "homesick game";
 description "See information about a game";
-name "Game Information";
-icon_url "/i/giantbomb.com/favicon.ico";
-source "www.giantbomb.com";
+name "GameInfo";
+icon_url "/i/thefreegamesdb.com/favicon.ico";
+source "thefreegamesdb.com & thegamesdb.net";
 topics "gaming";
 category "reference";
-attribution github => ['https://github.com/tophattedcoder','tophattedcoder'];
+attribution github => ['https://github.com/thomaspreece10','thomaspreece10'];
 
 triggers startend => "game","games";
-spice to => 'http://www.giantbomb.com/api/search/?api_key={{ENV{DDG_SPICE_GAMEBOMB_APIKEY}}}&format=json&query=$1&limit=1';
+spice to => 'http://tfgdb.com/API/DuckDuckGo.php?Name=$1';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
--- share/spice/game_info/game_info.js
+++ share/spice/game_info/game_info.js
@@ -1,42 +1,13 @@
-var ddg_spice_game_info_date_meta = {
-    day: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    month: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-}
 function ddg_spice_game_info(api_result) {
-    if (api_result == null || api_result.error != 'OK') return;
-    var main = api_result.results[0];
-    if(main == null || !DDG.isRelevant(main.name, ["game", "games"]))
-        return;
-    console.log(main.description);
+    if (api_result == null || api_result['status'] != 'OK') return;
+
     Spice.render({
-        data                     : main,
-        image_url                : main.image.small_url,
-        header1                  : main.name + ' (Games)',
-        source_url               : main.site_detail_url,
-        source_name              : "GameBomb",
+        data                     : api_result,
+        image_url                : api_result.image,
+        header1                  : api_result.name + ' (Games)',
+        source_url               : api_result.link,
+        source_name              : api_result.domain,
         template_normal          : 'game_info',
-        force_big_header         : true
+        force_no_favicon         : true
     });
 }
-Handlebars.registerHelper("release", function() {
-    "use strict";
-    var meta = ddg_spice_game_info_date_meta;
-    if(this.expected_release_day && this.expected_release_month && this.expected_release_year) {
-        var release = new Date(this.expected_release_year, this.expected_release_month, this.expected_release_day);
-        return "<br>"+(release < Date.now() ? "Released":"Releasing on") + " " + meta.day[release.getDay()] + " " + release.getDate() + " " + meta.month[release.getMonth()] + " " + release.getFullYear();
-    } else {
-        return "";
-    }
-});
-Handlebars.registerHelper("platforms", function() {
-    "use strict";
-    if(this.platforms.length == 0)
-        return "Not currently available";
-    var buf = "", max = this.platforms.length;
-    for(var i=0;i<max;i++) {
-        var curr = this.platforms[i];
-        buf += "<a href=\""+curr.site_detail_url+"\">"+curr.name+"</a>";
-        buf += i == max - 1 ? "" : (i == max - 2 ? " and " : ", ");
-    }
-    return "Available on "+buf;
-});
