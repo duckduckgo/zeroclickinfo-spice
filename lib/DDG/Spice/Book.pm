473022cb2569c7baa0f53e6430debb83347cff80
Jag Talon <talon.jag@gmail.com>
2013-10-29 15:18:28 -0400

    Books: Added the authors and subtitles in the relevancy.
    Queries such as "the shining by stephen king" or "Still Writing: The Pleasures and Perils of a Creative Life" should work.

--- lib/DDG/Spice/Book.pm
+++ lib/DDG/Spice/Book.pm
@@ -11,11 +11,11 @@ spice to => 'http://idreambooks.com/api/books/reviews.json?api_key={{ENV{DDG_SPI
 triggers startend => "idreambooks", "book reviews", "book review";
 
 spice wrap_jsonp_callback => 1;
-
 spice is_cached => 1;
 
 handle remainder => sub {
     return $_ if $_;
+    return;
 };
 
 1;
--- share/spice/book/book.js
+++ share/spice/book/book.js
@@ -4,17 +4,27 @@ function ddg_spice_book(api_result) {
 	return;
     }
 
+    // Get the query without the trigger words.
     var script = $('[src*="/js/spice/book/"]')[0];
     var source = $(script).attr("src");
     var query = source.match(/book\/([^\/]+)/)[1];
+    var decoded_query = decodeURIComponent(query);
+
+    api_result.books.splice(5);
 
     // Select the book to display from the returned books
     // Current logic is to select first book with critic reviews
     var data;
     for (var i = 0; i < api_result.books.length; i++) {
-        console.log(api_result.books[i].title, api_result.books[i].review_count, decodeURIComponent(query));
+	var title = api_result.books[i].title || "";
+	var sub_title = api_result.books[i].sub_title || "";
+	var author = api_result.books[i].author || "";
+
 	if((api_result.books[i].review_count || api_result.books[i].unrated_review_count) && 
-	    DDG.stringsRelevant(api_result.books[i].title, decodeURIComponent(query), [], 3, true)) {
+	   (DDG.stringsRelevant(title + " " + sub_title, decoded_query, [], 3, true) ||
+	    DDG.stringsRelevant(title, decoded_query, [], 3, true) ||
+	    DDG.stringsRelevant(title + " " + author, decoded_query, [], 3, true) ||
+	    DDG.stringsRelevant(title + " " + sub_title + " " + author, decoded_query, [], 3, true))) {
             data = api_result.books[i];
 	    break;
         }
@@ -24,36 +34,19 @@ function ddg_spice_book(api_result) {
 	return;
     }
 
+    // If the item that we got doesn't have the critic_reviews array,
+    // we copy over the unrated_reviews. This makes it easier to use the templates because 
+    // we're only using one variable.
     if(data.review_count === 0) {
 	data.critic_reviews = data.unrated_critic_reviews;
 	data.review_count = data.unrated_review_count;
     }
-
-    // Function to convert date from 2012-07-20 format to Jul 20, 2012
-    var prettyDate = function(date) {
-        var d = new Date(date);
-        if (d && !isNaN(d.getTime())) {
-            var m_names = ["Jan", "Feb", "Mar",  "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-            var curr_date = d.getUTCDate();
-            var curr_month = d.getUTCMonth();
-            var curr_year = d.getUTCFullYear();
-            return m_names[curr_month] + " " + curr_date + ", " + curr_year;
-        } else {
-            return null;
-        };
-    };
     
-    // Convert book publication date to pretty format
-    data.release_date = prettyDate(data.release_date);
-
     // Get only the year of release date for header 
-    data.release_year = (data.release_date || "").match(/\d{4}$/);
+    data.release_year = (data.release_date || "").match(/^\d{4}/);
 
     // Pick a random critic review out of all the reviews returned
     data.critic_review = data.critic_reviews[Math.floor(Math.random() * data.critic_reviews.length)];
-
-    // Convert pulication date of reviews to pretty format
-    data.critic_review.review_date = prettyDate(data.critic_review.review_date);
 
     var header = data.title;
     // Add year of release to header
@@ -80,3 +73,14 @@ function ddg_spice_book(api_result) {
 	 force_no_fold     : true
     });
 }
+
+Handlebars.registerHelper("prettyDate", function(date) {
+    var d = new Date(date);
+    if (d && !isNaN(d.getTime())) {
+        var m_names = ["Jan", "Feb", "Mar",  "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var curr_date = d.getUTCDate();
+        var curr_month = d.getUTCMonth();
+        var curr_year = d.getUTCFullYear();
+        return m_names[curr_month] + " " + curr_date + ", " + curr_year;
+    }
+});
