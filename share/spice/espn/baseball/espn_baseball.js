0a31a7e3040b1ade6eef7562f8e9d84808b40653
Caine Tighe <caine@duckduckgo.com>
2013-11-11 14:36:38 -0500

    ESPN: NBA year offset + 1.

--- share/spice/espn/baseball/espn_baseball.js
+++ share/spice/espn/baseball/espn_baseball.js
@@ -171,11 +171,15 @@ function ddg_spice_espn_team(api_result) {
 function ddg_spice_espn_events(api_result) {
     var events    = api_result.sports[0].leagues[0].events;
     ddg_spice_espn_player.events = [];
+    console.log(events);
 
     for (var i = events.length - 1; i > 0 && ddg_spice_espn_player.events.length < 5; i--) {
         var eventDate = new Date(events[i].date);
-        if (eventDate.getTime() > new Date().getTime() - 24*60*60*1000)
+	console.log(eventDate);
+        if (eventDate.getTime() > new Date().getTime() - 24*60*60*1000) {
+	    console.log('skipping event on ', eventDate);
             continue;
+	}
         eventDate.setMonth(eventDate.getMonth()+1);
 
         if (!events[i].competitions[0]) continue;
