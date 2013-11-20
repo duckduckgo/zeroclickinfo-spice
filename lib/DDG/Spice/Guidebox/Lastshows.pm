d472c306e99259cdfd2110b361abc29d589d5864
Zaahir Moolla <moollaza@fastmail.fm>
2013-11-20 16:29:09 -0500

    Guidebox - Removed faked trigger, no longer needed

--- lib/DDG/Spice/Guidebox/Lastshows.pm
+++ lib/DDG/Spice/Guidebox/Lastshows.pm
@@ -2,19 +2,6 @@ package DDG::Spice::Guidebox::Lastshows;
 
 use DDG::Spice;
 
-triggers any => "///***never_trigger***///";
-
 spice to => 'http://api-public.guidebox.com/v1.3/json/{{ENV{DDG_SPICE_GUIDEBOX_APIKEY}}}/$1/watch/all/20';
-
-spice wrap_jsonp_callback => 1;
-
-handle remainder => sub {
-    
-    # TODO
-    # Detect if iOS/Android and change api call
-    # ie. /best_available/ios/ or /best_available/android/
-    return  $_ if $_;
-    return;
-};
 
 1;
