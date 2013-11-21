7d594503ac3ad0d5cbb1ae445c028993b704148c
Zaahir Moolla <moollaza@fastmail.fm>
2013-11-21 14:57:11 -0500

    Dictionary - Clean up extra .pm files (only used for api endpoints)

--- lib/DDG/Spice/Dictionary/Hyphenation.pm
+++ lib/DDG/Spice/Dictionary/Hyphenation.pm
@@ -5,12 +5,6 @@ use DDG::Spice;
 attribution web => ['http://duckduckgo.com', 'DuckDuckGo'],
             twitter => ['http://twitter.com/duckduckgo', '@duckduckgo'];
 
-spice to => 'http://api.wordnik.com/v4/word.json/$1/hyphenation?includeRelated=true&api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}&callback={{callback}}';
-triggers startend => "///***never trigger***///";
-
-handle query_lc => sub {
-    return $_ if $_;
-    return;
-};
+spice to => 'http://api.wordnik.com/v4/word.json/$1/hyphenation?includeRelated=true&api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}';
 
 1;
