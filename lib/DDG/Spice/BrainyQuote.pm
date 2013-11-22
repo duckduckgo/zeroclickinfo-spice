a54ad64e755227bb80b420c0b66dd83848ebc376
Dax the Duck <dax@duckduckhack.(none)>
2013-11-22 17:59:37 -0500

    Additional comments, changed to point to production server.

--- lib/DDG/Spice/BrainyQuote.pm
+++ lib/DDG/Spice/BrainyQuote.pm
@@ -3,9 +3,43 @@ package DDG::Spice::BrainyQuote;
 
 use DDG::Spice;
 
+# The BrainyQuote Spice: Instant answers for famous quotations
+#
+# This spice will handle these type queries:
+#    By Name of person:
+#       "Abe Lincoln quotes"
+#       "Abraham Lincoln quotations"
+#       "JFK quotes"
+#       "quotes by John Kennedy"
+#    By Topic (quotes about a particular subject)
+#       "love quotes"   
+#       "funny quotes"
+#       "quote leadership"
+#    By Keyword (typically a single word in a quote, sometimes a short phrase)
+#       "purple quotes"
+#       "to be or not to be quotes"
+#       "chicago quotes"
+#
+#     No "full text" searching
+#     ------------------------
+#     Note that in keeping with the DuckDuckGo idea of only returning an exact answer,
+#     we don't do full text searching within the quote. 
+#     Therefore, given this Einstein quote:
+#        "Only two things are infinite, the universe and human stupidity, and I'm
+#         not sure about the former."
+# 
+#     No results will be returned if you search for:
+#            "Only two things are infinite quote"
+#     
+#     Ignored Searches
+#     ----------------
+#     The server side code will ignore certain queries in case people are clearly searching
+#     on something else, for example, searches for "stock quotes", "insurance quotes",
+#     "mutual fund quotes", "bond quotes", etc.
+#
 triggers startend => 'quote', 'quotes', 'quotations';
 
-spice to => 'http://dev.brainyquote.com/api/ddg?q=$1';
+spice to => 'http://www.brainyquote.com/api/ddg?q=$1';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
