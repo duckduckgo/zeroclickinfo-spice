26c5faf1a560302592f6b0ba6fa75155a058c7e4
Tom Bebbington <tophattedcoder@gmail.com>
2013-12-01 21:35:00 +0000

    Uses JSONP; Remove unneccessary setting; Fix regex; Use DDG methods in JS

--- lib/DDG/Spice/GameReleaseDate.pm
+++ lib/DDG/Spice/GameReleaseDate.pm
@@ -12,19 +12,15 @@ topics "entertainment", "everyday";
 category "entertainment";
 attribution github => ['https://github.com/tophattedcoder','Tom Bebbington'];
 
-spice to => 'http://www.giantbomb.com/api/search/?field_list=name,image,site_detail_url,original_release_date,aliases&api_key={{ENV{DDG_SPICE_GIANTBOMB_APIKEY}}}&format=json&query=$1&limit=1&callback={{callback}}&resources=game';
+spice to => 'http://www.giantbomb.com/api/search/?field_list=name,site_detail_url,original_release_date,aliases,number_of_user_reviews&api_key={{ENV{DDG_SPICE_GIANTBOMB_APIKEY}}}&format=jsonp&query=$1&limit=1&json_callback={{callback}}&resources=game';
 
-spice wrap_jsonp_callback => 1;
-
-spice is_cached => 1;
-
-triggers any => 'release', 'air', 'premiere', 'come out';
+triggers any => 'release', 'come out';
 
 handle remainder => sub {
-	s/ ?(for|of) ?//;
-	s/ ?date ?//;
-	s/ ?(will|did|does) ?//;
-	s/ ?when ?//;
+	s/^when //;
+	s/(?:^| )(did|does|will)(?: |$)//;
+	s/(?:^| )(for|of)(?: |$)//;
+	s/(?:^| )date(?: |$)//;
     return $_ if $_;
     return;
 };
--- share/spice/game_release_date/game_release_date.js
+++ share/spice/game_release_date/game_release_date.js
@@ -1,23 +1,21 @@
 function ddg_spice_game_release_date (api_result) {
     "use strict";
-    if(api_result.error != "OK" || !api_result.results || api_result.results.length === 0)
+    if(api_result == null || api_result.error != "OK" || !api_result.results || api_result.results.length === 0)
         return;
-    var data = null;
     var ignore = ["release", "date", "premiere", "of", "for", "when", "will", "did", "come", "out", "air", "game"];
-    for(var i=0; i < api_result.results.length; i++) {
-        var curr = api_result.results[i];
-        if(curr.original_release_date && DDG.isRelevant(curr.name+"\n"+(curr.aliases||""), ignore)) {
-            data = curr;
-            break;
-        }
-    }
-    if(data == null)
+    var games = api_result.results;
+    games.filter(function(game) {
+		return game.original_release_date !== null && DDG.isRelevant(game.name+" "+(game.aliases||""), ignore);
+	});
+	var game = DDG_bestResult(games, function(a, b) {
+		return a.number_of_user_reviews > b.number_of_user_reviews ? a : b;
+	});
+    if(game == null)
         return;
     Spice.render({
-        data             : data,
-        header1          : data.name + " (Game's Initial Release Date)",
-        image_url        : data.image == null ? null : data.image.thumb_url,
-        source_url       : data.site_detail_url,
+        data             : game,
+        header1          : game.name + " (Release Date)",
+        source_url       : game.site_detail_url,
         source_name      : 'GiantBomb',
         spice_name       : 'game_release_date'
     });
@@ -29,24 +27,6 @@ Handlebars.registerHelper("release_date", function() {
         day: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
         month: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     };
-    var parts = this.original_release_date.split(" ")[0].split("-");
-    var date = new Date(parts[0], parts[1] - 1, parts[2]);
-    var postfix_info = date.getDate() % 10;
-    var postfix;
-    switch(postfix_info) {
-        case 1:
-            postfix = "st";
-            break;
-        case 2:
-            postfix = "nd";
-            break;
-        case 3:
-            postfix = "rd";
-            break;
-        default:
-            postfix = "th";
-    }
-    if(date.getDate() > 10 && date.getDate() < 20)
-        postfix = "th";
-    return meta.day[date.getDay()] + " " + date.getDate() + postfix + " " + meta.month[date.getMonth()] + " " + date.getFullYear();
+    var date = DDG.getDateFromString(this.original_release_date);
+    return meta.day[date.getDay()] + " " + DDG.getOrdinal(date.getDate()) + " " + meta.month[date.getMonth()] + " " + date.getFullYear();
 });
