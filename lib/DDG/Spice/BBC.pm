3c6b02208b64ff512145f2d2ac03bf1b055366e7
Zaahir Moolla <moollaza@fastmail.fm>
2013-12-02 17:07:56 -0500

    BBC - fix up handling of optional return params

--- lib/DDG/Spice/BBC.pm
+++ lib/DDG/Spice/BBC.pm
@@ -14,9 +14,9 @@ topics "everyday";
 category "entertainment";
 attribution github => ['https://github.com/tophattedcoder','Tom Bebbington'];
 
-spice to => 'http://www.bbc.co.uk/$1/programmes/schedules/$3/$4.json';
+spice to => 'http://www.bbc.co.uk/$1/programmes/schedules/$3.json';
 
-spice from => '([^\/]+)(\/([^\/]+))?\/([^\/]+)(\/([^\/]+))?(\/([^\/]+))?';
+spice from => '([^/]+)/([^/]+)/([^/]+(/[^/]+)?)';
 
 spice wrap_jsonp_callback => 1;
 
@@ -75,27 +75,27 @@ handle query_lc => sub {
 
     # detect simple city radio names
     if($_ =~ /bbc radio( in| for)? (berksire|bristol|cambridgeshire|cornwall|cumbria|derby|devon|gloucestershire|humberside|jersey|kent|lancashire|leeds|leicester|manchester|merseyside|norfolk|northampton|nottingham|sheffield|shropshire|solent|stoke|suffolk|york)/) {
-        return ("radio$2", '', $day);
+        return ("radio$2", $day);
     }
     
-    return ('worldserviceradio', '', $day) if($_ =~ /bbc world (service|radio|service radio)?/);
-    return ('asiannetwork', '', $day) if($_ =~ /bbc asian network/);
-    return ('6music', '', $day) if($_ =~ /bbc radio (6|six)( music)?/);
-    return ('5livesportsextra', '', $day) if($_ =~ /bbc radio (5|five)( live)? extra/);
-    return ('5live', '', $day) if($_ =~ /bbc radio (5|five)( live)?/);
-    return ('radio4extra', '', $day) if($_ =~ /bbc radio (4|four) e?xtra/);
-    return ('radio4', '', $day) if($_ =~ /bbc radio (4|four)/);
-    return ('radio3', '', $day) if($_ =~ /bbc radio (3|three)/);
-    return ('radio2', '', $day) if($_ =~ /bbc radio (2|two)/);
-    return ('1xtra', '', $day) if($_ =~ /bbc radio (1|one) e?xtra/);
+    return ('worldserviceradio', $day) if($_ =~ /bbc world (service|radio|service radio)?/);
+    return ('asiannetwork', $day) if($_ =~ /bbc asian network/);
+    return ('6music', $day) if($_ =~ /bbc radio (6|six)( music)?/);
+    return ('5livesportsextra', $day) if($_ =~ /bbc radio (5|five)( live)? extra/);
+    return ('5live', $day) if($_ =~ /bbc radio (5|five)( live)?/);
+    return ('radio4extra', $day) if($_ =~ /bbc radio (4|four) e?xtra/);
+    return ('radio4', $day) if($_ =~ /bbc radio (4|four)/);
+    return ('radio3', $day) if($_ =~ /bbc radio (3|three)/);
+    return ('radio2', $day) if($_ =~ /bbc radio (2|two)/);
+    return ('1xtra', $day) if($_ =~ /bbc radio (1|one) e?xtra/);
     return ('radio1', 'england', $day) if($_ =~ /bbc radio( 1| one)?/);
-    return ('bbcalba', '', $day) if($_ =~ /bbc alba/);
-    return ('parliament', '', $day) if($_ =~ /bbc parliament/);
-    return ('bbcnews', '', $day) if($_ =~ /bbc news/);
-    return ('cbeebies', '', $day) if($_ =~ /cbeebies/);
-    return ('cbbc', '', $day) if($_ =~ /cbbc/);
-    return ('bbcfour', '', $day) if($_ =~ /bbc (4|four)/);
-    return ('bbcthree', '', $day) if($_ =~ /bbc (3|three)/);
+    return ('bbcalba', $day) if($_ =~ /bbc alba/);
+    return ('parliament', $day) if($_ =~ /bbc parliament/);
+    return ('bbcnews', $day) if($_ =~ /bbc news/);
+    return ('cbeebies', $day) if($_ =~ /cbeebies/);
+    return ('cbbc', $day) if($_ =~ /cbbc/);
+    return ('bbcfour', $day) if($_ =~ /bbc (4|four)/);
+    return ('bbcthree', $day) if($_ =~ /bbc (3|three)/);
     return ('bbctwo', $region, $day) if($_ =~ /bbc (2|two)/);
     return ('bbcone', $area, $day) if($_ =~ /bbc( 1| one)?/);
     return;
