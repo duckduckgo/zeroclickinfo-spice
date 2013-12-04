3d3eb70c05a71d0453d0a02a563fadf1998603d1
Jag Talon <jagtalon@duckduckgo.com>
2013-12-04 18:29:27 -0500

    Images: Double quotes to single quotes.

--- lib/DDG/Spice/Images.pm
+++ lib/DDG/Spice/Images.pm
@@ -3,7 +3,7 @@ package DDG::Spice::Images;
 use DDG::Spice;
 
 spice to => 'https://duckduckgo.com/i.js?q=$1&cb=DDG.duckbar.images.display';
-triggers any => "image", "images";
+triggers any => 'image', 'images';
 
 handle remainder => sub {
     return $_ if $_;
