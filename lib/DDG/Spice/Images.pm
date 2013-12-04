a4f960b660920b99e45c86ce5167096acfbee4fb
Jag Talon <jagtalon@duckduckgo.com>
2013-12-04 18:19:25 -0500

    Images: Remove trim--query removes stuff for us.

--- lib/DDG/Spice/Images.pm
+++ lib/DDG/Spice/Images.pm
@@ -1,13 +1,12 @@
 package DDG::Spice::Images;
 
 use DDG::Spice;
-use String::Trim;
 
 spice to => 'https://duckduckgo.com/i.js?q=$1&cb=DDG.duckbar.images.display';
 triggers query => qr/^(.*)\bimages?\b(.*)$/i;
 
 handle query => sub {
-    return trim($1) . " " . trim($2) if $_;
+    return "$1 $2" if $_;
     return;
 };
 
