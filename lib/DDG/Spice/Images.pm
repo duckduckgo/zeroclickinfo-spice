2c2ccd0a0c4f9bef53e188a14b6bde906effcf8a
Jag Talon <jagtalon@duckduckgo.com>
2013-12-04 18:23:29 -0500

    Images: "any" is faster for this.

--- lib/DDG/Spice/Images.pm
+++ lib/DDG/Spice/Images.pm
@@ -3,10 +3,10 @@ package DDG::Spice::Images;
 use DDG::Spice;
 
 spice to => 'https://duckduckgo.com/i.js?q=$1&cb=DDG.duckbar.images.display';
-triggers query => qr/^(.*)\bimages?\b(.*)$/i;
+triggers any => "image", "images";
 
-handle query => sub {
-    return "$1 $2" if $_;
+handle remainder => sub {
+    return $_ if $_;
     return;
 };
 
