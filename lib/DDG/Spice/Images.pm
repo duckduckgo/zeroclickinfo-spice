9fafb37aec31ddce6af5d9a69eaab5ed240c6632
Jag Talon <jagtalon@duckduckgo.com>
2013-12-04 18:14:54 -0500

    Images: Rearrange trim function.

--- lib/DDG/Spice/Images.pm
+++ lib/DDG/Spice/Images.pm
@@ -7,7 +7,7 @@ spice to => 'https://duckduckgo.com/i.js?q=$1&cb=DDG.duckbar.images.display';
 triggers query => qr/^(.*)\bimages?\b(.*)$/i;
 
 handle query => sub {
-    return trim("$1 $2") if $_;
+    return trim($1) . " " . trim($2) if $_;
     return;
 };
 
