dc6cf7145321ba781c414c7e3bf53d6a2f800854
caine tighe <caine@duckduckgo.com>
2013-12-05 18:37:58 -0500

    Images/Video: try relative link.

--- lib/DDG/Spice/Images.pm
+++ lib/DDG/Spice/Images.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Images;
 
 use DDG::Spice;
 
-spice to => 'https://duckduckgo.com/i.js?q=$1&o=json&cb={{callback}}';
+spice to => '/i.js?q=$1&o=json&cb={{callback}}';
 triggers any => 'image', 'images';
 
 handle remainder => sub {
