77adb059224adb46104a34569da77fc9cdd639a8
caine tighe <caine@duckduckgo.com>
2013-12-05 19:59:52 -0500

    DDG::Rewrite fails without a host.

--- lib/DDG/Spice/Images.pm
+++ lib/DDG/Spice/Images.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Images;
 
 use DDG::Spice;
 
-spice to => '/i.js?q=$1&o=json&cb={{callback}}';
+spice to => 'https://127.0.0.1/i.js?q=$1&o=json&cb={{callback}}';
 triggers any => 'image', 'images';
 
 handle remainder => sub {
