abab6c34addd68c75d199c9c11ed27dc845277b3
Kono Kim <kono@twinword.com>
2013-12-07 20:16:19 -0800

    Word Map:  front-end is displayed as well. 1.0.2

--- lib/DDG/Spice/WordMap.pm
+++ lib/DDG/Spice/WordMap.pm
@@ -3,22 +3,23 @@ package DDG::Spice::WordMap;
 
 use DDG::Spice;
 
-name "Word Map";
+name 		"Word Map";
 description "Generates semantically related words";
-source "Twinword";
+source 		"Twinword";
 primary_example_queries "similar to sound", "words like sound";
-category "reference";
-topics "words_and_games", "everyday";
-code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/WordMap.pm";
-icon_url "/i/tw_favicon.ico";
-attribution web => ['http://twinword.com','Twinword Inc.'],
+category 	"reference";
+topics 		"words_and_games", "everyday";
+code_url 	"https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/WordMap.pm";
+icon_url 	"/i/tw_favicon.ico";
+attribution web 	=> ['http://twinword.com','Twinword Inc.'],
             twitter => ['http://twitter.com/levelpump', '@levelpump'],
-            email => ['feedback@twinword.com','Twinword Inc.'];
+            email 	=> ['feedback@twinword.com','Twinword Inc.'];
 
 spice to => 'http://duckduckgo.twinword.com/api/v2/context/user/duckduckgo/?entry=$1&api_key={{ENV{DDG_SPICE_WORDMAP_APIKEY}}}';
 spice wrap_string_callback => 1;
 
-spice proxy_cache_valid => "418 1d";
+spice proxy_cache_valid => "418 1d";		# for not caching
+#spice proxy_cache_valid => "200 304 1d";	# for caching
 
 
 triggers startend => (
@@ -44,7 +45,7 @@ triggers startend => (
 	"word map", 
 	"word cluster", 
 	"word graph",
-	"twinword"
+	"more words like"
 );
 
 
--- share/spice/word_map/word_map.js
+++ share/spice/word_map/word_map.js
@@ -1,13 +1,25 @@
-function ddg_spice_word_map(api_result) {
-    if (!api_result.map) return;
-    if (200 != api_result.result_code) return;
+function ddg_spice_word_map(api_result) 
+{
+	var json = null;
+	if ( api_result instanceof Object )
+	{
+		json = api_result;
+	}
+	else
+	{
+		json = $.parseJSON(api_result);
+	}
 
-    Spice.render({
-        data             	: api_result,
+	if ( 200 != json.result_code ) return;
+
+    Spice.render
+    ({
+        data             	: json,
         force_big_header 	: true,
-        header1          	: api_result.entry,
-        source_url       	: 'http://twinword.com',
-        source_name      	: 'Twinword',
+        header1          	: "More words like " + json.entry,
+        source_url       	: 'http://levelpump.com/graph-dictionary.php?mailLink=' + json.encrypt_entry + '&from=ddg',
+        source_name      	: 'Twinword-Levelpump',
+        force_favicon_url	: 'http://files.twinword.com/uploads/7/2/1/6/7216232/tw_favicon.ico',
         template_normal  	: 'word_map'
     });
 }
