e9af2d6597384e89f7ac1dc351384cfe1c0646b5
Zaahir Moolla <moollaza@fastmail.fm>
2013-12-13 12:03:32 -0500

    FlashVersion - Better template

--- share/spice/flash_version/flash_version.js
+++ share/spice/flash_version/flash_version.js
@@ -2,11 +2,13 @@ function ddg_spice_flash_version () {
 
     // TODO: Probably better if we display all the plugins similar to https://www.mozilla.org/en-US/plugincheck/.
     // We can get that from window.navigator.plugins.
-    var context = { 
-        version: YAHOO.util.FlashDetect.installed
-                    ? YAHOO.util.FlashDetect.raw
-                    : "You don't have Flash installed"
+    var context = {
+        version:  YAHOO.util.FlashDetect.raw,
     };
+
+    if (!YAHOO.util.FlashDetect.installed){
+        context.error = true;
+    }
 
     // Display the plugin.
     Spice.render({
