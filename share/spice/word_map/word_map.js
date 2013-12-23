f641c78d9d41cfd47d3c1336bdeea2a4b0a1f882
Kono Kim <kono@twinword.com>
2013-12-23 09:10:39 -0800

    Word Map: modified to feedback as of Dec/23

--- share/spice/word_map/word_map.js
+++ share/spice/word_map/word_map.js
@@ -7,9 +7,11 @@ function ddg_spice_word_map(api_result)
     Spice.render({
         data             	: json,
         force_big_header 	: true,
-        header1          	: "Words similar to " + json.entry,
+        header1          	: "Related to " + json.entry + " (Levelpump)",
         source_url       	: 'http://levelpump.com/graph-dictionary.php?mailLink=' + json.encrypt_entry + '&from=ddg',
         source_name      	: 'Levelpump',
         template_normal  	: 'word_map'
     });
 }
+
+
