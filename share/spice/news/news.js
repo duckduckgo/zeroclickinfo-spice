33904e2bbd7282ab1c1446d74e42ea930342ca4b
caine tighe <caine@duckduckgo.com>
2013-12-23 14:50:25 -0500

    News: refactor a bit...

--- share/spice/news/news.js
+++ share/spice/news/news.js
@@ -13,12 +13,6 @@ function ddg_spice_news(apiResult) {
         "sort:date"
     ];
 
-    // Check if the word news is in the query.
-    var generic = false;
-    if((/\bnews\b/i).test(DDG.get_query())) {
-        generic = true;
-    }
-
     // Some sources need to be set by us.
     var getSource = function(story) {
         switch(story.syndicate) {
@@ -37,30 +31,21 @@ function ddg_spice_news(apiResult) {
         }
     };
 
-    for(var i = 0, story; story = apiResult[i]; i++) {
-        story.title = story.title.replace(/<b>|<\/b>|:/g, "");
-    }
-
-    // Relevancy:
-    // Check if the title is relevant to our query.
+    // Check if the title is relevant to the query.
     var goodStories = [];
-    //if(generic) {
-    goodStories = apiResult;
-    //} else {
     for(var i = 0, story; story = apiResult[i]; i++) {
+	// strip bold from story titles.
+        story.title = story.title.replace(/<b>|<\/b>|:/g, "");
+
         if(DDG.isRelevant(story.title, skip, 3)) {
             getSource(story);
             goodStories.push(story);
         }
     }
-    //}
 
-    // Exit if we didn't get any results.
-    if(goodStories.length === 0) {
-        return;
+    // If we found some good stories, display them.
+    if(goodStories.length > 0) {
+        DDG.duckbar.news.stories = goodStories;
+        DDG.duckbar.news.display(goodStories);
     }
-
-    // display good stories if there are some!
-    DDG.duckbar.news.stories = goodStories;
-    DDG.duckbar.news.display(goodStories);
 }
