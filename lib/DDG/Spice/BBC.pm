f74cd15d8f9688d94c5a885d8783fc2f8b3f6f52
Zaahir Moolla <moollaza@fastmail.fm>
2013-12-24 11:13:17 -0500

    BBC - should be case-insensitive

--- lib/DDG/Spice/BBC.pm
+++ lib/DDG/Spice/BBC.pm
@@ -26,19 +26,21 @@ triggers any => "schedule", "tv guide";
 
 handle remainder => sub {
 
+    my $query = lc $_;
+
     # no schedules available for BBC America or BBC Canada
-    return if $_ =~ /america|canada/;
+    return if $query =~ /america|canada/;
 
     # detect the day to look-up
     my $day;
-    if ($_ =~ /tomorrow|in a day|in 1 day/) {
+    if ($query =~ /tomorrow|in a day|in 1 day/) {
         $day = "tomorrow";
-    } elsif ($_ =~ /yesterday|a day ago|1 day ago|last night/) {
+    } elsif ($query =~ /yesterday|a day ago|1 day ago|last night/) {
         $day = "yesterday";
     } else {
         $day = "today";
     }
-    $_ =~ s/\s*(tomorrow(\s(night|evening))?|in a day|in 1 day|yesterday(\s(night|evening))?|a day ago|1 day ago|last night|today|tonight)\s*//;
+    $query =~ s/\s*(tomorrow(\s(night|evening))?|in a day|in 1 day|yesterday(\s(night|evening))?|a day ago|1 day ago|last night|today|tonight)\s*//;
 
     my $dt = DateTime->now->set_time_zone( $loc->time_zone );
     $dt->add( days => 1 ) if $day eq "tomorrow";
@@ -48,59 +50,59 @@ handle remainder => sub {
 
     # set the code for the location
     my $area;
-    if ($_ =~ /(north(ern)? )?ireland/) {
+    if ($query =~ /(north(ern)? )?ireland/) {
         $area = "ni";
-    } elsif ($_ =~ /(north|south) (east|west)/) {
+    } elsif ($query =~ /(north|south) (east|west)/) {
         $area = "$1_$2";
-    } elsif ($_ =~ /(south|east|west)/) {
+    } elsif ($query =~ /(south|east|west)/) {
         $area = "$1";
-    } elsif ($_ =~ /(scotland|cambridge|oxford|wales|yorkshire|london)/) {
+    } elsif ($query =~ /(scotland|cambridge|oxford|wales|yorkshire|london)/) {
         $area = "$1";
-    } elsif ($_ =~ /cambridgeshire/) {
+    } elsif ($query =~ /cambridgeshire/) {
         $area = "cambridge";
-    } elsif ($_ =~ /east yorkshire|yorks|lincs/) {
+    } elsif ($query =~ /east yorkshire|yorks|lincs/) {
         $area = "east_yorkshire";
-    } elsif ($_ =~ /channel islands/) {
+    } elsif ($query =~ /channel islands/) {
         $area = "channel_islands";
-    } elsif ($_ =~ /(east|west) midlands/) {
+    } elsif ($query =~ /(east|west) midlands/) {
         $area = "$1_midlands";
-    } elsif ($_ =~ /midlands/) {
+    } elsif ($query =~ /midlands/) {
         $area = "west_midlands";
-    } elsif ($_ =~ /cumbria/) {
+    } elsif ($query =~ /cumbria/) {
         $area = "north_east";
     } else {
         $area = "london";
     }
-    $_ =~ s/\s*((north(ern)? )?ireland|(north|south) (east|west)|south|east|west|scotland|cambridge|oxford|wales|yorkshire|london|cambridgeshire|east yorkshire|yorks|lincs|channel islands|(east|west) midlands|midlands|cumbria|england|uk|united kingdom|britain)\s*//;
+    $query =~ s/\s*((north(ern)? )?ireland|(north|south) (east|west)|south|east|west|scotland|cambridge|oxford|wales|yorkshire|london|cambridgeshire|east yorkshire|yorks|lincs|channel islands|(east|west) midlands|midlands|cumbria|england|uk|united kingdom|britain)\s*//;
 
     # look up the region
     my $region = $area =~ /scotland|wales|ni/ ? $area : "england";
 
     # detect simple city radio names
-    if ($_ =~ /bbc radio( in| for)? (berksire|bristol|cambridgeshire|cornwall|cumbria|derby|devon|gloucestershire|humberside|jersey|kent|lancashire|leeds|leicester|manchester|merseyside|norfolk|northampton|nottingham|sheffield|shropshire|solent|stoke|suffolk|york)/) {
+    if ($query =~ /bbc radio( in| for)? (berksire|bristol|cambridgeshire|cornwall|cumbria|derby|devon|gloucestershire|humberside|jersey|kent|lancashire|leeds|leicester|manchester|merseyside|norfolk|northampton|nottingham|sheffield|shropshire|solent|stoke|suffolk|york)/) {
         return ("radio$2", @date);
     }
 
-    return ('worldserviceradio', @date) if ($_ =~ /bbc world( service| radio| service radio)?/);
-    return ('asiannetwork', @date) if ($_ =~ /bbc asian network/);
-    return ('6music', @date) if ($_ =~ /bbc radio (6|six)( music)?/);
-    return ('5livesportsextra', @date) if ($_ =~ /bbc radio (5|five)( live)? extra/);
-    return ('5live', @date) if ($_ =~ /bbc radio (5|five)( live)?/);
-    return ('radio4extra', @date) if ($_ =~ /bbc radio (4|four) e?xtra/);
-    return ('radio4', @date) if ($_ =~ /bbc radio (4|four)/);
-    return ('radio3', @date) if ($_ =~ /bbc radio (3|three)/);
-    return ('radio2', @date) if ($_ =~ /bbc radio (2|two)/);
-    return ('1xtra', @date) if ($_ =~ /bbc radio (1|one) e?xtra/);
-    return ('radio1', 'england', @date) if ($_ =~ /bbc radio( 1| one)?$/);
-    return ('bbcalba', @date) if ($_ =~ /bbc alba/);
-    return ('parliament', @date) if ($_ =~ /bbc parliament/);
-    return ('bbcnews', @date) if ($_ =~ /bbc news/);
-    return ('cbeebies', @date) if ($_ =~ /cbeebies/);
-    return ('cbbc', @date) if ($_ =~ /cbbc/);
-    return ('bbcfour', @date) if ($_ =~ /bbc (4|four)/);
-    return ('bbcthree', @date) if ($_ =~ /bbc (3|three)/);
-    return ('bbctwo', $region, @date) if ($_ =~ /bbc (2|two)/);
-    return ('bbcone', $area, @date) if ($_ =~ /bbc( 1| one)?$/);
+    return ('worldserviceradio', @date) if ($query =~ /bbc world( service| radio| service radio)?/);
+    return ('asiannetwork', @date) if ($query =~ /bbc asian network/);
+    return ('6music', @date) if ($query =~ /bbc radio (6|six)( music)?/);
+    return ('5livesportsextra', @date) if ($query =~ /bbc radio (5|five)( live)? extra/);
+    return ('5live', @date) if ($query =~ /bbc radio (5|five)( live)?/);
+    return ('radio4extra', @date) if ($query =~ /bbc radio (4|four) e?xtra/);
+    return ('radio4', @date) if ($query =~ /bbc radio (4|four)/);
+    return ('radio3', @date) if ($query =~ /bbc radio (3|three)/);
+    return ('radio2', @date) if ($query =~ /bbc radio (2|two)/);
+    return ('1xtra', @date) if ($query =~ /bbc radio (1|one) e?xtra/);
+    return ('radio1', 'england', @date) if ($query =~ /bbc radio( 1| one)?$/);
+    return ('bbcalba', @date) if ($query =~ /bbc alba/);
+    return ('parliament', @date) if ($query =~ /bbc parliament/);
+    return ('bbcnews', @date) if ($query =~ /bbc news/);
+    return ('cbeebies', @date) if ($query =~ /cbeebies/);
+    return ('cbbc', @date) if ($query =~ /cbbc/);
+    return ('bbcfour', @date) if ($query =~ /bbc (4|four)/);
+    return ('bbcthree', @date) if ($query =~ /bbc (3|three)/);
+    return ('bbctwo', $region, @date) if ($query =~ /bbc (2|two)/);
+    return ('bbcone', $area, @date) if ($query =~ /bbc( 1| one)?$/);
     return;
 };
 
