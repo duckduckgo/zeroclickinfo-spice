a0f4b35144257972d7ea9417e4029db69cc85f82
Tom Bebbington <tophattedcoder@gmail.com>
2013-12-30 21:13:49 +0000

    Move URL parameters so they are in rough order of importance

--- lib/DDG/Spice/GameInfo.pm
+++ lib/DDG/Spice/GameInfo.pm
@@ -10,7 +10,7 @@ topics "gaming";
 category "reference";
 attribution github => ['https://github.com/tophattedcoder','tophattedcoder'];
 triggers startend => "video game","video games","giantbomb";
-spice to => 'http://www.giantbomb.com/api/search/?field_list=number_of_user_reviews,name,image,site_detail_url,original_game_rating,platforms,deck,original_release_date,aliases&api_key={{ENV{DDG_SPICE_GIANTBOMB_APIKEY}}}&format=jsonp&query=$1&limit=12&json_callback={{callback}}&resources=game';
+spice to => 'http://www.giantbomb.com/api/search/?api_key={{ENV{DDG_SPICE_GIANTBOMB_APIKEY}}}&format=jsonp&query=$1&limit=12&json_callback={{callback}}&resources=game&field_list=number_of_user_reviews,name,image,site_detail_url,original_game_rating,platforms,deck,original_release_date,aliases';
 
 handle remainder => sub {
     return $_ if $_;
