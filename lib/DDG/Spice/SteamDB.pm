2cf548a0830d239684a8ba3b5c2796b9daba201e
xPaw <github@xpaw.ru>
2014-01-04 15:22:02 +0200

    Add "steam appid" trigger which allows for searching "steam appid 440" (backend has been modified to support this too)

--- lib/DDG/Spice/SteamDB.pm
+++ lib/DDG/Spice/SteamDB.pm
@@ -13,7 +13,7 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/
 attribution github => ["https://github.com/xPaw", "xPaw"],
             twitter => ["https://twitter.com/thexpaw", "xPaw"];
 
-triggers startend => "steamdb", "steam game", "steam app";
+triggers startend => "steamdb", "steam game", "steam app", "steam appid";
 
 spice to => 'http://steamdb.info/api/GetMetadata/?search=$1&jsonp={{callback}}';
 
