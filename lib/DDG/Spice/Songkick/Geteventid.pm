a4cc5fd6fd259286794d5cf4742469d3ac7c2ddd
Jag Talon <jagtalon@duckduckgo.com>
2014-01-09 11:59:13 -0500

    Songkick: Should exit if we find an empty string.

--- lib/DDG/Spice/Songkick/Geteventid.pm
+++ lib/DDG/Spice/Songkick/Geteventid.pm
@@ -22,16 +22,18 @@ spice to => 'http://api.songkick.com/api/3.0/search/locations.json?apikey={{ENV{
 
 handle remainder => sub {
   # If the query isn't blank, then use it for the API query.
-  if (length($_) > 0) {
-    if ($_ =~ /^(around|in|near) (.+)$/) {
-      (my $loc = $2) =~ s/^the\sarea$//g;
-      if (length($loc) > 0) {
-        return $loc;
+  if ($_) {
+      if ($_ =~ /^(around|in|near) (.+)$/) {
+	  (my $loc = $2) =~ s/^the\sarea$//g;
+	  return $loc if $loc;
+	  # We want this to exit immediately after
+	  return;
+      } else {
+	  return;
       }
-    } else {
-      return;
-    }
   }
+
+  # If the query is empty, return the current location of the user.
   my $location = join(", ", $loc->city, $loc->region_name, $loc->country_name);
   return $location;
 };
