c03856f4f0fd0378cee48ec3413854f574e02373
Jag Talon <jagtalon@duckduckgo.com>
2014-01-10 09:19:26 -0500

    SteamDB: Add more data in the instant answer.
    - Add links to the Steam Store and Steam Community.
    - Add styling.
    - Add relevancy for extra measure.

--- share/spice/steam_db/steam_db.js
+++ share/spice/steam_db/steam_db.js
@@ -9,16 +9,29 @@ function ddg_spice_steam_db(api_result) {
         source = $(script).attr('src'),
         query = source.match(/steam_db\/([^\/]+)/)[1];
 
+    // Filter irrelevant games.
+    var skip_words = ["steamdb", "steam", "game", "app", "appid"];
+    var results = [];
+    for(var i = 0; i < api_result.data.length; i++) {
+	if(DDG.isRelevant(api_result.data[i].StoreName, skip_words)) {
+	    results.push(api_result.data[i]);
+	}
+    }
+
+    if(results.length === 0) {
+	return;
+    }
+
     // This displays the Spice instant answer.
     Spice.render({
-	data              : api_result.data,
+	data              : results,
 	source_name       : "steamdb.info",
 	source_url        : "http://steamdb.info/search/?a=app&type=1&q=" + query,
 	spice_name        : 'steam_db',
 	template_frame    : "list",
 	header1           : decodeURIComponent(query) + " (SteamDB)",
 	template_options  : {
-	    items: api_result.data,
+	    items: results,
 	    show: 3,
 	    max: 10,
 	    template_item: 'steam_db'
