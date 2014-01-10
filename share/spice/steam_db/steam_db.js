6b6ceaa920fd7906018fefbe2563852e466af111
Jag Talon <jagtalon@duckduckgo.com>
2014-01-10 08:18:20 -0500

    SteamDB: Changed formatting and added comments.

--- share/spice/steam_db/steam_db.js
+++ share/spice/steam_db/steam_db.js
@@ -1,25 +1,26 @@
 function ddg_spice_steam_db(api_result) {
-	if(!api_result || !api_result.success) {
-		return;
+    if(!api_result || !api_result.success) {
+	return;
+    }
+
+    // Get the original query.
+    // This is not a clean way of doing it, but it does the job.
+    var script = $('[src*="/js/spice/steam_db/"]')[0],
+    	source = $(script).attr('src'),
+    	query = source.match(/steam_db\/([^\/]+)/)[1];
+
+    // This displays the Spice instant answer.
+    Spice.render({
+	data              : api_result.data,
+	source_name       : "steamdb.info",
+	source_url        : "http://steamdb.info/search/?a=app&type=1&q=" + query,
+	spice_name        : 'steam_db',
+	template_frame    : "list",
+	header1           : decodeURIComponent(query) + " (SteamDB)",
+	template_options  : {
+	    items: api_result.data,
+	    show: 5,
+	    template_item: 'steam_db'
 	}
-	
-	var query = DDG.get_query().replace(/(steam)/, '').trim();
-	
-	var script = $('[src*="/js/spice/steam_db/"]')[0],
-	    source = $(script).attr('src'),
-	    query = source.match(/steam_db\/([^\/]+)/)[1];
-	
-	Spice.render({
-		data              : api_result.data,
-		source_name       : "steamdb.info",
-		source_url        : "http://steamdb.info/search/?a=app&type=1&q=" + query,
-		spice_name        : 'steam_db',
-		template_frame    : "list",
-		header1           : decodeURIComponent(query) + " (SteamDB)",
-		template_options  : {
-			items: api_result.data,
-			show: 5,
-			template_item: 'steam_db'
-		}
-	});
+    });
 };
