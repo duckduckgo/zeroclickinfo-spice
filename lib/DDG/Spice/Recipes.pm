e13eb1ed910d7364fc8fb4ba72777ec113047bc3
brian <brian@duckduckgo.com>
2014-01-15 11:04:24 -0500

    better logic/regex for matching/parsing recipe from search query

--- lib/DDG/Spice/Recipes.pm
+++ lib/DDG/Spice/Recipes.pm
@@ -16,10 +16,8 @@ triggers any => ('recipe', 'recipes', keys(%ingredients));
 spice to => 'http://api.yummly.com/v1/api/recipes?q=$1&requirePictures=true&maxResult=35&_app_id={{ENV{DDG_SPICE_YUMMLY_APPID}}}&_app_key={{ENV{DDG_SPICE_YUMMLY_APIKEY}}}&callback={{callback}}';
 
 handle query_lc => sub {
-	if (index($_,'recipe') != -1){
-		$_ =~ s/recipes|recipe//g;
-		$_ = trim($_);
-		return $_;
+	if ($_ =~ s/recipes?//gi) {
+		return trim($_);
 	}
 
 	my $ingredient_count = 0;
