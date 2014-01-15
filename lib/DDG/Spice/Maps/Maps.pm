8023f20e910e4cc7303674ef4cfadbdd87ba2e12
caine tighe <caine@duckduckgo.com>
2014-01-15 11:54:34 -0500

    DDG::Spice::Maps::Maps: force generics for now.

--- lib/DDG/Spice/Maps/Maps.pm
+++ lib/DDG/Spice/Maps/Maps.pm
@@ -6,14 +6,14 @@ spice to => 'http://open.mapquestapi.com/nominatim/v1/search?format=json&json_ca
 spice is_cached => 0;
 spice proxy_cache_valid => "418 1d";
 
-triggers startend => (
-    'map',
-    'maps',
-    'current location',
-    'where am i',
-);
+my %generic_map_queries = map {$_ => 0} ('map', 'maps', 'current location', 'where am i', 'here');
+
+triggers any => keys(%generic_map_queries);
 
 handle query_lc => sub {
+    # force generic;
+    return if !exists($generic_map_queries{$_});
+
     my $location = $loc->loc_str;
     return $location if $location;
 };
