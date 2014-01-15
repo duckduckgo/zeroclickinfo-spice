3d5783eb40cd9619ebb79db77d29177e0495b4d9
caine tighe <caine@duckduckgo.com>
2014-01-15 10:25:16 -0500

    Maps: mapbox prereqs.

--- share/spice/maps/directions/directions.js
+++ share/spice/maps/directions/directions.js
@@ -1,4 +1,5 @@
 function ddg_spice_maps_directions(directions) {
-    console.log(directions);
+    nrj('/js/mapbox/mapbox-1.5.2.js');
+    nrc('/js/mapbox/mapbox-1.5.2.css');
     DDG.maps.renderDirections(directions);
 }
--- share/spice/maps/maps/maps.js
+++ share/spice/maps/maps/maps.js
@@ -1,4 +1,5 @@
 function ddg_spice_maps_maps(place) {
-    console.log(place);
+    nrj('/js/mapbox/mapbox-1.5.2.js');
+    nrc('/js/mapbox/mapbox-1.5.2.css');
     DDG.maps.renderMap(place);
 }
