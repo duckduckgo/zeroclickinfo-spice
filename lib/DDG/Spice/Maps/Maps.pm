17d23c65f1f32caea7f7ae2e2521732ed72c94ed
caine tighe <caine@duckduckgo.com>
2014-01-15 10:44:29 -0500

    DDG::Spice::Maps::Maps: change spice to to mapquestapi url.

--- lib/DDG/Spice/Maps/Maps.pm
+++ lib/DDG/Spice/Maps/Maps.pm
@@ -2,7 +2,7 @@ package DDG::Spice::Maps::Maps;
 
 use DDG::Spice;
 
-spice to => 'https://127.0.0.1/map/$1';
+spice to => 'http://open.mapquestapi.com/nominatim/v1/search?format=json&json_callback={{callback}}&addressdetails=1&polygon=1&bounded=0&q=$1';
 spice is_cached => 0;
 spice proxy_cache_valid => "418 1d";
 
