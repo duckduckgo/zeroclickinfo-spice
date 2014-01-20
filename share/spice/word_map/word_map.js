51b3e37be0790a3c4c9e83c7f6048c5d005ead7e
Jag Talon <jagtalon@duckduckgo.com>
2014-01-20 13:37:10 -0500

    WordMap: Encode the URL parameter.

--- share/spice/word_map/word_map.js
+++ share/spice/word_map/word_map.js
@@ -7,7 +7,7 @@ function ddg_spice_word_map(api_result) {
         data             	: api_result,
         force_big_header 	: true,
         header1          	: "Related to " + api_result.entry + " (Levelpump)",
-        source_url       	: 'http://levelpump.com/graph-dictionary.php?mailLink=' + api_result.encrypt_entry + '&from=ddg',
+        source_url       	: 'http://levelpump.com/graph-dictionary.php?mailLink=' + encodeURIComponent(api_result.encrypt_entry) + '&from=ddg',
         source_name      	: 'Levelpump',
         template_normal  	: 'word_map',
 	force_favicon_url       : 'http://icons.duckduckgo.com/ip/www.levelpump.com.ico'
