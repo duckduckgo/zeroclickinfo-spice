8c51c10637d53f15da480f4562223987491e8496
caine tighe <caine@duckduckgo.com>
2014-01-20 15:12:07 -0500

    Maps: sync load mapbox.js assets.

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -1,5 +1,5 @@
 function ddg_spice_maps_maps(place) {
-    nrj('/js/mapbox/mapbox-1.5.2.js');
-    nrc('/js/mapbox/mapbox-1.5.2.css');
+    nrj('/js/mapbox/mapbox-1.5.2.js', 1);
+    nrc('/js/mapbox/mapbox-1.5.2.css', 1);
     DDG.maps.renderMap(place);
 }
