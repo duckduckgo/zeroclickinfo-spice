f3b510a714707d8ae7553143420199e6fb9d3a7a
Gabriel Weinberg <yegg@duckduckgo.com>
2014-01-20 20:20:20 -0500

    Grab assets immediately and not when called in callback.

--- share/spice/maps/places/maps_places.js
+++ share/spice/maps/places/maps_places.js
@@ -1,5 +1,6 @@
+nrj('/js/mapbox/mapbox-1.5.2.js');
+nrc('/js/mapbox/mapbox-1.5.2.css');
+
 function ddg_spice_maps_places(places) {
-    nrj('/js/mapbox/mapbox-1.5.2.js');
-    nrc('/js/mapbox/mapbox-1.5.2.css');
     DDG.maps.renderLocal(places);
 }
