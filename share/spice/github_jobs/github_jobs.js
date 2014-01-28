ff7479c2170c3d4fb5e69b76c626ec347e42bca1
brian <brian@duckduckgo.com>
2014-01-28 15:25:37 -0500

    first pass at github jobs tiles for bttf

--- share/spice/github_jobs/github_jobs.js
+++ share/spice/github_jobs/github_jobs.js
@@ -1,5 +1,5 @@
 function ddg_spice_github_jobs(api_result) {
-    if (api_result.length == 0) return;
+    if (api_result.length == 0) { return; }
 
     var jobs = api_result;
     var query = DDG.get_query();
@@ -7,24 +7,37 @@ function ddg_spice_github_jobs(api_result) {
 	jobs['description'] = query.replace(re, "$1");
 	jobs['location'] = query.replace(re, "$2");
 
+	var source_url = 'https://jobs.github.com/positions?description='
+                            + encodeURIComponent(jobs['description'])
+                            + "&location=" +  encodeURIComponent(jobs['location']);
+
+	
+	var desc = jobs['description'] + ' Jobs';
+	if(jobs['location']){
+		desc += ' in ' + jobs['location'];
+	}
+
     Spice.render({
         data             : api_result,
-        header1          : query + " (GitHub Jobs)",
-        source_url       : 'https://jobs.github.com/positions?description='
-                            + encodeURIComponent(jobs['description'])
-                            + "&location=" +  encodeURIComponent(jobs['location']),
-        source_name      : 'GitHub',
-        spice_name      : 'github',
+		relevant_items   : api_result,
+        source_url       : source_url,
+		source_name      : 'GitHub',
+        spice_name       : 'github_jobs',
 
-        template_frame   : 'list',
-        template_options: {
-            items: api_result,
-            template_item: "github_jobs",
-            show: 3,
-	    max: 10,
-            type: 'ul'
-        },
-        force_big_header : true,
-        force_no_fold    : true
+		meta: {
+			count: api_result.length,
+			count_meta: desc,
+			sourceUrl: source_url,
+			sourceName: 'GitHub',
+			sourceIcon: true
+		},
+
+		templates: {
+			// ?? passing the string 'github_jobs_item' will
+			// nest the template inside another tile template,
+			// but passing the reference doesn't...
+			item: Handlebars.templates.github_jobs_item,
+			detail: Handlebars.templates.github_jobs_detail
+		}
     });
 }
