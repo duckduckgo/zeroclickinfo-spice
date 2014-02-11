b1a5686e4a501837ab8e96d96354d37748c16e76
Adalbert Michelic <am@timeanddate.com>
2014-02-11 11:35:39 +0100

    fix tabs

--- lib/DDG/Spice/TimeAndDateHoliday.pm
+++ lib/DDG/Spice/TimeAndDateHoliday.pm
@@ -9,18 +9,18 @@ spice from => '([^/]+)/([^/]+)';
 spice to => 'http://www.timeanddate.com/scripts/ddg.php?m=whenis&c=$1&q=$2&callback={{callback}}';
 
 handle remainder => sub {
-	return unless ($_);
+    return unless ($_);
 
-	my ($q, $c);
-	if (/\s+in\s+(.*)$/p) {
-		($q, $c) = (${^PREMATCH}, $1);
-	} else {
-		($q, $c) = ($_, $loc->country_name);
-	}
-	$q =~ s/\///g;
-	$c =~ s/\///g;
+    my ($q, $c);
+    if (/\s+in\s+(.*)$/p) {
+        ($q, $c) = (${^PREMATCH}, $1);
+    } else {
+        ($q, $c) = ($_, $loc->country_name);
+    }
+    $q =~ s/\///g;
+    $c =~ s/\///g;
 
-	return $c, $q;
+    return $c, $q;
 };
 
 
