d5993c7f01d0bfed3b12b96a7c055d2e7a2d97af
Andrew Stine <illuminati1113@gmail.com>
2014-02-13 17:31:25 -0500

    MassOnTime - fixes to the code in MassOnTime backend

--- lib/DDG/Spice/MassOnTime.pm
+++ lib/DDG/Spice/MassOnTime.pm
@@ -7,20 +7,21 @@ description "Search for Catholic Religious Events";
 name "MassOnTime";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/MassOnTime.pm";
 icon_url "http://massontime.com/favicon.ico";
-topics "special_interest"
+topics "special_interest";
 category "reference";
 attribution github => ['https://github.com/astine','astine'];
 
 triggers start => "catholic";
 
-spice to => "http://massontime.com/nearest/$1";
+spice from => '([^/]*)/([^/]*)';
+spice to => 'http://massontime.com/nearest/$1/5/json?address=$2&api-key={{ENV{DDG_SPICE_MASSONTIME_APIKEY}}}';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
     return unless $_ =~ /^(church|parish|mass|confession|adoration)(s|es)?\s?(in|near|nearby|at)?\s*(.*)$/i;
-    $event_type = $1;
-    $address = $4;
-    return "$1/5/json?address=$4";
+    my $event_type = $1;
+    my $address = $4;
+    return $event_type, $address;
 };
 
 1;
