b09808b7f1ad281388225a31fb1478095c2bf1a4
Jag Talon <jagtalon@duckduckgo.com>
2014-02-14 10:56:19 -0500

    RubyGems: Remove "ruby", "gem", and "gems" triggers.
    These are ambiguous triggers.

--- lib/DDG/Spice/RubyGems.pm
+++ lib/DDG/Spice/RubyGems.pm
@@ -10,13 +10,11 @@ category 'programming';
 attribution github => ['https://github.com/koosha--', 'koosha--'],
             twitter => ['https://github.com/_koosha_', '_koosha_'];
 
-triggers startend => 'ruby', 'gem', 'gems', 'rubygem', 'rubygems';
+triggers startend => 'rubygem', 'rubygems';
 spice to => 'http://rubygems.org/api/v1/search.json?query=$1&callback={{callback}}';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
-    s/^\s+//;
-    s/\s+$//;
     return $_ if length $_;
     return;
 };
