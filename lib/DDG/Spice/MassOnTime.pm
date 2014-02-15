65aa0197056f580346d8224c6e0a47a20d220dc2
Andrew Stine <illuminati1113@gmail.com>
2014-02-14 22:42:13 -0500

    MassOnTime - 'church' now search type changed to 'parish' search type in api calls

--- lib/DDG/Spice/MassOnTime.pm
+++ lib/DDG/Spice/MassOnTime.pm
@@ -20,6 +20,7 @@ spice wrap_jsonp_callback => 1;
 handle remainder => sub {
     return unless $_ =~ /^(church|parish|mass|confession|adoration)(s|es)?\s?(in|near|nearby|at)?\s*(.*)$/i;
     my $event_type = $1;
+    $event_type = "parish" if $event_type eq "church";
     my $address = $4;
     return $event_type, $address;
 };
