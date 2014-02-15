81f3d339526c5a04a4139c74838e306b6d029471
Andrew Stine <illuminati1113@gmail.com>
2014-02-14 22:42:41 -0500

    MassOnTime - built the front end so it now works correctly

--- share/spice/mass_on_time/mass_on_time.js
+++ share/spice/mass_on_time/mass_on_time.js
@@ -1,12 +1,60 @@
 function ddg_spice_mass_on_time (api_result) {
   if (api_result.error) return;
 
+  var generate_header = function (query_details) {
+      type = "";
+      if (query_details.type == "mass")
+      { type = "Masses"; }
+      else if (query_details.type == "parish")
+      { type = "Parishes"; }
+      else
+      { type = query_details.type.charAt(0).toUpperCase() + query_details.type.slice(1) + "s"; }
+
+      return type + " near " + query_details.location;
+  };
+
+  Handlebars.registerHelper( "format_eventtypeid", function (eventtypeid) {
+      switch (eventtypeid) {
+	  case 1:
+	  return "Adoration";
+	  case 2:
+	  return "Confession";
+	  case 3:
+	  return "Devotion";
+	  case 5:
+	  return "Holy Day Mass";
+	  case 6:
+	  return "Holy Day Mass (Vigil)";
+	  case 7:
+	  return "Weekday Mass";
+	  case 8:
+	  return "Weekend Mass";
+      };
+  });
+
+  var pick_item_template = function (query_details) {
+      if (query_details.type == "parish")
+      { return "mass_on_time_church"; }
+      else
+      { return "mass_on_time"; }
+  };
+
   Spice.render({
-     data              : api_result,
-     force_big_header  : true,
-     header1           : api_result.name + ' (' + api_result.version + ')',
-     source_name       : "Mass On Time",
-     source_url        : 'http://massontime.com/' + api_result.name,
-     template_normal   : 'mass_on_time'
+      data              : api_result,
+      header1           : generate_header(api_result['query-details']),
+      source_name       : "Mass On Time",
+      spice_name        : 'mass_on_time',
+      source_url        : 'http://massontime.com/nearest/' + api_result['query-details'].type + 
+	  "/25",
+      //template_normal : 'mass_on_time',
+      template_frame   : 'list',
+      template_options : {
+          items: api_result.results,
+          show: 5,
+          template_item: pick_item_template(api_result['query-details'])
+      },
+      force_no_fold     : true,
+      force_big_header  : true,
+      force_favicon_url : 'http://massontime.com/favicon.ico'
 });
 }
