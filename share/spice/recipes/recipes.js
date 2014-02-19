472afa9199ec02a4e6bf84e28f8450802d0884fd
russell <russell@duckduckgo.com>
2014-02-19 10:45:32 -0500

    another query encoding chnage

--- share/spice/recipes/recipes.js
+++ share/spice/recipes/recipes.js
@@ -118,7 +118,7 @@ function ddg_spice_recipes(res) {
     var //normalizedData = normalizeData(res.matches),
 
         searchContainedRecipe = !!(query_encoded.match(/recipe/i)),
-        searchTerm = query_encoded.replace(/recipes|recipe/i,'').trim(),
+        searchTerm = query.replace(/recipes|recipe/i,'').trim(),
         moreUrl = res.attribution.url + '?q=' + searchTerm; // should replace trigger word or use the same logic that is used for the api call
 
     Spice.add({
