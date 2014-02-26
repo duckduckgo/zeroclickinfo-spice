fb49dc3b9b0d5af05a2bb7ca285a762b3752b9f1
aparna-hexagon <aparna2110@gmail.com>
2014-02-26 17:30:11 +0530

    triggers changed
    null value handled, removed Trailing comma

--- lib/DDG/Spice/9wsearch.pm
+++ lib/DDG/Spice/9wsearch.pm
@@ -2,24 +2,30 @@ package DDG::Spice::9wsearch;
 
 use DDG::Spice;
 
-name '9WSearch';
+
+name '9W Search';
 description '9W Search right answer to a financial question';
-primary_example_queries 'msft', 'goog';
+#primary_example_queries 'plos dinosaurs', 'plos echinoderm evolution';
+#secondary_example_queries 'plos dinosaurs title:metabolism';
 source '9WSearch';
 category 'finance';
 topics 'economy_and_finance';
 icon_url 'http://www.9wsearch.com/media/images/9wLogo.png';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/9wsearch.pm';
-attribution github => ['https://github.com/aparna-hexagon', 'Aparna'];
+#attribution twitter => 'nelas',
+#			github => ['nelas', 'Bruno C. Vellutini'],
+#			web => ['http://organelas.com/', 'organelas.com'];
 
+triggers startend => "information","9wsearch","description","company", "company information", "company description";
 spice to => 'http://www.9wsearch.com/widget/description.php?arg=$1';
 spice wrap_jsonp_callback => 1;
 
-my @triggers = share('triggers.txt')->slurp;
 
-triggers any => @triggers;
 
-handle query_lc => sub {
-  return $_ if $_;
+handle remainder => sub {
+    # Only trigger if the remainder does not exist in the skip hash.
+    return $_ if $_;
+    return;
 };
+
 1;
--- share/spice/9wsearch/9wsearch.js
+++ share/spice/9wsearch/9wsearch.js
@@ -1,12 +1,20 @@
 function ddg_spice_9wsearch (api_result) {
+
     if (api_result.error) return;
 
-    Spice.render({
-         data              : api_result,
-         force_big_header  : true,
-         header1           : api_result.name + ' (' + api_result.primary_symbol + ')',
-         source_name       : "9wsearch.com", // More at ...
-         source_url        : 'http://www.9wsearch.com/companies/' + api_result.primary_symbol,
-         template_normal   : '9wsearch',
-    });
+    if (api_result.result === 'not_found') return;
+
+    if (api_result.primary_symbol != '' && api_result.name != '')
+    {  
+        Spice.render({
+            data              : api_result,
+            force_big_header  : true,
+            header1           : api_result.name + ' (' + api_result.primary_symbol + ')',
+            source_name       : "9wsearch.com", // More at ...
+            source_url        : 'http://www.9wsearch.com/companies/' + api_result.primary_symbol,
+            template_normal   : '9wsearch'
+        });
+    }
+    else
+        return;
 }
