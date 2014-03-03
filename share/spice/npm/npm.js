5a5f1e5c4dd2ffb6c0e87450d1e5dad510aba4dc
Brad Cater <bradcater@bradcater-Aspire-5750>
2014-03-03 08:09:07 -0500

    Fix whitespace tabs-versus-spaces conflict in Npm Spice.

--- share/spice/npm/npm.js
+++ share/spice/npm/npm.js
@@ -1,18 +1,17 @@
 function ddg_spice_npm (api_result) {
   "use strict";
 
-	if (api_result.error) {
+  if (api_result.error) {
     return;
   }
 
-    Spice.render({
-         data              : api_result,
-         force_big_header  : true,
-         header1           : api_result.name + ' (' + api_result.version + ')',
-         source_name       : "npmjs.org", // More at ...
-         source_url        : 'http://npmjs.org/package/' + api_result.name,
-         template_normal   : 'npm',
-         template_small    : 'npm'
-    });
+  Spice.render({
+    data              : api_result,
+    force_big_header  : true,
+    header1           : api_result.name + ' (' + api_result.version + ')',
+    source_name       : "npmjs.org", // More at ...
+    source_url        : 'http://npmjs.org/package/' + api_result.name,
+    template_normal   : 'npm',
+    template_small    : 'npm'
+  });
 }
-
