d80204b11c475d8a7a4d57f6d30f8526b1d5f85d
Brad Cater <bradcater@bradcater-Aspire-5750>
2014-03-03 08:10:25 -0500

    Fix whitespace tabs-versus-spaces conflict in Urban_dictionary Spice.

--- share/spice/urban_dictionary/urban_dictionary.js
+++ share/spice/urban_dictionary/urban_dictionary.js
@@ -6,20 +6,20 @@
 function ddg_spice_urban_dictionary(response) {
   "use strict";
 
-	if (!(response || response.response.result_type === "exact"
-            || response.list || response.list[0])) {
-        return;
+  if (!(response || response.response.result_type === "exact"
+      || response.list || response.list[0])) {
+    return;
   }
 
-    var word       = response.list[0].word;
-	var definition = response.list[0].definition.replace(/(\r?\n)+/gi, '<br>');
+  var word       = response.list[0].word;
+  var definition = response.list[0].definition.replace(/(\r?\n)+/gi, '<br>');
 
-    Spice.render({
-        data             : { 'definition' : definition },
-        header1          : word + " (Urban Dictionary)",
-        source_url       : 'http://www.urbandictionary.com/define.php?term=' + word,
-        source_name      : 'Urban Dictionary',
-        template_normal  : 'urban_dictionary',
-        force_big_header : true,
-    });
+  Spice.render({
+    data             : { 'definition' : definition },
+    header1          : word + " (Urban Dictionary)",
+    source_url       : 'http://www.urbandictionary.com/define.php?term=' + word,
+    source_name      : 'Urban Dictionary',
+    template_normal  : 'urban_dictionary',
+    force_big_header : true,
+  });
 }
