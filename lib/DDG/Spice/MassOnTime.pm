18dfbbd58ca745f3009e61893378de5a20365e92
Andrew Stine <illuminati1113@gmail.com>
2014-03-06 18:19:23 -0500

    MassOnTime: Added some comments to clarify that the location API is being used as a fallback

--- lib/DDG/Spice/MassOnTime.pm
+++ lib/DDG/Spice/MassOnTime.pm
@@ -20,10 +20,15 @@ spice wrap_jsonp_callback => 1;
 handle remainder => sub {
     return unless $_ =~ /^(church|parish|mass|confession|adoration)(s|es)?(\s+in|\s+nearby|\s+near|\s+at)?\s*(.*)$/i;
     my $event_type = $1;
-    $event_type = "parish" if $event_type eq "church";
     my $address = $4;
+    
+    #MassOnTime API doesn't recognize 'church;, replace with 'parish'
+    $event_type = "parish" if $event_type eq "church";
+
+    #Handle blank addresses or 'me' using DDG location api
     $address = join(", ", $loc->city, $loc->region_name, $loc->country_name) 
         if ($address eq "me" or $address eq "" or not defined $address);
+
     return $event_type, $address;
 };
 
