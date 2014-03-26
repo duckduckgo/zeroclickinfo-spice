44845f199470ef10459831c29de5ea4980697e41
jdorweiler <jason@transistor.io>
2014-03-26 10:10:18 -0400

    Congress: removed youtube and only show next date for senate

--- share/spice/congress/congress.js
+++ share/spice/congress/congress.js
@@ -1,6 +1,6 @@
 function ddg_spice_congress(api_result) {
     "use strict";
-
+console.log(api_result);
     if (!api_result || !api_result.results || !api_result.results.length === 0) {
         return;
     }
@@ -54,14 +54,19 @@ function ddg_spice_congress(api_result) {
 // Creates a full name for a given representative
 Handlebars.registerHelper ('get_name', function() {
     "use strict";
-    return (this.title ? this.title + '. ' : '') + (this.first_name ? this.first_name + ' ' : '') 
-            + (this.middle_name ? this.middle_name + ' ' : '') 
+    return (this.title ? this.title + '. ' : '')
+            + (this.first_name ? this.first_name + ' ' : '') 
+            //+ (this.middle_name ? this.middle_name + ' ' : '') 
             + (this.last_name ? this.last_name : '');
 });
 
 // return the next election year
 Handlebars.registerHelper ('get_date', function() {
     "use strict";
-    return (this.term_end ? this.term_end.substring(0,4) : null);
+    if(this.chamber == 'senate'){
+        if(this.term_end)           
+            return "Next Election " + this.term_end.substring(0,4);
+    }
+    return null;
 
 });
