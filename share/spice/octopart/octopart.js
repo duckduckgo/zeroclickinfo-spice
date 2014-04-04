45988d8cfd10e4a9f889c31fd1d35467f6344fe6
russell <russell@duckduckgo.com>
2014-04-04 14:50:31 -0400

    octopart - temp

--- share/spice/octopart/octopart.js
+++ share/spice/octopart/octopart.js
@@ -1,58 +1,54 @@
 function ddg_spice_octopart (api_result) {
 
-    if(!api_result || !api_result.results || api_result.results.length === 0) {
-        return;
-    }
+    console.log(api_result);
 
-    var results = [],
-        isRelevant,
-        skip_words = [
-            "datasheet",
-            "specs",
-            "octopart"
-        ];
+    // if(!api_result || !api_result.results || api_result.results.length === 0) {
+    //     return;
+    // }
 
-    for(var i = 0; i < api_result.results.length; i += 1) {
-        var iter = api_result.results[i];
+    // var results = [],
+    //     isRelevant,
+    //     skip_words = [
+    //         "datasheet",
+    //         "specs",
+    //         "octopart"
+    //     ];
 
-        // Checks both the MPN and the manufacturer if the query is somehow relevant.
-        isRelevant = DDG.isRelevant(iter.item.mpn, skip_words, 4, true) || 
-            DDG.isRelevant(iter.item.manufacturer.displayname, skip_words, 4, true);
+    // for(var i = 0; i < api_result.results.length; i += 1) {
+    //     var iter = api_result.results[i];
 
-        // Check if we have images.
-        if(iter.item.images.length > 0 && isRelevant) {
-            results.push(iter);
-        // If an image doesn't exist, add a different image.
-        } else if(isRelevant) {
-            iter.item.images.push({
-                url_90px: "http://n1.octostatic.com/o3web/detail/images/camera-icon.png"
-            });
-            results.push(iter);
-        }
-    }
+    //     // Checks both the MPN and the manufacturer if the query is somehow relevant.
+    //     isRelevant = DDG.isRelevant(iter.item.mpn, skip_words, 4, true) || 
+    //         DDG.isRelevant(iter.item.manufacturer.displayname, skip_words, 4, true);
 
-    if(results.length === 0) {
-        return;
-    }
+    //     // Check if we have images.
+    //     if(iter.item.images.length > 0 && isRelevant) {
+    //         results.push(iter);
+    //     // If an image doesn't exist, add a different image.
+    //     } else if(isRelevant) {
+    //         iter.item.images.push({
+    //             url_90px: "http://n1.octostatic.com/o3web/detail/images/camera-icon.png"
+    //         });
+    //         results.push(iter);
+    //     }
+    // }
+
+    // if(results.length === 0) {
+    //     return;
+    // }
 
     Spice.add({
         sourceName : 'Octopart',
+        data: api_result.results,
         id: "octopart",
         sourceUrl : 'http://octopart.com/partsearch#search/requestData&q=' + api_result.request.q,
         header1 : api_result.request.q + " (Octopart)",
         view: "Tiles",
         templates: {
             item: Spice.octopart.octopart,
-            detail: Spice.octopart.octopart_details,
-            items: results
+            detail: Spice.octopart.octopart_details
         },
-        items_test: {
-            data: results,
-            image: function(item) { return item.item.images[0].url_90px; },
-            title: function(item) { return item.item.mpn; }
-            // rating: function(item) { return item.rating; },
-            // pricerange: pricerange
-        },
+
         
     });
 };
@@ -61,10 +57,4 @@ Handlebars.registerHelper("toFixed", function(number) {
     return number.toFixed(2);
 });
 
-Handlebars.registerHelper("escapeURL", function(string) {
-    return encodeURIComponent(string);
-});
 
-Handlebars.registerHelper("trim", function(string) {
-    return string.replace(/[^:]+:\s/, "");
-});
