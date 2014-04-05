f9e3d8acfbdecdc9d8f5441350ffb3abd8cdfb98
Brad Cater <bradcater@bradcater-Aspire-5750>
2014-04-05 14:08:39 -0400

    Add Maven Spice metadata.

--- lib/DDG/Spice/Maven.pm
+++ lib/DDG/Spice/Maven.pm
@@ -8,6 +8,15 @@ attribution github  => ['https://github.com/nicoulaj', 'nicoulaj'],
 
 triggers startend => "maven", "mvn";
 
+name "Maven";
+source "Maven Central Repository";
+icon_url "http://search.maven.org/favicon.ico";
+description "Search packages available on the Maven Central Repository."
+primary_example_queries "maven pegasus";
+category "software";
+topics "computing", "programming";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Maven.pm";
+
 spice to => 'http://search.maven.org/solrsearch/select?q=$1&rows=5&wt=json&callback={{callback}}';
 spice wrap_jsonp_callback => 1;
 spice proxy_cache_valid   => "418 1d";
