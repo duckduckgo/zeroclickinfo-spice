ad12ecd1f1536ce82b4bdf1eab3805861b79b79a
Jag Talon <jagtalon@duckduckgo.com>
2014-04-07 10:50:52 -0400

    InTheaters: Add detail template.
    It's a modified version of the recipes instant answer.

--- share/spice/in_theaters/in_theaters.js
+++ share/spice/in_theaters/in_theaters.js
@@ -9,14 +9,14 @@
 
 	Spice.add({
 	    id: 'in_theaters',
-	    name: 'Movies in Theaters',
+	    name: 'Now Showing',
 	    data: api_result.movies,
 	    meta: {
 		sourceName: 'Rotten Tomatoes',
-		sourceUrl: 'http://www.rottentomatoes.com/',
+		sourceUrl: 'http://www.rottentomatoes.com/movie/in-theaters/',
 		sourceIcon: true,
 		total: api_result.movies,
-		itemType: 'Movies in Theaters'
+		itemType: 'Now Showing'
 	    },
 	    normalize: function(o) {
 		// The critic's score ranges from 0 to 100, but the helper function starRating
@@ -25,45 +25,44 @@
 		return o;
 	    },
 	    templates: {
-		item: Spice.in_theaters.in_theaters
-//		detail: Spice.in_theaters.in_theaters_detail
+		item: Spice.in_theaters.in_theaters,
+		detail: Spice.in_theaters.in_theaters_detail
 	    }
 	});
     }
-}(this));
 
-// Convert minutes to hr. min. format.
-// e.g. {{time 90}} will return 1 hr. 30 min.
-Handlebars.registerHelper("time", function(runtime) {
-    var hour = 0,
-        minute = 0;
+    // Convert minutes to hr. min. format.
+    // e.g. {{time 90}} will return 1 hr. 30 min.
+    Handlebars.registerHelper("time", function(runtime) {
+	var hour = 0,
+            minute = 0;
 
-    if(runtime) {
-        if(runtime >= 60) {
-            hour = Math.floor(runtime / 60);
-            minute = runtime - (hour * 60);
-        } else {
-            minute = runtime;
-        }
-        hour = hour + 'hr. ';
-        minute += 'min.';
-        return hour + minute;
-    }
-});
+	if(runtime) {
+            if(runtime >= 60) {
+		hour = Math.floor(runtime / 60);
+		minute = runtime - (hour * 60);
+            } else {
+		minute = runtime;
+            }
+            hour = hour + ' hr ';
+            minute += ' min';
+            return hour + minute;
+	}
+    });
 
-// Guarantee that we're only going to show five movies.
-Handlebars.registerHelper("list", function(items, options) {
-    var out = "";
-    for(var i = 0; i < items.length && i < 5; i += 1) {
-        out += options.fn(items[i]);
-    }
-    return out;
-});
+    // Guarantee that we're only going to show five movies.
+    Handlebars.registerHelper("list", function(items, options) {
+	var out = "";
+	for(var i = 0; i < items.length && i < 5; i += 1) {
+            out += options.fn(items[i]);
+	}
+	return out;
+    });
 
-Handlebars.registerHelper("star_rating", function(score) {
+    Handlebars.registerHelper("star_rating", function(score) {
         var r = (score / 20) - 1,
-            s = "";
-
+        s = "";
+	
         if (r > 0) {
             for (var i = 0; i < r; i++) {
                 s += "&#9733;";
@@ -73,17 +72,19 @@ Handlebars.registerHelper("star_rating", function(score) {
         if (s.length === 0) {
             s = "";
         }
-
+	
         return s;
-});
+    });
 
-Handlebars.registerHelper("checkRating", function(critics_rating) {
-    return critics_rating || "No Rating";
-});
+    Handlebars.registerHelper("checkRating", function(critics_rating) {
+	return critics_rating || "";
+    });
 
-Handlebars.registerHelper("checkScore", function(critics_score) {
-    if(critics_score === -1) {
-        return "";
-    }
-    return ": " + critics_score + "%";
-})
+    Handlebars.registerHelper("checkScore", function(critics_score) {
+	if(critics_score === -1) {
+            return "";
+	}
+	return ": " + critics_score + "%";
+    });
+}(this));
+
