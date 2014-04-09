888897af078c9f59c017320d56e1730d12c170b7
Jag Talon <jagtalon@duckduckgo.com>
2014-04-09 01:02:52 -0400

    movie.js: initial port to bttf

--- share/spice/movie/movie.js
+++ share/spice/movie/movie.js
@@ -1,113 +1,120 @@
-function ddg_spice_movie (api_result) {
-    "use strict";
-
-    if (api_result.total === 0) {
-        return;
-    }
-
-    var ignore = ["movie", "film", "rotten", "rating", "rt", "tomatoes", "release date"];
-    var result, max_score = 0;
-
-    // Assign a ranking value for the movie. This isn't a complete sorting value though
-    // also we are blindling assuming these values exist
-    var score = function(m) {
-        var s = m.ratings.critics_score * m.ratings.audience_score;
-        if (s > max_score) max_score = s;
-        // If any above are undefined, s is undefined.
-        return s;
+(function(env) {
+    env.ddg_spice_movie = function(api_result) {
+	console.log(api_result);
     };
+}(this));
 
-    // returns the more relevant of the two movies
-    var better = function(currentbest, next) {
-        // If score() returns undefined, this is false, so we're still OK.
-        return (score(next) > score(currentbest) &&
-                (next.year > currentbest.year) &&
-                DDG.isRelevant(next.title, ignore)) ? next : currentbest;
-    };
+// function ddg_spice_movie (api_result) {
+//     "use strict";
 
-    result = DDG_bestResult(api_result.movies, better);
+//     if (api_result.total === 0) {
+//         return;
+//     }
 
-    // Favor the first result if the max score is within 1% of the score for the first result.
-    if (result !== api_result.movies[0] && Math.abs(score(api_result.movies[0]) - max_score) / max_score < 0.1) {
-        result = api_result.movies[0];
-    }
+//     var ignore = ["movie", "film", "rotten", "rating", "rt", "tomatoes", "release date"];
+//     var result, max_score = 0;
 
-    // Check if the movie that we have is relevant enough.
-    if(!DDG.isRelevant(result.title, ignore)) {
-        return;
-    }
+//     // Assign a ranking value for the movie. This isn't a complete sorting value though
+//     // also we are blindling assuming these values exist
+//     var score = function(m) {
+//         var s = m.ratings.critics_score * m.ratings.audience_score;
+//         if (s > max_score) max_score = s;
+//         // If any above are undefined, s is undefined.
+//         return s;
+//     };
 
-    var checkYear = function(year) {
-        if(year) {
-            return " (" + year + ")";
-        }
-        return "";
-    };
+//     // returns the more relevant of the two movies
+//     var better = function(currentbest, next) {
+//         // If score() returns undefined, this is false, so we're still OK.
+//         return (score(next) > score(currentbest) &&
+//                 (next.year > currentbest.year) &&
+//                 DDG.isRelevant(next.title, ignore)) ? next : currentbest;
+//     };
 
-    if ((result.synopsis && result.synopsis.length) ||
-        (result.critics_consensus && result.critics_consensus.length)) {
-        result.hasContent = true;
-    }
+//     result = DDG_bestResult(api_result.movies, better);
 
-    var data = api_result.movies.map(function(m){
-        m.rating = Math.round(m.ratings.critics_score / 20);
-        return m;
-    });
+//     // Favor the first result if the max score is within 1% of the score for the first result.
+//     if (result !== api_result.movies[0] && Math.abs(score(api_result.movies[0]) - max_score) / max_score < 0.1) {
+//         result = api_result.movies[0];
+//     }
 
-    var searchTerm = rqd.replace(new RegExp(ignore.join('|'),'i'),'').trim();
+//     // Check if the movie that we have is relevant enough.
+//     if(!DDG.isRelevant(result.title, ignore)) {
+//         return;
+//     }
 
-    Spice.add({
-        id: 'movie',
-        name: 'Movies',
+//     var checkYear = function(year) {
+//         if(year) {
+//             return " (" + year + ")";
+//         }
+//         return "";
+//     };
 
-        data: data,
+//     if ((result.synopsis && result.synopsis.length) ||
+//         (result.critics_consensus && result.critics_consensus.length)) {
+//         result.hasContent = true;
+//     }
 
-        meta: {
-            sourceName: 'Rotten Tomatoes',
-            sourceUrl: 'http://rottentomatoes.com/search/?search=' + searchTerm,
-            sourceIcon: true,
-            count: api_result.movies.length,
-            itemType: 'Movies'
-        },
+//     var data = api_result.movies.map(function(m){
+//         m.rating = Math.round(m.ratings.critics_score / 20);
+//         return m;
+//     });
 
-        templates: {
-            item: 'movie_item',
-        }
-    });
-}
+//     var searchTerm = rqd.replace(new RegExp(ignore.join('|'),'i'),'').trim();
 
-/*
- * rating_adjective
- *
- * help make the description of the movie gramatically correct
- * used in reference to the rating of the movie, as in
- *   'an' R rated movie, or
- *   'a'  PG rated movie
- */
-Handlebars.registerHelper("rating_adjective", function() {
-    "use strict";
+//     Spice.add({
+//         id: 'movie',
+//         name: 'Movies',
 
-    return (this.mpaa_rating === "R" ||
-            this.mpaa_rating === "NC-17" ||
-            this.mpaa_rating === "Unrated") ?  "an" :"a";
-});
+//         data: data,
 
-/* star rating */
-Handlebars.registerHelper("star_rating", function(score) {
-    "use strict";
+//         meta: {
+//             sourceName: 'Rotten Tomatoes',
+//             sourceUrl: 'http://rottentomatoes.com/search/?search=' + searchTerm,
+//             sourceIcon: true,
+//             count: api_result.movies.length,
+//             itemType: 'Movies'
+//         },
 
-    var r = (score / 20) - 1;
-    var s = "";
+//         templates: {
+//             item: 'movie_item',
+//         }
+//     });
+// }
 
-    if (r > 0) {
-        for (var i = 0; i < r; i++) {
-            s += "&#9733;";
-        }
-    }
+// /*
+//  * rating_adjective
+//  *
+//  * help make the description of the movie gramatically correct
+//  * used in reference to the rating of the movie, as in
+//  *   'an' R rated movie, or
+//  *   'a'  PG rated movie
+//  */
+// Handlebars.registerHelper("rating_adjective", function() {
+//     "use strict";
 
-    if (s.length === 0) {
-        s = "0 Stars";
-    }
+//     return (this.mpaa_rating === "R" ||
+//             this.mpaa_rating === "NC-17" ||
+//             this.mpaa_rating === "Unrated") ?  "an" :"a";
+// });
 
-    return s;
-});
+// /* star rating */
+// Handlebars.registerHelper("star_rating", function(score) {
+//     "use strict";
+
+//     var r = (score / 20) - 1;
+//     var s = "";
+
+//     if (r > 0) {
+//         for (var i = 0; i < r; i++) {
+//             s += "&#9733;";
+//         }
+//     }
+
+//     if (s.length === 0) {
+//         s = "0 Stars";
+//     }
+
+//     return s;
+// });
+
