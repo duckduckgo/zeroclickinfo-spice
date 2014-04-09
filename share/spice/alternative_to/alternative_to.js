34b3675d819e7429c7032e476907932877d1c02a
Jag Talon <jagtalon@duckduckgo.com>
2014-04-08 22:52:59 -0400

    alternative_to: initial port to BTTF. :cake:

--- share/spice/alternative_to/alternative_to.js
+++ share/spice/alternative_to/alternative_to.js
@@ -1,26 +1,24 @@
-function ddg_spice_alternative_to(api_result) {
+(function(env) {
     "use strict";
 
-    if(!api_result || !api_result.Items || api_result.Items.length === 0) {
-        return;
-    }
-
-    Spice.add({
-        data                     : api_result,
-        sourceName              : 'AlternativeTo',
-        sourceUrl               : api_result.Url,
-        id               : 'alternative_to',
-        
-        view: "Tiles",
-        templates         : {
-            items                : api_result.Items,
-            item: Spice.alternative_to.alternative_to,
-            // detail: Spice.alternative_to.alternative_to_details, // testing with no detail
-            li_height            : 65,
-            single_item_handler  : function(obj) {            // gets called in the event of a single result
-                obj.header1 = obj.data.Items[0].Name;         // set the header
-                obj.image_url = obj.data.Items[0].IconUrl;    // set the image
-            }
-        },
-    });
-}
+    env.ddg_spice_alternative_to = function(api_result) {
+	Spice.add({
+	    id: 'alternative_to',
+	    name: 'Alternative Software',
+	    data: api_result.Items,
+	    meta: {
+		total: api_result.Items,
+		searchTerm: 'something',
+		itemType: 'Alternative to',
+		sourceUrl: 'http://alternativeto.net/',
+		sourceName: 'AlternativeTo'
+	    },
+	    normalize: function(o) {
+		return o;
+	    },
+	    templates: {
+		item: Spice.alternative_to.alternative_to
+	    }
+	});
+    };
+}(this));
