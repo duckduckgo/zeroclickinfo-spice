e60fc6ca47fa9921a411e851f9c5a8fe14f84f30
Jag Talon <jagtalon@duckduckgo.com>
2014-04-09 00:54:25 -0400

    alternative_to.js: Fix the searchTerm

--- share/spice/alternative_to/alternative_to.js
+++ share/spice/alternative_to/alternative_to.js
@@ -1,6 +1,10 @@
 (function(env) {
     "use strict";
 
+    var script = $('[src*="/js/spice/alternative_to/"]')[0];
+    var source = $(script).attr("src");
+    var query = source.match(/alternative_to\/([^\/]*)/)[1];
+
     env.ddg_spice_alternative_to = function(api_result) {
 	Spice.add({
 	    id: 'alternative_to',
@@ -8,8 +12,8 @@
 	    data: api_result.Items,
 	    meta: {
 		total: api_result.Items,
-		searchTerm: 'something',
-		itemType: 'Alternative to',
+		searchTerm: decodeURIComponent(query),
+		itemType: 'Alternatives ',
 		sourceUrl: 'http://alternativeto.net/',
 		sourceName: 'AlternativeTo',
 		sourceIcon: true
