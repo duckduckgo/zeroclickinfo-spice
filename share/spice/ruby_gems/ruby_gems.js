43c561ef2f387c8aa9490ba7b2e77465783350ae
Jason Dorweiler <jddorweiler@gmail.com>
2014-04-10 14:38:03 -0400

    ruby: started api conversion

--- share/spice/ruby_gems/ruby_gems.js
+++ share/spice/ruby_gems/ruby_gems.js
@@ -1,30 +1,30 @@
-function ddg_spice_ruby_gems(api_result) {
-    "use strict";
-    
-    if (api_result.length === 0) {
-        return;
+(function(env) {    
+    env.ddg_spice_ruby_gems = function(api_result) {
+        "use strict";
+        
+        if (api_result.length === 0) {
+            return;
+        }
+
+        // Get the original query.
+        var script = $('[src*="/js/spice/ruby_gems/"]')[0];
+        var source = $(script).attr("src");
+        var query = source.match(/ruby_gems\/([^\/]*)/)[1];
+
+        // Display the instant answer.
+        Spice.add({
+            id: "rubygems",
+            name: "RubyGems",
+            data: api_result,
+            meta: {
+                sourceUrl: 'http://rubygems.org/search?utf8=%E2%9C%93&query=' + encodeURIComponent(query),
+                sourceName: 'RubyGems',
+                total: api_result.length,
+                itemType: decodeURIComponent(query) + " (RubyGems)",
+            },
+            templates:{
+                item: Spice.ruby_gems.item
+            }
+        });
     }
-
-    // Get the original query.
-    var script = $('[src*="/js/spice/ruby_gems/"]')[0];
-    var source = $(script).attr("src");
-    var query = source.match(/ruby_gems\/([^\/]*)/)[1];
-
-    // Display the instant answer.
-    Spice.render({
-        data             : api_result,
-        header1          : decodeURIComponent(query) + " (RubyGems)",
-        source_url       : 'http://rubygems.org/search?utf8=%E2%9C%93&query=' + encodeURIComponent(query),
-        source_name      : 'RubyGems',
-        spice_name       : 'ruby_gems',
-        template_frame   : 'list',
-        template_options : {
-            items: api_result,
-            show: 5,
-	    max: 10,
-            template_item: 'ruby_gems'
-        },
-        force_no_fold    : true,
-        force_big_header : true
-    });
-}
+}(this));
