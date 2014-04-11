806ee61cc021a63f5f72e08ca3844395b7d1d904
Silabsoft <admin@silabsoft.org>
2014-04-11 16:00:13 -0400

    modified to show high and low of buy sell rates

--- share/spice/dogecoin/dogecoin.js
+++ share/spice/dogecoin/dogecoin.js
@@ -1,35 +1,52 @@
 function ddg_spice_dogecoin(api_result) {
     "use strict";
-
+ 
     if (!api_result.last) {
         return;
     }
-
-	
-    var usd = {
-        price:"$"+api_result.last,
-        title: "USD"
+ 
+   function getRates(type){
+        var low = 0;
+        var high = 0;
+        api_result.orders.forEach(function(entry) {
+            if(entry.type === type){
+                var temp = parseFloat(entry.rate);
+                if(temp > high){
+                    high = temp;
+                }
+                if(temp < low || low === 0){
+                    low = temp;
+                }
+            }
+        });    
+        return {high:high,low:low};
+    }
+    
+ 
+    var buy = {
+        price:getRates("buy"),
+        title: "BUY"
     };
-    var doge = {
-        price: "Ð1",
-        title: "Ð"
+    var sell = {
+        price: getRates("sell"),
+        title: "SELL"
     };
     Spice.render({
-        header1           : "Dogecoin Exchange Prices: (1 DOGE)" ,
+        header1           : "Dogecoin Exchange Prices (USD)" ,
         spice_name        : "dogecoin",        
         data             : api_result,
         source_url       : 'https://prelude.io',
         source_name      : 'Prelude',
         template_normal  : 'dogecoin',
         force_favicon_url : "https://prelude.io/favicon.ico",
-
+ 
         template_frame   : "twopane",
         template_options : {
-           left : { template: "dogecoin",data: usd},
-           right : { template: "dogecoin",data: doge},
+           left : { template: "dogecoin",data: buy},
+           right : { template: "dogecoin",data: sell},
        },
         force_no_fold    : true,
        force_big_header : true
     });
-
+ 
 }
