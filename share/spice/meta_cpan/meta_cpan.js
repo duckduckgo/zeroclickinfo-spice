3b26e5563018919da1e2b4cb019c9e85a84cc92d
Jason Dorweiler <jddorweiler@gmail.com>
2014-04-11 09:53:31 -0400

    metaCPAN: update api

--- share/spice/meta_cpan/meta_cpan.js
+++ share/spice/meta_cpan/meta_cpan.js
@@ -1,23 +1,27 @@
-function ddg_spice_meta_cpan(api_response) {
-    "use strict";
+(function(env) {
+    env.ddg_spice_meta_cpan = function(api_response) {
+        "use strict";
 
-    if (!(api_response.author && api_response.version)) {
-        return;
+        if (!(api_response && api_response.author && api_response.version)) {
+            return;
+        }
+
+        var query = DDG.get_query().replace(/\s*(metacpan|meta cpan|cpanm?)\s*/i, '').replace(/-/g, '::');
+        var link = "search?q=" + encodeURIComponent(query);
+        if (api_response.module && api_response.module.length > 0 && api_response.module[0].associated_pod) {
+            link = "module/" + api_response.module[0].associated_pod;
+        }
+        console.log(api_response);
+        api_response.sourceUrl = 'https://metacpan.org/' + link;
+        api_response.sourceName = "MetaCPAN";
+
+        Spice.add({
+            id: "meta_cpan",
+            name: "MetaCPAN",
+            data: api_response,
+            templates: {
+                detail: Spice.meta_cpan.detail
+            }
+        });
     }
-
-    var query = DDG.get_query().replace(/\s*(metacpan|meta cpan|cpanm?)\s*/i, '').replace(/-/g, '::');
-    var link = "search?q=" + encodeURIComponent(query);
-    if (api_response.module && api_response.module.length > 0 && api_response.module[0].associated_pod) {
-        link = "module/" + api_response.module[0].associated_pod;
-    }
-
-    Spice.render({
-        data             : api_response,
-        header1          : query + " (MetaCPAN)",
-        source_url       : 'https://metacpan.org/' + link,
-        source_name      : 'MetaCPAN',
-        template_normal  : 'meta_cpan',
-        force_big_header : true,
-        force_no_fold    : true
-    });
-}
+}(this));
