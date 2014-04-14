27074d32aba7b5f8bc85c94e8dcec51b85da0b89
Jason Dorweiler <jddorweiler@gmail.com>
2014-04-14 17:35:45 -0400

    Expatistan: update api

--- share/spice/expatistan/expatistan.js
+++ share/spice/expatistan/expatistan.js
@@ -1,17 +1,22 @@
-function ddg_spice_expatistan(api_result) {
-    "use strict";
+(function(env){
+    env.ddg_spice_expatistan = function(api_result) {
+        "use strict";
 
-    if(!api_result || api_result.status !== 'OK') {
-        return;
-    }
-
-    Spice.add({
-        data             : api_result,
-        sourceUrl       : api_result.sourceUrl,
-        sourceName      : 'Expatistan',
-        templates: {
-            item: Spice.expatistan.expatistan,
-            detail: Spice.expatistan.expatistan
+        if(!api_result || api_result.status !== 'OK') {
+            return;
         }
-    });
-}
+
+        Spice.add({
+            id: "expatistan",
+            name: "Expatistan",
+            data             : api_result,
+            meta: {
+                sourceUrl       : api_result.sourceUrl,
+                sourceName      : 'Expatistan',
+            },
+            templates: {
+                detail: Spice.expatistan.detail
+            }
+        });
+    }
+}(this));
