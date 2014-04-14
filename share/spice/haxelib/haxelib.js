cc0474e065dcff7d9ad7b550e04e8a880e8219b0
Jason Dorweiler <jddorweiler@gmail.com>
2014-04-14 17:25:57 -0400

    haxelib: removed favicon

--- share/spice/haxelib/haxelib.js
+++ share/spice/haxelib/haxelib.js
@@ -14,7 +14,6 @@
                 itemType: response.info.name + " ("+response.info.curversion+") by " + response.info.owner,
                 sourceUrl       : 'http://lib.haxe.org/p/' + encodeURIComponent(response.info.name),
                 sourceName      : 'Haxelib',
-                sourceIconUrl: DDG.get_asset_path('haxelib', 'haxelib.org.ico')
             },
             templates: {
                 detail: Spice.haxelib.detail
