37cef8888de4302af03cf14cca5ef0b9a6d091ae
Jason Dorweiler <jddorweiler@gmail.com>
2014-04-15 13:54:37 -0400

    wordMap: update api

--- share/spice/word_map/word_map.js
+++ share/spice/word_map/word_map.js
@@ -1,22 +1,23 @@
-function ddg_spice_word_map(api_result) {
-    "use strict";
+(function(env){    
+    env.ddg_spice_word_map = function(api_result) {
+        "use strict";
 
-    if ( "200" !== api_result.result_code ) {
-	return;
+        if ( (!api_result) || ("200" !== api_result.result_code)) {
+    	return;
+        }
+
+        Spice.add({
+            id: 'word_map',
+            name: 'Word Map',
+            data             	: api_result,
+            meta: {
+                itemType: "Related to " + api_result.entry + " (Levelpump)",
+                sourceUrl: 'http://levelpump.com/graph-dictionary.php?mailLink=' + encodeURIComponent(api_result.encrypt_entry) + '&from=ddg',
+                sourceName: 'Levelpump'
+            },
+            templates: {
+                detail: Spice.word_map.detail
+            }
+        });
     }
-
-    Spice.add({
-        data             	: api_result,
-        
-        header1          	: "Related to " + api_result.entry + " (Levelpump)",
-        sourceUrl       	: 'http://levelpump.com/graph-dictionary.php?mailLink=' + encodeURIComponent(api_result.encrypt_entry) + '&from=ddg',
-        sourceName      	: 'Levelpump',
-        templates: {
-            item: Spice.word_map.word_map,
-            detail: Spice.word_map.word_map
-        },
-	force_favicon_url       : 'http://icons.duckduckgo.com/ip/www.levelpump.com.ico'
-    });
-}
-
-
+}(this));
