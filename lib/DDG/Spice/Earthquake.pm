b2ba50caebb9e2059a8f899fa1a84714942ace01
yzwx <diamanse.github@outlook.com>
2014-04-17 07:30:43 -0400

    more metadata

--- lib/DDG/Spice/Earthquake.pm
+++ lib/DDG/Spice/Earthquake.pm
@@ -2,21 +2,27 @@ package DDG::Spice::Earthquake;
 
 use DDG::Spice;
 
-name "Earthquake";
-description "Displays information about recent earthquakes worldwide";
-attribution github => ['https://github.com/', ''];
+name 'Earthquake';
+source 'USGS';
+icon_url '/i/www.usgs.gov.ico';
+description 'List recent earthquakes worldwide';
+primary_example_queries 'earthquake';
+category 'geography';
+topics 'geography';
+code_url 'https://github.com/yzwx/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Earthquake.pm';
+attribution github => ['https://github.com/yzwx', 'yzwx'];
 
 triggers startend => (
      'earthquake',
      'earthquakes'
-    );
+);
 
 spice to => 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
     return '' if $_ eq '';
-	return;
+    return;
 };
 
 1;
