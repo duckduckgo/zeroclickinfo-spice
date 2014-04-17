af4c29c94a551b89895c0a9be049123bfdd0a314
Caine Tighe <caine@duckduckgo.com>
2014-04-16 21:27:15 -0400

    DDG::Spice::Maps::Places: disable caching.

--- lib/DDG/Spice/Maps/Places.pm
+++ lib/DDG/Spice/Maps/Places.pm
@@ -4,6 +4,10 @@ use DDG::Spice;
 
 spice to => 'https://127.0.0.1/local.js?q=$1&cb={{callback}}';
 
+# no caching.
+spice proxy_cache_valid => "418 1d";
+spice is_cached => 0;
+
 triggers any => (
     'near',
     'local',
