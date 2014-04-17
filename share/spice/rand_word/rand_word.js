c459dfad815613696838c72aa09f41177a8f2cce
Russell Holt <russell@duckduckgo.com>
2014-04-16 23:34:30 -0400

    rand_word name

--- share/spice/rand_word/rand_word.js
+++ share/spice/rand_word/rand_word.js
@@ -8,6 +8,7 @@ function ddg_spice_rand_word(api_result) {
     Spice.add({
 		id: "rand_word",
         data             : api_result,
+        name: "Random Word",
         meta: {
             sourceUrl       : 'http://wordnik.com',
             sourceName      : 'Wordnik'
