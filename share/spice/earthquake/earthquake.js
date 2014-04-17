af0c5c9ce084e4440c82366ecdc8735bcaee8dbb
yzwx <diamanse.github@outlook.com>
2014-04-17 07:36:51 -0400

    minor changes

--- share/spice/earthquake/earthquake.js
+++ share/spice/earthquake/earthquake.js
@@ -1,32 +1,32 @@
 function ddg_spice_earthquake(response) {
     "use strict";
 	
-	var results = [];
+    var results = [];
 	
-	for (var i = 0; i < response.features.length; i++) {
-	    results.push(response.features[i]);
-	}
+    for (var i = 0; i < response.features.length; i++) {
+        results.push(response.features[i]);
+    }
 
-	if (!results.length) {
-		return;
-	}
+    if (!results.length) {
+        return;
+    }
 
     Spice.render({
         data             : results,
-	header1          : 'Earthquakes',
+        header1          : 'Earthquakes',
         source_name      : 'USGS',
-	source_url       : 'http://earthquake.usgs.gov/earthquakes/map/',
-	spice_name       : 'earthquake',
+        source_url       : 'http://earthquake.usgs.gov/earthquakes/map/',
+        spice_name       : 'earthquake',
         template_frame   : 'list',
-	template_options : {
-		items: results,
-		show: 2,
-		max: 10,
-		template_item: 'earthquake'
-	},
-	force_big_header : true,
-	force_space_after: true,
-	force_no_fold    : true
+        template_options : {
+            items: results,
+            show: 2,
+            max: 10,
+            template_item: 'earthquake'
+        },
+        force_big_header : true,
+        force_space_after: true,
+        force_no_fold    : true
     });
 }
 
@@ -34,5 +34,5 @@ function ddg_spice_earthquake(response) {
 Handlebars.registerHelper('UTC', function (time) {
     "use strict";
 
-    return new Date(time).toUTCString()
+    return new Date(time).toUTCString();
 });
