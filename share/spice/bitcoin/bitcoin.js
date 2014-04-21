2c675c8fc12709af2fa24ecadcb6d126c1da48cc
Jag Talon <jagtalon@duckduckgo.com>
2014-04-21 09:27:45 -0400

    bitcoin.js: initial convertion to the new Spice API.

--- share/spice/bitcoin/bitcoin.js
+++ share/spice/bitcoin/bitcoin.js
@@ -1,90 +1,96 @@
-function ddg_spice_bitcoin(api_result) {
-    "use strict"
+(function(env) {
+    env.ddg_spice_bitcoin = function(api_result) {
+	
+    };
+}(this));
 
-    if (!api_result) {
-        return;
-    }
+// function ddg_spice_bitcoin(api_result) {
+//     "use strict"
 
-    var DEFAULT_CURRENCY = "USD";
-    var symbolsAtEnd = ["PLN", "SEK"];
+//     if (!api_result) {
+//         return;
+//     }
 
-    function getFormattedPrice(currency, price, symbol) {
+//     var DEFAULT_CURRENCY = "USD";
+//     var symbolsAtEnd = ["PLN", "SEK"];
 
-        price = price.toFixed(2).toString();
-        if ($.inArray(currency, symbolsAtEnd) !== -1) {
-            return price + symbol;
-        }
-        return symbol + price;
-    }
+//     function getFormattedPrice(currency, price, symbol) {
 
-    function getQueryParams() {
+//         price = price.toFixed(2).toString();
+//         if ($.inArray(currency, symbolsAtEnd) !== -1) {
+//             return price + symbol;
+//         }
+//         return symbol + price;
+//     }
 
-        var script = $('[src*="/js/spice/bitcoin/"]')[0];
-        var source = $(script).attr("src");
-        var query = source.match(/bitcoin\/([^\/]*)/)[1];
-        query = $.trim(decodeURIComponent(query));
+//     function getQueryParams() {
 
-        if (!query) {
-            return [];
-        }
+//         var script = $('[src*="/js/spice/bitcoin/"]')[0];
+//         var source = $(script).attr("src");
+//         var query = source.match(/bitcoin\/([^\/]*)/)[1];
+//         query = $.trim(decodeURIComponent(query));
 
-        return query.split(/\s+/g);
-    }
+//         if (!query) {
+//             return [];
+//         }
+
+//         return query.split(/\s+/g);
+//     }
     
-    var params = getQueryParams();
+//     var params = getQueryParams();
 
-    var prices = null;
-    var currency = null;
+//     var prices = null;
+//     var currency = null;
 
-    if (params.length == 0) {
+//     if (params.length == 0) {
 
-        currency = DEFAULT_CURRENCY;
-        prices = api_result[currency];
-    }
-    else if (params.length > 1) {
+//         currency = DEFAULT_CURRENCY;
+//         prices = api_result[currency];
+//     }
+//     else if (params.length > 1) {
 
-        // Just allow the currency as a parameter
-        return;
-    } 
-    else {
+//         // Just allow the currency as a parameter
+//         return;
+//     } 
+//     else {
 
-        currency = $.trim(params[0].toUpperCase());
-        prices = api_result[currency];
+//         currency = $.trim(params[0].toUpperCase());
+//         prices = api_result[currency];
 
-        if (!prices) {
-            // Is not a valid currency
-            return;
-        }
-    }        
+//         if (!prices) {
+//             // Is not a valid currency
+//             return;
+//         }
+//     }        
 
-    var buy = {
-        formatted_price: getFormattedPrice(currency, prices.buy, prices.symbol),
-        title: "BUY"
-    };
+//     var buy = {
+//         formatted_price: getFormattedPrice(currency, prices.buy, prices.symbol),
+//         title: "BUY"
+//     };
 
-    var sell = {
-        formatted_price: getFormattedPrice(currency, prices.sell, prices.symbol),
-        title: "SELL"
-    };
+//     var sell = {
+//         formatted_price: getFormattedPrice(currency, prices.sell, prices.symbol),
+//         title: "SELL"
+//     };
 
-    Spice.add({
-        header1           : "Bitcoin Exchange Prices (" + currency + ")" ,
-        sourceName       : "Blockchain",
-        id        : "bitcoin",
-        sourceUrl        : 'http://markets.blockchain.info/',
-        force_favicon_url : "http://blockchain.info/favicon.ico",
+//     Spice.add({
+//         header1           : "Bitcoin Exchange Prices (" + currency + ")" ,
+//         sourceName       : "Blockchain",
+//         id        : "bitcoin",
+//         sourceUrl        : 'http://markets.blockchain.info/',
+//         force_favicon_url : "http://blockchain.info/favicon.ico",
 
-        template_frame   : "twopane",
-        templates : {
-            left : { template: "bitcoin", data: buy },
-            right : { template: "bitcoin", data: sell }
-        },
+//         template_frame   : "twopane",
+//         templates : {
+//             left : { template: "bitcoin", data: buy },
+//             right : { template: "bitcoin", data: sell }
+//         },
         
         
-    });
+//     });
 
-    // Adjust the box margins--can't do this in css
-    $("#zero_click_wrapper2 #zero_click_abstract").attr("style", 
-							"padding-left: 2px !important;" +
-							"margin-left: 2px !important;");
-}
+//     // Adjust the box margins--can't do this in css
+//     $("#zero_click_wrapper2 #zero_click_abstract").attr("style", 
+// 							"padding-left: 2px !important;" +
+// 							"margin-left: 2px !important;");
+// }
