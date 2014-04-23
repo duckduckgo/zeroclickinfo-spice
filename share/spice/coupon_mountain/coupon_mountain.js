2867600769fc105b4315e899796de1356db8d431
Zaahir Moolla <moollaza@duckduckgo.com>
2014-04-22 22:47:14 -0400

    CouponMountain: Begin port to BTTF -- WIP

--- share/spice/coupon_mountain/coupon_mountain.js
+++ share/spice/coupon_mountain/coupon_mountain.js
@@ -1,62 +1,66 @@
-function ddg_spice_coupon_mountain (api_result) {
-  "use strict";
-    
-    if (api_result.count < 1) {
-	return;
+(function(env){
+    'use strict';
+
+    env.ddg_spice_coupon_mountain = function(api_result) {
+        if (api_result.count < 1) {
+            return;
+        }
+
+        Spice.add({
+            id: 'coupon_mountain',
+            name: 'Coupon Mountain',
+            data: api_result.coupon,
+            meta: {
+                searchTerm: api_result.keyword ? encodeURIComponent(api_result.keyword) : null,
+                itemType: api_result.keyword ? api_result.keyword + ' (CouponMountain)' : 'Coupon Search (CouponMountain)',
+                sourceName: 'CouponMountain',
+                sourceUrl: 'http://www.couponmountain.com/search.php?searchtext='+ keyword
+            },
+            templates: {
+                item: Spice.coupon_mountain.item,
+                detail: Spice.coupon_mountain.detail,
+            }
+        });
+
+        // Manually trigger our callback function,
+        // item_callback doesn't fire for single result
+        if (api_result.count === 1) {
+            highlight_code();
+        }
+    };
+
+    // highlight coupon code on detail area opening
+    function highlight_code () {
+        var coupon_code = $('#coupon_code');
+        coupon_code.click(function() {
+            coupon_code.focus().select();
+        }).click();
     }
 
-	var header = api_result.keyword
-			? api_result.keyword + " (CouponMountain)"
-			: "Coupon Search (CouponMountain)",
-		keyword = encodeURIComponent(api_result.keyword);
+    Spice.registerHelper('check_expiry', function(string, options) {
+        'use strict';
 
-    console.log(api_result);
-	Spice.add({
-		data                     : api_result.coupon,
-		id               : "coupon_mountain",
-		sourceName              : 'CouponMountain',
-		sourceUrl               : 'http://www.couponmountain.com/search.php?searchtext='+ keyword,
-		templates         : {
-		    item: Spice.coupon_mountain.coupon_mountain,
-		    detail: Spice.coupon_mountain.coupon_mountain_detail,
-		}
-	});
+        // 3333-03-03 mean coupon has no expiry date
+        if (string && string != '3333-03-03'){
+            return options.fn(this);
+        } else {
+           return;
+        }
+    });
 
-	// highlight coupon code on detail area opening
-	function highlight_code () {
-		var coupon_code = $("#coupon_code");
-		coupon_code.click(function() {
-			coupon_code.focus().select();
-		}).click();
-	}
+    Spice.registerHelper('dateString', function(string) {
+        'use strict';
 
-	// Manually trigger our callback function,
-	// item_callback doesn't fire for single result
-	if (api_result.count === 1) {
-    highlight_code();
-  }
-}
+        var date = DDG.getDateFromString(string),
+            months = [ 'Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.'];
 
-Handlebars.registerHelper("check_expiry", function(string, options) {
-  "use strict";
+        return 'Expires ' + months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+    });
 
-	if (string && string != "3333-03-03"){
-		return options.fn(this);
-	} else {
-		return;
-	}
-});
+    Spice.registerHelper('stripExpiry', function(string) {
+        'use strict';
 
-Handlebars.registerHelper("dateString", function(string) {
-  "use strict";
+        return string.replace(/(offer|good through|expires|ends|valid \w+) .+$/i, '');
+    });
 
-	var date = DDG.getDateFromString(string),
-		months = [ 'Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.'];
-	return "Expires " + months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
-});
-
-Handlebars.registerHelper("stripExpiry", function(string) {
-  "use strict";
-
-	return string.replace(/(offer|good through|expires|ends|valid \w+) .+$/i, "");
-});
+})(this);
