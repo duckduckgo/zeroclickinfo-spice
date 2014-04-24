b9f83d8f1ab486ec5b6da8a7c4e11de81863e0f4
Jag Talon <jagtalon@duckduckgo.com>
2014-04-24 15:09:10 -0400

    soundcloud: displaying images now

--- share/spice/sound_cloud/sound_cloud.js
+++ share/spice/sound_cloud/sound_cloud.js
@@ -1,7 +1,24 @@
 (function(env) {
     "use strict";
 
+    var script = $('[src*="/js/spice/sound_cloud/"]')[0];
+    var source = $(script).attr("src");
+    var query = source.match(/sound_cloud\/([^\/]*)/)[1];
+
     env.ddg_spice_sound_cloud = function(api_result) {
-	console.log(api_result);
+	Spice.add({
+	    id: 'soundcloud',
+	    name: 'SoundCloud',
+	    data: api_result,
+	    meta: {
+		sourceName: 'SoundCloud',
+		sourceUrl: 'https://soundcloud.com/search?q=' + query,
+		sourceIcon: true,
+		itemType: 'Tracks'
+	    },
+	    templates: {
+		item: Spice.sound_cloud.sound_cloud
+	    }
+	});
     };
 }(this));
