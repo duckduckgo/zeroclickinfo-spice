7e9c119d24e14b3f74f1175f946dd0d84f7fa678
Zaahir Moolla <moollaza@duckduckgo.com>
2014-04-24 12:05:58 -0400

    DNS: Updated for BTTF

--- share/spice/dns/dns.js
+++ share/spice/dns/dns.js
@@ -1,34 +1,37 @@
-function ddg_spice_dns(api_result) {
-    "use strict";
+(function(env){
+    'use strict'
+    
+    env.ddg_spice_dns = function(api_result) {
 
-    if (!api_result
-            || !api_result.response
-            || !api_result.response.records
-            || api_result.response.records < 1) {
-        return;
-    }
+        if (!api_result
+                || !api_result.response
+                || !api_result.response.records
+                || api_result.response.records < 1) {
+            return;
+        }
 
-    api_result.response.records =
-        api_result.response.records.sort(
-            function(a, b) {
-                if (a.type == b.type) {
-                    return parseInt(a.priority) > parseInt(b.priority)
-                } else {
-                  return a.type > b.type;
-                }
-            });
+        api_result.response.records =
+            api_result.response.records.sort(
+                function(a, b) {
+                    if (a.type == b.type) {
+                        return parseInt(a.priority) > parseInt(b.priority)
+                    } else {
+                      return a.type > b.type;
+                    }
+                });
 
-    Spice.add({
-        data              : api_result.response,
-        //header1         : api_result.query.domain + ' (ViewDNS)',
-        sourceUrl        : 'http://www.viewdns.info/dnsrecord/?domain='
-                            + api_result.query.domain,
-        sourceName       : 'ViewDNS',
-        templates: {
-            item: Spice.dns.dns,
-            detail: Spice.dns.dns
-        },
-        force_favicon_url : 'http://viewdns.info/favicon.ico',
-        
-    });
-}
+        Spice.add({
+            id: 'dns',
+            name: 'DNS Records',
+            data: api_result.response,
+            meta: {
+                sourceUrl: 'http://www.viewdns.info/dnsrecord/?domain=' + api_result.query.domain,
+                sourceName: 'ViewDNS',
+                sourceIconUrl: 'http://viewdns.info/favicon.ico'
+            },
+            templates: {
+                detail: Spice.dns.detail
+            }
+        });
+    };
+})(this);
