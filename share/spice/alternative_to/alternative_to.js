4a5b4f51d9d50d7f6c1a344ce72edc36956c83d3
Zaahir Moolla <moollaza@duckduckgo.com>
2014-04-25 22:33:34 -0400

    AlternativeTo: cleanup code for tutorial, changed normalize fn to HB helper

--- share/spice/alternative_to/alternative_to.js
+++ share/spice/alternative_to/alternative_to.js
@@ -1,34 +1,27 @@
 (function(env) {
     "use strict";
 
-    function get_platform(Platforms) {
-	if(Platforms.length > 1) {
-	    return "Multiplatform";
-	}
-	return Platforms[0];
-    }
-    
     env.ddg_spice_alternative_to = function(api_result) {
-	Spice.add({
-	    id: 'alternative_to',
-	    name: 'Alternative Software',
-	    data: api_result.Items,
-	    meta: {
-		searchTerm: api_result.Name,
-		itemType: 'Alternatives ',
-		sourceUrl: 'http://alternativeto.net/',
-		sourceName: 'AlternativeTo',
-		sourceIcon: true
-	    },
-	    normalize: function(o) {
-		return { 
-		    title: o.Name,
-		    Platforms: get_platform(o.Platforms)
-		};
-	    },
-	    templates: {
-		item: Spice.alternative_to.item,
-	    }
-	});
+        Spice.add({
+            id: 'alternative_to',
+            name: 'Alternative Software',
+            data: api_result.Items,
+            meta: {
+                searchTerm: api_result.Name,
+                itemType: 'Alternatives ',
+                sourceUrl: 'http://alternativeto.net/',
+                sourceName: 'AlternativeTo',
+                sourceIcon: true
+            },
+            templates: {
+                item: Spice.alternative_to.item,
+            }
+        });
     };
+
+    Handlebars.registerHelper("getPlatform", function (platforms) {
+
+        return (platforms.length > 1) ? "Multiplatform" : platforms[0];
+    });
+
 }(this));
