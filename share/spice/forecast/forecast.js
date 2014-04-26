6992b0eb487deb86ffa2ed575e4b416393aeec94
Russell Holt <russell@duckduckgo.com>
2014-04-26 17:18:10 -0400

    template group updates

--- share/spice/forecast/forecast.js
+++ share/spice/forecast/forecast.js
@@ -268,7 +268,6 @@ function ddg_spice_forecast(r) {
         },
 
         templates: {
-            // custom_item: Spice.forecast.forecast_item,
             item_custom: Spice.forecast.forecast_item,
             detail_mobile: Spice.forecast.forecast_detail_mobile
         }
