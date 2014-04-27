df149e58de63a3e9ef838b995b3547c9fa86be0f
Zaahir Moolla <moollaza@duckduckgo.com>
2014-04-26 20:55:48 -0400

    Songkick GetEventId: cleanup, fixed indentation

--- share/spice/songkick/geteventid/songkick_geteventid.js
+++ share/spice/songkick/geteventid/songkick_geteventid.js
@@ -1,86 +1,79 @@
 (function(env) {
     "use strict";
 
-    $.ajaxSetup({
-	cache: true
-    });
+    $.ajaxSetup({cache: true});
 
     var metadata = {};
+    
     env.ddg_spice_songkick_geteventid = function(api_result) {
 
-	console.log(api_result);
+        var totalEntries = DDG.getProperty(api_result, 'resultsPage.results.totalEntries'),
+            location = DDG.getProperty(api_result, 'resultsPage.results.location'),
+            considered_location = DDG.getProperty(api_result, 'resultsPage.results.location.0')
 
-	if(!api_result || !api_result.resultsPage || !api_result.resultsPage.results) {
-	    return;
-	}
+        if( !(totalEntries.length && location.length)) {
+            return;
+        }
 
-	if(api_result.resultsPage.results.totalEntries === 0 || api_result.resultsPage.results.length === 0) {
-	    return;
-	}
+        var metro_area_id = considered_location.metroArea.id;
 
-	if(!api_result.resultsPage.results.location || api_result.resultsPage.results.location.length === 0) {
-	    return;
-	}
+        metadata = {
+            metro_area_uri: considered_location.metroArea.uri,
+            city: considered_location.city.displayName,
+            metro_area: considered_location.metroArea.displayName
+        };
 
-	var considered_location = api_result.resultsPage.results.location[0];
-	var metro_area_id = considered_location.metroArea.id;
-	metadata = {
-	    metro_area_uri: considered_location.metroArea.uri,
-	    city: considered_location.city.displayName,
-	    metro_area: considered_location.metroArea.displayName
-	};
-	
-
-	console.log("LOG");
-	$.getScript("/js/spice/songkick/events/" + metro_area_id);
+        $.getScript("/js/spice/songkick/events/" + metro_area_id);
     };
-
-    function padding(t) {
-	return  +t < 10 ? "0" + t : t;
-    }
-
-    function twelve_hour(t) {
-	if(!t) {
-	    return;
-	}
-	
-	var time = t.split(/:/),
-	    hours = +time[0],
-	    minutes = +time[1];
-
-	return hours % 12 + ":" + padding(minutes) + (hours > 12 ? 'PM' : 'AM'); 
-    }
 
     env.ddg_spice_songkick_events = function(api_result) {
-	var events = api_result.resultsPage.results.event;
+        var events = api_result.resultsPage.results.event;
 
-	Spice.add({
-	    id: 'songkick',
-	    name: 'Concerts',
-	    data: events,
-	    meta: {
-		sourceName: 'Songkick',
-		sourceUrl: encodeURI(metadata.metro_area_uri),
-		sourceIcon: true,
-		itemType: 'Concerts in ' + metadata.city
-	    },
-	    normalize: function(o) {
-		if(o.performance.length > 0 && o.venue.displayName !== "Unknown venue") {
-		    return {
-			artist: o.performance[0].displayName,
-			venue: o.venue.displayName,
-			start: {
-			    date: o.start.date,
-			    time: twelve_hour(o.start.time)
-			}
-		    };
-		}
+        Spice.add({
+            id: 'songkick',
+            name: 'Concerts',
+            data: events,
+            meta: {
+                sourceName: 'Songkick',
+                sourceUrl: encodeURI(metadata.metro_area_uri),
+                searchTerm: metadata.city,
+                itemType: 'Concerts'
+            },
+            normalize: function(item) {
+                if(item.performance.length > 0 && item.venue.displayName !== "Unknown venue") {
+                    return {
+                        artist: item.performance[0].displayName,
+                        venue: item.venue.displayName,
+                        start: {
+                            date: item.start.date,
+                            time: twelve_hour(item.start.time)
+                        }
+                    };
+                }
 
-		return null;
-	    },
-	    templates: {
-		item: Spice.songkick_geteventid.item
-	    }
-	});
+                return null;
+            },
+            templates: {
+                item: Spice.songkick_geteventid.item
+            }
+        });
     };
+
+    // Pad timestring with '0' if needed
+    function padding(t) {
+        return  +t < 10 ? "0" + t : t;
+    }
+
+    // convert military time to 12H
+    function twelve_hour(t) {
+        if(!t) {
+            return;
+        }
+
+        var time = t.split(/:/),
+            hours = +time[0],
+            minutes = +time[1];
+
+        return hours % 12 + ":" + padding(minutes) + (hours > 12 ? 'PM' : 'AM'); 
+    }
 }(this));
