25232c484044a139d84d7985932ebda1c3462fd0
Zaahir Moolla <moollaza@duckduckgo.com>
2014-04-26 20:52:53 -0400

    Bootic: cleanup, fixed indentation

--- share/spice/bootic/bootic.js
+++ share/spice/bootic/bootic.js
@@ -2,48 +2,46 @@
     "use strict";
 
     env.ddg_spice_bootic = function(api_result) {
-	if($.isEmptyObject(api_result.products)) {
-	    return;
-	}
+        if($.isEmptyObject(api_result.products)) {
+            return;
+        }
 
-	var query = api_result.input_query ? 
-	    '?initial=1&q=' + encodeURIComponent(api_result.input_query) : '';
+        var query = api_result.input_query ? 
+            '?initial=1&q=' + encodeURIComponent(api_result.input_query) : '';
 
-	var result = [];
-	for(var i = 0; i < api_result.sorted.length; i++) {
-	    result.push(api_result.products[api_result.sorted[i]]);
-	}
+        var result = [];
+        for(var i = 0; i < api_result.sorted.length; i++) {
+            result.push(api_result.products[api_result.sorted[i]]);
+        }
 
-	Spice.add({
-	    id: 'bootic',
-	    name: 'Bootic',
-	    data: result,
-	    meta: {
-		sourceName: 'Bootic',
-		sourceUrl: 'http://www.bootic.com/?q=' + query,
-		sourceIcon: true,
-		itemType: api_result.input_query
-	    },
-	    normalize: function(o) {
-		var picture = o.pictures[0];
-		picture.replace(/_pictures\/\d+x\d+/, "_pictures/200x300");
-		picture = "http://static.bootic.com/_pictures/" + picture;
+        Spice.add({
+            id: 'bootic',
+            name: 'Bootic',
+            data: result,
+            meta: {
+                sourceName: 'Bootic',
+                sourceUrl: 'http://www.bootic.com/?q=' + query,
+                itemType: api_result.input_query
+            },
+            normalize: function(item) {
+                var picture = item.pictures[0];
+                picture.replace(/_pictures\/\d+x\d+/, "_pictures/200x300");
+                picture = "http://static.bootic.com/_pictures/" + picture;
 
-		return {
-		    rating: "Unrated",
-		    parentId: o.id,
-		    url: "http://bootic.com" + o.url,
-		    img: picture,
-		    title: o.name,
-		    heading: o.name,
-		    brand: o.shop_name,
-		    price: "$" + o.price
-		};
-	    },
-	    templates: {
-		item: 'products_item'
-	    }
-	});
+                return {
+                    rating: "unrated",
+                    parentId: item.id,
+                    url: "http://bootic.com" + item.url,
+                    img: picture,
+                    title: item.name,
+                    heading: item.name,
+                    brand: item.shop_name,
+                    price: "$" + item.price
+                };
+            },
+            templates: {
+                item: 'products_item'
+            }
+        });
     };
 }(this));
-
