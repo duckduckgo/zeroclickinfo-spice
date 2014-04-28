1f1f254e014acccb6305ec2282fc0479c4553c50
Jason Dorweiler <jddorweiler@gmail.com>
2014-04-27 23:46:55 -0400

    cpan: update for record template

--- share/spice/meta_cpan/meta_cpan.js
+++ share/spice/meta_cpan/meta_cpan.js
@@ -1,37 +1,27 @@
 (function(env) {
-    env.ddg_spice_meta_cpan = function(api_response) {
+    env.ddg_spice_meta_cpan = function(api_result) {
         "use strict";
 
-        if (!(api_response && api_response.author && api_response.version)) {
+        if (!(api_result && api_result.author && api_result.version)) {
             return;
         }
 
         var query = DDG.get_query().replace(/\s*(metacpan|meta cpan|cpanm?)\s*/i, '').replace(/-/g, '::');
         var link = "search?q=" + encodeURIComponent(query);
-        if (api_response.module && api_response.module.length > 0 && api_response.module[0].associated_pod) {
-            link = "module/" + api_response.module[0].associated_pod;
+        if (api_result.module && api_result.module.length > 0 && api_result.module[0].associated_pod) {
+            link = "module/" + api_result.module[0].associated_pod;
         }
 
         Spice.add({
             id: "meta_cpan",
             name: "MetaCPAN",
-            data: {response: api_response, list: ['Abstract','Author','Version','Description']},
+            data: {record_values : api_result, record_keys: ['abstract','author','version','description']},
             meta: {
                 sourceName: "MetaCPAN",
                 sourceUrl: 'https://metacpan.org/' + link,
             },
-
-            normalize : function(item) {
-                var desc = item.response.pod;
-                return {
-                    'Author': item.response.author || null,
-                    'Description': ((desc.length > 340) ? desc.slice(0, 340) + ' ...' : null),
-                    'Version': item.response.version || null,
-                    'Abstract': item.response.abstract || null
-                };
-            },
             templates: {
-                detail: DDG.templates.record_detail,
+                detail: DDG.templates.record
             }
         });
     }
