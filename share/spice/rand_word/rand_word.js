47d86ef01a0915a986faf1205a087198c38e2c2f
Jason Dorweiler <jddorweiler@gmail.com>
2014-04-29 13:13:59 -0400

    rand_word: template group

--- share/spice/rand_word/rand_word.js
+++ share/spice/rand_word/rand_word.js
@@ -1,21 +1,26 @@
-function ddg_spice_rand_word(api_result) {
+(function(env) {
     "use strict";
+    env.ddg_spice_rand_word = function(api_result) {
 
     if (!api_result.word) {
         return;
     }
 
     Spice.add({
-	id: "rand_word",
+	   id: "rand_word",
         data: api_result,
         name: "Random Word",
         meta: {
             sourceUrl: 'http://wordnik.com',
             sourceName: 'Wordnik',
-	    sourceIcon: true
+            sourceIcon: true
         },
+        template_group: 'info',
         templates: {
-            detail: Spice.rand_word.detail
-        },
+                options: {
+                    content: Spice.rand_word.content
+                }
+            }
     });
 }
+}(this));
