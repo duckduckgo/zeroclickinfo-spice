908fcd837a962ad6c7edc73a531d2027f390f3bd
Caine Tighe <caine@duckduckgo.com>
2014-05-06 18:12:47 -0400

    Chains: move to building RE in core and exporting file to prevent using internal lib functions in ZCI-Spice.

--- lib/DDG/Spice/Maps/Chains.pm
+++ lib/DDG/Spice/Maps/Chains.pm
@@ -1,9 +1,6 @@
 package DDG::Spice::Maps::Chains;
 
 use DDG::Spice;
-use DDG::Util::Regexp;
-use DDG::Util::Constants qw( make_regex );
-use DDG::Util::String qw( trim_ws );
 
 spice to => 'https://127.0.0.1/local.js?q=$1&cb={{callback}}';
 
@@ -11,11 +8,11 @@ spice to => 'https://127.0.0.1/local.js?q=$1&cb={{callback}}';
 spice proxy_cache_valid => "418 1d";
 spice is_cached => 0;
 
-my @chains = map { lc(trim_ws($_)) } share('chains.txt')->slurp;
-my $chains_qr = make_regex(\@chains);
+# Chains regular expression built from core DDG.
+my $chains_re = share('chains_re')->slurp;
 
 # consider query_clean and strip_grammar permutation on slurp
-triggers query_lc => qr/^($chains_qr)$/;
+triggers query_lc => qr/^($chains_re)$/ios;
 
 handle query_lc => sub {
     return $_;
