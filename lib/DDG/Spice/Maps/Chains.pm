8e8db368cfcd01c4757553ea74714eede1cb86a5
Caine Tighe <caine@duckduckgo.com>
2014-05-06 18:15:46 -0400

    Chains: capture already happens upon re generation.

--- lib/DDG/Spice/Maps/Chains.pm
+++ lib/DDG/Spice/Maps/Chains.pm
@@ -12,7 +12,7 @@ spice is_cached => 0;
 my $chains_re = share('chains_re')->slurp;
 
 # consider query_clean and strip_grammar permutation on slurp
-triggers query_lc => qr/^($chains_re)$/ios;
+triggers query_lc => qr/^$chains_re$/ios;
 
 handle query_lc => sub {
     return $_;
