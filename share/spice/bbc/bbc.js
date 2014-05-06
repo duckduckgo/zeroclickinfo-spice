acdae176dc97c9ea19090ae9a28ba017bd76b9e6
Doug Brown <doug@duckduckgo.com>
2014-05-05 20:29:36 -0400

    use .tile--vid styling for bbc for correct aspect-ratio on tile images

--- share/spice/bbc/bbc.js
+++ share/spice/bbc/bbc.js
@@ -65,7 +65,7 @@
                 item: 'basic_image_item',
                 detail: Spice.bbc.detail,
                 options: {
-                    tileClass: 'tile--c--w'
+                    tileClass: 'tile--vid  tile--c'
                 }
             }
         });
