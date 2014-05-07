f3a69d7594c484776f7d4efb760edc17897b568b
Caine Tighe <caine@duckduckgo.com>
2014-05-07 10:04:55 -0400

    Chains (js): use places js. HT @russellholt

--- share/spice/maps/chains/maps_chains.js
+++ share/spice/maps/chains/maps_chains.js
@@ -1,24 +1,10 @@
-// execute dependencies immediately on file parse
-nrj("/js/mapbox/mapbox-1.5.2.js",1);
-nrc("/js/mapbox/mapbox-1.5.2.css",1);
+// load places since this is a subset
+nrj(DDG.get_asset_path('maps/places', 'maps_places.spice.js'));
 
 // the plugin callback by another name
 var ddg_spice_maps_chains = function (chains) {
-
-    // sub function where 'chains' is always defined
-    var f2 = function() {
-        // check for the mapbox object
-        if (!window["L"]) {
-
-            // wait for it
-            window.setTimeout(f2, 50);
-
-            // could check for a max value here
-            return;
-        }
-        DDG.duckbar.add_local(chains);
-    };
-
-    f2();
+    if (typeof(ddg_spice_maps_places) === 'function') {
+	ddg_spice_maps_places(chains);
+    }
 };
 
