72b75bb1a04288f301fa709767ac980e89dd8653
Brian <brian@duckduckgo.com>
2014-05-12 15:50:38 -0400

    fmt

--- share/spice/zipcode/zipcode.js
+++ share/spice/zipcode/zipcode.js
@@ -15,41 +15,41 @@ DDG.require('maps',function(){
             foundMatch = 0;
 
 
-		// Display the Spice plugin.
-		Spice.add({
+        // Display the Spice plugin.
+        Spice.add({
 
-			id: 'zipcode',
-			name: 'Places',
-			from: 'maps', // shouldn't have to do this, backend is sending maps signal, not zipcode
-			data: api_result.places.place,
-			view: 'Map',
-			model: 'Location',
+            id: 'zipcode',
+            name: 'Places',
+            from: 'maps', // shouldn't have to do this, backend is sending maps signal, not zipcode
+            data: api_result.places.place,
+            view: 'Map',
+            model: 'Location',
 
-			normalize: function(item) {
-				var zip = item.name.replace(/\s+/, "");
+            normalize: function(item) {
+                var zip = item.name.replace(/\s+/, "");
 
-				if (!foundMatch && zip === searchZip && (searchCountry === 'ZZ' || item['country attrs'].code === searchCountry)) {
-					var ne = item.boundingBox.northEast,
-						sw = item.boundingBox.southWest,
-						polygon = [
-							[ne.longitude,ne.latitude],
-							[ne.longitude,sw.latitude],
-							[sw.longitude,sw.latitude],
-							[sw.longitude,ne.latitude]
-						];
+                if (!foundMatch && zip === searchZip && (searchCountry === 'ZZ' || item['country attrs'].code === searchCountry)) {
+                    var ne = item.boundingBox.northEast,
+                        sw = item.boundingBox.southWest,
+                        polygon = [
+                            [ne.longitude,ne.latitude],
+                            [ne.longitude,sw.latitude],
+                            [sw.longitude,sw.latitude],
+                            [sw.longitude,ne.latitude]
+                        ];
 
-					foundMatch = true;
+                    foundMatch = true;
 
-					return {
-						name: zip,
-						address: [item.admin2,item.admin1].join(", "),
-						coordinates: item.centroid,
-						// polygonPoints: polygon
-					};
-				}
+                    return {
+                        name: zip,
+                        address: [item.admin2,item.admin1].join(", "),
+                        coordinates: item.centroid,
+                        // polygonPoints: polygon
+                    };
+                }
 
-				return null;
-			}
-		});
+                return null;
+            }
+        });
     }
 });
