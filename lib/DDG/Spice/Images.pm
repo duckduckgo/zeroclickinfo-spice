b175824cc0b53195706c60af0802ca4fce3f64f2
Caine Tighe <caine@duckduckgo.com>
2014-05-18 14:02:52 -0400

    Move localhost refrences to duckduckgo.com in preparation for launch.

--- lib/DDG/Spice/Images.pm
+++ lib/DDG/Spice/Images.pm
@@ -4,7 +4,7 @@ use DDG::Spice;
 
 use String::Trim;
 
-spice to => 'https://127.0.0.1/i.js?q=$1&o=json&cb={{callback}}';
+spice to => 'https://duckduckgo.com/i.js?q=$1&o=json&cb={{callback}}';
 
 my %skip = map { $_ => 0 } (
     'google image',
