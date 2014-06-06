e82cde377ebf6b5c2007a3e7a90cc99aca30ce15
devongundry <devon@koa.la>
2014-06-06 13:59:40 -0700

    use api key as a query parameter

--- lib/DDG/Spice/BitcoinBalance.pm
+++ lib/DDG/Spice/BitcoinBalance.pm
@@ -20,7 +20,7 @@ attribution github => ['https://github.com/chain-engineering','chain.com'],
 triggers query_raw => qr/^[13][1-9A-HJ-NP-Za-km-z]{26,33}$/;
 # This regular expression identifies the unique properties of a Bitcoin Address.
 
-spice to => 'https://{{ENV{BITCOIN_BALANCE_SPICE_APIKEY}}}@api.chain.com/v1/bitcoin/addresses/$1';
+spice to => 'https://api.chain.com/v1/bitcoin/addresses/$1?key={{ENV{BITCOIN_BALANCE_SPICE_APIKEY}}}';
 # The Chain API requires an API key. Chain has granted the spice a free account with unlimited access. We can provide this to DuckDuckGo before release.
 
 spice wrap_jsonp_callback => 1;
