9505a66268e505100b38591ab12b1a61e15c0fa3
Alex Acevedo <hackallofit@hackallofit-inspiron-530s>
2014-06-08 23:14:37 -0400

    Added copious attribution info.

--- lib/DDG/Spice/Pokedex.pm
+++ lib/DDG/Spice/Pokedex.pm
@@ -8,6 +8,13 @@ triggers startend => 'pokedex', 'pokedéx', 'pokemon', 'pokémon';
 
 spice to => 'http://pokeapi.co/api/v1/pokemon/$1/';
 
+attribution web => ['http://alex-acevedo.info', 'Alex Acevedo']
+			github => ['http://github.com/nes630', 'nes630']
+			twitter => "hackallofit"
+			email => ['hackallofit@outlook.com', 'hackallofit']
+			facebook => ['http://www.facebook.com/alex.acevedo.1232', 'Alex Acevedo']
+
+
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
