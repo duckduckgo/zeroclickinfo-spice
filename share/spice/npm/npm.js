bf1e7089386896a0f62478987d5d423dfa0c1638
Dax the Duck <dax@duckduckhack.(none)>
2014-06-10 16:28:31 -0400

    minor changes

--- share/spice/npm/npm.js
+++ share/spice/npm/npm.js
@@ -8,9 +8,9 @@ function ddg_spice_npm (api_result) {
   Spice.render({
     data              : api_result,
     force_big_header  : true,
-    header1           : api_result.name + ' (' + api_result.version + ')',
-    source_name       : "npmjs.org", // More at ...
-    source_url        : 'http://npmjs.org/package/' + api_result.name,
+    header1           : api_result.name + ' --- ' + api_result.version,
+    source_name       : "spoonacular.com", // More at ...
+    source_url        : 'http://spoonacular.com/' + api_result.name,
     template_normal   : 'npm',
     template_small    : 'npm'
   });
