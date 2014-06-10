64038262e24491119927588ff4b28d403bfc620a
Ado Matejov <a.matejov@centrum.sk>
2014-06-10 18:08:47 +0200

    template

--- share/spice/similar_sites/similar_sites.js
+++ share/spice/similar_sites/similar_sites.js
@@ -1,35 +1,44 @@
 (function(env) {
 
-env.ddg_spice_similar_sites = function(api_result) {
+    env.ddg_spice_similar_sites = function(api_result) {
 
-    var script = $('[src*="/js/spice/similar_sites/"]')[0],
-            source = $(script).attr("src"),
-            query = source.match(/similar_sites\/([^\/]+)/)[1];
+        var script = $('[src*="/js/spice/similar_sites/"]')[0],
+                source = $(script).attr("src"),
+                query = source.match(/similar_sites\/([^\/]+)/)[1];
 
-    Spice.add({
-        id: "SimilarSites",
-        name: "SimilarSites",
+        if(!api_result || !api_result.SimilarSites || api_result.SimilarSites.length === 0) {
+            return Spice.failed('similar_sites');
+        }
 
-        data: api_results.SimilarSites,
+        Spice.add({
+            id: "SimilarSites",
+            name: "Similar Sites",
 
-        meta: {
-            total: api_results.SimilarSites.length,
-            sourceName: 'SimilarSites',
-            sourceUrl: 'http://www.similarsites.com/site/' + query,
-            itemType: 'Similar Sites'
-        },
+            data: api_result.SimilarSites,
 
-        normalize = function(item){
-            return {
-                title: item.url,
-                url: item.Url,
-                rating: parseFloat(item.Score) * 100
-            };
-        },
+            meta: {
+                total: api_result.SimilarSites.length,
+                sourceName: 'SimilarSites',
+                sourceUrl: 'http://www.similarsites.com/site/' + query,
+                itemType: 'Similar Sites'
+            },
 
-        template_group: 'products',
-    });
+            normalize : function(item){
+                return {
+                    title: item.Url,
+                    url: "http://" + item.Url,
+                    rating: item.Score*100
+                };
+            },
 
-}
+            templates: {
+                group: 'products_simple',
+                options: {
+                        rating: true,
+                    }
+            }
+        });
+
+    }
 
 })(this);
