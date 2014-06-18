8256cbe3fed504af67a26c26734a16abf927e8bb
Zaahir Moolla <moollaza@gmail.com>
2014-06-18 13:35:11 -0400

    cleanup gravatar.js fix indentation, wrap in closure, change template_group -> group

--- share/spice/gravatar/gravatar.js
+++ share/spice/gravatar/gravatar.js
@@ -1,98 +1,89 @@
-// Description:
-// Shows information about a Gravatar user.
-//
-// Dependencies:
-// None.
-//
-// Commands:
-// gravatar gravatar@duckduckgo.com - shows information about the e-mail address.
-// gravatar matt - shows information about a user.
+(function(env) {
+        "use strict";
 
-function ddg_spice_gravatar (api_result) {
-    "use strict";
+        env.ddg_spice_gravatar = function(api_result) {
 
-    // Check for errors.
-    if(!api_result || !api_result.entry || api_result.entry.length === 0) {
-        return Spice.failed('gravatar');
-    }
+            // Check for errors.
+            if (!api_result || !api_result.entry || api_result.entry.length === 0) {
+                return Spice.failed('gravatar');
+            }
 
-    // Get the name of the user (if available).
-    var getName = function(entry) {
-        if(!entry.name || !entry.displayName) {
-            return;
+            // Display the spice plugin.
+            Spice.add({
+                data: api_result,
+                id: "gravatar",
+                name: "Avatar",
+
+                meta: {
+                    sourceName: "Gravatar",
+                    sourceUrl: api_result.entry[0].profileUrl,
+                    sourceIconUrl: 'http://gravatar.com/favicon.ico'
+                },
+                normalize: function() {
+                    return {
+                        image: api_result.entry[0].thumbnailUrl + ".png",
+                        title: getName(api_result.entry[0])
+                    };
+                },
+                templates: {
+                    group: 'info',
+                    options: {
+                        content: Spice.gravatar.content
+                    }
+                },
+            });
         }
 
-        if(entry.name.formatted) {
-            return entry.name.formatted;
-        } else if(entry.name.givenName && entry.name.familyName) {
-            return entry.name.givenName + " " + entry.name.familyName;
-        }
-        return entry.displayName;
-    };
+        // Get the name of the user (if available).
+        var getName = function(entry) {
+            if (!entry.name || !entry.displayName) {
+                return;
+            }
 
-    // Display the spice plugin.
-    Spice.add({
-        data              : api_result,
-		id: "gravatar",
-        name: "Avatar",
-        
-        meta: {
-            sourceName       : "Gravatar",
-            sourceUrl        : api_result.entry[0].profileUrl
-        },
-        normalize: function() {
-            return {
-                image         : api_result.entry[0].thumbnailUrl + ".png",
-                title         : getName(api_result.entry[0])
-            };
-        },
-        template_group: 'info',
+            if (entry.name.formatted) {
+                return entry.name.formatted;
+            } else if (entry.name.givenName && entry.name.familyName) {
+                return entry.name.givenName + " " + entry.name.familyName;
+            }
+            return entry.displayName;
+        };
 
-        templates: {
-	    options: {
-		content: Spice.gravatar.content
-	    }
-        },
+        // Find the primary e-mail.
+        Handlebars.registerHelper("Gravatar_getEmail", function(emails, options) {
+            "use strict";
 
-        force_favicon_url : 'http://gravatar.com/favicon.ico'
-    });
-}
+            // Check if the variable exists.
+            if (!emails) {
+                return;
+            }
 
-// Find the primary e-mail.
-Handlebars.registerHelper("Gravatar_getEmail", function(emails, options) {
-    "use strict";
+            // Find the primary email.
+            for (var i = 0; i < emails.length; i += 1) {
+                if (emails[i].primary) {
+                    return options.fn(emails[i]);
+                }
+            }
+        });
 
-    // Check if the variable exists.
-    if(!emails) {
-        return;
-    }
+        // If we don't have any information to display, just show this.
+        Handlebars.registerHelper("Gravatar_fallbackInfo", function(emails, aboutMe, currentLocation, accounts, context, options) {
+            "use strict";
 
-    // Find the primary email.
-    for(var i = 0; i < emails.length; i += 1) {
-        if(emails[i].primary) {
-            return options.fn(emails[i]);
-        }
-    }
-});
+            if (!emails && !aboutMe && !currentLocation && !accounts) {
+                return options.fn(context);
+            }
+        });
 
-// If we don't have any information to display, just show this.
-Handlebars.registerHelper("Gravatar_fallbackInfo", function(emails, aboutMe, currentLocation, accounts, context, options) {
-    "use strict";
+        // This is for favicons that don't work.
+        Handlebars.registerHelper("Gravatar_checkDomain", function(domain) {
+            "use strict";
 
-    if(!emails && !aboutMe && !currentLocation && !accounts) {
-        return options.fn(context);
-    }
-});
-
-// This is for favicons that don't work.
-Handlebars.registerHelper("Gravatar_checkDomain", function(domain) {
-    "use strict";
-
-    if(domain === "plus.google.com") {
-        return "google.com";
-    }
-    if(domain === "yelp.com") {
-        return "/iu/?u=http://s3-media2.ak.yelpcdn.com/assets/2/www/img/118ff475a341/ico/favicon.ico";
-    }
-    return domain;
-});
+            if (domain === "plus.google.com") {
+                return "google.com";
+            }
+            if (domain === "yelp.com") {
+                return "/iu/?u=http://s3-media2.ak.yelpcdn.com/assets/2/www/img/118ff475a341/ico/favicon.ico";
+            }
+            return domain;
+        });
+    }();
