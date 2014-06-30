b00dbd286b62b47f04e4fb5d1cf34d5b87c5a2fd
Usman Raza <usman.r123@gmail.com>
2014-06-30 15:20:25 +0500

    moved global variables to function

--- lib/DDG/Spice/TodayInHistory.pm
+++ lib/DDG/Spice/TodayInHistory.pm
@@ -13,7 +13,7 @@ primary_example_queries "today in history", "this day in history";
 secondary_example_queries "historical events on 27 June";
 category "dates";
 topics "everyday","trivia";
-code_url "https://github.com/duckduckgo/zeroclickinfo-goodie/blob/master/lib/DDG/Spice/TodayInHistory.pm";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/TodayInHistory.pm";
 attribution github => ["https://github.com/killerfish", "Usman Raza"],
             twitter => ["https://twitter.com/f1shie", "Usman Raza"];
 
@@ -23,73 +23,63 @@ triggers startend => "today in history";
 triggers start => "historical events on", "this day in history";
 
 # define variables
-my $currentDay, my $currentMonth, my $currentYear;
-my $givenDay, my $givenMonth, my $month;
-my $parameter1, my $parameter2;
 my %days = ('1'=>'31','2'=>'29','3'=>'31','4'=>'30','5'=>'31','6'=>'30','7'=>'31','8'=>'31','9'=>'30','10'=>'31','11'=>'30','12'=>'31');
 
 # functions:
 # check and process if we got 2 parameters (month and day)
-sub readParameters {
+sub readParameters {	
+	# read parameters - do not trigger if invalid parameters are found (delimiters: - / . ' ')
+  	my ($parameter1, $parameter2) = split (/[-\/. ]/, $_[0]);
   	if ($parameter2) {
+		my $value;
     		# e.g. "historical events on June 12"
-    		return 1 if((readMonth($parameter1)) && (readDay($parameter2)));
+    		return $value if (($value = readparams($parameter1, $parameter2)) ne 'fail');
     		# e.g. "historical events on 16 Sep"
-    		return 1 if((readMonth($parameter2)) && (readDay($parameter1)));
+    		return $value if (($value = readparams($parameter2, $parameter1)) ne 'fail');
    	} 
-  	# no parameter -> "today in history"
+  	# no parameter - use today date
    	else {
-   		useToday();
-    		return 1;
+   		return useToday();
   	}
   	return;
 }
 
-# check if parameter is a valid month (using strptime to allow different notations)
-sub readMonth {
+# validate parameters and return day_month on success
+sub readparams {
+	my $givenDay, my $givenMonth, my $month, my $evalfail;
   	# month name as full name (April), in abbreviated form (Apr) or as 2 digit (04) 
   	my @monthPatterns = ('%B', '%b', '%m');
-
   	for my $monthPattern (@monthPatterns) {
-    		return 1 if(eval {
-      			my $validDate = Time::Piece->strptime("2000/$_[0]/1", "%Y/".$monthPattern."/%d");
+      		$evalfail = 1 if ( eval {
+			my $validDate = Time::Piece->strptime("2000/$_[0]/1", "%Y/".$monthPattern."/%d");
       			$givenDay = 0;
       			$month = $validDate->mon;
       			$givenMonth = $validDate->fullmonth;
-    		})
+		})	
   	}
-}
-
-# check if parameter is a valid day (given month)
-sub readDay {
-		return 0 if($_[0] =~ m/\D+/);
-      		return 0 if($_[0] > 31 || $_[0] > $days{$month});
-		if($_[0] =~ m/^0{1}([1-9]){1}$/)
-		{
-        		$givenDay = $1;
-		} 
-		else {
-      			$givenDay = $_[0];
-		}
-		return 1;
+	return 'fail' if(!$evalfail);
+	return 'fail' if($_[1] =~ m/\D+/);
+      	return 'fail' if($_[1] > 31 || $_[1] > $days{$month});
+	if($_[1] =~ m/^0{1}([1-9]){1}$/) {
+        	$givenDay = $1;
+	} else {
+      		$givenDay = $_[1];
+	}
+	return $givenDay."_".$givenMonth;
 }
 
 # use today's date if no parameter passed
 sub useToday {
-  	$givenDay = $currentDay;
-  	$givenMonth = $currentMonth;
+        my $t = DateTime->now;
+        $t->set_time_zone($loc->time_zone);
+	return $t->mday."_".$t->month_name;
 }
 
 handle remainder => sub {
 	# check current date in users timezone
-        my $t = DateTime->now;
-        $t->set_time_zone($loc->time_zone);
-        $currentDay = $t->mday;
-        $currentMonth = $t->month_name;
-
-	# read parameters - do not trigger if invalid parameters are found (delimiters: - / . ' ')
-  	($parameter1, $parameter2) = split (/[-\/. ]/, $_);
-  	return if (!readParameters());
-	return $givenDay."_".$givenMonth;
+  	if (my $var = readParameters($_)) {
+		return $var;
+	}
+	return;
 };	
 1;
