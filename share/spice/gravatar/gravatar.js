64cf604572e683184337c2bfdeefac6bd55a2716
Zaahir Moolla <moollaza@duckduckgo.com>
2014-06-30 16:13:06 -0400

    Gravatar - add missing closing paren, fix indentation, remove unnecessary use strict statements

--- share/spice/gravatar/gravatar.js
+++ share/spice/gravatar/gravatar.js
@@ -1,89 +1,83 @@
 (function(env) {
-        "use strict";
+    "use strict";
 
-        env.ddg_spice_gravatar = function(api_result) {
+    env.ddg_spice_gravatar = function(api_result) {
 
-            // Check for errors.
-            if (!api_result || !api_result.entry || api_result.entry.length === 0) {
-                return Spice.failed('gravatar');
-            }
-
-            // Display the spice plugin.
-            Spice.add({
-                data: api_result,
-                id: "gravatar",
-                name: "Avatar",
-
-                meta: {
-                    sourceName: "Gravatar",
-                    sourceUrl: api_result.entry[0].profileUrl,
-                    sourceIconUrl: 'http://gravatar.com/favicon.ico'
-                },
-                normalize: function() {
-                    return {
-                        image: api_result.entry[0].thumbnailUrl + ".png",
-                        title: getName(api_result.entry[0])
-                    };
-                },
-                templates: {
-                    group: 'info',
-                    options: {
-                        content: Spice.gravatar.content
-                    }
-                },
-            });
+        // Check for errors.
+        if (!api_result || !api_result.entry || api_result.entry.length === 0) {
+            return Spice.failed('gravatar');
         }
 
-        // Get the name of the user (if available).
-        var getName = function(entry) {
-            if (!entry.name || !entry.displayName) {
-                return;
-            }
+        // Display the spice plugin.
+        Spice.add({
+            data: api_result,
+            id: "gravatar",
+            name: "Avatar",
 
-            if (entry.name.formatted) {
-                return entry.name.formatted;
-            } else if (entry.name.givenName && entry.name.familyName) {
-                return entry.name.givenName + " " + entry.name.familyName;
-            }
-            return entry.displayName;
-        };
-
-        // Find the primary e-mail.
-        Handlebars.registerHelper("Gravatar_getEmail", function(emails, options) {
-            "use strict";
-
-            // Check if the variable exists.
-            if (!emails) {
-                return;
-            }
-
-            // Find the primary email.
-            for (var i = 0; i < emails.length; i += 1) {
-                if (emails[i].primary) {
-                    return options.fn(emails[i]);
-                }
-            }
+            meta: {
+                sourceName: "Gravatar",
+            sourceUrl: api_result.entry[0].profileUrl,
+            sourceIconUrl: 'http://gravatar.com/favicon.ico'
+            },
+            normalize: function() {
+                return {
+                    image: api_result.entry[0].thumbnailUrl + ".png",
+        title: getName(api_result.entry[0])
+                };
+            },
+            templates: {
+                group: 'info',
+        options: {
+            content: Spice.gravatar.content
+        }
+            },
         });
+    };
 
-        // If we don't have any information to display, just show this.
-        Handlebars.registerHelper("Gravatar_fallbackInfo", function(emails, aboutMe, currentLocation, accounts, context, options) {
-            "use strict";
+    // Get the name of the user (if available).
+    function getName (entry) {
+        if (!entry.name || !entry.displayName) {
+            return;
+        }
 
-            if (!emails && !aboutMe && !currentLocation && !accounts) {
-                return options.fn(context);
+        if (entry.name.formatted) {
+            return entry.name.formatted;
+        } else if (entry.name.givenName && entry.name.familyName) {
+            return entry.name.givenName + " " + entry.name.familyName;
+        }
+        return entry.displayName;
+    }
+
+    // Find the primary e-mail.
+    Handlebars.registerHelper("Gravatar_getEmail", function(emails, options) {
+        // Check if the variable exists.
+        if (!emails) {
+            return;
+        }
+
+        // Find the primary email.
+        for (var i = 0; i < emails.length; i += 1) {
+            if (emails[i].primary) {
+                return options.fn(emails[i]);
             }
-        });
+        }
+    });
 
-        // This is for favicons that don't work.
-        Handlebars.registerHelper("Gravatar_checkDomain", function(domain) {
-            "use strict";
+    // If we don't have any information to display, just show this.
+    Handlebars.registerHelper("Gravatar_fallbackInfo", function(emails, aboutMe, currentLocation, accounts, context, options) {
+        if (!emails && !aboutMe && !currentLocation && !accounts) {
+            return options.fn(context);
+        }
+    });
 
-            if (domain === "plus.google.com") {
-                return "google.com";
-            }
-            if (domain === "yelp.com") {
-                return "/iu/?u=http://s3-media2.ak.yelpcdn.com/assets/2/www/img/118ff475a341/ico/favicon.ico";
-            }
-            return domain;
-        });
-    }();
+    // This is for favicons that don't work.
+    Handlebars.registerHelper("Gravatar_checkDomain", function(domain) {
+        if (domain === "plus.google.com") {
+            return "google.com";
+        }
+        if (domain === "yelp.com") {
+            return "/iu/?u=http://s3-media2.ak.yelpcdn.com/assets/2/www/img/118ff475a341/ico/favicon.ico";
+        }
+        return domain;
+    });
+}());
