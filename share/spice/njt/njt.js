f9a1e7b4fa64a0f9ee2a211651df3f5c2cae0c20
Matthew Ramina <mattramina@gmail.com>
2014-07-02 17:45:29 -0400

    NJT: visual redesign

--- share/spice/njt/njt.js
+++ share/spice/njt/njt.js
@@ -14,31 +14,32 @@
             name: 'NJ Transit',
             data: sorted,
             meta: {
-                heading: "Commuter Trains from " + api_result.origin + " to " + api_result.destination,
+                heading: api_result.origin + " to " + api_result.destination,
                 sourceUrl: api_result.url,
                 sourceName: "NJ Transit"
             },
             templates: {
-                group: 'text',
-                detail: false,
-                item_detail: false,
-                options: {
-                    footer: Spice.njt.footer
-                }
+                item_custom: Spice.njt.train_item
             },
             normalize: function(item){
-                var status_class = "";
-                if (!item.status) {item.status = "On Time";}
-                if (item.status === "Cancelled") {status_class = "njt__cancelled";}
-                else if (item.status == "Delayed" || delayed(item)) {status_class = "njt__delayed";}
-                else if (item.status == "All Aboard") {status_class = "njt__allaboard";}
-                else if (item.status == "Boarding" || item.status == "Stand By") {status_class = "njt__boarding";}
+                var status_class = "",
+                    cancelled = false,
+                    classes = {
+                        "Cancelled": "njt__cancelled",
+                        "Delayed": "njt__delayed",
+                        "All Aboard": "njt__allaboard",
+                        "Boarding": "njt__boarding",
+                        "Stand By": "njt__boarding"
+                    };
+
+                if (!item.status) item.status = "On Time";
                 return {
-                    url: api_result.url,
-                    title: format_time(item.departure_time) + " (" + item.train + ")",
-                    subtitle: item.line,
-                    description: "Arrives " + format_time(item.arrival_time),
-                    status_class: status_class
+                    departure_time: actualDepartureTime(item),
+                    arrival_time: format_time(timeInMins(item.arrival_time)),
+                    status: actualStatus(item),
+                    line: item.line.replace('Line', ''),
+                    cancelled: item.status === "Cancelled",
+                    status_class: delayedMins(item) > 0 ? "njt__delayed" : classes[item.status]
                 };
             }
         });
@@ -52,10 +53,10 @@
         return s;
     }
 
-    //takes a time string from format 14:05:00 and converts it to human-readable (2:05 PM)
+    //takes a time in minutes and converts it to human-readable (2:05 PM)
     function format_time(t){
-        var hour = parseInt(t.split(':')[0]),
-            minute = parseInt(t.split(':')[1]),
+        var hour = Math.floor(t / 60),
+            minute = t % 60,
             ampm = (hour >= 24) ? 'AM' : (hour >= 12) ? 'PM' : 'AM';
         if (hour > 24){
             hour -= 24;
@@ -71,15 +72,34 @@
         return hour*60 + minute;
     }
 
-    function delayed(item){
+    //find how many minutes the train is delayed
+    function delayedMins(item){
         var match = new RegExp(/In (\d+) Min/).exec(item.status);
         if (match && match.length > 1){
             var mins = parseInt(match[1]),
                 now = new Date(),
                 nmins = now.getMinutes(),
                 nhours = now.getHours();
-            return timeInMins(item.departure_time) < nhours*60 + nmins + mins;
+            return (nhours*60 + nmins + mins) - timeInMins(item.departure_time);
         }
-        return false;
+        return 0;
+    }
+
+    function actualDepartureTime(item){
+        return format_time(timeInMins(item.departure_time) + delayedMins(item));
+    }
+
+    function actualStatus(item){
+        if (delayedMins(item) > 0){
+            return 'Delayed from ' + format_time(timeInMins(item.departure_time));
+        }
+        if (item.status === "Boarding") {
+            return 'Now Boarding'
+        }
+        var match = new RegExp(/In (\d+) Min/).exec(item.status);
+        if (match && match.length > 1){
+            return 'Departs in ' + match[1] + 'm';
+        }
+        return item.status;
     }
 }(this));
