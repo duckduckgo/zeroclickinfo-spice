4cb940e6d51248cace1f804b57278272169f7d6f
Matthew Ramina <mattramina@gmail.com>
2014-07-02 13:59:52 -0400

    NJT: javascript cleanup, comments

--- lib/DDG/Spice/NJT.pm
+++ lib/DDG/Spice/NJT.pm
@@ -17,6 +17,8 @@ spice to => 'http://njt-api.appspot.com/njt/times/$1';
 spice wrap_jsonp_callback => 1;
 spice proxy_cache_valid => "418 1d";
 
+#load a list of stops so we don't trigger this if we don't get njt stops
+#(the triggers are similar to SEPTA's)
 my @stops = share('stops.txt')->slurp;
 
 sub is_stop {
@@ -33,8 +35,11 @@ handle remainder => sub {
 	my $orig = $1;
 	my $dest = $3;
 	if (is_stop($orig) and is_stop($dest)){
+		#lowercase the stop names found in the query and change the spaces to dashes
 		my $orig = join "-", map { lc } split /\s+/, $orig;
 		my $dest = join "-", map { lc } split /\s+/, $dest;
+		#if the word between the two stop names is "to", then we're going from the first stop to the second
+		#if it's "from", then we're going from the second stop to the first
 		return $2 eq 'to' ? ($orig, $dest) : ($dest, $orig);
 	}
 	return;
