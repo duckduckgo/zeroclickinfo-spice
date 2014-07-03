3576ebe6c77caacccf24eb0976f5e5c0209a5ca1
rodrigue-h <rodrigue.hajjar@gmail.com>
2014-07-03 22:42:39 +0000

    moved triggers to text file

--- lib/DDG/Spice/Kwixer.pm
+++ lib/DDG/Spice/Kwixer.pm
@@ -18,24 +18,9 @@ attribution twitter => ['kwixerapp','Kwixer'],
 spice to => 'https://www.kwixer.com/api/search?filter=movie&take=40&source=ddg&lang=en&query=$1';
 spice wrap_jsonp_callback => 1;
 
-my @triggers = ('new movies with','new movies featuring','new movies starring',
-				'new movie with','new movie featuring','new movie starring',
-				'newest movies with','newest movies featuring','newest movies starring',
-				'newest movie with','newest movie featuring','newest movie starring',
-				'new films with','new films starring','new films featuring',
-				'new film with','new film starring','new film featuring',
-				'newest films with','newest films starring','newest films featuring',
-				'newest film with','newest film starring','newest film featuring',
-	'movie with','movies with', 'movies starring','film with','films with','films starring','film starring','movies featuring','films featuring',
-    'movies by','movies directed by','movies director', 
-    'movie by','movie directed by','movie director',  
-	'films by','films directed by','films director',
-    'film by','film directed by','film director',
-    'director',
-	'actor','actress',
-	'kwixer');
+my @triggers = share("triggers.txt")->slurp;
 triggers start => @triggers;
-triggers end => ('actor','actress', 'director');
+#triggers end => ('actor','actress', 'director');
 handle query => sub {
 	return $_ if $_;
   	return;
