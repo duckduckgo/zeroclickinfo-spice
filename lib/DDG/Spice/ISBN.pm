70827c910c77d751a8981b8940da92925f2cd79f
nishanths <nishanth.gerrard@gmail.com>
2014-07-09 01:36:10 -0700

    Change to "media" template

--- lib/DDG/Spice/ISBN.pm
+++ lib/DDG/Spice/ISBN.pm
@@ -1,28 +1,26 @@
 package DDG::Spice::ISBN;
-# ABSTRACT: Returns book details for a query ISBN
+# ABSTRACT: Returns book details for a query ISBN.
 
 use DDG::Spice;
 
-primary_example_queries "isbn 0330287001", "isbn number 0-06-250217-4";
-secondary_example_queries "0-330-2870 0-1  ISBN", "978 0 7432 4722 1 isbn number";
-description "Returns book details for a query ISBN";
-name "ISBN Search";
-source "https://openlibrary.org/";
-code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/ISBN.pm";
-category "reference";
-topics "everyday", "entertainment", "special_interest";
-attribution github => ["https://github.com/nishanths", "Nishanth Shanmugham"],
-            twitter => ["https://twitter.com/nshanmugham", "Nishanth Shanmugham"],
-            web => ["http://nishanths.github.io", "Nishanth Shanmugham"];
+primary_example_queries 	"isbn 0330287001", "ISBN number 0-06-250217-4";
+secondary_example_queries 	"0-330-2870 0-1  ISBN", "978 0 7432 4722 1 ISBN lookup";
+name 						"ISBN";
+description 				"Returns book details for a query ISBN";
+source 						"https://openlibrary.org/";
+code_url 					"https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/ISBN.pm";
+category 					"reference";
+topics 						"everyday", "entertainment", "special_interest";
+attribution github 	=> 		["https://github.com/nishanths/", "Nishanth Shanmugham"],
+            twitter => 		["https://twitter.com/nshanmugham/", "Nishanth Shanmugham"],
+            web 	=> 		["http://nishanths.github.io/", "Nishanth Shanmugham"];
 
-triggers startend => 'isbn', 'isbn number';
-spice to => 'https://openlibrary.org/api/books?bibkeys=ISBN:$1&callback={{callback}}&format=javascript&jscmd=data';
+triggers startend	=> 		'isbn', 'isbn number', 'isbn lookup';
+spice to 			=> 		'https://openlibrary.org/api/books?bibkeys=ISBN:$1&callback={{callback}}&format=javascript&jscmd=data';
 
 handle remainder => sub {
-    $_ =~ s/\s//g; # remove all whitespace
-    $_ =~ s/-//g; # remove '-'
-
-    return $_ if (/^\d{9}$|^\d{10}$|^\d{13}$/); # only number strings of length 9 or 10 or 13
+    $_ =~ s/[\s-]//g; 						# remove all whitespace and hyphens
+    return $_ if (/^\d{10}$|^\d{13}$/); 	# only number strings of length 10 or 13
     return;
 };
 
--- share/spice/isbn/isbn.js
+++ share/spice/isbn/isbn.js
@@ -1,53 +1,68 @@
 (function (env) {
     "use strict";
-    env.ddg_spice_isbn = function(api_result) {
+    env.ddg_spice_isbn = function (api_result) {
 
-        if (api_result.error) {
-            return Spice.failed('ISBN');
+        if (!api_result || api_result.error) { 
+            return Spice.failed('isbn'); 
         }
 
-        // Get the enveloping property name from the response
-        var response_envelope;
-       	for (var name in api_result) { 
-       		response_envelope = name; // only one property
-       	}
-
-        // Format the title to title case (sometimes the received response has inconsistent formatting)
-        api_result[response_envelope].title = api_result[response_envelope].title.toTitleCase();
-        api_result[response_envelope].publishers[0].name = api_result[response_envelope].publishers[0].name.toTitleCase();
-
+        api_result = api_result[Object.keys(api_result)[0]]; // Strip off envelope
+        
         Spice.add({
-            id: "isbn",
-            name: "Book",
-            data: api_result[response_envelope],
+            id: 'isbn',
+            name: 'Book',
+            data: api_result,
+            signal: 'high',
+
+            meta: {
+                itemType: 'Book',
+                sourceName: 'Open Library',
+                sourceUrl: api_result.url
+            },
+
+            normalize: function (item) {
+                var main_title = toTitleCase(item.title); // fail if not present
+                if (DDG.getProperty(item, "authors.0.name")) {
+                    main_title += "(" + item.authors[0].name +")";
+                }
+                return {
+                    img             :   DDG.getProperty(item.cover, "medium"),
+                    heading         :   main_title, 
+                    publisher       :   toTitleCase(item.publishers[0].name), // fail if not present
+                    year            :   /\d{4}/.exec(DDG.getProperty(item, "publish_date")), // get only the year
+                    pages           :   DDG.getProperty(item, "number_of_pages"),
+                    url             :   item.url // fail if not present
+                };
+            },
+
             templates: {
-                group: 'base',
-                options:{
-                    content: Spice.isbn.content,
-                    moreAt: false,
+                group: 'media',
+                options: {
+                    subtitle_content: Spice.isbn.subtitle_content,
+                    buy: Spice.isbn.buy,
+                    ratingText: false
                 }
             }
         });
-    };
 
-    // To Title Case 2.1 – http://individed.com/code/to-title-case/
-    // Copyright © 2008–2013 David Gouch. Licensed under the MIT License.
-    String.prototype.toTitleCase = function(){
-        "use strict";
-        var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
-        return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
-        if (index > 0 && index + match.length !== title.length &&
-            match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-            (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-            title.charAt(index - 1).search(/[^\s-]/) < 0) {
-            return match.toLowerCase();
-        }
-        if (match.substr(1).search(/[A-Z]|\../) > -1) {
-            return match;
-        }
-        return match.charAt(0).toUpperCase() + match.substr(1);
-      });
-    };
+        // modified version of https://github.com/gouch/to-title-case/blob/master/to-title-case.js
+        function toTitleCase (string) {
+            var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+            return string.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
+            if (index > 0 && index + match.length !== title.length &&
+              match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
+              (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
+              title.charAt(index - 1).search(/[^\s-]/) < 0) {
+              return match.toLowerCase();
+            }
 
+            if (match.substr(1).search(/[A-Z]|\../) > -1) {
+              return match;
+            }
+
+            return match.charAt(0).toUpperCase() + match.substr(1);
+          });
+        };
+
+    };
 }(this));
-
