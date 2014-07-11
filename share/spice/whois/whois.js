1343798a959dff90328d94bbb8dbbb5656dc00d7
b1ake <blake@blakejennelle.com>
2014-07-11 19:00:51 +0000

    Rename api_output -> api_result, which is DDG-preferred variable name.

--- share/spice/whois/whois.js
+++ share/spice/whois/whois.js
@@ -5,23 +5,23 @@
     var is_debug = false;
 
     // spice callback function
-    env.ddg_spice_whois = function(api_output) {
+    env.ddg_spice_whois = function(api_result) {
         // for debugging
-        if(is_debug) console.log('api_output:', api_output);
+        if(is_debug) console.log('api_result:', api_result);
 
 	// Check for API error and exit early if found
 	// (with error message when in debug mode)
-	if (!api_output || api_output.error || !api_output.WhoisRecord) {
-	    if(is_debug) console.log("Error with whois API. api_output:", api_output || 'undefined');
+	if (!api_result || api_result.error || !api_result.WhoisRecord) {
+	    if(is_debug) console.log("Error with whois API. api_result:", api_result || 'undefined');
 
 	    return Spice.failed('whois');
 	}
 
 	// normalize the api output
-	api_output = normalize_api_output(api_output);
+	api_result = normalize_api_result(api_result);
 
         // is the domain available?
-	var is_avail = is_domain_available(api_output);
+	var is_avail = is_domain_available(api_result);
 
 	// if the domain isn't available, do we want to show
 	// whois information?
@@ -33,10 +33,10 @@
 	// decide which template to show, if any
 	if(is_avail) {
 	    // show message saying the domain is available
-	    show_available(api_output);
+	    show_available(api_result);
 	} else if(is_whois_allowed) {
 	    // show whois info for the domain
-	    show_whois(api_output);
+	    show_whois(api_result);
 	} else {
 	    // by default, show nothing
 	}
@@ -45,8 +45,8 @@
 
     // Returns whether the domain is available,
     // based on the API result that was returned.
-    var is_domain_available = function(api_output) {
-	return !api_output['registered'];
+    var is_domain_available = function(api_result) {
+	return !api_result['registered'];
     };
 
     // Returns whether we should show whois data if this
@@ -61,16 +61,16 @@
 	return /\s/.test($.trim(query));
     };
 
-    var normalize_api_output = function(api_output) {
+    var normalize_api_result = function(api_result) {
 
 	// store the domain's various contacts in an array.
 	//
 	// we'll iterate through this array in order, using
 	// info from the first contact that contains the field we want.
 	var contacts = [
-	    api_output.WhoisRecord.registrant,
-	    api_output.WhoisRecord.administrativeContact,
-	    api_output.WhoisRecord.technicalContact
+	    api_result.WhoisRecord.registrant,
+	    api_result.WhoisRecord.administrativeContact,
+	    api_result.WhoisRecord.technicalContact
 	];  
 
 	// return the normalized output as a hash
@@ -79,8 +79,8 @@
 	    // these first fields are not displayed
 	    // (hence the camelCase, which the user will not see)
 
-	    'domainName': api_output.WhoisRecord.domainName,
-	    'registered': !!api_output.WhoisRecord.registrant, // boolean flag
+	    'domainName': api_result.WhoisRecord.domainName,
+	    'registered': !!api_result.WhoisRecord.registrant, // boolean flag
 
 	    // the remaining fields are displayed
 	    // (hence the user-friendly capitalization and spaces)
@@ -89,11 +89,11 @@
 	    'Email': get_first_by_key(contacts, 'email'),
 
 	    // trim dates so they are shown without times
-	    'Last updated': api_output.WhoisRecord.updatedDate
-	                    && api_output.WhoisRecord.updatedDate.replace(/^(.*)?\s(.*)?$/, '$1'),
+	    'Last updated': api_result.WhoisRecord.updatedDate
+	                    && api_result.WhoisRecord.updatedDate.replace(/^(.*)?\s(.*)?$/, '$1'),
 
-	    'Expires': api_output.WhoisRecord.expiresDate
-	               && api_output.WhoisRecord.expiresDate.replace(/^(.*)?\s(.*)?$/, '$1'),
+	    'Expires': api_result.WhoisRecord.expiresDate
+	               && api_result.WhoisRecord.expiresDate.replace(/^(.*)?\s(.*)?$/, '$1'),
 
 	};
     }
@@ -122,14 +122,14 @@
     }
 
     // Data that's shared between the two Spice.add calls.
-    var get_shared_spice_data = function(api_output) {
+    var get_shared_spice_data = function(api_result) {
 	return {
             id: "whois",
             name: "Whois",
             meta: {
 		sourceName: "Whois API",
 		sourceUrl: 'http://www.whoisxmlapi.com/whois-api-doc.php#whoisserver/WhoisService?rid=2&domainName='
-		    + api_output.domainName
+		    + api_result.domainName
 		    + '&outputFormat=json&target=raw'
             },
             templates: {
@@ -142,26 +142,26 @@
     };
 
     // Show message saying that the domain is available.
-    var show_available = function(api_output) {
-	if(is_debug) console.log('api result in show_available', api_output);
+    var show_available = function(api_result) {
+	if(is_debug) console.log('api result in show_available', api_result);
 
-	var shared_spice_data = get_shared_spice_data(api_output);
+	var shared_spice_data = get_shared_spice_data(api_result);
 
 	// add the attributes specific to this template
-        shared_spice_data.data = api_output;
+        shared_spice_data.data = api_result;
 	shared_spice_data.templates.options.content = Spice.whois.available;
 
 	Spice.add(shared_spice_data);
     };
 
     // Show whois info for the domain using the 'record' template.
-    var show_whois = function(api_output) {
+    var show_whois = function(api_result) {
 
-	var shared_spice_data = get_shared_spice_data(api_output);
+	var shared_spice_data = get_shared_spice_data(api_result);
 
 	// add the attributes specific to this template
 	shared_spice_data.data = {
-	    'record_data': api_output,
+	    'record_data': api_result,
 	    'record_keys': ['Registered to', 'Email', 'Last updated', 'Expires']
 	};
 	shared_spice_data.templates.options.content = 'record';
