f892ebe7caa22af6713eeeea5cf83920f601eeba
Sam <sam@bigcurl.de>
2014-07-11 20:58:21 +0200

    Added spice for wasgehtheuteab

---
+++ lib/DDG/Spice/WGHA.pm
@@ -0,0 +1,33 @@
+package DDG::Spice::WGHA;
+# ABSTRACT: Search for events on was geht heute ab.de.
+
+use DDG::Spice;
+
+primary_example_queries "was geht in frankfurt";
+secondary_example_queries "was geht in berlin";
+description "Events in Germany";
+name "WGHA Events";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/WGHA.pm";
+topics "entertainment", "special_interest";
+category  "entertainment";
+attribution web => ['http://www.wgha.de','Samuel Goebert'],
+            email => ['http://www.wgha.de','Samuel Goebert'];
+
+my @triggers = ( "was geht", "wasgeht", "party", "parties", "partys", "feiern",
+  "aus gehen", "ausgehen", "nachtleben", "weg gehen", "weggehen", "tanzen",
+  "club", "clubs", "disco", "event", "events", "veranstaltung" );
+
+triggers any => @triggers;
+
+spice to => 'http://localhost:3000/duckduckgo/events.json?q=$1&callback={{callback}}&api_key={{ENV{DDG_SPICE_WGHA_APIKEY}}}';
+spice proxy_cache_valid => "200 1h";
+
+handle query_lc => sub {
+  my $input = $_;
+  # return if $loc->country_code ne 'DE';
+  # print $loc->city;
+  map { return $input if $input =~ s/(^|\s)$_(\s|$)// and $input ne '' } @triggers;
+  return;
+};
+
+1;
---
+++ t/WGHA.t
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use DDG::Test::Spice;
+
+use DDG::Test::Location;
+use DDG::Request;
+
+ddg_spice_test(
+    [qw( DDG::Spice::WGHA )],
+    DDG::Request->new(
+        query_raw => "was geht heute ab in frankfurt",
+        location => test_location("de")
+    ) => test_spice(
+        '/js/spice/wgha/heute%20ab%20in%20frankfurt',
+        call_type => 'include',
+        caller => 'DDG::Spice::WGHA',
+        is_cached => 0,
+    ),
+    DDG::Request->new(
+        query_raw => "was geht in berlin",
+        location => test_location("de")
+    ) => test_spice(
+        '/js/spice/wgha/in%20berlin',
+        caller    => 'DDG::Spice::WGHA',
+        is_cached => 0,
+    ),
+);
+
+done_testing;
+
