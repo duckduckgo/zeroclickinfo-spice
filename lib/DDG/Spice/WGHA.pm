08743c3f2717123e8ae1cab4ce1cc616152fe18f
Sam <sam@bigcurl.de>
2014-07-18 21:07:20 +0200

    added real valid input for proxy_cache_valid

--- lib/DDG/Spice/WGHA.pm
+++ lib/DDG/Spice/WGHA.pm
@@ -20,7 +20,7 @@ my @triggers = ( "was geht", "wasgeht", "party", "parties", "partys", "feiern",
 triggers any => @triggers;
 
 spice to => 'http://www.wasgehtheuteab.de/duckduckgo/events.json?q=$1&callback={{callback}}&api_key={{ENV{DDG_SPICE_WGHA_APIKEY}}}';
-spice proxy_cache_valid => "200 1h";
+spice proxy_cache_valid => "200 60m";
 
 handle query_lc => sub {
   return $_;
