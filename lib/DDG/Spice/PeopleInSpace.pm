d76e66c4146d6cdf4c30ef92eceaba7988a46a2d
elebow <elebow@users.noreply.github.com>
2014-07-18 17:10:25 -0400

    PeopleInSpace: Add missing commas.

--- lib/DDG/Spice/PeopleInSpace.pm
+++ lib/DDG/Spice/PeopleInSpace.pm
@@ -15,17 +15,17 @@ attribution github  => ["elebow", "Eddie Lebow"];
 #python3 << EEE
 #for n in ["people", "humans", "astronauts", "cosmonauts"]:
 #	for p in ["space", "orbit"]:
-#		print('"%s in %s", "%s are in %s", "%s who are in %s"' % (n, p, n, p, n, p))
+#		print('"%s in %s", "%s are in %s", "%s who are in %s",' % (n, p, n, p, n, p))
 #EEE
 
-triggers any => "people in space", "people are in space", "people who are in space"
-    "people in orbit", "people are in orbit", "people who are in orbit"
-    "humans in space", "humans are in space", "humans who are in space"
-    "humans in orbit", "humans are in orbit", "humans who are in orbit"
-    "astronauts in space", "astronauts are in space", "astronauts who are in space"
-    "astronauts in orbit", "astronauts are in orbit", "astronauts who are in orbit"
-    "cosmonauts in space", "cosmonauts are in space", "cosmonauts who are in space"
-    "cosmonauts in orbit", "cosmonauts are in orbit", "cosmonauts who are in orbit"
+triggers any => "people in space", "people are in space", "people who are in space",
+    "people in orbit", "people are in orbit", "people who are in orbit",
+    "humans in space", "humans are in space", "humans who are in space",
+    "humans in orbit", "humans are in orbit", "humans who are in orbit",
+    "astronauts in space", "astronauts are in space", "astronauts who are in space",
+    "astronauts in orbit", "astronauts are in orbit", "astronauts who are in orbit",
+    "cosmonauts in space", "cosmonauts are in space", "cosmonauts who are in space",
+    "cosmonauts in orbit", "cosmonauts are in orbit", "cosmonauts who are in orbit",
     "who's in space", "who's in orbit", "who is in space", "who is in orbit", "anyone in space", "anyone in orbit",
     "whose in space", "whose in orbit";
 
