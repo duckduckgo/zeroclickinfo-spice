c5251030bbaa39962f7b739be0250e57bfd67d48
Dylan Lloyd <dylan@dylansserver.com>
2014-07-18 21:04:07 -0700

    use closure & callback for shorten.js & fixes

--- share/spice/shorten/shorten.js
+++ share/spice/shorten/shorten.js
@@ -1,37 +1,37 @@
-function ddg_spice_shorten(api_result) {
+(function(env) {
     "use strict";
+    env.ddg_spice_shorten = function (api_result) {
+        // Exit immediately if we find an error message.
+        if (!api_result || !api_result.shorturl || api_result.errorcode) {
+            return;
+        }
 
-    // Exit immediately if we find an error message.
-    if (!api_result || !api_result.shorturl || api_result.errorcode) {
-        return;
+        // Check if it is a mobile browser (needs work). This was inspired by is.gd.
+        api_result.mobile = false;
+        if(window.navigator.userAgent.match(/Android|iPhone|iPad|iPod/i)) {
+            api_result.mobile = true;
+        }
+
+        Spice.add({
+            id: 'shorten',
+            data: api_result,
+            name: 'Shortened Link (is.gd)',
+            meta: {
+                sourceUrl : 'http://is.gd/',
+                sourceName : 'is.gd',
+            },
+            templates: {
+                group: 'text',
+                options: {
+                    content: Spice.shorten.shorten
+                }
+            },
+        });
+
+        // If we displayed an input box, make sure we focus on it.
+        var url = $('input#shorten-url');
+        url.click(function() {
+            url.focus().select();
+        }).click();
     }
-
-    // Check if it is a mobile browser (needs work). This was inspired by is.gd.
-    api_result.mobile = false;
-    if(window.navigator.userAgent.match(/Android|iPhone|iPad|iPod/i)) {
-        api_result.mobile = true;
-    }
-
-    Spice.add({
-        id: 'shorten',
-        data: api_result,
-        header1: "Shortened Link (is.gd)",
-        meta: {
-            sourceUrl : "http://is.gd/",
-            sourceName : "is.gd",
-        },
-        templates: {
-            group: 'base',
-            options: {
-                content: Spice.shorten.shorten
-            }
-        },
-        favicon_style : "inline"
-    });
-
-    // If we displayed an input box, make sure we focus on it.
-    var url = $("input#shorten-url");
-    url.click(function() {
-        url.focus().select();
-    }).click();
-}
+}(this))
