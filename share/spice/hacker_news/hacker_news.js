73961210db82b56fae24cb759a4188328028930b
Matthew Ramina <mattramina@gmail.com>
2014-07-20 14:02:14 -0400

    HackerNews: add fuzzy date to tiles

--- share/spice/hacker_news/hacker_news.js
+++ share/spice/hacker_news/hacker_news.js
@@ -5,7 +5,10 @@
         var script = $('[src*="/js/spice/hacker_news/"]')[0],
             source = $(script).attr("src"),
             query = source.match(/hacker_news\/([^\/]+)/)[1],
-            sourceUrl = 'https://hn.algolia.com/#!/story/forever/0/' + query;
+            sourceUrl = 'https://hn.algolia.com/#!/story/forever/0/' + query,
+            now = Math.floor(new Date().getTime() / 1000);
+
+        console.log(api_result);
 
         if(!api_result || !api_result.hits || api_result.hits.length === 0) {
             return Spice.failed('hacker_news');
@@ -31,12 +34,22 @@
                     num_comments: item.num_comments || 0,
                     commentsLabel: (item.num_comments == 1) ? 'Comment' : 'Comments',
                     domain: 'news.ycombinator.com',
+                    date: fuzzyDate(now - item.created_at_i),
                     id: item.objectID
                 };
             },
             templates: {
                 item_custom: Spice.hacker_news.item
-            }
+            },
+            sort_fields: {
+                score: function(a, b){
+                    return (a.points > b.points) ? -1 : 1;
+                },
+                date: function(a, b){
+                    return (a.created_at_i > b.created_at_i) ? -1 : 1;
+                }
+            },
+            sort_default: 'score'
             /*
             relevancy: {
                 skip_words: [ 'hacker', 'news', 'hn', 'hackernews', 'news.yc', 'news.ycombinator.com', 'hn search', 'search' ],
@@ -47,5 +60,31 @@
             },
             */
         });
+    }
+
+    function fuzzyDate(delta){
+        var MINUTE = 60,
+            HOUR = 60 * MINUTE,
+            DAY = 24 * HOUR,
+            MONTH = 30 * DAY,
+            YEAR = 365 * MONTH;
+
+        if (delta < 45*MINUTE){
+            return Math.ceil(delta / MINUTE) + " minutes ago";
+        } else if (delta < 90*MINUTE) {
+            return 'an hour ago';
+        } else if (delta < 24*HOUR){
+            return Math.ceil(delta / HOUR) + " hours ago";
+        } else if (delta < 48*HOUR){
+            return 'yesterday';
+        } else if (delta < 30*DAY){
+            return Math.ceil(delta / DAY) + " days ago";
+        } else if (delta < 12*MONTH){
+            var months = Math.ceil(delta / MONTH);
+            return (months === 1) ? 'a month ago' : months + " months ago";
+        } else {
+            var years = Math.ceil(delta / YEAR);
+            return (years === 1) ? 'a year ago' : years + " years ago";
+        }
     }
 }(this));
