a0b3e5ba2adbd8f50e28cbc8e0205d8f54cf04d3
Sam <sam@bigcurl.de>
2014-07-21 21:46:56 +0200

    save some cpu cycles in the browser by providing the date in the right format

--- share/spice/wgha/wgha.js
+++ share/spice/wgha/wgha.js
@@ -32,15 +32,4 @@
         }
     );
 };
-
-Handlebars.registerHelper("WGHA_formatDate", function(created_at) {
-    var pattern = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/;
-    var match = pattern.exec(created_at);
-    if(match != null) {
-        var event_date = new Date(match[1], parseInt(match[2])-1, match[3], match[4], match[5], match[6]);
-        return event_date.getUTCDate() + "." + (event_date.getUTCMonth() + 1) +
-                "." + event_date.getUTCFullYear() +" ab " + event_date.getUTCHours() +
-                ":" + (event_date.getMinutes()<10?'0':'') + event_date.getMinutes() + "Uhr";
-    } else {return;}
-});
 }(this));
