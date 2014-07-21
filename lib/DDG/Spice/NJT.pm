c3041a8c617972e2ad60accd1eef9ad342e4998a
Matthew Ramina <mattramina@gmail.com>
2014-07-21 15:39:12 -0400

    NJT: tabs -> spaces

--- lib/DDG/Spice/NJT.pm
+++ lib/DDG/Spice/NJT.pm
@@ -24,27 +24,27 @@ my @stops = share('stops.txt')->slurp;
 #check if the stop name is in the list of stops
 #(using the same matching algorithm as the backend)
 sub is_stop {
-	foreach my $stop (@stops){
-		return 1 if index(lc $stop, lc $_[0]) > -1;
-	}
-	return;
+    foreach my $stop (@stops){
+        return 1 if index(lc $stop, lc $_[0]) > -1;
+    }
+    return;
 };
 
 triggers any => "next train", "train times", "train schedule", "njt", "nj transit", "new jersey transit";
 
 handle remainder => sub {
-	return unless /(?:from |to )?(.+) (to|from) (.+)/;
-	my $orig = $1;
-	my $dest = $3;
-	if (is_stop($orig) and is_stop($dest)){
-		#lowercase the stop names found in the query and change the spaces to dashes
-		my $orig = join "-", map { lc } split /\s+/, $orig;
-		my $dest = join "-", map { lc } split /\s+/, $dest;
-		#if the word between the two stop names is "to", then we're going from the first stop to the second
-		#if it's "from", then we're going from the second stop to the first
-		return $2 eq 'to' ? ($orig, $dest) : ($dest, $orig);
-	}
-	return;
+    return unless /(?:from |to )?(.+) (to|from) (.+)/;
+    my $orig = $1;
+    my $dest = $3;
+    if (is_stop($orig) and is_stop($dest)){
+        #lowercase the stop names found in the query and change the spaces to dashes
+        my $orig = join "-", map { lc } split /\s+/, $orig;
+        my $dest = join "-", map { lc } split /\s+/, $dest;
+        #if the word between the two stop names is "to", then we're going from the first stop to the second
+        #if it's "from", then we're going from the second stop to the first
+        return $2 eq 'to' ? ($orig, $dest) : ($dest, $orig);
+    }
+    return;
 };
 
 1;
