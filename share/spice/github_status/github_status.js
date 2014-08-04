6d298e54198f8327d2dc2b8f3d70f1c07ee3d92e
Logan Garcia <logangarcia@openmailbox.org>
2014-08-03 22:54:39 -0700

    implement relative date for the "Updated" section

--- share/spice/github_status/github_status.js
+++ share/spice/github_status/github_status.js
@@ -1,6 +1,8 @@
 (function (env) {
     "use strict";
     env.ddg_spice_github_status = function(api_result) {
+        var d = Math.floor(new Date(api_result.created_on).getTime() / 1000),
+            now = Math.floor(new Date().getTime() / 1000);
         
         if (api_result.error) {
             return Spice.failed('github_status');
@@ -14,6 +16,11 @@
                 sourceName: "GitHub",
                 sourceUrl: "https://status.github.com/",
             },
+            normalize: function(item) {
+                return {
+                    description: relativeDate(now - d)
+                };
+            },
             templates: {
                 group: 'base',
                 options:{
@@ -22,5 +29,31 @@
                 }
             }
         });
-    };
+    }
+
+    function relativeDate(delta) {
+        var MINUTE = 60,
+            HOUR = 60 * MINUTE,
+            DAY = 24 * HOUR,
+            MONTH = 30 * DAY,
+            YEAR = 12 * MONTH;
+
+        if (delta < 45*MINUTE){
+            return Math.round(delta / MINUTE) + " minutes ago";
+        } else if (delta < 90*MINUTE) {
+            return 'an hour ago';
+        } else if (delta < 24*HOUR){
+            return Math.round(delta / HOUR) + " hours ago";
+        } else if (delta < 48*HOUR){
+            return 'yesterday';
+        } else if (delta < 30*DAY){
+            return Math.round(delta / DAY) + " days ago";
+        } else if (delta < 12*MONTH){
+            var months = Math.round(delta / MONTH);
+            return (months === 1) ? 'a month ago' : months + " months ago";
+        } else {
+            var years = Math.round(delta / YEAR);
+            return (years === 1) ? 'a year ago' : years + " years ago";
+        }
+    }
 }(this));
