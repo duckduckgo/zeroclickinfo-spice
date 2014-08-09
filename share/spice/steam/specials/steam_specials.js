6011a8661ec5faa1a1b5c1d7789895b00dbfaf99
Usman Raza <usman.r123@gmail.com>
2014-08-09 18:41:15 +0500

    updated ui

--- share/spice/steam/specials/steam_specials.js
+++ share/spice/steam/specials/steam_specials.js
@@ -40,10 +40,11 @@
             if(api_result[gameids[i]].success == true) { //successfully fetched information on game
                 results.push(api_result[gameids[i]].data.price_overview);
                 results[j].url = "http://store.steampowered.com/app/"+api_result[gameids[i]].data.steam_appid;
+                results[j].dev = api_result[gameids[i]].data.developers;
                 results[j].name = api_result[gameids[i]].data.name;
                 results[j].image = api_result[gameids[i]].data.header_image;
                 if(api_result[gameids[i]].data.metacritic) {
-                    results[j].metacritic = api_result[gameids[i]].data.metacritic;
+                    results[j].metacritic = api_result[gameids[i]].data.metacritic.score;
                 } 
                 if(api_result[gameids[i]].data.genres) {
                     $.each(api_result[gameids[i]].data.genres, function(index, value) {
@@ -71,11 +72,34 @@
                 sourceUrl: 'http://store.steampowered.com/search/?specials=1',
                 sourceName: 'Steam'
             },
+            normalize: function(item) {
+                if (!item.metacritic) {
+                     item.metacritic = 0;
+                }
+                return {
+                    image: item.image,
+                    title: item.name,
+                    img: item.image,
+                    heading: item.name,
+                    url: item.url,
+                    price: "$" + (item.final/100),
+                    brand: item.dev,
+                    rating: (item.metacritic/20),
+                    //reviewCount: item.metacritic
+                };
+            },
             templates: {
-                group: 'base',
-                detail: Spice.steam_specials.detail,
+             //   group: 'products',
+                item: 'products_item',
+                detail: 'products_detail',
+                item_detail: 'products_item_detail',
+                wrap_detail: 'base_detail',
                 options: {
-                    content: Spice.steam_specials.content
+                    buy: Spice.steam_specials.buy,
+                    price: true,
+                    brand: true,
+                    rating: true,
+                    abstract:false
                 }
             },
         });
