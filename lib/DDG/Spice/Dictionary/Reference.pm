5384a23cd7a34ba8826196da3c42a444115c9083
Caine Tighe <caine@duckduckgo.com>
2014-08-21 09:38:10 -0400

    DDG::Spice::Dictionary: Increase caching of definitions from wordnik to 30 days.

--- lib/DDG/Spice/Dictionary/Reference.pm
+++ lib/DDG/Spice/Dictionary/Reference.pm
@@ -6,6 +6,8 @@ attribution web => ['http://duckduckgo.com', 'DuckDuckGo'],
             twitter => ['http://twitter.com/duckduckgo', '@duckduckgo'];
 
 spice to => 'http://api.wordnik.com/v4/word.json/$1/definitions?includeRelated=true&includeTags=true&limit=3&api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}&callback={{callback}}';
+spice proxy_cache_valid => '200 30d';
+
 triggers startend => "///***never trigger***///";
 
 handle query_lc => sub {
