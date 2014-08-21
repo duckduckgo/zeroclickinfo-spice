fef5d5fdbec2355f2b033f03ea025cc417f2ca85
nishanths <nishanth.gerrard@gmail.com>
2014-08-21 01:25:12 -0500

    Switch API source to Amazon

--- share/spice/isbn/isbn.js
+++ share/spice/isbn/isbn.js
@@ -6,39 +6,50 @@
             return Spice.failed('isbn'); 
         }
 
-        api_result = api_result[Object.keys(api_result)[0]]; // Strip off envelope
-
         Spice.add({
             id: 'isbn',
             name: 'Book',
-            data: api_result,
+            data: api_result.results[0],
             signal: 'high',
 
             meta: {
                 itemType: 'Book',
-                sourceName: 'Open Library',
-                sourceUrl: api_result.url
+                sourceName: 'Amazon',
+                sourceUrl: api_result.results[0].url
             },
             
             normalize: function (item) {
-                var main_title = toTitleCase(DDG.getProperty(item, "title"));
-                if (DDG.getProperty(item, "authors.0.name")) {
-                    main_title += "(" + item.authors[0].name +")";
-                }
+                var heading = /(.*)(?:\s\((.*)\))?\s\(Book\)/.exec(DDG.getProperty(item, "heading")),
+                    book_title = heading[1];
+
+                // Example: item.heading = "So Long, and Thanks for All the Fish (Hitch-Hikers Guide to the Galaxy, No. 4) (Book)"
+                // "So Long, and Thanks for All the Fish" ... group 1
+                // "Hitch-Hikers Guide to the Galaxy, No. 4" ... group 2 (not used as of now) ... group 2 can also be absent, for example in "Fahrenheit 451: A Novel (Book)"
+                // "(Book)" is discarded
+
+                var metadata = /.*\s\((.+),\s(\d+)pgs?\)\s*released\s*(\d{4})/.exec(DDG.getProperty(item, "abstract")),
+                    publisher = metadata[1],
+                    pages = metadata[2],
+                    year = metadata[3];
+
+                // Example: item.abstract = "Fahrenheit 451: A Novel is a book by Ray Bradbury (Simon & Schuster, 208pgs)  released 2003-09-23."
+                // "Simon & Schuster" ... group 1
+                // "208" ... group 2
+                // "2003" ... group 3
+                
                 return {
                     relevancy: {
-                        primary: [
-                            { required: 'publishers[0].name'},
-                            { required: 'url'},
-                            { required: 'title'}
+                        primary: [  // don't display answer if any of the required properties is absent
+                            { required: 'heading'},
+                            { required: 'brand'} // author
                         ]
                     },
-                    img             :   DDG.getProperty(item, "cover.medium"),
-                    heading         :   main_title, 
-                    publisher       :   toTitleCase(DDG.getProperty(item, "publishers.0.name")),
-                    year            :   /\d{4}/.exec(DDG.getProperty(item, "publish_date")), // get only the year
-                    pages           :   DDG.getProperty(item, "number_of_pages"),
-                    url             :   DDG.getProperty(item, "url")
+
+                    heading         :   book_title + "(" + (DDG.getProperty(item, "brand")) + ")", // Book Title (Author)
+                    publisher       :   publisher,
+                    pages           :   pages,
+                    year            :   year
+                    // img_m, img, url, abstract automatically set by template
                 };
             },
 
@@ -46,30 +57,9 @@
                 group: 'media',
                 options: {
                     subtitle_content: Spice.isbn.subtitle_content,
-                    buy: Spice.isbn.buy,
                     ratingText: false
                 }
             }
         });
-
-        // modified version of https://github.com/gouch/to-title-case/blob/master/to-title-case.js
-        function toTitleCase (string) {
-            var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
-            return string.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
-            if (index > 0 && index + match.length !== title.length &&
-              match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-              (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-              title.charAt(index - 1).search(/[^\s-]/) < 0) {
-              return match.toLowerCase();
-            }
-
-            if (match.substr(1).search(/[A-Z]|\../) > -1) {
-              return match;
-            }
-
-            return match.charAt(0).toUpperCase() + match.substr(1);
-          });
-        };
-
     };
 }(this));
