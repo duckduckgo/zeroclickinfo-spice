104484195acc6500c210bdc22a34cb1d037dfe87
MariagraziaAlastra <alastra.mariagrazia@gmail.com>
2014-08-22 12:57:17 +0000

    Coupon Mountain - better handling of null response

--- share/spice/coupon_mountain/coupon_mountain.js
+++ share/spice/coupon_mountain/coupon_mountain.js
@@ -1,8 +1,8 @@
-(function(env){
+(function(env) {
     'use strict';
 
     env.ddg_spice_coupon_mountain = function(api_result) {
-        if (api_result.count < 1) {
+        if (!DDG.getProperty(api_result, 'count')) {
             return Spice.failed('coupon_mountain');
         }
 
@@ -14,14 +14,14 @@
                 searchTerm: api_result.keyword,
                 itemType: 'Coupons',
                 sourceName: 'CouponMountain',
-                sourceUrl: 'http://www.couponmountain.com/search.php?searchtext='+ api_result.keyword
+                sourceUrl: 'http://www.couponmountain.com/search.php?searchtext=' + api_result.keyword
             },
-            normalize: function(item){
+            normalize: function(item) {
                 // Get the discount from the description
                 var getDiscount = function() {
                     var regex = /.[0-9]+%?/i;
                     var result = regex.exec(item.name);
-                    if (result) {
+                    if(result) {
                         return result.join('');
                     }
                     return '';
@@ -36,7 +36,7 @@
                 var getSymbol = function(string) {
                     var regex = /[^0-9\s]/i;
                     var result = regex.exec(string);
-                    if (result) {
+                    if(result) {
                         return result.join('');
                     }
                     return '';
@@ -67,8 +67,8 @@
                 }
             },
             sort_fields: {
-                merName: function(a,b) {
-                    return (a.merName < b.merName) ? -1 : 1;
+                merName: function(a, b) {
+                    return(a.merName < b.merName) ? -1 : 1;
                 }
             },
             sort_default: 'merName',
@@ -77,33 +77,34 @@
         });
     };
 
-    function getExpiry (dateString) {
+    function getExpiry(dateString) {
         // 3333-03-03 means coupon has no expiry date
-        if (!dateString || dateString === '3333-03-03'){
+        if(!dateString || dateString === '3333-03-03') {
             return null;
         }
     }
 
     Spice.registerHelper('CouponMountain_dateString', function(string) {
-        var months = [ 'Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.'],
+        var months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'],
             date = DDG.getDateFromString(dateString);
 
         return 'Expires: ' + months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
     });
 
-    function stripExpiry (string) {
+    function stripExpiry(string) {
         return string.replace(/ (hurry, )?(offer|good through|expires|ends|valid \w+) .+$/i, '');
     };
 
     // Highlight coupon code text
-    function highlightCode () {
+
+    function highlightCode() {
 
         var couponCode = $('.zci--coupon_mountain input.tag');
 
-        if (couponCode) {
+        if(couponCode) {
             couponCode.click(function() {
                 couponCode.focus().select();
-             });
+            });
         }
     };
 
