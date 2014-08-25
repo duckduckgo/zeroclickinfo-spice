b35b860b092c37e019bc7751d8e41c84bbf3f4e0
osu <osu@quodvis.net>
2014-08-25 19:49:30 -0400

    add format to advertised bandwidth value

--- share/spice/tor_relay/tor_relay.js
+++ share/spice/tor_relay/tor_relay.js
@@ -28,6 +28,8 @@
             for (var prop in api_response.relays[0]) {
                 if (prop === "last_seen") {
                     data.record_data[prop.replace("_", " ")] = toTime(api_response.relays[0][prop]);
+                } else if (prop === "advertised_bandwidth") {
+                    data.record_data[prop.replace("_", " ")] = bandwidth(api_response.relays[0][prop]);
                 } else if (typeof(api_response.relays[0][prop]) === 'object') {
                     data.record_data[prop.replace("_", " ")] = api_response.relays[0][prop].join(', ');
                 } else if (typeof(api_response.relays[0][prop]) !== 'string') {
@@ -76,6 +78,30 @@
 
         return ((delta === 0) ? "now" : time + "ago");
     };
+
+    // Convert bandwith to formatted bandwidth
+    function bandwidth(value) {
+        var formatted = 'n/a';
+
+        value = parseInt(value, 10);
+        if(value !== -1 && !isNaN(value)){
+            var bandwidthKB = value / 1000;
+            var bandwidthMB = bandwidthKB / 1000;
+
+            if (bandwidthMB >= 1) {
+                formatted = Math.round(bandwidthMB * 100) / 100 + ' MB/s';
+            } else {
+                if (bandwidthKB >= 1) {
+                    formatted = Math.round(bandwidthKB * 100) / 100 + ' kB/s';
+                } else {
+                    formatted = value + ' B/s';
+                }   
+            }   
+        }   
+
+        return formatted;
+    };
+
 
 }(this));
 
