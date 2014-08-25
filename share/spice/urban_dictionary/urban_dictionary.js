9874d6416630cd7a1ded0ce291ee8ccdcb927fff
MariagraziaAlastra <alastra.mariagrazia@gmail.com>
2014-08-25 10:10:23 +0000

    Urban Dictionary - follow the style of the Dictionary IA

--- share/spice/urban_dictionary/urban_dictionary.js
+++ share/spice/urban_dictionary/urban_dictionary.js
@@ -3,39 +3,43 @@
 
 // Note: This plugin can display adult content and profanity.
 (function(env) {
-  env.ddg_spice_urban_dictionary = function(response) {
-    "use strict";
+    env.ddg_spice_urban_dictionary = function(response) {
+        "use strict";
 
-    if (!(response && response.result_type === "exact"
-          && response.list && response.list[0])) {
-        return Spice.failed('urban_dictionary');
-    }
-
-    var word       = response.list[0].word;
-
-    Spice.add({
-      id: "urban_dictionary",
-      name: "Dictionary",
-      data: {
-        record_data: response.list[0],
-        record_keys: ['word', 'definition', 'example']
-      },
-      meta: {
-        sourceUrl: 'http://www.urbandictionary.com/define.php?term=' + word,
-        sourceName: 'Urban Dictionary'
-      },
-      templates: {
-        group: 'base',
-        options: {
-          content: 'record',
-          moreAt: true
+        if(!(response && response.result_type === "exact" && response.list && response.list[0])) {
+            return Spice.failed('urban_dictionary');
         }
-      },
-      normalize : function(item){
-        return{
-          definition: item.record_data.definition.replace(/(\r?\n)+/gi, '')
-        };
-      }
+
+        var word = response.list[0].word;
+
+        Spice.add({
+            id: "urban_dictionary",
+            name: "Dictionary",
+            data: response.list[0],
+            meta: {
+                sourceUrl: 'http://www.urbandictionary.com/define.php?term=' + word,
+                sourceName: 'Urban Dictionary'
+            },
+            templates: {
+                group: 'base',
+                options: {
+                    content: Spice.urban_dictionary.content,
+                    moreAt: true
+                }
+            },
+            normalize: function(item) {
+                return {
+                    definition: item.definition.replace(/(\r?\n)+/gi, ' ')
+                };
+            }
+        });
+
+        // Call the Wordnik API to get the audio.
+        $.getScript(dictionary_path + "/audio/" + word);
+    }
+    
+    Handlebars.registerHelper("UD_example", function(example) {
+        var exampleList = example.replace(/(\r?\n)+/gi, '</div><div class="example">')
+        return new Handlebars.SafeString('<div class="example">' + exampleList + '</div>');
     });
-  }
 }(this));
