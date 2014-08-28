b6d90230496f802df8979fe344c6c52fc6f2cc16
Matthew Ramina <mattramina@gmail.com>
2014-08-28 18:11:43 -0400

    SEPTA: switch to YAML definition of stops and their aliases

--- lib/DDG/Spice/SEPTA.pm
+++ lib/DDG/Spice/SEPTA.pm
@@ -1,6 +1,7 @@
 package DDG::Spice::SEPTA;
 
 use DDG::Spice;
+use YAML::Tiny;
 
 primary_example_queries "next train from Villanova to Paoli";
 secondary_example_queries "train times to paoli from Villanova";
@@ -23,34 +24,44 @@ spice proxy_cache_valid => "418 1d";
 
 triggers any => "next train", "train times", "train schedule", "septa";
 
-my @stops = share('stops.txt')->slurp;
-my @alias_list = share('aliases.txt')->slurp;
-
-#remove \r and \n from the lines that were slurped in
-sub trim_crlf($) {
-    my $string = shift;
-    $string =~ s/[\x0A\x0D]//g;
-    return $string;
+#add the canonical names to the arrays in the stop hash
+sub fix_hash {
+    my (%hash) = @_;
+    foreach my $key (keys %hash) {
+        my $value = $hash{$key};
+        if ($value) {
+            #there are other aliases, add the canonical name to that array
+            push(@$value, $key);
+            $hash{$key} = [ @$value ];
+        } else {
+            #there are no aliases, so just create an anonymous array with the canonical name
+            $hash{$key} = [ $key ];
+        }
+    }
+    return %hash;
 }
 
-sub map_aliases($) {
-    /(.+);(.+)/;
-    return lc $1, $2;
-}
-
-@stops = map { trim_crlf($_) } @stops;
-@alias_list = map { trim_crlf($_) } @alias_list;
-my %aliases = map { map_aliases($_) } @alias_list;
+#YAML::Tiny can handle keys with null values
+my %stops = %{YAML::Tiny->read(share('stops.yml'))->[0]};
+%stops = fix_hash(%stops);
 
 #find a stop from a partial name (e.g. converts "30th street" -> "30th Street Station")
-#names sourced from GTFS at njt-api.appspot.com/septa/stops
 sub normalize_stop {
     my @matches = ();  #list of stop matches
     my $in = lc $_[0];
-    return $aliases{$in} if exists $aliases{$in}; #check our alias list
-    foreach my $stop (@stops){
-        return $stop if ($in) eq (lc $stop);  #if they're exactly equal, return the stop
-        push(@matches, $stop) if index(lc $stop, $in) > -1;  #if the stop name contains the input, add it to matches
+    $in =~ s/\bstation//;
+
+    foreach my $stop (keys %stops) {
+        #look through each alias of each stop
+        foreach my $i (@{$stops{$stop}}) {
+            #if they're exactly equal, return the stop
+            return $stop if ($in) eq (lc $i);
+            #if the stop name contains the input, add it to matches
+            if (index(lc $i, $in) > -1) {
+                push(@matches, $stop);
+                last;
+            }
+        }
     }
     return $matches[0] if scalar(@matches) == 1;  #if we have one match, return it
     return;  #if we have no matches or too many, then we don't know the stop :(
