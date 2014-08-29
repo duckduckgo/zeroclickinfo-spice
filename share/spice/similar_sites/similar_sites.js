33a867e2e8b70a6693d1ae149f2fe83844a91ce5
Adrian Matejov <a.matejov@centrum.sk>
2014-08-29 12:21:50 +0200

    tile view changed to list style

--- share/spice/similar_sites/similar_sites.js
+++ share/spice/similar_sites/similar_sites.js
@@ -6,43 +6,65 @@
                 source = $(script).attr("src"),
                 query = source.match(/similar_sites\/([^\/]+)/)[1];
 
-        if(!api_result || !api_result.SimilarSites || api_result.SimilarSites.length === 0) {
+        if(!api_result || !api_result.num === 0) {
             return Spice.failed('similar_sites');
         }
+
+        var num = api_result.num;
+        var show_more = true;
+        if (num < 5)
+            show_more = false;
+
+        delete api_result['num'];
+        delete api_result['status'];
 
         Spice.add({
             id: "similar_sites",
             name: "Similar Sites",
 
-            data: api_result.SimilarSites,
+            data: {
+                results: api_result,
+                num: num,
+                show_more: show_more
+            },
 
             meta: {
-                total: api_result.SimilarSites.length,
+                total: num,
                 sourceName: 'SimilarSites',
                 sourceUrl: 'http://www.similarsites.com/site/' + query,
+                sourceIcon: true,
                 itemType: 'Similar Sites'
             },
 
-            normalize : function(item){
+            /*normalize : function(item){
                 return {
                     title: item.Url,
                     url: "http://" + item.Url,
                     rating: item.Score*100,
                     icon: "http://icons.duckduckgo.com/ip/" + item.Url + ".ico",
                 };
-            },
+            },*/
 
             templates: {
-                group: 'icon',
-                detail: false,
-                item_detail: false,
+                group: 'base',
                 options: {
-                        footer: Spice.similar_sites.rating,
-                        rating: true,
-                    }
+                    content: Spice.similar_sites.content
+                }
             }
         });
+    };
 
-    }
+    Handlebars.registerHelper('list', function(items, from, to) {
+        var out = "<ul>";
 
+        if (to === 0)
+            to = Object.keys(items).length;
+
+        for(var i = from; i < to; i++) {
+            //out += "<img src='http://icons.duckduckgo.com/ip/" + items["r" + i].replace("http://", "")  + ".ico'/>"
+            out += "<a href='" + items["r" + i] + "'><li style='list-style-image:url(http://icons.duckduckgo.com/ip/" + items["r" + i].replace("http://", "")  + ".ico);'>" + items["r" + i] + "</li></a>";
+        }
+
+        return out + "</ul>";
+    });
 })(this);
