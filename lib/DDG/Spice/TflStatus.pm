29eaae08119848ca7090ef816987982d8c1b396f
idlem1nd <tim@darkshines.co.uk>
2014-09-03 20:09:57 +0100

    Disable caching of TfL API responses
    We want realtime status information, so we must disable the default
    caching of API responses.

--- lib/DDG/Spice/TflStatus.pm
+++ lib/DDG/Spice/TflStatus.pm
@@ -15,6 +15,7 @@ attribution github  => ['https://github.com/idlem1nd', 'Tim Williams'],
 
 spice to => 'https://api.tfl.gov.uk/Line/$1/Status?app_id=4b57df3f&app_key=a3b1543277303ffaff1c3122044a6653';
 spice wrap_jsonp_callback => 1;
+spice proxy_cache_valid => "418 1d";
 
 triggers end => 'line status', 'line tfl';
 
