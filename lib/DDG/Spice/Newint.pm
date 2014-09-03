c6c61dbfac3fc102f5b32f379e05fb655acb5e25
sighmon <simon.loffler@gmail.com>
2014-09-03 19:59:39 -0400

    Added New Internationalist magazine instant answers using the trigger newint.

---
+++ lib/DDG/Spice/Newint.pm
@@ -0,0 +1,24 @@
+package DDG::Spice::Newint;
+# ABSTRACT: Returns a list of New Internationalist magazines.
+
+use DDG::Spice;
+
+name "newint";
+description "List all New Internationalist magazines or search for a particular issue.";
+primary_example_queries "newint";
+secondary_example_queries "newint gold";
+category "reference";
+topics "everyday", "entertainment";
+attribution github => ["https://github.com/sighmon", "Simon Loffler"],
+            twitter => ["https://twitter.com/sighmon", "sighmon"];
+
+triggers startend => "newint";
+
+spice to => 'https://digital.newint.com.au/issues.json?utf8=✓&query=$1';
+spice wrap_jsonp_callback => 1;
+
+handle remainder => sub {
+    return lc $_;
+};
+
+1;
