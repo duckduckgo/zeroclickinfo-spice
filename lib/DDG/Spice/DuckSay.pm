0de66691d08eaabdd5af064d061f888e192c8b53
Chris Tan <csytan@gmail.com>
2014-09-02 20:37:22 -0400

    Added instant answer metadata

--- lib/DDG/Spice/DuckSay.pm
+++ lib/DDG/Spice/DuckSay.pm
@@ -2,6 +2,15 @@ package DDG::Spice::DuckSay;
 
 use DDG::Spice;
 
+name 'DuckSay';
+description 'Like cowsay, but with ducks';
+primary_example_queries 'ducksay';
+secondary_example_queries 'ducksay quack quack quack!';
+category 'entertainment';
+topics 'geek', 'words_and_games', 'entertainment';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/DuckSay.pm';
+attribution github => ['https://github.com/csytan', 'Chris Tan'];
+
 triggers start => 'ducksay';
 
 spice to => 'http://www.reddit.com/r/Jokes/search.json?q=selftext%3Aduck&restrict_sr=on&sort=hot&t=all&jsonp={{callback}}';
