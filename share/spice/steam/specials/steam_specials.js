8fcb32a0548dfc229f334d1459664bf7b5b4dced
Usman Raza <usman.r123@gmail.com>
2014-09-03 21:12:40 +0500

    adding new changes

--- share/spice/steam/specials/steam_specials.js
+++ share/spice/steam/specials/steam_specials.js
@@ -14,9 +14,9 @@
         }
 
         var idarray = api_result.specials.items;
-        queryexec = encodeURIComponent(($.map(idarray, function(elem) {
+        queryexec = encodeURIComponent($.map(idarray, function(elem) {
                 return elem.id;
-        }).join(",")));
+        }).join(","));
         $.getScript("/js/spice/steam/list_specials/" + queryexec);
     };
 
@@ -36,29 +36,40 @@
         // Organize data to display
         for(var i = 0; i < gameids.length; i++) {
             if(api_result[gameids[i]].success == true) { //successfully fetched information on game
+                
                 results.push(api_result[gameids[i]].data.price_overview);
                 results[j].url = "http://store.steampowered.com/app/"+api_result[gameids[i]].data.steam_appid;
                 results[j].dev = api_result[gameids[i]].data.developers[0];
                 results[j].name = api_result[gameids[i]].data.name;
                 results[j].image = api_result[gameids[i]].data.header_image;
+                
                 if(api_result[gameids[i]].data.metacritic) {
                     results[j].metacritic = api_result[gameids[i]].data.metacritic.score;
                 } 
+                
                 if(api_result[gameids[i]].data.genres) {
                     $.each(api_result[gameids[i]].data.genres, function(index, value) {
                         genre.push(value.description);
                     });
                     results[j].genre = genre.join(", ");
                 }
+                
                 results[j].platforms = api_result[gameids[i]].data.platforms;
+                for (var platform in results[j].platforms) {
+                    if(results[j].platforms[platform] == "No") {
+                         delete results[j].platforms[platform];
+                    }
+                }
+              
                 //results[j].ss = (api_result[gameids[i]].data.screenshots).slice(0,4); //screenshots maybe used in future
                 genre = [];
                 j++;
             }
         }
+
         
         // Return if not able to retrieve information for any game
-        if(!results) {
+        if(!results.length) {
             return Spice.failed('specials');
         }
 
@@ -72,12 +83,13 @@
                 sourceName: 'Steam'
             },
             normalize: function(item) {
+                console.log(item.dev);
                 return {
                     image: item.image,
                     img: item.image,
                     heading: item.name,
                     url: item.url,
-                    price: "USD $" + (item.final/100),
+                    price: "USD $" + (item.final/100),  
                     brand: item.dev,
                     rating: item.metacritic,
                     original_price: "USD $" + (item.initial/100),
@@ -88,7 +100,6 @@
                 group: 'products',
                 options: {
                     buy: Spice.steam_specials.buy,
-                    detailVariant: 'light',
                     rating: false
                 }
             },
@@ -97,12 +108,4 @@
             }
         });
     };
-    
-    Spice.registerHelper('steamspecial_booleval', function(boolevaluate) {
-        if(boolevaluate == true) {
-            return "Yes";
-        } else {
-            return "No";
-        }
-    });
 }(this));
