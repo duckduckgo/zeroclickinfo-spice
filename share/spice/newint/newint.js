8afe0004e79ea9604be0abe6597d99cf9622e7f4
sighmon <simon.loffler@gmail.com>
2014-09-04 17:54:38 -0400

    Updated the newint spice to use the products template.

--- share/spice/newint/newint.js
+++ share/spice/newint/newint.js
@@ -11,7 +11,7 @@
             name: "Newint",
             data: api_result,
             meta: {
-                itemType: "Magazines",
+                itemType: "New Internationalist magazines",
                 sourceName: "digital.newint.com.au",
                 sourceUrl: 'https://digital.newint.com.au/issues'
             },
@@ -20,15 +20,21 @@
                     "April", "May", "June", "July", "August", "September", 
                     "October", "November", "December");
                 return {
-                    release_date: month_names[DDG.getDateFromString(item.release).getMonth()] + " " + DDG.getDateFromString(item.release).getFullYear()
+                    release_date: month_names[DDG.getDateFromString(item.release).getMonth()] + " " + DDG.getDateFromString(item.release).getFullYear(),
+                    img: item.cover.thumb2x.url,
+                    price: item.price ? item.price : "AUD$7.50",
+                    heading: item.title,
+                    brand: "New Internationalist",
+                    url: "https://digital.newint.com.au/issues/" + item.id
                 };
             },
             templates: {
-                group: 'base',
-                detail: false,
+                group: 'products',
                 options:{
-                    content: Spice.newint.content,
-                    moreAt: true
+                    rating: false,
+                    moreAt: true,
+                    subtitle_content: Spice.newint.content,
+                    buy: Spice.newint.buy
                 }
             }
         });
