11860d4cfddbca4137c5ebe0d211fc7e841393b1
Logan Garcia <logangarcia@openmailbox.org>
2014-09-04 03:47:58 -0700

    replace updated field with body field in all cases

--- share/spice/github_status/github_status.js
+++ share/spice/github_status/github_status.js
@@ -6,9 +6,6 @@
             return Spice.failed('github_status');
         }
 
-        var d = Math.floor((DDG.getDateFromString(api_result.created_on)).getTime() / 1000),
-            now = Math.floor(new Date().getTime() / 1000);
-
         Spice.add({
             id: "github_status",
             name: "Status",
@@ -16,11 +13,6 @@
             meta: {
                 sourceName: "GitHub",
                 sourceUrl: "https://status.github.com/",
-            },
-            normalize: function(item) {
-                return {
-                    description: relativeDate(now - d)
-                };
             },
             templates: {
                 group: 'base',
@@ -35,30 +27,4 @@
     Spice.registerHelper("GithubStatus_ifCond", function(string1, string2, options) {
         return ((string1 === string2) ? options.fn(this) : options.inverse(this));
     });
-
-    function relativeDate(delta) {
-        var MINUTE = 60,
-            HOUR = 60 * MINUTE,
-            DAY = 24 * HOUR,
-            MONTH = 30 * DAY,
-            YEAR = 12 * MONTH;
-
-        if (delta < 45*MINUTE){
-            return Math.round(delta / MINUTE) + " minutes ago";
-        } else if (delta < 90*MINUTE) {
-            return 'an hour ago';
-        } else if (delta < 24*HOUR){
-            return Math.round(delta / HOUR) + " hours ago";
-        } else if (delta < 48*HOUR){
-            return 'yesterday';
-        } else if (delta < 30*DAY){
-            return Math.round(delta / DAY) + " days ago";
-        } else if (delta < 12*MONTH){
-            var months = Math.round(delta / MONTH);
-            return (months === 1) ? 'a month ago' : months + " months ago";
-        } else {
-            var years = Math.round(delta / YEAR);
-            return (years === 1) ? 'a year ago' : years + " years ago";
-        }
-    }
 }(this));
