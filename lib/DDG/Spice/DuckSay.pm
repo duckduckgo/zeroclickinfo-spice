15bb73b40fd99cefeba643a2c109ad55220a0564
Chris Tan <csytan@gmail.com>
2014-09-06 15:34:54 -0400

    DuckSay: added "daxsays" trigger

--- lib/DDG/Spice/DuckSay.pm
+++ lib/DDG/Spice/DuckSay.pm
@@ -4,15 +4,15 @@ use DDG::Spice;
 
 name 'DuckSay';
 description 'Like cowsay, but with ducks';
-primary_example_queries 'ducksay';
-secondary_example_queries 'ducksay quack quack quack!';
+primary_example_queries 'ducksay', 'daxsays';
+secondary_example_queries 'ducksay quack!', 'daxsays quack!';
 source 'Reddit /r/jokes';
 category 'entertainment';
 topics 'geek', 'words_and_games', 'entertainment';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/DuckSay.pm';
 attribution github => ['https://github.com/csytan', 'Chris Tan'];
 
-triggers start => 'ducksay';
+triggers start => 'ducksay', 'daxsays';
 
 spice is_unsafe => 1;
 spice to => 'http://www.reddit.com/r/Jokes/search.json?q=selftext%3Aduck&restrict_sr=on&sort=hot&t=all&jsonp={{callback}}';
