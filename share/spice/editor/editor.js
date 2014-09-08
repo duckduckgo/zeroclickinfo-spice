55d9883c832cf50efea692ffc79aee5f49bb1390
MariagraziaAlastra <alastra.mariagrazia@gmail.com>
2014-09-08 20:51:42 +0000

    Editor - use $.getScript()

--- share/spice/editor/editor.js
+++ share/spice/editor/editor.js
@@ -7,7 +7,7 @@
         var source = $(script).attr("src");
         var language = source.match(/editor\/([^\/]+)/)[1];
 
-        nrj("share/spice/editor/ace.js");
+        /*nrj("share/spice/editor/ace.js");
 
         // Function referenced in ace.js
         window.aceScriptLoaded = function() {
@@ -42,7 +42,7 @@
             $("#" + editor_id).keydown(function(e) {
                 e.stopPropagation();
             });
-        }
+        }*/
 
         Spice.add({
             id: 'editor',
@@ -73,6 +73,41 @@
                     content: Spice.editor.editor
                 }
             },
+        });
+        //Need this to make sure jQuery does add a uniqe key to the requested URL (which causes problems)
+        $.ajaxSetup({
+            cache: true
+        });
+        $.getScript("/share/spice/editor/ace.js", function(data, textStatus, jqxhr) {
+            var editor_id = "ace-editor";
+            var editor = ace.edit(editor_id);
+
+            // Set theme based on DDG theme
+            if($(".dark-header").length) {
+                editor.setTheme("ace/theme/monokai");
+            } else {
+                editor.setTheme("ace/theme/eclipse");
+            }
+
+            // If the user changes DDG theme, change the editor theme too
+            $(".nav-menu__theme--default").click(function(e) {
+                editor.setTheme("ace/theme/eclipse");
+                e.stopPropagation();
+            });
+
+            $(".nav-menu__theme--d").click(function(e) {
+                editor.setTheme("ace/theme/monokai");
+                e.stopPropagation();
+            });
+
+            editor.getSession().setMode("ace/mode/" + language);
+
+            $("#" + editor_id).height("400px");
+
+            // Stop DDG keybindings, when editor has focus
+            $("#" + editor_id).keydown(function(e) {
+                e.stopPropagation();
+            });
         });
     };
 }(this));
