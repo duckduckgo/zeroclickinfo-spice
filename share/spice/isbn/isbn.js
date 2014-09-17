f4ab654643c5aa6901ce04b9156d578e40fe819f
jagtalon <jag@jagtalon.com>
2014-09-17 14:09:38 +0000

    ISBN: From #890

--- share/spice/isbn/isbn.js
+++ share/spice/isbn/isbn.js
@@ -7,68 +7,54 @@
             return Spice.failed('isbn');
         }
 
-        var items = api_result.results,
-        loadedRatingsData = false;
-
+        var items = api_result.results;
+        
         Spice.add({
             id: 'isbn',
-            name: 'Book',
+            name: 'Books',
             data: items[0],
-            signal: 'high',
-
+            allowMultipleCalls: true,
+            model: 'Product',
             meta: {
-                itemType: 'Book',
+                itemType: 'Products',
                 sourceName: 'Amazon',
-                sourceUrl: api_result.results[0].url
+                sourceUrl: api_result.more_at,
+                sourceIcon: true,
+                next: api_result.next
             },
-            
             templates: {
                 group: 'products',
                 options: {
-                    rating: false,
-                    reviewCount: false,
-                    buy: Spice.isbn.buy
+                    buy: 'products_amazon_buy'
                 }
             },
+            onItemShown: function(item) {
+                if (item.loadedRatings) { return; }
 
-            // From ./share/spice/amazon/amazon.js
-            // wait until after tab is shown to make
-            // the individual ajax requests to get ratings data. Allows
-            // products to get to show() asap so we have fewer fallbacks,
-            // and saves on useless http requests for hidden products tabs.
-            onShow: function() {
-                // only do this on the first time the tab is shown:
-                if (loadedRatingsData) {
-                    return;
-                }
-
-                var onGotRatingsData = function(r, s, x) {
-                    if (r.stars.match(/stars-(\d)-(\d)/)) {
-                        this.rating = RegExp.$1 + "." + RegExp.$2;
-                    }
-                    this.reviewCount = r.reviews;
-
-                    // this is kind of dirty, relies on the item.$html being the
-                    // memory ref to the tile dom, and re-renders the stars/renders block of html
-                    // now that we have updated ratings data:
-                    if (this.$html) {
-                        var $ratingsWrapper = this.$html.find('.tile__rating,.detail__rating');
-                        if ($ratingsWrapper && $ratingsWrapper.length) {
-                            $ratingsWrapper.html(Handlebars.helpers.starsAndReviews(this.rating, this.reviewCount, this.url_review, true));
-                        }
-                    }
-                }
-                
-                var item = items[0],
-                    arg = item.rating,
+                var arg = item.rating,
                     url = '/m.js?r=';
 
                 arg = arg.replace(/(?:.com.au|.com.br|.cn|.fr|.de|.in|.it|.co.jp|.mx|.es|.co.uk|.com|.ca?)/i, '');
                 arg = arg.replace('http://www.amazon/reviews/iframe?', '');
-                $.getJSON(url + encodeURIComponent(arg), onGotRatingsData.bind(item));
 
-                // set flag so we only load it once:
-                loadedRatingsData = true;
+                $.getJSON(url + encodeURIComponent(arg), function(r) {
+                    if (r.stars.match(/stars-(\d)-(\d)/)) {
+                        item.rating = RegExp.$1 + "." + RegExp.$2;
+                    }
+                    item.reviewCount = r.reviews;
+
+                    // this is kind of dirty, relies on the item.$html being the
+                    // memory ref to the tile dom, and re-renders the stars/renders block of html
+                    // now that we have updated ratings data:
+                    if (item.$html) {
+                        var $ratingsWrapper = item.$html.find('.tile__rating,.detail__rating');
+                        if ($ratingsWrapper && $ratingsWrapper.length) {
+                            $ratingsWrapper.html(Handlebars.helpers.starsAndReviews(item.rating, item.reviewCount, item.url_review, true));
+                        }
+                    }
+                });
+
+                item.loadedRatings = true;
             }
         });
     };
