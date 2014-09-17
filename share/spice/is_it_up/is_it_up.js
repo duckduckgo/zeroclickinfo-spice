1d922ec20543bd56b078c90dabea5c531c8f2bb4
Usman Raza <usman.r123@gmail.com>
2014-09-17 15:03:28 +0500

    updated to strict comparison

--- share/spice/is_it_up/is_it_up.js
+++ share/spice/is_it_up/is_it_up.js
@@ -27,7 +27,7 @@
     }
     
     Handlebars.registerHelper ('if_value', function(a, b, options) {
-        if (a == b) {
+        if (a === b) {
             return options.fn(this);
         } else {
             return options.inverse(this);
