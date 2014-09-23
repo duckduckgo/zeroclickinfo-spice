0e19a1f11153b99bcabc8daefcedacb83be4e43b
sighmon <simon.loffler@gmail.com>
2014-09-22 20:51:42 -0400

    Removed the line breaks within the editor's letter.

--- share/spice/newint/newint.js
+++ share/spice/newint/newint.js
@@ -19,7 +19,7 @@
                 var month_names = new Array("January", "February", "March", 
                     "April", "May", "June", "July", "August", "September", 
                     "October", "November", "December");
-                var editors_letter_strip_tags = item.editors_letter_html.replace(/<h[1-6]>(.*?)<\/h[1-6]>/g, "<p>$1.</p>");
+                var editors_letter_strip_tags = item.editors_letter_html.replace(/<h[1-6]>(.*?)<\/h[1-6]>/g, "<p>$1. </p>");
                 editors_letter_strip_tags = editors_letter_strip_tags.replace(/<a(.*?)>(.*?)<\/a>/g, "$2");
                 return {
                     release_date: month_names[DDG.getDateFromString(item.release).getMonth()] + " " + DDG.getDateFromString(item.release).getFullYear(),
@@ -27,7 +27,7 @@
                     img_m: item.cover.thumb2x.url,
                     price: item.price ? item.price : "AUD$7.50",
                     heading: item.title,
-                    editors_letter_html: editors_letter_strip_tags,
+                    editors_letter_html: DDG.strip_html(editors_letter_strip_tags),
                     brand: "New Internationalist",
                     url: "https://digital.newint.com.au/issues/" + item.id
                 };
