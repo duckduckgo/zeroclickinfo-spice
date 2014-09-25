c16e33b5e985cce6674e8842770e644cd61ed948
b1ake <blake@blakejennelle.com>
2014-09-25 20:56:01 +0000

    Remove phrases that usually aren't part of a movie title.

--- lib/DDG/Spice/Movie.pm
+++ lib/DDG/Spice/Movie.pm
@@ -20,20 +20,32 @@ spice to => 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey={{
 # It's important that 'movie info' precede 'movie' so that the handler
 # encounters it first and removes both words, rather than encountering 'movie'
 # first in the list, removing it, and leaving the word 'info.'
-my @triggers = ( 'movie info', 'movie', 'movies', 'film', 'rt', 'rotten tomatoes', 
-                 'rating', 'ratings', 'rotten', 'release date', 'runtime', 'run time',
-                 'running time', 'cast of', 'cast', 'casting', 'actors in', 
-                 'actor in', 'actors', 'actor', 'actress in', 'actress' );
+my @phrases_to_remove = ( 'movie info', 'movie', 'movies', 'film', 'rotten tomatoes', 'rotten', 
+			  'rating', 'ratings', 'release date', 'runtime', 'run time',
+			  'running time', 'cast of', 'cast', 'casting', 'actors in',
+			  'actor in', 'actors', 'actor', 'actress in', 'actress' );
 
-triggers startend => @triggers;
+# need a trigger here to prevent duckpan complaints
+triggers query_raw => qr/DUMMY_TRIGGER_SO_DUCKPAN_DOES_NOT_COMPLAIN/;
 
-handle query_lc => sub {
-    # spice triggers are called when a trigger is part of a hyphenated word
-    # i.e.: asus rt-66nu
-    # this makes sure that only space deliminated words fire this spice
-    my $input = $_;
-    map { return $input if $input =~ s/(^|\s)$_(\s|$)// and $input ne '' } @triggers;
-    return; 
+# remove phrases that usually aren't part of the movie's title.
+# these phrases often appear in deep triggers.
+handle sub {
+    return remove_phrases($_, \@phrases_to_remove);
 };
+
+# Remove all instances of an array of phrases from a string.
+# Each phrase must appear at word boundaries to be removed.
+sub remove_phrases {
+    my ($str, $phrases) = @_;
+    return $str if !$str;   # if falsy, return the string unchanged
+
+    # remove each phrase (case insensitive)
+    foreach my $phrase (@$phrases) {
+	$str =~ s/\b\Q$phrase\E\b//gi;
+    }
+
+    return $str;
+}
 
 1;
