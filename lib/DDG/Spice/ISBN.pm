1d247aaa6abf8a682094bb13b050745565953d08
Matt Miller <mwmiller@outlook.com>
2014-10-18 16:14:56 +0200

    ISBN: allow for bare ISBN entry.
    We'll trigger if there are at least 10 digits plus spaces and dashes
    in a row.
    
    Then clean it up and see if we can find just 10 or 13 digits in a row
    which pass check digit verification.  If so, we rock on.

--- lib/DDG/Spice/ISBN.pm
+++ lib/DDG/Spice/ISBN.pm
@@ -1,9 +1,12 @@
 package DDG::Spice::ISBN;
 # ABSTRACT: Returns book details for a query ISBN.
 
-use DDG::Spice;
 use strict;
 use warnings;
+
+use DDG::Spice;
+
+use List::Util qw( sum );
 
 primary_example_queries     "isbn 0330287001", "ISBN number 0-06-250217-4";
 secondary_example_queries   "0-330-2870 0-1  ISBN", "978 0 7432 4722 1 ISBN lookup";
@@ -17,14 +20,41 @@ attribution github  =>      ["https://github.com/nishanths/", "Nishanth Shanmugh
             twitter =>      ["https://twitter.com/nshanmugham/", "Nishanth Shanmugham"],
             web     =>      ["http://nishanths.github.io/", "Nishanth Shanmugham"];
 
-triggers startend   =>      'isbn', 'isbn number', 'isbn lookup';
 spice to            =>      'https://duckduckgo.com/m.js?q=$1&cb={{callback}}';
 spice is_cached     =>      1;
 
-handle remainder => sub {
-    $_ =~ s/[\s-]//g;                       # remove all whitespace and hyphens
-    return $_ if (/^\d{10}$|^\d{13}$/);     # return only if number string of length 10 or 13
-    return;
+triggers query_raw => qr/[0-9\s-]{10,}/;    # Look for at least 10 digits, spaces and dashes in a row;
+
+my %skip_words = map { $_ => 1 } qw( isbn number lookup );    # Appreciate if they tried to give us more context.
+
+handle query_lc => sub {
+    my $query_cleaned = join '', (grep { defined $_ && !$skip_words{$_} } split /[\s-]/);
+
+    return unless (looks_like_isbn($query_cleaned));
+
+    return $query_cleaned;
 };
+
+sub looks_like_isbn {
+    my $number = shift;
+
+    return unless ($number =~ /^(?:\d{10}|\d{13})$/);    # All ISBNs contain 10 or 13 digits
+
+    my @nums        = split //, $number;
+    my $check_digit = pop @nums;                         # Last digit is the check digit
+    my $count       = 0;                                 # Using pre-increment.
+    my $checksum;
+    if (scalar @nums == 12) {
+        # ISBN-13: (10 - (Even-place digits * 3 + odd odd-place digits)) mod 10
+        $checksum = (10 - sum map { (++$count % 2) ? $_ : $_ * 3 } @nums) % 10;
+    } else {
+        # ISBN-10: (digit * 11 - place) % 11
+        $checksum = 11 - (sum map { (11 - ++$count) * $_ } @nums) % 11;
+    }
+
+    $checksum = $checksum % 10;                          # Considering the above 10 to 0, basically;
+
+    return $checksum == $check_digit;
+}
 
 1;
