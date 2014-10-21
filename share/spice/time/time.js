b17144e2bdd4deee1980a9b0fb56b8a685dc4cbc
chrisjwilsoncom <chris@chrisjwilson.com>
2014-10-21 01:29:49 +0000

    Time: Update moments.js load, this slows the page load down..

--- share/spice/time/time.js
+++ share/spice/time/time.js
@@ -6,11 +6,12 @@
             return Spice.failed('time');
         }
         
-        nrj("share/spice/time/moment.min.js");
-
-        var timeData = {
-            time: moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
-            date: final_date,
+       $.ajaxSetup({'cache':true});
+       $.getScript("/share/spice/time/moment.min.js", function() {
+          var ISOTime = api_result.locations[0].time.iso;
+          var data = moment(ISOTime).zone(ISOTime); //phase the time and zone
+           var timeData = {
+            time: data,
             place: api_result.locations[0].geo.name + ", " + api_result.locations[0].geo.country.name
         }
         
@@ -30,5 +31,9 @@
                 }
             }
         });
+        });
+
+           
+      
     };
 }(this));
