8721c9887c6ea33b1d6a4528111449968f40da52
jagtalon <jag@jagtalon.com>
2014-10-23 11:54:02 +0000

    Convert 'text' groups into 'base'
    We've modified the 'text' group template a bit, and, as a result, we found that some of the IAs now look wonky. Changing them to a 'base' template fixed those issues.

--- share/spice/rhymes/rhymes.js
+++ share/spice/rhymes/rhymes.js
@@ -2,47 +2,47 @@ function ddg_spice_rhymes (api_result) {
     "use strict";
 
     var query = DDG.get_query()
-                .replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
+    .replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
 
     if (!api_result.length) {
         return Spice.failed('rhymes');
     }
-    
+
     var words = [], count=0;
-    
+
     for(var i=0, l = api_result.length; i<l; i++) {
         var word = api_result[i];
-	
+
         if (word.score === 300 && !word.flags.match(/a/)) {
             words.push(word);
-	    if (++count > 15)
-		break;
+            if (++count > 15)
+                break;
         }
     }
 
     if(words.length === 0) {
-	return;
+        return;
     }
 
     var title = DDG.capitalize(query.toLowerCase());
     Spice.add({
-	data: { 
-	    words: words, 
-	    query: title
-	},
-	id: "rhymes",
+        data: { 
+            words: words, 
+            query: title
+        },
+        id: "rhymes",
         name: "Rhymes",
         meta: {
             sourceUrl: 'http://rhymebrain.com/en/What_rhymes_with_' +
-                           encodeURIComponent(query) + '.html',
+            encodeURIComponent(query) + '.html',
             sourceName: 'RhymeBrain',
             sourceIcon: true
         },
         templates: {
-            group: 'text',
+            group: 'base',
             options: {
                 content: Spice.rhymes.content,
-		moreAt: true
+                moreAt: true
             }
         }
     });
