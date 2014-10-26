e344ea04818670383339be8be56d1f01608b4d06
chrisjwilsoncom <chris@chrisjwilson.com>
2014-10-26 05:10:48 +0000

    Change dateStr to dateObj. Combine 12 hour convert function.

--- share/spice/time/time.js
+++ share/spice/time/time.js
@@ -6,31 +6,31 @@
             return Spice.failed('time');
         }
 
-        var dateStr = DDG.getDateFromString(api_result.locations[0].time.iso),
+        var dateObj = DDG.getDateFromString(api_result.locations[0].time.iso),
             months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December'),
             days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
-        //Convert 24 hour returned by getHours to 12 hour value
-        var get12Hour = function(hours) {
-            return {
-                "amPM_hours": (hours % 12) || 12,
-                "amPM": hours >= 12 ? "PM" : "AM"
-            };
-        };
+        //Convert 24 hour time to 12 hour time
+        function toPrettyTime(date) {
+          var hours = date.getHours(),
+              minutes = date.getMinutes()
+          return { 
+            hours: (hours % 12) || 12,
+            minutes: minutes<10 ? '0'+minutes : minutes,
+            amPM: hours >= 12 ? "PM" : "AM"
+          }
+        }
+
         
         //Add leading to getMinutes value
         function pad(n){return n<10 ? '0'+n : n}
 
         var dateTime = {
-            time: {
-                hours: get12Hour(dateStr.getHours()).amPM_hours,
-                minutes: pad(dateStr.getMinutes()),
-                amPM: get12Hour(dateStr.getHours()).amPM
-            },
-            dayName: days[dateStr.getDay()],
-            day: dateStr.getDate(),
-            monthName: months[dateStr.getMonth()],
-            year: dateStr.getFullYear(),
+            time: toPrettyTime(dateObj),
+            dayName: days[dateObj.getDay()],
+            day: dateObj.getDate(),
+            monthName: months[dateObj.getMonth()],
+            year: dateObj.getFullYear(),
             placeName: api_result.locations[0].geo.name,
             offset: api_result.locations[0].time.timezone.offset.replace(/0|:/g, ""),
             zone: api_result.locations[0].time.timezone.zonename,
