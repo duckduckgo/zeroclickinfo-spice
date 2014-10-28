de70b2a1dd5b19ae75d725539d1cd63cae48a5c3
chrisjwilsoncom <chris@chrisjwilson.com>
2014-10-28 02:37:53 +0000

    Time: Fix caching parm

--- lib/DDG/Spice/Time.pm
+++ lib/DDG/Spice/Time.pm
@@ -11,14 +11,16 @@ topics "everyday";
 category "geography";
 attribution github  => ['https://github.com/chrisjwilsoncom', 'chrisjwilsoncom'];
 
-spice is_cached => 0;
+spice proxy_cache_valid => "418 1d";
 spice to => 'http://api.xmltime.com/timeservice?accesskey={{ENV{DDG_SPICE_TIME_AND_DATE_ACCESSKEY}}}&secretkey={{ENV{DDG_SPICE_TIME_AND_DATE_SECRETKEY}}}&out=js&prettyprint=1&callback={{callback}}&query=$1&time=1&tz=1&verbosetime=1';
 
 triggers any => "time", "what time is it in", "time in";
 
 handle remainder => sub {
-	return $_ if $_;
-	return;
+    my $q = shift;
+    $q =~ s/,//g;
+    return unless $q;
+    return $q;
 };
 
 1;
