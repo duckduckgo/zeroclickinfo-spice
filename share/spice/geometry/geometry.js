7ee6b5df023d8f297b992c1777901d8676db58c5
pixunil <pixunil@gmx.de>
2014-10-29 16:46:34 +0100

    Geometry: added rect shape and support for multiple parameters

--- share/spice/geometry/geometry.js
+++ share/spice/geometry/geometry.js
@@ -1,18 +1,35 @@
 (function(env){
     "use strict";
 
-    function getParameter(query, trigger){
+    function getParameter(query, trigger, count){
         var regex, result;
-        regex = new RegExp("(?:" + trigger + ")\\s*[=:]?\\s*(\\d+(?:[.,]\\d+)?)");
-        //(?:trigger)\s?[=:]?\s? => matches the name of the formula, following by optional whitespace, = or :, whitespace
-        //(\d+(?:[.,]\d+)?) => matches the number, as integer or float
+        if(!count){
+            regex = new RegExp("(?:" + trigger + ")\\s*[=:]?\\s*(\\d+(?:[.,]\\d+)?)");
+            //(?:trigger)\s?[=:]?\s? => matches the name of the formula, following by optional whitespace, = or :, whitespace
+            //(\d+(?:[.,]\d+)?) => matches the number, as integer or float
 
-        result = query.match(regex);
-        if(result === null) return null; //no match
-        result = result[1].replace(",", "."); //stores the result as string and replace commas with points
-        result = parseFloat(result);
+            result = query.match(regex);
+            if(result === null) return null; //no match
+            result = result[1].replace(",", "."); //stores the result as string and replace commas with points
+            result = parseFloat(result);
 
-        return result;
+            return result;
+        } else {
+            regex = new RegExp("(?:" + trigger + ")\\s*[=:]?\\s*((?:\\d+(?:[.,]\\d+)?)(?:[,;]?\\s+\\d+(?:[.,]\\d+)?){" + count + "})");
+            //(?:trigger)\s?[=:]?\s? => matches the name of the formula, following by optional whitespace, = or :, whitespace
+            //(\d+(?:[.,]\d+)?) => matches the first number, as integer or float
+            //(?:[,;]?\s+\d+(?:[.,]\d+)?){count} => matches all other numbers, as integer or float
+
+            result = query.match(regex);
+            if(result === null) return null; //no match
+            result = result[1].split(/\s+/); //split up the numbers
+            for(var i = 0, l = result.length; i < l; ++i){
+                result[i].replace(",", ".");
+                result[i] = parseFloat(result[i]);
+            }
+
+            return result;
+        }
     }
 
     function format(number){
@@ -82,9 +99,60 @@
                 [2, 2]
             ],
             getParameter: function(query){
-                return getParameter(query, 'a|length|size');
+                return [getParameter(query, "a|length|size")];
             },
-            parameterName: "a"
+            parameterNames: ["a"]
+        },
+        rect: {
+            formulas: [{
+                name: "area",
+                html: "ab",
+                calc: function(a, b){
+                    return a * b;
+                }
+            }, {
+                name: "perimeter",
+                html: "2(a+b)",
+                calc: function(a, b){
+                    return 2 * (a + b);
+                }
+            }, {
+                name: "diagonal",
+                html: "&radic;a<sup>2</sup>+b<sup>2</sup>",
+                calc: function(a, b){
+                    return Math.sqrt(a * a + b * b);
+                }
+            }],
+            svg: [{
+                path: "M 0,0 h 160 v 120 h -160 z",
+                class: "fill"
+            }, {
+                path: "M 0,0 h 160 m 0,120 h -160 m 160,0 v -120 m -160,0 v 120",
+                class: "stroke"
+            }, {
+                path: "M 0,0 l 160,120",
+                class: "stroke special"
+            }],
+            pairs: [
+                [0, 0],
+                [1, 1],
+                [2, 2]
+            ],
+            getParameter: function(query){
+                var p;
+                p = getParameter(query, "length|size", 1);
+                if(p !== null)
+                    return p;
+
+                p = [
+                    getParameter(query, "a"),
+                    getParameter(query, "b")
+                ];
+                if(p[0] !== null && p[1] !== null)
+                    return p;
+                return null;
+            },
+            parameterNames: ["a", "b"]
         }
     };
 
@@ -93,6 +161,7 @@
             shape,
             data = {},
             pairs,
+            parameter,
             i, l,
             j, k,
             got = false;
@@ -114,14 +183,13 @@
         data.formulas = shape.formulas;
         data.svg = shape.svg;
         pairs = shape.pairs;
-        data.parameter = shape.getParameter(query);
-        data.parameterName = shape.parameterName;
+        parameter = shape.getParameter(query);
 
         for(i = 0, l = data.formulas.length; i < l; ++i){
             data.formulas[i].symbol = formulas[data.formulas[i].name];
             data.formulas[i].name = DDG.capitalize(data.formulas[i].name);
             if(data.parameter !== null)
-                data.formulas[i].result = format(data.formulas[i].calc(data.parameter));
+                data.formulas[i].result = format(data.formulas[i].calc.apply(0, parameter));
             if(query.match(data.formulas[i].name, "i")){
                 //cleanup formulas
                 data.formulas = [data.formulas[i]];
@@ -134,6 +202,16 @@
                     }
                 }
                 break;
+            }
+        }
+
+        if(parameter !== null){
+            data.parameter = [];
+            for(i = 0, l = parameter.length; i < l; ++i){
+                data.parameter[i] = {
+                    symbol: shape.parameterNames[i],
+                    value: parameter[i]
+                };
             }
         }
 
