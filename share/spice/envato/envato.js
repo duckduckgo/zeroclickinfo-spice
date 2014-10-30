a214faab8994bbc1b6071dff6c8d1f451e2b16fd
Marcin Dziewulski <hello@mobily.pl>
2014-10-30 11:04:38 +0100

    audio player for audiojungle results

--- share/spice/envato/envato.js
+++ share/spice/envato/envato.js
@@ -7,35 +7,77 @@
 
 		if (fail) return Spice.failed('envato');
 
-		Spice.add({
-			id: 'envato',
-			name: 'Envato Marketplace',
-			data: api.search,
-			normalize: function(item){
-				return {
-					url: item.url,
-					title: item.description,
-					price: '$'+item.item_info.cost.replace('.00', ''),
-					rating: item.item_info.rating_decimal,
-					brand: item.item_info.user,
-					img: item.item_info.thumbnail,
-					image: item.item_info.thumbnail,
-					img_m: item.item_info.thumbnail,
-					heading: item.description,
-					abstract: '',
-					reviewCount: ''
+		var query = DDG.get_query(),
+			markets = ['themeforest', 'codecanyon', 'videohive', 'audiojungle', 'graphicriver', 'photodune', '3docean', 'activeden'],
+			spice = {
+				id: 'envato',
+				name: 'Envato Marketplace',
+				data: api.search,
+				normalize: function(item){
+					return {
+						url: item.url,
+						title: item.description,
+						price: '$'+item.item_info.cost.replace('.00', ''),
+						rating: item.item_info.rating_decimal,
+						brand: item.item_info.user,
+						img: item.item_info.thumbnail,
+						image: item.item_info.thumbnail,
+						img_m: item.item_info.thumbnail,
+						heading: item.description,
+						abstract: '',
+						reviewCount: ''
+					}
+				},
+				templates: {
+					group: 'products',
+					options: {
+						buy: Spice.envato.buy,
+						brand: true,
+						rating: true,
+						price: true
+					}
 				}
 			},
-			templates: {
-				group: 'products',
-				options: {
-                    buy: Spice.envato.buy,
-                    brand: true,
-                    rating: true,
-                    price: true
-                }
+			marketplace = query.match(markets.join('|'))
+
+		if (marketplace && marketplace[0] == 'audiojungle'){
+			spice.normalize = function(item){
+				var info = item.item_info, duration = 0, x = info.length.split(':')
+
+				switch (x.length){
+					case 1:
+						duration = ~~x[0]
+						break;
+					case 2:
+						duration = (~~x[0] * 60) + ~~x[1]
+						break;
+					case 3:
+						duration = (~~x[0] * 3600) + (~~x[0] * 60) + ~~x[1]
+						break;
+				}
+
+				return {
+					image: info.thumbnail,
+					hearts: info.sales,
+					duration: duration*1000,
+					title: item.description,
+					url: item.url,
+					streamURL: info.preview_url,
+					price: info.cost
+				}
 			}
-		});
+
+			spice.templates = {
+				item_custom: 'audio_item',
+				options: {
+					footer: Spice.envato.audio
+				}
+			}
+
+			spice.view = spice.model = 'Audio'
+		}
+
+		Spice.add(spice);
 	}
 
 }(this));
