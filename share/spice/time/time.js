b575efaae491e8f359cf641c79a39f1dd97542e0
chrisjwilsoncom <chris@chrisjwilson.com>
2014-10-30 03:31:33 +0000

    Time: Added more relevancy checking, name + state. Modify api_result var

--- share/spice/time/time.js
+++ share/spice/time/time.js
@@ -1,31 +1,44 @@
 (function(env) {
     "use strict";
-    env.ddg_spice_time = function(api_result) {
+    env.ddg_spice_time = function(api) {
 
-        if (!api_result || api_result.info == "No matches") {
+        if (!api || api.info == "No matches") {
             return Spice.failed('time');
         }
         
          var script = $('[src*="/js/spice/time/"]')[0],
-            source = $(script).attr("src"),
-            query = decodeURIComponent(source.match(/time\/([^\/]+)/)[1]).toLowerCase();
+             source = $(script).attr("src"),
+             query = decodeURIComponent(source.match(/time\/([^\/]+)/)[1]).toLowerCase();
         
          var locID = 0;
          var locArr = [];
         
-        //Check if we have more then one location  
-        if(api_result.locations.length > 1) {
-            for (var i = 0, len = api_result.locations.length; i < len; i++) {
-                //Match exact geo.name
-                if(api_result.locations[i].geo.name.toLowerCase() == query) {
+        if(api.locations.length > 1) {
+            for (var i = 0, len = api.locations.length; i < len; i++) {
+                //Match geo.name only
+                if(api.locations[i].geo.name.toLowerCase() == query) {
+                    locArr.push(i);
+                }
+                //Match geo.name + geo.country.id
+                if(api.locations[i].geo.name.toLowerCase() + " " + api.locations[i].geo.country.id == query) {
+                    locArr.push(i);
+                }
+                //Match geo.name + geo.country.name
+                if(api.locations[i].geo.state) {
+                if(api.locations[i].geo.name.toLowerCase() + " " + api.locations[i].geo.state.toLowerCase() == query) {
+                    locArr.push(i);
+                }
+                }
+                //Match geo.name + geo.state 
+                if(api.locations[i].geo.name.toLowerCase() + " " + api.locations[i].geo.country.name.toLowerCase() == query) {
                     locArr.push(i);
                 }
             }
             //Assign first (most relevant) location ID.
-             locID = locArr[0];
+             locID = locArr[0] ? locArr[0] : 0
         }
-
-        var dateObj = DDG.getDateFromString(api_result.locations[locID].time.iso),
+        
+        var dateObj = DDG.getDateFromString(api.locations[locID].time.iso),
             months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December'),
             days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
@@ -46,10 +59,10 @@
             day: dateObj.getDate(),
             monthName: months[dateObj.getMonth()],
             year: dateObj.getFullYear(),
-            placeName: api_result.locations[locID].geo.name,
-            offset: api_result.locations[locID].time.timezone.offset.replace(/0|:/g, ""),
-            zone: api_result.locations[locID].time.timezone.zonename,
-            country: api_result.locations[locID].geo.country.name
+            placeName: api.locations[locID].geo.name,
+            offset: api.locations[locID].time.timezone.offset.replace(/0|:/g, ""),
+            zone: api.locations[locID].time.timezone.zonename,
+            country: api.locations[locID].geo.country.name
         }
 
         Spice.add({
@@ -58,7 +71,7 @@
             data: dateTime,
             meta: {
                 sourceName: "timeanddate.com",
-                sourceUrl: 'http://www.timeanddate.com/worldclock/city.html?n=' + api_result.locations[locID].id
+                sourceUrl: 'http://www.timeanddate.com/worldclock/city.html?n=' + api.locations[locID].id
             },
             templates: {
                 group: 'base',
