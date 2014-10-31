50cf865ec5aeb4409ab4329d84911cb2124bc973
Marcin Dziewulski <hello@mobily.pl>
2014-10-31 16:18:03 +0100

    spaces instead of tabs, various fixes

--- share/spice/envato/envato.js
+++ share/spice/envato/envato.js
@@ -1,83 +1,86 @@
 (function(env){
-	'use strict';
+    'use strict';
 
-	env.ddg_spice_envato = function(api){
-		var has = api.hasOwnProperty('search'),
-			fail = !api || !has || (has && !api.search.length);
+    env.ddg_spice_envato = function(api){
+        var has = api.hasOwnProperty('search'),
+            fail = !api || !has || (has && !api.search.length);
 
-		if (fail) return Spice.failed('envato');
+        if (fail) return Spice.failed('envato');
 
-		var query = DDG.get_query(),
-			markets = ['themeforest', 'codecanyon', 'videohive', 'audiojungle', 'graphicriver', '3docean', 'activeden'],
-			spice = {
-				id: 'envato',
-				name: 'Envato Marketplace',
-				data: api.search,
-				normalize: function(item){
-					return {
-						url: item.url,
-						title: item.description,
-						price: '$'+item.item_info.cost.replace('.00', ''),
-						rating: item.item_info.rating_decimal,
-						brand: item.item_info.user,
-						img: item.item_info.thumbnail,
-						image: item.item_info.thumbnail,
-						img_m: item.item_info.thumbnail,
-						heading: item.description,
-						abstract: '',
-						reviewCount: ''
-					}
-				},
-				templates: {
-					group: 'products',
-					options: {
-						buy: Spice.envato.buy,
-						brand: true,
-						rating: true,
-						price: true
-					}
-				}
-			},
-			marketplace = query.match(markets.join('|'))
+        var query = DDG.get_query(),
+            markets = ['themeforest', 'codecanyon', 'videohive', 'audiojungle', 'graphicriver', '3docean', 'activeden'],
+            spice = {
+                id: 'envato',
+                name: 'Envato Marketplace',
+                data: api.search,
+                normalize: function(item){
+                    var info = item.item_info;
 
-		if (marketplace && marketplace[0] == 'audiojungle'){
-			spice.normalize = function(item){
-				var info = item.item_info, duration = 0, x = info.length.split(':')
+                    return {
+                        url: item.url,
+                        price: '$'+info.cost.replace('.00', ''),
+                        rating: info.rating_decimal,
+                        brand: info.user,
+                        img: info.thumbnail,
+                        heading: item.description,
+                        reviewCount: info.sales
+                    }
+                },
+                templates: {
+                    group: 'products',
+                    detail: false,
+                    item_detail: false,
+                    options: {
+                        buy: Spice.envato.buy,
+                        brand: true,
+                        rating: true,
+                        price: true
+                    }
+                }
+            },
+            marketplace = query.match(markets.join('|'))
 
-				switch (x.length){
-					case 1:
-						duration = ~~x[0]
-						break;
-					case 2:
-						duration = (~~x[0] * 60) + ~~x[1]
-						break;
-					case 3:
-						duration = (~~x[0] * 3600) + (~~x[1] * 60) + ~~x[2]
-						break;
-				}
 
-				return {
-					image: info.thumbnail,
-					hearts: info.sales,
-					duration: duration*1000,
-					title: item.description,
-					url: item.url,
-					streamURL: info.preview_url,
-					price: info.cost.replace('.00', '')
-				}
-			}
+        // if audiojungle then switch to audio layout
+        
+        if (marketplace && marketplace[0] == 'audiojungle'){
+            spice.normalize = function(item){
+                var info = item.item_info, duration = 0, x = info.length.split(':')
 
-			spice.templates = {
-				item_custom: 'audio_item',
-				options: {
-					footer: Spice.envato.audio
-				}
-			}
+                switch (x.length){
+                    case 1:
+                        duration = +x[0]
+                        break;
+                    case 2:
+                        duration = (+x[0] * 60) + +x[1]
+                        break;
+                    case 3:
+                        duration = (+x[0] * 3600) + (+x[1] * 60) + (+x[2])
+                        break;
+                }
 
-			spice.view = spice.model = 'Audio'
-		}
+                return {
+                    image: info.thumbnail,
+                    hearts: info.sales,
+                    duration: duration*1000,
+                    title: item.description,
+                    url: item.url,
+                    streamURL: info.preview_url,
+                    price: info.cost.replace('.00', '')
+                }
+            }
 
-		Spice.add(spice);
-	}
+            spice.templates = {
+                item_custom: 'audio_item',
+                options: {
+                    footer: Spice.envato.audio
+                }
+            }
+
+            spice.view = spice.model = 'Audio'
+        }
+
+        Spice.add(spice);
+    }
 
 }(this));
