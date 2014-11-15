5415a06f8fc4aa9bc71c2104bbea4d2f0b5c403b
mooseDog <robin.hoover@gmail.com>
2014-11-15 20:55:34 +0000

    Tiles All Sorted

--- share/spice/vt_beer_trail/vt_beer_trail.js
+++ share/spice/vt_beer_trail/vt_beer_trail.js
@@ -8,25 +8,22 @@
             return Spice.failed('vt_beer_trail');
         }
         
-//         var brewerLocation = function location(item) {
-//             return item.city + ',' + item.statestate;
-//         };
-        
         // Render the response
         Spice.add({
             
             id: "vt_beer_trail",
             name: "VT Beer Trail",
             data: api_result,
+            
             meta: {
                 sourceName: "VT Beer Trail",
                 sourceUrl: 'http://www.vtbeertrail.com',
                 sourceIcon: true,
-//                 sourceIconUrl: DDG.get_asset_path('vt_beer_trail','vtbeertrail.com.ico'),
                 searchTerm: 'Vermont',
                 itemType: 'Breweries'
                 
             },
+            
             normalize: function(item) {
                 return {
                     image: 'http://vtbeertrail.com/' + item.vba100,
@@ -34,6 +31,14 @@
                     description: item.city + ', ' + item.state
                 };
             },
+            
+            sort_fields: {
+                brewer: function(a,b) {
+                    return (a.selector < b.selector) ? -1 : 1;
+                }
+            },
+            sort_default: 'brewer',
+            
             templates: {
                 group: 'info',
                 //item: 'basic_image_item', //Spice.vt_beer_trail.vt_beer_trail_item references custom template vt_beer_trail_item.handlebars
