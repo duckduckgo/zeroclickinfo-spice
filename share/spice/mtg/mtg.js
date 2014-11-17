9280a01a219d1196a54ae18f54f3ccebfeca6d47
puskin94 <puskin94@users.noreply.github.com>
2014-11-17 21:39:23 +0100

    Repulled ex-new Spice: Mtg

---
+++ share/spice/mtg/mtg.js
@@ -0,0 +1,31 @@
+(function (env) {
+    "use strict";
+    env.ddg_spice_mtg = function(api_result){
+
+
+        if (api_result.error) {
+            return Spice.failed('mtg');
+        }
+
+        Spice.add({
+            id: "mtg",
+            name: "Magic The Gathering",
+            data: api_result[0],
+            meta: {
+                sourceUrl: "http://www.mtgdb.info/cards/" + api_result[0].id,
+                sourceName: "http://mtgdb.info",
+                sourceIcon: true,
+
+            },
+            templates: {
+                group: 'base',
+                options:{
+                    content: Spice.mtg.content,
+                    moreAt: true
+                }
+            }
+        });
+        
+    
+    };
+}(this));
