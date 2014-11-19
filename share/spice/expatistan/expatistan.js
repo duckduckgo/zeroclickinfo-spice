2ea4e94b934d002db49ab8bfe0694cb166e6bcf9
chrisjwilsoncom <chris@chrisjwilson.com>
2014-11-19 02:33:57 +0000

    Expatistan: Display Result For Country Comparison
    The Expatistan API will return a "abstract" result like "Cost of living in <b>London</b> is 23% higher than in <b>Paris</b>" for a country comparision.
    Due to the regexp used to split the string based on the index (number) the firstline var containes no data when a country comparision is performed.
    Simply check if we have an index (number) if not just return the firstline exactly how it is recieved from the API.

--- share/spice/expatistan/expatistan.js
+++ share/spice/expatistan/expatistan.js
@@ -24,12 +24,17 @@
                 var lines = api_result.abstract.split(/of\s[0-9]+\s/);
                 var cost = api_result.abstract.match(/\s[0-9]+\s/);
 
+              
                 var firstLine = '';
                 var secondLine = '';
+                
                 if (lines.length === 2 && cost.length >= 1) {
                     firstLine = lines[0].replace(' has a', '') + ' =' + cost[0];
                     secondLine = lines[1];
-                }
+                }              
+
+                if(!cost) firstLine = lines[0].replace(/\./, "");   
+                
                 return {
                     title: DDG.strip_html(firstLine),
                     subtitle: DDG.strip_html(secondLine)
