d4dc1dce5171dec3b9f5fd038a9bcd6659515056
puskin94 <puskin94@users.noreply.github.com>
2014-11-19 08:42:05 +0100

    Update BaconIpsum.pm

--- lib/DDG/Spice/BaconIpsum.pm
+++ lib/DDG/Spice/BaconIpsum.pm
@@ -3,7 +3,7 @@ package DDG::Spice::BaconIpsum;
 
 use DDG::Spice;
 
-spice is_cached => 1;
+spice proxy_cache_valid => "200 1d";
 
 name "BaconIpsum";
 description "This Spice returns a number of BaconIpsum paragraphs";
@@ -12,13 +12,16 @@ primary_example_queries "baconipsum 4", "baconipsum 9";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/BaconIpsum.pm";
 attribution github => ["puskin94", "puskin"];
 
-triggers any => "baconipsum", "Baconipsum", "BaconIpsum";
+triggers startend => "baconipsum";
 
 spice to => 'http://baconipsum.com/api/?type=all-meat&paras=$1';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
-	return $_ if $_;
+	my $num = shift;
+	return unless $num;
+	return unless $num =~ qr/^\d+$/; 
+	return $num;
     return $_;
 };
 
