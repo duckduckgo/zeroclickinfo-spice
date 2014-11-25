21104f34eeea4787150d628e020be5dbe1c8e17c
Joe Wegner <joe@bellycard.com>
2014-11-25 14:05:03 -0600

    grab language in perl

--- share/spice/in_every_lang/in_every_lang.js
+++ share/spice/in_every_lang/in_every_lang.js
@@ -1,53 +1,19 @@
 (function (env) {
     "use strict";
-    // Take the ineverylang.com standard language strings, and translate them into some more readable search query terms
-    var language_translations = {
-      "C": [" c"],
-      "C++": ["c++", "cpp"],
-      "C#": ["c#", "csharp"],
-      "Erlang": ["erlang"],
-      "Go": ["go"],
-      "Haskell": ["haskell"],
-      "Java": ["java"],
-      "Javascript": ["javascript", "js", "jquery"],
-      "MATLAB": ["mat"],
-      "Objective-C": ["objc", "obj-c", "obc", "objective-c", "objectivec"],
-      "Perl": ["perl"],
-      "PHP": ["php"],
-      "Python": ["python"],
-      "R": [" r"],
-      "Ruby": ["ruby"],
-      "Scala": ["scala"],
-      "Swift": ["swift"]
-    };
 
     env.ddg_spice_in_every_lang = function(api_results){
-      var query = DDG.get_query().toLowerCase();
-      var lang;
 
-      // Try to find which language the query is looking for
-      for(lang in api_results) {
-        var solution = api_results[lang];
+      var script = $('[src*="/js/spice/in_every_lang/"]')[0];
+      var source = $(script).attr("src");
+      var matches = source.match(/in_every_lang\/[^\/]+\/([^\/]*)/) || [];
 
-        var langTranslations = language_translations[lang];
+      if(matches.length) {
+        var query = decodeURIComponent(matches[1]);
+      }
 
-        var selectedLanguage = false;
-        for(var i=0; i<langTranslations.length; i++) {
-          var translation = langTranslations[i];
-
-          if(translation.length === 2 && translation[0] === " ") {
-            if(query[query.length - 1] == translation[1]) {
-              selectedLanguage = lang;
-              break;
-            }
-          } else if(query.indexOf(langTranslations[i]) > -1) {
-            selectedLanguage = lang;
-            break;
-          }
-       }
-
-       if(selectedLanguage) break;
-
+      var selectedLanguage = false;
+      if(matches.length && typeof(api_results[query]) !== "undefined") {
+        selectedLanguage = query;
       }
 
       // Use the searched for language - if not, just use the first language returned
