704d13328a9f970379d0f7bc47b32b5cea7b875c
Joe Wegner <joe@bellycard.com>
2014-11-25 14:05:03 -0600

    grab language in perl

--- lib/DDG/Spice/InEveryLang.pm
+++ lib/DDG/Spice/InEveryLang.pm
@@ -2,6 +2,25 @@ package DDG::Spice::InEveryLang;
 # ABSTRACT: Returns code examples for popular coding puzzles
 
 use DDG::Spice;
+use JSON qw( );
+
+my $json = JSON->new;
+my $languages_raw = share('languages.json')->slurp;
+my $languages = $json->decode($languages_raw);
+
+my @language_translations = ();
+my @language_regexps = ();
+for( @{$languages->{languages}} ) {
+  my $language = $_->{language};
+  $language =~ s/\#/sharp/g;
+  $language =~ s/\+/plus/g;
+  $language =~ s/\-/dash/g;
+  push(@language_regexps, "(?<".$language.">".join("|", @{ $_->{triggers} }).")");
+}
+
+my $language_finder = join("|", @language_regexps);
+$language_finder =~ s/\+/\\+/g;
+
 
 primary_example_queries "Fizz Buzz in C";
 description "Shows a code puzzle example";
@@ -16,21 +35,39 @@ attribution github  => ['https://github.com/josephwegner', 'josephwegner'],
 triggers startend => "fizz buzz", "fizzbuzz", "quine", "fiboniacci sequence", "binary search";
 
 spice to => 'http://www.ineverylang.com/$1.json';
+spice from => '(^[^\/]+)';
 spice wrap_jsonp_callback => 1;
 
 handle query_lc => sub {
   $_ =~ m/(fizz ?buzz)|(quine)|(fibonacci sequence)|(binary search)/;
+  my $puzzle;
   if($1) {
-    return 'fizz-buzz';
+    $puzzle = 'fizz-buzz';
   } elsif($2) {
-    return 'quine';
+    $puzzle = 'quine';
   } elsif ($3) {
-    return 'fibonacci-sequence';
+    $puzzle = 'fibonacci-sequence';
   } elsif ($4) {
-    return 'binary-search';
+    $puzzle = 'binary-search';
   } else {
     return;
   }
+
+  $_ =~ /$language_finder/;
+  my @languages = keys %+;
+  my $languages_length = @languages;
+
+  if($languages_length > 0) {
+    my $language = pop(@languages);
+    $language =~ s/sharp/\#/g;
+    $language =~ s/plus/\+/g;
+    $language =~ s/dash/\-/g;
+    return $puzzle, $language;
+  } else {
+    return $puzzle
+  }
+  
+
 };
 
 1;
