6cec0f6579bd034de0c915e2daf01b664d134f25
chrisjwilsoncom <chris@chrisjwilson.com>
2014-11-25 12:48:23 +0000

    TFL Status: Update Template to Text. Add API error Handling
    Removed content.handlebars and switched to using the text template.

--- share/spice/tfl_status/tfl_status.js
+++ share/spice/tfl_status/tfl_status.js
@@ -1,19 +1,32 @@
 (function (env) {
     "use strict";
     env.ddg_spice_tfl_status = function(api_result){
-
+        
+    if (!api_result) {
+        return Spice.failed('tfl_status');
+    }
+        
+    var title = api_result[0].lineStatuses[0].statusSeverityDescription + " on the " + api_result[0].name + " line.",
+        subtitle = api_result[0].lineStatuses[0].reason,
+        sourceUrl = 'http://tfl.gov.uk/tube-dlr-overground/status/#line-' + api_result[0].id;
+        
         Spice.add({
             id: "tfl_status",
             name: "Travel",
             data: api_result,
             meta: {
                 sourceName: "tfl.gov.uk",
-                sourceUrl: 'http://tfl.gov.uk/tube-dlr-overground/status/#line-' + api_result[0].id
+                sourceUrl: sourceUrl
+            },
+            normalize: function(item) {
+                return {
+                    title: title,
+                    subtitle: subtitle
+                };
             },
             templates: {
-                group: 'base',
+                group: 'text',
                 options: {
-                    content: Spice.tfl_status.content,
                     moreAt: true
                 }
             }
