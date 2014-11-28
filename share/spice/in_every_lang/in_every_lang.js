96adff30846223d259bc0a4c01b8d0173390eb3e
jagtalon <jag@jagtalon.com>
2014-11-28 16:25:27 +0000

    InEveryLang: We need to exit if api_results is empty.

--- share/spice/in_every_lang/in_every_lang.js
+++ share/spice/in_every_lang/in_every_lang.js
@@ -1,50 +1,54 @@
 (function (env) {
-        "use strict";
+    "use strict";
 
-        env.ddg_spice_in_every_lang = function(api_results){
+    env.ddg_spice_in_every_lang = function(api_results){
 
-            var script = $('[src*="/js/spice/in_every_lang/"]')[0];
-            var source = $(script).attr("src");
-            var matches = source.match(/in_every_lang\/[^\/]+\/([^\/]*)/) || [];
+        if(!api_results) {
+            return Spice.failed("in_every_lang");
+        }
+        
+        var script = $('[src*="/js/spice/in_every_lang/"]')[0];
+        var source = $(script).attr("src");
+        var matches = source.match(/in_every_lang\/[^\/]+\/([^\/]*)/) || [];
 
-            if(matches.length) {
-                var query = decodeURIComponent(matches[1]);
+        if(matches.length) {
+            var query = decodeURIComponent(matches[1]);
+        }
+
+        var selectedLanguage;
+        if(matches.length && typeof(api_results[query]) !== "undefined") {
+            selectedLanguage = query;
+        } else {
+            return Spice.failed('in_every_lang');
+        }
+
+        // Use the searched for language - if not, just use the first language returned
+        var solution = api_results[selectedLanguage];
+        var solutionUrl = solution.url;
+
+        Spice.add({
+            id: "in_every_lang",
+            name: "Code Snippet",
+            data: solution,
+            meta: {
+                sourceName: "ineverylang.com",
+                sourceUrl: solutionUrl
+            },
+
+            normalize: function(item) {
+                return {
+                    title: solution.puzzle.prettyName,
+                    subtitle: selectedLanguage 
+                }    
+            },
+
+            templates: {
+                group: 'text',
+                options: {
+                    content: Spice.in_every_lang.content,
+                    moreAt: true
+                }
             }
-
-            var selectedLanguage;
-            if(matches.length && typeof(api_results[query]) !== "undefined") {
-                selectedLanguage = query;
-            } else {
-              return Spice.failed('in_every_lang');
-            }
-
-            // Use the searched for language - if not, just use the first language returned
-            var solution = api_results[selectedLanguage];
-            var solutionUrl = solution.url;
-
-            Spice.add({
-                    id: "in_every_lang",
-                    name: "Code Snippet",
-                    data: solution,
-                    meta: {
-                            sourceName: "ineverylang.com",
-                            sourceUrl: solutionUrl
-                    },
-                    
-                    normalize: function(item) {
-                            return {
-                                title: solution.puzzle.prettyName,
-                                subtitle: selectedLanguage 
-                            }    
-                    },
-
-                    templates: {
-                            group: 'text',
-                            options: {
-                                    content: Spice.in_every_lang.content,
-                                    moreAt: true
-                            }
-                    }
-            });
-        };
+        });
+    };
 }(this));
