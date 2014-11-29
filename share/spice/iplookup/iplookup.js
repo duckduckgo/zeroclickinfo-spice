a6638813bb8859f89079e6a97ec0114520ef2994
chrisjwilsoncom <chris@chrisjwilson.com>
2014-11-29 02:25:59 +0000

    IPLookup: Remove Handlebars Template, Switch to returning data via JS

--- share/spice/iplookup/iplookup.js
+++ share/spice/iplookup/iplookup.js
@@ -5,6 +5,13 @@
         if (!api_result || api_result.error) {
             return Spice.failed('iplookup');
         }
+                
+        var location,
+            blacklists
+        api_result.ddg.shortloc ? location=" ("+api_result.ddg.shortloc+")" : location="";
+        api_result.ddg.blacklists ? blacklists="Blacklists: <br>"+api_result.ddg.blacklists : blacklists="";
+        var title_info = api_result.ddg.famlist[0],
+            subtitle_info = "Owner: " + api_result.ddg.shortwho + location + blacklists
 
         Spice.add({
             id: "iplookup",
@@ -14,10 +21,16 @@
                 sourceName: "RobTex",
                 sourceUrl: api_result.ddg.lnk
             },
+            normalize: function(item) {
+                return {
+                    title: title_info,
+                    subtitle: subtitle_info
+                };
+            },
             templates: {
-                group: 'base',
+                group: 'text',
                 options: {
-                    content: Spice.iplookup.content,
+                    //content: Spice.iplookup.content,
                     moreAt: true
                 }
             },
