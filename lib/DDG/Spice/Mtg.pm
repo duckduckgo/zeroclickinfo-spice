0b4ba6fe6cd824a6ef80dcceb7081f720da68682
puskin94 <puskin94@users.noreply.github.com>
2014-11-29 17:13:10 +0100

    Update Mtg.pm

--- lib/DDG/Spice/Mtg.pm
+++ lib/DDG/Spice/Mtg.pm
@@ -4,6 +4,15 @@ package DDG::Spice::Mtg;
 
 use DDG::Spice;
 
+name "Mtg Card Search";
+description "mtg <card name> Returns infos on the Magic The Gathering Card";
+primary_example_queries "mtg nullify", "mtg Boros Reckoner";
+
+category "entertainment";
+topics "entertainment", "gaming";
+code_url "https://github.com/puskin94/zeroclickinfo-spice/blob/mtg/lib/DDG/Spice/Mtg.pm";
+attribution github => ["https://github.com/puskin94", "puskin"];
+
 triggers start => 'mtg';
 spice to => 'http://api.mtgdb.info/cards/$1';
 spice wrap_jsonp_callback => 1;
