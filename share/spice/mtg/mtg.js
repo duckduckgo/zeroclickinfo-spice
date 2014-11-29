73caffdb98c8ad30638b021464fcd268517d2eeb
puskin94 <puskin94@users.noreply.github.com>
2014-11-29 17:01:56 +0100

    Update mtg.js

--- share/spice/mtg/mtg.js
+++ share/spice/mtg/mtg.js
@@ -2,10 +2,39 @@
     "use strict";
     env.ddg_spice_mtg = function(api_result){
 
-
         if (api_result.error) {
             return Spice.failed('mtg');
         }
+
+
+        var infoboxItems = {
+            manaCost: "Mana Cost",
+            convertedManaCost: "Converted Mana Cost",
+            cardSetName: "Card Set Name",
+            type: "Type",
+            subType: "Sub Type",
+            power: "Power",
+            toughness: "Toughness",
+            loyalty: "Loyalty",
+            rarity: "Rarity",
+            artist: "Artist",
+            cardSetId: "Card Set Id",
+            token: "Token",
+            promo: "Promo"
+        };
+
+        var infoboxData = [{
+            heading: 'Card Details',
+        }];
+
+        $.each(infoboxItems, function(key, value){
+            if (api_result[0][key]){
+                infoboxData.push({
+                    label: value,
+                    value: api_result[0][key]
+                });
+            }
+        });
 
         Spice.add({
             id: "mtg",
@@ -17,16 +46,16 @@
                 sourceIcon: true,
 
             },
+            normalize: function(item) {
+                return {
+                    title: item.name,
+                    image:  "http://api.mtgdb.info/content/card_images/" + item.id + ".jpeg",
+                    infoboxData: infoboxData
+                };
+            },
             templates: {
-                group: 'base',
-                options:{
-                    content: Spice.mtg.content,
-                    moreAt: true
-                }
+                group: 'info'
             }
         });
-        
-    
     };
 }(this));
-
