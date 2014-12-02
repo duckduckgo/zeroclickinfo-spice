1229558026a358cb8fd26e6ba191fca1aa489e6e
Usman Raza <usman.r123@gmail.com>
2014-12-02 13:32:36 +0500

    add closue, and music note

--- share/spice/lastfm/top_tracks/lastfm_top_tracks.js
+++ share/spice/lastfm/top_tracks/lastfm_top_tracks.js
@@ -1,48 +1,52 @@
-ddg_spice_lastfm_top_tracks = function (api_result) {
+(function (env) {
+    "use strict";
+    env.ddg_spice_lastfm_top_tracks = function (api_result) {
 
-    if(api_result.error || !api_result.toptracks || !api_result.toptracks.track || api_result.toptracks.track.length === 0) {
-        Spice.failed('lastfm_top_tracks');
-    }
- 
-    Spice.add({
-        id: 'lastfm_top_tracks',
-        name: 'Top Songs (' + api_result.toptracks['@attr'].country + ')',
-        data: api_result.toptracks.track,
-        meta: {
-            itemType: 'Tracks',
-            sourceName: 'Last.fm',
-            sourceUrl: 'http://www.last.fm/charts/tracks/top',
-            sourceIconUrl: 'http://cdn.last.fm/flatness/favicon.2.ico'
-        },
-        normalize: function(item) {
-            var seconds = item.duration % 60;
-            if (seconds < 10) {
-                seconds = "0" + seconds;
-            }
- 
-            if (!item.image) {
-                return;
-            }
-
-            return {
-                img: item.image[2]["#text"],
-                heading: item.name + " - " +item.artist.name,
-                url: item.url,
-                reviewCount: "〜 " + DDG.commifyNumber(item.listeners),
-                price: Math.floor(item.duration / 60) + ":" + seconds,
-            };
-        },
-        templates: {
-            group: 'products',
-            detail: false,
-            item_detail: false,
-            options: {
-                moreAt: true,
-                reviewCount: true
-            }
-        },
-        onShow: function () {
-                Spice.getDOM('lastfm_top_tracks').find('.stars').hide();
+        if(api_result.error || !api_result.toptracks || !api_result.toptracks.track || api_result.toptracks.track.length === 0) {
+            Spice.failed('lastfm_top_tracks');
         }
-    });
-}
+ 
+        Spice.add({
+            id: 'lastfm_top_tracks',
+            name: 'Top Songs (' + api_result.toptracks['@attr'].country + ')',
+            data: api_result.toptracks.track,
+            meta: {
+                itemType: 'Tracks',
+                sourceName: 'Last.fm',
+                sourceUrl: 'http://www.last.fm/charts/tracks/top',
+                sourceIconUrl: 'http://cdn.last.fm/flatness/favicon.2.ico'
+            },
+            normalize: function(item) {
+                var seconds = item.duration % 60;
+                if (seconds < 10) {
+                    seconds = "0" + seconds;
+                }
+ 
+                if (!item.image) {
+                    return;
+                }
+
+                return {
+                    img: item.image[2]["#text"],
+                    heading: item.name + " - " +item.artist.name,
+                    url: item.url,
+                    reviewCount: "♬ " + DDG.commifyNumber(item.listeners),
+                    price: Math.floor(item.duration / 60) + ":" + seconds,
+                };
+            },
+            templates: {
+                group: 'products',
+                detail: false,
+                item_detail: false,
+                options: {
+                    moreAt: true,
+                    reviewCount: true
+                }
+            },
+            onShow: function () {
+                Spice.getDOM('lastfm_top_tracks').find('.stars').hide();
+            }
+        });
+    };
+}(this));
+
