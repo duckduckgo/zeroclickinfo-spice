7a2ff6544cbb7664df2fd5fcc7542039a81baef2
puskin94 <puskin94@users.noreply.github.com>
2014-12-04 11:07:01 +0100

    Added Metadata

--- lib/DDG/Spice/Launchbug.pm
+++ lib/DDG/Spice/Launchbug.pm
@@ -1,31 +1,25 @@
 package DDG::Spice::Launchbug;
-# ABSTRACT: Write an abstract here
-# Start at https://duck.co/duckduckhack/spice_overview if you are new
-# to instant answer development
 
 use DDG::Spice;
 
 spice is_cached => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
+
 name "Launchbug";
 description "Succinct explanation of what this instant answer does";
 primary_example_queries "first example query", "second example query";
 secondary_example_queries "optional -- demonstrate any additional triggers";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
-# category "";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
-# topics "";
+category "programming";
+topics "programming";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Launchbug.pm";
-attribution github => ["GitHubAccount", "Friendly Name"],
-            twitter => "twitterhandle";
+attribution github => ["https://github.com/puskin94/", "puskin"];
 
-# Triggers
+
 triggers startend => "launchbug";
 spice to => 'https://api.launchpad.net/devel/bugs/$1?ws.accept=application%2Fjson';
 spice wrap_jsonp_callback => 1;
 
-# Handle statement
+
 handle remainder => sub {
 
     my $num = shift;
