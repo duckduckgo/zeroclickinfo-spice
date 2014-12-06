0b2dbd0d9d064444dd67c8a5b9da0d59a130ecb7
tvdavid <david@tvmaze.com>
2014-12-06 14:23:28 -0500

    add previous/latest episode IA

---
+++ share/spice/tvmaze/previousepisode/tvmaze_previousepisode.js
@@ -0,0 +1,32 @@
+(function (env) {
+    "use strict";
+
+    env.ddg_spice_tvmaze_previousepisode = function(api_result){
+        if (!api_result._embedded) {
+            return Spice.failed('tvmaze');
+        }
+
+        Spice.add({
+            id: "tvmaze",
+            name: "TV Shows",
+            data: api_result,
+            meta: {
+                sourceName: "TVmaze.com",
+                sourceUrl: api_result._embedded.previousepisode.url
+            },
+            normalize: function(item){
+                return {
+                    image: item.image ? item.image.medium : null,
+                    title: item.name
+                };
+            },
+            templates: {
+                group: 'info',
+                options: {
+                    content: Spice.tvmaze_previousepisode.content,
+                    moreAt: true
+                }
+            }
+        });
+    };
+}(this));
