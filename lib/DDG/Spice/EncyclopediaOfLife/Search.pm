f15357ec3dce5ec1fe280cf81f97f73ae825db5d
steveglick <steven@theglicks.org>
2014-12-06 19:46:21 +0000

    Add keywords

--- lib/DDG/Spice/EncyclopediaOfLife/Search.pm
+++ lib/DDG/Spice/EncyclopediaOfLife/Search.pm
@@ -6,7 +6,7 @@ name "Encyclopedia of Life";
 description "Get information about plant and animal life";
 source "Encyclopedia of Life";
 primary_example_queries "animal robin";
-secondary_example_queries "animal sparrow";
+secondary_example_queries "plant daisy";
 icon_url "http://eol.org/assets/favicon-9de6ee8ce10b9ad7b2662236411f4539.ico";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/EncyclopediaOfLife/Search.pm";
 attribution web => ['http://www.steveglick.net', 'Steve Glick'],
@@ -14,7 +14,7 @@ attribution web => ['http://www.steveglick.net', 'Steve Glick'],
             twitter => ['https://twitter.com/stevenmglick', 'stevenmglick'];
 
 spice to => 'http://eol.org/api/search/$1.json?callback={{callback}}';
-triggers startend => 'animal';
+triggers startend => 'animal', 'plant';
 
 handle remainder => sub {
     return $_ if $_;
