a43165770cc9d19a715aa0efb2d9b56932283e6c
tvdavid <david@tvmaze.com>
2014-12-06 11:27:06 -0500

    use official infobox implementation

--- share/spice/tvmaze/show/tvmaze_show.js
+++ share/spice/tvmaze/show/tvmaze_show.js
@@ -15,16 +15,63 @@
                 sourceUrl: api_result.url
             },
 			normalize: function(item){
+				var infoboxData = [
+					{
+						heading: 'Show Information'
+					},
+					{
+						label: 'Show Type',
+						value: item.type
+					},
+					{
+						label: 'Show Status',
+						value: item.status
+					},
+					{
+						label: 'Premiered on',
+						value: item.premiered
+					}
+				];
+				
+				if (item.network) {
+					infoboxData.push(
+						{
+							label: 'Network',
+							value: item.network.name,
+						},
+						{
+							label: 'Country of origin',
+							value: item.network.country.name
+						}
+					);
+				} else if (item.webChannel) {
+					infoboxData.push(
+						{
+							label: 'Web Channel',
+							value: item.webChannel.name,
+						}
+					);
+					
+					if (item.webChannel.country) {
+						infoboxData.push(
+							{
+								label: 'Country of origin',
+								value: item.webChannel.country.name,
+							}
+						);					
+					}
+				}
+			
 				return {
 					image: item.image ? item.image.medium : null,
 					title: item.name,
 					description: DDG.strip_html(item.summary),
+					infoboxData: infoboxData
 				};
 			},
             templates: {
                 group: 'info',
                 options: {
-                    aux: Spice.tvmaze_show.infobox,
                     moreAt: true,
                 }
             },
