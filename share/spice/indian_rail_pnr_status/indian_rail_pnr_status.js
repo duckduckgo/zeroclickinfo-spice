323a11fd95f89e279cb2bb3b6d14a8dc96e6b96b
jee1mr <14.jeevan@gmail.com>
2014-12-06 10:22:41 +0000

    Added 'moreAt', extra fail condition and changed template to text

--- share/spice/indian_rail_pnr_status/indian_rail_pnr_status.js
+++ share/spice/indian_rail_pnr_status/indian_rail_pnr_status.js
@@ -2,7 +2,7 @@
     "use strict";
     env.ddg_spice_indian_rail_pnr_status = function(api_result){
 
-        if (api_result.error) {
+        if (api_result.error || !api_result.result) {
             return Spice.failed('indian_rail_pnr_status');
         }
 
@@ -10,6 +10,10 @@
             id: "indian_rail_pnr_status",
             name: "PNR Enquiry",
             data: api_result,
+            meta: {
+                sourceName: "erail.in",
+                sourceUrl: 'http://erail.in/indian-railway-pnr-status?pnr=' + api_result.result.pnr
+            },
             templates: {
                 group: 'text',
                 options:{
