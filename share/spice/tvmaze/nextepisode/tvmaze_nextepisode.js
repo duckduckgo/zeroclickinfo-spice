e9a3779ee5a4fcbfdf78b487c634800a7ac1a0a7
tvdavid <david@tvmaze.com>
2014-12-06 14:09:10 -0500

    adhere to style guide

--- share/spice/tvmaze/nextepisode/tvmaze_nextepisode.js
+++ share/spice/tvmaze/nextepisode/tvmaze_nextepisode.js
@@ -1,6 +1,6 @@
 (function (env) {
     "use strict";
-    
+
     env.ddg_spice_tvmaze_nextepisode = function(api_result){
         if (!api_result._embedded) {
             return Spice.failed('tvmaze');
@@ -14,19 +14,19 @@
                 sourceName: "TVmaze.com",
                 sourceUrl: api_result._embedded.nextepisode.url
             },
-			normalize: function(item){
-				return {
-					image: item.image ? item.image.medium : null,
-					title: item.name,
-				};
-			},
+            normalize: function(item){
+                return {
+                    image: item.image ? item.image.medium : null,
+                    title: item.name
+                };
+            },
             templates: {
                 group: 'info',
                 options: {
                     content: Spice.tvmaze_nextepisode.content,
-                    moreAt: true,
+                    moreAt: true
                 }
-            },
+            }
         });
     };
 }(this));
--- share/spice/tvmaze/show/tvmaze_show.js
+++ share/spice/tvmaze/show/tvmaze_show.js
@@ -1,6 +1,6 @@
 (function (env) {
     "use strict";
-    
+
     env.ddg_spice_tvmaze_show = function(api_result){
         if (!api_result.id) {
             return Spice.failed('tvmaze');
@@ -14,67 +14,67 @@
                 sourceName: "TVmaze.com",
                 sourceUrl: api_result.url
             },
-			normalize: function(item){
-				var infoboxData = [
-					{
-						heading: 'Show Information'
-					},
-					{
-						label: 'Show Type',
-						value: item.type
-					},
-					{
-						label: 'Show Status',
-						value: item.status
-					},
-					{
-						label: 'Premiered on',
-						value: item.premiered
-					}
-				];
-				
-				if (item.network) {
-					infoboxData.push(
-						{
-							label: 'Network',
-							value: item.network.name,
-						},
-						{
-							label: 'Country of origin',
-							value: item.network.country.name
-						}
-					);
-				} else if (item.webChannel) {
-					infoboxData.push(
-						{
-							label: 'Web Channel',
-							value: item.webChannel.name,
-						}
-					);
-					
-					if (item.webChannel.country) {
-						infoboxData.push(
-							{
-								label: 'Country of origin',
-								value: item.webChannel.country.name,
-							}
-						);					
-					}
-				}
-			
-				return {
-					image: item.image ? item.image.medium : null,
-					title: item.name,
-					description: DDG.strip_html(item.summary),
-					infoboxData: infoboxData
-				};
-			},
+            normalize: function(item){
+                var infoboxData = [
+                    {
+                        heading: 'Show Information'
+                    },
+                    {
+                        label: 'Show Type',
+                        value: item.type
+                    },
+                    {
+                        label: 'Show Status',
+                        value: item.status
+                    },
+                    {
+                        label: 'Premiered on',
+                        value: item.premiered
+                    }
+                ];
+
+                if (item.network) {
+                    infoboxData.push(
+                        {
+                            label: 'Network',
+                            value: item.network.name
+                        },
+                        {
+                            label: 'Country of origin',
+                            value: item.network.country.name
+                        }
+                    );
+                } else if (item.webChannel) {
+                    infoboxData.push(
+                        {
+                            label: 'Web Channel',
+                            value: item.webChannel.name
+                        }
+                    );
+
+                    if (item.webChannel.country) {
+                        infoboxData.push(
+                            {
+                                label: 'Country of origin',
+                                value: item.webChannel.country.name
+                            }
+                        );
+                    }
+                }
+
+                return {
+                    image: item.image ? item.image.medium : null,
+                    title: item.name,
+                    description: DDG.strip_html(item.summary),
+                    infoboxData: infoboxData
+                };
+            },
             templates: {
                 group: 'info',
                 options: {
-                    moreAt: true,
+                    moreAt: true
                 }
-            },
+            }
         });
     };
 }(this));
