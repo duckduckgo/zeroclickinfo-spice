e759882419dc503f1c7966c0a799944eeade64a3
schluchc <christian.schluchter@gmail.com>
2014-12-08 23:31:26 +0800

    color codes added

--- share/spice/aqi/aqi.js
+++ share/spice/aqi/aqi.js
@@ -17,17 +17,16 @@
                 sourceUrl: 'http://www.airnow.gov/?action=airnow.local_city&zipcode=' + zip
             },
             templates: {
-                group: 'text',
-                detail: false,
-                item_detail: false
-            },
-            normalize : function(item){
-                return{
-                    title: item.ParameterName + ": " + item.AQI + " (" + item.Category.Name + ")",
-                    subtitle: item.ReportingArea + ", " + item.StateCode,
-                    description: item.DateObserved + ", " + item.HourObserved + ":00 " + item.LocalTimeZone
+                group: 'base',
+                options:{
+                    content: Spice.aqi.content,
+                    moreAt: true
                 }
             }
         })
     }
+
+    Spice.registerHelper("AQI_ifCond", function(string1, string2, options) {
+        return ((string1 === string2) ? options.fn(this) : options.inverse(this));
+    });
 }(this));
