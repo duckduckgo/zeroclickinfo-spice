50c65b085d56df2b7c517eca47eeac5645980fed
puskin94 <puskin94@users.noreply.github.com>
2014-12-08 17:41:11 +0100

    Changed triggers

--- lib/DDG/Spice/Launchbug.pm
+++ lib/DDG/Spice/Launchbug.pm
@@ -7,21 +7,20 @@ spice is_cached => 1;
 
 name "Launchbug";
 description "Returns infos about a given bug-id on Launchpad.net";
-primary_example_queries "launchbug 7", "launchbug 2983";
+primary_example_queries "LP: 7", "LP: #2983", "(LP: #1234)";
 category "programming";
 topics "programming";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Launchbug.pm";
 attribution github => ["https://github.com/puskin94/", "puskin"];
 
-
-triggers startend => "launchbug";
+triggers query_raw => qr/^\(?LP\: \#?(\d+)\)?/;
 spice to => 'https://api.launchpad.net/devel/bugs/$1?ws.accept=application%2Fjson';
 spice wrap_jsonp_callback => 1;
 
 
-handle remainder => sub {
+handle query_raw => sub {
 
-    my $num = shift;
+    my $num = $1;
     return unless $num && $num =~ qr/^\d+$/;
     return $num;
 
