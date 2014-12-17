41ec986d87887196283582774cbae34146bd13a8
puskin94 <puskin94@users.noreply.github.com>
2014-12-17 19:14:45 +0100

    Added a trigger for brackets

--- lib/DDG/Spice/Launchbug.pm
+++ lib/DDG/Spice/Launchbug.pm
@@ -13,14 +13,14 @@ topics "programming";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Launchbug.pm";
 attribution github => ["https://github.com/puskin94/", "puskin"];
 
-triggers any => "launchbug", "lp", "bugid";
+triggers any => "launchbug", "lp", "bugid", "(lp";
 spice to => 'https://api.launchpad.net/devel/bugs/$1?ws.accept=application%2Fjson';
 spice wrap_jsonp_callback => 1;
 
 
 handle remainder => sub {
 
-    return unless $_ && $_ =~ qr/^\#?(\d+)$/;
+    return unless $_ && $_ =~ qr/^\#?(\d+)\)?$/;
     return $1;
 
 };
