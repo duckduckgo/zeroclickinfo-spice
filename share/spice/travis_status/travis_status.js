8b7789673b548529cc2caf780d3de79cfe57a54a
Joe Wegner <joe@bellycard.com>
2014-12-17 09:20:17 -0600

    show incident descriptions

--- share/spice/travis_status/travis_status.js
+++ share/spice/travis_status/travis_status.js
@@ -5,10 +5,26 @@
             return Spice.failed('travis_status');
         }
 
+        var relevantDetails = {
+          status: api_result.status.indicator,
+          description: api_result.status.description,
+          activeIncidents: []
+        };
+
+        for(var i=0,max=api_result.incidents.length; i<max; i++) {
+          var incident = api_result.incidents[i];
+
+          if(incident.status === 'monitoring' || incident.status === 'investigating' || incident.status === 'identified') {
+            relevantDetails.activeIncidents.push(incident); 
+          }
+        }
+
+        console.log(relevantDetails);
+
         Spice.add({
             id: "travis_status",
             name: "Status",
-            data: api_result,
+            data: relevantDetails,
             meta: {
                 sourceName: api_result.page.name,
                 sourceUrl: api_result.page.url
@@ -29,5 +45,13 @@
 
     Spice.registerHelper("TravisStatus_ifNotEmpty", function(string, options) {
         return ((typeof(string) !== "undefined" && string !== '') ? options.fn(this) : options.inverse(this));
+    });
+
+    Spice.registerHelper("TravisStatus_iterateOver", function(list, block) {
+      var result = '';
+      for(var i=0,max=list.length; i<max; i++) {
+        result += block.fn(list[i]);
+      }
+      return result;
     });
 }(this));
