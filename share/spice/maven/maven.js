4db4542de20884699f0e05b4bed72bd2c230c38c
Chris Wilson <chris@chrisjwilson.com>
2014-12-20 11:30:03 +0800

    Maven: Rewrite to comply with new Spice. Again.

--- share/spice/maven/maven.js
+++ share/spice/maven/maven.js
@@ -1,27 +1,29 @@
 function ddg_spice_maven(api_result) {
     "use strict";
 
-    if (api_result.responseHeader.status
-        || api_result.response.numFound == 0) {
+    if (!api_result || api_result.responseHeader.status || api_result.response.numFound == 0) {
         return Spice.failed('maven');
     }
-    
-    Spice.add({
-        data             : api_result,
-        header1          : api_result.responseHeader.params.q
-                            + " (Maven Central Repository)",
-        sourceUrl       : 'http://search.maven.org/#search%7Cga%7C1%7C'
-                            + encodeURIComponent(api_result.responseHeader.params.q),
-        sourceName      : 'Maven Central Repository',
-        templates: {
-            item: Spice.maven.maven,
-            detail: Spice.maven.maven
-        },
-        
-        
-    });
 
-    $("#zero_click_abstract").css({
-        "margin": "4px 12px 0px 2px !important",
+    Spice.add({
+        id: "maven",
+        name: "Software",
+        data: api_result,
+        meta: {
+            sourceName: "maven.org",
+            sourceUrl: 'http://search.maven.org/#search%7Cga%7C1%7C' + encodeURIComponent(api_result.responseHeader.params.q),
+        },
+        normalize: function(item) {
+            return {
+                title: api_result.responseHeader.params.q + " (Maven Central Repository)"
+            }  
+        },
+        templates: {
+            group: 'text',
+            options: {
+                content: Spice.maven.content,
+                moreAt: true
+            }
+        }
     });
 }
