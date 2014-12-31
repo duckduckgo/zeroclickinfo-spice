d77af6ac0e2c6fea1c76daee47e27b7b961c7999
Matthew Ramina <mattramina@gmail.com>
2014-12-31 10:43:54 -0500

    Ducksay: safe take 2

--- lib/DDG/Spice/DuckSay.pm
+++ lib/DDG/Spice/DuckSay.pm
@@ -1,7 +1,7 @@
 package DDG::Spice::DuckSay;
-
+ 
 use DDG::Spice;
-
+ 
 name 'DuckSay';
 description 'Like cowsay, but with ducks';
 primary_example_queries 'ducksay', 'daxsays';
@@ -11,20 +11,17 @@ category 'entertainment';
 topics 'geek', 'words_and_games', 'entertainment';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/DuckSay.pm';
 attribution github => ['https://github.com/csytan', 'Chris Tan'];
-
+ 
 triggers start => 'ducksay', 'daxsays';
-
+ 
 spice is_unsafe => 1;
 spice to => 'http://www.reddit.com/r/Jokes/search.json?q=selftext%3Aduck&restrict_sr=on&sort=hot&t=all&jsonp={{callback}}';
-
+ 
 handle remainder => sub {
-    spice call_type => 'include';
     if ($_ ne '') {
-        # Skip calling reddit's API if text is given
-        spice call_type => 'self';
-        return call, {is_unsafe => 0};
+        return {call => '/js/spice/duck_say/', call_type => 'self', is_unsafe => 0};
     }
     return $_;
 };
-
+ 
 1;
