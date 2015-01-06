e4fcb3c0e116415a7f784b38c71f790b492f954d
jdorweiler <jddorweiler@gmail.com>
2015-01-06 13:27:03 -0500

    fix metadata

--- lib/DDG/Spice/Amazon.pm
+++ lib/DDG/Spice/Amazon.pm
@@ -2,6 +2,10 @@ package DDG::Spice::Amazon;
 
 use DDG::Spice;
 
+primary_example_queries "shoes";
+description "Shows Amazon products";
+name "Products";
+
 spice to => 'https://duckduckgo.com/m.js?q=$1&cb=ddg_spice_amazon';
 
 # trigger on source name
