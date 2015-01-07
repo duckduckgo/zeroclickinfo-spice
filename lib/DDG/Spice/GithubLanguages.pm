199304b5f1ef1d2b373017f036813887e9822c41
joerussbowman <bowman.joseph@gmail.com>
2015-01-07 02:19:09 +0000

    removed unecessary comments. cleaned up variable declaration of . Added quotes around language call, currently that's working for 'trigger query' searches but not 'query trigger'. Also have found that quotes in the search string results in an error display which needs to be resolved before I can update the pull request. Will need to add tests for all of this as well.

--- lib/DDG/Spice/GithubLanguages.pm
+++ lib/DDG/Spice/GithubLanguages.pm
@@ -1,27 +1,21 @@
 package DDG::Spice::GithubLanguages;
-# ABSTRACT: Write an abstract here
-# Start at https://duck.co/duckduckhack/spice_overview if you are new
-# to instant answer development
+# ABSTRACT: Search Github for respositories by language
 
 use DDG::Spice;
 
 spice is_cached => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "GithubLanguages";
 source "GitHub";
 icon_url "https://github.com/favicon.ico";
 description "Github search by language";
 primary_example_queries "javascript redis", "mongodb javascript";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
 category "programming";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
 topics "programming";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/GithubLanguages.pm";
 attribution github => ["https://github.com/joerussbowman", "Joe Bowman"],
             twitter => "joerussbowman";
 
-# Triggers
 my @triggers = share("triggers.txt")->slurp;
 triggers startend => @triggers;
 
@@ -30,18 +24,17 @@ my $langs = join("|", @triggers);
 
 spice to => 'https://api.github.com/search/repositories?q=$1&sort=stars&callback={{callback}}';
 
-# Handle statement
 handle query_lc => sub {
 
     my $query = $_;
-    my $l = ""; 
+    my $l; 
     if ($query =~ /^($langs)\b/ || $query =~ /\b($langs)$/) {
         $l = $1;
     } 
     
-    $query =~ s/^($langs)\b|(\b$langs)$//;
+    $query =~ s/^($langs)\b|\b($langs)$//;
 
-    return "$query language:$l";
+    return "$query language:\"$l\"";
 };
 
 1;
