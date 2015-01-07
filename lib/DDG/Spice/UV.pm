a212445bb622fa0e10d3844d30f7e87d04f10de2
Bjoern <codio@blinker.net>
2015-01-07 14:41:01 +0000

    basic perl backend

--- lib/DDG/Spice/UV.pm
+++ lib/DDG/Spice/UV.pm
@@ -1,5 +1,6 @@
 package DDG::Spice::UV;
-# ABSTRACT: Write an abstract here
+# Use the web service provided by EPA to display UV Index for locations in the US.
+# The web service is described here: http://www.epa.gov/enviro/facts/services.html#uvindex
 # Start at https://duck.co/duckduckhack/spice_overview if you are new
 # to instant answer development
 
@@ -14,26 +15,28 @@ icon_url "";
 description "Display the UV Index for a given location in the US";
 primary_example_queries "uv", "uv index";
 secondary_example_queries "optional -- demonstrate any additional triggers";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
+# see https://duck.co/duckduckhack/metadata#category
 category "location_aware";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
+# see https://duck.co/duckduckhack/metadata#topics
 topics "everyday", "travel", "geography";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/UV.pm";
 attribution github => ["https://github.com/Bjoern", "Bjoern Guenzel"],
             twitter => "https://twitter.com/fractality", "Bjoern Guenzel";
 
+spice from => '([^/]*)/?([^/]*)';
+spice to => 'http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/CITY/$1/STATE/$2/JSON'
+
 # Triggers
-triggers any => "triggerWord", "trigger phrase";
+triggers any => "uv";
 
 # Handle statement
 handle remainder => sub {
+    #atm only works with automatically detected location of user
+    return unless $loc
+    #and only if location is in the US
+    return unless $loc->country_code eq 'US'
 
-    # optional - regex guard
-    # return unless qr/^\w+/;
-
-    return unless $_;    # Guard against "no answer"
-
-    return $_;
+    return $loc->city, $loc->region;
 };
 
 1;
