d2c085dd3253d27f88f04b4e40d308408c2e64f3
MariagraziaAlastra <alastra.mariagrazia@gmail.com>
2015-01-14 13:15:04 +0000

    SeatGeek Sports - make use of DDG.getOrdinal()

--- share/spice/seat_geek/sports/seat_geek_sports.js
+++ share/spice/seat_geek/sports/seat_geek_sports.js
@@ -58,14 +58,7 @@
                 function getDay(date) {
                     if(date) {
                         var day = date.getDate();
-                        var suffix = "";
-                        switch (day) {
-                            case 1: suffix = "st"; break;
-                            case 2: suffix = "nd"; break;
-                            case 3: suffix = "rd"; break;
-                            default: suffix = "th"; break;
-                        }
-                        return day + suffix;
+                        return DDG.getOrdinal(day);
                     }
 
                     return;
