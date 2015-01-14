f535c077b583ae0346b3bd8792f4b4cde03049d4
Bjoern <codio@blinker.net>
2015-01-14 21:57:12 +0000

    generated skeleton for Medicine instant answer

---
+++ lib/DDG/Spice/Medicine.pm
@@ -0,0 +1,39 @@
+package DDG::Spice::Medicine;
+# ABSTRACT: Write an abstract here
+# Start at https://duck.co/duckduckhack/spice_overview if you are new
+# to instant answer development
+
+use DDG::Spice;
+
+spice is_cached => 1;
+
+# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
+name "Medicine";
+source "";
+icon_url "";
+description "Succinct explanation of what this instant answer does";
+primary_example_queries "first example query", "second example query";
+secondary_example_queries "optional -- demonstrate any additional triggers";
+# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
+# category "";
+# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
+# topics "";
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Medicine.pm";
+attribution github => ["GitHubAccount", "Friendly Name"],
+            twitter => "twitterhandle";
+
+# Triggers
+triggers any => "triggerWord", "trigger phrase";
+
+# Handle statement
+handle remainder => sub {
+
+    # optional - regex guard
+    # return unless qr/^\w+/;
+
+    return unless $_;    # Guard against "no answer"
+
+    return $_;
+};
+
+1;
