7b7cd90f13054d34c125d06ae52e35b9ca4c7581
puskin94 <puskin94@users.noreply.github.com>
2015-01-16 18:44:44 +0100

    changed .com with .net

--- share/spice/launchbug/launchbug.js
+++ share/spice/launchbug/launchbug.js
@@ -39,7 +39,7 @@
             name: "Launchpad",
             data: api_result,
             meta: {
-                sourceName: "Launchpad.com",
+                sourceName: "Launchpad.net",
                 sourceUrl: api_result.web_link
             },
             normalize: function(item) {
