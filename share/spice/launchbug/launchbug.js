4a96d7533ce31ffdb26924d90039137aa2850c25
puskin94 <puskin94@users.noreply.github.com>
2015-01-19 09:14:05 +0100

    removed dot

--- share/spice/launchbug/launchbug.js
+++ share/spice/launchbug/launchbug.js
@@ -39,7 +39,7 @@
             name: "Launchpad",
             data: api_result,
             meta: {
-                sourceName: "Launchpad.net",
+                sourceName: "Launchpad",
                 sourceUrl: api_result.web_link
             },
             normalize: function(item) {
