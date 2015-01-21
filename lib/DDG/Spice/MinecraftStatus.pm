20669d5eb209d5225748f888026584c30eae7b52
destruc7i0n <dscdsouza@outlook.com>
2015-01-21 13:42:19 -0500

    Added working (so far!) basis of this project
    3 new files

--- lib/DDG/Spice/MinecraftStatus.pm
+++ lib/DDG/Spice/MinecraftStatus.pm
@@ -9,11 +9,10 @@ description "Gives the current status of Minecraft";
 primary_example_queries "is minecraft down", "why is minecraft not working";
 category 'special';
 topics 'words_and_games';
-# code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/MinecraftStatus.pm";
-attribution
-    web => ['https://thedestruc7i0n.ca', "Colin D'Souza"],
-    twitter => ['https://www.twitter.com/TheDestruc7i0n',"Colin D'Souza"];
-    github => ['destruc7i0n', "Colin D'Souza"];
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/MinecraftStatus.pm";
+attribution web     => ['https://thedestruc7i0n.ca', "Colin D'Souza"],
+			twitter => ['https://www.twitter.com/TheDestruc7i0n',"Colin D'Souza"],
+    		github  => ['destruc7i0n', "Colin D'Souza"];
 
 triggers any => 'why is minecraft down', 'minecraft status', 'mcstatus';
 
