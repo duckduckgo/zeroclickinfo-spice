b3f8ff8fc01ade3d8c5a7a9dab3b5037f21ca78e
destruc7i0n <dscdsouza@outlook.com>
2015-01-21 14:35:18 -0500

    Forgot to add this...

--- share/spice/minecraft_status/minecraft_status.js
+++ share/spice/minecraft_status/minecraft_status.js
@@ -1,1 +1,30 @@
-#
+(function (env) {
+    "use strict";
+    env.ddg_spice_minecraft_status = function(api_result) {
+        
+        if (!api_result) {
+            return Spice.failed('minecraft_status');
+        }
+
+        Spice.add({
+            id: "minecraft_status",
+            name: "Status",
+            data: api_result,
+            meta: {
+                sourceName: "Mojang",
+                sourceUrl: "https://help.mojang.com/",
+            },
+            templates: {
+                group: 'base',
+                options:{
+                    content: Spice.minecraft_status.content,
+                    moreAt: false
+                }
+            }
+        });
+    }
+
+    Spice.registerHelper("MinecraftStatus_ifCond", function(string1, string2, options) {
+        return ((string1 === string2) ? options.fn(this) : options.inverse(this));
+    });
+}(this));
