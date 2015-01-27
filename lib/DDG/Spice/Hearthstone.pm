92f0906fe64d1c46251243767989b57d3ba4c3a5
Akryum <guillaume.b.chau@gmail.com>
2015-01-27 22:50:29 +0000

    Fixed attribution github link in metadata

--- lib/DDG/Spice/Hearthstone.pm
+++ lib/DDG/Spice/Hearthstone.pm
@@ -12,7 +12,7 @@ description "Get a preview of any Hearthstone card.";
 primary_example_queries "hearthstone leeroy jenkins", "hearthstone leeroy";
 category "entertainment";
 topics "gaming";
-code_url "https://github.com/Akryum/zeroclickinfo-spice/lib/DDG/Spice/Hearthstone.pm";
+code_url "https://github.com/Akryum/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Hearthstone.pm";
 attribution github => ["https://github.com/Akryum", "Akryum"],
             twitter => "Akryum";
 
