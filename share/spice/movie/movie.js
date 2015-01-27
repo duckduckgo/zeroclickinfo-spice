6a324cbfdaf9a8e852a79caa140a8913cc25bb4f
Chris Wilson <chris@chrisjwilson.com>
2015-01-27 14:50:50 +0800

    Movies: Improve code readability, indentation'

--- share/spice/movie/movie.js
+++ share/spice/movie/movie.js
@@ -1,36 +1,36 @@
 (function(env) {
     "use strict";
-    
+
     // A change in the Rotten Tomatoes API returns images that end in _tmb.
     // This changes this to _det.
     function toDetail(img) {
         return img.replace(/tmb\.(jpg|png)/, "det.$1");
     }
-    
+
     function get_image(critics_rating) {
-        if(!critics_rating) {
+        if (!critics_rating) {
             return;
         }
-        
+
         // The filename is the same as the critics_rating, but
         // lowercased and with spaces converted to dashes.
         critics_rating = critics_rating.toLowerCase().replace(/ /, '-');
         return DDG.get_asset_path('in_theaters', critics_rating + ((DDG.is3x || DDG.is2x) ? '.retina.png' : '.png'));
     }
-    
+
     env.ddg_spice_movie = function(api_result) {
-        if(!api_result || !api_result.movies || !api_result.movies.length) {
+        if (!api_result || !api_result.movies || !api_result.movies.length) {
             return Spice.failed('movie');
         }
-        
+
         // Get original query.
         var script = $('[src*="/js/spice/movie/"]')[0],
             source = $(script).attr("src"),
             query = decodeURIComponent(source.match(/movie\/([^\/]+)/)[1]);
-        
+
         //Remove parenthesis to match: Movie Name (2014)
-        if (query.match(/\(.+?\)/g)) { 
-            query = query.replace(/\(|\)/g, ''); 
+        if (query.match(/\(.+?\)/g)) {
+            query = query.replace(/\(|\)/g, '');
         }
 
         //Check if the query contains a year
@@ -38,21 +38,23 @@
             singleResult = [];
 
         //Ensure year is vaild between 1900 - current year + 5
-        year = (year >= 1900 && year <= new Date().getFullYear()+5) ? year : null;
+        year = (year >= 1900 && year <= new Date().getFullYear() + 5) ? year : null;
 
 
-        if(year) {
-        ///Check movie year and title against query
-        $.each(api_result.movies, function(key, result) {
-        if(!result.title.match(/\d{4}/)) { // don't run this check if the movie title contains a date
-           if(result.year == year && query.replace(year,'').trim() == result.title.toLowerCase()) {
-                singleResult.push(result);
-           }
+        if (year) {
+            ///Check movie year and title against query
+            $.each(api_result.movies, function(key, result) {
+                if (!result.title.match(/\d{4}/)) { // don't run this check if the movie title contains a date
+                    if (result.year == year && query.replace(year, '').trim() == result.title.toLowerCase()) {
+                        singleResult.push(result);
+                    }
+                }
+            });
+            if (singleResult.length > 0) {
+                api_result.movies = singleResult;
+            } // return a single result if we have an exact match
         }
-        });
-            if(singleResult.length>0) {api_result.movies = singleResult;} // return a single result if we have an exact match
-        }
-        
+
         Spice.add({
             id: 'movie',
             name: 'Movies',
@@ -96,7 +98,7 @@
                 }]
             }
         });
-        
+
         // Make sure we hide the title and ratings.
         // It looks nice to show only the poster of the movie.
         var $dom = Spice.getDOM('movie')
@@ -104,13 +106,13 @@
             $dom.find('.tile__body').hide();
         }
     };
-    
+
     // Convert minutes to hr. min. format.
     // e.g. {{time 90}} will return 1 hr. 30 min.
     Handlebars.registerHelper("time", function(runtime) {
         var hours = '',
             minutes = runtime;
-        if(runtime >= 60) {
+        if (runtime >= 60) {
             hours = Math.floor(runtime / 60) + ' hr. ';
             minutes = (runtime % 60);
         }
