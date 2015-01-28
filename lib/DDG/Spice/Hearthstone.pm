0fc5bb38154feeeb4c3415c271de2d79b046d7f0
Akryum <guillaume.b.chau@gmail.com>
2015-01-28 18:47:04 +0000

    Now using info template, added trigger keyword blacklist

--- lib/DDG/Spice/Hearthstone.pm
+++ lib/DDG/Spice/Hearthstone.pm
@@ -9,7 +9,7 @@ name "Hearthstone Card Search";
 source "Bytevortex (Gamepedia Proxy)";
 icon_url "http://eu.battle.net/hearthstone/static/images/icons/favicon.ico";
 description "Get a preview of any Hearthstone card.";
-primary_example_queries "hearthstone leeroy jenkins", "hearthstone leeroy";
+primary_example_queries "hearthstone leeroy jenkins", "hearthstone leeroy", "leeroy hearthstone", "hearthstone mirror";
 category "entertainment";
 topics "gaming";
 code_url "https://github.com/Akryum/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Hearthstone.pm";
@@ -17,7 +17,7 @@ attribution github => ["https://github.com/Akryum", "Akryum"],
             twitter => "Akryum";
 
 # Triggers
-triggers any => "hearthstone";
+triggers startend => "hearthstone";
 
 spice to => 'http://bytevortex.net/hearthstone.php?search=$1';
 
@@ -27,6 +27,11 @@ spice wrap_jsonp_callback => 1;
 handle remainder => sub {
 
     return unless $_;    # Guard against "no answer"
+    
+    # Keyword Blacklist
+    if(/(game|instruction|stoves|warcraft|deck|forum|wiki)/) {
+        return;
+    }
     
     return $_;
 };
