d1f1b2db33a8645f9a7598a5e1f5adeb2553bb34
Chris Wilson <chris@chrisjwilson.com>
2015-01-29 21:18:09 +0800

    Twitter: Add skip words to slurp file.

--- lib/DDG/Spice/Twitter.pm
+++ lib/DDG/Spice/Twitter.pm
@@ -19,17 +19,15 @@ attribution github => ["https://github.com/duckduckgo/", "DuckDuckGo"],
 spice to => 'https://duckduckgo.com/tw.js?user=$1&callback={{callback}}&current=1';
 triggers query => qr/^(?:twitter\s)?@([a-z0-9_]+)$|^twitter\s([a-z0-9_]+)$/i;
 
-# Possibly add skip words to a file for slurping?
-my $skip = join "|", ("apis?",
-		      "developers?",
-		      "users?",
-		      "search(es)?");
+
+# skip words from file
+my $skip = join "|", share('skipwords.txt')->slurp(chomp => 1);
 
 handle matches => sub { 
     if ($1) {
-	return $1;
+	   return $1;
     } elsif ($2) {
-	return $2 unless ($2 =~ m/^($skip)$/i)
+	   return $2 unless ($2 =~ m/^($skip)$/i)
     }
     return;
 }; 
