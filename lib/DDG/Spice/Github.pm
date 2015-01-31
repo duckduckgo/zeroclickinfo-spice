cc1c93091edfb87889275b43224d48d65df6d9c9
puskin94 <giovanni.puskin@gmail.com>
2015-01-31 15:24:38 +0100

    identation pt. 2

--- lib/DDG/Spice/Github.pm
+++ lib/DDG/Spice/Github.pm
@@ -17,9 +17,9 @@ spice to => 'https://api.github.com/legacy/repos/search/$1?callback={{callback}}
 spice proxy_cache_valid => '200 30d';
 
 handle remainder_lc => sub {
-	return unless $_;
-	return $_ unless /^jobs\b|\bjobs$|^status\b|\bstatus$/;
-	return;
+    return unless $_;
+    return $_ unless /^jobs\b|\bjobs$|^status\b|\bstatus$/;
+    return;
 };
 
 1;
