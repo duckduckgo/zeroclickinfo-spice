b7d965ef17148037440225939598d6df8f9c59b1
Mak Kolybabi <mak@kolybabi.com>
2015-02-01 19:08:37 +0000

    Added suggestions to make tiles look like most recent demo, and revert to custom template for single-result.

--- share/spice/tor_node/tor_node.js
+++ share/spice/tor_node/tor_node.js
@@ -26,49 +26,55 @@
             return Spice.failed("tor_node");
         }
 
+        // Format the node's properties to be more friendly.
+        for (i = 0; i < nodes.length; i++) {
+            node = nodes[i];
+
+            node.url = "https://atlas.torproject.org/#details/" + node.ref;
+
+            // Ensure the node has a title.
+            if (node.nickname && node.nickname !== "Unnamed") {
+                node.title = node.nickname;
+            }
+            else {
+                node.title = "(Unnamed)";
+            }
+        }
+ 
         // Filter out the trigger from the query.
         var words = DDG.get_query().split(" ");
         var query = (words[0] =~ /tor|onion/i) ? words.pop() : words.shift();
 
-        // Give a name to the result type.
-        var type = (nodes.length === 1) ? "Tor Node" : "Tor Nodes";
+        // We use different templates depending on the number of nodes.
+        if (nodes.length === 1) {
+            return singleNode(query, nodes[0]);
+        }
+        return multipleNodes(query, nodes);
+    };
 
+    function singleNode (query, node) {
         Spice.add({
             id: "tor_node",
-            name: type,
-            data: nodes,
+            name: "Answer",
+            data: node,
             meta: {
-                itemType: type.toLowerCase(),
+                itemType: "Tor " + node.type,
                 sourceName: "Tor Atlas",
-                sourceUrl: "https://atlas.torproject.org/#search/" + query
+                sourceUrl: "https://atlas.torproject.org/#details/" + node.ref
             },
             templates: {
                 group: "text",
+                detail: Spice.tor_node.detail,
                 options: {
-                    content: "record",
                     footer: Spice.tor_node.footer,
                     rowHighlight: true,
                     moreAt: true
                 }
             },
             normalize: function(node) {
-                var subtitle = node.nickname;
-                if (!subtitle || subtitle === "Unnamed") {
-                    subtitle = "Unnamed " + node.type;
-                }
-
-                var data = {};
-
-                data["Type"] = node.type;
-                data[node.ref_name] = node.ref;
-
-                if (node.nickname && node.nickname !== "Unnamed") {
-                    data["Nickname"] = node.nickname;
-                }
-
+                // Format contact info.
                 if (node.contact) {
                     node.contact = node.contact.replace(/mailto:/g, "");
-                    data["Contact"] = node.contact;
                 }
 
                 // Bundle together all IPs used by a node.
@@ -79,44 +85,76 @@
                 if (node.dir_address) {
                     node.addrs.push(node.dir_address);
                 }
-                node.addrs = $.unique($.map(node.addrs, function(addr) {
-                    return addr.replace(/:[0-9]+$/, "");
-                }));
-                if (node.addrs.length === 1) {
-                    data["IP"] = node.addrs[0];
-                }
-                else {
-                    data["IPs"] = node.addrs.join(", ");
-                }
+                node.addrs = $.unique($.map(node.addrs, stripPort));
 
                 // Handle the node's locale.
-                node.country_icon = DDG.settings.region.getXSmallIconURL(getCountryCode(node.country));
-                if (node.country_name) {
-                    data["Country"] = node.country_name;
-                }
+                node.icon = DDG.settings.region.getXSmallIconURL(getCountryCode(node.country));
 
                 // Handle the node's uptime history.
-                node.first_seen_relative = timeFromNow(node.first_seen);
-                node.last_seen_relative = timeFromNow(node.last_seen);
-                data["First Seen"] = prettifyTimestamp(node.first_seen);
-                data["Last Seen"] = prettifyTimestamp(node.last_seen);
+                node.first_seen = prettifyTimestamp(node.first_seen);
+                node.last_seen = prettifyTimestamp(node.last_seen);
 
-                // Handle the node's current state.
-                node.status = node.running ? "Online" : "Offline";
-                data["Status"] = node.status;
-
-                data["Flags"] = node.flags.join(", ");
-
-                return {
-                    record_data: data,
-                    title: node.ref,
-                    subtitle: subtitle,
-                    icon: node.country_icon,
-                    url: "https://atlas.torproject.org/#details/" + node.ref
-                };
+                return node;
             }
         });
-    };
+    }
+
+    function multipleNodes (query, nodes) {
+        Spice.add({
+            id: "tor_node",
+            name: "Answer",
+            data: nodes,
+            meta: {
+                itemType: "Tor Nodes",
+                sourceName: "Tor Atlas",
+                sourceUrl: "https://atlas.torproject.org/#search/" + query
+            },
+            templates: {
+                group: "icon",
+                detail: false,
+                item_detail: false,
+                options: {
+                    footer: Spice.tor_node.footer,
+                    moreAt: true
+                }
+            },
+            normalize: function(node) {
+                var data = {};
+
+                data.title = node.title;
+                data.url = node.url;
+
+                data.description = "";
+                if (node.or_addresses !== 1) {
+                    data.description = stripPort(node.or_addresses[0]) + "\n";
+                }
+                data.description += node.ref;
+
+                // Handle the node's locale.
+                data.icon = DDG.settings.region.getSmallIconURL(getCountryCode(node.country));
+
+                // Handle the node's uptime history.
+                data.online = node.running;
+                if (data.online) {
+                    data.uptime = timeFromNow(node.last_restarted);
+                }
+                else {
+                    data.downtime = timeFromNow(node.last_seen);
+                }
+
+                // Make the bandwidth friendly.
+                if (node.advertised_bandwidth) {
+                    data.bandwidth = prettifyBitrate(node.advertised_bandwidth);
+                }
+
+                return data;
+            }
+        });
+    }
+
+    function stripPort(addr) {
+        return addr.replace(/:[0-9]+$/, "");
+    }
 
     function getCountryCode(cc) {
         // I've seen at least one flag from Onionoo not be found due to a
@@ -126,6 +164,37 @@
         };
 
         return mapping[cc] ? mapping[cc] : cc;
+    }
+
+    function prettifyBitrate(bitrate) {
+        var rateTiBps = 1099511627776;
+        var rateGiBps = 1073741824;
+        var rateMiBps = 1048576;
+        var rateKiBps = 1020;
+
+        var magnitude, letter;
+        if (bitrate >= rateTiBps) {
+            magnitude = rateTiBps;
+            letter = "T";
+        }
+        else if (bitrate >= rateGiBps) {
+            magnitude = rateGiBps;
+            letter = "G";
+        }
+        else if (bitrate >= rateMiBps) {
+            magnitude = rateMiBps;
+            letter = "M";
+        }
+        else if (bitrate >= rateKiBps) {
+            magnitude = rateKiBps;
+            letter = "K";
+        }
+        else {
+            magnitude = 1;
+            letter = "";
+        }
+
+        return (bitrate / magnitude).toFixed(2) + " " + letter + "iB/s";
     }
 
     // Based on function in whois spice.
@@ -150,12 +219,13 @@
     // Based on function in reddit_search spice.
     function timeFromNow(timestamp) {
        // Scaling factors for various time periods.
-       var yearMillisec   = 31540000000;
-       var monthMillisec  = 2628000000;
-       var dayMillisec    = 86400000;
-       var hourMillisec   = 3600000;
-       var minuteMillisec = 60000;
-       var secondMillisec = 1000;
+       var scales = [
+           ["y", 31540000000],
+           ["w", 604800000],
+           ["d", 86400000],
+           ["h", 3600000],
+           ["m", 60000]
+       ];
 
        // Convert time node was seen to milliseconds.
        var fields = timestamp.split(" ");
@@ -163,59 +233,31 @@
        fields = fields[1].split(":");
        var dateSeen = new Date(
            dateDay.getTime() +
-           fields[0] * hourMillisec +
-           fields[1] * minuteMillisec +
-           fields[2] * secondMillisec
+           fields[0] * scales[3][1] +
+           fields[1] * scales[4][1] +
+           fields[2] * 1000
        );
        var dateNow = new Date();
+       var durationMS = dateNow.getTime() - dateSeen.getTime();
+       var duration = new Array();
 
-       // Get how many milliseconds from now the node was seen.
-       var dateSeenMillisec = dateNow.getTime() - dateSeen.getTime();
+       for (var i = 0; i < scales.length; i++) {
+           var suffix = scales[i][0];
+           var millis = scales[i][1];
 
-       // Get how many years, months and days from now the node was seen.
-       var yearsFromNow = dateSeenMillisec / yearMillisec;
-       var monthsFromNow = dateSeenMillisec / monthMillisec;
-       var daysFromNow = dateSeenMillisec / dayMillisec;
-       var stringDate = "";
-
-       // If the node was seen more than one year ago, set the returned value to
-       // the number of years in between.
-       if (yearsFromNow > 1) {
-           var years = Math.floor(yearsFromNow);
-           stringDate = years + " year";
-           if (years > 1) {
-               stringDate += "s";
+           if (durationMS < millis) {
+               continue;
            }
-           stringDate += " ago";
+
+           var used = Math.floor(durationMS / millis);
+           duration.push(used + suffix);
+           durationMS -= used * millis;
        }
 
-       // Otherwise, if the node was seen more than one month ago, set the
-       // returned value to the number of months in between
-       else if (monthsFromNow > 1) {
-           var months = Math.floor(monthsFromNow);
-           stringDate = months + " month";
-           if (months > 1) {
-             stringDate += "s";
-           }
-           stringDate += " ago";
+       if (duration.length === 0) {
+           return "now";
        }
 
-       // Otherwise, if the node was seen more than one day ago, set the
-       // returned value to the number of days in between
-       else if (daysFromNow > 1) {
-           var days = Math.floor(daysFromNow);
-           stringDate = days + " day";
-           if (days > 1) {
-               stringDate += "s";
-           }
-           stringDate += " ago";
-       }
-
-       // Finally, the node was seen today.
-       else {
-           stringDate = "today";
-       }
-
-       return stringDate;
+       return duration.join(" ");
     }
 }(this));
