97a2b337aadf8a4fdc0cd736c9c4d08dda55abad
Chris Wilson <chris@chrisjwilson.com>
2015-02-01 15:42:58 +0800

    TravisStatus: Update tests, include comments

--- lib/DDG/Spice/TravisStatus.pm
+++ lib/DDG/Spice/TravisStatus.pm
@@ -21,10 +21,10 @@ spice to => 'http://www.traviscistatus.com/index.json';
 spice wrap_jsonp_callback => 1;
 
 handle query_lc => sub {
-	my $query = trim($_);
-	return $query if $query =~ m/^travisci$/;
-	$query =~ s/$triggers\s+//g;
-    return trim($query) if $query =~ /^(is\s*)?(system)?\s*(status|up|down)\??$/i;
+	my $query = trim($_); # trim
+	return $query if $query =~ m/^travis(\s)?ci$/; # return if travisci, travis ci
+	$query =~ s/$triggers\s+//g; # remove triggers
+    return trim($query) if $query =~ /^(is\s*)?(system)?\s*(status|up|down)\??$/i; # return if query contains status etc
     return;
 };
 
