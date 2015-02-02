bf8e888a2fa7d1a134ab1627906944bdd69d807b
Adalbert Michelic <am@timeanddate.com>
2015-02-02 15:41:07 +0100

    [TI-70] Update to reflect new templates

--- share/spice/time_and_date_holiday/time_and_date_holiday.js
+++ share/spice/time_and_date_holiday/time_and_date_holiday.js
@@ -1,25 +1,36 @@
-function ddg_spice_time_and_date_holiday(api_result) {
-    if(!api_result || !api_result.h || api_result.h.length === 0) {
-        return;
-    }
-    var query = DDG.get_query(),
-        header = 'When is '+api_result.q+' in '+api_result.c,
-        url = 'http://www.timeanddate.com/',
-        source = url + 'search/results.html?query=' + encodeURIComponent(query);
+(function (env) {
+	"use strict";
+	env.ddg_spice_time_and_date_holiday = function(api_result) {
+	    if(!api_result || !api_result.h || api_result.h.length === 0) {
+			return Spice.failed('time_and_date_holiday');
+	    }
+	    var query = DDG.get_query(),
+	        url = 'http://www.timeanddate.com',
+			source = url + '/search/results.html?query=' + encodeURIComponent(query);
 
-    Spice.render({
-        data: api_result.h,
-        header1: header,
-        source_name: 'timeanddate.com',
-        source_url: source,
-        spice_name: 'time_and_date_holidays',
-        template_frame: 'list',
-        template_options: {
-            items: api_result.h,
-            show: 2,
-            max: 10,
-            template_item: 'time_and_date_holiday'
-        },
-        force_big_header: true
-    });
-};
+		Spice.add({
+	        id: 'time_and_date_holiday',
+	        name: 'Holidays',
+	        data: api_result.h,
+			normalize: function(item) {
+				return {
+					title: item.n,
+					description: item.a,
+					image: item.p,
+					url: url + item.u
+				};
+			},
+			meta: {
+				sourceName: 'timeanddate.com',
+				sourceUrl: source
+			},
+			templates: {
+				group: 'info',
+				options: {
+					content: Spice.time_and_date_holiday.info,
+					moreAt: true
+				}
+			}
+		});
+	};
+}(this));
