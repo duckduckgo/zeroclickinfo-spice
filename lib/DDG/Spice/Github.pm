4a5b19796361daf678aa4736b21ceb07d6ac6789
joerussbowman <bowman.joseph@gmail.com>
2015-02-07 15:07:10 +0000

    got a good start, still a few things to work out. game maker language seems to be one that is refusing to work right. Also the javascript needs updating for parsing the date from the newer api

--- lib/DDG/Spice/Github.pm
+++ lib/DDG/Spice/Github.pm
@@ -13,13 +13,59 @@ attribution web => ['http://dylansserver.com','Dylan Lloyd'],
             email => ['dylan@dylansserver.com','Dylan Lloyd'];
 
 triggers startend => "github";
-spice to => 'https://api.github.com/legacy/repos/search/$1?callback={{callback}}';
+#spice to => 'https://api.github.com/legacy/repos/search/$1?callback={{callback}}';
+
+my @triggers = share("triggers.txt")->slurp;
+triggers startend => @triggers;
+
+chomp(@triggers);
+my $langs = join("|", map(quotemeta, @triggers));
+
+spice to => 'https://api.github.com/search/repositories?q=$1&sort=stars&callback={{callback}}';
+
 spice proxy_cache_valid => '200 30d';
 
-handle remainder_lc => sub {
-    return unless $_;
+#handle remainder_lc => sub {
+#    return unless $_;
+#    return $_ unless /^jobs\b|\bjobs$|^status\b|\bstatus$/;
+#    return;
+#};
+
+handle query_lc => sub {
+    s/^github\s+|\s+github$//;
+    
+    my $query = $_;
+    my $l; 
+    # structuring the search this way gives the first langauge specified
+    # precedence. So have 'python javascript' passes the query
+    # 'javascript language:python' to github.
+    if (/($langs) /||/ ($langs)/ || /^($langs)/ || /($langs)$/) {
+        $l = $1;
+        $query =~ s/($langs) | ($langs)|^($langs)|($langs)$//;
+
+        # make sure there is an actual query, and not just a language term search
+        #for ($query) {
+        #    $query =~ s/^\s*//; 
+        #    $query =~ s/\s*$//;
+        #    if ($query eq "") {
+        #        # playing around with the main site, the live version does load
+        #        # something for just 'github bash' so go ahead and pass the query
+        #        return $_;
+        #    }
+        #}
+
+        # These is no separate language parameter for the query to 
+        # Github. You specify language as a part of the raw query string
+        # passed to the api like on the web form interface. 
+        return "${query} language:\"${l}\"";
+    } 
+    
+    
+    
+    
     return $_ unless /^jobs\b|\bjobs$|^status\b|\bstatus$/;
     return;
 };
+
 
 1;
