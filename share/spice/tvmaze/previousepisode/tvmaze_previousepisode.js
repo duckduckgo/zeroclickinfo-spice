56b6b8f4d98afc1fe824041a8a909c1bb88863e8
Zaahir Moolla <moollaza@duckduckgo.com>
2015-02-07 16:44:54 +0000

    new layout for prev/next episode, added relevancy checking

--- share/spice/tvmaze/previousepisode/tvmaze_previousepisode.js
+++ share/spice/tvmaze/previousepisode/tvmaze_previousepisode.js
@@ -14,26 +14,106 @@
                 sourceName: "TVmaze.com",
                 sourceUrl: api_result._embedded.previousepisode.url
             },
+            relevancy: {
+                primary: [
+                    { key: 'name' },
+                    { required: '_embedded.previousepisode.name' },
+                    { required: '_embedded.previousepisode.airdate' },
+                ]
+            },
             normalize: function(item){
+                var infoboxData = [
+                    {
+                        label: 'Show',
+                        value: item.name
+                    },
+                    {   label: 'Aired',
+                        value: dateformat(item._embedded.previousepisode.airdate)
+                    },
+                    {
+                        label: 'Episode',
+                        value: item._embedded.previousepisode.number
+                    },
+                    {
+                        label: 'Season',
+                        value: item._embedded.previousepisode.season
+                    },
+                    {
+                        label: 'Show Type',
+                        value: item.type
+                    },
+                    {
+                        label: 'Show Status',
+                        value: item.status
+                    }
+                ];
+
+                if (item.network) {
+                    infoboxData.push(
+                        {
+                            label: 'Network',
+                            value: item.network.name
+                        },
+                        {
+                            label: 'Country of origin',
+                            value: item.network.country.name
+                        }
+                    );
+                } else if (item.webChannel) {
+                    infoboxData.push(
+                        {
+                            label: 'Web Channel',
+                            value: item.webChannel.name
+                        }
+                    );
+
+                    if (item.webChannel.country) {
+                        infoboxData.push(
+                            {
+                                label: 'Country of origin',
+                                value: item.webChannel.country.name
+                            }
+                        );
+                    }
+                }
+
                 return {
                     image: item.image ? item.image.medium : null,
-                    title: item.name
+                    title: item._embedded.previousepisode.name,
+                    description: DDG.strip_html(item._embedded.previousepisode.summary) || "No description available.",
+                    infoboxData: infoboxData
                 };
             },
             templates: {
                 group: 'info',
                 options: {
-                    content: Spice.tvmaze_previousepisode.content,
                     moreAt: true
                 }
             }
         });
     };
 
-    Handlebars.registerHelper("tvmaze_dateformat", function (date) {
-        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        var dateObj = new Date(date);
+    function dateformat (date) {
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+            dateObj = new Date(date),
+            hours = dateObj.getHours(),
+            minutes = dateObj.getMinutes(),
+            date = dateObj.toDateString();
 
-        return months[dateObj.getMonth()] + " " + dateObj.getDate() + ", " + dateObj.getFullYear();
-    });
+        date = date.substring(0, date.lastIndexOf(" "));
+
+        // AM or PM?
+        var suffix = (hours >= 12) ? "PM" : "AM";
+        // Convert to 12-hour time.
+        if(hours > 12) {
+            hours -= 12;
+        } else if(hours === 0) {
+            hours = 12;
+        }
+        // Add leading zeroes.
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+
+        return months[dateObj.getMonth()] + " " + dateObj.getDate() + ", " + dateObj.getFullYear() + " at " + hours + ":" + minutes + " " + suffix;
+    };
+    
 }(this));
