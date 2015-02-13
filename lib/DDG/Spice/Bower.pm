6b1846fc6447084ae5ddfd049cb26cc66008d876
francisbrito <fr.br94@gmail.com>
2015-02-13 08:21:47 +0000

    Add instant answer metadata.

--- lib/DDG/Spice/Bower.pm
+++ lib/DDG/Spice/Bower.pm
@@ -3,6 +3,16 @@ package DDG::Spice::Bower;
 
 use DDG::Spice;
 
+name "Bower";
+category "programming";
+topics "sysadmin", "programming";
+primary_example_queries "bower underscore";
+description "Shows a list of Bower packages.";
+attribution github  => ['https://github.com/francisbrito', 'francisbrito'],
+            twitter => ['https://twitter.com/frxbr', 'frxbr'];
+code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Bower.pm";
+
+
 triggers startend => 'bower';
 
 spice to => 'http://bower.herokuapp.com/packages/search/$1';
