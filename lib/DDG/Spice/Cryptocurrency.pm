139bed2f28c0385144b9f1cbe6c1da81700397cc
claytonspinner <clayton.spinner@gmail.com>
2015-02-14 22:53:05 +0000

    Cryptocurrency Spice: adds most of the remaining functionality

--- lib/DDG/Spice/Cryptocurrency.pm
+++ lib/DDG/Spice/Cryptocurrency.pm
@@ -1,8 +1,9 @@
 package DDG::Spice::Cryptocurrency;
 # ABSTRACT: Cryptocurrency converter and exchange rate lookup from www.cryptonator.com
-# Borrows a lot from the Currency Spice
+# Borrows heavily from the Currency Spice
 
 use DDG::Spice;
+use JSON;
 with 'DDG::SpiceRole::NumberStyler';
 
 # Get all the valid currencies from a text file.
@@ -17,6 +18,23 @@ foreach my $currency (@currencies){
     push(@currTriggers, @currency);
     $currHash{$currency[0]} = \@currency;
 }
+
+# Used when a single currency is given in the query.
+# These currencies should be handled by DDG::Spice::Currency or DDG::Spice::Bitcoin.
+my @excludedCurrencies = (
+    'btc',
+    'aud',
+    'cny',
+    'eur',
+    'gbp',
+    'hkd',
+    'jpy',
+    'nzd',
+    'pln',
+    'rur',
+    'sgd',
+    'usd'
+);
 
 #Define regexes
 # TODO 1. Add 'exchange rate, converted to' as possible match for $into_qr
@@ -33,12 +51,7 @@ my $guard = qr/^$question_prefix($number_re*)\s?($currency_qr)(?:$into_qr|$vs_qr
 
 triggers query_lc => qr/$currency_qr/;
 
-# triggers any => "///***never_trigger***///";
-
-
 spice from => '([^/]+)/([^/]+)/([^/]*)';
-
-# Hack to get around cryptonator.com not accepting URL encoded strings.
 spice to => 'http://www.cryptonator.com/api/$1/$2?primary=$3';
 spice wrap_jsonp_callback => 1;
 
@@ -57,12 +70,67 @@ sub getCode {
     }
 }
 
-# TODO Implenting happy path. Need to handle everything else.
-handle query_lc => sub {
+# This function is responsible for processing the input.
+# - Setting default values.
+# - Checking if the input number is valid.
+sub checkCurrencyCode {
+    my($amount, $from, $to) = @_;
+    my %currencies = map { $_ => 1 } @excludedCurrencies;
     my $endpoint = '';
     my $query = '';
     my $query2 = '';
+    
+    # Check if it's a valid number.
+    # If it isn't, return early.
+    my $styler = number_style_for($amount);
+    return unless $styler;
+    
+    my $normalized_number = $styler->for_computation($amount);
+    
+    # There are cases where people type in "2016 bitcoin", so we don't want to trigger on those queries.
+    # The first cryptocoins appeared in 2008, so dates before that could be valid amounts.
+    if($normalized_number >= 2008 && $normalized_number < 2100 && (length($from) == 0 || length($to) == 0)) {
+        return 0;
+    }
+    
+    $from = getCode($from) || '';
+    $to = getCode($to) || '';
+    
+    # Return early if we get a query like "btc to btc".
+    if($from eq $to) {
+        return 0;
+    }
+    
+    # Return early if we don't get a currency to convert from.
+    if($from eq '') {
+        return 0;
+    }
+    
+    # If both currencies are available, use the ticker endpoint
+    if (length($from) && length($to)) {
+        # Return early if both currencies are in the excluded list
+        # Allows searches like "ftc to aud" but excludes searches like "aud to usd"
+        if (exists($currencies{$from}) && exists($currencies{$from})) {
+            return;
+        }
+        $endpoint = 'ticker';
+        $query = $from . '-' . $to;
+        $query2 = '';
+    }
+    # For a single currency, call the secondaries endpoint
+    else {
+        # Return early if the single currency is in the excluded list
+        if (exists($currencies{$from})) {
+            return;
+        }
+        $endpoint = 'secondaries';
+        $query = $from;
+        $query2 = $from;
+    }
+    return $endpoint, $query, $query2;
+}
 
+handle query_lc => sub {
     if (/$guard/) {
         my ($amount, $from, $alt_amount, $to) = ($1, $2, $3, $4 || '');
         
@@ -70,27 +138,21 @@ handle query_lc => sub {
         if(length($amount) && length($alt_amount)) {
             return;
         }
-        
-        # If both currencies are available, use the ticker endpoint
-        if (length($from) && length($to)) {
-            print ("Using /ticker endpoint\n");
-            $endpoint = 'ticker';
-            $query = $from . '-' . $to;
-            $query2 = '';
+        # Case where the first amount is available.
+        elsif(length($amount)) {
+            return checkCurrencyCode($amount, $from, $to);
         }
+        # Case where the second amount is available.
+        # We switch the $from and $to here.
+        elsif(length($alt_amount)) {
+            return checkCurrencyCode($alt_amount, $to, $from);
+        }
+        # Case where neither of them are available.
         else {
-            print ("Using /secondaries endpoint\n");
-            $endpoint = 'secondaries';
-            $query = $from;
-            $query2 = $from;
+            return checkCurrencyCode(1, $from, $to);
         }
-        
-        print "Endpoint = " . $endpoint . "\n";
-        print "Query = " . $query . "\n";
-        print "Query2 = " . $query2 . "\n";
-        
-        return $endpoint, $query, $query2;
     }
+    
     return;
 };
 
