17426c82c4f3fb4b61a718c1b91fbe76bc073147
AkA84 <finalmente2@tin.it>
2015-02-15 13:48:38 +0000

    Improvements
    * Use the Auxiliary Information Box to display the pokemon's stats
    * Don't display an image if the API doesn't return any sprites for the pokemon
    * JS refactoring

--- share/spice/pokemon/pokemon.js
+++ share/spice/pokemon/pokemon.js
@@ -1,29 +1,31 @@
 (function (env) {
     "use strict";
+
     env.ddg_spice_pokemon = function(api_result){
-        var POKEAPI_IMAGE_URL = 'http://pokeapi.co/media/img/{id}.png';
+        var POKEAPI_SPRITE_URL = 'http://pokeapi.co/media/img/{id}.png';
+        var INFOBOX_PROPS = ['hp', 'attack', 'defense', 'height', 'weight', 'speed'];
         
-        // Validate the response (customize for your Spice)
         if (!api_result) {
             return Spice.failed('pokemon');
         }
 
-        // Render the response
         Spice.add({
             id: 'pokemon',
             name: 'Pokedex',
             data: api_result,
             meta: {
                 sourceName: 'pokeapi.co',
-                sourceUrl: 'http://pokeapi.co/api/v1/pokemon/' + api_result.name,
+                sourceUrl: 'http://pokeapi.co/',
                 sourceIconUrl: 'http://pokeapi.co/static/favicon.ico'
             },
             normalize: function(item) {
-              item.image = POKEAPI_IMAGE_URL.replace('{id}', item.national_id);
-              item.title = item.name;
-              item.types = item.types.map(function(type) { return DDG.capitalize(type.name); });
-              
-              return item;
+                return {
+                    title: item.name,
+                    image: getSprite.call(item),
+                    infoboxData: getInfoboxData.call(item),
+                    eggGroups: getCollectionNames.call(item, 'egg_groups'),
+                    types: getCollectionNames.call(item, 'types')
+                };
             },
             templates: {
                 group: 'info',
@@ -33,9 +35,50 @@
                 }
             }
         });
+
+        /**
+         * Returns the list of capitalized names from a collection
+         *
+         * @param {Array} collection
+         * @return {Array}
+         */
+        function getCollectionNames(collection) {
+            return this[collection].map(function(element) {
+                return DDG.capitalize(element.name);
+            });
+        }
+
+        /**
+         * Returns the list of label/value pairs to display in the Info Box
+         *
+         * @return {Array}
+         */
+        function getInfoboxData() {
+            var infoboxData = [{ heading: 'Stats:' }];
+
+            for( var prop in this ) {
+                if( INFOBOX_PROPS.indexOf(prop) !== -1 && parseInt(this[prop], 10) ) {
+                    infoboxData.push({
+                        label: DDG.capitalize(prop),
+                        value: this[prop]
+                    });
+                }
+            }
+
+            return infoboxData;
+        }
+
+        /**
+         * Returns the url of the pokemon's sprite or null if the API doesn't provide any
+         *
+         * @return {String / null}
+         */
+        function getSprite() {
+            if( this.sprites.length !== 0 ) {
+                return POKEAPI_SPRITE_URL.replace('{id}', this.national_id);
+            } else {
+                return null;
+            }
+        }
     };
-    
-    Handlebars.registerHelper("valueOrDash", function(value) {
-        return parseInt(value, 10) || '-';
-    });
 }(this));
