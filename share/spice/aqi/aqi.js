ff9ad59cc0d9769cf4faab2cfd5e859f2db6f902
schluchc <christian.schluchter@gmail.com>
2015-02-22 19:38:54 +0100

    design finally close to what we want, some code cleanup still needed

--- share/spice/aqi/aqi.js
+++ share/spice/aqi/aqi.js
@@ -10,34 +10,57 @@
 
     // Check if is array
     var results = [];
-    if (!($.isArray(api_result))) {
-      results = [api_result];
-    } else {
+    if (($.isArray(api_result))) {
       results = api_result;
+    } else {
+      results = [api_result];
     }
 
-    var common_info = results[0];
-    //todo: check if common info is valid for all data
+    var first_item = results[0];
+
+    var data_item = [{
+      title: 'Air Quality Indices for ' + first_item.ReportingArea + ', ' + first_item.StateCode,
+      subtitle: "Data as of " + first_item.DateObserved + ", " + first_item.HourObserved + ":00 " +  first_item.LocalTimeZone,
+      aqi_measurements: results
+    }];
 
     Spice.add({
       id: "aqi",
       name: "AQI",
-      data: results,
+      data: data_item,
       meta: {
         sourceName: "airnowapi.org",
         sourceUrl: 'http://www.airnow.gov/?action=airnow.local_city&zipcode=' + zip,
-        primaryText: 'Air Quality Indices for ' + common_info.ReportingArea + ', ' + common_info.StateCode,
-        secondaryText: "Data as of " + common_info.DateObserved + ", " + common_info.HourObserved + ":00 " +  common_info.LocalTimeZone,
+      },
+      normalize: function(item){
+        return {
+          title: item.title,
+          subtitle: item.subtitle
+        };
       },
       templates: {
         group: 'text',
-        item: Spice.aqi.content,
-        item_detail: false
+        item: false,
+        options: {
+          content: Spice.aqi.content,
+          moreAt: true
+        }
       }
     });
   }
 
-  Spice.registerHelper("AQI_ifCond", function(string1, string2, options) {
-      return ((string1 === string2) ? options.fn(this) : options.inverse(this));
+  Spice.registerHelper("aqi_circle_category", function(string1, options) {
+    if (string1 == "Good")
+      return "good";
+    else if (string1 == "Moderate")
+      return "moderate";
+    else if (string1 == "Unhealthy for Sensitive Groups")
+      return "usg";
+    else if (string1 =="Unhealthy")
+      return "unhealthy";
+    else if (string1 == "Very Unhealty")
+      return "very_unhealthy";
+    else
+      return "hazardous";
   });
 }(this));
