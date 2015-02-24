49551e38d624c61eed0e00eddf7de6fa94091c4a
AkA84 <finalmente2@tin.it>
2015-02-24 23:12:20 +0000

    Move private function definitions

--- share/spice/pokemon/data/pokemon_data.js
+++ share/spice/pokemon/data/pokemon_data.js
@@ -1,16 +1,15 @@
 (function (env) {
     "use strict";
 
-    var ID = 'pokemon';
+    var ID = 'pokemon',
+        DESCRIPTION_ENDPOINT = '/js/spice/pokemon/description/{id}',
+        POKEAPI_SPRITE_URL = 'http://pokeapi.co/media/img/{id}.png',
+        INFOBOX_PROPS = ['hp', 'attack', 'defense', 'height', 'weight', 'speed'];
 
     /**
      *  [ Pokemon::Data ]
      */
     env.ddg_spice_pokemon_data = function(api_result){
-        var DESCRIPTION_ENDPOINT = '/js/spice/pokemon/description/{id}',
-            POKEAPI_SPRITE_URL = 'http://pokeapi.co/media/img/{id}.png',
-            INFOBOX_PROPS = ['hp', 'attack', 'defense', 'height', 'weight', 'speed'];
-
         if (!api_result) {
             return Spice.failed('pokemon');
         }
@@ -45,67 +44,6 @@
         if( api_result.descriptions.length > 0 ) {
             fetchDescription(api_result.descriptions);
         }
-
-        /**
-         * Chooses a random pokemon's description from the available sets
-         * and then calls the Pokemon::Description endpoint to fetch its full text
-         *
-         * @param {Array} descriptions
-         */
-        function fetchDescription(descriptions) {
-            var randIndex = Math.floor(Math.random() * descriptions.length),
-                id = descriptions[randIndex].resource_uri.match(/(\d+)\/$/)[1];
-
-            $.getScript(DESCRIPTION_ENDPOINT.replace('{id}', id));
-        }
-
-        /**
-         * Returns the list of capitalized names from a collection
-         *
-         * @context {item}
-         * @param {Array} collection
-         * @return {Array}
-         */
-        function getCollectionNames(collection) {
-            return $.map(this[collection], function(element) {
-                return DDG.capitalize(element.name);
-            });
-        }
-
-        /**
-         * Returns the list of label/value pairs to display in the Info Box
-         *
-         * @context {item}
-         * @return {Array}
-         */
-        function getInfoboxData() {
-            var infoboxData = [{ heading: 'Stats:' }];
-
-            for( var prop in this ) {
-                if( INFOBOX_PROPS.indexOf(prop) !== -1 && parseInt(this[prop], 10) ) {
-                    infoboxData.push({
-                        label: DDG.capitalize(prop),
-                        value: this[prop]
-                    });
-                }
-            }
-
-            return infoboxData;
-        }
-
-        /**
-         * Returns the url of the pokemon's sprite or null if the API doesn't provide any
-         *
-         * @context {item}
-         * @return {String / null}
-         */
-        function getSprite() {
-            if( this.sprites.length !== 0 ) {
-                return POKEAPI_SPRITE_URL.replace('{id}', this.national_id);
-            } else {
-                return null;
-            }
-        }
     };
 
     /**
@@ -116,4 +54,65 @@
 
         Spice.getDOM(ID).find('.pokemon__description').html(description);
     };
+    
+    /**
+     * Chooses a random pokemon's description from the available sets
+     * and then calls the Pokemon::Description endpoint to fetch its full text
+     *
+     * @param {Array} descriptions
+     */
+    function fetchDescription(descriptions) {
+        var randIndex = Math.floor(Math.random() * descriptions.length),
+            id = descriptions[randIndex].resource_uri.match(/(\d+)\/$/)[1];
+
+        $.getScript(DESCRIPTION_ENDPOINT.replace('{id}', id));
+    }
+
+    /**
+     * Returns the list of capitalized names from a collection
+     *
+     * @context {item}
+     * @param {Array} collection
+     * @return {Array}
+     */
+    function getCollectionNames(collection) {
+        return $.map(this[collection], function(element) {
+            return DDG.capitalize(element.name);
+        });
+    }
+
+    /**
+     * Returns the list of label/value pairs to display in the Info Box
+     *
+     * @context {item}
+     * @return {Array}
+     */
+    function getInfoboxData() {
+        var infoboxData = [{ heading: 'Stats:' }];
+
+        for( var prop in this ) {
+            if( INFOBOX_PROPS.indexOf(prop) !== -1 && parseInt(this[prop], 10) ) {
+                infoboxData.push({
+                    label: DDG.capitalize(prop),
+                    value: this[prop]
+                });
+            }
+        }
+
+        return infoboxData;
+    }
+
+    /**
+     * Returns the url of the pokemon's sprite or null if the API doesn't provide any
+     *
+     * @context {item}
+     * @return {String / null}
+     */
+    function getSprite() {
+        if( this.sprites.length !== 0 ) {
+            return POKEAPI_SPRITE_URL.replace('{id}', this.national_id);
+        } else {
+            return null;
+        }
+    }
 }(this));
