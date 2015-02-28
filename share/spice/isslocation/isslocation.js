85800f83bf8d8556d7ee5632a104f9069b5b3c87
Thomas Denizou <javathunderman@users.noreply.github.com>
2015-02-28 14:21:27 -0500

    Fixing more issues

--- share/spice/isslocation/isslocation.js
+++ share/spice/isslocation/isslocation.js
@@ -2,7 +2,7 @@
     "use strict";
     env.ddg_spice_isslocation = function(api_result) {
         
-       if(!api_result || api_result !== "success") {
+       if (!api_result || !api_result.message || api_result.message !== "success"){
             return Spice.failed('isslocation');
         }
         var issLatitude = (Math.round(api_result.iss_position.latitude * 100) / 100).toFixed(2);
@@ -11,18 +11,19 @@
     DDG.require('maps', function() {
         Spice.add({
             id: "isslocation",
-            name: "ISSLocation",
+            name: "Map",
             model: 'Place',
 
             view: 'Map',
             data: [{
+                url: "http://www.nasa.gov/mission_pages/station/main/index.html",
                 display_name: "International Space Station",
                 name: "International Space Station",
                 lat: issLatitude,
                 lon: issLongitude
             }],
             meta: {
-                zoomlevel: 1;
+                zoomLevel: 1,
                 sourceName: "open-notify.org",
                 sourceUrl: 'http://open-notify.org'
             },
