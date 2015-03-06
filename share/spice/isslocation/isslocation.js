a0c4c79d4b81e347ce8fca8f9caa2f197fe745cd
Thomas Denizou <javathunderman@users.noreply.github.com>
2015-03-06 11:25:16 -0500

    Fixed JS code

--- share/spice/isslocation/isslocation.js
+++ share/spice/isslocation/isslocation.js
@@ -8,32 +8,33 @@
         var issLatitude = (Math.round(api_result.iss_position.latitude * 100) / 100).toFixed(2);
         var issLongitude = (Math.round(api_result.iss_position.longitude * 100) / 100).toFixed(2);
     
-    DDG.require('maps', function() {
-        Spice.add({
-            id: "isslocation",
-            name: "Map",
-            model: 'Place',
-
-            view: 'Map',
-            data: [{
-                url: "http://www.nasa.gov/mission_pages/station/main/index.html",
-                display_name: "International Space Station",
-                name: "International Space Station",
-                lat: issLatitude,
-                lon: issLongitude
-            }],
-            meta: {
-                zoomLevel: 1,
-                sourceName: "open-notify.org",
-                sourceUrl: 'http://open-notify.org'
-            },
-            normalize: function(item) {
-                
-                return {
+        DDG.require('maps', function() {
+            Spice.add({
+                id: "isslocation",
+                name: "Map",
+                model: 'Place',
+ 
+                view: 'Map',
+                data: [{
+                    url: "http://www.nasa.gov/mission_pages/station/main/index.html",
+                    display_name: "International Space Station",
+                    name: "International Space Station",
                     lat: issLatitude,
                     lon: issLongitude
-                };
-            }
+                }],
+                meta: {
+                    zoomLevel: 1,
+                    sourceName: "open-notify.org",
+                    sourceUrl: 'http://open-notify.org'
+                },
+                normalize: function(item) {
+ 
+                    return {
+                        lat: issLatitude,
+                        lon: issLongitude
+                    };
+                }
+            });
         });
-    };
+    }
 }(this));
