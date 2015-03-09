6d501e66c83bac7a154432be15cf9680c810f715
Usman Raza <usman.r123@gmail.com>
2015-03-09 21:26:24 +0500

    update npm with more information

--- share/spice/npm/npm.js
+++ share/spice/npm/npm.js
@@ -14,11 +14,34 @@
                 sourceName: "npmjs",
                 sourceUrl: 'http://npmjs.org/package/' + api_result.name
             },
-            
             normalize: function(item) {
+                var boxData = [{heading: 'Package Information:'}];
+                
+                boxData.push({
+                    label: "Author: " + item.author.name
+                });
+ 
+                boxData.push({
+                    label: "Project Homepage: " + item.homepage
+                });
+
+                boxData.push({
+                    label: "Repository: " + item.repository.url
+                });
+
+                var dependencies = $.map(item.dependencies, function(val, key) {
+                    return key + " (" + val + ")";
+                }).join(", ");
+
+                boxData.push({
+                    label: "Dependencies: " + dependencies
+                });
+
                 return {
                     title: item.name + " " + item.version,
-                    subtitle: item.description
+                    subtitle: item.description,
+                    infoboxData: boxData,
+                    dependency: dependencies
                 }  
             },
 
