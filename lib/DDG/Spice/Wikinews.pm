436a7d398044aa0a228d968c03f80555e27a7797
Bibhas <iambibhas@gmail.com>
2015-03-13 04:51:03 +0530

    spice/wikinews - updated author info

--- lib/DDG/Spice/Wikinews.pm
+++ lib/DDG/Spice/Wikinews.pm
@@ -12,9 +12,9 @@ category "time_sensitive";
 
 ## Me!
 attribution
-    github => "konr",
-    twitter => "konr",
-    web => "http://konr.mobi";
+    github => "iambibhas",
+    twitter => "bibhasdn",
+    web => "https://bibhas.in";
 
 ## Source, set as valid for 1 hour
 source "Wikinews";
@@ -23,8 +23,7 @@ spice proxy_cache_valid => "any 1h";
 
 ## The 'latest news' are merely the 10 last members
 ## of the 'Published' category.
-spice to => 'http://en.wikinews.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Published&format=json&cmsort=timestamp&cmdir=desc&cmprop=timestamp|ids|title&callback={{callback}}';
-
+spice to => 'http://en.wikinews.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Published&format=json&cmsort=timestamp&cmdir=newer&cmprop=ids|title|timestamp&callback={{callback}}';
 
 ## Simply get the latest news published.
 ## To get the latest news ON SOMETHING, we could:
@@ -39,9 +38,7 @@ spice to => 'http://en.wikinews.org/w/api.php?action=query&list=categorymembers&
 ## ---------------------------------------------------------
 # https://en.wikinews.org/w/api.php?action=help
 handle remainder => sub {
-    return '';
+    return $_ if $_;
 };
-
-
 
 1;
