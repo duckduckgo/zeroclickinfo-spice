1a54370d79127dc08f16914e50b10d07912000ec
Bibhas <iambibhas@gmail.com>
2015-03-13 11:10:10 +0530

    spice/wikinews - using the text template, added footer template

--- lib/DDG/Spice/Wikinews.pm
+++ lib/DDG/Spice/Wikinews.pm
@@ -19,11 +19,11 @@ attribution
 ## Source, set as valid for 1 hour
 source "Wikinews";
 spice is_cached => 1;
-spice proxy_cache_valid => "any 1h";
+spice proxy_cache_valid => "5m";
 
 ## The 'latest news' are merely the 10 last members
 ## of the 'Published' category.
-spice to => 'http://en.wikinews.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Published&format=json&cmsort=timestamp&cmdir=newer&cmprop=ids|title|timestamp&callback={{callback}}';
+spice to => 'http://en.wikinews.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Published&format=json&cmsort=timestamp&cmdir=desc&cmprop=ids|title|timestamp&callback={{callback}}';
 
 ## Simply get the latest news published.
 ## To get the latest news ON SOMETHING, we could:
