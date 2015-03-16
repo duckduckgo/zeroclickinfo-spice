aad6eff954c070f348a13046cf76b0457a1b1da3
puskin94 <giovanni.puskin@gmail.com>
2015-03-16 16:45:17 +0100

    added more triggers

--- lib/DDG/Spice/Timer.pm
+++ lib/DDG/Spice/Timer.pm
@@ -18,7 +18,7 @@ triggers start => ['time'];
 spice call_type => 'self';
 
 handle remainder => sub {
-    return unless /^( ?([\d.]+ ?(m(in(utes?)?)?|s(ec(onds?)?)?|h(ours?)?|hr)|online) ?)+$/ || $_ eq '';
+    return unless /^( ?([\d.]+ ?(m(in((ute)?s?)?)?|s(ec((ond)?s?)?)?|h((our)?s?)?|hr)|online) ?)+$/ || $_ eq '';
     return '';
 };
 
