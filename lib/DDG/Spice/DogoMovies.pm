f0aab1e911f2faa32d5e8a1287cadaa511df5488
dogomedia <jchen@dogomedia.com>
2015-03-17 15:58:07 +0000

    Update example queries and attributions

--- lib/DDG/Spice/DogoMovies.pm
+++ lib/DDG/Spice/DogoMovies.pm
@@ -3,13 +3,14 @@ package DDG::Spice::DogoMovies;
 use DDG::Spice;
 
 primary_example_queries "kids movies";
-secondary_example_queries "frozen kids movie", "hunger games kids movie reviews", "big hero 6 movie kids review";
-description "Search the DOGOmovies database for kids movie reviews and ratings";
+secondary_example_queries "frozen kids movie", "hunger games movies";
+description "Search for kids movie reviews and ratings";
 name "DOGOmovies";
 code_url "https://github.com/dogomedia/zeroclickinfo-spice/blob/master/lib/DDG/Spice/DogoMovies.pm";
 topics "entertainment", "everyday";
 category "entertainment";
-attribution github => ['https://github.com/dogomedia','DOGO Media'];
+attribution twitter => ['http://twitter.com/dogomovies','DOGOmovies'],
+            github  => ['https://github.com/dogomedia','DOGO Media, Inc.'];
 
 triggers any => "dogomovies", "dogo movies", "movie", "movies", "film", "films", "dvd", "dvds", "trailer", "trailers";
 
