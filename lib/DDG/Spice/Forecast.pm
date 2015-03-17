ee05c885fc3edb701df1032039d0d0d8bcf93165
Tommy Leung <tommy@tommyleung.com>
2015-03-17 17:09:02 -0400

    Adding back in important explanation

--- lib/DDG/Spice/Forecast.pm
+++ lib/DDG/Spice/Forecast.pm
@@ -51,6 +51,18 @@ handle remainder => sub {
     return $location, 'current', {is_cached => 0};
 };
 
+# 2015.03.17 tommytommytommy:
+# This IA overtriggers because we don't have a way to check that
+# the string following "weather" is an actual location.
+# For now, limit this IA to only trigger when the query is "weather", and
+# only send the user's location string.
+#
+# If the user specifies a location, we will try to internally infer
+# that location and make the appropriate upstream request.
+# When we have implemented places detection, we can restore the relevancy and triggering
+# functionality for this module by removing the block above and uncommenting
+# all the relevant lines.
+
 # handle query_lc => sub {
 #    my $location = '';
 #
