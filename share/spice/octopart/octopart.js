9f375cee93403d48a103bb4432af084413d3b533
moollaza <moollaza@fastmail.fm>
2015-03-19 22:51:40 +0000

    Update to actual API v3 params

--- share/spice/octopart/octopart.js
+++ share/spice/octopart/octopart.js
@@ -1,5 +1,32 @@
 (function (env) {
     "use strict";
+
+    // iterate over each distributors
+    // to get matching large and medium size images
+    function get_images(imagesets) {
+        var images = {};
+
+        $.each(imagesets, function(){
+            if (!is_mobile && this.large_image && this.medium_image){
+                images.large  = DDG.toHTTP(this.large_image.url);
+                images.medium = DDG.toHTTP(this.medium_image.url);
+                return false;
+
+            // fallback in case we never find a large image
+            // or on mobile device
+            } else if (this.medium_image){
+                images.medium = DDG.toHTTP(this.medium_image.url);
+                images.large = images.medium;
+
+            // worst case
+            } else if (this.small_image){
+                images.medium = DDG.toHTTP(this.small_image.url);
+                images.large = images.medium;
+            }
+        });
+        return images;
+    }
+
     env.ddg_spice_octopart = function (api_result) {
 
         if(!api_result || !api_result.results || api_result.results.length === 0) {
@@ -20,19 +47,19 @@
 
                 item = item.item; // our item object is wrapped in an "item" property
 
-                var img_url = item.images[0].url;
+                var images = get_images(item.imagesets);
 
                 return {
-                    brand: item.manufacturer.displayname,
-                    price: item.avg_price[1] + ' $' + item.avg_price[0].toFixed(2),
-                    img: img_url,
-                    img_m: img_url,
-                    url: item.detail_url,
+                    brand: item.brand.name,
+                    price: item.avg_price_v2[1] + ' $' + item.avg_price_v2[0].toFixed(2),
+                    img: images.medium,
+                    img_m: images.large,
+                    url: item.octopart_url,
                     title: item.mpn,
                     heading: item.mpn,
                     abstract: item.short_description,
                     datasheet: item.datasheets[0].url,
-                    market_status: item.market_status.replace(/^\w+: /, '') // strip out "GOOD: " from market_status
+                    market_status: item.market_status_v2.replace(/^\w+: /, '') // strip out "GOOD: " from market_status
                 };
             },
             templates: {
@@ -49,10 +76,10 @@
             relevancy: {
                 primary: [
                     { required: 'item.short_description' },
-                    { required: 'item.detail_url' },
+                    { required: 'item.octopart_url' },
                     { required: 'item.datasheets' },
-                    { required: 'item.images' },
-                    { required: 'item.avg_price' },
+                    { required: 'item.imagesets' },
+                    { required: 'item.avg_price_v2' },
                 ],
             }
         });
