7a0f71d4a00ae0f88a3c848cc725f3291b96211d
Bibhas <iambibhas@gmail.com>
2015-03-22 12:00:31 +0530

    spice/deb - using remainder instead of query_lc

--- lib/DDG/Spice/DebVersion.pm
+++ lib/DDG/Spice/DebVersion.pm
@@ -10,18 +10,12 @@ spice to => 'http://sources.debian.net/api/src/$1/';
 
 spice wrap_string_callback => 1;
 
-handle query_lc => sub {
-    if (/^\s*deb(ian| )\s*ver(sion| )\s*([\w\.\-\ ]+)\s*$/i) {
-        print "\n\n\n$1\n\n\n";
-        my $DebPackage = $3;
-        $DebPackage =~ s/\s+$//g;
-        $DebPackage =~ s/^\s+//g;
-        $DebPackage =~ s/\s+/-/g;
-
-	    return $DebPackage;
-
-    }
-    return;
+handle remainder => sub {
+    my $DebPackage = lc $_;
+    $DebPackage =~ s/\s+$//g;
+    $DebPackage =~ s/^\s+//g;
+    $DebPackage =~ s/\s+/-/g;
+    return $DebPackage;
 };
 
 1;
