880625e6762058a7b94b8ab31cd56a3246b9a1ce
Uriger <uri.gertzek@getevents.co>
2015-03-25 10:06:19 +0000

    Added usage of moment.js library and a new footer. Also changed the regex to be less complex.

--- share/spice/get_events/get_events.js
+++ share/spice/get_events/get_events.js
@@ -1,36 +1,6 @@
 (function (env) {
     "use strict";
     
-    
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-    // private functions
-    function getDate(date) {
-        if (date) {
-            date = date.replace(/T.*/, '');
-            var remix_date = date.split("-");
-            date = remix_date[1] + "/" + remix_date[2] + "/" + remix_date[0];
-        
-            date = new Date(date);
-            return date;
-        }
-        return ;
-    }
-
-    function getMonth(date) {
-        if (date) {
-            var month = months[parseInt(date.getMonth())];
-            return month.toUpperCase();
-        }
-        return;
-    }
-
-    function getDay(date) {
-        if (date) {
-            var day = date.getDate();
-            return day
-        }
-        return ;
-    }
 
     function buildUrl(id) {
         return 'https://getevents.co/events/preview/'+id;
@@ -44,35 +14,44 @@
             return Spice.failed('get_events');
         }
         
-        Spice.add({
-            id: "get_events",
-            name: "Events",
-            data: api_result.events,
-            meta: {
-                sourceName: "GetEvents",
-                sourceUrl: 'http://getevents.co',
-                itemType: "Upcoming Events"
-            },
-            normalize: function(item) {
-                return {
-                    url: buildUrl(item.id),
-                    title: item.name,
-                    place: item.venue.name,
-                    img: item.image_small_url,
-                    month: getMonth(getDate(item.start_date)),
-                    day: getDay(getDate(item.start_date))
-                };
-            },
-            templates: {
-                group: 'products',
-                item: Spice.get_events.item,
-                detail: false,
-                item_detail: false,
-                options:{
-                    moreAt: true,
-                    rating: false
+        DDG.require('moment.js', function(){
+            Spice.add({
+                id: "get_events",
+                name: "Events",
+                data: api_result.events,
+                meta: {
+                    sourceName: "GetEvents",
+                    sourceUrl: 'http://getevents.co',
+                    itemType: "Upcoming Events"
+                },
+                normalize: function(item) {
+                    return {
+                        url: buildUrl(item.id),
+                        description: '',
+                        title: item.name,
+                        place: item.venue.name,
+                        img: item.image_small_url,
+                        month: moment(item.start_date).format('MMM'),
+                        day: moment(item.start_date).format('D')
+                    };
+                },
+                templates: {
+                    group: 'text',
+                    detail: false,
+                    item_detail: false,
+                    options: {
+                        footer: Spice.get_events.footer,
+                        moreAt: true,
+                        rating: false
+                    },
+                    variants: {
+                        tileTitle: '3line-small',
+                        tileFooter: '4line'
+                    }
                 }
-            }
+            });
         });
-    };
+        
+        
+   };
 }(this));
