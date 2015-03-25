a92542f0a156efe4b76b444d80fb3857e496db34
Bibhas <iambibhas@gmail.com>
2015-03-25 21:59:10 +0530

    spice/sun_rise_set - added twitter attribution

--- lib/DDG/Spice/SunRiseSet.pm
+++ lib/DDG/Spice/SunRiseSet.pm
@@ -12,7 +12,8 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/
 icon_url "/i/timeanddate.com.ico";
 topics "everyday";
 category "geography";
-attribution github  => ['https://github.com/iambibhas', 'Bibhas'];
+attribution github  => ['https://github.com/iambibhas', 'Bibhas'],
+            twitter => ['https://twitter.com/bibhasdn', 'Bibhas D'];
 
 spice to => 'http://api.xmltime.com/timeservice?accesskey={{ENV{DDG_SPICE_TIME_AND_DATE_ACCESSKEY}}}&secretkey={{ENV{DDG_SPICE_TIME_AND_DATE_SECRETKEY}}}&out=js&query=$1&time=1&tz=1&verbosetime=1&sun=1';
 spice wrap_jsonp_callback => 1;
