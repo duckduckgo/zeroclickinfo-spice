24012a0fbf28a56d2b2ff687df0ecbc757e870bf
Bibhas <iambibhas@gmail.com>
2015-03-26 01:11:55 +0530

    spice/similar_sites - removed helper function

--- share/spice/similar_sites/similar_sites.js
+++ share/spice/similar_sites/similar_sites.js
@@ -6,7 +6,7 @@
                 source = $(script).attr("src"),
                 query = source.match(/similar_sites\/([^\/]+)/)[1];
 
-        if(!api_result || api_result.num === 0) {
+        if(!api_result || api_result.num === 0 || api_result.status !== 'ok') {
             return Spice.failed('similar_sites');
         }
 
@@ -26,6 +26,27 @@
                 more_at: query
             },
 
+            normalize: function (data) {
+                var sites = [];
+
+                for (var i = 0; i < data.num; i++) {
+                    var url = data.results['r' + i];
+                    sites.push({
+                        url: url,
+                        name: url.replace("http://", "")
+                                                   .replace("https://", "")
+                                                   .replace("www.", "")
+                                                   .replace(/\/+$/g, '')
+                    });
+                };
+                console.log(sites);
+
+                return {
+                    title: 'Websites similar to ' + query,
+                    sites: sites
+                }
+            },
+
             meta: {
                 total: num,
                 sourceName: 'SimilarSites',
@@ -35,9 +56,10 @@
             },
 
             templates: {
-                group: 'base',
+                group: 'text',
                 options: {
-                    content: Spice.similar_sites.content
+                    content: Spice.similar_sites.content,
+                    moreAt: true
                 }
             },
 
@@ -45,10 +67,10 @@
                 var toggle = false;
                 var $icon = $(".zci--similar_sites .chomp--link__icn");
                 $icon.attr('data-content', "+");
-                
+
                 var $more = $(".zci--similar_sites .chomp--link__mr");
                 var $less = $(".zci--similar_sites .chomp--link__ls");
-                
+
                 $("#show_more").click(function() {
                     $more.toggle();
                     $less.toggle();
@@ -66,30 +88,4 @@
         });
     };
 
-    Handlebars.registerHelper('similar_sites_list', function(items, from, to) {
-        var out = "";
-        var link;
-
-        if (to === 0)
-            to = Object.keys(items).length - 2;
-
-        for(var i = from; i < to; i++) {
-            link = items["r" + i].replace("http://", "")
-                                 .replace("https://", "");
-            if (link.slice(-1) == '/') {
-                link = link.slice(0, -1);
-                link = link.replace('www.', '');
-            }
-
-            out += "<div>"
-                +  "<img src='https://icons.duckduckgo.com/ip/" + link + ".ico'"
-                +  " width='16px' height='16px' />"
-                +  "<a class='tx-clr--dk' href='" 
-                +       items["r" + i] + "'>" + link 
-                +  "</a>"
-                +  "</div>"
-        }
-
-        return out;
-    });
 })(this);
