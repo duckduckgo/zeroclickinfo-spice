f26eccf0b93bf1fd73a0222359d4f8c0d7da6b79
Bibhas <iambibhas@gmail.com>
2015-03-29 02:00:43 +0530

    spice/time_and_date_holiday - using text and tile layout based on results

--- share/spice/time_and_date_holiday/time_and_date_holiday.js
+++ share/spice/time_and_date_holiday/time_and_date_holiday.js
@@ -8,29 +8,76 @@
 	        url = 'http://www.timeanddate.com',
 			source = url + '/search/results.html?query=' + encodeURIComponent(query);
 
-		Spice.add({
-	        id: 'time_and_date_holiday',
-	        name: 'Holidays',
-	        data: api_result.h,
-			normalize: function(item) {
-				return {
-					title: item.n,
-					description: item.a,
-					image: item.p,
-					url: url + item.u
-				};
-			},
-			meta: {
-				sourceName: 'timeanddate.com',
-				sourceUrl: source
-			},
-			templates: {
-				group: 'info',
-				options: {
-					content: Spice.time_and_date_holiday.info,
-					moreAt: true
-				}
-			}
-		});
+        if (api_result.h.length == 1){
+            var holiday = api_result.h[0];
+
+            if (holiday.d.length <= 0) {
+                return Spice.failed('time_and_date_holiday');
+            }
+
+            // selecting the first date only as it's the closest
+
+            Spice.add({
+                id: 'time_and_date_holiday',
+                name: 'Holidays',
+                data: api_result.h,
+                normalize: function(item) {
+                    var date = item.d[0];
+                    var next_date = '';
+
+                    if (item.d.length > 1) {
+                        next_date = item.d[1];
+                    }
+
+                    return {
+                        title: date,
+                        subtitle: item.n,
+                        description: item.a,
+                        next_date: next_date
+                    };
+                },
+                meta: {
+                    sourceName: 'timeanddate.com',
+                    sourceUrl: url + holiday.u
+                },
+                templates: {
+                    group: 'text',
+                    options: {
+                        content: Spice.time_and_date_holiday.singleday,
+                        moreAt: true
+                    }
+                }
+            });
+        } else {
+            Spice.add({
+                id: 'time_and_date_holiday',
+                name: 'Holidays',
+                data: api_result.h,
+                normalize: function(item) {
+                    if (item.d.length <= 0) {
+                        return Spice.failed('time_and_date_holiday');
+                    }
+                    var date = item.d[0];
+
+                    return {
+                        title: date,
+                        subtitle: item.n,
+                        description: item.a,
+                        url: url + item.u,
+                    };
+                },
+                meta: {
+                    sourceName: 'timeanddate.com',
+                    sourceUrl: url + source
+                },
+                templates: {
+                    group: 'text',
+                    detail: false,
+                    variants: {
+                        tileTitle: '1line',
+                    },
+                }
+            });
+        }
 	};
 }(this));
