2186790b136b60eb52522217d6eefa490167b7d9
Uriger <uri.gertzek@getevents.co>
2015-04-07 11:10:26 +0000

    new design

--- share/spice/get_events/get_events.js
+++ share/spice/get_events/get_events.js
@@ -1,57 +1,63 @@
 (function (env) {
     "use strict";
-    
 
     function buildUrl(id) {
         return 'https://getevents.co/events/preview/'+id;
-        //For testing
-        // return 'https://staging.getevents.co/events/preview/'+id;
     }
-    
-    // callback
+
+    function getAddress(obj) {
+        var address = [];
+        $.each(obj, function(k,v){
+            if (k !== "country" && v.length){
+                address.push(v);
+            }
+        });
+        return address;
+    }
+
     env.ddg_spice_get_events = function(api_result){
         if (api_result.error) {
             return Spice.failed('get_events');
         }
-        
+
+        DDG.require('maps', function(){
         DDG.require('moment.js', function(){
             Spice.add({
                 id: "get_events",
-                name: "Events",
+                name: "Local Events",
+                model: "Place",
+                view: "Places",
                 data: api_result.events,
                 meta: {
                     sourceName: "GetEvents",
                     sourceUrl: 'http://getevents.co',
-                    itemType: "Upcoming Events"
+                    itemType: "Upcoming Local Events",
+                    formattedSourceName: 'More on GetEvents'
                 },
-                normalize: function(item) {
+                normalize: function(item){
                     return {
                         url: buildUrl(item.id),
-                        description: '',
-                        title: item.name,
+                        name: item.name,
+                        description: item.description,
                         place: item.venue.name,
-                        img: item.image_small_url,
-                        month: moment(item.start_date).format('MMM'),
-                        day: moment(item.start_date).format('D')
+                        city: item.venue.city,
+                        address_lines: getAddress(item.venue.address),
+                        image: item.image_large_url,
+                        start: moment(item.start_date).format('MMM D'),
+                        end: moment(item.end_date).format('MMM D'),
+                        lat: item.venue.lat,
+                        lon: item.venue.lng,
+                        meta: {
+                            sourceName: item.source_name
+                        }
                     };
                 },
                 templates: {
-                    group: 'text',
-                    detail: false,
-                    item_detail: false,
-                    options: {
-                        footer: Spice.get_events.footer,
-                        moreAt: true,
-                        rating: false
-                    },
-                    variants: {
-                        tileTitle: '3line-small',
-                        tileFooter: '4line'
-                    }
+                    group: 'places',
+                    item: Spice.get_events.item
                 }
             });
         });
-        
-        
+        });
    };
 }(this));
