8d7fd7bcf887436bf3ee9c432d81fa8ea7fabd66
EaswareApps <vishnu@easwareapps.com>
2015-04-09 10:20:39 +0530

    Adding English to Malayalam Dictionary

--- share/spice/malayalam_dictionary/easware_apps/malayalam_dictionary_easware_apps.js
+++ share/spice/malayalam_dictionary/easware_apps/malayalam_dictionary_easware_apps.js
@@ -19,9 +19,9 @@
                 sourceUrl: 'http://olam.in/Dictionary/?action=en_ml&q=' + api_result[0].word
             },normalize: function(item) {
 					var meanings = [];
-					
+					var limit = 5;
 					//limiting meanings to 5 and remove last entry in defenitions (link to website)
-					var display = (item.definitions.length-1 > 5)?5:item.definitions.length-1;
+					var display = (item.definitions.length-1 > limit)?limit:item.definitions.length-1;
 					for(var i=0;i<display;i++){
 						meanings.push(item.definitions[i])
 					}
