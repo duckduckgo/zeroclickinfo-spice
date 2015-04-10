541338eca3d7f5a8418ab6580635b5a2a5b3fe7a
Bibhas <iambibhas@gmail.com>
2015-04-10 22:25:39 +0530

    upc - removed cache

--- lib/DDG/Spice/Upc.pm
+++ lib/DDG/Spice/Upc.pm
@@ -12,7 +12,6 @@ triggers any => "upc", "upc code";
 spice to => 'http://api.upcdatabase.org/json/{{ENV{DDG_SPICE_UPC_APIKEY}}}/$1';
 spice wrap_jsonp_callback => 1;
 spice is_cached => 1;
-spice proxy_cache_valid => "200 1d";
 
 handle remainder => sub {
     return $_ if $_ =~ /^(\d+)$/;
