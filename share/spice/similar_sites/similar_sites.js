816adb8d0e72c31c3ae296490a06b8374c6eda7d
Zaahir Moolla <moollaza@duckduckgo.com>
2015-04-10 16:28:29 +0000

    Revert "SimilarSites: convert to list group, simplify"
    This reverts commit 6c740ba91fcbd7729b8d1d9b33de5d21251612bf.

--- share/spice/similar_sites/similar_sites.js
+++ share/spice/similar_sites/similar_sites.js
@@ -11,33 +11,63 @@
             return Spice.failed('similar_sites');
         }
 
+        var num = api_result.num;
+        var show_more = true;
+        if (num < 5)
+            show_more = false;
+
         Spice.add({
             id: 'similar_sites',
             name: 'Similar Sites',
-            data: api_result,
+
+            data: {
+                results: api_result,
+                num: num,
+                show_more: show_more,
+                more_at: query
+            },
+
             normalize: function (data) {
                 var sites = [];
 
                 for (var i = 0; i < data.num; i++) {
-                    var url = data['r' + i];
+                    var url = data.results['r' + i];
                     sites.push({
                         url: url,
                         name: url.replace(/^https?:\/\/(www\.)?|\/+$/g, "")
                     });
-                }
+                };
 
                 return {
-                    list: sites
-                };
+                    sites: sites
+                }
             },
+
             meta: {
+                total: num,
                 sourceName: 'SimilarSites',
                 sourceUrl: 'http://www.similarsites.com/site/' + query,
+                sourceIcon: true,
+                itemType: 'Similar Sites'
             },
+
             templates: {
-                group: 'list',
+                group: 'base',
                 options: {
-                    list_content: Spice.similar_sites.list_content
+                    content: Spice.similar_sites.content
+                }
+            },
+
+            onShow: function() {
+                var $dom = Spice.getDOM('similar_sites');
+                if ($dom && $dom.length) {
+                    var $hidden = $dom.find('.is-hidden'),
+                        $show_more = $dom.find('.show_more');
+
+                    $show_more.click(function() {
+                        $hidden.toggle();
+                        $show_more.toggleClass('is-expanded');
+                    });
                 }
             }
         });
