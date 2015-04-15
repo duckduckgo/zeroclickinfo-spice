18e1d60619a0f83ce40edd1ce45db247f6933ef3
chrisjwilsoncom <chris@chrisjwilson.com>
2015-04-15 14:51:46 +0000

    Bitcoin: Switch back to using Biteasy, Update CSS, Use switch to change spice data/templates, Update tests

--- lib/DDG/Spice/Bitcoin.pm
+++ lib/DDG/Spice/Bitcoin.pm
@@ -17,21 +17,26 @@ secondary_example_queries "btc address 19KJZ5aqWnrrCaNAS5Pbd4uknPFBxjALRt",
                           "btc block 00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048",
                           "bitcoin block 0000000000000000137fda2ad0d6f81b76ee0a7c3c1224630115034b16afdaef";
 name "Bitcoin";
-source "http://blockchain.com";
+source "https://api.biteasy.com";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Bitcoin.pm";
 topics "economy_and_finance";
 category "finance";
-icon_url "https://blockchain.info/favicon.ico";
-attribution github => ["MrChrisW", "Chris Wilson"],
+icon_url "https://www.biteasy.com/favicon.ico";
+attribution github => ['https://github.com/biteasy','biteasy.com'],
+            email => ['support@biteasy.com','biteasy.com'],
+            twitter => ["biteasy", 'biteasy.com'],
+            web => ['https://biteasy.com','biteasy.com'],
+            github => ["MrChrisW", "Chris Wilson"],
             web => ["http://chrisjwilson.com", "Chris Wilson"];
-            
-my $addressTriggers = '^[13][1-9A-HJ-NP-Za-km-z]{26,33}$';
+
+my $addressTriggers = '^(?:bitcoin address|btc address)?\s*([13][1-9A-HJ-NP-Za-km-z]{26,33})$';
 my $txTriggers = '^(?:bitcoin transaction|btc transaction)?\s*([a-fA-F0-9]{64})$';
 my $blockTriggers = '^(?:bitcoin block|btc block)?\s*(0{8}[a-fA-F0-9]{56})$';
 
 triggers query_raw => qr/$addressTriggers|$txTriggers|$blockTriggers/;
 
-spice to => 'https://blockchain.info/$1/$2?format=json';
+spice to => 'https://api.biteasy.com/blockchain/v1/$1/$2?api_key={{ENV{DDG_SPICE_BITEASY_APIKEY}}}';
+
 spice from => '(.*)/(.*)';
 
 spice wrap_jsonp_callback => 1;
@@ -41,11 +46,11 @@ spice proxy_cache_valid => "418 1d";
 handle query_raw => sub { 
     return unless $_;
     #remove trigger words
-    s/(b[a-z]{0,7})\s(transaction|block)\s+|//g; 
+    s/(b[a-z]{0,7})\s(transaction|block|address)\s+|//g; 
     # switch api endpoints based on query
-    return "address", $_ if m/$addressTriggers/; 
-    return "rawblock", $_ if m/$blockTriggers/;
-    return "tx", $_ if m/$txTriggers/;
+    return "addresses", $_ if m/$addressTriggers/; 
+    return "blocks", $_ if m/$blockTriggers/;
+    return "transactions", $_ if m/$txTriggers/;
 };
 
 1;
