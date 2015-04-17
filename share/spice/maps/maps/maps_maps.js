bbf4232e8e21629dfc5bc17c8279fabd8be6a705
Tommy Leung <tommy@duckduckgo.com>
2015-04-17 15:12:20 -0400

    Use Spice.add() for maps

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -1,5 +1,23 @@
 DDG.require('maps',function(){
-    ddg_spice_maps_maps = function(place) {
-        DDG.duckbar.add_map(place);
+    ddg_spice_maps_maps = function(d) {
+        
+        if (!d || !d.length) { return Spice.failed('maps'); }
+
+        console.log("RAW MAP OBJECT %o", d);
+
+        d = [d[0]]; // OSM sends back a bunch of places, just want the first one for now
+                                
+        if (!DDG.isRelevant(d[0].display_name.toLowerCase())) { 
+            console.log("Maps failed relevancy", { log: "duckbar" });
+            return Spice.failed('maps'); 
+        } 
+        
+        Spice.add({
+            data: d,
+            id: "maps",
+            name: "maps",
+            model: "Place"
+        });
+
     };
 });
