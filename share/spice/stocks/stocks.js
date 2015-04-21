0a94ff380c24e5ab8ef58f97d8c3ee8b31346659
Zaahir Moolla <moollaza@duckduckgo.com>
2015-04-21 20:35:35 +0000

    Switchover Stocks from YCharts to Xignite

--- share/spice/stocks/stocks.js
+++ share/spice/stocks/stocks.js
@@ -2,45 +2,51 @@
     'use strict';
     env.ddg_spice_stocks = function(api_result){
 
-        if (!api_result || !api_result.length) {
+        if (!api_result || api_result.Outcome !== "Success") {
             return Spice.failed('stocks');
         }
 
-        var result = api_result[0];
+        DDG.require('moment.js', function(){
+            Spice.add({
+                id: 'stocks',
+                name: 'Stock',
+                data: api_result,
+                meta: {
+                    sourceName: 'Xignite',
+                    sourceUrl: "http://www.nasdaq.com/symbol/" + api_result.Security.Symbol + "/real-time"
+                },
+                normalize: function(data){
+                    var change = data.ChangeFromPreviousClose.toString(),
+                        percentChange = data.PercentChangeFromPreviousClose.toString(),
+                        changeDir;
+                    moment().utcOffset(data.UTCOffset);
 
-        // url is path on their domain:
-        result.url = 'http://ycharts.com' + result.url;
+                    // remove +/- from change attributes and add up/down class:
+                    if (change.charAt(0) === '+') {
+                        changeDir = 'up';
+                    } else if(change.charAt(0) === '-') {
+                        changeDir = 'down';
+                    } else {
+                        changeDir = 'same';
+                    }
 
-        // add title tag for link:
-        result.urlTitle = 'View more ' + result.name + ' stock data at YCharts';
-
-        // remove +/- from change attributes and add up/down class:
-        if (result.change.charAt(0) === '+') {
-            result.quoteChangeDir = 'up';
-        } else if(result.change.charAt(0) === '-') {
-            result.quoteChangeDir = 'down';
-        } else {
-            result.quoteChangeDir = 'same';
-        }
-
-        result.change = result.change.replace(/^[+|-]/,'');
-        result.change_percent = result.change_percent.replace(/^[+|-]/,'');
-
-        Spice.add({
-            id: 'stocks',
-            name: 'Stock',
-            data: result,
-            meta: {
-                sourceName: 'YCharts',
-                sourceUrl: result.url
-            },
-            templates: {
-                group: 'base',
-                options: {
-                    content: Spice.stocks.content,
-                    moreAt: true
+                    return {
+                        urlTitle: 'View more ' + data.name + ' stock data at NASDAQ',
+                        quoteChangeDir: changeDir,
+                        change: change.replace(/^[+|-]/,''),
+                        change_percent: percentChange.replace(/^[+|-]/,''),
+                        date: moment(data.Date).format("MMM DD"),
+                        time: moment(data.Time, "hh:mm:ss A").format("h:mm A")
+                    };
+                },
+                templates: {
+                    group: 'base',
+                    options: {
+                        content: Spice.stocks.content,
+                        moreAt: true
+                    }
                 }
-            }
+            });
         });
     };
 }(this));
