4552919afc772ec94058891e89ab635eee5a45e5
statista <lars.pohlmann@statista.com>
2015-04-23 15:18:20 +0000

    Statista DuckDuckGo init

---
+++ lib/DDG/Spice/Statista.pm
@@ -0,0 +1,28 @@
+package DDG::Spice::Statista;
+# ABSTRACT: Returns statistics from statista.com.
+
+use strict;
+use DDG::Spice;
+
+primary_example_queries "statistics about soccer";
+description "Shows example statistics";
+name "Statista.com";
+category "facts";
+
+my $api_key = '5db81c2a174170dac75620a16f553dd9';
+my $limit = 10;
+my $lang = 'xx';
+my @triggers = share('triggers.txt')->slurp;
+
+triggers any => @triggers;
+
+spice to => 'http://ec2-use-api-1397534038.us-east-1.elb.amazonaws.com/searchJson/apiKey/'.$api_key.'/q/$1/sort/0/lang/'.$lang.'/limit/'.$limit.'/datefrom/0/dateto/0';
+
+spice wrap_jsonp_callback => 1;
+
+handle remainder => sub {
+    return $_ if $_;
+    return;
+};
+
+1;
