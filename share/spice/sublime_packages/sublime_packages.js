5d907619946e53988ce4f30302d6dea97ece153a
Zaahir Moolla <moollaza@duckduckgo.com>
2015-06-02 19:04:42 +0000

    typo fixes

--- share/spice/sublime_packages/sublime_packages.js
+++ share/spice/sublime_packages/sublime_packages.js
@@ -42,8 +42,8 @@
                         elClass: {
                             tileTitle: "tx--17 tx-clr--slate",
                             tileSubtitle: "tx--14 tx-clr--grey-light",
-                            snppet: "tx--14 tx-clr--slate-light",
-                            tileFooter: "tx--14 tx-clr--grey-light"
+                            tileSnippet: "tx--14 tx-clr--slate-light",
+                            tileFoot: "tx--14 tx-clr--grey-light"
                         }
                     },
                     // Sort by number of unique installs
