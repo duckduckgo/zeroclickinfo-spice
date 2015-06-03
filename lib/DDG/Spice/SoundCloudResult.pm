09788940640279e9ae11befc190a45742402df10
jagtalon <jag@jagtalon.com>
2015-06-03 20:03:13 +0000

    SoundCloud: Work around the broken tokens.

--- lib/DDG/Spice/SoundCloudResult.pm
+++ lib/DDG/Spice/SoundCloudResult.pm
@@ -1,4 +1,4 @@
-package DDG::Spice::SoundCloud;
+package DDG::Spice::SoundCloudResult;
 # ABSTRACT: Audio file search on SoundCloud
 
 use strict;
@@ -16,10 +16,9 @@ attribution web => ['http://jordanscales.com', 'Jordan Scales'],
             github => ['http://github.com/jdan', 'Jordan Scales'],
             twitter => ['http://twitter.com/jdan', 'Jordan Scales'];
 
-spice to => 'http://api.soundcloud.com/tracks.json?client_id={{ENV{DDG_SPICE_SOUNDCLOUD_APIKEY}}}&q=&limit=0&filter=streamable';
-spice wrap_jsonp_callback => 1;
+spice to => 'http://api.soundcloud.com/tracks.json?client_id={{ENV{DDG_SPICE_SOUNDCLOUD_APIKEY}}}&q=$1&limit=35&filter=streamable';
 
-triggers startend => "sc", "soundcloud", "sound cloud";
+triggers startend => "//***never trigger***//";
 
 handle remainder => sub {
     return if $_ eq '';
