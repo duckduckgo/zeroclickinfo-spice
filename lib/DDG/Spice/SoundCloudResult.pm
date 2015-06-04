157302dd114de4d10eb76d2884373f00c706a058
jagtalon <jag@jagtalon.com>
2015-06-04 15:46:41 +0000

    SoundCloudResult.pm: Remove metadata.

--- lib/DDG/Spice/SoundCloudResult.pm
+++ lib/DDG/Spice/SoundCloudResult.pm
@@ -4,18 +4,6 @@ package DDG::Spice::SoundCloudResult;
 use strict;
 use DDG::Spice;
 
-description "Displays audio from SoundCloud";
-name "SoundCloud";
-primary_example_queries "soundcloud MGMT";
-secondary_example_queries "sc the new yorker";
-topics "entertainment", "music";
-category "entertainment";
-code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/SoundCloud.pm";
-attribution web => ['http://jordanscales.com', 'Jordan Scales'],
-            email => ['scalesjordan@gmail.com', 'Jordan Scales'],
-            github => ['http://github.com/jdan', 'Jordan Scales'],
-            twitter => ['http://twitter.com/jdan', 'Jordan Scales'];
-
 spice to => 'http://api.soundcloud.com/tracks.json?client_id={{ENV{DDG_SPICE_SOUNDCLOUD_APIKEY}}}&q=$1&limit=35&filter=streamable';
 
 triggers startend => "//***never trigger***//";
