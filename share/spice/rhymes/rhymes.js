7ea756b289b34b781463342e97bf1263feacebde
Bibhas <iambibhas@gmail.com>
2015-06-10 15:30:28 +0530

    rhymes - wrapping in the closure of new API

--- share/spice/rhymes/rhymes.js
+++ share/spice/rhymes/rhymes.js
@@ -1,49 +1,51 @@
-function ddg_spice_rhymes (api_result) {
+(function(env) {
     "use strict";
+    env.ddg_spice_rhymes = function(api_result) {
 
-    var query = DDG.get_query()
-    .replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
+        var query = DDG.get_query()
+        .replace(/^(what|rhymes?( with| for)?) |\?/gi, "");
 
-    if (!api_result.length) {
-        return Spice.failed('rhymes');
-    }
-
-    var words = [], count=0;
-
-    for(var i=0, l = api_result.length; i<l; i++) {
-        var word = api_result[i];
-
-        if (word.score === 300 && !word.flags.match(/a/)) {
-            words.push(word);
-            if (++count > 15)
-                break;
+        if (!api_result.length) {
+            return Spice.failed('rhymes');
         }
-    }
 
-    if(words.length === 0) {
-        return Spice.failed('rhymes');
-    }
+        var words = [], count=0;
 
-    var title = DDG.capitalize(query.toLowerCase());
-    Spice.add({
-        data: {
-            words: words,
-            query: title
-        },
-        id: "rhymes",
-        name: "Rhymes",
-        meta: {
-            sourceUrl: 'http://rhymebrain.com/en/What_rhymes_with_' +
-            encodeURIComponent(query) + '.html',
-            sourceName: 'RhymeBrain',
-            sourceIcon: true
-        },
-        templates: {
-            group: 'base',
-            options: {
-                content: Spice.rhymes.content,
-                moreAt: true
+        for(var i=0, l = api_result.length; i<l; i++) {
+            var word = api_result[i];
+
+            if (word.score === 300 && !word.flags.match(/a/)) {
+                words.push(word);
+                if (++count > 15)
+                    break;
             }
         }
-    });
-}
+
+        if(words.length === 0) {
+            return Spice.failed('rhymes');
+        }
+
+        var title = DDG.capitalize(query.toLowerCase());
+        Spice.add({
+            data: {
+                words: words,
+                query: title
+            },
+            id: "rhymes",
+            name: "Rhymes",
+            meta: {
+                sourceUrl: 'http://rhymebrain.com/en/What_rhymes_with_' +
+                encodeURIComponent(query) + '.html',
+                sourceName: 'RhymeBrain',
+                sourceIcon: true
+            },
+            templates: {
+                group: 'base',
+                options: {
+                    content: Spice.rhymes.content,
+                    moreAt: true
+                }
+            }
+        });
+    }
+}(this));
