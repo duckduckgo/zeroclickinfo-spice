af6df5698e389d3573a0021f454f09cef9a4fc1f
Bibhas <iambibhas@gmail.com>
2015-06-11 11:40:57 +0530

    anime - deleted CSS, using movies template, added more tests

--- lib/DDG/Spice/Anime.pm
+++ lib/DDG/Spice/Anime.pm
@@ -3,9 +3,11 @@ package DDG::Spice::Anime;
 use strict;
 use DDG::Spice;
 
-primary_example_queries "naruto anime";
+name "Anime";
 description "Anime information from Hummingbird";
-name "Movie";
+source "Hummingbird";
+primary_example_queries "naruto anime";
+secondary_example_queries "naruto hummingbird";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Anime.pm";
 topics "entertainment", "everyday";
 category "entertainment";
@@ -15,12 +17,6 @@ attribution github => ['https://github.com/jlcarvalho','Jean Lucas de Carvalho']
 spice proxy_cache_valid => "200 30d";
 spice to => 'https://hummingbird.me/api/v1/search/anime?query=$1';
 
-# It's important that 'movie info' precede 'movie' so that the handler
-# encounters it first and removes both words, rather than encountering 'movie'
-# first in the list, removing it, and leaving the word 'info.'
-
-# This spice will usually be triggered by deep triggers,
-# with a few extra triggers that deep might miss.
 triggers startend => 'anime', 'hummingbird';
 spice wrap_jsonp_callback => 1;
 
