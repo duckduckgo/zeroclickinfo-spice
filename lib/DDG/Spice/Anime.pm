daef8d481d99a8fd4db71cd974cc7b13ac30af34
Bibhas <iambibhas@gmail.com>
2015-06-13 00:08:44 +0530

    anime - updated attributions

--- lib/DDG/Spice/Anime.pm
+++ lib/DDG/Spice/Anime.pm
@@ -11,8 +11,8 @@ secondary_example_queries "naruto hummingbird";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Anime.pm";
 topics "entertainment", "everyday";
 category "entertainment";
-attribution github => ['https://github.com/jlcarvalho','Jean Lucas de Carvalho'],
-           twitter => ['https://twitter.com/JLCarv','Jean Lucas de Carvalho'];
+attribution github => ['https://github.com/iambibhas','Bibhas'],
+           twitter => ['https://twitter.com/bibhasdn','Bibhas D'];
 
 spice proxy_cache_valid => "200 30d";
 spice to => 'https://hummingbird.me/api/v1/search/anime?query=$1';
