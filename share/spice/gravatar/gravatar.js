6b37b569544199d372b1a9e0ba72f5a42c9402a8
Kevin Kwa <kev.kwa@gmail.com>
2015-06-14 12:32:30 +0800

    Gravatar - switch to Icon template group - amend normalize and templates - remove unnecessary css - add test cases - remove content.handlebars

--- share/spice/gravatar/gravatar.js
+++ share/spice/gravatar/gravatar.js
@@ -4,7 +4,7 @@
     env.ddg_spice_gravatar = function(api_result) {
 
         // Check for errors.
-        if(!api_result || !api_result.entry || api_result.entry.length === 0) {
+        if (!api_result || !api_result.entry || api_result.entry.length === 0) {
             return Spice.failed('gravatar');
         }
 
@@ -20,77 +20,57 @@
                 sourceUrl: api_result.entry[0].profileUrl,
                 sourceIconUrl: 'http://gravatar.com/favicon.ico'
             },
+
             normalize: function() {
                 // Get the name of the user.
                 function getName(entry) {
-                    if(!entry.name || !entry.displayName) {
+                    if (!entry.name || !entry.displayName) {
                         var splitted = api_result.entry[0].profileUrl.split("/");
                         return splitted[splitted.length - 1];
                     }
-
-                    if(entry.name.formatted) {
+                    if (entry.name.formatted) {
                         return entry.name.formatted;
-                    } else if(entry.name.givenName && entry.name.familyName) {
+                    } else if (entry.name.givenName && entry.name.familyName) {
                         return entry.name.givenName + " " + entry.name.familyName;
                     }
                     return entry.displayName;
                 }
 
+                function getCurrentLocation(entry) {
+                    if (entry.currentLocation) {
+                        return entry.currentLocation;
+                    } else {
+                        return "No Location Specified";
+                    }
+                }
+
+                function getAccounts(entry) {
+                    var accounts = [];
+                    if (entry.accounts) {
+                        $.each(entry.accounts, function() {
+                            accounts.push({
+                                text: DDG.capitalize(this.shortname),
+                                href: this.url
+                            });
+                        });
+                    } else {
+                        accounts = ["Additional Information Unavailable"];
+                    }
+                    return accounts;
+                }
+
                 return {
                     image: api_result.entry[0].thumbnailUrl + ".png",
                     title: getName(api_result.entry[0]),
-                    currentLocation: api_result.entry[0].currentLocation,
-                    accounts: api_result.entry[0].accounts,
-                    aboutMe: api_result.entry[0].aboutMe
+                    subtitle: getCurrentLocation(api_result.entry[0]),
+                    altSubtitle: getAccounts(api_result.entry[0]),
+                    description: api_result.entry[0].aboutMe
                 };
             },
+
             templates: {
-                group: 'base',
-                options: {
-                    content: Spice.gravatar.content,
-                    moreAt: true
-                }
-            },
-            onShow: function() {
-                $(".zci--gravatar .more").click(function() {
-                    $('.expanded_social').removeClass("hidden").addClass("visible");
-                    $('.condensed_social').removeClass("visible").addClass("hidden");
-                });
-            },
+                group: 'icon'
+            }
         });
     };
-
-    // Find the primary e-mail.
-    Handlebars.registerHelper("Gravatar_getEmail", function(emails, options) {
-        // Check if the variable exists.
-        if(!emails) {
-            return;
-        }
-
-        // Find the primary email.
-        for(var i = 0; i < emails.length; i += 1) {
-            if(emails[i].primary) {
-                return options.fn(emails[i]);
-            }
-        }
-    });
-
-    // If we don't have any information to display, just show this.
-    Handlebars.registerHelper("Gravatar_fallbackInfo", function(emails, aboutMe, currentLocation, accounts, context, options) {
-        if(!emails && !aboutMe && !currentLocation && !accounts) {
-            return options.fn(context);
-        }
-    });
-
-    // Return the name of the domain, capitalized and without the ".com" part
-    Handlebars.registerHelper("Gravatar_formatDomain", function(domain) {
-        if(domain === "plus.google.com") {
-            return "Google+";
-        }
-        if(domain.match(/.*wordpress.*/i)) {
-            return "Wordpress";
-        } else {
-            return DDG.capitalize(domain).replace(".com", "");
-        }
-    });
 }(this));
