fa5845406683609487bcf21c0c023d68923f35b5
Doug Brown <doug@duckduckgo.com>
2015-06-15 16:41:35 -0400

    try to use a more consistent naming convention for the spice sports call

--- share/spice/sports/mlb/mlb_games.js
+++ share/spice/sports/mlb/mlb_games.js
@@ -1,89 +1,93 @@
-function ddg_spice_mlb_games(apiResult) {
+(function (env) {
+    "use strict";
 
-    if (!apiResult || || !apiResult.data || !apiResult.data.games || !apiResult.data.games.length) {
-        return Spice.failed('mlbgames');
-    }
-    
-    DDG.require('moment.js', function(){
-        Spice.add({
-            id: 'mlb_games',
-            name: 'MLB Games',
+    env.ddg_spice_sports_mlb_games = function(apiResult) {
 
-            allowMultipleCalls: true,
+        if (!apiResult || !apiResult.data || !apiResult.data.games || !apiResult.data.games.length) {
+            return Spice.failed('mlb_games');
+        }
+        
+        DDG.require('moment.js', function(){
+            Spice.add({
+                id: 'mlb_games',
+                name: 'MLB Games',
 
-            data: apiResult.data.games,
+                allowMultipleCalls: true,
 
-            model: 'MLBGame',
-            from: apiResult.from,
-            signal: apiResult.signal,        
+                data: apiResult.data.games,
 
-            noDetail: true,
-            expandItems: true,
-            mostRelevant: apiResult.data.most_relevant_game_id,
+                model: 'MLBGame',
+                from: 'mlb_games',
+                signal: apiResult.signal,
 
-            meta: {
-                idField: 'id',
-                showMoreAt: true,
-                sourceIcon: false,
-                sourceUrl: "http://www.bleacherreport.com/",
-                sourceName: 'Bleacher Report',
-                secondaryText: (!is_mobile) ? '<span class="tx-clr--grey-dark">Data from SportsData</span>' : false,
-                hideModeSwitch: true,
-                variableTileWidth: true,
-                itemType: "Games"
-            },
+                noDetail: true,
+                expandItems: true,
+                mostRelevant: apiResult.data.most_relevant_game_id,
 
-            templates: {
-                item: 'mlb_score_item'
-            },
-            
-            normalize: function(attrs) {
-                attrs.canExpand = false;
+                meta: {
+                    idField: 'id',
+                    showMoreAt: true,
+                    sourceIcon: false,
+                    sourceUrl: "http://www.bleacherreport.com/",
+                    sourceName: 'Bleacher Report',
+                    secondaryText: (!is_mobile) ? '<span class="tx-clr--grey-dark">Data from SportsData</span>' : false,
+                    hideModeSwitch: true,
+                    variableTileWidth: true,
+                    itemType: "Games"
+                },
+
+                templates: {
+                    item: 'mlb_score_item'
+                },
                 
-                // Game Finished/In-Progress
-                if (attrs.pitchers || (attrs.score && attrs.score.pitch_count) || attrs.status === "complete") {
-                    attrs.canExpand = true;
+                normalize: function(attrs) {
+                    attrs.canExpand = false;
                     
-                    var inning = -1,
-                        placeholderStr = '&nbsp;';
-                    
-                    // finished games
-                    if (attrs.status === "closed" || attrs.status === "complete") {
-                        placeholderStr = '<span class="tx-clr--grey-light">&bull;</span>';
-                        inning = attrs.score.away.innings.length-1;
-                        attrs.complete = true;
-                    } else {
-                        inning = attrs.score.pitch_count.inning-1;
+                    // Game Finished/In-Progress
+                    if (attrs.pitchers || (attrs.score && attrs.score.pitch_count) || attrs.status === "complete") {
+                        attrs.canExpand = true;
                         
-                        // always display placeholders up to 9 innings
-                        for(var i=inning+1; i < 9; i++) {
-                            attrs.score.home.innings[i] = 
-                            attrs.score.away.innings[i] = { 
-                                runs: "", 
-                                number: i+1, 
-                                sequence: i+1, 
-                                type: "inning" 
-                            };
-                        }
+                        var inning = -1,
+                            placeholderStr = '&nbsp;';
                         
-                        // mark current inning
-                        if (attrs.score.pitch_count.inning_half === "B") {
-                            attrs.score.home.innings[inning].current = true;
-                            attrs.score.pitch_count.half_over = true;
+                        // finished games
+                        if (attrs.status === "closed" || attrs.status === "complete") {
+                            placeholderStr = '<span class="tx-clr--grey-light">&bull;</span>';
+                            inning = attrs.score.away.innings.length-1;
+                            attrs.complete = true;
                         } else {
-                            attrs.score.away.innings[inning].current = true;
+                            inning = attrs.score.pitch_count.inning-1;
+                            
+                            // always display placeholders up to 9 innings
+                            for(var i=inning+1; i < 9; i++) {
+                                attrs.score.home.innings[i] = 
+                                attrs.score.away.innings[i] = { 
+                                    runs: "", 
+                                    number: i+1, 
+                                    sequence: i+1, 
+                                    type: "inning" 
+                                };
+                            }
+                            
+                            // mark current inning
+                            if (attrs.score.pitch_count.inning_half === "B") {
+                                attrs.score.home.innings[inning].current = true;
+                                attrs.score.pitch_count.half_over = true;
+                            } else {
+                                attrs.score.away.innings[inning].current = true;
+                            }
+                        }
+                        
+                        if (inning > -1 && attrs.score.home.innings[inning].runs === 'X') {
+                            // replace un-played inning 'X' with something more stylish
+                            attrs.score.home.innings[inning].runs = placeholderStr;
                         }
                     }
                     
-                    if (inning > -1 && attrs.score.home.innings[inning].runs === 'X') {
-                        // replace un-played inning 'X' with something more stylish
-                        attrs.score.home.innings[inning].runs = placeholderStr;
-                    }
+                    return attrs;
                 }
-                
-                return attrs;
-            }
+            });
         });
-    });
+    }
 
-}
+}(this));
