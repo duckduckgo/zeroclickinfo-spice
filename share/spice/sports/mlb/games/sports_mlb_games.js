cdddadb503cca73d6942d16d1d920fedbb17a52b
Doug Brown <doug@duckduckgo.com>
2015-06-23 12:25:13 -0400

    (mlb scores) remove 'allowMultipleCalls' for now

--- share/spice/sports/mlb/games/sports_mlb_games.js
+++ share/spice/sports/mlb/games/sports_mlb_games.js
@@ -12,8 +12,6 @@
                 id: 'mlb_games',
                 name: 'MLB Games',
 
-                allowMultipleCalls: true,
-
                 data: gameData(apiResult.data),
 
                 from: apiResult.from,
