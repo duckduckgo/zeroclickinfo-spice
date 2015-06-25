ded3bf133aeb81a134a77f38456509556acdae54
Kevin Kwa <kevin.kwa@theaesthetictheory.com>
2015-06-25 10:55:24 +0800

    Google Plus - Remove semi-colon in GooglePlus.pm

--- lib/DDG/Spice/GooglePlus.pm
+++ lib/DDG/Spice/GooglePlus.pm
@@ -11,9 +11,9 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/
 topics "everyday", "social";
 category "ids";
 attribution github => ['https://github.com/jagtalon','jagtalon'],
-            twitter => ['http://twitter.com/juantalon','jagtalon'];
-            github => ['https://github.com/altern8tif', 'altern8tif'];
-            twitter => ['http://twitter.com/altern8tif', 'altern8tif'];
+            twitter => ['http://twitter.com/juantalon','jagtalon'],
+            github => ['https://github.com/altern8tif', 'altern8tif'],
+            twitter => ['http://twitter.com/altern8tif', 'altern8tif']
 
 spice to => 'https://www.googleapis.com/plus/v1/people/?query=$1&key={{ENV{DDG_SPICE_GOOGLE_PLUS_APIKEY}}}&callback={{callback}}&maxResults=12';
 spice proxy_ssl_session_reuse => "off";
