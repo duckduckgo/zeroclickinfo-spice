c5ad0adebf386c264a42801f091db422fbe265a1
gokul1794 <gokul.shanth@gmail.com>
2015-06-25 05:21:55 +0000

    Idented code, Added credits, added #292929 to dark theme

--- share/spice/snow/snow.js
+++ share/spice/snow/snow.js
@@ -44,124 +44,117 @@ function nrio (api_result) {
             options: {
                 content: Spice.snow.content,
                 moreAt: true
-            }
+            }            
         },
-        onShow: function(){
-         // add the snowflakes, if it's snowing
-         if(!api_result.is_snowing){
-             return; 
+       onShow: function(){
+           // add the snowflakes, if it's snowing
+           if(!api_result.is_snowing){
+                return; 
              }
-            else{
-                makeItSnow();
-            }
-        }
+           else{
+               makeItSnow();
+           }
+       }
     });
-
-    
-    
-    
+ 
     var $dom = Spice.getDOM('snow');
     $dom.css({
-        background: '#0C0F2B',
         color: '#fff'
     });
-
+// Thanks to Ricard Torres[http://php.quicoto.com/snow-html-canvas/]
 function makeItSnow() {
+    var COUNT = 700;
+    var masthead = document.querySelector('.zci.zci--snow');
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var width = masthead.clientWidth;
+    var height = masthead.clientHeight;
+    var i = 0;
+    var active = false;
 
-  var COUNT = 700;
-  var masthead = document.querySelector('.zci.zci--snow');
-  var canvas = document.createElement('canvas');
-  var ctx = canvas.getContext('2d');
-  var width = masthead.clientWidth;
-  var height = masthead.clientHeight;
-  var i = 0;
-  var active = false;
+    function onResize() {
+        width = masthead.clientWidth;
+        height = masthead.clientHeight;
+        canvas.width = width;
+        canvas.height = height;
+        ctx.fillStyle = '#FFF';
+        
+        var wasActive = active;
+        active = width > 600;
 
-  function onResize() {
-    width = masthead.clientWidth;
-    height = masthead.clientHeight;
-    canvas.width = width;
-    canvas.height = height;
-    ctx.fillStyle = '#FFF';
-
-    var wasActive = active;
-    active = width > 600;
-
-    if (!wasActive && active)
-      requestAnimFrame(update);
-  }
-
-  var Snowflake = function () {
-    this.x = 0;
-    this.y = 0;
-    this.vy = 0;
-    this.vx = 0;
-    this.r = 0;
-
-    this.reset();
-  }
-
-  Snowflake.prototype.reset = function() {
-    this.x = Math.random() * width;
-    this.y = Math.random() * -height;
-    this.vy = 1 + Math.random() * 3;
-    this.vx = 0.5 - Math.random();
-    this.r = 1 + Math.random() * 2;
-    this.o = 0.5 + Math.random() * 0.5;
-  }
-
-  canvas.style.position = 'absolute';
-  canvas.style.left = canvas.style.top = '0';
-
-  var snowflakes = [], snowflake;
-  for (i = 0; i < COUNT; i++) {
-    snowflake = new Snowflake();
-    snowflake.reset();
-    snowflakes.push(snowflake);
-  }
-
-  function update() {
-
-    ctx.clearRect(0, 0, width, height);
-
-    if (!active)
-      return;
-
-    for (i = 0; i < COUNT; i++) {
-      snowflake = snowflakes[i];
-      snowflake.y += snowflake.vy;
-      snowflake.x += snowflake.vx;
-
-      ctx.globalAlpha = snowflake.o;
-      ctx.beginPath();
-      ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
-      ctx.closePath();
-      ctx.fill();
-
-      if (snowflake.y > height) {
-        snowflake.reset();
-      }
+        if (!wasActive && active)
+            requestAnimFrame(update);
+    }
+    
+    var Snowflake = function () {
+        this.x = 0;
+        this.y = 0;
+        this.vy = 0;
+        this.vx = 0;
+        this.r = 0;
+        
+        this.reset();
     }
 
-    requestAnimFrame(update);
-  }
+    Snowflake.prototype.reset = function() {
+        this.x = Math.random() * width;
+        this.y = Math.random() * -height;
+        this.vy = 1 + Math.random() * 3;
+        this.vx = 0.5 - Math.random();
+        this.r = 1 + Math.random() * 2;
+        this.o = 0.5 + Math.random() * 0.5;
+    }
 
-  // shim layer with setTimeout fallback
-  window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            function( callback ){
-              window.setTimeout(callback, 1000 / 60);
-            };
-  })();
+    canvas.style.position = 'absolute';
+    canvas.style.left = canvas.style.top = '0';
 
-  onResize();
-  window.addEventListener('resize', onResize, false);
+    var snowflakes = [], snowflake;
+    for (i = 0; i < COUNT; i++) {
+        snowflake = new Snowflake();
+        snowflake.reset();
+        snowflakes.push(snowflake);
+    }
 
-  masthead.appendChild(canvas);
+    function update() {
+        
+        ctx.clearRect(0, 0, width, height);
+
+        if (!active)
+            return;
+
+        for (i = 0; i < COUNT; i++) {
+            snowflake = snowflakes[i];
+            snowflake.y += snowflake.vy;
+            snowflake.x += snowflake.vx;
+
+        ctx.globalAlpha = snowflake.o;
+        ctx.beginPath();
+        ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+
+        if (snowflake.y > height) {
+            snowflake.reset();
+        }
+        }
+
+        requestAnimFrame(update);
+    }
+
+     // shim layer with setTimeout fallback
+     window.requestAnimFrame = (function() {
+         return  window.requestAnimationFrame       ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame    ||
+        function( callback ){
+             window.setTimeout(callback, 1000 / 60);
+         };
+     })();
+    
+    onResize();
+    window.addEventListener('resize', onResize, false);
+
+    masthead.appendChild(canvas);
 }
 
-
- 
 };
