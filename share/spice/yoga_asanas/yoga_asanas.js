a234c29c48adcc0aa2fb4715daf571b7bc753ad4
Zach Thompson <zach@duckduckgo.com>
2015-06-28 11:28:42 -0600

    New spice to consume and display DDG::Longtail::YogaAsanasAPI

---
+++ share/spice/yoga_asanas/yoga_asanas.js
@@ -0,0 +1,34 @@
+function ddg_spice_yoga_asanas(apiResult) {
+
+    if (!apiResult || !apiResult.response.result["numFound"]) {
+        return Spice.failed('yoga_asanas');
+    }
+
+	for(i = 0; i < apiResult.response.result.length; i++){
+		var a = apiResult.response.result[i];
+		Spice.add({
+			id: 'yoga_asanas',
+			name: 'Yoga Asanas',
+
+			data: a,
+			normalize: function(x){
+				return {
+					image: a.meta.img,
+					title: a.title,
+					subtitle: a.paragraph
+				};
+			},
+
+			meta: {
+				srcName: a.meta.srcName,
+				srcUrl: a.meta.srcUrl,
+				srcIcon: a.meta.favicon
+			},
+
+			templates: {
+				group: 'media'
+			},
+	
+		});
+	}
+}
