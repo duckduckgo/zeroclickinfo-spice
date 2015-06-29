d6fcc1ea37402c041bf3620c84c6248145edbd18
Zach Thompson <zach@duckduckgo.com>
2015-06-29 12:07:36 -0600

    YogaAsanas: fix bug in regex yoga_asanas.js: path fixes

--- lib/DDG/Spice/YogaAsanas.pm
+++ lib/DDG/Spice/YogaAsanas.pm
@@ -4,15 +4,16 @@ package DDG::Spice::YogaAsanas;
 use strict;
 use DDG::Spice;
 
-spice to => 'http://jason.duckduckgo.com:8000/solr/staging/select?indent=on&version=2.2&defType=edismax&qt=dismax&q=$1 source_match:yoga_postures&fq=&wt=json&start=0&rows=75&json.wrf={{callback}}';
-triggers any => qr{
+spice to => 'http://jason.duckduckgo.com:8000/solr/staging/select?version=2.2&defType=edismax&qt=dismax&q=$1 source_match:yoga_asanas_api&fq=&wt=json&start=0&rows=75&json.wrf={{callback}}';
+
+triggers query_lc => qr{
 	(?:\w+asanas?\b)|
 	(?:\w+sahita\b)|
 	(?:\bvinyasa\b)|
 	(?:\w+pitham\b)|
 	(?:samasthitih?)|
 	(?:stplutih??)|
-	(?:yoga\s+(?:postures?|poses?)|
+	(?:yoga\s+(?:postures?|poses?))|
 	(?:sun\s+salutation)|
 	(?:surya\s+namaskara)|
 	(?:(?:ashtanga|yoga)\s+(primary|intermediate|advanced)\s+series)|
--- share/spice/yoga_asanas/yoga_asanas.js
+++ share/spice/yoga_asanas/yoga_asanas.js
@@ -1,11 +1,14 @@
 function ddg_spice_yoga_asanas(apiResult) {
 
-    if (!apiResult || !apiResult.response.result.numFound) {
+    if (!apiResult || !apiResult.response.numFound) {
         return Spice.failed('yoga_asanas');
     }
 
-	for(i = 0; i < apiResult.response.result.length; i++){
-		var a = apiResult.response.result[i];
+	for(i = 0; i < apiResult.response.numFound; i++){
+		var a = apiResult.response.docs[i];
+		var meta = eval("(" + a.meta + ")");
+		delete a.meta;
+
 		Spice.add({
 			id: 'yoga_asanas',
 			name: 'Yoga Asanas',
@@ -13,20 +16,20 @@ function ddg_spice_yoga_asanas(apiResult) {
 			data: a,
 			normalize: function(x){
 				return {
-					image: a.meta.img,
+					image: meta.img,
 					title: a.title,
 					subtitle: a.paragraph
 				};
 			},
 
 			meta: {
-				srcName: a.meta.srcName,
-				srcUrl: a.meta.srcUrl,
-				srcIcon: a.meta.favicon
+				srcName: meta.srcName,
+				srcUrl: meta.srcUrl,
+				srcIcon: meta.favicon
 			},
 
 			templates: {
-				group: 'media'
+				group: 'media',
 			},
 	
 		});
