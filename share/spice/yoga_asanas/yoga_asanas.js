7df4bfd63ee2a9e3e673bcf2628eb03f1411fffd
Zach Thompson <zach@duckduckgo.com>
2015-06-29 14:19:01 -0600

    Send all postures as array

--- share/spice/yoga_asanas/yoga_asanas.js
+++ share/spice/yoga_asanas/yoga_asanas.js
@@ -4,34 +4,30 @@ function ddg_spice_yoga_asanas(apiResult) {
         return Spice.failed('yoga_asanas');
     }
 
-	for(i = 0; i < apiResult.response.numFound; i++){
-		var a = apiResult.response.docs[i];
-		var meta = eval("(" + a.meta + ")");
-		delete a.meta;
+	Spice.add({
+		id: 'yoga_asanas',
+		name: 'Yoga Asanas',
 
-		Spice.add({
-			id: 'yoga_asanas',
-			name: 'Yoga Asanas',
+		data: apiResult.response.docs,
+		normalize: function(a){
+			var meta = eval("(" + a.meta + ")");
+			delete a.meta;
 
-			data: a,
-			normalize: function(x){
-				return {
-					image: meta.img,
-					title: a.title,
-					subtitle: a.paragraph
-				};
-			},
+			return {
+				image: meta.img,
+				title: a.title,
+				subtitle: a.paragraph,
+				meta: {
+					srcName: meta.srcName,
+					srcUrl: meta.srcUrl,
+					srcIcon: meta.favicon
+				}
+			};
+		},
 
-			meta: {
-				srcName: meta.srcName,
-				srcUrl: meta.srcUrl,
-				srcIcon: meta.favicon
-			},
+		templates: {
+			group: 'media',
+		},
 
-			templates: {
-				group: 'media',
-			},
-	
-		});
-	}
+	});
 }
