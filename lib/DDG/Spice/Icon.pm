e2021597e42845e11f7c756ca8fe90f26d40d5a3
Bibhas <iambibhas@gmail.com>
2015-07-01 12:48:11 +0530

    icon - added example queries

--- lib/DDG/Spice/Icon.pm
+++ lib/DDG/Spice/Icon.pm
@@ -3,33 +3,28 @@ package DDG::Spice::Icon;
 
 use DDG::Spice;
 
-# Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching
 spice is_cached => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "Icon";
 source "IconFinder";
 icon_url "https://api.iconfinder.com/favicon.ico";
 description "Find icons for the given term";
-primary_example_queries "first example query", "second example query";
-secondary_example_queries "optional -- demonstrate any additional triggers";
+primary_example_queries "female icon", "dog icons";
+secondary_example_queries "female free icon", "female premium icon";
 category "reference";
 topics "web_design";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Icon.pm";
 attribution github => ["iambibhas", "Bibhas D"],
             twitter => "bibhasdn";
 
-# API endpoint - https://duck.co/duckduckhack/spice_attributes#spice-codetocode
 spice to => 'https://api.iconfinder.com/v2/icons/search?query=$1&style=&vector=all&premium=$2';
 spice wrap_jsonp_callback => 1;
 
 spice from => '(.+)/(.+)';
 
-# Triggers - https://duck.co/duckduckhack/spice_triggers
 triggers startend => "icon", "icons", "free icon", "icon free",
                      "premium icon", "icon premium";
 
-# Handle statement
 handle query_lc => sub {
     $_ =~ s/\s?icons?\s?//g;
     my $premium = 'all';
