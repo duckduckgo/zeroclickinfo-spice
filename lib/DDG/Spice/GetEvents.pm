ac4962d3e9fbf6c49553a11448a2fd183accb0f5
Uriger <uri.gertzek@getevents.co>
2015-07-02 07:27:00 +0000

    Added city, country possibilty

--- lib/DDG/Spice/GetEvents.pm
+++ lib/DDG/Spice/GetEvents.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 use Text::Trim;
 
 
-primary_example_queries "events in chicago","what to do in chicago","things to do in chicago","events near me";
+primary_example_queries "events in chicago","what to do in chicago, il","things to do in chicago, illinois","events near me";
 secondary_example_queries "chicago events","chicago things to do";
 
 description "Upcoming events from GetEvents";
@@ -17,22 +17,29 @@ triggers startend => 'events near me','nearby events';
 triggers start =>'events in','what to do in','things to do in';
 triggers end => 'things to do','events';
 
-spice to => 'https://api.getevents.co/ddg?city=$1';
+spice to => 'https://api.getevents.co/ddg?city=$1&country=$2';
+spice from => "(.+)/(.*)";
 
 
 spice wrap_jsonp_callback => 1;
+
+
+
+
 
 my $pattern = '';
 my @triggers = ('events in', 'what to do in', 'things to do in','things to do', 'events');
 
 handle query_lc => sub {
+    my $re = '^(\b[a-zA-Z]+\b\s*\b[a-zA-Z]*\b),?\s*(\b[a-zA-Z]*\b\s*\b[a-zA-Z]*\b)$' ;
     my $query = $_;
+    my $c = '';
     if (($query eq 'events near me') || ($query eq 'nearby events')) {
         # Verifying that $loc and the relevant geo attributes (city) are valid.
         return unless $loc && $loc->city ;
         
         my $city = $loc->city;
-        return $city, {is_cached => 0};
+        return $city,'', {is_cached => 0};
     } 
     else {
     
@@ -41,10 +48,14 @@ handle query_lc => sub {
              s/$pattern//g ;
         }
         
+        # Triming string and breaking it by city, country/state
         my $query = trim($_); 
-        return $query if $query;
+        $query =~ /^(\b[a-zA-Z]+\b\s*\b[a-zA-Z]*\b),?\s*(\b[a-zA-Z]*\b\s*\b[a-zA-Z]*\b)$/ ;
+
+        return $1,$2 if $1;
         return;
     }
 };
 
 1;
+
