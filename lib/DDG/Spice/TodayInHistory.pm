8c276d1c97f5e2f977ee68af17a146e975f70cd6
Zaahir Moolla <moollaza@duckduckgo.com>
2015-07-03 20:11:24 +0000

    refactor and simplify date parsing

--- lib/DDG/Spice/TodayInHistory.pm
+++ lib/DDG/Spice/TodayInHistory.pm
@@ -4,8 +4,8 @@ package DDG::Spice::TodayInHistory;
 use strict;
 use DDG::Spice;
 use DateTime;
-use DateTime::TimeZone;
-use Time::Piece;
+use Date::Parse;
+use DateTime::Format::Natural;
 
 name "TodayInHistory";
 source "http://wikipedia.com";
@@ -18,68 +18,26 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/
 attribution github => ["https://github.com/killerfish", "Usman Raza"],
             twitter => ["https://twitter.com/f1shie", "Usman Raza"];
 
-spice to => 'https://en.wikipedia.org/w/api.php?action=query&titles=$1&prop=revisions&rvlimit=1&rvprop=content&format=json&continue=&redirects&callback={{callback}}';
+spice to => 'https://en.wikipedia.org/w/api.php?action=query&titles=$1&prop=revisions&rvlimit=1&rvprop=content&format=json&redirects&callback={{callback}}';
 
 triggers startend => "today in history";
 triggers start => "historical events on", "this day in history";
 
-# define variables
-my %days = ('1'=>'31','2'=>'29','3'=>'31','4'=>'30','5'=>'31','6'=>'30','7'=>'31','8'=>'31','9'=>'30','10'=>'31','11'=>'30','12'=>'31');
-
-# functions:
-# check and process if we got 2 parameters (month and day)
-sub readParams {
-  # read parameters - do not trigger if invalid parameters are found (delimiters: - / . ' ')
-    my ($parameter1, $parameter2) = split (/[-\/. ]/, $_[0]);
-    if ($parameter2) {
-    my $value;
-        # e.g. "historical events on June 12"
-        return $value if (($value = validateParams($parameter1, $parameter2)) ne 'fail');
-        # e.g. "historical events on 16 Sep"
-        return $value if (($value = validateParams($parameter2, $parameter1)) ne 'fail');
-    }
-    # no parameter - use today date
-    else {
-        return useToday();
-    }
-}
-
-# validate parameters and return day_month on success
-sub validateParams {
-  my $givenDay, my $givenMonth, my $month, my $evalfail;
-    # month name as full name (April), in abbreviated form (Apr) or as 2 digit (04)
-    my @monthPatterns = ('%B', '%b', '%m');
-    for my $monthPattern (@monthPatterns) {
-        $evalfail = 1 if ( eval {
-            my $validDate = Time::Piece->strptime("2000/$_[0]/1", "%Y/".$monthPattern."/%d");
-                $givenDay = 0;
-                $month = $validDate->mon;
-                $givenMonth = $validDate->fullmonth;
-        })
-    }
-    return 'fail' if (!$evalfail);
-    return 'fail' if ($_[1] =~ m/\D+/);
-    return 'fail' if ($_[1] > 31 || $_[1] > $days{$month});
-    if ($_[1] =~ m/^0{1}([1-9]){1}$/) {
-        $givenDay = $1;
-    } else {
-        $givenDay = $_[1];
-    }
-    return $givenDay."_".$givenMonth;
-}
-
-# use today's date if no parameter passed
-sub useToday {
-    my $t = DateTime->now;
-    $t->set_time_zone($loc->time_zone);
-    return $t->mday."_".$t->month_name;
-}
-
 handle remainder => sub {
-  # check current date in users timezone
-    if (my $date = readParams($_)) {
-        return $date;
+
+    my $dt = DateTime->now;
+
+    # no date specified
+    # use today's date
+    unless ($_){
+        $dt->set_time_zone($loc->time_zone);
+        return $dt->mday."_".$dt->month_name;
     }
-    return;
+
+    my $parser = DateTime::Format::Natural->new;
+    my $date = $parser->parse_datetime($_);
+
+    return unless ($parser->success);
+    return $date->day."_".$date->month;
 };
 1;
