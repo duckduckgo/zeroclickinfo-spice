0e70840f67d2e2a84df80a091225abaf8579381e
Zaahir Moolla <moollaza@duckduckgo.com>
2015-07-03 03:38:44 +0000

    convert tabs to spaces

--- lib/DDG/Spice/TodayInHistory.pm
+++ lib/DDG/Spice/TodayInHistory.pm
@@ -29,57 +29,57 @@ my %days = ('1'=>'31','2'=>'29','3'=>'31','4'=>'30','5'=>'31','6'=>'30','7'=>'31
 # functions:
 # check and process if we got 2 parameters (month and day)
 sub readParams {
-	# read parameters - do not trigger if invalid parameters are found (delimiters: - / . ' ')
-  	my ($parameter1, $parameter2) = split (/[-\/. ]/, $_[0]);
-  	if ($parameter2) {
-		my $value;
-            # e.g. "historical events on June 12"
-            return $value if (($value = validateParams($parameter1, $parameter2)) ne 'fail');
-            # e.g. "historical events on 16 Sep"
-            return $value if (($value = validateParams($parameter2, $parameter1)) ne 'fail');
-   	}
-  	# no parameter - use today date
-   	else {
-   		return useToday();
-  	}
+  # read parameters - do not trigger if invalid parameters are found (delimiters: - / . ' ')
+    my ($parameter1, $parameter2) = split (/[-\/. ]/, $_[0]);
+    if ($parameter2) {
+    my $value;
+        # e.g. "historical events on June 12"
+        return $value if (($value = validateParams($parameter1, $parameter2)) ne 'fail');
+        # e.g. "historical events on 16 Sep"
+        return $value if (($value = validateParams($parameter2, $parameter1)) ne 'fail');
+    }
+    # no parameter - use today date
+    else {
+        return useToday();
+    }
 }
 
 # validate parameters and return day_month on success
 sub validateParams {
-	my $givenDay, my $givenMonth, my $month, my $evalfail;
-  	# month name as full name (April), in abbreviated form (Apr) or as 2 digit (04) 
-  	my @monthPatterns = ('%B', '%b', '%m');
-  	for my $monthPattern (@monthPatterns) {
-      		$evalfail = 1 if ( eval {
-			my $validDate = Time::Piece->strptime("2000/$_[0]/1", "%Y/".$monthPattern."/%d");
-      			$givenDay = 0;
-      			$month = $validDate->mon;
-      			$givenMonth = $validDate->fullmonth;
-		})
-  	}
-	return 'fail' if(!$evalfail);
-	return 'fail' if($_[1] =~ m/\D+/);
-      	return 'fail' if($_[1] > 31 || $_[1] > $days{$month});
-	if($_[1] =~ m/^0{1}([1-9]){1}$/) {
-        	$givenDay = $1;
-	} else {
-      		$givenDay = $_[1];
-	}
-	return $givenDay."_".$givenMonth;
+  my $givenDay, my $givenMonth, my $month, my $evalfail;
+    # month name as full name (April), in abbreviated form (Apr) or as 2 digit (04)
+    my @monthPatterns = ('%B', '%b', '%m');
+    for my $monthPattern (@monthPatterns) {
+        $evalfail = 1 if ( eval {
+            my $validDate = Time::Piece->strptime("2000/$_[0]/1", "%Y/".$monthPattern."/%d");
+                $givenDay = 0;
+                $month = $validDate->mon;
+                $givenMonth = $validDate->fullmonth;
+        })
+    }
+    return 'fail' if (!$evalfail);
+    return 'fail' if ($_[1] =~ m/\D+/);
+    return 'fail' if ($_[1] > 31 || $_[1] > $days{$month});
+    if ($_[1] =~ m/^0{1}([1-9]){1}$/) {
+        $givenDay = $1;
+    } else {
+        $givenDay = $_[1];
+    }
+    return $givenDay."_".$givenMonth;
 }
 
 # use today's date if no parameter passed
 sub useToday {
     my $t = DateTime->now;
     $t->set_time_zone($loc->time_zone);
-	return $t->mday."_".$t->month_name;
+    return $t->mday."_".$t->month_name;
 }
 
 handle remainder => sub {
-	# check current date in users timezone
-  	if (my $date = readParams($_)) {
-		return $date;
-	}
-	return;
+  # check current date in users timezone
+    if (my $date = readParams($_)) {
+        return $date;
+    }
+    return;
 };
 1;
