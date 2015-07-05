aeef5e94f4d53b3f63f38762832d580cfc6a025d
rubinovitz <jrubinovitz@gmail.com>
2015-07-05 22:04:14 +0000

    in crunchbase_collections.js make checks all data being rendered exists before appending spice

--- share/spice/crunchbase/collections/crunchbase_collections.js
+++ share/spice/crunchbase/collections/crunchbase_collections.js
@@ -1,42 +1,65 @@
 (function (env) {
     "use strict";
     env.ddg_spice_crunchbase_collections = function(api_result){
-        // Validate the response
-        if (!api_result || api_result.error || api_result.data.items.length === 0) {
+        // throw error if no result, error result, no items, or array of 0 items
+        if (!api_result || api_result.error || !DDG.getProperty(api_result, 'data.items') || api_result.data.items.length === 0) {
             return Spice.failed('crunchbase');
         }
+        var data = api_result.data, // get org data
+	    items = data.items, // get all orgs
+	    firstResult = items[0], // get first org to render for now
+	    permalink = firstResult.path; // get endpoint of first org
 
-        var metadata = api_result.metadata, // get metadata for base urls
-            data = api_result.data, // get org data
-            items = data.items, // get all orgs
-            wwwPathPrefix = metadata.www_path_prefix,
-            imagePathPrefix = metadata.image_path_prefix,
-            firstResult = api_result.data.items[0], // get first org to render for now
-            permalink = firstResult.path; // get endpoint of first org
+	var metadata = api_result.metadata; // get metadata for base urls
+	// get base urls if metdata exists
+	if (metadata){
+            var wwwPathPrefix = metadata.www_path_prefix,
+                imagePathPrefix = metadata.image_path_prefix;
+	}
 
         // get organization information from organization endpoint
 	$.getJSON('js/spice/crunchbase/organizations/'+ permalink.split('/')[1],function(response) {
-            // Render the response
-            Spice.add({
-                id: "crunchbase",
-		name: "Answer",
-		data: response.data,
-		meta: {
-                    sourceName: "Crunchbase",
-                    sourceUrl: api_result.metadata.www_path_prefix + permalink
-                },
-                normalize: function(item) {
-                    return  { 
-                        description: DDG.strip_html(item.properties.description),
-                        url: wwwPathPrefix + permalink,
-                        image: imagePathPrefix + item.relationships.primary_image.items[0].path,
-                        title: item.title
-		    };
-		},
-		templates: {
-                    group: 'info'
+            // return error if no response to organization query
+            if (!response) {
+                return Spice.failed('crunchbase');
+            } else {
+                var orgData = DDG.getProperty(response, 'data');
+		var description = DDG.getProperty(orgData, 'properties.description');
+		// return spice failure if no description to render
+		if (!description || description === '' || description === ' '){   
+                       return Spice.failed('crunchbase');
+                // if there is description add spice
+                } else {
+                    var spiceMeta = {sourceName: 'Crunchbase'};
+		    // check metadata exists before adding it + permalink to source url
+		    if (metadata){
+                        spiceMeta.sourceUrl = wwwPathPrefix + permalink;
+		    }
+                    // Render the response
+		    Spice.add({
+                        id: 'crunchbase',
+			name: 'Answer',
+			data: orgData,
+			meta: spiceMeta,
+			normalize: function(item) {
+			    var spiceData = { 
+				description: DDG.strip_html(description),
+				url: wwwPathPrefix + permalink,
+				title: item.title
+			    };
+			    // add  primary image if exists
+			    if (item.relationships.primary_image.items.length > 0) {
+			       spiceData.image = imagePathPrefix + item.relationships.primary_image.items[0].path;
+			    }
+			    // return spice data
+			    return spiceData; 
+			},
+			templates: {
+			    group: 'info'
+			}
+		    });
                 }
-            });
+            }
         });
     };
 }(this));
