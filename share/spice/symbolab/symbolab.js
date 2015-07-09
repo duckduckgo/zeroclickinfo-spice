2d894d86a65063f6b65a428151f64592959549e7
Zaahir Moolla <moollaza@duckduckgo.com>
2015-07-09 21:19:23 +0000

    Testing intial symbolab spice

--- share/spice/symbolab/symbolab.js
+++ share/spice/symbolab/symbolab.js
@@ -1,58 +1,33 @@
 (function (env) {
     "use strict";
-    env.ddg_spice_symbolab = function(api_result){
+    env.ddg_spice_symbolab = function (api_result) {
 
         // Validate the response (customize for your Spice)
-        if (!api_result || (!api_result.page && !api_result.solution)) {
+        if (!api_result || !api_result.solution) {
             return Spice.failed('symbolab');
         }
 
-        if (api_result.page){
+        DDG.require('mathquill', function (){
             Spice.add({
                 id: "symbolab",
-                name: "Symbolab",
-                data: api_result,
+                name: "Calculator",
+                data: api_result.solution,
                 meta: {
-                    sourceName: "Symbolab.com",
-                    sourceUrl: api_result.page.url,
-                    sourceIconUrl: 'https://www.symbolab.com/favicon.ico'
-                },
-                normalize: function(item) {
-                    return {
-                        description: api_result.page.description,
-                        image: api_result.page.img,
-                        title: "Symbolab - " +api_result.page.title
-                    }; 
-                },
-                templates: {
-                    group: 'info'
-                }
-            });
-        }
-        else if (api_result.solution){
-            Spice.add({
-                id: "symbolab",
-                name: "Symbolab",
-                data: api_result,
-                meta: {
-                    sourceName: "Symbolab.com",
+                    sourceName: "Symbolab",
                     sourceUrl: api_result.solution.url
                 },
-                normalize: function(item) {
+                normalize: function (data) {
                     return {
-                        latex: api_result.solution.presentation,
-                        url: api_result.solution.url,
-                        hasPlot: api_result.solution.hasPlot
-                    }; 
+                        latex: data.presentation
+                    };
                 },
                 templates: {
-                    group: 'base',
+                    group: 'text',
                     options: {
-                        content: Spice.symbolab.solution,
-                        moreAt: false
+                        content: Spice.symbolab.content
                     }
                 }
             });
-        }
+        });
     };
 }(this));
