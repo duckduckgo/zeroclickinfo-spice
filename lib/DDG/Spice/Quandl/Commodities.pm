6260a7837f7462b68c1341762f9d78d4840813ee
brianrisk <brian@geneffects.com>
2015-07-10 18:07:03 +0000

    new 'Commodities' spice

---
+++ lib/DDG/Spice/Quandl/Commodities.pm
@@ -0,0 +1,53 @@
+package DDG::Spice::Quandl::Commodities;
+
+use DDG::Spice;
+use Text::Trim;
+use YAML::XS 'LoadFile';
+
+# meta data
+primary_example_queries "coal price";
+secondary_example_queries "cost of silver";
+description "Returns current commodity prices";
+name "Commodities";
+code_url "https://github.com/brianrisk/zeroclickinfo-spice";
+icon_url "https://www.quandl.com/favicon.ico";
+topics "economy_and_finance";
+category "facts";
+attribution web => ["https://www.quandl.com", "Quandl"],
+            twitter => "quandl";
+            
+# hash associating triggers with indicator codes
+my $primary_hash = LoadFile(share('commodities_primary.yml')); 
+
+# triggers sorted by length so more specific is used first
+my @primary_keys = sort { length $b <=> length $a } keys($primary_hash);
+my $primary_qr = join "|", @primary_keys;
+
+# hash associating secondary triggers with their codes
+my $secondary_hash = LoadFile(share('commodities_secondary.yml'));
+
+# array of secondary triggers
+my @secondary_keys = keys($secondary_hash);
+my $secondary_qr = join "|", @secondary_keys;
+
+# defining our triggers
+triggers any => @primary_keys;
+
+# to set an environmental variable:
+# duckpan env set <name> <value>
+
+# set spice parameters
+spice to => 'https://www.quandl.com/api/v1/datasets/$1.json?auth_token={{ENV{DDG_SPICE_QUANDL_APIKEY}}}&rows=2';
+spice wrap_jsonp_callback => 1;
+spice proxy_cache_valid => "418 1d";
+
+handle sub {
+
+    
+    return "OFDP/ALUMINIUM";
+};
+
+1;
+
+
+
---
+++ share/spice/quandl/commodities/quandl_commodities.js
@@ -0,0 +1,110 @@
+/**
+* Created with DuckDuckHack.
+* User: brianrisk
+* Date: 2015-07-10
+* Time: 12:00 PM
+* To change this template use Tools | Templates.
+*/
+
+
+(function (env) {
+    'use strict';
+    env.ddg_spice_quandl_commodities = function(api_result){
+
+        if (!api_result) {
+            return Spice.failed('quandl_commodities');
+        }
+
+        var result = api_result;
+
+        // we need two data points to get percent change
+        if (result.data.length < 2) {
+            return Spice.failed('quandl_commodities');
+        }
+        
+        var quandlCode = result.source_code + "/" + result.code;
+        
+        // setting title and units based on database/dataset string
+        result.header = "Price of ";
+        if (quandlCode === "OFDP/ALUMINIUM") {
+           result.header += "Aluminum";
+           result.units = "$USD/Tonne"
+        }
+
+        // url to the data set page
+        result.url = 'https://quandl.com/' + result.source_code + "/" + result.code;
+
+        // add title tag for link:
+        result.urlTitle = 'View more data at Quandl';
+
+        var recentValue = result.data[0][1];
+        var previousValue = result.data[1][1];
+
+        // month array to make string version of date
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        // adding in the previous date point date
+        var fromDate = new Date(result.data[1][0]);
+        var fromDateString = months[fromDate.getUTCMonth()] + " " + fromDate.getUTCDate() + ", " + fromDate.getFullYear();
+        result.from_date = fromDateString;
+
+        // reformatting the current data point date (to_date)
+        var toDate = new Date(result.to_date);
+        var toDateString = months[toDate.getUTCMonth()] + " " + toDate.getUTCDate() + ", " + toDate.getFullYear();
+        result.to_date = toDateString;
+
+        // splitting title into header and subheader
+        result.subheader = 'Prices';
+
+        // getting rounded percentage
+        var percentChange = 10000 * ((recentValue - previousValue) / Math.abs(previousValue));
+        percentChange = Math.round(percentChange);
+        percentChange /= 100;
+        result.change_percent = Math.abs(percentChange);
+
+        // setting style based on change
+        if (percentChange > 0) result.changeDirection = 'up';
+        if (percentChange < 0) result.changeDirection = 'down';
+        if (percentChange == 0) result.changeDirection = 'up';
+
+        // the most recent fundamental value
+        var value = recentValue;
+        if (Math.abs(value) >= Math.pow(10,12)) {
+            value = Math.round(value / Math.pow(10,10)) / Math.pow(10,2);
+            value = value + " trillion";
+        } else if (Math.abs(value) >= Math.pow(10,9)) {
+            value = Math.round(value / Math.pow(10,7)) / Math.pow(10,2);
+            value = value + " billion";
+        } else if (Math.abs(value) >= Math.pow(10,6)) {
+            value = Math.round(value / Math.pow(10,4)) / Math.pow(10,2);
+            value = value + " million";
+        } else if (Math.abs(value) >= Math.pow(10,3)) {
+            value = Math.round(value / Math.pow(10,1)) / Math.pow(10,2);
+            value = value + " thousand";
+        } else {
+            value = Math.round(value * 100) / 100;
+        }
+        result.value = value;
+
+
+        Spice.add({
+            id: 'quandl_commodities',
+            name: 'Commodities',
+            data: result,
+            meta: {
+                sourceName: 'Quandl',
+                sourceUrl: result.url,
+                sourceIconUrl:  DDG.get_asset_path('quandl/commodities','quandl32x32.png')
+            },
+            templates: {
+                group: 'base',
+                options: {
+                    content: Spice.quandl_commodities.content,
+                    moreAt: true
+                }
+            }
+        });
+    };
+}(this));
+
+
