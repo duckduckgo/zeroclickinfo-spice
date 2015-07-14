84020608fce11db790f63e08d8d3ec66b88c1039
alohaas <livhaas@gmail.com>
2015-07-13 21:09:28 -0400

    passing data to the template

--- share/spice/meetup/meetup.js
+++ share/spice/meetup/meetup.js
@@ -4,42 +4,7 @@
 
         if (!api_result || api_result.error) {
             return Spice.failed('meetup');
-        }
-        console.log(api_result);
-        // We don't want all of the meetup information
-        // Only take what we will use in the template
-        // We'll build a new object that looks like this
-        // meetupContent: {
-
-        // }÷
-        // var meetupContent = {},
-        //     meetupContent[meetups] = [],
-        //     meetupContent[queryResults] = api_result.meta.total_count,
-
-
-
-
-        // meetup: {
-        //   name: 'name',
-        //   category: category.name,
-        //   members: members,
-        //   nextEvent: next_event.name
-        // }
-
-        // function buildNewResultsArray(array){
-        //   array.forEach(function(){
-        //     var newMeetup = {};
-        //     // element, index, array
-        //     newMeetup[name] = element.name;
-        //     newMeetup[category] = element.category.name;
-        //     newMeetup[members] = element.members;
-        //     newMeetup[nextEvent] = element.next_event.name;
-        //     meetupContent.meetups.push(newMeetup);
-        //   });
-        // }
-
-
-
+        }  
 
         Spice.add({
           id: "meetup",
