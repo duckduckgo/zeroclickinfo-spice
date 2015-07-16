9f02b5987ed6679a27af424022dc6b084fc62ebc
Zach Thompson <zach@duckduckgo.com>
2015-07-16 07:19:04 -0600

    YogaAsanas.pm: add proximity match to query for sun salutations yoga_asanas.js: de-dupe results

--- share/spice/yoga_asanas/yoga_asanas.js
+++ share/spice/yoga_asanas/yoga_asanas.js
@@ -1,14 +1,36 @@
 function ddg_spice_yoga_asanas(apiResult) {
 
-    if (!apiResult || !apiResult.response.numFound) {
+    var numFound = apiResult.response.numFound,
+		docs = apiResult.response.docs,
+		i,
+		d,
+	    prev_title,
+		prev_srcName,
+		title,
+		srcName,
+		deDuped = [];
+
+    if (!apiResult || !numFound) {
         return Spice.failed('yoga_asanas');
     }
+
+	for(i = 0;i < numFound; i++){
+		console.log(docs[i]);
+		d = docs[i];
+		title = d.title;
+		srcName = d.meta.srcName;
+		if( (title != prev_title) || (srcName != prev_srcName) ){
+			deDuped.push(d);
+		}
+		prev_title = title;
+		prev_srcName = srcName;
+	}
 
 	Spice.add({
 		id: 'yoga_asanas',
 		name: 'Yoga Asanas',
 
-		data: apiResult.response.docs,
+		data: deDuped,
 		normalize: function(a){
 			var meta = eval("(" + a.meta + ")");
 			delete a.meta;
