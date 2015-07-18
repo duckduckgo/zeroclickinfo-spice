6d7d794f43a71443c6251c0345ee772310e63c9c
Zach Thompson <zach@duckduckgo.com>
2015-07-18 07:41:29 -0600

    YogaAsanas: add utf8 versions of some characters to make regex more robust

--- lib/DDG/Spice/YogaAsanas.pm
+++ lib/DDG/Spice/YogaAsanas.pm
@@ -2,30 +2,34 @@ package DDG::Spice::YogaAsanas;
 # ABSTRACT: Displays yoga asanas and sequences 
 
 use strict;
+use utf8;
 use DDG::Spice;
 
 spice to => 'http://jason.duckduckgo.com:8000/solr/staging/select?version=2.2&defType=edismax&qt=dismax&q=$1 source_match:yoga_asanas_api&fq=&wt=json&start=0&rows=75&sort=p_count+asc&json.wrf={{callback}}';
 
 triggers query_lc => qr{
-	(?:\w+asanas?\b)|
+	(?:\w+[aā]sanas?\b)|
 	(?:\w+sahita\b)|
 	(?:\bvinyasa\b)|
 	(?:\w+pitham\b)|
 	(?:samasthitih?)|
-	(?:stplutih??)|
+	(?:stplutih?)|
 	(?:postures?|poses?)|
 	(?:sun\s+salutation)|
-	(?:surya\s+namaskara)|
+	(?:s[uū]rya[\s-]+namask[aā]ra)|
 	(?:(?:ashtanga|yoga)\s+(primary|intermediate|advanced)\s+series)|
-	(?:yoga[\s-]+ch?ikitsa)|
+	(?:yoga[\s-]+ch?ikits[aā])|
 	(?:(?:standing|finishing)\s+(?:sequence|poses?|postures?))|
-	(?:nadi[\s-]+sh?odhana)|
+	(?:n[aā][ḍd]i[\s-]+[sś]h?odhana)|
 	(?:sthira[\s-]+bhaga)
-}xi;
+}x;
 
 handle query  => sub {
 	my $q = $_;
-	if(/(?:surya\s+namaskara)|(?:sun\s+salutation)/){
+
+	# This makes sure "a" or "b", if specified, is associated with
+	# surya namaskara/sun salutation
+	if(/(?:s[uū]rya[\s-]+namask[aā]ra)|(?:sun\s+salutation)/){
 		$q = "\"$q\"~5";
 	}
 	return $q;
