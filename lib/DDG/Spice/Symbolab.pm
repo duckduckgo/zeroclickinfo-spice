7ad1fc9dc54de88fb78c058ec9885a0438fab9e7
levaly <lev.alyshayev@gmail.com>
2015-07-22 13:33:53 +0000

    added changes

--- lib/DDG/Spice/Symbolab.pm
+++ lib/DDG/Spice/Symbolab.pm
@@ -9,11 +9,14 @@ description "Get a solution to a math problem from symbolab.com";
 primary_example_queries "compute integral of x^2", "solve x+2>3x";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Symbolab.pm";
 
-spice to => 'http://www.symbolab.com/ddg?query=$1';
+spice to => 'http://symhost.noip.us:9000/ddg?query=$1';
 spice wrap_jsonp_callback => 1;
 
-triggers start => "calculate", "solve", "compute";
-handle query => sub {
+triggers startend => "calculate", "solve", "compute";
+handle remainder => sub {
+    if ($_ eq "") {
+        return;
+    }
     return $_;
 };
 
