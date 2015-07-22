24a7411f3a2be7c40822f413830f4e4f38a3a42c
Zach Thompson <zach@duckduckgo.com>
2015-07-22 14:48:14 -0600

    Let dup do the de-duping

--- share/spice/yoga_asanas/yoga_asanas.js
+++ share/spice/yoga_asanas/yoga_asanas.js
@@ -1,42 +1,46 @@
 function ddg_spice_yoga_asanas(apiResult) {
-
-    var numFound = apiResult.response.numFound,
-        docs = apiResult.response.docs,
+    var numFound,
+        docs,
         i,
         seen = {},
         d,
         seen_key,
-        deDuped = [];
+        asanas = {};
 
-    if (!apiResult || !numFound) {
+    if (!apiResult || !apiResult.response.numFound) {
         return Spice.failed('yoga_asanas');
     }
 
-    //de-dupe by default
-    for(i = 0;i < numFound; i++){
-        d = docs[i];
-        d.meta = eval("(" + d.meta + ")");
-        seen_key = d.title + d.meta.srcName;
-        if(seen_key in seen){
-            continue;
-        }
-        else{
-            deDuped.push(d);
-            seen[seen_key] = true;
-        }
-    }
+    numFound = apiResult.response.numFound;
+    docs = apiResult.response.docs;
 
-    // unde-dupe if it's a sequence
-    if( (docs[docs.length - 1].meta.order - docs[0].meta.order) == (numFound - 1) ){
-        deDuped = docs;
-    }
-
-    Spice.add({
+    console.log("docs: ", docs);
+    console.log("meta 0: ". docs);
+    asanas = {
         id: 'yoga_asanas',
         name: 'Yoga Asanas',
 
-        data: deDuped,
-        normalize: function(a){
+        data: docs,
+        templates: {
+            group: 'media',
+            item_detail: false,
+            detail: false,
+            options: {
+                moreAt: true,
+                footer: Spice.yoga_asanas.footer
+            }
+        }
+    };
+
+    console.log("asanas.data: ", asanas.data);
+    console.log("order diff: ",docs[docs.length - 1].meta.order - docs[0].meta.order); 
+    console.log("numFound: ", numFound);
+    // if not a sequence de-dupe 
+    if(!((docs[docs.length - 1].meta.order - docs[0].meta.order) == (numFound - 1))){
+        asanas.relevancy = {dup: ['title', 'srcName']};
+    }
+
+    asanas.normalize =  function(a){
             var meta = a.meta;
             delete a.meta;
 
@@ -44,21 +48,12 @@ function ddg_spice_yoga_asanas(apiResult) {
                 image: meta.img,
                 title: a.title,
                 altSubtitle: a.paragraph,
+                url: meta.srcUrl,
                 srcName: meta.srcName,
                 srcUrl: meta.srcUrl,
                 srcIcon: meta.favicon
             };
-        },
+        };
 
-        templates: {
-            group: 'media',
-            item_detail: false,
-            detail: false,
-            options: {
-                moreAt: false,
-                footer: Spice.yoga_asanas.footer
-            }
-        },
-
-    });
+    Spice.add(asanas);
 }
