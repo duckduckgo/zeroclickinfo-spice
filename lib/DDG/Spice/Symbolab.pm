071e7214749eb494da3d22f3aa26009a6b8f76c3
Lev <lev@Levs-MacBook-Pro.local>
2015-07-22 16:40:15 +0300

    pointing to production server

--- lib/DDG/Spice/Symbolab.pm
+++ lib/DDG/Spice/Symbolab.pm
@@ -9,7 +9,7 @@ description "Get a solution to a math problem from symbolab.com";
 primary_example_queries "compute integral of x^2", "solve x+2>3x";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/Symbolab.pm";
 
-spice to => 'http://symhost.noip.us:9000/ddg?query=$1';
+spice to => 'https://www.symbolab.com/ddg?query=$1';
 spice wrap_jsonp_callback => 1;
 
 triggers startend => "calculate", "solve", "compute";
