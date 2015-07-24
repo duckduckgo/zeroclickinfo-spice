3f707bf8608279ae7530d628fd04ae613a776e44
Zach Thompson <zach@duckduckgo.com>
2015-07-24 15:11:30 -0600

    Update some trigger words; switch jason to longtail

--- lib/DDG/Spice/YogaAsanas.pm
+++ lib/DDG/Spice/YogaAsanas.pm
@@ -5,7 +5,7 @@ use strict;
 use utf8;
 use DDG::Spice;
 
-spice to => 'http://jason.duckduckgo.com:8000/solr/staging/select?version=2.2&defType=edismax&qt=dismax&q=$1+source_match:yoga_asanas_api&fq=&wt=json&start=0&rows=75&sort=p_count+asc&json.wrf={{callback}}';
+spice to => 'http://longtail.duckduckgo.com:8000/solr/staging/select?version=2.2&defType=edismax&qt=dismax&q=$1+source_match:yoga_asanas_api&fq=&wt=json&start=0&rows=75&sort=p_count+asc&json.wrf={{callback}}';
 
 triggers query_lc => qr{
 	(?:\w+[aā]sanas?\b)|
@@ -13,7 +13,7 @@ triggers query_lc => qr{
 	(?:\bvinyasa\b)|
 	(?:\w+pitham\b)|
 	(?:samasthitih?)|
-	(?:stplutih?)|
+	(?:tplutih?)|
 	(?:postures?|poses?)|
 	(?:sun\s+salutation)|
 	(?:s[uū]rya[\s-]+namask[aā]ra)|
@@ -27,8 +27,8 @@ triggers query_lc => qr{
 handle query  => sub {
 	my $q = $_;
 
-	# This makes sure "a" or "b", if specified, is associated with
-	# with either the practice or the posture, but not both 
+	# This makes sure "a" or "b", if specified, and the other query words
+	# are associated with with either a practice or a posture, but not both
 	return "\"$q\"~10";
 };
 
