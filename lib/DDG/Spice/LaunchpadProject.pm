4fac71ff2f5f7f124e60c567f62b4672d4f56838
Logan Garcia <logangarcia@fastmail.fm>
2015-07-29 17:47:46 -0700

    change the cache of LaunchpadProject.pm

--- lib/DDG/Spice/LaunchpadProject.pm
+++ lib/DDG/Spice/LaunchpadProject.pm
@@ -1,11 +1,9 @@
 package DDG::Spice::LaunchpadProject;
-# ABSTRACT: Searches Launchpad projects
+# ABSTRACT: Search for information about Launchpad projects
 
 use strict;
 use DDG::Spice;
 use Text::Trim;
-
-spice is_cached => 1;
 
 name "LaunchpadProject";
 description "Searches Launchpad projects";
@@ -18,6 +16,8 @@ attribution github => ['https://github.com/Feral2k/','Logan Garcia'],
 
 spice to => 'https://api.launchpad.net/devel/projects?ws.op=search&text=$1&ws.size=30';
 spice wrap_jsonp_callback => 1;
+
+spice proxy_cache_valid => '200 30d';
 
 triggers startend => "launchpad project", "lp project";
 
