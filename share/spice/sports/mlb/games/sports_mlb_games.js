8370010d0caea4bb0f06865ef73a0206b3140eef
Doug Brown <doug@duckduckgo.com>
2015-07-31 11:47:59 -0400

    update sports require in nfl and mlb, create overall sports stub

--- share/spice/sports/mlb/games/sports_mlb_games.js
+++ share/spice/sports/mlb/games/sports_mlb_games.js
@@ -9,7 +9,7 @@
             return Spice.failed('mlb_games');
         }
         
-        DDG.require('sportsGames', function(){
+        DDG.require('sports', function(){
             Games = env.ddg_spice_sports_games;
                 
             Games.init(MLBInit);
