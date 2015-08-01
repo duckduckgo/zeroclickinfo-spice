1699593a3a62a2069d455fb71972e3a76fa944bc
Zach Thompson <zach@duckduckgo.com>
2015-08-01 09:35:40 -0600

    Begin attempt to convert DDG::Goodie::CheatSheets to a spice using our longtail api

---
+++ lib/DDG/Spice/CheatSheets.pm
@@ -0,0 +1,15 @@
+package DDG::Spice::CheatSheets;
+# ABSTRACT: Load basic cheat sheets from cheat_sheet_api 
+
+use DDG::Spice;
+use DDP;
+
+spice to => 'http://jason.duckduckgo.com:8000/solr/staging/select?version=2.2&defType=edismax&qt=dismax&q=$1 source_match:cheat_sheets_api&fq=&wt=json&start=0&rows=2&json.wrf={{callback}}';
+
+triggers query => qr{^.+$};
+
+handle query_lc => sub {
+	return $_;
+};
+
+1;
