0776fdba5b5bf5f78938c9f7ae37df65857f3381
cskksc <ckoparkar@live.in>
2015-08-10 18:31:35 +0000

    Limit results to 30. Use ===.

--- lib/DDG/Spice/Hackage.pm
+++ lib/DDG/Spice/Hackage.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 
 triggers startend => 'haskell', 'hoogle', 'hackage';
 
-spice to => 'https://www.haskell.org/hoogle/?mode=json&hoogle=$1';
+spice to => 'https://www.haskell.org/hoogle/?mode=json&hoogle=$1&count=30';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
--- share/spice/hackage/hackage.js
+++ share/spice/hackage/hackage.js
@@ -5,55 +5,46 @@
 //     "self"=>"package JSON-Combinator",
 //     "docs"=> "A combinator library on top of a generalised JSON type.\n\nVersion 0.2.8 "}]}
 
+// get documentation string
+function docString(doc) {
+    return doc.replace(/Version .*/,"").trim();
+}
+
+// get version from docs.
+// supported version formats: [1.2, 1.2.3, 1.2.3.4]
+function version(doc){
+    var version = doc.match(/\d\.\d\.?\d?\.?\d?/);
+    return version === null ? undefined : version[0];
+}
+
+// converts "1.2.3" to 1230
+// converts "0.0.1" to 10
+// helps in comparing version strings in different format.
+function versionInt(v) {
+    if (v === undefined) {
+        return -1;
+    }
+    v = v.replace(/\./g,"")
+    // Assuming semantic version groups do not exceed 4
+    var toAppend = 4 - v.length;
+    v = v + Array(toAppend + 1).join("0");
+    return parseInt(v);
+}
+
+
 (function (env) {
     "use strict";
     env.ddg_spice_hackage = function(api_result) {
-
-        // get documentation string
-        function docString(doc) {
-            return doc.replace(/Version .*/,"");
-        }
-
-        // get version from docs.
-        // supported version formats: [1.2, 1.2.3, 1.2.3.4]
-        function version(doc){
-            try {
-                var version = doc.match(/\d\.\d\.?\d?\.?\d?/);
-                return version;
-            }
-            catch (err) {
-                return undefined;
-            }
-        }
-
-        // converts "1.2.3" to 1230
-        // converts "0.0.1" to 10
-        // helps in comparing version strings in different format.
-        function versionInt(v) {
-            if (v == undefined) {
-                return -1;
-            }
-            v = v.replace(/\./g,"")
-            // Assuming semantic version groups do not exceed 4
-            var toAppend = 4 - v.length;
-            v = v + Array(toAppend + 1).join("0");
-            return parseInt(v);
-        }
-
 
         if(!api_result || api_result.results.length == 0) {
             return Spice.failed('hackage');
         }
         var results = api_result.results;
-        if (results.length > 30)
-            results = results.splice(0,30);
-
 
         // Get the original query
         var script = $('[src*="/js/spice/hackage/"]')[0];
         var source = $(script).attr("src");
         var query = source.match(/hackage\/([^\/]*)/)[1];
-
         Spice.add({
             id: "hackage",
             name: "Hoogle",
@@ -61,21 +52,21 @@
             meta: {
                 sourceName: "Hoogle",
                 sourceUrl: "https://www.haskell.org/hoogle/?hoogle=" + query,
-                snippetChars: 85
             },
             templates: {
                 group: 'text',
                 detail: false,
                 item_detail: false,
                 variants: {
-                    tileTitle: "2line"
+                    tileTitle: "2line",
+                    tileSnippet: "small"
                 }
             },
 
             normalize : function(item) {
                 var v = version(item.docs);
                 var description = docString(item.docs);
-                if (description == "") {
+                if (description === "" || description.length < 2) {
                     return;
                 }
                 var info = {
@@ -91,8 +82,8 @@
 
             sort_fields: {
                 moreVersions: function(a, b) {
-                    var x = versionInt(version(a));
-                    var y = versionInt(version(b));
+                    var x = versionInt(version(a.docs));
+                    var y = versionInt(version(b.docs));
                     return ((x < y) ? 1 : ((x > y) ? -1 : 0));
                 }
             },
