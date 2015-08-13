1ba9ac4108cd296397b1ec131bdfc6fd53d27baa
cskksc <ckoparkar@live.in>
2015-08-13 04:17:35 +0000

    Remove 'package' from query. Avoid global functions.

--- share/spice/hackage/hackage.js
+++ share/spice/hackage/hackage.js
@@ -5,38 +5,38 @@
 //     "self"=>"package JSON-Combinator",
 //     "docs"=> "A combinator library on top of a generalised JSON type.\n\nVersion 0.2.8 "}]}
 
-// get documentation string
-function docString(doc) {
-    return doc.replace(/Version .*/,"").trim();
-}
-
-// get version from docs.
-// supported version formats: [1.2, 1.2.3, 1.2.3.4]
-function version(doc){
-    var version = doc.match(/\d\.\d\.?\d?\.?\d?/);
-    return version === null ? undefined : version[0];
-}
-
-// converts "1.2.3" to 1230
-// converts "0.0.1" to 10
-// helps in comparing version strings in different format.
-function versionInt(v) {
-    if (v === undefined) {
-        return -1;
-    }
-    v = v.replace(/\./g,"")
-    // Assuming semantic version groups do not exceed 4
-    var toAppend = 4 - v.length;
-    v = v + Array(toAppend + 1).join("0");
-    return parseInt(v);
-}
-
-
 (function (env) {
     "use strict";
+
+    // get documentation string
+    function docString(doc) {
+        return doc.replace(/Version .*/,"").trim();
+    }
+
+    // get version from docs.
+    // supported version formats: [1.2, 1.2.3, 1.2.3.4]
+    function version(doc){
+        var version = doc.match(/\d\.\d\.?\d?\.?\d?/);
+        return version === null ? undefined : version[0];
+    }
+
+    // converts "1.2.3" to 1230
+    // converts "0.0.1" to 10
+    // helps in comparing version strings in different format.
+    function versionInt(v) {
+        if (v === undefined) {
+            return -1;
+        }
+        v = v.replace(/\./g,"")
+        // Assuming semantic version groups do not exceed 4
+        var toAppend = 4 - v.length;
+        v = v + Array(toAppend + 1).join("0");
+        return parseInt(v);
+    }
+
     env.ddg_spice_hackage = function(api_result) {
 
-        if(!api_result || api_result.results.length == 0) {
+        if(!api_result.results || api_result.results.length == 0) {
             return Spice.failed('hackage');
         }
         var results = api_result.results;
@@ -68,7 +68,7 @@ function versionInt(v) {
                 var v = version(item.docs);
                 var description = docString(item.docs);
                 if (description === "" || description.length < 2) {
-                    return;
+                    return null;
                 }
                 var info = {
                     title: item.self,
--- t/Hackage.t
+++ t/Hackage.t
@@ -14,6 +14,26 @@ ddg_spice_test(
          '/js/spice/hackage/json',
          call_type => 'include',
          caller => 'DDG::Spice::Hackage'
+    ),
+    'haskell json package' => test_spice(
+         '/js/spice/hackage/json',
+         call_type => 'include',
+         caller => 'DDG::Spice::Hackage'
+    ),
+    'haskell package json' => test_spice(
+         '/js/spice/hackage/json',
+         call_type => 'include',
+         caller => 'DDG::Spice::Hackage'
+    ),
+    'haskell packages json' => test_spice(
+         '/js/spice/hackage/json',
+         call_type => 'include',
+         caller => 'DDG::Spice::Hackage'
+    ),
+    'haskell json packages' => test_spice(
+         '/js/spice/hackage/json',
+         call_type => 'include',
+         caller => 'DDG::Spice::Hackage'
     )
 );
 
