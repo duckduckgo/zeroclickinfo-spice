0fe82fc2f80f4a1f317e51e521b4e2f5c6380f40
Zach Thompson <zach@duckduckgo.com>
2015-08-17 14:44:20 -0600

    Use goodie triggers/remainder

--- lib/DDG/Spice/CheatSheets.pm
+++ lib/DDG/Spice/CheatSheets.pm
@@ -4,11 +4,25 @@ package DDG::Spice::CheatSheets;
 use DDG::Spice;
 use DDP;
 
-spice to => 'http://jason.duckduckgo.com:8000/solr/staging/select?version=2.2&defType=edismax&qt=dismax&q=$1 source_match:cheat_sheet_api&fq=&wt=json&start=0&rows=2&json.wrf={{callback}}';
+spice to => 'http://zach2.duckduckgo.com:8000/solr/staging/select?version=2.2&defType=edismax&qt=dismax&q=$1 source_match:cheat_sheet_api&fq=&wt=json&start=0&rows=2&json.wrf={{callback}}';
 
-triggers query => qr{^.+$};
+#triggers query => qr{^.+$};
 
-handle query_lc => sub {
+triggers startend => (
+    'char', 'chars',
+    'character', 'characters',
+    'cheat sheet', 'cheatsheet',
+    'command', 'commands',
+    'example', 'examples',
+    'guide', 'help',
+    'quick reference', 'reference',
+    'shortcut', 'shortcuts',
+    'symbol', 'symbols',
+    'key bindings', 'keys', 'default keys'
+);
+
+#handle query_lc => sub {
+handle remainder => sub {
 	return $_;
 };
 
