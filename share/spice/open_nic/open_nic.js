922fe5b06946f3cd97d88122b827202d11a66f9b
cylgom <cylgomppl@tutanota.de>
2015-08-18 09:27:50 +0000

    opennic spice addition

---
+++ share/spice/open_nic/open_nic.js
@@ -0,0 +1,28 @@
+(function (env) {
+    "use strict";
+    env.ddg_spice_open_nic = function(api_result){
+
+        if (!api_result || api_result.error) {
+            return Spice.failed('open_nic');
+        }
+
+        Spice.add({
+            id: "open_nic",
+            name: "OpenNIC DNS",
+            data: {
+                list: api_result
+            },
+            meta: {
+                sourceName: "OpenNIC",
+                sourceUrl: 'https://www.opennicproject.org'
+            },
+            templates: {
+                group: 'list',
+                options: {
+                    list_content: Spice.open_nic.content,
+                    moreAt: true
+                }
+            }
+        });
+    };
+}(this));
