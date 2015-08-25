1c454c31819bfa4705029fb1c564a381be4cde9d
Zach Thompson <zach@duckduckgo.com>
2015-08-25 14:57:04 -0600

    Do it the modern spicey way to satisfy Jag's "pretentious nitpick" :D

--- share/spice/yoga_asanas/yoga_asanas.js
+++ share/spice/yoga_asanas/yoga_asanas.js
@@ -1,46 +1,49 @@
+(function(env) {
+    "use strict";
 
-function ddg_spice_yoga_asanas(apiResult) {
+    env.ddg_spice_yoga_asanas = function(api_result){
 
-    if (!apiResult || !apiResult.response.numFound) {
-        return Spice.failed('yoga_asanas');
-    }
+        if (!api_result || !api_result.response.numFound) {
+            return Spice.failed('yoga_asanas');
+        }
 
-    var docs = apiResult.response.docs,
-        first = JSON.parse(docs[0].meta).order,
-        last = JSON.parse(docs[docs.length - 1].meta).order,
-        asanas = {
-            id: 'yoga_asanas',
-            name: 'Yoga Asanas',
-            data: docs,
-            normalize: function(item){
-                var meta = JSON.parse(item.meta);
-                // if not deleted will break "More at" links
-                delete item.meta;
+        var docs = api_result.response.docs,
+            first = JSON.parse(docs[0].meta).order,
+            last = JSON.parse(docs[docs.length - 1].meta).order,
+            asanas = {
+                id: 'yoga_asanas',
+                name: 'Yoga Asanas',
+                data: docs,
+                normalize: function(item){
+                    var meta = JSON.parse(item.meta);
+                    // if not deleted will break "More at" links
+                    delete item.meta;
 
-                return {
-                    image: meta.img,
-                    altSubtitle: item.paragraph,
-                    url: meta.srcUrl,
-                    srcName: meta.srcName,
-                    srcUrl: meta.srcUrl,
-                    srcIcon: meta.favicon
-                };
-            },
-            templates: {
-                group: 'media',
-                item_detail: false,
-                detail: false,
-                options: {
-                    moreAt: false,
-                    footer: Spice.yoga_asanas.footer
+                    return {
+                        image: meta.img,
+                        altSubtitle: item.paragraph,
+                        url: meta.srcUrl,
+                        srcName: meta.srcName,
+                        srcUrl: meta.srcUrl,
+                        srcIcon: meta.favicon
+                    };
+                },
+                templates: {
+                    group: 'media',
+                    item_detail: false,
+                    detail: false,
+                    options: {
+                        moreAt: false,
+                        footer: Spice.yoga_asanas.footer
+                    }
                 }
-            }
-        };
+            };
 
-    // if not a sequence de-dupe
-    if( (last - first) !== (apiResult.response.numFound - 1) ){
-        asanas.relevancy = { dup: ['title', 'srcName'] };
-    }
+        // if not a sequence de-dupe
+        if( (last - first) !== (api_result.response.numFound - 1) ){
+            asanas.relevancy = { dup: ['title', 'srcName'] };
+        }
 
-    Spice.add(asanas);
-}
+        Spice.add(asanas);
+    };
+}(this));
