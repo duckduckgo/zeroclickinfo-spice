dbd53be99b959be15c94b6810053853379243062
AcriCAA <acri.corey@gmail.com>
2015-08-25 18:13:11 +0000

    UPDATED.  I've updated the code to conform to the latest changes from @mariosimone except I have commented out the location query and have not changed the file names. It would be easier for me and make more sense to change the file names once everything else is working. I think the css is not being applied and I am sure how to link that to the IA layout.  There are also still numbers showing on the markers and I am not sure why. @jagtalon

--- share/spice/indego_bikeshare_locations/indego_bikeshare_locations.js
+++ share/spice/indego_bikeshare_locations/indego_bikeshare_locations.js
@@ -1,42 +1,43 @@
-(function (env) {
+(function(env) {
     "use strict";
+
+    var references = {
+       philadelphia: {
+            lat: 39.9452735, 
+            lon: -75.1485887      
+           
+           }
+    };
 
     env.ddg_spice_indego_bikeshare_locations = function(api_result) {
         if (!api_result || !api_result.features) {
             return Spice.failed('indego_bikeshare_locations');
         }
 
-        function normalize(item) {
-            
-            if (item.hasOwnProperty('properties') && item.hasOwnProperty('geometry')){
-            
-                if (item.geometry.length < 2 || item.geometry.length > 2 || item.properties.kioskPublicStatus !== 'Active') {
-                    return null;
-                }
-                var subtitle = item.properties.bikesAvailable + ' bike' + (item.properties.bikesAvailable  == 1 ? '' : 's') + ' | ' + item.properties.docksAvailable + ' dock' + (item.properties.docksAvailable == 1 ? '' : 's') ;
-                return {
-                    id: item.properties.kioskId,
-                    name: item.properties.name,            
-                    lat: (item.geometry.coordinates[1]).toString(),
-                    lon: (item.geometry.coordinates[0]).toString(),
-                    url: 'https://www.rideindego.com/stations/',
-                        data_front: {
-                            title: item.properties.name,
-                            altSubtitle: subtitle,
-                        },
-                        data_back: {
-                            title: item.properties.name,
-                            altSubtitle: subtitle,
-                            footer_content: Spice.indego_bikeshare_locations.foot_back,
-                            lastCommunication: moment(new Date(item.lastCommunicationTime)).fromNow()
-
-                        }
-                };
-            }
-       }
+       // var script = $('[src*="/js/spice/indego_bikeshare_locations/indego_bikeshare_locations/"]')[0],
+         //   source = $(script).attr("src"),
+        //    query = source.match(/indego_bikeshare_locations\/([^\/]+)/)[1];
 
         DDG.require('moment.js', function() {
             DDG.require('maps', function() {
+             moment.locale('en', {
+                    relativeTime : {
+                        past:   "%s ago",
+                        s:    "a sec",
+                        ss:   "%d sec",
+                        m:    "a min",
+                        mm:   "%d min",
+                        h:    "an hour",
+                        hh:   "%d hours",
+                        d:    "a day",
+                        dd:   "%d days",
+                        M:    "a month",
+                        MM:   "%d months",
+                        y:    "a year",
+                        yy:   "%d years"
+                    }
+                });    
+            
                 Spice.add({
                     id: 'indego_bikeshare_locations',
                     name: 'Indego Bike Share',
@@ -46,20 +47,41 @@
                         sourceUrl: 'http://www.rideindego.com/stations/',
                         itemType: 'station map'
                     },
-
                     model: 'Place',
                     view: 'Places',
                     templates: {
-                        group: 'places',
-                        item: 'basic_flipping_item',
-                        variants: {
-                            tile: 'basic4'
+                        item: 'base_item',
+                        options: {
+                            content: Spice.indego_bikeshare_locations.content
                         },
+                        variants: {
+                            tile: 'narrow'
+                        }
                     },
+                    sort_fields: {
+                        distance: function(a, b) {
+                            return a.distanceToReference - b.distanceToReference;
+                        }
+                    },
+                    sort_default: 'distance',
                     data: api_result.features,
-                    normalize: normalize        
+                    normalize: function(item) {
+                        if (item.geometry.length < 2 || item.geometry.length > 2 || item.properties.kioskPublicStatus !== 'Active') {
+                            return null;
+                        }
+                        return {
+                            name: item.properties.name,            
+                            lat: (item.geometry.coordinates[1]).toString(),
+                            lon: (item.geometry.coordinates[0]).toString(),    
+                          //  distanceToReference: pointOfReference ? L.latLng(item.latitude, item.longitude).distanceTo(pointOfReference) : item.properties.kioskId,
+                            title: item.properties.name,
+                            docksAvailable: item.properties.docksAvailable,
+                            bikesAvailable:item.properties.bikesAvailable,
+                            lastCommunication: moment(new Date(item.lastCommunicationTime)).fromNow()
+                        };
+                    }
                 });
             });
         });
-       }
+    }
 }(this));
