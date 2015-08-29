2c1df7e675b231bdb487ad4a8234cf68ce8c9157
alexander95015 <alexander95015@gmail.com>
2015-08-29 12:56:03 +0000

    polls ia

---
+++ share/spice/polls/polls.js
@@ -0,0 +1,58 @@
+(function (env) {
+    "use strict";
+    env.ddg_spice_polls = function(api_result){
+
+        // Validate the response (customize for your Spice)
+        if (!api_result || api_result.error) {
+            return Spice.failed('polls');
+        }
+        
+        var results = api_result;
+
+        if (!results) {
+            return Spice.failed('polls');
+        }
+        
+        if(results.length > 50){
+            results = results.splice(50);
+        }
+
+        // Render the response
+        DDG.require("moment.js", function() {
+        Spice.add({
+            id: "polls",
+
+            // Customize these properties
+            name: "Polls",
+            data: api_result,
+            meta: {
+                sourceName: "HuffPost Pollster",
+                sourceUrl: 'http://elections.huffingtonpost.com/pollster'
+            },
+            templates: {
+                group: 'text',
+                options: {
+                    detail: false,
+                    item_detail: false,
+                    footer: Spice.polls.footer,
+                }
+            },
+            normalize: function(item) {
+                    return {
+                        title: item.title,
+                        subtitle: item.poll_count + " polls",
+                        url: item.url,
+                        updateTime: moment(item.last_updated).fromNow()
+                    };
+                },
+            sort_fields: {
+                    last_updated: function(a, b) {
+                        var x = a.last_updated;
+                        var y = b.last_updated;
+                        return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+                    }
+                },
+            sort_default: 'last_updated'
+        }); });
+    };
+}(this));
