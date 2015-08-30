b02fa139d7dd8f928dd28d88e34e62b537e635bd
alexander95015 <alexander95015@gmail.com>
2015-08-30 01:06:13 +0000

    stylistic changes

--- share/spice/political_polls/political_polls.js
+++ share/spice/political_polls/political_polls.js
@@ -3,56 +3,53 @@
     env.ddg_spice_political_polls = function(api_result){
 
         // Validate the response (customize for your Spice)
-        if (!api_result || api_result.error) {
+        if (!api_result || api_result.length == 0 || api_result.error) {
             return Spice.failed('political_polls');
         }
         
-        var results = api_result;
-
-        if (!results) {
-            return Spice.failed('political_polls');
-        }
-        
-        if(results.length > 50){
+        if(api_result.length > 50){
             results = results.splice(50);
         }
 
         // Render the response
         DDG.require("moment.js", function() {
-        Spice.add({
-            id: "polls",
+            Spice.add({
+                id: "polls",
 
-            // Customize these properties
-            name: "Polls",
-            data: api_result,
-            meta: {
-                sourceName: "HuffPost Pollster",
-                sourceUrl: 'http://elections.huffingtonpost.com/pollster'
-            },
-            templates: {
-                group: 'text',
-                options: {
-                    detail: false,
-                    item_detail: false,
-                    footer: Spice.political_polls.footer,
-                }
-            },
-            normalize: function(item) {
-                    return {
-                        title: item.title,
-                        subtitle: item.poll_count + " polls",
-                        url: item.url,
-                        updateTime: moment(item.last_updated).fromNow()
-                    };
+                // Customize these properties
+                name: "Polls",
+                allowMultipleCalls: true;
+                data: api_result,
+                meta: {
+                    sourceName: "HuffPost Pollster",
+                    sourceUrl: 'http://elections.huffingtonpost.com/pollster',
+                    next: api_result.next,
                 },
-            sort_fields: {
-                    last_updated: function(a, b) {
-                        var x = a.last_updated;
-                        var y = b.last_updated;
-                        return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+                templates: {
+                    group: 'text',
+                    options: {
+                        detail: false,
+                        item_detail: false,
+                        footer: Spice.political_polls.footer,
                     }
                 },
-            sort_default: 'last_updated'
-        }); });
+                normalize: function(item) {
+                        return {
+                            title: item.title,
+                            subtitle: item.poll_count + " polls",
+                            url: item.url,
+                            updateTime: moment(item.last_updated).fromNow()
+                        };
+                    },
+                sort_fields: {
+                        last_updated: function(a, b) {
+                            var x = a.last_updated;
+                            var y = b.last_updated;
+                            return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+                        }
+                    },
+                sort_default: 'last_updated'
+            });
+        });
     };
 }(this));
