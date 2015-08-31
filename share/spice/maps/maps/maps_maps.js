ebc3418e1adce07790369fb2dbbf8d6538c6b240
b2 <b2@duckduckgo.com>
2015-08-31 10:51:19 -0700

    Handle mapbox geocoder response data

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -1,14 +1,10 @@
 DDG.require('maps',function(){
     ddg_spice_maps_maps = function(response) {
         
-        if (!response || !response.length) { return Spice.failed('maps'); }
+        if (!response) { return Spice.failed('maps'); }
 
         // OSM sends back a bunch of places, just want the first one for now
-        response = [response[0]]; 
-                                
-        if (!DDG.isRelevant(response[0].display_name.toLowerCase())) { 
-            return Spice.failed('maps'); 
-        } 
+        response = response.features[0]; 
         
         Spice.add({
             data: response,
