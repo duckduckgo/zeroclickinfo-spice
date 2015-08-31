a8b5e50855cd461289da45e26304ed10aa766c12
b2 <b2@duckduckgo.com>
2015-08-31 12:00:03 -0700

    remove callback param from request url

--- lib/DDG/Spice/Maps/Maps.pm
+++ lib/DDG/Spice/Maps/Maps.pm
@@ -4,7 +4,7 @@ package DDG::Spice::Maps::Maps;
 use strict;
 use DDG::Spice;
 
-spice to => 'http://api.mapbox.com/v4/geocode/mapbox.places/$1.json?access_token={{ENV{DDG_SPICE_MAPBOX_KEY}}}&callback={{callback}}';
+spice to => 'http://api.mapbox.com/v4/geocode/mapbox.places/$1.json?access_token={{ENV{DDG_SPICE_MAPBOX_KEY}}}';
 spice is_cached => 0;
 spice proxy_cache_valid => "418 1d";
 spice wrap_jsonp_callback => 1; 
