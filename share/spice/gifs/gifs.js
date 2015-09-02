4d5b23d3b6231b2c14e05dc221c85a4bce66bd77
Zaahir Moolla <moollaza@fastmail.fm>
2015-09-02 19:37:32 -0400

    undo accidental change to Gif Spice

--- share/spice/gifs/gifs.js
+++ share/spice/gifs/gifs.js
@@ -26,7 +26,7 @@
                     return null;
                 }
                 return {
-                    thumbnail: item.images.fixed_height.url,
+                    thumbnail: item.images.fixed_height_still.url,
                     image: item.images.fixed_height.url,
                     url: item.url,
                     height: item.images.fixed_height.height,
