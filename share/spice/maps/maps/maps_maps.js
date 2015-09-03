daf7b67af46df00cf092f0fdfe58c2c49ad41918
Tommy Leung <tommy@duckduckgo.com>
2015-09-03 17:19:10 -0400

    Modify maps ID

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -1,18 +1,18 @@
 DDG.require('maps',function(){
     ddg_spice_maps_maps = function(response) {
-        
+
         if (!response || !response.length) { return Spice.failed('maps'); }
 
         // OSM sends back a bunch of places, just want the first one for now
-        response = [response[0]]; 
-                                
-        if (!DDG.isRelevant(response[0].display_name.toLowerCase())) { 
-            return Spice.failed('maps'); 
-        } 
-        
+        response = [response[0]];
+
+        if (!DDG.isRelevant(response[0].display_name.toLowerCase())) {
+            return Spice.failed('maps_maps');
+        }
+
         Spice.add({
             data: response,
-            id: "maps",
+            id: "maps_maps",
             name: "maps",
             model: "Place"
         });
