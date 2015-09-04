c5c5ef7af2f7c75de1201415625187abfe6f8531
Tommy Leung <tommy@duckduckgo.com>
2015-09-04 00:38:01 -0400

    Rename maps_maps in Spice.add call

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -1,7 +1,7 @@
 DDG.require('maps',function(){
     ddg_spice_maps_maps = function(response) {
 
-        if (!response || !response.length) { return Spice.failed('maps'); }
+        if (!response || !response.length) { return Spice.failed('maps_maps'); }
 
         // OSM sends back a bunch of places, just want the first one for now
         response = [response[0]];
