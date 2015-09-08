a44db7187cb121a736462569ff8670d5bcc0ca94
b2 <b2@duckduckgo.com>
2015-09-08 17:11:47 -0400

    changing relevancy function from 0.9 to 0.75, adding caching

--- lib/DDG/Spice/Maps/Maps.pm
+++ lib/DDG/Spice/Maps/Maps.pm
@@ -5,7 +5,7 @@ use strict;
 use DDG::Spice;
 
 spice to => 'http://api.mapbox.com/v4/geocode/mapbox.places/$1.json?access_token={{ENV{DDG_SPICE_MAPBOX_KEY}}}';
-spice is_cached => 0;
+spice is_cached => 1;
 spice proxy_cache_valid => "418 1d";
 spice wrap_jsonp_callback => 1; 
 
