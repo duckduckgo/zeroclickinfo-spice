5ec64e204732b7e101fd1611f44a212ca501f871
Brian Stoner <brian@duckduckgo.com>
2015-10-19 17:51:52 -0400

    few more tweaks

--- share/spice/people_in_space/people_in_space.js
+++ share/spice/people_in_space/people_in_space.js
@@ -16,7 +16,7 @@
                     name: "Answer",
                     data: api_result.people,
                     meta: {
-                        primaryText: (api_result.number === 1 ? "Person in Space" : "People in Space"),
+                        primaryText: (api_result.number === 1 ? "1 Person Is In Space Right Now" : api_result.number + " People Are In Space Right Now"),
                         sourceName: "People in Space",
                         sourceUrl: "http://www.howmanypeopleareinspacerightnow.com/"
                     },
