590c4bd4543a041621a4d68348f7d1ca0921c07f
codenirvana <codenirvana28@gmail.com>
2015-11-03 01:59:04 +0000

    defined object outside

--- share/spice/rand_word/rand_word.js
+++ share/spice/rand_word/rand_word.js
@@ -12,45 +12,39 @@
             list_of_list.push(api_result.splice(0, 4));
         }
 
+        var spiceObj =  {
+            id: "rand_word",
+            data: {
+                title: 'Random Words',
+                list: list_of_list
+            },
+            name: "Answer",
+            meta: {
+                sourceUrl: 'https://wordnik.com/randoml',
+                sourceName: 'Wordnik',
+                sourceIcon: true
+            },
+            templates: {
+                group: 'list',
+                options: {
+                    list_content: Spice.rand_word.content,
+                    moreAt: true
+                }
+            }
+        };
+        
         if ( list_of_list.length == 1 ) {
-            Spice.add({
-                id: "rand_word",
-                data: list_of_list[0],
-                name: "Answer",
-                meta: {
-                    sourceUrl: 'https://wordnik.com/randoml',
-                    sourceName: 'Wordnik',
-                    sourceIcon: true
-                },
-                templates: {
-                    group: 'text',
-                    options: {
-                        content: Spice.rand_word.single_word,
-                        moreAt: true
-                    }
+            spiceObj.data = list_of_list[0];
+            spiceObj.templates = {
+                group: 'text',
+                options: {
+                    content: Spice.rand_word.single_word,
+                    moreAt: true
                 }
-            });
-        } else {
-            Spice.add({
-                id: "rand_word",
-                data: {
-                    title: 'Random Words',
-                    list: list_of_list
-                },
-                name: "Answer",
-                meta: {
-                    sourceUrl: 'https://wordnik.com/randoml',
-                    sourceName: 'Wordnik',
-                    sourceIcon: true
-                },
-                templates: {
-                    group: 'list',
-                    options: {
-                        list_content: Spice.rand_word.content,
-                        moreAt: true
-                    }
-                }
-            });
+            };
         }
+        
+        Spice.add( spiceObj );
+        
     }
 }(this));
