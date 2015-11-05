9f0b4c47509281bd9b7a3b2243d72006d3447721
Brian Buttrick <b2@duckduckgo.com>
2015-11-04 21:46:19 -0500

    adding pubmed wireframe

---
+++ lib/DDG/Spice/Pubmed.pm
@@ -0,0 +1,20 @@
+package DDG::Spice::Pubmed;
+
+use strict;
+use DDG::Spice;
+
+primary_example_queries 'pubmed crispr';
+description 'Search for pubmed for abstracts';
+name 'Pubmed';
+
+triggers startend => ('pubmed');
+
+spice to => 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=$1&retmode=json';
+
+spice wrap_jsonp_callback => 1;
+
+handle remainder_lc => sub {
+    return $_;
+};
+
+1;
