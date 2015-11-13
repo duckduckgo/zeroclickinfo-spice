fa9e0df68a66ac36a60e856e092f663d4633ae1a
Zaahir Moolla <moollaza@duckduckgo.com>
2015-11-12 19:54:19 -0500

    Show loading symbol while tile gif loads, on hover

--- share/spice/riffsy/riffsy.js
+++ share/spice/riffsy/riffsy.js
@@ -8,7 +8,7 @@
 
         var _shown = false,
             _gifs = {};
-        
+
         Spice.add({
             id: ianame,
             name: 'GIFs',
@@ -41,17 +41,53 @@
             },
             onShow: function() {
                 if (_shown) return;
-                $(".zci--"+ianame).find(".tile--img").each(function(){
+
+                var $dom = $(".zci--"+ianame);
+
+                // hide info overlay for gifs
+                $dom.find(".tile--img__details").hide();
+
+                // create loading gif image
+                var $loading = $("<img />")
+                    .attr("src", DDG.get_asset_path("riffsy", "loading.gif"))
+                    .addClass("tile__img--loading");
+
+                // inject our loading gif
+                $loading.insertBefore(".tile--img__media__i").hide();
+
+                // ensure we show/hide loading gif
+                // $img.one("load", function() {
+                //     $(this).closest(".tile__img--loading").hide();
+                // }).each(function() {
+                //     if (this.complete) {
+                //         console.log("YOLO");
+                //         $(this).load();
+                //     }
+                // });
+
+                $dom.find(".tile--img").each(function(){
                     var $img = $(this).find("img.tile--img__img"),
                         _src = $img.data("src"),
-                        _gif = _gifs[_src];
+                        _gif = _gifs[_src],
+                        $loading = $(this).find(".tile__img--loading");
+
+
+                    $img.load(function() {
+                        if ($(this).hasClass("gif")){
+                            $loading.hide();
+                        }
+                    });
 
                     // add callback to tile becase
                     // existing hover callback (for image details) was causing problems
                     $(this).hover(
                         function(){
+                            $loading.show();
                             $img.attr("src", _gif);
+                            $img.addClass("gif");
                         }, function(){
+                            $img.removeClass("gif");
+                            $loading.hide();
                             $img.attr("src", _src);
                         }
                     );
