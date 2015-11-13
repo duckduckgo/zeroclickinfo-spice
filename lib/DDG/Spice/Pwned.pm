75ce0b8facebc356def39d0328a4e7c6ddfaff51
Zaahir Moolla <moollaza@duckduckgo.com>
2015-11-12 23:23:25 -0500

    update design with larger snippert, footer and subtitle

--- lib/DDG/Spice/Pwned.pm
+++ lib/DDG/Spice/Pwned.pm
@@ -14,7 +14,7 @@ spice wrap_jsonp_callback => 1;
 spice error_fallback => 'Email address not found';
 
 handle remainder_lc => sub {
-    return $_ if $_ =~ /.+\@.+/;
+    return $_ if $_ =~ /\b([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,})\b/;
     return;
 };
 
