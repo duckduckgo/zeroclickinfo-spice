647f98c66216922f7a97654f991b0950dde700c1
levaly <lev.alyshayev@gmail.com>
2015-11-23 14:41:02 +0000

    - changed the triggers to regex trigger. - increased the test suite. - simple cases like '1+1' are no longer handled by symbolab - removed triggers for "solve, calculate, compute"

--- lib/DDG/Spice/Symbolab.pm
+++ lib/DDG/Spice/Symbolab.pm
@@ -12,12 +12,20 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/
 spice to => 'https://www.symbolab.com/ddg?query=$1';
 spice wrap_jsonp_callback => 1;
 
-triggers startend => "calculate", "solve", "compute";
-handle remainder => sub {
-    if ($_ =~ /[\^\*\=\+\-\/\\]/ or $_ =~ /\b(sin|cos|tan|cot|csc|sec|ln|log|sqrt|integral)/i) {
-        return $_;
+triggers query_lc => qr/\>|\<|\+|\^|\*|\=|\\|\/|\b(tan|sin|cos|csc|cot|sec|ln|log|sqrt|derivative|integral|limit)\b/;
+
+handle query_lc => sub {
+    my $query = $_;
+    
+    $query =~ s/(calculate|solve|compute) ?//;
+    
+    # checks if its a simple case that only has numbers, and math operations, in which case it assumes 
+    # DDG calculator goodie should handle this and returns
+    if ($query =~ /^(\d| |\+|\-|\*|\^|\/|sqrt|sin|cos|tan|cot|ln|log|sec|csc|\(|\))+$/){
+        return;
     }
-    return;
+    
+    return $query;
 };
 
 1;
