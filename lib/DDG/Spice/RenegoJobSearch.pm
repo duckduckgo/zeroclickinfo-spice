9fc2b62056500b9c35c82ef49a325fc9f62f9db5
Sebastian <cid@cid-notebook.localdomain>
2015-12-07 16:39:43 +0100

    [EDIT] fixed typo and added correct partner id in url

--- lib/DDG/Spice/RenegoJobSearch.pm
+++ lib/DDG/Spice/RenegoJobSearch.pm
@@ -8,7 +8,7 @@ spice is_cached => 1;
 spice proxy_cache_valid => "200 1d";
 spice wrap_jsonp_callback => 1;
 
-spice to => 'https://services.renego.net/jobs/ws.php?action=getjobs&sid=3&partnerid=38&what=$1&signkey={{ENV{DDG_SPICE_RENEGO_APIKEY}}}&blocksize=10&orderby=1&radius=25&offset=0&where=$2&fields=inserted|title|description_short|host|url_its|inserted|job_company_name|location_name&rt=json';
+spice to => 'https://services.renego.net/jobs/ws.php?action=getjobs&sid=3&partnerid=43&what=$1&signkey={{ENV{DDG_SPICE_RENEGO_APIKEY}}}&blocksize=10&orderby=1&radius=25&offset=0&where=$2&fields=inserted|title|description_short|host|url_its|inserted|job_company_name|location_name&rt=json';
 spice from => '(.*)/(.*)';
 
 triggers any => 'jobs', 'job';
@@ -19,7 +19,7 @@ description "Renego Job Search";
 name "RenegoJobSearch";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/RenegoJobSearch.pm";
 topics "special_interest";
-attribution github => ['https://github.com/mightycid','Sebastian Szeracki']
+attribution github => ['https://github.com/mightycid','Sebastian Szeracki'];
 
 # Handle statement
 handle remainder => sub {
