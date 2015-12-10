f95eccdb5d2e8dde5bc41e7f41d745266a72361d
Zaahir Moolla <moollaza@fastmail.fm>
2015-12-10 10:04:16 -0500

    PoliticilaPolls: Remove unneeeded comments

--- lib/DDG/Spice/PoliticalPolls.pm
+++ lib/DDG/Spice/PoliticalPolls.pm
@@ -3,19 +3,15 @@ package DDG::Spice::PoliticalPolls;
 
 use DDG::Spice;
 
-# Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching-api-responses
 spice is_cached => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "Political Polls";
 source "HuffPost Pollster";
 icon_url "http://elections.huffingtonpost.com/favicon.ico";
 description "Provides polling data from the United States.";
 primary_example_queries "US polls", "polls";
 secondary_example_queries "election polls";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
 category "reference";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
 topics "special_interest";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/PoliticalPolls.pm";
 attribution github => ["alexander95015", "Alexander"],
@@ -24,18 +20,11 @@ attribution github => ["alexander95015", "Alexander"],
 
 spice wrap_jsonp_callback => 1;
 
-# API endpoint - https://duck.co/duckduckhack/spice_attributes#spice-codetocode
 spice to => 'http://elections.huffingtonpost.com/pollster/api/charts.json?state=US';
 
-# Triggers - https://duck.co/duckduckhack/spice_triggers
 triggers startend => "poll", "polls";
 
-# Handle statement
 handle remainder => sub {
-
-    # optional - regex guard
-    # return unless qr/^\w+/;
-
     return $_;
 };
 
