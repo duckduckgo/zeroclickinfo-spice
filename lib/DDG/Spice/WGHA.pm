6584d0edfcdc2b2fe8fe876e83cf02c833f0f851
Sam <sam@bigcurl.de>
2015-12-20 08:39:33 +0100

    wgha spice: added cities to precheck on the ddg if a usefull result can be delivered

--- lib/DDG/Spice/WGHA.pm
+++ lib/DDG/Spice/WGHA.pm
@@ -15,7 +15,8 @@ category  "entertainment";
 attribution web =>   ['https://www.wasgehtheuteab.de','Samuel Goebert'],
             email => ['info@wasgehtheuteab.de','Samuel Goebert'];
 
-my @triggers = ("was geht", "wasgeht", "aus gehen", "ausgehen", "nachtleben", "party", "parties", "partys", "feiern", "abfeiern", "weg gehen","weggehen", "tanz", "tanzen", "veranstaltung", "veranstaltungen", "fest" );
+my @triggers = ("was geht", "wasgeht", "aus gehen", "ausgehen", "nachtleben", "party", "parties", "partys", "feiern", "abfeiern", "weg gehen","weggehen", "tanz", "tanzen", "veranstaltung", "veranstaltungen", "fest", "disko", "disco" );
+my $cities = "Aschaffenburg|Augsburg|Berlin|Berlin-Biesdorf|Berlin-Friedrichshain|Berlin-Karow|Berlin-Köpenick|Berlin-Lichtenberg|Berlin-Marzahn|Berlin-Mitte|Berlin-Oberschöneweide|Berlin-Pankow|Berlin-Treptow|Bremen|Cologne|Darmstadt|Dieburg|Dortmund|Dreieich|Dresden|Dresden-Altstadt|Dresden-Neustadt|Düsseldorf|Essen|FFM|Frankfurt|Frankfurt am Main|Freiburg|Freiburg im Breisgau|Friedrichshain|Griesheim|Halle|Hamburg|Hamburg-Eimsbüttel|Hamburg-Mitte|Hannover|Hanover|Heidelberg|Kassel|Koeln|Germany|Kreuzberg|Köln|Leipzig|Leipzig-Connewitz|Leipzig-Leutzsch|Leipzig-Plagwitz|Leipzig-Stötteritz|Mainz|Mannheim|Munich|Mühlheim|München|München|Germany|Münster|Neu Isenburg|Neukölln|Nuremberg|Nürnberg|Offenbach|Prenzlauer Berg|Sankt Pauli|Schöneberg|Sersheim|Stuttgart|Wiesbaden";
 
 triggers any => @triggers;
 
@@ -23,8 +24,15 @@ spice to => 'http://ddg.wasgehtheuteab.de/duckduckgo/events.json?q=$1&callback={
 spice proxy_cache_valid => "200 5m";
 
 handle query_lc => sub {
-  $a = $_ . ' ' . $loc->city;
-  return $a;
+  return if $loc->country_code ne 'DE';
+
+  my $q = $_ . ' ' . $loc->city; # attach city of query origin to query
+
+  if ($q =~ /$cities/i) {
+   return $q;
+ }
+
+ return;
 };
 
 1;
