94d4e4bcf3e8ccc0e3a20f56be583d72ee25ff3c
Chris Wilson <chris@chrisjwilson.com>
2016-01-05 09:38:23 +0000

    FlashVersion: Fix indentation

--- share/spice/flash_version/flash_version.js
+++ share/spice/flash_version/flash_version.js
@@ -1,34 +1,31 @@
 (function(env) {
     "use strict";
-
+    
     env.ddg_spice_flash_version = function() {
-
-	if(!FlashDetect) {
-	    return Spice.failed('flash_version');
-	}
-
-	// Display the plugin.
-	Spice.add({
+        
+        if(!FlashDetect) {
+            return Spice.failed('flash_version');
+        }
+        
+        Spice.add({
             data: {
-		installed: FlashDetect.installed,
-		raw: FlashDetect.raw
-	    },
-	    id: 'flash_version',
-	    name: 'Software',
-	    meta: {
-		sourceName: 'Adobe',
-		sourceUrl: 'https://get.adobe.com/flashplayer/',
-		sourceIcon: true
-	    },
+                installed: FlashDetect.installed,
+                raw: FlashDetect.raw
+            },
+            id: 'flash_version',
+            name: 'Software',
+            meta: {
+                sourceName: 'Adobe',
+                sourceUrl: 'https://get.adobe.com/flashplayer/',
+                sourceIcon: true
+            },
             templates: {
-		group: 'base',
-		options: {
-		    content: Spice.flash_version.content,
-		    moreAt: true
-		}
+                group: 'base',
+                options: {
+                    content: Spice.flash_version.content,
+                    moreAt: true
+                }
             }
-	});
+        });
     };
 }(this));
-
-ddg_spice_flash_version();
