cf14244c4afcad892177ae0457ee256c0866a35e
Chris Wilson <chris@chrisjwilson.com>
2016-01-05 09:44:25 +0000

    FlashVersion: Use DDG.require for 'flash_detect.js' file

--- share/spice/flash_version/flash_version.js
+++ share/spice/flash_version/flash_version.js
@@ -3,29 +3,33 @@
     
     env.ddg_spice_flash_version = function() {
         
-        if(!FlashDetect) {
-            return Spice.failed('flash_version');
-        }
-        
-        Spice.add({
-            data: {
-                installed: FlashDetect.installed,
-                raw: FlashDetect.raw
-            },
-            id: 'flash_version',
-            name: 'Software',
-            meta: {
-                sourceName: 'Adobe',
-                sourceUrl: 'https://get.adobe.com/flashplayer/',
-                sourceIcon: true
-            },
-            templates: {
-                group: 'base',
-                options: {
-                    content: Spice.flash_version.content,
-                    moreAt: true
-                }
+        DDG.require('flash_detect.js', function(){
+            
+            if(!FlashDetect) {
+                return Spice.failed('flash_version');
             }
+            
+            Spice.add({
+                data: {
+                    installed: FlashDetect.installed,
+                    raw: FlashDetect.raw
+                },
+                id: 'flash_version',
+                name: 'Software',
+                meta: {
+                    sourceName: 'Adobe',
+                    sourceUrl: 'https://get.adobe.com/flashplayer/',
+                    sourceIcon: true
+                },
+                templates: {
+                    group: 'base',
+                    options: {
+                        content: Spice.flash_version.content,
+                        moreAt: true
+                    }
+                }
+            });
+
         });
     };
 }(this));
