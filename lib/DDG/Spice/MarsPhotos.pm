de12812ab0da633c14ee426a0d698120eac8676f
jdorweiler <jason@transistor.io>
2016-01-12 19:57:05 +0000

    clean up comments

--- lib/DDG/Spice/MarsPhotos.pm
+++ lib/DDG/Spice/MarsPhotos.pm
@@ -1,29 +1,21 @@
 package DDG::Spice::MarsPhotos;
-
-# ABSTRACT: Write an abstract here
-# Start at https://duck.co/duckduckhack/spice_overview if you are new
-# to instant answer development
-
 use DDG::Spice;
 
-# Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching-api-responses
 spice is_cached => 1;
-spice proxy_cache_valid => "200 1d"; # defaults to this automatically
+spice proxy_cache_valid => "200 1d";
+spice wrap_jsonp_callback => 1;
 
-spice wrap_jsonp_callback => 1; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
-
-# API endpoint - https://duck.co/duckduckhack/spice_attributes#spice-codetocode
-spice to => 'https://api.nasa.gov/mars-photos/api/v1/rovers/$1/photos?earth_date=$2&api_key=166ZFfXJ7BZRsxlodV4qS6OAwBq5uREbEzre2Pyr';
+# get real key later
+spice to => 'https://api.nasa.gov/mars-photos/api/v1/rovers/$1/photos?earth_date=$2&api_key=DEMO_KEY';
 spice from => '([^/]+)/?(?:([^/]+)/?(?:([^/]+)|)|)';
 
-# Triggers - https://duck.co/duckduckhack/spice_triggers
 triggers any => 'mars images', 'mars photos';
 
-# Handle statement
 handle remainder => sub {
     my $remainder = $_;
     my ($rover) = $remainder =~ /(curiosity|spirit|opportunity)/i;
     $rover //= "curiosity";
+
     # date needs to be yyyy-mm-dd
     my $date = $remainder;
     $date =~ s/$rover//;
--- share/spice/mars_photos/mars_photos.js
+++ share/spice/mars_photos/mars_photos.js
@@ -1,13 +1,10 @@
 (function (env) {
     "use strict";
     env.ddg_spice_mars_photos = function(api_result){
-
-        // Validate the response (customize for your Spice)
         if (!api_result || api_result.error) {
             return Spice.failed('mars_photos');
         }
 
-        // Render the response
         Spice.add({
             id: "mars_photos",
             name: "Mars Photos",
@@ -17,7 +14,6 @@
                 sourceUrl: 'http://nasa.gov/url/to/details/' + api_result.name
             },
             normalize: function(item) {
-                   console.log(item);
                 return {
                     image: item.img_src,
                     url: item.img_src
