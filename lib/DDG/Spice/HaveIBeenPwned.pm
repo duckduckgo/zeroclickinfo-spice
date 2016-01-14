44bb60fe3973ba0142787129ca832d4b55f2103c
javathunderman <javathunderman@emposoft.tk>
2016-01-14 18:40:11 +0000

    remove metadata

--- lib/DDG/Spice/HaveIBeenPwned.pm
+++ lib/DDG/Spice/HaveIBeenPwned.pm
@@ -8,22 +8,9 @@ use strict;
 spice is_cached => 1;
 
 
-name "HaveIBeenPwned";
-source "http://haveibeenpwned.com";
-
-description "Returns whether or not your account was in a hacker data dump. ";
-primary_example_queries "have i been pwned, pwned, hibp";
-category "special";
-topics "geek";
-code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/HaveIBeenPwned.pm";
-attribution github => ["javathunderman", "Thomas Denizou"],
-            twitter => "javathunderman";
-
-spice to => 'https://haveibeenpwned.com/api/v2/breachedaccount/{account}';
-
-
 triggers any => "have i been pwned", "pwned", "hibp";
 
+spice to => 'https://haveibeenpwned.com/api/v2/breachedaccount/{account}';
 
 handle remainder => sub {
     my ($username) = @_;
