09048d6691272ad501b7ab020fbc09141e20009f
Sebastian Szeracki <mightyid@gmail.com>
2016-01-15 09:54:26 +0100

    [EDIT] changed number of results from 10 to 40

--- lib/DDG/Spice/RenegoJobSearch.pm
+++ lib/DDG/Spice/RenegoJobSearch.pm
@@ -8,7 +8,7 @@ spice is_cached => 1;
 spice proxy_cache_valid => "200 1d";
 spice wrap_jsonp_callback => 1;
 
-spice to => 'https://services.renego.net/jobs/ws.php?action=getjobs&sid=3&partnerid=43&what=$1&signkey={{ENV{DDG_SPICE_RENEGO_APIKEY}}}&blocksize=10&orderby=1&radius=25&offset=0&where=$2&fields=inserted|title|description_short|host|url_its|inserted|job_company_name|location_name&rt=json';
+spice to => 'https://services.renego.net/jobs/ws.php?action=getjobs&sid=3&partnerid=43&what=$1&signkey={{ENV{DDG_SPICE_RENEGO_APIKEY}}}&blocksize=40&orderby=1&radius=25&offset=0&where=$2&fields=inserted|title|description_short|host|url_its|inserted|job_company_name|location_name&rt=json';
 spice from => '(.*)/(.*)';
 
 triggers any => 'jobs', 'job', 'jobsuche', 'stellenangebot', 'stellenangebote', 'stellenanzeige', 'stellenanzeigen', 'stellenmarkt', 'arbeit', 'karriere';
