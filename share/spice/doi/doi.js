9904a92a2c86e85acc2472032c2a6dcca069e5d3
John Barrett <john@jbrt.org>
2016-01-19 17:41:14 +0000

    Removing trailing commas

--- share/spice/doi/doi.js
+++ share/spice/doi/doi.js
@@ -20,7 +20,7 @@
                 return {
                     url: item.URL,
                     title: item.title,
-                    subtitle: sprintf( '%s, %s, DOI:%s', format_authors(item.author), item.issued.raw, item.DOI ),
+                    subtitle: sprintf( '%s, %s, DOI:%s', format_authors(item.author), item.issued.raw, item.DOI )
                 }
             },
             templates: {
@@ -29,7 +29,7 @@
                     moreAt: true,
                     moreText: {
                         href: "/js/spice/doi_bibtex/" + api_result.DOI,
-                        text: 'BibTeX',
+                        text: 'BibTeX'
                     }
                 }
             }
