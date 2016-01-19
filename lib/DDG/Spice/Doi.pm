7325bd6070def67dbaa933c8ea2fd5981de3d80e
John Barrett <john@jbrt.org>
2016-01-19 17:27:23 +0000

    Use crossref.org
    (For now until we can support dx.doi.org's redirecting)

--- lib/DDG/Spice/Doi.pm
+++ lib/DDG/Spice/Doi.pm
@@ -5,10 +5,8 @@ use DDG::Spice;
 # Regex from http://stackoverflow.com/a/10324802/946226
 triggers query_lc => qr%\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])\S)+)\b%;
 
-# This would work better, but needs content negotiation
-spice to => 'http://dx.doi.org/$1';
+spice to => 'http://api.crossref.org/works/$1/transform/application/vnd.citationstyles.csl+json';
 spice wrap_jsonp_callback => 1;
-spice accept_header => 'application/vnd.citationstyles.csl+json';
 
 spice is_cached => 1;
 spice proxy_cache_valid => '200 7d';
