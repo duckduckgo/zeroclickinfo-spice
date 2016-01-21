43cdc3cafa950f53e66e80151543f860f534916b
Mailkov <miticomilko@hotmail.com>
2016-01-21 14:46:43 +0000

    set trigger words

--- lib/DDG/Spice/PackageTracking.pm
+++ lib/DDG/Spice/PackageTracking.pm
@@ -1,6 +1,7 @@
 package DDG::Spice::PackageTracking;
 
 use DDG::Spice;
+use YAML::XS 'LoadFile';
 
 spice is_cached => 1;
 spice proxy_cache_valid => "200 1d";
@@ -9,7 +10,9 @@ spice wrap_jsonp_callback => 1;
 
 spice to => 'https://api.packagetrackr.com/ddg/v1/track/simple?n=$1&api_key={{ENV{DDG_SPICE_PACKAGETRACKR_API_KEY}}}';
 
-triggers any => 'package';
+my $carriers = LoadFile(share('carriers.yml'));
+
+triggers any => @$carriers, 'package', 'track package', 'shipping status', 'package tracking';
 
 handle remainder => sub {
     return $_ if $_;
