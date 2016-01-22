1a855fa35a981751b4a4ed132e8332880aab48f5
John Barrett <john@jbrt.org>
2016-01-22 10:24:49 +0000

    Get original arxiv link from links array

--- share/spice/arxiv/arxiv.js
+++ share/spice/arxiv/arxiv.js
@@ -1,6 +1,7 @@
 (function (env) {
     "use strict";
     env.ddg_spice_arxiv = function(api_result){
+        var url = get_arxiv_rel_link( api_result.feed.entry.link );
 
         if (!api_result) {
             return Spice.failed('arxiv');
@@ -13,7 +14,7 @@
             data: api_result,
             meta: {
                 sourceName: "arxiv.org",
-                sourceUrl: api_result.feed.entry.link[0].href
+                sourceUrl: url
             },
             normalize: function(item) {
                 return {
@@ -21,7 +22,7 @@
                         item.feed.entry.title.text,
                         item.feed.entry.published.text.replace( /^(\d{4}).*/, "$1" )
                     ),
-                    url: item.feed.entry.link[0].href,
+                    url: url,
                     subtitle: item.feed.entry.author.map( function(e) {
                         return e.name.text;
                     } ).join(', '),
@@ -35,5 +36,16 @@
                 }
             }
         });
+
+        function get_arxiv_rel_link(links) {
+            for ( var link in links ) {
+                if ( links[link].rel === "alternate" && links[link].type === "text/html" ) {
+                    return links[link].href;
+                }
+            }
+
+            return links[0].href;
+        }
+
     };
 }(this));
