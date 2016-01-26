157ca8be2d789c96661c68dcaa0a87eb4bec1f1a
aforvo <alex@forvo.com>
2016-01-26 16:36:58 +0000

    Check the value of country and sex

--- share/spice/forvo/forvo.js
+++ share/spice/forvo/forvo.js
@@ -50,3 +50,26 @@ Handlebars.registerHelper("forvo_sex", function(sex) {
     return "Female";
 });
 
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
