ee26dae485539287416adf96215f0ad06b7e0394
olivia <olivia@duckduckgo.com>
2016-01-30 08:32:31 -0500

    magic update

--- share/spice/magic_the_gathering/magic_the_gathering.js
+++ share/spice/magic_the_gathering/magic_the_gathering.js
@@ -20,31 +20,51 @@
             normalize: function(item) {
                 if (item.name === DDG.get_query()){
                     item.exactMatch = true;
+
                 } 
                 var card_image = DDG.toHTTP(item.editions[0].image_url);
                 var infoboxData = [
                     { heading: "Card Details" },
                     { label: "Types", value: item.types },
                     { label: "Subtypes", value: item.subtypes },
-                    { label: "Colors", value: item.colors }
+                    { label: "Colors", value: item.colors },
+                    { label: "CMC", value: item.cmc },
+                    { label: "Cost", value: item.cost },
+                    { label: "Power", value: item.power },
+                    { label: "Toughness", value: item.toughness }
                 ];
                 return {
                     title: item.name,
                     description: item.text,
                     subtitle: item.editions[0].flavor,
-                    url: item.store_url,
+                    url: item.editions[0].store_url,
                     image: card_image,
                     infoboxData: infoboxData
                 };
             },
             templates: {
-                group: 'info',
+                group: "media",
                 options: {
-                    rating: false,
                     aux: true,
                     moreAt: true
                 }
             }
         });
     };
+    function pushArrayData(arr) {
+        
+    }
+    //function getInfoBoxData(item) {
+      //  var infoboxData = [{
+        //    heading: 'Card Details:'
+       // }];
+       // for (var i = 1; i <= 15; i++) {
+       //     if(item["strIngredient" + i] !== "") {
+       //         infoboxData.push({
+       //             label: item["strMeasure" + i] + "" + item["strIngredient" + i]
+       //         });
+       //     }
+      //  }
+      // return infoboxData;
+   // }
 }(this));
