108073564f450fc4af39a0fd56194c29f2e636ff
tyasaka <contact@tyleryasaka.me>
2016-01-30 04:11:41 +0000

    Changed template
    Also removed some extraneous code from WordOfTheDay.pm.
    
     Changes to be committed:
    
    	modified:   lib/DDG/Spice/WordOfTheDay.pm
    	new file:   share/spice/word_of_the_day/content.handlebars
    	new file:   share/spice/word_of_the_day/word_of_the_day.css
    	modified:   share/spice/word_of_the_day/word_of_the_day.js

--- lib/DDG/Spice/WordOfTheDay.pm
+++ lib/DDG/Spice/WordOfTheDay.pm
@@ -12,23 +12,6 @@ spice wrap_jsonp_callback => 1;
 # for testing, run: DDG_SPICE_WORDNIK_APIKEY=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5 duckpan server
 spice to => 'http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}';
 
-spice alt_to => {
-	audio => {
-		to => 'http://api.wordnik.com/v4/word.json/$1/audio?limit=10&useCanonical=false&api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}&callback={{callback}}',
-		proxy_cache_valid => '418 1d'
-	},
-	hyphenation => {
-		to => 'http://api.wordnik.com/v4/word.json/$1/hyphenation?includeRelated=true&api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}&callback={{callback}}'
-	},
-	pronunciation => {
-		to => 'http://api.wordnik.com/v4/word.json/$1/pronunciations?limit=1&useCanonical=false&api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}&callback={{callback}}'
-	},
-	reference => {
-		to => 'http://api.wordnik.com/v4/word.json/$1/definitions?includeRelated=true&includeTags=true&limit=3&api_key={{ENV{DDG_SPICE_WORDNIK_APIKEY}}}&callback={{callback}}',
-		proxy_cache_valid => '200 30d'
-	}
-};
-
 triggers start =>
     'word of the day',
     'dictionary word of the day',
--- share/spice/word_of_the_day/word_of_the_day.js
+++ share/spice/word_of_the_day/word_of_the_day.js
@@ -9,22 +9,54 @@
         Spice.add({
             id: "word_of_the_day",
             name: "Answer",
-            data: api_result,
+            data: {
+                word: api_result.word,
+                definitions: api_result.definitions
+            },
             meta: {
                 sourceUrl: 'https://www.wordnik.com/word-of-the-day',
                 sourceName: 'Wordnik'
             },
-            normalize: function(item) {
-                return {
-                    url: 'https://www.wordnik.com/word-of-the-day',
-                    title: api_result.word,
-                    subtitle: api_result.definitions[0].partOfSpeech,
-                    description: api_result.definitions[0].text
-                };
-            },
             templates: {
-                group: 'info'
+                group: 'base',
+                options: {
+                    content: Spice.word_of_the_day.content
+                }
             }
         });
     };
 }(this));
+
+// We should shorten the part of speech before displaying the definition.
+Spice.registerHelper("Dictionary_formatPartOfSpeech", function(text) {
+    "use strict";
+    
+    var parts_of_speech = {
+        "interjection": "interj.",
+        "noun": "n.",
+        "verb-intransitive": "v.",
+        "verb-transitive": "v.",
+        "adjective": "adj.",
+        "adverb": "adv.",
+        "verb": "v.",
+        "pronoun": "pro.",
+        "conjunction": "conj.",
+        "preposition": "prep.",
+        "auxiliary-verb": "v.",
+        "undefined": "",
+        "noun-plural": "n.",
+        "abbreviation": "abbr.",
+        "proper-noun": "n."
+    };
+
+    return parts_of_speech[text] || text;
+});
+
+// Make sure we replace xref to an anchor tag.
+// <xref> comes from the Wordnik API.
+Spice.registerHelper("Dictionary_formatDefinition", function(text) {
+    "use strict";
+
+    // Replace the xref tag with an anchor tag.
+    return text.replace(/<xref>([^<]+)<\/xref>/g,"<a class='reference' href='https://www.wordnik.com/words/$1'>$1</a>");
+});
