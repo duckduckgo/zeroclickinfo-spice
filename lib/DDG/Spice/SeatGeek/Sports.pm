8de5dfdcb53488de10b4f5f4e3168f4d14657e5b
Amir Sekhavati <amir.sekhavati@gmail.com>
2016-02-01 21:18:33 +0000

    SeatGeek: Change IA's that use the SeatGeek API to use HTTPS

--- lib/DDG/Spice/SeatGeek/Sports.pm
+++ lib/DDG/Spice/SeatGeek/Sports.pm
@@ -5,7 +5,7 @@ use DDG::Spice;
 
 triggers startend => 'upcoming matches', 'events', 'event', 'upcoming match', 'matches', 'sports', 'schedule', 'tickets', 'games';
 
-spice to => 'http://api.seatgeek.com/2/events?q=$1&taxonomies.name=sports&per_page=50&callback={{callback}}';
+spice to => 'https://api.seatgeek.com/2/events?q=$1&taxonomies.name=sports&per_page=50&callback={{callback}}';
 
 handle remainder_lc => sub {
     # Removes spaces from the beginning of the query
