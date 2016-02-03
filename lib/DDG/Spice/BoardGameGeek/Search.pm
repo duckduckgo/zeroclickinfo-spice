9c304c8c769d066227a5e676e6b8eb13b9efc2ac
Andrey Pissantchev <pissantchev@gmail.com>
2016-02-03 17:45:55 +0000

    BoargGameGeek: try and use year to disambiguate board games

--- lib/DDG/Spice/BoardGameGeek/Search.pm
+++ lib/DDG/Spice/BoardGameGeek/Search.pm
@@ -9,7 +9,7 @@ spice proxy_cache_valid => "200 7d";
 spice wrap_jsonp_callback => 1;
 
 # Need to escape string passed to &u= param, except $1
-my $search_url = uri_esc("http://www.boardgamegeek.com/xmlapi2/search?type=boardgame&exact=1&query=");
+my $search_url = uri_esc("http://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=");
 my $detail_url = uri_esc("http://www.boardgamegeek.com/xmlapi2/thing?stats=1&id=");
 
 spice to => 'https://duckduckgo.com/x.js?u=' . $search_url . '$1';
