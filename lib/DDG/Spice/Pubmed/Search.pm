3b66c331b1006c68fec4edc69740b48beb70952c
Brian Buttrick <b2@duckduckgo.com>
2016-02-03 10:38:12 -0500

    remove modules replaced by alt_to

--- lib/DDG/Spice/Pubmed/Search.pm
+++ lib/DDG/Spice/Pubmed/Search.pm
@@ -2,6 +2,7 @@ package DDG::Spice::Pubmed::Search;
 
 use strict;
 use DDG::Spice;
+use URI::Escape;
 
 primary_example_queries 'pubmed crispr';
 description 'Search for pubmed for abstracts';
@@ -10,6 +11,16 @@ name 'Pubmed';
 triggers startend => ('pubmed');
 
 spice to => 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=$1&retmode=json';
+
+spice alt_to => {
+    abstract => {
+        # handle XML
+        to => 'http://duckduckgo.com/x.js?u=' . uri_escape("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract&id=") . '$1'
+    },
+    ids => {
+        to => 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&retmax=10&id=$1'
+    }
+};
 
 spice wrap_jsonp_callback => 1;
 
