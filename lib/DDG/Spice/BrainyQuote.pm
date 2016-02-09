4161a5a8685d39a6d2aee679fa4bd1860b5d262f
Prateek Narendra <pnpninja@users.noreply.github.com>
2016-02-09 21:53:11 +0530

    Update BrainyQuote.pm
    Added description to denote that no conflict exists with other IAs

--- lib/DDG/Spice/BrainyQuote.pm
+++ lib/DDG/Spice/BrainyQuote.pm
@@ -17,6 +17,7 @@ handle remainder => sub {
     }
     
     # Avoid triggering on 'stock' quotes; these are handled by Stocks IA
+    # Also avoid triggering on 'quote of the day' and 'quote for the day'; these are handled by QuoteOfTheDay IA
     if ($req->query_lc =~ m/stock quote/ || $req->query_lc =~ m/quote of the day/ || $req->query_lc =~ m/quote for the day/ ) {
        return;
     }
