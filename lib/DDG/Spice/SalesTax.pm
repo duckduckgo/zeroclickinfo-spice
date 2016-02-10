65ce819ef945f70a586f04aa0dfcf9a0b19ebb54
ajain1 <ajain@jframeworks.com>
2016-02-10 21:47:55 +0000

    Commits for issue# 2497, Ref: https://github.com/duckduckgo/zeroclickinfo-spice/issues/2497

--- lib/DDG/Spice/SalesTax.pm
+++ lib/DDG/Spice/SalesTax.pm
@@ -4,41 +4,38 @@ package DDG::Spice::SalesTax;
 use strict;
 use DDG::Spice;
 use Locale::SubCountry;
-use YAML::XS 'LoadFile';
+#use YAML::XS 'LoadFile';
 
-triggers any => 'sales tax for', 'sales tax', 'sales tax in';
-spice from => '(.*)/(.*)';
-spice to => 'https://taxrates.api.avalara.com/postal?country=usa&postal=$1&apikey={{ENV{DDG_SPICE_SALESTAX_APIKEY}}}';
+triggers any => 'sales tax for', 'sales tax in';
+spice to => 'http://dev.snapcx.io:9100/taxv1/getStateTaxRate?request_id=DDG_Requestor&state=$1';
 spice wrap_jsonp_callback => 1;
 
 #Create US SubCountry object
 my $US = new Locale::SubCountry("US");
 
-#State to Zip Code
-my $zipcodes = LoadFile(share('zipcodes.yml'));
-
 # Handle statement
 handle remainder_lc => sub {
-    my ($state,$zip); #Define vars
+    my ($state, $stateCode); #Define vars
     s/^what is (the)?//g; # strip common words
-    return unless $_; # Guard against "no answer"
+    return '' unless $_; # Guard against "no answer"
 
     # Washington D.C is a district and is not supported by the SubCountry package.
     if(m/\b(washington\s(dc|d\.c))\b/i) {
         $state = "Washington D.C";
+        $stateCode = "DC";
     } else {
         # $US->full_name returns the full state name based on the ISO3166 code
+        $stateCode = $_;
         $state = $US->full_name($_); # Check for state using ISO code (PA)
         if($state eq "unknown") {
             $state = $US->full_name($US->code($_)); # If state is "unknown" search for code using full state name (Pennsylvania)
+            $stateCode = $US->code($_);
         }
     }
 
-    $zip = $zipcodes->{$state}; # state name=> zip code
-
     # error checking
-    return if $state eq "unknown";
-    return unless (defined $state and defined $zip);
-    return $zip, $state; # return result
+    return '' if $state eq "unknown";
+    return '' unless (defined $state and $stateCode);
+    return $stateCode; # return result
 };
 1;
--- share/spice/sales_tax/sales_tax.js
+++ share/spice/sales_tax/sales_tax.js
@@ -1,76 +1,57 @@
 (function (env) {
     "use strict";
     env.ddg_spice_sales_tax = function(api_result){
-
+        console.log("API Result Header.status is "+api_result.header.status);
+        console.log("Total infos object are "+api_result.stateSalesTaxInfos.length);
         // Error checking
-        if (!api_result || api_result.rates.length === 0) {
-            return Spice.failed('sales_tax');
+        if (!api_result || api_result.header.status !== "SUCCESS") {
+            return Spice.failed('sales_tax2');
         }
-
-        // Query    ZIP   STATE
-        // Example: 06101/Connecticut
-        var script = $('[src*="/js/spice/sales_tax/"]')[0],
-            source = $(script).attr("src"),
-            query = source.match(/sales_tax\/([^\/]+)\/([^\/]+)/);
-
-        // Declare vars
-        var stateName = decodeURIComponent(query[2]),
-            apiObjectName,
-            taxRate,
-            titleResult,
-            subtitleResult,
-            statNameTmp
-
-        // The API can return multiple rate results
-        // Loop through results:
-        // If the state name returned matches that of the state searched use the rate value
-        for (var i = 0; i < api_result.rates.length; i++) {
-            //Lowercase state names
-            apiObjectName = api_result.rates[i].name.toLowerCase();
-            statNameTmp = stateName.toLowerCase();
-            
-            if(apiObjectName == statNameTmp) {
-               taxRate = api_result.rates[i].rate;
-            // workaround washington d.c = district of columbia
-            } else if(apiObjectName == "district of columbia" && statNameTmp == "washington d.c") {
-                taxRate = api_result.rates[i].rate;
-            }
-
-        }
-
-        // Fail if we have no rate (edge case)
-        if (taxRate === undefined) {
-            return Spice.failed('sales_tax');
-        }
-
-        // title and subtitle
-        titleResult = taxRate+"%";
-        subtitleResult = stateName+" - "+"State Tax"
-
-        // Fix for sourceUrl washington d.c => washington-dc
-        // clean stateName for use as sourceUrl
-        stateName = stateName.replace(/\./,"").replace(/\s/,"-").toLowerCase();
 
         // Display
         Spice.add({
-            id: "sales_tax",
-            name: "Answer",
-            data: titleResult,
+            id: "snapcx_sales_tax",
+            name: "Finance",
+            data: api_result.stateSalesTaxInfos,
             meta: {
-                sourceName: "Avalara",
-                sourceUrl: 'http://www.taxrates.com/state-rates/' + stateName
+                //itemType: "Results",
+                //searchTerm: api_result.query,
+                sourceName: "snapCX.io",
+                sourceIcon: false,
+                sourceIconUrl: "http://snapcx.io/favicon.ico",
+                sourceUrl: "https://snapcx.io"
             },
             normalize: function(item) {
+                var stateName, titleResult, subtitleResult, minTaxRate, maxTaxRate, noTaxState;
+
+                stateName   = item.stateName;
+                minTaxRate  = (item.minimumTaxRate * 100).toFixed(2) +"%";
+                maxTaxRate  = (item.maximumTaxRate * 100).toFixed(2) +"%";
+                noTaxState  = item.noTaxState;
+                titleResult = stateName+" - "+"Sales Tax";
+                console.log("Title is "+titleResult);
+
+                if (noTaxState === false) {
+                    if (minTaxRate === maxTaxRate) {
+                        subtitleResult = "Sales Tax is "+minTaxRate;
+                    } else {
+                        subtitleResult = "Sale Tax ranges from "+minTaxRate+" to "+maxTaxRate;
+                    }
+                } else {
+                    subtitleResult = "No sales tax for this state";
+                }
                 return {
                     title: titleResult,
-                    subtitle: subtitleResult
+                    description: subtitleResult                    
                 };
             },
             templates: {
                 group: 'text',
                 options: {
-                    moreAt: true
-                }
+                    moreAt: true                    
+                },
+                detail: false,
+                item_detail: false
             }
         });
     }
