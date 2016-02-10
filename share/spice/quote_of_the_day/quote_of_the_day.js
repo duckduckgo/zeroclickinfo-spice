1e5cc1894b9db2dd2e0e1e60397db20254677c30
Prateek Narendra <pnpninja@users.noreply.github.com>
2016-02-10 21:04:26 +0530

    Update quote_of_the_day.js
    Removed all auto generated comments

--- share/spice/quote_of_the_day/quote_of_the_day.js
+++ share/spice/quote_of_the_day/quote_of_the_day.js
@@ -2,16 +2,13 @@
     "use strict";
     env.ddg_spice_quote_of_the_day = function(api_result) {
 
-        // Validate the response (customize for your Spice)
         if (!api_result || api_result.error) {
             return Spice.failed('quote_of_the_day');
         }
 
-        // Render the response
         Spice.add({
             id: "quote_of_the_day",
 
-            // Customize these properties
             name: "Quote of the Day",
             data: api_result,
             meta: {
@@ -20,7 +17,6 @@
             },
             normalize: function(item) {
                 return {
-                    // customize as needed for your chosen template
                     quote : item.contents.quotes[0].quote,
                     person: item.contents.quotes[0].author,
                     len : parseInt(item.contents.quotes[0].length) + item.contents.quotes[0].author.length
