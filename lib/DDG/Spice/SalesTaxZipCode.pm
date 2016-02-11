c104708203463f0a1e52b39d22ff92d1d4d86149
ajain1 <ajain@jframeworks.com>
2016-02-11 20:02:44 +0000

    Added test file., removed log statements and commented out code.

--- lib/DDG/Spice/SalesTaxZipCode.pm
+++ lib/DDG/Spice/SalesTaxZipCode.pm
@@ -1,5 +1,5 @@
 package DDG::Spice::SalesTaxZipCode;
-#ABSTRACT: Returns the sales tax for any US 5 digit zipcode. If query has 5 digit zipcode and other trigger word, then it will process.
+#ABSTRACT: Returns the sales tax for any US 5 digit zipcode. If query has trigger words and 5 digit zipcode then it will process.
 
 use strict;
 use DDG::Spice;
@@ -9,14 +9,11 @@ triggers any => 'sales tax for', 'sales tax in', 'sales tax';
 spice to => 'http://dev.snapcx.io:9100/taxv1/salesTaxInfo?request_id=DDG_Requestor&zipcode=$1';
 spice wrap_jsonp_callback => 1;
 spice is_cached => 1;
-#my $zipCodeRegEX = "^[0-9]{5}$";
-#Create US SubCountry object
-my $US = new Locale::SubCountry("US");
 
 # Handle statement
 handle remainder_lc => sub {
-    my ($zipcode); #Define vars
     s/^what is (the)?//g; # strip common words
+    #Only process 5 digit keyword only. 
     return $_ if $_ =~ /^[0-9]{5}$/;
     return;
 };
