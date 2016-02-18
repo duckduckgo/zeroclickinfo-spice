311a0fde1919c43361eeb0239db7315b8f7ff7b1
rsilvola <codio@rsilvola.com>
2016-02-18 15:59:04 +0000

    Fix indentation

--- share/spice/justdeleteme/justdeleteme.js
+++ share/spice/justdeleteme/justdeleteme.js
@@ -4,18 +4,18 @@
         if (!api_result || api_result.length < 1) {
             return Spice.failed('justdeleteme');
         }
-        
+
         // get the remainder
         var script = $('[src*="/js/spice/justdeleteme/"]')[0],
-        source = $(script).attr("src"),
-        query = source.match(/justdeleteme\/([^\/]+)/)[1],
-        decodedQuery = decodeURIComponent(query).split(" ")[0].toLowerCase();
-//         console.log("decodedQuery: " + decodedQuery);
-        
+            source = $(script).attr("src"),
+            query = source.match(/justdeleteme\/([^\/]+)/)[1],
+            decodedQuery = decodeURIComponent(query).split(" ")[0].toLowerCase();
+        //         console.log("decodedQuery: " + decodedQuery);
+
         var isRelevantName = function(item) {
             if (!item.name)
                 return false;
-			if (item.name.toLowerCase().contains(decodedQuery))
+            if (item.name.toLowerCase().contains(decodedQuery))
                 return true;   
             return false;
         };
@@ -27,21 +27,18 @@
                     return true;
             return false;
         };
-        
+
         var short_list = api_result.filter(isRelevantName);
-         if (short_list.length < 1) {            
-             var short_list = api_result.filter(isRelevantDomain);
-         }
+        if (short_list.length < 1) {            
+            var short_list = api_result.filter(isRelevantDomain);
+        }
         if (short_list.length < 1) {
             return Spice.failed('justdeleteme');
         }
-        
-        // Render the response
+
         Spice.add({
             id: "justdeleteme",
-
-            // Customize these properties
-            name: "Reference",
+            name: "Answer",
             meta: {
                 sourceName: 'Just Delete Me',
                 sourceUrl: "http://justdelete.me/#" + decodedQuery
@@ -59,7 +56,7 @@
                 item: 'text_item',
                 detail: 'basic_info_detail',
                 options: {
-		            content: Spice.justdeleteme.jdm_item_detail,
+                    content: Spice.justdeleteme.jdm_item_detail,
                     moreAt: true,
                     aux: false
                 }
