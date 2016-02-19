ad153991e061ae4c8ff52cbf92176f43aaf8fb97
HunterMeyer <huntermeyer@live.com>
2016-02-18 23:42:50 -0500

    Remove extra lines

--- lib/DDG/Spice/JrDevJobs.pm
+++ lib/DDG/Spice/JrDevJobs.pm
@@ -6,9 +6,7 @@ use DDG::Spice;
 trigger any => 'junior developer jobs', 'jobs for jr developer', 'entry level software jobs';
 
 spice to => 'https://www.jrdevjobs.com/jobs.json?query=$1';
-
 spice is_cached => 1;
-
 spice wrap_jsonp_callback => 1;
 
 handle query => sub {
