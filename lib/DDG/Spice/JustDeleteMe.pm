f225089b2d9a42fdfea43596fd1be312f58808ad
rsilvola <codio@rsilvola.com>
2016-02-19 13:04:31 +0000

    Trigger account can now be anywhere

--- lib/DDG/Spice/JustDeleteMe.pm
+++ lib/DDG/Spice/JustDeleteMe.pm
@@ -7,16 +7,42 @@ spice is_cached => 1;
 spice wrap_jsonp_callback => 1;
 spice to => 'https://raw.githubusercontent.com/rmlewisuk/justdelete.me/master/sites.json';
 
+my $MIN_QUERY_LENGTH = 4;
 my @tr = qw (delete cancel remove);
-my @my = map {"$_ my"} @tr;
-my @acc = map { "$_ account" } (@tr, @my);
-my @on = map {"$_ on"} @acc;
-my @all = (@my, @acc, @on);
-triggers any => @all;
+my @pred = qw (my on);
+my @req = qw (account accounts);
+triggers any => @tr;
 
-handle remainder => sub {
-    $_ =~ s/,//ig;
-    return unless $_;
+handle query_parts => sub {
+    my $query = validate(@_);
+    if (length($query) < $MIN_QUERY_LENGTH) {
+        return;
+    }
+    else {
+        return $query;
+    }
 };
+
+sub validate {
+    my @qparts = @_;
+    my @trigs = (@tr, @pred);
+    my @required;
+    # strip triggers and preds
+    for (my $i = 0; $i<scalar(@trigs); $i++) {
+        @qparts = grep(!/^($trigs[$i])$/i, @qparts);
+    }
+    # check for required words
+    for (my $i = 0; $i<scalar(@req); $i++) {
+        @required = (@required, grep(/^($req[$i])$/i, @qparts));
+    }
+    #strip required words and return rest as string
+    if (scalar(@required) > 0) {
+        for (my $i = 0; $i<scalar(@req); $i++) {
+            @qparts = grep(!/^($req[$i])$/i, @qparts);
+        }
+        return join(" ", @qparts);
+    }
+    return "";
+}
 
 1;
