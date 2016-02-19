e392fd72bcb194c8213deacc4f270c2c2c71e353
rsilvola <codio@rsilvola.com>
2016-02-19 13:46:08 +0000

    Compare strings in same case

--- share/spice/just_delete_me/just_delete_me.js
+++ share/spice/just_delete_me/just_delete_me.js
@@ -15,7 +15,7 @@
                     }),
             api_result = exact ? [exact] : api_result.filter(function (item) {
                         item.domains =  !item.domains ? [] : typeof(item.domains) === "string" ? [item.domains] : item.domains;
-                        if (item.name.contains(decodedQuery) || item.domains.some(function(domain) {return domain.contains(decodedQuery);}))
+                        if (item.name.toLowerCase().contains(decodedQuery) || item.domains.some(function(domain) {return domain.toLowerCase().contains(decodedQuery);}))
                             return true;
                         });
 
