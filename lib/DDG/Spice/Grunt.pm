4f51d38feed2aa370b89f546ec815ae9289e6a2f
jagtalon <jag@jagtalon.com>
2016-02-22 13:02:32 +0000

    Grunt: switch from "any" triggers to "startend"

--- lib/DDG/Spice/Grunt.pm
+++ lib/DDG/Spice/Grunt.pm
@@ -6,7 +6,7 @@ spice is_cached => 1;
 spice wrap_jsonp_callback => 1;
 spice to => 'http://gruntjs.com/plugin-list.json';
 
-triggers any => 'grunt plugin', 'grunt plugins', 'gruntjs', 'gruntjs plugin', 'gruntjs plugins', 'grunt.js plugin', 'grunt.js plugins';
+triggers startend => 'grunt plugin', 'grunt plugins', 'gruntjs', 'gruntjs plugin', 'gruntjs plugins', 'grunt.js plugin', 'grunt.js plugins';
 
 handle remainder => sub {
     return $_ if length $_;
