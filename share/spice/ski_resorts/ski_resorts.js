a52afb1dd4849bf9827ce540c8367ad63c2755c5
fpalmer <felixpalmer@gmail.com>
2016-02-25 14:17:24 +0000

    Clean up to use DDG style

--- share/spice/ski_resorts/ski_resorts.js
+++ share/spice/ski_resorts/ski_resorts.js
@@ -6,9 +6,6 @@
         if (!api_result || api_result.error) {
             return Spice.failed('ski_resorts');
         }
-
-        var lon = (Math.round(api_result.location[0] * 100) / 100).toFixed(2);
-        var lat = (Math.round(api_result.location[1] * 100) / 100).toFixed(2);
     
         DDG.require('maps', function() {
             Spice.add({
@@ -20,23 +17,27 @@
                     item: 'places_item',
                     detail: 'places_detail'
                 },
-                data: [{
-                    id: api_result.name,
-                    name: api_result.title,
-                    url: 'http://www.piste.io/' + api_result.name,
-                    image: 'http://www.piste.io/preview/' + api_result.name + '.jpg',
-                    ratingImageURL: 'http://www.piste.io/favicon.ico',
-                    address_lines: [
-                        'Ski resort in ' + api_result.countryName,
-                        '32 novice runs',
-                        '16 intermediate runs',
-                        '8 expert runs'
-                    ],
-                    city: 'New York City',
+                data: api_result,
+                normalize: function(data){
+                    return {
+                        id: data.name,
+                        name: data.title,
+                        url: 'http://www.piste.io/' + data.name,
+                        image: 'http://www.piste.io/preview/' + data.name + '.jpg',
+                        ratingImageURL: 'http://www.piste.io/favicon.ico',
+                        //address: 'Hi there', 
+                        address_lines: [
+                            'Ski resort in ' + data.countryName,
+                            '32 novice runs',
+                            '16 intermediate runs',
+                            '8 expert runs'
+                        ],
+                        city: 'New York City',
 
-                    lon: lon,
-                    lat: lat
-                }],
+                        lon: (Math.round(data.location[0] * 100) / 100).toFixed(2),
+                        lat: (Math.round(data.location[1] * 100) / 100).toFixed(2)
+                    }
+                },
                 meta: {
                     zoomLevel: 8,
                     sourceName: "Show piste map",
