0958363130974bf0b44399ff19c8c1783fd8d1ec
Amir Sekhavati <amir.sekhavati@gmail.com>
2016-02-27 19:02:39 +0000

    Specify version in requests made to timeanddate.com API

--- lib/DDG/Spice/Time.pm
+++ lib/DDG/Spice/Time.pm
@@ -7,7 +7,7 @@ use Text::Trim;
 use YAML::XS 'LoadFile';
 
 spice proxy_cache_valid => "418 1d";
-spice to => 'http://api.xmltime.com/timeservice?accesskey={{ENV{DDG_SPICE_TIME_AND_DATE_ACCESSKEY}}}&secretkey={{ENV{DDG_SPICE_TIME_AND_DATE_SECRETKEY}}}&out=js&callback={{callback}}&query=$1&time=1&tz=1&verbosetime=1';
+spice to => 'http://api.xmltime.com/timeservice?accesskey={{ENV{DDG_SPICE_TIME_AND_DATE_ACCESSKEY}}}&secretkey={{ENV{DDG_SPICE_TIME_AND_DATE_SECRETKEY}}}&out=js&callback={{callback}}&query=$1&time=1&tz=1&verbosetime=1&version=1';
 
 triggers any => "time";
 
