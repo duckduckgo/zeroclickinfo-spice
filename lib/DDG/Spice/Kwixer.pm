d84b4e8f938a9f1dd5ed620a856959fe87b49544
amoraleda <alvaro.moraleda@gmail.com>
2016-03-01 03:17:10 +0000

    Added 'end' trigger and simplified trigger words

--- lib/DDG/Spice/Kwixer.pm
+++ lib/DDG/Spice/Kwixer.pm
@@ -7,9 +7,12 @@ use DDG::Spice;
 spice to => 'https://www.kwixer.com/api/search?filter=movie&take=40&source=ddg&lang=en&query=$1';
 spice wrap_jsonp_callback => 1;
 
-my @triggers = share("triggers.txt")->slurp;
+my @base_words = map { ("$_", "new $_", "newest $_") } ("movie", "movies", "film", "films");
+my @triggers = map { ("$_", "$_ with", "$_ featuring", "$_ starring", "$_ by", "$_ directed by", "$_ director") } @base_words;
+
 triggers start => @triggers;
-#triggers end => ('actor','actress', 'director');
+triggers end => @base_words;
+
 handle query => sub {
     return $_ if $_;
     return;
