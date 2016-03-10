dba8fbfdb3429fbb3b7c1e4e695ece49eb791b8f
Mailkov <miticomilko@hotmail.com>
2016-03-10 00:04:11 +0000

    fix null value and ups-package logo

--- share/spice/package_tracking/package_tracking.js
+++ share/spice/package_tracking/package_tracking.js
@@ -4,6 +4,10 @@
         if (!api_result || api_result.error) {
             return Spice.failed('package_tracking');
         }
+        var logo = api_result.c;
+        if (logo == 'ups-packages') {
+            logo = 'ups';
+        }
         Spice.add({
             id: "package_tracking",
             name: "Answer",
@@ -11,22 +15,22 @@
                 title: api_result.status_description,  
                 subtitle: api_result.location,
                 description: "Last update: " + DDG.getDateFromString(api_result.progress_at),
-                image: DDG.get_asset_path('package_tracking', 'logos/' + api_result.c + '.png'),
+                image: DDG.get_asset_path('package_tracking', 'logos/' + logo + '.png'),
                 infoboxData: [
                     {
                         heading: "Info shipping"
                     },
                     {
                         label: "Shipped at: ",
-                        value: DDG.getDateFromString(api_result.shipped_at)
+                        value: api_result.shipped_at != null ? DDG.getDateFromString(api_result.shipped_at) : ""
                     },
                     {
                         label: "Scheduled delivery at: ",
-                        value: DDG.getDateFromString(api_result.est_delivery_at)
+                        value: api_result.est_delivery_at != null ? DDG.getDateFromString(api_result.est_delivery_at) : ""
                     },
                     {
                         label: "Delivery at: ",
-                        value: DDG.getDateFromString(api_result.act_delivery_at)
+                        value:  api_result.act_delivery_at != null ? DDG.getDateFromString(api_result.act_delivery_at) : ""
                     },
                 ]
             },
