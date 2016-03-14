080b771dc9ccd7defff10907525e36560b2b251a
Mailkov <miticomilko@hotmail.com>
2016-03-14 16:54:06 +0000

    add use of moment.js

--- share/spice/package_tracking/package_tracking.js
+++ share/spice/package_tracking/package_tracking.js
@@ -1,6 +1,6 @@
 (function (env) {
     "use strict";
-    env.ddg_spice_package_tracking = function(api_result){
+    env.ddg_spice_package_tracking = function(api_result){ 
         if (!api_result || api_result.error) {
             return Spice.failed('package_tracking');
         }
@@ -8,35 +8,37 @@
         if (logo == 'ups-packages') {
             logo = 'ups';
         }
-        Spice.add({
-            id: "package_tracking",
-            name: "Answer",
-            data: {
-                title: api_result.status_description,  
-                subtitle: api_result.location,
-                description: "Last update: " + DDG.getDateFromString(api_result.progress_at),
-                image: DDG.get_asset_path('package_tracking', 'logos/' + logo + '.png'),
-                infoboxData: [
-                    {
-                        heading: "Info shipping"
-                    },
-                    {
-                        label: "Shipped at: ",
-                        value: api_result.shipped_at != null ? DDG.getDateFromString(api_result.shipped_at) : ""
-                    },
-                    {
-                        label: "Scheduled delivery at: ",
-                        value: api_result.est_delivery_at != null ? DDG.getDateFromString(api_result.est_delivery_at) : ""
-                    },
-                    {
-                        label: "Delivery at: ",
-                        value:  api_result.act_delivery_at != null ? DDG.getDateFromString(api_result.act_delivery_at) : ""
-                    },
-                ]
-            },
-            templates: {
-                group: 'info',
-            }
+        DDG.require('moment.js', function() {
+            Spice.add({
+                id: "package_tracking",
+                name: "Answer",
+                data: {
+                    title: api_result.status_description,  
+                    subtitle: api_result.location,
+                    description: "Last update: " + DDG.getDateFromString(api_result.progress_at),
+                    image: DDG.get_asset_path('package_tracking', 'logos/' + logo + '.png'),
+                    infoboxData: [
+                        {
+                            heading: "Info shipping"
+                        },
+                        {
+                            label: "Shipped at: ",
+                            value: (api_result.shipped_at) ? moment.format('MMM DD, YYYY') : ""
+                        },
+                        {
+                            label: "Scheduled delivery at: ",
+                            value: (api_result.est_delivery_at) ? moment.format('MMM DD, YYYY') : ""
+                        },
+                        {
+                            label: "Delivery at: ",
+                            value: (api_result.act_delivery_at) ? moment().format('MMM DD, YYYY') : ""
+                        },
+                    ]
+                },
+                templates: {
+                    group: 'info',
+                }
+            });
         });
     };
 }(this));
