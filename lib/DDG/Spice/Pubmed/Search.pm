e79c25493450f60866bc88d26553e3eac4d2830c
Zaahir Moolla <moollaza@duckduckgo.com>
2016-03-17 14:30:54 -0400

    Adjust tile width, title length; Cleanup JS

--- lib/DDG/Spice/Pubmed/Search.pm
+++ lib/DDG/Spice/Pubmed/Search.pm
@@ -4,11 +4,7 @@ use strict;
 use DDG::Spice;
 use URI::Escape;
 
-primary_example_queries 'pubmed crispr';
-description 'Search for pubmed for abstracts';
-name 'Pubmed';
-
-triggers startend => ('pubmed');
+triggers startend => 'pubmed';
 
 spice to => 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=$1&retmode=json';
 
@@ -18,7 +14,7 @@ spice alt_to => {
         to => 'http://duckduckgo.com/x.js?u=' . uri_escape("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract&id=") . '$1'
     },
     ids => {
-        to => 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&retmax=10&id=$1'
+        to => 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&retmax=20&id=$1'
     }
 };
 
--- share/spice/pubmed/search/pubmed_search.js
+++ share/spice/pubmed/search/pubmed_search.js
@@ -1,76 +1,86 @@
 (function (env) {
     "use strict";
     env.ddg_spice_pubmed_search = function(api_result){
+
         if (!api_result || api_result.error) {
             return Spice.failed('pubmed');
         }
-        api_result = api_result.esearchresult.idlist;
-        var url = '/js/spice/pubmed/ids/';
+
+        var idlist = api_result.esearchresult.idlist,
+            url = '/js/spice/pubmed/ids/' + idlist.join(),
+            query = api_result.esearchresult.translationset[0].from;
+
         $.ajax({
-            url: url + api_result.join(),
+            url: url,
             async: false,
             dataType: 'json',
-            success: function(r) {                                                                                             
+            success: function(r) {
                 delete r.result.uids;
                 api_result = r.result;
-                var articles = Object.keys(api_result).map(function(key){return api_result[key]});
+                var articles = Object.keys(api_result).map(function(key){
+                        return api_result[key];
+                    });
                 api_result = articles;
-            }
-        });    
-        Spice.add({
-            id: "pubmed",
-            name: "Reference",
-            data: api_result,
-            meta: {
-                sourceName: "Pubmed",
-                sourceUrl: 'http://www.ncbi.nlm.nih.gov/pubmed/',
-                rerender: [ 'description' ]
-            },
-            normalize: function(item) {
-                var authors = [];
-                if (item.authors) {
-                    for (var i in item.authors) {
-                        authors.push(item.authors[i].name);
-                    }
-                }
-                var tmp_url = 'http://www.ncbi.nlm.nih.gov/pubmed/' + item.uid;
-                return {
-                    title: item.title,
-                    url: tmp_url,
-                    subtitle: authors.join(", ") || ' ',
-                    description: '',
-                    tileTitle: "3line-small" 
-                }
-            },
-            onItemShown: function(item) {
-                var url = '/js/spice/pubmed/abstract/'+item.uid;
-                if (item.loadedAbstract) { return; }
-                $.ajax({
-                    url: url,
-                    dataType: 'json',
-                    success: function(r) {
-                        var abstract_text = '';
-                        if ( typeof r.PubmedArticleSet.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText.text != 'undefined' ) {
-                            abstract_text = r.PubmedArticleSet.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText.text;
-                        } else {
-                            abstract_text = 'No abstract available';
+
+                Spice.add({
+                    id: "pubmed",
+                    name: "Reference",
+                    data: api_result,
+                    meta: {
+                        sourceName: "Pubmed",
+                        sourceUrl: 'http://www.ncbi.nlm.nih.gov/pubmed/?term=' + query,
+                        rerender: [ 'description' ],
+                        snippetChars: 140
+                    },
+                    normalize: function(item) {
+                        var authors = [];
+                        if (item.authors) {
+                            for (var i in item.authors) {
+                                authors.push(item.authors[i].name);
+                            }
                         }
-                        item.set('description', abstract_text);
+                        var tmp_url = 'http://www.ncbi.nlm.nih.gov/pubmed/' + item.uid;
+                        return {
+                            title: item.title,
+                            url: tmp_url,
+                            subtitle: authors.join(", ") || ' ',
+                            description: '',
+                            tileTitle: "3line-small"
+                        };
+                    },
+                    onItemShown: function(item) {
+                        var url = '/js/spice/pubmed/abstract/' + item.uid;
+                        if (item.loadedAbstract) { return; }
+                        $.ajax({
+                            url: url,
+                            dataType: 'json',
+                            success: function(r) {
+                                var abstract_text = '';
+                                if ( typeof r.PubmedArticleSet.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText.text != 'undefined' ) {
+                                    abstract_text = r.PubmedArticleSet.PubmedArticle.MedlineCitation.Article.Abstract.AbstractText.text;
+                                } else {
+                                    abstract_text = 'No abstract available';
+                                }
+                                item.set('description', abstract_text);
+                            }
+                        });
+
+                        item.loadedAbstract = 1;
+                    },
+                    templates: {
+                        group: 'text',
+                        variants: {
+                            tileTitle: '4line',
+                            tileSnippet: "large",
+                            tile: 'wide'
+                        },
+                        options: {
+                            moreAt: true,
+                            footer: Spice.pubmed_search.footer
+                        }
                     }
                 });
-
-                item.loadedAbstract = 1;
-            },
-            templates: {
-                group: 'text',
-                variants: {
-                    tileSnippet: "large"
-                },
-                options: {
-                    moreAt: true
-                }
             }
         });
     };
 }(this));
-
