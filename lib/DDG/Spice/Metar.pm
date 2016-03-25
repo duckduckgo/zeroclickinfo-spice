7795ee9b87baffd31c5bea2567768f4630628aa0
sgil <santix91@gmail.com>
2016-03-25 17:42:49 +0000

    Change trigger to 'startend'

--- lib/DDG/Spice/Metar.pm
+++ lib/DDG/Spice/Metar.pm
@@ -10,7 +10,7 @@ spice to => 'http://avwx.rest/api/metar.php?station=$1&format=JSON';
 spice proxy_cache_valid => "418 1d"; # Do not cache responses.
 spice wrap_jsonp_callback => 1;
 
-triggers any => 'metar';
+triggers startend => 'metar';
 
 handle words => sub {
     return if (scalar(@_) != 2); # Not enough parameters or too many.
