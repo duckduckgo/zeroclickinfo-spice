3fdd977b30ad636253672fa6bd6495658481338e
Jason Dorweiller <jason@duckduckgo.com>
2016-03-30 10:40:44 -0400

    check env variable

--- lib/DDG/Spice/Experimentalia.pm
+++ lib/DDG/Spice/Experimentalia.pm
@@ -10,7 +10,11 @@ spice to => 'https://beta.duckduckgo.com/installed.json';
 spice wrap_jsonp_callback => 1;
 #spice is_cached => 0;
 spice proxy_cache_valid => "418 1d";
+
+# export SHOW_IA_TESTING=1
+my $show_ia = $ENV{SHOW_IA_TESTING} || 0;
 handle query_lc => sub {
-        return $_ if $_;
+    return unless $show_ia;
+    return $_ if $_;
 };
 1;
