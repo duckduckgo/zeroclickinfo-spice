fc1552923a3b4ce0e10b5e1fb0f5211c0cbb23aa
tclark1 <taiteclark1@gmail.com>
2016-04-03 14:38:30 +0000

    The IA displays for teams now, and some fixes for missing values are on the way

--- share/spice/first_robotics_teams/first_robotics_teams.js
+++ share/spice/first_robotics_teams/first_robotics_teams.js
@@ -2,18 +2,21 @@
     "use strict";
     env.ddg_spice_first_robotics_teams = function(api_result) {
 
-        // Validate the response (customize for your Spice)
         if (!api_result || api_result.error || api_result.length === 0) {
             return Spice.failed('first_robotics_teams');
         }
-
-        // Render the response
+            
+        //var teamNumber = api_result.team_number;
+        if (api_result.motto === null) {api_result.motto = 'none';}
+        else {api_result.motto = '\"'+api_result.motto+'\"'}
+        
         Spice.add({
             id: 'first_robotics_teams',
-
-            // Customize these properties
             name: 'Reference',
+
             data: {
+                title: 'FIRST Robotics Competition Team ' + api_result.team_number,
+                subtitle: api_result.nickname,
                 record_data: {
                     'Full name': api_result.name,
                     'Location': api_result.location,
@@ -29,12 +32,12 @@
             templates: {
                 group: 'list',
                 options: {
-                    title: 'FIRST Robotics Team' + api_result.team_number.,
-                    subtitle: api_result.nickname,
                     content: 'record',
-                    
                     moreAt: true,
+                  
+                    // Refer to the team's website
                     moreText: {
+                        text: 'Team website',
                         href: api_result.website
                     }
                 }
