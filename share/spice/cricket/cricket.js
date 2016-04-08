005cbcca0d17961836a2f49516d54c1bcccb7436
Vikash <vikashkv@thoughtworks.com>
2016-04-09 03:28:45 +0530

    Add match content panel

--- share/spice/cricket/cricket.js
+++ share/spice/cricket/cricket.js
@@ -18,7 +18,7 @@
                     //TODO: update display
                     primaryText: "<span style='color:deepskyblue;'>" + data.SeriesName + ", Schedule & Score</span>",
                     idField: "matchid",
-                    selectedItem: "193870",
+                    selectedItem: getSelected(data),
                     scrollToSelectedItem: true,
                     itemType: l("Games"),
                     itemsHighlight: false,
@@ -28,52 +28,18 @@
                     sourceUrl: 'http://example.com/url/to/details/' + api_result.name
                 },
                 normalize: function (item) {
-                    //var response = api_result.query.results.Series[0];
-                    //var data = {
-                    //    // customize as needed for your chosen template
-                    //    name: response.SeriesName,
-                    //    description: "Score & Schedule",
-                    //    image: item.icon,
-                    //    matches: []
-                    //};
-                    //var matches = {};
-                    //for (var i = 0; i < response.Schedule.Match.length; i++) {
-                    //    var match = response.Schedule.Match[i];
-                    //    var matchDetails = {
-                    //        matchid: match.matchid,
-                    //        mtype: match.mtype.toUpperCase(),
-                    //        matchNo: match.MatchNo,
-                    //        Venue: match.Venue,
-                    //        date: moment(match.StartDate).format('ddd, Do MMM'),
-                    //        startTime: moment(match.StartDate).format('LT'),
-                    //        startDate: Date.parse(moment(match.StartDate).format("YYYY-MM-DD")),
-                    //        matchTimeSpan: match.MatchTimeSpan,
-                    //        teams: match.Team
-                    //    };
-                    //    if (!matches[moment(match.StartDate).format('LL')]) {
-                    //        matches[moment(match.StartDate).format('LL')] = []
-                    //    }
-                    //    matches[moment(match.StartDate).format('LL')].push(matchDetails);
-                    //}
-                    //var matchList = [];
-                    //for (var date in matches) {
-                    //    matchList.push(matches[date]);
-                    //}
-                    //matchList.sort(function (a, b) {
-                    //    return a[0].startDate - b[0].startDate;
-                    //});
-                    //data.matches = matchList;
-                    //return setDefault(data);
                     return {
                         matchid: item.matchid,
                         mtype: item.mtype.toUpperCase(),
                         matchNo: item.MatchNo,
-                        Venue: item.Venue,
+                        venue: item.Venue.content,
                         date: moment(item.StartDate).format('ddd, Do MMM'),
+                        relativeTime: moment(item.StartDate).calendar(),
                         startTime: moment(item.StartDate).format('LT'),
                         startDate: Date.parse(moment(item.StartDate).format("YYYY-MM-DD")),
                         matchTimeSpan: item.MatchTimeSpan,
                         teams: item.Team,
+                        result:item.Result,
                     };
                 },
                 templates: {
@@ -90,6 +56,11 @@
                 }
             });
         });
+        //TODO: return latest match id
+        function getSelected(data) {
+            return 193870;
+        }
+
         function setDefault(data) {
             var today = Date.parse(new Date());
             var diff = Math.abs(data.matches[0][0].startDate - today);
@@ -103,19 +74,6 @@
     };
     env.show = function (id) {
         console.log(id);
-        var contentEls = document.getElementsByClassName('zci-cricket-content');
-        for (var i = 0; i < contentEls.length; i++) {
-            //contentEls[i].classList.remove("zci-cricket-active");
-            contentEls[i].classList.add("zci-cricket-inactive");
-            if (contentEls[i].classList.contains(id)) {
-                contentEls[i].classList.remove("zci-cricket-inactive");
-            }
-        }
-        var listEls = document.querySelectorAll('.zci-cricket-list li');
-
-        for (var i = 0; i < listEls.length; i++) {
-            listEls[i].classList.remove("active");
-        }
-        document.getElementById(id).classList.add('active');
+        var listEls = document.getElementById(id + "").classList.toggle("zci-cricket-inactive");
     };
 }(this));
