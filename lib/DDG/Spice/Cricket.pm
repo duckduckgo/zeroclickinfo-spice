b7a6b07829ef430aa07fcb2e9f447dd83cdf29f6
Vikash <vikashkv@thoughtworks.com>
2016-04-10 00:28:56 +0530

    format cricket perl module

--- lib/DDG/Spice/Cricket.pm
+++ lib/DDG/Spice/Cricket.pm
@@ -14,8 +14,7 @@ spice proxy_cache_valid => "200 1d"; # defaults to this automatically
 spice wrap_jsonp_callback => 0; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
 
 # API endpoint - http://docs.duckduckhack.com/walkthroughs/forum-lookup.html#api-endpoint
-spice to =>
-    'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20cricket.series.upcoming&format=json&env=store%3A%2F%2F0TxIGQMQbObzvU4Apia0V0&callback={{callback}}';
+spice to => 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20cricket.series.ongoing&format=json&diagnostics=true&env=store%3A%2F%2F0TxIGQMQbObzvU4Apia0V0&callback={{callback}}';
 
 # Triggers - http://docs.duckduckhack.com/walkthroughs/forum-lookup.html#triggers
 triggers query_lc => qr/cricket|ipl scores{0,1}|schedules{0,1}/;
