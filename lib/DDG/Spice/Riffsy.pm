a58e0c528c690d1a3bebb3079c7d93507f2a25c6
sagarwal <a.swapnil@iitg.ernet.in>
2016-04-15 20:42:17 +0000

    Restrict character matching to Emoji only

--- lib/DDG/Spice/Riffsy.pm
+++ lib/DDG/Spice/Riffsy.pm
@@ -10,10 +10,13 @@ spice proxy_cache_valid => "200 1d";
 spice to => 'http://api1.riffsy.com/v1/search?tag=$1&key={{ENV{DDG_SPICE_RIFFSY_APIKEY}}}&safesearch=strict&searchtype=ddg&limit=50';
 spice wrap_jsonp_callback => 1;
 
-triggers query_lc => qr/^\X$|^(\X+|[\w\s]+?) gifs?$/;
+triggers query_lc => qr/^(?<match>\X)$|^(\X+|[\w\s]+?) gifs?$/;
 
 handle query_lc => sub {
     return if !$_ || $_ =~ /^[a-zA-Z0-9]$/;
+    my @emojis = map { chomp $_; $_ } share('emoji_sources.txt')->slurp;
+    my $emoji_regex = join '|', @emojis;
+    return if defined $+{match} && $+{match} !~ qr#$emoji_regex#i;
     return $_;
 };
 
