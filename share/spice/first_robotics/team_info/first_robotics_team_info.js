9466597beda324ebf55e1d21310416a2e3dc59e7
tclark1 <taiteclark1@gmail.com>
2016-04-19 16:33:32 +0000

    Move Spice.add() to a callback within getJSON, making it possible to display the last year a team has competed.

--- share/spice/first_robotics/team_info/first_robotics_team_info.js
+++ share/spice/first_robotics/team_info/first_robotics_team_info.js
@@ -2,17 +2,6 @@
     
     "use strict";
     
-    var last_year_competed = 0;
-    
-    /*env.ddg_spice_first_robotics_years_competed = function(api_result) {
-        
-        // Grab the most recent year the team participated from the end of the array the API returns
-        if (api_result && api_result.length != 0 && api_result != null) {
-            last_year_competed = api_result[api_result.length - 1];
-        }
-        
-    };*/
-    
     env.ddg_spice_first_robotics_team_info = function(api_result) {
 
         if (!api_result || api_result.error || api_result.length === 0 || !api_result.name) {
@@ -32,48 +21,51 @@
             };
         }
         
-        
-        Spice.add({
-            id: 'first_robotics_teams',
-            name: 'Reference',
-            data: api_result,
-            meta: {
-                sourceName: 'The Blue Alliance',
-                sourceUrl: 'https://www.thebluealliance.com/team/' + api_result.team_number
-            },
-            normalize: function(item) {
-                var team_data = {
-                    'Full name': item.name,
-                    'Location': item.location,
-                    'Rookie year': item.rookie_year
-                };
-                
-                // Remove space for last year competed if none is provided
-                $.getJSON('/js/spice/first_robotics/years_competed/' + item.team_number, 
-                     function(data) {
-                        last_year_competed = data[data.length - 1];
+        // Get the last year the team competed from a different API call
+        $.getJSON('/js/spice/first_robotics/years_competed/' + api_result.team_number, 
+                  
+            // Only call Spice.add() once the second API call is returned
+            function(data) {
+
+                Spice.add({
+                    id: 'first_robotics',
+                    name: 'Reference',
+                    data: api_result,
+                    meta: {
+                        sourceName: 'The Blue Alliance',
+                        sourceUrl: 'https://www.thebluealliance.com/team/' + api_result.team_number
+                    },
+                    normalize: function(item) {
+                        var team_data = {
+                            'Full name': item.name,
+                            'Location': item.location,
+                            'Rookie year': item.rookie_year
+                        };
+                        
+                        // Only show the last year competed if there is an array of years
+                        if (data && data.length != 0 && !data.error) {
+                            team_data['Last competed'] = data[data.length - 1];
+                        }
+
+                        // Ditto for motto
+                        if (item.motto && item.motto != null) {
+                            team_data['Motto'] = item.motto;
+                        }
+
+                        return {
+                            title: 'FIRST Robotics Competition Team ' + item.team_number,
+                            subtitle: item.nickname,
+                            record_data: team_data
+                        };
+                    },
+                    templates: {
+                        group: 'list',
+                        options: template_options
+                    }
                 });
-                //last_year_competed = years_competed[years_competed.length - 1];
-                
-                //if (last_year_competed != 0) {
-                    team_data['Last competed'] = last_year_competed;
-                //}
-                
-                // Ditto for motto
-                if (item.motto && item.motto != null) {
-                    team_data['Motto'] = item.motto;
-                }
-                
-                return {
-                    title: 'FIRST Robotics Competition Team ' + item.team_number,
-                    subtitle: item.nickname,
-                    record_data: team_data
-                };
-            },
-            templates: {
-                group: 'list',
-                options: template_options
-            }
-        });
+            });
+        
+        
+        
     };
 }(this));
