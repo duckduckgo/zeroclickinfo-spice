c0c65c4ec975893dfa6b2cab969b2a8429372995
levaly <lev.alyshayev@gmail.com>
2016-04-20 11:37:07 +0000

    support queries fo the sort "integral of x^2"

--- lib/DDG/Spice/Symbolab.pm
+++ lib/DDG/Spice/Symbolab.pm
@@ -7,9 +7,10 @@ spice is_cached => 1;
 spice to => 'https://www.symbolab.com/ddg?query=$1';
 spice wrap_jsonp_callback => 1;
 
-triggers startend => "calculate", "solve", "compute";
-handle remainder => sub {
-    if ($_ =~ /[\^\*\=\+\-\/\\]/ or $_ =~ /\b(sin|cos|tan|cot|csc|sec|ln|log|sqrt|integral)/i) {
+triggers startend => "calculate", "solve", "compute", "integral", "integrate", "integration", "antiderivative", "derivative", "derive", "differentiate", "derivation";
+handle query_lc => sub {
+    print "Recevied: $_\n";
+    if ($_ =~ /[\^\*\=\+\-\/\\]/ or $_ =~ /\b(sin|cos|tan|cot|csc|sec|ln|log|sqrt|integr|deriv|diff)/i) {
         return $_;
     }
     return;
