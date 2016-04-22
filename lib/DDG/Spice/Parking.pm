ec7c45748d05580a2ec1ef3598abbb6939efe90d
Eric Johnson <eric.git@iijo.org>
2016-04-21 22:45:24 -0400

    Fix regex

--- lib/DDG/Spice/Parking.pm
+++ lib/DDG/Spice/Parking.pm
@@ -10,7 +10,7 @@ triggers any => 'parking panda', 'parkingpanda', 'parking', 'to park';
 spice to => 'https://www.parkingpanda.com/api/v2/Locations/?ref=duckduck&search=$1';
 spice wrap_jsonp_callback => 1;
 spice proxy_cache_valid => '200 304 30m';
-spice from => '(.+?)/(.+)';
+spice from => '(.+)';
 
 handle remainder => sub {
     return unless $_;    # Guard against "no answer"
