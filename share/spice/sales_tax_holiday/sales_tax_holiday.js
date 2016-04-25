5c6010f1dcf6d9c9cec2ed087d89c3579abac8e4
ajain1 <ajain@jframeworks.com>
2016-04-25 20:10:14 +0000

    Update UI with help of Zaahir. PR# 2502

--- share/spice/sales_tax_holiday/sales_tax_holiday.js
+++ share/spice/sales_tax_holiday/sales_tax_holiday.js
@@ -1,89 +1,77 @@
 (function (env) {
     "use strict";
-    env.ddg_spice_sales_tax_holiday = function(api_result){
-        // Error checking
-        var errorMessage = "UNKNOWN ERROR";
-        var isError = false;
-        var isMultipleResults = false;
-        if (!api_result 
-         || (typeof api_result.header === 'undefined')
-         || api_result.header.status !== "SUCCESS" 
-         || !api_result.taxHolidays
-         || (typeof api_result.taxHolidays === 'undefined')
-         || api_result.taxHolidays.length < 1) {
-            if(api_result.header.errorMessage 
-            && api_result.header.errorCode 
-            && api_result.header.errorCode === 'NOT_FOUND') {
-                isError = true;
-                errorMessage = api_result.header.errorMessage;
-            } else {         
-              return Spice.failed('sales_tax_holiday');
-            }
+    env.ddg_spice_sales_tax_holiday = function (api_result) {
+
+        if (!api_result || !api_result.header || api_result.header.status !== "SUCCESS" ) {
+            return Spice.failed('sales_tax_holiday');
         }
 
-        if (api_result.taxHolidays.length > 1) {
-            isMultipleResults = true;
+        if (api_result.taxHolidays.length > 1 ) {
+            return Spice.failed('sales_tax_holiday');
         }
-        // Display
-      DDG.require("moment.js", function(){
-        Spice.add({
-            id: "sales_tax_holiday",
-            name: "Answer",
-            data: isError? {"errorMessage" : errorMessage} : api_result.taxHolidays,
-            meta: {
-                sourceName: "snapCX",
-                sourceIcon: false,
-                sourceIconUrl: "http://snapcx.io/favicon.ico",
-                sourceUrl: "https://snapcx.io/salesTax"
-            },
-            normalize: function(item) {
-                var stateName   = item.stateName;
-                if (!isError) {
-                   var titleResult = stateName;
-                   if (!isMultipleResults) {
-                       titleResult += " - "+"Sales Tax Holidays";
-                   } 
-                } else {
-                   var titleResult = item.errorMessage;
-                }
 
-                if (item.dates && !(typeof item.dates === 'undefined')) {
-                  var dates = item.dates;
-                  var subtitleResult = "";
-                  for (var dt = 0; dt < dates.length; dt++) {
-                      if (dt > 0) subtitleResult += ", ";
-                      //subtitleResult += moment(dates[dt]).format("MMM DD, YYYY"); 
-                      subtitleResult += moment(dates[dt]).format("DD MMM YYYY"); 
-                  }  
-                }
-                                
-                if (item.taxHolidayItems && !(typeof item.taxHolidayItems === 'undefined')) {
-                  var taxHolidayItems = item.taxHolidayItems;
-                  var description = "Included items are ";
-                  for (var i = 0; i < taxHolidayItems.length; i++) {
-                      if (i > 0) description += ", ";
-                      description += taxHolidayItems[i].description;
-                      if (!(typeof taxHolidayItems[i].maximumPriceLimit === 'undefined') && taxHolidayItems[i].maximumPriceLimit > 0.0 
-                         )
-                      description += "-$"+taxHolidayItems[i].maximumPriceLimit;
-                  }  
-                }
-                return {
-                    title: titleResult,
-                    subtitle: subtitleResult,
-                    description: description                    
-                };
-            },
-            templates: {
-                group: 'text',
-                options: {
-                    moreAt: true
+        DDG.require("moment.js", function () {
+
+            Spice.add({
+                id: "sales_tax_holiday",
+                name: "Answer",
+                data: api_result.taxHolidays[0] || api_result.header,
+                meta: {
+                    sourceName: "snapCX",
+                    sourceUrl: "https://snapcx.io/salesTax"
                 },
-                variants: {
-                  tileSnippet: 'large'
-                }            
-            } //end of 'templates block
-        }); //end of Spice.add(..)
-      }); //end of moment.js dependency
+                normalize: function (item) {
+
+                    var stateName = item.stateName,
+                        year = moment().year(),
+                        titleResult = stateName + " - " + year + " Sales Tax Holidays";
+
+                    if (item.dates.length) {
+                        var dateMap = {};
+                        $.each(item.dates, function() {
+                            var obj = moment(this),
+                                m = obj.format("MMM"),
+                                d = obj.format("D");
+
+                            if (dateMap[m]){
+                                dateMap[m].push(d);
+                            } else {
+                                dateMap[m] = [d];
+                            }
+                        });
+
+                        var dateSub = [];
+                        $.each(dateMap, function(k, v){
+                            var datesString = k + " " + v.join(", ");
+                            dateSub.push(datesString);
+                        });
+                    }
+
+                    if (item.taxHolidayItems.length) {
+                        var record_data = {};
+                        $.each(item.taxHolidayItems, function() {
+                            if (this.maximumPriceLimit && this.maximumPriceLimit > 0.0){
+                                record_data[this.description] = "$" + this.maximumPriceLimit;
+                            } else {
+                                record_data[this.description] = "Not Defined";
+                            }
+                        });
+                    }
+
+                    return {
+                        title: titleResult,
+                        subtitle: dateSub,
+                        record_data: record_data
+                    };
+                },
+                templates: {
+                    group: 'list',
+                    options: {
+                        moreAt: true,
+                        content: "record"
+                    }
+                }
+            });
+        });
     }
 }(this));
