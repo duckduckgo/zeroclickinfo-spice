c16b0a99860e4da5239a9ae10058de8e7c2c3ac3
anguyen <anthony.nguyen@queensu.ca>
2016-05-03 20:57:24 +0000

    Fixed most issues, submission

--- share/spice/syntax/syntax.js
+++ share/spice/syntax/syntax.js
@@ -1,38 +1,44 @@
 (function (env) {
     "use strict";
     env.ddg_spice_syntax = function(api_result) {
+        
+        function getParameterByName(name) {
+            var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+            return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+        }
+        var script = $('[src*="/js/spice/syntax/"]')[0],
+            source = $(script).attr("src"),
+            query = source.match(/syntax\/([^\/]+)/)[1],
+            decodedQuery = decodeURIComponent(query);
 
         // Validate the response (customize for your Spice)
         if (!api_result || api_result.error || api_result.length == 0) {
             return Spice.failed('syntax');
         }
         var query = getParameterByName('q');
-        
         // Render the response
         Spice.add({
             id: "syntax",
 
             // Customize these properties
-            name: "Syntax",
+            name: "Programming",
             data: api_result[0],
             meta: {
                 sourceName: "SyntaxDB.com",
-                sourceUrl: 'https://syntaxdb.com/reference/search?search=' + query
+                sourceUrl: 'https://syntaxdb.com/reference/search?search=' +  decodedQuery,
+                sourceIconUrl: 'https://syntaxdb.com/images/favicon.ico'
             },
             normalize: function(item) {
-                var conceptInfo;
-                
                 return {
                     // customize as needed for your chosen template
                     title: item.concept_search,
-                    subtitle: item.description,
-                    url: 'https://syntaxdb.com/ref/' + api_result[0].language_permalink + '/' + api_result[0].concept_permalink
+                    subtitle: "Syntax",
                 };
             },
             templates: {
                 group: 'text',
                 options: {
-                    content: Spice.syntax.content,
+                    content: Spice.syntax.syntax,
                     moreAt: true
                 }
             }
