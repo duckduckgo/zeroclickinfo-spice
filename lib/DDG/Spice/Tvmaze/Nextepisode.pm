a9b56261ed4362cd646d9e40a690f6c5ba3be674
Zacqary <plankhead@gmail.com>
2016-05-07 05:03:55 +0000

    TVMaze accepts show names with punctuation

--- lib/DDG/Spice/Tvmaze/Nextepisode.pm
+++ lib/DDG/Spice/Tvmaze/Nextepisode.pm
@@ -17,15 +17,15 @@ spice wrap_jsonp_callback => 1;
 handle query_lc => sub {
     return unless $_;
         
-    return $4 if $_ =~ /(next|upcoming) (episode|airdate) (in|of|for|from)? ?([a-z0-9 ]+)/;
+    return $4 if $_ =~ /(next|upcoming) (episode|airdate) (in|of|for|from)? ?(.+?)/;
         
-    return $2 if $_ =~ /(next|upcoming) ([a-z0-9 ]+) episode/;
+    return $2 if $_ =~ /(next|upcoming) (.+?) episode/;
 
-    return $1 if $_ =~ /([a-z0-9 ]+) (next|upcoming) (episode|airdate)/;
+    return $1 if $_ =~ /(.+?) (next|upcoming) (episode|airdate)/;
         
-    return $2 if $_ =~ /(when does) ([a-z0-9 ]+) (start|come back|come out|air|return) ?(on)?/;
+    return $2 if $_ =~ /(when does) (.+?) (start|come back|come out|air|return) ?(on)?/;
 
-    return $2 if $_ =~ /(when does) ([a-z0-9 ]+) (season [0-9]+) (start|come out|air)/;
+    return $2 if $_ =~ /(when does) (.+?) (season [0-9]+) (start|come out|air)/;
 
     return;
 };
