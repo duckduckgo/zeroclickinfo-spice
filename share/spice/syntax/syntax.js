6526f7f1f6d9a94b59b3b4e3dc757f4884b079a2
anguyen <anthony.nguyen@queensu.ca>
2016-05-10 18:57:14 +0000

    Used get_query within DDG namespace

--- share/spice/syntax/syntax.js
+++ share/spice/syntax/syntax.js
@@ -1,31 +1,20 @@
 (function (env) {
     "use strict";
     env.ddg_spice_syntax = function(api_result) {
-        
-        function getParameterByName(name) {
-            var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-            return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-        }
-        var script = $('[src*="/js/spice/syntax/"]')[0],
-            source = $(script).attr("src"),
-            query = source.match(/syntax\/([^\/]+)/)[1],
-            decodedQuery = decodeURIComponent(query);
-
         // Validate the response (customize for your Spice)
         if (!api_result || api_result.error || api_result.length == 0) {
             return Spice.failed('syntax');
         }
-        var query = getParameterByName('q');
+        var query = DDG.get_query().replace("syntax", '');
         // Render the response
         Spice.add({
             id: "syntax",
-
             // Customize these properties
             name: "Programming",
             data: api_result[0],
             meta: {
                 sourceName: "SyntaxDB.com",
-                sourceUrl: 'https://syntaxdb.com/reference/search?search=' +  decodedQuery,
+                sourceUrl: 'https://syntaxdb.com/reference/search?search=' +  query,
                 sourceIconUrl: 'https://syntaxdb.com/images/favicon.ico'
             },
             normalize: function(item) {
