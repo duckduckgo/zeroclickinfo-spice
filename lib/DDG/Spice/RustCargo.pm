5ee2f00068ba8e1e2a72c27794e20f3cd437f56b
TomBebbington <tophattedcoder@gmail.com>
2016-05-22 11:56:45 +0000

    Rust Cargo: Search for crates instead of displaying a single crate
    This uses the same layout as the GitHub IA and sorts the relevant crates by downloads.
    It displays a crates' name, version, date of last release, description and number of downloads.

--- lib/DDG/Spice/RustCargo.pm
+++ lib/DDG/Spice/RustCargo.pm
@@ -6,7 +6,7 @@ use DDG::Spice;
 
 triggers startend => 'cargo package', 'cargo packages', 'rust package', 'rust packages', 'rust cargo';
 
-spice to => 'https://crates.io/api/v1/crates/$1';
+spice to => 'https://crates.io/api/v1/crates?q=$1';
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
