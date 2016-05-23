5c57621ec64acb7bc76df67f8b41e7fb27e1c4b9
iammrigank <me.mrig@gmail.com>
2016-05-23 17:21:55 +0000

    Goodreads

---
+++ share/spice/goodreads/goodreads.js
@@ -0,0 +1,95 @@
+(function (env) {
+    "use strict";
+
+    env.ddg_spice_goodreads = function (api_result) {
+
+        // Validating response
+        if (!api_result || api_result.GoodreadsResponse.search["total-results"].text == "0") {
+            return Spice.failed('goodreads');
+        }
+        
+        var book_details_uri = '/js/spice/goodreads_book_details/',
+            book_image_uri = '/js/spice/goodreads_book_image/';
+
+        $.ajaxSetup({
+            cache: true
+        });
+
+        // Render the response
+        Spice.add({
+            id: 'goodreads',
+            name: 'Books',
+            data: api_result.GoodreadsResponse.search.results.work,
+            meta: {
+                searchTerm: api_result.GoodreadsResponse.search.query.text,
+                itemType: 'Books',
+                sourceUrl: "http://www.goodreads.com/",
+                sourceName: 'Goodreads',
+                rerender: [
+                    'abstract',
+                    'heading',
+                    'img_m',
+                    'image'
+                ]
+            },
+
+            normalize: function (item) {
+                var book = item.best_book;
+
+                return {
+                    url: "https://www.goodreads.com/book/show/" + book.id.text,
+                    title: book.title.text,
+                    image: book.image_url.text,
+                    img_m: book.image_url.text,
+                    rating: parseFloat(item.average_rating.text),
+                    ratingText: item.average_rating.text,
+                    brand: book.author.name.text,
+                    reviewCount: parseInt(item.ratings_count.text),
+                    yop: item.original_publication_year.text,
+                    reviews_count: item.text_reviews_count.text,
+                    author: book.author.name.text,
+                    link_to_author: "https://www.goodreads.com/author/show/" + book.author.name.text
+                };
+            },
+
+            onItemShown: function (item) {
+
+                var book = item.best_book;
+
+                $.getScript(book_details_uri + book.id.text, function (data) {
+                    var book = data.GoodreadsResponse.book;
+                    var description = book.description.text || "-- No Description Found --";
+
+                    item.set({
+                        abstract: description && Handlebars.helpers.ellipsis(description.replace(/<\/?\w*>/gm, ''), 400),
+                        heading: book.title.text,
+                        isbn: book.isbn && book.isbn.text,
+                        isbn13: book.isbn13 && book.isbn13.text
+                    });
+
+                    $.getJSON(book_image_uri + data.GoodreadsResponse.book.isbn13.text, function (image_object) {
+                        item.set({
+                            img_m: image_object.results[0].img_m
+                        });
+                    });
+                });
+            },
+
+            templates: {
+                group: "products_simple",
+                options: {
+                    rating: true,
+                    brand: true,
+                    moreAt: true,
+                    ratingtext: true,
+                    subtitle_content: Spice.goodreads.details
+                },
+
+                variants: {
+                    tile: 'poster',
+                    tileTitle: "2line"
+                }
+            }
+        });
+    };
+}(this));
