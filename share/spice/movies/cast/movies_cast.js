5d990aa0544d164a82fb14b184481b42055055b0
Richard Whalen <richard@thumbtack.com>
2016-05-30 11:06:35 -0700

    Address Code review comments for Movie Cast IA

--- share/spice/movies/cast/movies_cast.js
+++ share/spice/movies/cast/movies_cast.js
@@ -7,11 +7,7 @@
 
     var DETAIL_ENDPOINT = '/js/spice/movies/details/';
     var POSTER_URL = 'http://image.tmdb.org/t/p/w185';
-
-    var getMovieUrl = function(id) {
-        return 'http://www.themoviedb.org/movie/' + id;
-    }
-    Handlebars.registerHelper("getMovieUrl", getMovieUrl);
+    var MOVIE_URL = 'http://www.themoviedb.org/movie/';
 
     env.ddg_spice_movies_cast = function(data) {
         // This isn't exactly pretty, but I can't think of any
@@ -24,52 +20,51 @@
         );
 
         if (hasFailed) {
-            return Spice.failed('cast');
+            return Spice.failed('movies_cast');
         }
 
-        $.getScript(DETAIL_ENDPOINT + data.results[0].id)
-    };
+        $.getJSON(DETAIL_ENDPOINT + data.results[0].id, function(api_result) {
+            var hasFailed = (
+                !api_result ||
+                !api_result.results ||
+                !api_result.results.length
+            );
 
-    env.ddg_spice_movies_details = function(api_result) {
-        var hasFailed = (
-            !api_result ||
-            !api_result.results ||
-            !api_result.results.length
-        );
-
-        if (hasFailed) {
-            return Spice.failed('cast');
-        }
-
-        Spice.add({
-            id: 'cast',
-            name: 'Movies',
-            data: api_result.results,
-            meta: {
-                sourceName: 'TheMovieDb',
-                sourceUrl: 'http://www.themoviedb.org/person/'
-                    + api_result.results[0].id,
-                primaryText: 'Showing ' + api_result.results.length + ' Movies'
-            },
-            normalize: function(item) {
-                return {
-                    heading: item.title,
-                    url: getMovieUrl(item.id),
-                    rating: item.vote_average / 2, // normalize from 10 to 5 pt scale
-                    reviewCount: item.vote_count,
-                    brand: item.release_date,
-                    abstract: item.overview,
-                    image: POSTER_URL + item.poster_path,
-                    img_m: POSTER_URL + item.poster_path,
-                };
-            },
-            templates: {
-                group: 'movies',
-                options: {
-                    rating: true,
-                    buy: Spice.movies_cast.buy,
-                }
+            if (hasFailed) {
+                return Spice.failed('movies_cast');
             }
+
+            Spice.add({
+                id: 'cast',
+                name: 'Movies',
+                data: api_result.results,
+                meta: {
+                    sourceName: 'TheMovieDb',
+                    sourceUrl: 'http://www.themoviedb.org/person/'
+                        + api_result.results[0].id,
+                    primaryText: 'Showing ' + api_result.results.length + ' Movies'
+                },
+                normalize: function(item) {
+                    return {
+                        heading: item.title,
+                        url: MOVIE_URL + item.id,
+                        rating: item.vote_average / 2, // normalize from 10 to 5 pt scale
+                        reviewCount: item.vote_count,
+                        brand: item.release_date,
+                        abstract: item.overview,
+                        image: POSTER_URL + item.poster_path,
+                        img_m: POSTER_URL + item.poster_path,
+                    };
+                },
+                templates: {
+                    group: 'movies',
+                    options: {
+                        rating: true,
+                        buy: Spice.movies_cast.buy,
+                    }
+                }
+            });
+
         });
-    }
+    };
 }(this));
