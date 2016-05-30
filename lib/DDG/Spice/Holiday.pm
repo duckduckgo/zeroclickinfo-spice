7ae6ad3a6297c6de6154dbd5cf28752a1940dba1
Amir Sekhavati <amir.sekhavati@gmail.com>
2016-05-30 12:21:19 +0000

    Holiday: simplify IA logic by removing checks for 'tense'

--- lib/DDG/Spice/Holiday.pm
+++ lib/DDG/Spice/Holiday.pm
@@ -10,10 +10,10 @@ spice is_cached => 1;
 spice proxy_cache_valid => "200 30d";
 spice wrap_jsonp_callback => 0;
 
-spice from => '([^/]+)/([^/]+)/([^/]+)/([^/]+)';
-spice to => 'http://www.timeanddate.com/scripts/ddg.php?m=whenis&c=$2&q=$3&y=$4&callback={{callback}}';
+spice from => '([^/]+)/([^/]+)/([^/]+)';
+spice to => 'http://www.timeanddate.com/scripts/ddg.php?m=whenis&c=$1&q=$2&y=$3&callback={{callback}}';
 
-my @triggers  = ('when is', 'when was', 'what day is', 'what day was');
+my @triggers  = ('when is', 'what day is');
 my $countries = join('|', share("countries.txt")->slurp(chomp => 1));
 
 triggers start => @triggers;
@@ -74,8 +74,7 @@ handle remainder_lc => sub {
 	# These are the min/max years available from the API (as of Feb 2016, API version 2)
     return unless ($chosenYear >= 1600 && $chosenYear <= 2400);
 	
-    # ***todo*** remove hardcoded 'is' later
-	return "is", $chosenCountry, $chosenHoliday, $chosenYear; 
+	return $chosenCountry, $chosenHoliday, $chosenYear; 
 };
 
 1;
--- share/spice/holiday/holiday.js
+++ share/spice/holiday/holiday.js
@@ -1,32 +1,23 @@
 (function (env) {
     "use strict";
-
-    // No jQuery appending a timestamp when calling $.getScript().
-    $.ajaxSetup({ cache: true });
-
     env.ddg_spice_holiday = function(api_result) {
-        if(!api_result || !api_result.h) {
+        
+        if (!api_result || !api_result.h || api_result.h.length === 0 || api_result.h.length !== 1) {
             return Spice.failed('holiday');
         }
 
-        var script = $('[src*="/js/spice/holiday/"]')[0],
-            query_matches = $(script).attr("src").match(/holiday\/([^\/]+)\/([^\/]+)\/([^\/]+)\/(\d*)/),
-            year = query_matches[4],
-            query = query_matches[3],
-            country = query_matches[2],
-            tense = query_matches[1],
-            url = 'http://www.timeanddate.com',
-            source = url + '/search/results.html?query=' + query;
+        var script = $('[src*="/js/spice/holiday/"]')[0];
+        var query_matches = $(script).attr("src").match(/holiday\/([^\/]+)\/([^\/]+)\/(\d*)/);
+        var country = query_matches[1];
+        var query = query_matches[2];
+        var year = query_matches[3];
+        var url = 'http://www.timeanddate.com';
+        var source = url + '/search/results.html?query=' + query;
 
-        // Retry with US if nothing is returned.
-        if (api_result.h.length === 0) {
-            if (country !== 'United%20States') {
-                $.getScript('/js/spice/holiday/' + tense + '/United%20States/' + query + '/' + year);
-            } else {
-                return Spice.failed('holiday');
-            }
+        if (!year || !query || !country) {
+            return Spice.failed('holiday');
         }
-
+        
         DDG.require('moment.js', function() {
             var meta_obj = {
                     itemType: "Holidays",
@@ -37,113 +28,46 @@
                 data;
 
             var events = api_result.h;
+            data = events[0];
 
-            // Filtering events
-            // for "christmas" only show "Christmas Day" etc
-            // add more to the list as needed
-            var single_days = {
-                'christmas' : ['Christmas', 'Christmas Day'],
-                'easter'    : ['Easter Sunday', 'Easter Day']
+            if (!data.o || data.o.length <= 0) {
+                 return Spice.failed('holiday');
+            }
+
+            normalize_fn = function(item) {
+                var date = item.o[0],
+                    next_date = null,
+                    subtitle = item.n;
+                
+                if (date.s !== null) {
+                    subtitle += ', observed in ' + DDG.strip_html(date.s) + '.';
+                }
+                
+                var data = {
+                    title: date.d,
+                    subtitle: subtitle,
+                    name: item.n,
+                    description: item.a,
+                };
+                
+                return data;
             };
-            var days = Object.keys(single_days);
-            events = events.filter(function(item) {
-                for (var i = 0; i < days.length; i++) {
-                    if (query === days[i] && single_days[days[i]].indexOf(item.n) === -1) {
-                        return false;
-                    }
+
+            meta_obj.sourceUrl = url + data.u;
+
+            template_obj = {
+                group: 'text',
+                options: {
+                    moreAt: true
                 }
-                // Check holiday name for relevancy, ignoring "day" and non-alpha characters (e.g. [.-'])
-                if (DDG.stringsRelevant(DDG.strip_non_alpha(item.n), DDG.strip_non_alpha(query), ['day'], 2)){
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-
-            if (events.length === 0) {
-                return Spice.failed('holiday');
-            }
-
-            if (events.length === 1) {
-                data = events[0];
-
-                if (!data.o || data.o.length <= 0) {
-                    return Spice.failed('holiday');
-                }
-
-                // If the event date is past in the current year and past tense
-                // was used, requery for the previous year.  If the year is
-                // specified let that trump tense.
-                var event_date = moment(new Date(data.o[0].d));
-                var current_date = moment();
-                if ((tense === "was") && event_date.isAfter(current_date) && (year.length === 0)) {
-                    $.getScript('/js/spice/holiday/' + tense + '/' + country + '/' + query + '/' + event_date.subtract(1, 'year').format('YYYY'));
-                    return;
-                }
-
-                normalize_fn = function(item) {
-                    var date = item.o[0],
-                        next_date = null,
-                        subtitle = item.n;
-
-                    if (date.s !== null) {
-                        subtitle += ', observed in ' + DDG.strip_html(date.s) + '.';
-                    }
-
-                    var data = {
-                        title: date.d,
-                        subtitle: subtitle,
-                        name: item.n,
-                        description: item.a,
-                    };
-
-                    return data;
-                };
-
-                meta_obj.sourceUrl = url + data.u;
-
-                template_obj = {
-                    group: 'text',
-                    options: {
-                        moreAt: true
-                    }
-                };
-
-            } else {
-                data = events;
-                normalize_fn = function(item) {
-                    var date = item.o[0],
-                        subtitle = item.n;
-
-                    if (date.s !== null) {
-                        subtitle += ', observed in ' + DDG.strip_html(date.s) + '.';
-                    }
-
-                    return {
-                        title: moment(new Date(date.d)).format('dddd, MMM D, YYYY'),
-                        subtitle: subtitle,
-                        name: item.n,
-                        description: item.a,
-                        url: url + item.u,
-                    };
-                };
-                meta_obj.sourceUrl = source;
-                template_obj = {
-                    group: 'text',
-                    detail: false,
-                    variants: {
-                        tile: 'basic1',
-                        tileTitle: '2line'
-                    }
-                };
-            }
-
+            };
+            
             Spice.add({
                 id: 'holiday',
                 name: 'Answer',
                 data: data,
                 normalize: normalize_fn,
-                meta: meta_obj,
+                meta: {meta_obj},
                 templates: template_obj
             });
         });
