1ed539a197301621a9e603f3fe52f96775621b0d
navneetzz <navneetnnavneet1@gmail.com>
2016-05-30 14:16:35 +0000

    modified some files

--- lib/DDG/Spice/NewYorkTimes.pm
+++ lib/DDG/Spice/NewYorkTimes.pm
@@ -4,7 +4,7 @@ package DDG::Spice::NewYorkTimes;
 use strict;
 use DDG::Spice;
 
-spice to => 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=a35e46f11734e2f1d0bfa230ec538fbe:18:73980400&title=$1';
+spice to => 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key={{ENV{DDG_SPICE_NewYorkTimes_APIKEY}}}&title=$1';
 
 triggers startend => "new york times", "new york times best seller";
 
