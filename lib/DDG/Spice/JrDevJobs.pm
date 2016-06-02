593475f4a74b606e4b36c747a1d397b3dd759391
HunterMeyer <huntermeyer@live.com>
2016-06-02 01:36:58 -0400

    Update regex and use startend triggers

--- lib/DDG/Spice/JrDevJobs.pm
+++ lib/DDG/Spice/JrDevJobs.pm
@@ -3,15 +3,14 @@ package DDG::Spice::JrDevJobs;
 
 use DDG::Spice;
 
-triggers start => qw(jrdevjobs);
+triggers startend => qw(jrdevjobs);
 
 spice to => 'https://www.jrdevjobs.com/jobs.json?query=$1';
 spice is_cached => 1;
 spice wrap_jsonp_callback => 1;
 
 handle remainder => sub {
-   s/(jobs)//;
-   s/(job)//;
+   s/(jobs?|developer)//i;
    s/(developer)//;
 
     return $_ if $_;
