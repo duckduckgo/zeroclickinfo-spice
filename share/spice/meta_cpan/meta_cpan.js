e9418ae429254fa25f662318adec80e004a64a14
John Barrett <john@jbrt.org>
2016-06-07 16:53:17 +0100

    Remove old data retrieval, replace error condition

--- share/spice/meta_cpan/meta_cpan.js
+++ share/spice/meta_cpan/meta_cpan.js
@@ -2,20 +2,8 @@
     env.ddg_spice_meta_cpan = function(api_result) {
         "use strict";
 
-        if (!(api_result && api_result.author && api_result.version)) {
-            return Spice.failed('meta_cpan');
-        }
-        var script = $('[src*="/js/spice/meta_cpan/"]')[0],
-            source = $(script).attr("src"),
-            query = decodeURIComponent(source.match(/meta_cpan\/([^\/]+)/)[1]),
-            link = "search?q=" + encodeURIComponent(query),
-            module_name = api_result.name;
-
-        if (api_result.module && api_result.module.length > 0) {
-            module_name = api_result.module[0].name;
-            if (api_result.module[0].associated_pod) {
-                link = "module/" + api_result.module[0].associated_pod;
-            }
+        if ( !api_result || api_result.hits.hits.length < 1 ) {
+            return DDH.failed('meta_cpan');
         }
 
         Spice.add({
