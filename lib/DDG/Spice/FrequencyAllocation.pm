e6c4ec1f91c58fa1b734d1be5b01e07cea8918ff
ahernandez <aurelien.hernandez27@gmail.com>
2016-06-12 20:37:11 +0000

    Add electromagnetic wave frequency allocation to instant answer

---
+++ lib/DDG/Spice/FrequencyAllocation.pm
@@ -0,0 +1,22 @@
+package DDG::Spice::FrequencyAllocation;
+
+# ABSTRACT: Return informations about a given electromagnetic wave frequency. Version 1.
+
+use DDG::Spice;
+
+spice is_cached => 1;
+spice proxy_cache_valid => "200 1d";
+
+spice wrap_jsonp_callback => 0;
+spice from => '(\w+)/(\w+)';
+spice to => 'https://data.fcc.gov/api/spectrum-view/services/advancedSearch/getSpectrumBands?frequencyFrom=$1&frequencyTo=$2&format=jsonp&callback={{callback}}';
+
+triggers startend => 'frequency', 'freq', 'mhz';
+
+handle remainder => sub {
+    
+    return $_,$_+1 if $_ =~ /^[0-9]{3,4}$/;
+    return;
+};
+
+1;
