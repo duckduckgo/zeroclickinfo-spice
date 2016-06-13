ae945288803de62c1e5caf8180419a936fd2cfdd
sdua <sahildua2305@gmail.com>
2016-06-13 13:07:35 +0000

    Update to new Spice API

--- share/spice/search_code/search_code.js
+++ share/spice/search_code/search_code.js
@@ -1,52 +1,62 @@
-function ddg_spice_search_code(api_response) {
+(function(env) {
     "use strict";
+    env.ddg_spice_search_code = function(api_result) {
 
-    var query = api_response.query;
-    var data = api_response.results;
+        var query = api_result.query;
+        var data = api_result.results;
 
-    if(!data.length || data.length === 0) {
-        return Spice.failed('search_code');
-    }
-
-    var result;
-    for (var i = 0; i < data.length; i++) {
-        var checkRelevancy = [data[i].name, data[i].displayname, data[i].namespace].join(" ");
-        if (DDG.isRelevant(checkRelevancy, [], 2)) {
-            result = data[i];
-            break;
-        }
-    }
-
-    if (!result) {
-        return Spice.failed('search_code');
-    }
-
-    function formatName(result) {
-        var formatted_name = result.name;
-
-        if (result.displayname !== '' || result.namespace !== '') {
-            formatted_name += ' (';
-            if (result.displayname !== '') {
-                formatted_name += result.displayname;
-            }
-            if (result.namespace !== '') {
-                formatted_name += (result.displayname ? ', ' : '') + result.namespace;
-            }
-            formatted_name += ')';
+        if(!data.length || data.length === 0) {
+            return Spice.failed('search_code');
         }
 
-        return formatted_name;
-    }
+        var result;
+        for (var i = 0; i < data.length; i++) {
+            var checkRelevancy = [data[i].name, data[i].displayname, data[i].namespace].join(" ");
+            if (DDG.isRelevant(checkRelevancy, [], 2)) {
+                result = data[i];
+                break;
+            }
+        }
 
-    Spice.add({
-        data             : result,
-        header1          : formatName(result),
-        sourceUrl       : 'https://searchcode.com/?q=' + query,
-        sourceName      : 'search[code]',
-        templates: {
-            item: Spice.search_code.search_code,
-            detail: Spice.search_code.search_code
-        },
-        
-    });
-}
+        if (!result) {
+            return Spice.failed('search_code');
+        }
+
+        function formatName(result) {
+            var formatted_name = result.name;
+
+            if ((result.displayname && result.displayname !== '') || (result.namespace && result.namespace !== '')) {
+                formatted_name += ' (';
+                if (result.displayname && result.displayname !== '') {
+                    formatted_name += result.displayname;
+                }
+                if (result.namespace && result.namespace !== '') {
+                    formatted_name += (result.displayname ? ', ' : '') + result.namespace;
+                }
+                formatted_name += ')';
+            }
+
+            return formatted_name;
+        }
+
+        result.title = formatName(result);
+
+        Spice.add({
+            id: 'search_code',
+            name: 'Answer',
+            data: result,
+            meta: {
+                sourceURL: 'https://searchcode.com/?q=' + query,
+                sourceName: 'search[code]',
+                sourceIcon: true
+            },
+            templates: {
+                group: 'base',
+                options: {
+                    content: Spice.search_code.search_code,
+                    moreAt: true
+                }
+            }
+        });
+    }
+}(this));
