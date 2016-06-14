daac8f0c2efa2061440071ee0d9885dc2b28c9d6
John Barrett <john@jbrt.org>
2016-06-14 18:54:36 +0100

    Get 'More at MetaCPAN' URL from query

--- share/spice/meta_cpan/meta_cpan.js
+++ share/spice/meta_cpan/meta_cpan.js
@@ -2,6 +2,10 @@
     env.ddg_spice_meta_cpan = function(api_result) {
         "use strict";
 
+        var script = $('[src*="/js/spice/meta_cpan/"]')[0];
+        var source = $(script).attr("src");
+        var query = source.match(/meta_cpan\/([^\/]*)/)[1];
+
         if ( !api_result || api_result.hits.hits.length < 1 ) {
             return DDH.failed('meta_cpan');
         }
@@ -12,7 +16,8 @@
             data: api_result.hits.hits,
             meta: {
                 itemType: 'Software',
-                sourceName: 'MetaCPAN'
+                sourceName: 'MetaCPAN',
+                sourceUrl: 'https://metacpan.org/search?size=50&search_type=modules&q=' + encodeURIComponent(query)
             },
             normalize: function(item) {
                 if ( !item.fields.module || !item.fields.module[0] ) {
