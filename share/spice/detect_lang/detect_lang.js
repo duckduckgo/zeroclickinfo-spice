16919d07576aaecec4d132f6799760f0e4f1440f
sdua <sahildua2305@gmail.com>
2016-06-15 22:11:16 +0000

    Changed to text template

--- share/spice/detect_lang/detect_lang.js
+++ share/spice/detect_lang/detect_lang.js
@@ -1,5 +1,115 @@
 (function(env) {
     "use strict";
+
+    var getPercentConfidence = function (confidence) {
+        var percentage = Math.round(confidence * 100);
+        return (percentage > 100 ? 100 : percentage) + "% sure";
+    }
+
+    var expandLang = function (language) {
+        var langs = {
+            af: "Afrikaans",
+            am: "Amharic",
+            ar: "Arabic",
+            az: "Azerbaijani",
+            be: "Belarusian",
+            bg: "Bulgarian",
+            bn: "Bengali",
+            bo: "Tibetan",
+            ca: "Catalan",
+            chr: "Cherokee",
+            cs: "Czech",
+            cy: "Welsh",
+            da: "Danish",
+            de: "German",
+            dv: "Dhivehi",
+            el: "Greek",
+            en: "English",
+            es: "Spanish",
+            et: "Estonian",
+            eu: "Basque",
+            fa: "Persian",
+            fi: "Finnish",
+            fil: "Tagalog",
+            fr: "French",
+            ga: "Irish",
+            gl: "Galician",
+            gu: "Gujarati",
+            he: "Hebrew",
+            hi: "Hindi",
+            hr: "Croatian",
+            ht: "Haitian Creole",
+            hu: "Hungarian",
+            hy: "Armenian",
+            id: "Indonesian",
+            is: "Icelandic",
+            it: "Italian",
+            iu: "Inuktitut",
+            ja: "Japanese",
+            ka: "Georgian",
+            km: "Khmer",
+            kn: "Kannada",
+            ko: "Korean",
+            lo: "Laothian",
+            lt: "Lithuanian",
+            lv: "Latvian",
+            mk: "Macedonian",
+            ml: "Malayalam",
+            ms: "Malay",
+            mt: "Maltese",
+            my: "Burmese",
+            nb: "Norwegian",
+            nl: "Dutch",
+            or: "Oriya",
+            pa: "Punjabi",
+            pl: "Polish",
+            pt: "Portuguese",
+            ro: "Romanian",
+            ru: "Russian",
+            si: "Sinhalese",
+            sk: "Slovak",
+            sl: "Slovenian",
+            sq: "Albanian",
+            sr: "Serbian",
+            sv: "Swedish",
+            sw: "Swahili",
+            syr: "Syriac",
+            ta: "Tamil",
+            te: "Telugu",
+            th: "Thai",
+            tr: "Turkish",
+            uk: "Ukrainian",
+            ur: "Urdu",
+            vi: "Vietnamese",
+            yi: "Yiddish",
+            zh: "Chinese"
+        };
+
+        return langs[language] || "";
+    };
+
+    var formatTitle = function (first, second) {
+        var title = "This text is";
+        if (first.isReliable) {
+            title += " definitely ";
+        } else {
+            title += " probably ";
+        }
+        title += expandLang(first.language);
+        title += " (" + getPercentConfidence(first.confidence) + ").";
+        return title;
+    }
+
+    var formatSubtitle = function (second) {
+        var subtitle = null;
+        if (!second) {
+            return subtitle;
+        }
+        subtitle = "But it could also be " + expandLang(second.language);
+        subtitle += " (" + getPercentConfidence(second.confidence) + ").";
+        return subtitle;
+    }
+
     env.ddg_spice_detect_lang = function (api_result) {
 
         // Check for any errors.
@@ -16,89 +126,6 @@
             return 0;
         });
 
-        var expandLang = function(language) {
-            var langs = {
-                af: "Afrikaans",
-                am: "Amharic",
-                ar: "Arabic",
-                az: "Azerbaijani",
-                be: "Belarusian",
-                bg: "Bulgarian",
-                bn: "Bengali",
-                bo: "Tibetan",
-                ca: "Catalan",
-                chr: "Cherokee",
-                cs: "Czech",
-                cy: "Welsh",
-                da: "Danish",
-                de: "German",
-                dv: "Dhivehi",
-                el: "Greek",
-                en: "English",
-                es: "Spanish",
-                et: "Estonian",
-                eu: "Basque",
-                fa: "Persian",
-                fi: "Finnish",
-                fil: "Tagalog",
-                fr: "French",
-                ga: "Irish",
-                gl: "Galician",
-                gu: "Gujarati",
-                he: "Hebrew",
-                hi: "Hindi",
-                hr: "Croatian",
-                ht: "Haitian Creole",
-                hu: "Hungarian",
-                hy: "Armenian",
-                id: "Indonesian",
-                is: "Icelandic",
-                it: "Italian",
-                iu: "Inuktitut",
-                ja: "Japanese",
-                ka: "Georgian",
-                km: "Khmer",
-                kn: "Kannada",
-                ko: "Korean",
-                lo: "Laothian",
-                lt: "Lithuanian",
-                lv: "Latvian",
-                mk: "Macedonian",
-                ml: "Malayalam",
-                ms: "Malay",
-                mt: "Maltese",
-                my: "Burmese",
-                nb: "Norwegian",
-                nl: "Dutch",
-                or: "Oriya",
-                pa: "Punjabi",
-                pl: "Polish",
-                pt: "Portuguese",
-                ro: "Romanian",
-                ru: "Russian",
-                si: "Sinhalese",
-                sk: "Slovak",
-                sl: "Slovenian",
-                sq: "Albanian",
-                sr: "Serbian",
-                sv: "Swedish",
-                sw: "Swahili",
-                syr: "Syriac",
-                ta: "Tamil",
-                te: "Telugu",
-                th: "Thai",
-                tr: "Turkish",
-                uk: "Ukrainian",
-                ur: "Urdu",
-                vi: "Vietnamese",
-                yi: "Yiddish",
-                zh: "Chinese"
-            };
-
-            return langs[language] || "";
-        };
-        Handlebars.registerHelper("DetectLang_expandLang", expandLang);
-
         if(expandLang(api_result.data.detections[0].language) === "") {
             return;
         }
@@ -113,29 +140,29 @@
         // Display the plug-in.
         Spice.add({
             id: 'detect_lang',
+            name: "Answer",
             data: {
                 first: d0,
                 second: d1
             },
-            name: "Answer",
             meta: {
                 sourceUrl: "http://detectlanguage.com/",
                 sourceName: "Detect Language",
             },
             signal: 'high',
+            normalize: function(item) {
+                return {
+                    title: formatTitle(d0, d1),
+                    subtitle: formatSubtitle(d1)
+                }
+            },
             templates: {
-                group: 'base',
+                group: 'text',
                 options: {
-                    content: Spice.detect_lang.content,
                     moreAt: true
                 }
             }
         });
     };
 
-    Handlebars.registerHelper("DetectLang_toPercent", function(confidence) {
-        "use strict";
-        var percentage = Math.round(confidence * 100);
-        return (percentage > 100 ? 100 : percentage) + "% sure";
-    });
 }(this));
