3681823b4a4eb1542b7d56c703684ba16228ed46
John Barrett <john@jbrt.org>
2016-06-20 18:48:55 +0100

    Format date with moment.js

--- share/spice/meta_cpan/meta_cpan.js
+++ share/spice/meta_cpan/meta_cpan.js
@@ -10,52 +10,50 @@
             return DDH.failed('meta_cpan');
         }
 
-        Spice.add({
-            id: "meta_cpan",
-            name: "Software",
-            data: api_result.hits.hits,
-            meta: {
-                itemType: 'Software',
-                sourceName: 'MetaCPAN',
-                sourceUrl: 'https://metacpan.org/search?size=50&search_type=modules&q=' + encodeURIComponent(query)
-            },
-            normalize: function(item) {
-                if ( !item.fields.module || !item.fields.module[0] ||
-                     ( !item.fields.description && !item.fields.abstract )
-                ) {
-                    return null;
-                }
-
-                var description = item.fields.description || item.fields.abstract;
-                var version = item.fields.version || item.fields.module[0].version
-                var date = ( item.fields.date )
-                    ? new Date(item.fields.date).toLocaleDateString('en-US', {
-                        year: 'numeric',
-                        month: 'short',
-                        day: 'numeric' }
-                        )
-                    : ""
-
-                return {
-                    title: item.fields.module[0].name,
-                    url: 'https://metacpan.org/pod/' + item.fields.module[0].name,
-                    description: description,
-                    author: item.fields.author,
-                    version: version,
-                    date: date
-                };
-            },
-            templates: {
-                group: 'text',
-                detail: false,
-                item_detail: false,
-                options: {
-                    footer: Spice.meta_cpan.footer
+        DDG.require('moment.js', function() {
+            Spice.add({
+                id: "meta_cpan",
+                name: "Software",
+                data: api_result.hits.hits,
+                meta: {
+                    itemType: 'Software',
+                    sourceName: 'MetaCPAN',
+                    sourceUrl: 'https://metacpan.org/search?size=50&search_type=modules&q=' + encodeURIComponent(query)
                 },
-                variants: {
-                    tile: 'basic4'
+                normalize: function(item) {
+                    if ( !item.fields.module || !item.fields.module[0] ||
+                         ( !item.fields.description && !item.fields.abstract )
+                    ) {
+                        return null;
+                    }
+
+                    var description = item.fields.description || item.fields.abstract;
+                    var version = item.fields.version || item.fields.module[0].version
+                    var date = ( item.fields.date )
+                        ? moment(item.fields.date).format("DD MMM YYYY")
+                        : ""
+
+                    return {
+                        title: item.fields.module[0].name,
+                        url: 'https://metacpan.org/pod/' + item.fields.module[0].name,
+                        description: description,
+                        author: item.fields.author,
+                        version: version,
+                        date: date
+                    };
+                },
+                templates: {
+                    group: 'text',
+                    detail: false,
+                    item_detail: false,
+                    options: {
+                        footer: Spice.meta_cpan.footer
+                    },
+                    variants: {
+                        tile: 'basic4'
+                    }
                 }
-            }
+            })
         });
     };
 }(this));
