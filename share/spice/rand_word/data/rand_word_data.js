166d8cd2979d576924d855142e883bf5ea067c6a
vbrar <veerpal707@gmail.com>
2016-06-23 15:46:35 +0000

    make second api call using promises

--- share/spice/rand_word/data/rand_word_data.js
+++ share/spice/rand_word/data/rand_word_data.js
@@ -1,56 +1,21 @@
 (function(env) {
-        "use strict";
-        env.ddg_spice_rand_word_data = function(api_result) {
-            if(!api_result || api_result.length === 0) {
-                return Spice.failed('rand_word');
-            }
-            var first = api_result[0];
-            // dividing the whole array into an array of 4 elements each
-            var list_of_list = [];
-            while(api_result.length > 0) {
-                list_of_list.push(api_result.splice(0, 4));
-            }
-            var spiceObj = {
-                id: "rand_word",
-                data: {
-                    title: 'Random Words',
-                    list: list_of_list
-                },
-                name: "Answer",
-                meta: {
-                    sourceUrl: 'https://wordnik.com/randoml',
-                    sourceName: 'Wordnik',
-                    sourceIcon: true
-                },
-                templates: {
-                    group: 'list',
-                    options: {
-                        list_content: Spice.rand_word_data.content,
-                        moreAt: true
-                    }
-                }
-            };
-            if(list_of_list[0].length == 1) {
-             
-                $.getScript("/js/spice/rand_word/fetch_id/" + first.word);
-                
-            }
-            console.log("goodbye")
-             Spice.add(spiceObj);
-        };
- env.ddg_spice_rand_word_fetch_id = function(api_result) {
-    console.log("hello")
+    "use strict";
+    env.ddg_spice_rand_word_data = function(api_result) {
         if(!api_result || api_result.length === 0) {
             return Spice.failed('rand_word');
         }
+        console.log(api_result)
         var first = api_result[0];
-        console.log("hello");
+        // dividing the whole array into an array of 4 elements each
+        var list_of_list = [];
+        while(api_result.length > 0) {
+            list_of_list.push(api_result.splice(0, 4));
+        }
         var spiceObj = {
             id: "rand_word",
             data: {
-                title: "Random Word",
-                word: first.word,
-                definition: first.text
+                title: 'Random Words',
+                list: list_of_list
             },
             name: "Answer",
             meta: {
@@ -59,14 +24,41 @@
                 sourceIcon: true
             },
             templates: {
-                group: 'text',
+                group: 'list',
                 options: {
-                    content: Spice.rand_word.single_word,
+                    list_content: Spice.rand_word_data.content,
                     moreAt: true
                 }
             }
         };
-     console.log("hello");
+        if(list_of_list[0].length == 1) {
+            get_definition(first.word).done(function(api_result) {
+                console.log(2)
+				var first = api_result[0];
+                console.log(first.text);
+                spiceObj.data = {
+                    title: "Random Word",
+                    word: first.word,
+                    definition: first.text
+                };
+                spiceObj.templates = {
+                    group: 'text',
+                    options: {
+                        content: Spice.rand_word_data.single_word,
+                        moreAt: true
+                    }
+                };
+                 console.log('end1');
+                 Spice.add(spiceObj);
+            });
+        }else{
+        console.log('end2');
         Spice.add(spiceObj);
+        }
+    }
+
+    function get_definition(word) {
+        console.log(1);
+        return $.getJSON("/js/spice/rand_word/fetch_id/" + word);
     }
 }(this));
