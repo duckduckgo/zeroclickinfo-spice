8f880933b299e70dcc723eceff844fd4f9bddc83
ssalii <chester.work@gmail.com>
2016-06-25 18:17:41 +0000

    Add second API call endpoint to Spice Add loading preview animation Add tests

--- share/spice/twitch/channel_info/twitch_channel_info.js
+++ share/spice/twitch/channel_info/twitch_channel_info.js
@@ -1,21 +1,25 @@
 (function (env) {
     "use strict";
+    
+    var ID = 'twitch_channel_info';
+    var STREAM_ENDPOINT = '/js/spice/twitch/stream/';
+    
     env.ddg_spice_twitch_channel_info = function(api_result) {
-        // Validate the response (customize for your Spice)
+        // Validate the response
         if (!api_result || api_result.error) {
             return Spice.failed('twitch_channel_info');
         }
-
+        
+        var name = api_result.name.trim().toLowerCase();
+        
         // Render the response
         Spice.add({
-            id: "twitch_channel_info",
-
-            // Customize these properties
-            name: "Games",
+            id: ID,
+            name: "Streaming",
             data: api_result,
             meta: {
                 sourceName: "Twitch",
-                sourceUrl: 'https://www.twitch.tv/' + api_result.name,
+                sourceUrl: 'https://www.twitch.tv/' + name,
                 sourceIconUrl: 'https://www.twitch.tv/favicon.ico',
                 itemType: "Twitch Channel",
             },
@@ -32,17 +36,26 @@
                     content: Spice.twitch_channel_info.twitch_channel_info,
                 }
             },
-            onItemShown: function(item) {
+            // Check stream status after render and load preview if it's LIVE
+            onShow: function() {
                 $.ajaxSetup({ cache: true });
-                $.getJSON('https://api.twitch.tv/kraken/streams/' + item.name, function(data) {
-                    if(data && data.stream && data.stream.preview){
-                        var src = 'https://images.duckduckgo.com/iu/?f=1&u=' + encodeURIComponent(data.stream.preview.medium);
-                        var $img = $('<img src="' + src + '" class="c-info__img  js-detail-img">').on('load', function(){
-                            $('.zci--twitch_channel_info').find('.c-info__img-wrap').addClass('channel__live');
-                            $('.zci--twitch_channel_info').find('.c-info__img-wrap__in').html($img); 
-                        });
-                    }
-                });
+                Spice.getDOM(ID).find('.c-info__img-wrap').addClass('channel__loading');
+                $.getJSON(STREAM_ENDPOINT + name)
+                    .done(function(data) {
+                        if(data && data.stream && data.stream.preview){
+                            var src = 'https://images.duckduckgo.com/iu/?f=1&u=' + encodeURIComponent(data.stream.preview.medium);
+                            var $img = $('<img src="' + src + '" class="c-info__img  js-detail-img">').on('load', function(){
+                                Spice.getDOM(ID).find('.c-info__img-wrap').removeClass('channel__loading').addClass('channel__live');
+                                Spice.getDOM(ID).find('.c-info__img-wrap__in').html($img); 
+                            });
+                        }
+                    })
+                    .fail(function() {
+                        
+                    })
+                    .always(function() {
+                        Spice.getDOM(ID).find('.c-info__img-wrap').removeClass('channel__loading');    
+                    });
             }
         });
     };
