d91eb49dd1526264f9599f449d4659738c5c342a
Jason Dorweiller <jason@duckduckgo.com>
2016-06-27 15:47:33 -0400

    read from separate file

--- share/spice/github/github.js
+++ share/spice/github/github.js
@@ -15,25 +15,26 @@
         if (results.length)
             results = results[0];
 
-        results.infoboxData = [
-            {label: "Stars", value:  results.stargazers_count},
-            {label: "Forks", value: results.forks }, 
-            {label: "Open Issues", value: results.open_issues},
-            {label: "Last Update", value: results.pushed_at}
-        ];
-
         var key = results.full_name.replace(/^.*\//, '');
-        var fatheads = {
-                "jquery": "?q=jquery&ia=about",
-                "react": "?q=react+javascript+library&ia=about",
-                "opencv": "?q=opencv&ia=about",
-                "bootstrap": "?q=bootstrap+front-end+framework&ia=about"
-        };
-
         results.title = key;
         results.url = results.html_url;
         results.imageTile = true;
+        
+        var fatheads;
+        $.getJSON(DDG.get_asset_path('github', 'fatheads.json'), function (data) { fatheads = data; });
 
+        DDG.require("moment.js", function(){
+            results.infoboxData = [
+                {label: "Clone URL", value: results.clone_url},
+                {label: "SSH URL", value: results.ssh_url},
+                {label: "Last Update", value: get_days(results.pushed_at)},
+                {label: "Stars", value:  results.stargazers_count},
+                {label: "Forks", value: results.forks }, 
+                {label: "Open Issues", value: results.open_issues},
+                {label: "Default Branch", value: results.default_branch}
+                
+            ];
+        
             Spice.add({
                 id: "github",
                 name: "Software",
@@ -55,21 +56,44 @@
                     ]
                 },
                 onItemShown: function(item){
-                    var apiReq = fatheads[key] + "&o=json";
                     if(!fatheads[key]){
                         return;
                     }
-                    console.log(apiReq);
-                    $.getJSON(apiReq,
-                        function(data){
-                           item.set({ 
-                               description : data.Abstract, 
-                               image: data.Image
-                           });
-                        }
-                   );
+                    var apiReq = fatheads[key].url + "&o=json";
+                    
+                    // check if abstract is set already to avoid calling
+                    // api again if we switch tabs
+                    if(!fatheads[key].abstract){
+                        $.getJSON(apiReq,
+                            function(data){
+                                fatheads[key].image = data.Image;
+                                fatheads[key].abstract = data.Abstract;
+                            
+                                item.set({ 
+                                    description : fatheads[key].abstract, 
+                                    image: fatheads[key].image
+                                });
+                            });
+                    }
                 }
             });
-
+            
+            function get_days(date) {
+                var last_update = new Date(date),
+                now = new Date(),
+                elapsed_days = days(now - last_update);
+                
+                if (elapsed_days <= 1){
+                    return "Today";
+                }
+                else{
+                    return Math.ceil(elapsed_days) + " days ago";
+                }
+            }
+            
+            function days(ms) {
+                return ms/84400000;
+            }
+        });
     }
 }(this));
