02981c54408cf1616e47be6ceb535e202aa1d270
g.e. link <link@grace.cat>
2016-06-29 17:29:49 +0200

    no longer need to strip b tag because they won't be added in the backend

--- share/spice/news/news.js
+++ share/spice/news/news.js
@@ -51,8 +51,6 @@
         // Check if the title is relevant to the query.
         var goodStories = [];
         for(var i = 0, story; story = api_result.results[i]; i++) {
-        // strip bold from story titles.
-            story.title = story.title.replace(/<b>|<\/b>|:/g, "");
             if (DDG.isRelevant(story.title, skip)) {
                 setSourceOnStory(story);
                 story.sortableDate = parseInt(story.date || 0);
