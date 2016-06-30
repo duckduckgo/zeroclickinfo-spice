2d87aa1b213bdc81d0c7be3277ce4a756c58b73b
ssalii <chester.work@gmail.com>
2016-06-30 23:41:51 +0000

    Remove custom template and some CSS

--- share/spice/twitch/channel_info/twitch_channel_info.js
+++ share/spice/twitch/channel_info/twitch_channel_info.js
@@ -3,6 +3,7 @@
     
     var ID = 'twitch_channel_info';
     var STREAM_ENDPOINT = '/js/spice/twitch/stream/';
+    var BASE = 'https://www.twitch.tv/';
     
     env.ddg_spice_twitch_channel_info = function(api_result) {
         // Validate the response
@@ -19,42 +20,80 @@
             data: api_result,
             meta: {
                 sourceName: "Twitch",
-                sourceUrl: 'https://www.twitch.tv/' + name,
-                sourceIconUrl: 'https://www.twitch.tv/favicon.ico',
-                itemType: "Twitch Channel",
+                sourceUrl: BASE + name,
+                sourceIconUrl: BASE + 'favicon.ico'
             },
             normalize: function(item) {
+                var getLink = function(url, label){
+                    return '<a href="' + url + '" title="">' + label + '</a>';
+                }
                 return {
+                    url: BASE + item.name,
+                    title: item.name,
+                    subtitle: [
+                        'Offline',
+                        new Handlebars.SafeString('Playing <strong>' + getLink(BASE + 'directory/game/' + item.game, item.game) + '</strong>')
+                    ],
+                    game: item.game,
+                    game_url: BASE + item.game, 
                     image: item.video_banner,
-                    views: item.views.toLocaleString(),
-                    followers: item.followers.toLocaleString()  
+                    description: item.status,
+                    infoboxData: [
+                        {
+                            heading: 'Channel info'
+                        },
+                        {
+                            label: 'Followers',
+                            value: item.followers.toLocaleString()
+                        },
+                        {
+                            label: 'Views',
+                            value: item.views.toLocaleString()
+                        }
+                    ]
                 };
             },
             templates: {
-                group: 'media',
+                group: 'info',
                 options: {
-                    content: Spice.twitch_channel_info.twitch_channel_info,
+                    content: Spice.twitch_channel_info.content
                 }
             },
             // Check stream status after render and load preview if it's LIVE
-            onShow: function() {
-                $.ajaxSetup({ cache: true });
+            onItemShown: function(item) {
                 Spice.getDOM(ID).find('.c-info__img-wrap').addClass('channel__loading');
+                
+                $.ajaxSetup({ cache: true });
                 $.getJSON(STREAM_ENDPOINT + name)
                     .done(function(data) {
-                        if(data && data.stream && data.stream.preview){
+                        if(Object.keys(data).length === 0 || !data.stream) return;
+                    
+                        // Replace 'Offline' string with 'Live'
+                        var status = Spice.getDOM(ID).find('.c-info__title__sub').html()
+                        Spice.getDOM(ID).find('.c-info__title__sub').html(status.replace('Offline', 'Online'));
+                    
+                        // Update viewers count
+                        if(data.stream.viewers){
+                            Spice.getDOM(ID).find('.c-info__title__sub')
+                                .append('<span class="sep"></span>' + data.stream.viewers + ' viewers');
+                        }
+                        
+                        // Update stream preview
+                        if(data.stream.preview){
                             var src = 'https://images.duckduckgo.com/iu/?f=1&u=' + encodeURIComponent(data.stream.preview.medium);
-                            var $img = $('<img src="' + src + '" class="c-info__img  js-detail-img">').on('load', function(){
+                            var $img = $('<img src="' + src + '" class="c-info__img  js-detail-img">');
+                            Spice.getDOM(ID).find('.c-info__img-wrap__in').html($img);
+                            $img.on('load', function(){
                                 Spice.getDOM(ID).find('.c-info__img-wrap').removeClass('channel__loading').addClass('channel__live');
-                                Spice.getDOM(ID).find('.c-info__img-wrap__in').html($img); 
                             });
                         }
+                        
                     })
                     .fail(function() {
                         
                     })
                     .always(function() {
-                        Spice.getDOM(ID).find('.c-info__img-wrap').removeClass('channel__loading');    
+                        Spice.getDOM(ID).find('.c-info__img-wrap').removeClass('channel__loading');  
                     });
             }
         });
