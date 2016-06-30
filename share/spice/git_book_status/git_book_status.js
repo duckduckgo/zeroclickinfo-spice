06ac9071f46fd23756f93821b5101da089d8d8d7
qqq3 <aiqcms@gmail.com>
2016-06-30 16:09:43 +0300

    Change type template

--- share/spice/git_book_status/git_book_status.js
+++ share/spice/git_book_status/git_book_status.js
@@ -5,34 +5,34 @@
         if (!api_result) {
             return Spice.failed('git_book_status');
         }
-        var dataDetails = {
-            status: api_result.status.indicator,
-            description: api_result.status.description
-        }
 
         Spice.add({
             id: "git_book_status",
             name: "Answer",
-            data: dataDetails,
+            data: api_result,
+            normalize: function(item) {
+                return {
+                    status: item.status.indicator,
+                    subtitle: item.status.description
+                };
+            },
             meta: {
                 sourceName: api_result.page.name,
                 sourceUrl: api_result.page.url
             },
             templates: {
-                group: 'base',
+                group: 'text',
                 options: {
-                    content: Spice.git_book_status.content,
+                    title_content: Spice.git_book_status.title_content,
                     moreAt: true
                 }
             }
         });
     }
 
+
     Spice.registerHelper("GitbookStatus_indicator", function(string1, string2, options) {
        return ((string1 == string2) ? options.fn(this) : options.inverse(this)); 
     });
 
-    Spice.registerHelper("GitbookStatus_ifNotEmpty", function(string, options) {
-        return ((typeof(string) !== "undefined" && string !== '') ? options.fn(this) : options.inverse(this));
-    });
 }(this));
