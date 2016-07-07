111d1c48e8701988a873a908e143f9c4e912be1e
John Barrett <john@jbrt.org>
2016-07-07 14:44:12 +0100

    Attempt to extract module name when additional words are in query

--- lib/DDG/Spice/MetaCPAN.pm
+++ lib/DDG/Spice/MetaCPAN.pm
@@ -15,6 +15,7 @@ triggers startend => "cpan", "cpanm", "metacpan", "mcpan", "meta cpan", "perl mo
 handle remainder => sub {
     if ($_) {
         $_ =~ s/-/::/g;
+        $_ = ( grep { /::/ } split /\s+/, $_ )[0] || $_;
         my $clean = $_ =~ s/::/ /gr;
         return $_, $clean;
     }
