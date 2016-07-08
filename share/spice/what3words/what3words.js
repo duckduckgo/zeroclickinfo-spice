2045b6a5866d611271e42aab6d18cd442758f457
Zaahir Moolla <moollaza@duckduckgo.com>
2016-07-08 15:03:33 -0400

    Show address and city using ArcGIS API

--- share/spice/what3words/what3words.js
+++ share/spice/what3words/what3words.js
@@ -6,31 +6,36 @@
             return Spice.failed('what3words');
         }
 
-        DDG.require('maps', function () {
-            Spice.add({
-                id: "what3words",
-                name: "Map",
-                data: {
-                    display_name: api_result.words,
-                    name: api_result.words,
-                    lat: api_result.geometry.lat,
-                    lon: api_result.geometry.lng,
-                    displayLatLon: api_result.geometry.lat + ", " + api_result.geometry.lng
-                },
-                model: 'Place',
-                view: 'Map',
-                meta: {
-                    zoomLevel: 15,
-                    sourceName: "What3Words",
-                    sourceUrl: api_result.map
-                },
-                templates: {
-                    group: 'map',
-                    options: {
-                        moreAt: true
-                    }
-                }
-            });
-        })
+        var location =  encodeURIComponent( api_result.geometry.lng + "," + api_result.geometry.lat );
+        $.getJSON('/js/spice/what3words_arcgis/' + location, function(data) {
+            if (data.address){
+                DDG.require('maps', function () {
+                    Spice.add({
+                        id: "what3words",
+                        name: "Map",
+                        data: {
+                            name: api_result.words,
+                            lat: api_result.geometry.lat,
+                            lon: api_result.geometry.lng,
+                            displayLatLon: api_result.geometry.lat + ", " + api_result.geometry.lng,
+                            address: data.address.Address + ", " + data.address.City
+                        },
+                        model: 'Place',
+                        view: 'Map',
+                        meta: {
+                            zoomLevel: 15,
+                            sourceName: "What3Words",
+                            sourceUrl: api_result.map
+                        },
+                        templates: {
+                            group: 'map',
+                            options: {
+                                moreAt: true
+                            }
+                        }
+                    });
+                });
+            }
+        });
     };
 }(this));
