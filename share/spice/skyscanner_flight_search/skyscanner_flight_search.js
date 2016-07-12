42d207f6493085449a361a5b5a8942f1d430a5b6
sheyman <sam.heyman@gmail.com>
2016-07-12 17:23:25 +0000

    adding logic to remove any flights with no price

--- share/spice/skyscanner_flight_search/skyscanner_flight_search.js
+++ share/spice/skyscanner_flight_search/skyscanner_flight_search.js
@@ -28,25 +28,26 @@
             var outbound_date = "";
             var return_date = "";
             
-            price = current.Price; 
-            destination = placesById[current.DestinationId].Name;
-            quote_ref = (current.QuoteIds) ? ((current.QuoteIds.length >1) ? current.QuoteIds[0]: current.QuoteIds) : '';
+            if (current.Price) {
+                price = current.Price; 
+                destination = placesById[current.DestinationId].Name;
+                quote_ref = (current.QuoteIds) ? ((current.QuoteIds.length >1) ? current.QuoteIds[0]: current.QuoteIds) : '';
             
-            quote_date_time = (quote_ref != '') ? quotesById[quote_ref].QuoteDateTime : "N/A";
+                quote_date_time = (quote_ref != '') ? quotesById[quote_ref].QuoteDateTime : "N/A";
             
-            outbound_date = (quote_ref != '') ? quotesById[quote_ref].OutboundLeg.DepartureDate : "N/A";
-            return_date = (quote_ref != '') ? quotesById[quote_ref].InboundLeg.DepartureDate : "N/A";
+                outbound_date = (quote_ref != '') ? quotesById[quote_ref].OutboundLeg.DepartureDate : "N/A";
+                return_date = (quote_ref != '') ? quotesById[quote_ref].InboundLeg.DepartureDate : "N/A";
             
-            flights.push({
-                flight_price: price,
-                flight_destination: destination,
-                flight_quote: quote_ref,
-                flight_datetime: quote_date_time,
-                flight_outbound_date: outbound_date,
-                flight_return_date: return_date
-            });
+                flights.push({
+                    flight_price: price,
+                    flight_destination: destination,
+                    flight_quote: quote_ref,
+                    flight_datetime: quote_date_time,
+                    flight_outbound_date: outbound_date,
+                    flight_return_date: return_date
+                });
+            }
         }
-        
         
         // Fail if no flights to show
         if (flights.length === 0) {
@@ -67,13 +68,11 @@
                 itemType: (flights.length === 1) ? 'Result' : 'Results'
             },
             normalize: function(item) {
-
                 return {
                     // customize as needed for your chosen template
                     title: "£" + item.flight_price,
                     subtitle: item.flight_destination,
                     description: "Outbound: " + item.flight_outbound_date + ", return: " + item.flight_return_date 
-                    
                 };
             },
             templates: {
