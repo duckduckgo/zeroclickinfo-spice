77d2534d8a604e66d493942514030b85097d4989
sheyman <sam.heyman@gmail.com>
2016-07-12 16:57:37 +0000

    adding the destination, departure date and arrival date lookup using the reduce function

--- share/spice/skyscanner_flight_search/skyscanner_flight_search.js
+++ share/spice/skyscanner_flight_search/skyscanner_flight_search.js
@@ -14,26 +14,36 @@
         var flights = [];
         var routes = api_result.Routes;
         var placesById = api_result.Places.reduce(function (result, place) { result[place.PlaceId] = place; return result; }, {});
-        var quotes = api_result.Quotes;
+        var quotesById = api_result.Quotes.reduce(function (result, quote) { result[quote.QuoteId] = quote; return result; }, {});
         
         // iterate through the routes and build the flights array with:
         // price, destination, o/b date, i/b date and carrier
         
         for (var i=0; i < routes.length; i++) {
             var current = routes[i];
-            var flight_price = "";
-            var flight_destination = "";
+            var price = "";
+            var destination = "";
+            var quote_ref = "";
+            var quote_date_time = "";
+            var outbound_date = "";
+            var return_date = "";
             
-            function findPlaceById(places, id) { 
-                return places.PlaceId === id;
-            }
+            price = current.Price; 
+            destination = placesById[current.DestinationId].Name;
+            quote_ref = (current.QuoteIds) ? ((current.QuoteIds.length >1) ? current.QuoteIds[0]: current.QuoteIds) : '';
             
-            flight_price = current.Price; 
-            flight_destination = placesById[current.DestinationId].Name;
+            quote_date_time = (quote_ref != '') ? quotesById[quote_ref].QuoteDateTime : "N/A";
+            
+            outbound_date = (quote_ref != '') ? quotesById[quote_ref].OutboundLeg.DepartureDate : "N/A";
+            return_date = (quote_ref != '') ? quotesById[quote_ref].InboundLeg.DepartureDate : "N/A";
             
             flights.push({
-                price: flight_price,
-                destination: flight_destination
+                flight_price: price,
+                flight_destination: destination,
+                flight_quote: quote_ref,
+                flight_datetime: quote_date_time,
+                flight_outbound_date: outbound_date,
+                flight_return_date: return_date
             });
         }
         
@@ -60,18 +70,22 @@
 
                 return {
                     // customize as needed for your chosen template
-                    title: "£" + item.price,
-                    subtitle: item.destination
+                    title: "£" + item.flight_price,
+                    subtitle: item.flight_destination,
+                    description: "Outbound: " + item.flight_outbound_date + ", return: " + item.flight_return_date 
+                    
                 };
             },
             templates: {
                 group: 'text',
                 detail: false,
-                item_detail: false,                
+                item_detail: false,
                 variants: {
-                    tileTitle: "3line-small",
-                    tileFooter: "3line"
-                }
+                    tile: 'basic1',
+                    tileTitle: '1line',
+                    tileFooter: '2line',
+                    tileSnippet: 'large'
+                },
             }
         });
     };
