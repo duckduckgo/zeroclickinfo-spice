cbb863fc1768f3de284bca10a5f8b0a1fafad4db
Zaahir Moolla <moollaza@duckduckgo.com>
2016-07-14 14:48:53 -0400

    Improve query validation

--- lib/DDG/Spice/PackageTracking.pm
+++ lib/DDG/Spice/PackageTracking.pm
@@ -1,6 +1,7 @@
 package DDG::Spice::PackageTracking;
 
 use DDG::Spice;
+use Text::Trim;
 use YAML::XS 'LoadFile';
 
 spice is_cached => 1;
@@ -11,12 +12,19 @@ spice wrap_jsonp_callback => 1;
 spice to => 'https://api.packagetrackr.com/ddg/v1/track/simple?n=$1&api_key={{ENV{DDG_SPICE_PACKAGETRACKR_API_KEY}}}';
 
 my $carriers = LoadFile(share('carriers.yml'));
+my @triggers = ('package', 'track package', 'shipping status', 'package tracking');
+my $triggers_re = join "|", @triggers;
+my $carriers_re = join "|", @$carriers;
 
-triggers startend => @$carriers, 'package', 'track package', 'shipping status', 'package tracking';
+triggers startend => @$carriers, @triggers;
 
-handle remainder => sub {
-    return $_ if $_;
-    return;
+handle remainder_lc => sub {
+    return unless $_;
+    s/\b$triggers_re\b//;
+    s/\b$carriers_re\b//;
+    trim($_);
+    return unless m/^[a-z0-9\-]+$/;
+    return $_;
 };
 
 1;
