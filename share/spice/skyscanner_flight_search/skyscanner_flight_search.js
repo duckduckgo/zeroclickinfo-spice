b6c0ad3c1085410d6cf0ed782589aaa26d161850
sheyman <sam.heyman@gmail.com>
2016-07-14 15:54:22 +0000

    adding more info to the tiles and additional meta data

--- share/spice/skyscanner_flight_search/skyscanner_flight_search.js
+++ share/spice/skyscanner_flight_search/skyscanner_flight_search.js
@@ -22,7 +22,8 @@
         for (var i=0; i < routes.length; i++) {
             var current = routes[i];
             var price = "";
-            var destination = "";
+            var destination_city = "";
+            var destination_airport = "";
             var quote_ref = "";
             var quote_date_time = "";
             var outbound_date = "";
@@ -30,7 +31,9 @@
             
             if (current.Price) {
                 price = current.Price; 
-                destination = placesById[current.DestinationId].Name;
+                destination_city = placesById[current.DestinationId].CityName;
+                destination_airport = placesById[current.DestinationId].SkyscannerCode;
+                
                 quote_ref = (current.QuoteIds) ? ((current.QuoteIds.length >1) ? current.QuoteIds[0]: current.QuoteIds) : '';
             
                 quote_date_time = (quote_ref != '') ? quotesById[quote_ref].QuoteDateTime : "N/A";
@@ -40,7 +43,8 @@
             
                 flights.push({
                     flight_price: price,
-                    flight_destination: destination,
+                    flight_destination_city: destination_city,
+                    flight_destination_airport: destination_airport,
                     flight_quote: quote_ref,
                     flight_datetime: quote_date_time,
                     flight_outbound_date: outbound_date,
@@ -58,34 +62,64 @@
         Spice.add({
             id: 'skyscanner_flight_search',
 
-            // Customize these properties
+            // This is the result tab header
             name: 'Flights',
+            // data to pass to the template
             data: flights,
             meta: {
                 sourceName: 'Skyscanner',
                 sourceUrl: 'Skyscanner.net',
                 total: flights,
-                itemType: (flights.length === 1) ? 'Result' : 'Results'
+                // You can either show Results: or customised text (primaryText)
+                itemType: (flights.length === 1) ? 'Result' : 'Results',
+                primaryText: "Flights from Edinburgh to your destination",
+                // add regex to retrieve destination from search query
+                searchTerm: 'flights to <your destination>',
+                sourceLogo: {
+                    url: 'https://upload.wikimedia.org/wikipedia/en/9/90/SkyscannerLogo.png',
+                    width: '70px',
+               
+                }
             },
-            normalize: function(item) {
-                return {
-                    // customize as needed for your chosen template
-                    title: "£" + item.flight_price,
-                    subtitle: item.flight_destination,
-                    description: "Outbound: " + item.flight_outbound_date + ", return: " + item.flight_return_date 
-                };
-            },
+            
             templates: {
                 group: 'text',
                 detail: false,
                 item_detail: false,
-                variants: {
-                    tile: 'basic1',
-                    tileTitle: '1line',
-                    tileFooter: '2line',
-                    tileSnippet: 'large'
+                options: {
+                    //footer: true,
+                    dateBadge: true,
                 },
-            }
+                // use variants to modify the template's visual appearance, pre-determined css classes (or combinations of classes) from the DDG style guide
+                variants: {
+                    //tile: 'basic4',
+                    tileTitle: '1line-small',
+                    // snippet can be large or small (height of the tile)
+                    tileSnippet: 'small',
+                    tileSubtitle: '1line',
+                    //tileFooter: '2line',
+                    
+                    //detail: 'dark'
+                }
+                // ariants don't suffice in customizing your templates' appearance, you may directly specify classes from the DDG style guide through the 
+                // elClass property of templates. This feature is mainly used for specifying text size and color.
+                // elClass {
+                //    ...
+                // }
+            },
+                  
+            normalize: function(item) {
+                return {
+                    // customize as needed for your chosen template
+                    icon: 'http://s1.apideeplink.com/images/airlines/BE.png',
+                    title: item.flight_destination_city,
+                    altSubtitle: (item.flight_destination_airport === item.flight_destination_city) ? ' ' : item.flight_destination_airport,
+                    //subtitle: "Outbound: " + item.flight_outbound_date + ", return: " + item.flight_return_date,
+                    description: "from £" + item.flight_price,
+                    //footer: 'url here',
+                    //dateBadge: item.flight_outbound_date
+                };
+            },
         });
     };
     
