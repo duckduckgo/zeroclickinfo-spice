210a842c6fcc02834e676fb09dc35e6e2843e6ad
Gautam krishna.R <rgautamkrishna@gmail.com>
2016-08-13 13:27:02 +0530

    Update DogoBooks.pm

--- lib/DDG/Spice/DogoBooks.pm
+++ lib/DDG/Spice/DogoBooks.pm
@@ -2,12 +2,15 @@ package DDG::Spice::DogoBooks;
 
 use DDG::Spice;
 
-triggers any => "dogobooks", "dogo books", "dogo", "kids", "kid", "child", "children";
+triggers startend => "dogobooks", "dogo books", "dogo";
+triggers start => "kids", "kid", "child", "children";
+triggers end => "books";
 
 spice to => 'http://api.dogomedia.com/api/v2/books/search.json?query=$1&api_key={{ENV{DDG_SPICE_DOGO_APIKEY}}}';
 spice wrap_jsonp_callback => 1;
 
 handle query_lc => sub {
+    
     return $_ if $_ =~ /book/i;
     return;
 };
