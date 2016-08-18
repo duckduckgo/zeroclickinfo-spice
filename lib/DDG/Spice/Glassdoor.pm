5937051f00286c720235c277f13b5d5b04274f94
Kevin Thomas <kevintab@tutanota.com>
2016-08-18 20:13:07 +0530

    Update Glassdoor.pm

--- lib/DDG/Spice/Glassdoor.pm
+++ lib/DDG/Spice/Glassdoor.pm
@@ -11,7 +11,7 @@ spice proxy_cache_valid => '200 1d';
 
 spice wrap_jsonp_callback => 1; 
 
-spice to => 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p={{ENV{DDG_SPICE_GLASSDOOR_PARTNERID}}}&t.k={{ENV{DDG_SPICE_GLASSDOOR_APIKEY}}}&action=employers&q=$1&userip=0.0.0.0&useragent=Mozilla/%2F4.0'; #https://hn.algolia.com/api/v1/search?query=$1&tags=story
+spice to => 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p={{ENV{DDG_SPICE_GLASSDOOR_PARTNERID}}}&t.k={{ENV{DDG_SPICE_GLASSDOOR_APIKEY}}}&action=employers&q=$1&userip=0.0.0.0&useragent=Mozilla/%2F4.0';
 
 triggers startend => "glassdoor";
 
