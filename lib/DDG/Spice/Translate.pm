51bb871a5c4b2c34a7c8e79692fdccd25e6aa852
David Sevilla Martín <dsevilla192@icloud.com>
2016-10-30 16:24:22 +0000

    Corrected the Translate.pm content, it has QuoteOfTheDay from tried testing

--- lib/DDG/Spice/Translate.pm
+++ lib/DDG/Spice/Translate.pm
@@ -2,45 +2,25 @@ package DDG::Spice::Translate;
 
 # ABSTRACT: Translator
 
-# # Start at http://docs.duckduckhack.com/walkthroughs/forum-lookup.html if you are new
-# # to instant answer development
-
-# use DDG::Spice;
-# # use strict;
-# # use warnings;
-
-# spice wrap_jsonp_callback => 1;
-# spice to => 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=$1';
-
-# triggers start => 'translate', 'translate english';
-
-# # Handle statement
-# handle remainder => sub {
-
-#     # Query is in $_ or @_, depending on the handle you chose...if you
-#     # need to do something with it before returning
-#     # return $_ if $_;
-#     return '';
-# };
-
-# 1;
+# Start at http://docs.duckduckhack.com/walkthroughs/forum-lookup.html if you are new
+# to instant answer development
 
 use DDG::Spice;
-
-spice is_cached => 1;
-spice proxy_cache_valid => "200 1d";
+use strict;
+use warnings;
 
 spice wrap_jsonp_callback => 1;
-spice to => 'http://quotes.rest/qod.json';
+spice to => 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=$1';
 
-triggers start => 'quote of the day','quote for the day';
+triggers start => 'translate', 'translate english';
 
 # Handle statement
 handle remainder => sub {
 
-    # Query is in $_...if you need to do something with it before returning
-    return if $_; # bail out because there is a remainder
-    return '';    #return an empty string to make API call
+    # Query is in $_ or @_, depending on the handle you chose...if you
+    # need to do something with it before returning
+    return $_ if $_;
+    return '';
 };
 
 1;
