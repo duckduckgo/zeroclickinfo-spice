fc4a67aa4d3ec45767925efe98c5aa68b51c72aa
David Sevilla Martín <dsevilla192@icloud.com>
2016-10-30 17:30:02 +0000

    Removed comments from translate.js, they were code from testing with QuoteOfTheDay. Whoops

--- share/spice/translate/translate.js
+++ share/spice/translate/translate.js
@@ -51,39 +51,3 @@
         });
     };
 }(this));
-
-// (function (env) {
-//     "use strict";
-//     env.ddg_spice_quote_of_the_day = function(api_result) {
-
-//         if (!api_result || api_result.error) {
-//             return Spice.failed('quote_of_the_day');
-//         }
-
-//         Spice.add({
-//             id: "quote_of_the_day",
-//             signal:"high",
-//             name: "Quote of the Day",
-//             data: api_result,
-//             meta: {
-//                 sourceName: "They Said So",
-//                 sourceUrl: "https://theysaidso.com/quote/"+api_result.contents.quotes[0].id
-//             },
-//             normalize: function(item) {
-//                 return {
-//                     quote : item.contents.quotes[0].quote,
-//                     person: item.contents.quotes[0].author,
-//                     len : parseInt(item.contents.quotes[0].length) + item.contents.quotes[0].author.length
-//                 };
-//             },
-//             templates: {
-//                 group: 'base',
-//                 options: {
-//                     content: Spice.quote_of_the_day.content,
-//                     moreAt: true
-//                 }
-//             }
-//         });
-//     };
-// }(this));
-
