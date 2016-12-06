0fb90996ef1ce8d7239bd3043c89a141dc119bdd
Jeyadoss Manimaran <jeyadossmanimaran@gmail.com>
2016-12-06 12:38:07 +0000

    Reviewed the changes to issue #3050

--- lib/DDG/Spice/Maven.pm
+++ lib/DDG/Spice/Maven.pm
@@ -4,15 +4,22 @@ package DDG::Spice::Maven;
 use strict;
 use DDG::Spice;
 
-triggers startend => "maven", "mvn","library","repository","repo","package";
-
+triggers any => "maven", "mvn","repository","repo","java library","package","library java","lib java","java lib","module java","java module";
+# triggers any=> "/([a-z].+)?.?(java|maven|mvn).+?([a-z].+)?.+?(repository|repo|library|package)/ig","/([a-z].+)?.+?(repository|repo|library|package).+?([a-z].+)?.+?(java|maven|mvn)/ig";
 spice to => 'http://search.maven.org/solrsearch/select?q=$1&rows=5&wt=json&callback={{callback}}';
 spice wrap_jsonp_callback => 1;
 spice proxy_cache_valid   => "418 1d";
 
 handle remainder => sub {
-    return $_ if $_;
-    return;
+if($req->matched_trigger=~ m/java (library|lib|module)/ |$req->matched_trigger=~ m/(lib|library|module) java/ )
+{
+return $req->matched_trigger
+}
+else
+{
+return $_ if $_;
+return;
+}
 };
 
 1;
