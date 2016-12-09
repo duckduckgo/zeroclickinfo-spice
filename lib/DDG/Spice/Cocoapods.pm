744381e64bc1f333e01683e452049482c5978ece
Zaahir Moolla <moollaza@duckduckgo.com>
2016-12-08 19:43:12 -0500

    Set empty referer header to make cocoapoads api happy

--- lib/DDG/Spice/Cocoapods.pm
+++ lib/DDG/Spice/Cocoapods.pm
@@ -8,6 +8,7 @@ triggers startend => 'cocoapods';
 
 spice to => 'http://search.cocoapods.org/api/v1/pods.flat.hash.json?query=$1';
 spice wrap_jsonp_callback => 1;
+spice headers => { Referer => '' };
 
 handle remainder => sub {
 	return lc $_ if $_;
