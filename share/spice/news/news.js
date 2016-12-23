bfb84a6a7cdfbd2cd338e57affb935694c2aaecd
Brian Stoner <brian@duckduckgo.com>
2016-12-22 19:46:12 -0500

    kill all news relevancy

--- share/spice/news/news.js
+++ share/spice/news/news.js
@@ -2,87 +2,37 @@
     "use strict";
     env.ddg_spice_news = function (api_result) {
 
-        if (!api_result || !api_result.results) {
+        if (!api_result || !api_result.results || !api_result.results.length) {
             return Spice.failed('news');
         }
-
-        // Words that we have to skip in DDG.isRelevant.
-        var skip = [
-            "news",
-            "headline",
-            "headlines",
-            "latest",
-            "breaking",
-            "update",
-            "s:d",
-            "sort:date"
-        ];
 
         // Some sources need to be set by us.
         var setSourceOnStory = function(story) {
             switch(story.syndicate) {
-            case "Topsy":
-                story.source = story.author || "Topsy";
-                break;
-            case "NewsCred":
-                if(story.source) {
-                    if(story.author) {
-                        story.source = story.source + " by " + story.author;
+                case "Topsy":
+                    story.source = story.author || "Topsy";
+                    break;
+                case "NewsCred":
+                    if(story.source) {
+                        if(story.author) {
+                            story.source = story.source + " by " + story.author;
+                        }
+                    } else {
+                        story.source = "NewsCred";
                     }
-                } else {
-                    story.source = "NewsCred";
+                    break;
                 }
-                break;
-            }
-        };
-
-        var entityWords = [];
-        if (Spice.news && Spice.news.entities && Spice.news.entities.length) {
-            for (var j = 0, entity; entity = Spice.news.entities[j]; j++) {
-                var tmpEntityWords = entity.split(" ");
-                for (var k = 0, entityWord; entityWord = tmpEntityWords[k]; k++) {
-                    if (entityWord.length > 3) {
-                        entityWords.push(entityWord);
-                    }
-                }
-            }
-        }
+            };
 
         // Check if the title is relevant to the query.
         var goodStories = [];
         for(var i = 0, story; story = api_result.results[i]; i++) {
-            if (DDG.isRelevant(story.title, skip)) {
-                setSourceOnStory(story);
-                story.sortableDate = parseInt(story.date || 0);
-                goodStories.push(story);
-            }
-
-            // additional news relevancy for entities. story need only
-            // contain one word from one entity to be good. strict indexof
-            // check though.
-            else if (entityWords.length > 0) {
-                var storyOk = 0;
-                var tmpStoryTitle = story.title.toLowerCase();
-
-                for (var k = 0, entityWord; entityWord = entityWords[k]; k++) {
-                    if (tmpStoryTitle.indexOf(entityWord) !== -1) {
-                        storyOk = 1;
-                        break;
-                    }
-                }
-
-                if (storyOk) {
-                    setSourceOnStory(story);
-                    goodStories.push(story);
-                }
-            }
+            setSourceOnStory(story);
+            story.sortableDate = parseInt(story.date || 0);
+            goodStories.push(story);
         }
 
         var searchTerm = DDG.get_query().replace(/(?: news|news ?)/i, '').trim();
-
-        if (goodStories < 3) {
-            return Spice.failed('news');
-        }
 
         Spice.add({
             id: 'news',
@@ -90,7 +40,7 @@
             data: goodStories,
             ads: api_result.ads,
             meta: {
-		idField: 'url',
+                idField: 'url',
                 count: goodStories.length,
                 searchTerm: searchTerm,
                 itemType: 'Recent News',
