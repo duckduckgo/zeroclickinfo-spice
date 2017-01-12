08bb6ae3e3ee1929ea2541cb0301626e1b2ff736
Brian Stoner <brian@duckduckgo.com>
2017-01-12 13:48:30 -0500

    update to the right experiment id

--- share/spice/news/news.js
+++ share/spice/news/news.js
@@ -6,7 +6,7 @@
             return Spice.failed('news');
         }
 
-        var useRelevancy = DDG.opensearch.installed.experiment === "organic_ux" && DDG.opensearch.installed.variant === 'b',
+        var useRelevancy = DDG.opensearch.installed.experiment === "static_news" && DDG.opensearch.installed.variant === 'b',
             entityWords = [],
             goodStories = [],
             searchTerm = DDG.get_query().replace(/(?: news|news ?)/i, '').trim(),
