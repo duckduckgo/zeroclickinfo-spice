60edd812627a22e60c5c79fb11918553cd0e4181
Brian Stoner <brian@duckduckgo.com>
2017-01-23 10:55:07 -0500

    skip relevancy for maps prototype

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -3,6 +3,17 @@ DDG.require('maps',function(){
 
         if (!response || !response.features || !response.features.length) { return Spice.failed('maps_maps'); }
 
+        // TEMP: disable relevancy and pass all results through
+        // for map module experiment:
+        if (DDG.opensearch.installed.experiment === 'map_module' && DDG.opensearch.installed.variant === 'a') {
+            return Spice.add({
+                data: response.features,
+                id: 'maps_maps',
+                name: 'maps',
+                model: 'Place'
+            });
+        }
+
         // Mapbox sends back a bunch of places, just want the first one for now
         response = response.features[0];
 
