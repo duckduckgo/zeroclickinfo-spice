69f8ca4b857194deca29b53be38a5af3c3e7af38
synthtech 0 <synthtech0@gmail.com>
2017-01-25 01:17:28 +0000

    Reduce size of response

--- lib/DDG/Spice/Anime.pm
+++ lib/DDG/Spice/Anime.pm
@@ -4,7 +4,7 @@ use strict;
 use DDG::Spice;
 
 spice proxy_cache_valid => "200 30d";
-spice to => 'http://kitsu.io/api/edge/anime?filter[text]=$1&page[limit]=20';
+spice to => 'http://kitsu.io/api/edge/anime?filter[text]=$1&page[limit]=20&fields[anime]=slug,synopsis,canonicalTitle,averageRating,posterImage,episodeCount,episodeLength';
 
 triggers startend => 'anime', 'kitsu';
 spice wrap_jsonp_callback => 1;
