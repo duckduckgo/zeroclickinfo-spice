be459c31c2fdf4dd4041801f056d047a426b235f
Brian Stoner <brian@duckduckgo.com>
2017-01-24 20:45:04 -0500

    remove frontend maps relevancy

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -3,17 +3,8 @@ DDG.require('maps',function(){
 
         if (!response || !response.features || !response.features.length) { return Spice.failed('maps_maps'); }
 
-        // Mapbox sends back a bunch of places, just want the first one for now
-        response = response.features[0];
-
-        var skipArray = [ "directions", "map", "maps", "st", "street", "ave", "avenue", "dr", "drive", "pl", "place", "apt", "suite" ];
-
-        if ( response.relevance < 0.9 && !( response.place_name && DDG.isRelevant(response.place_name.toLowerCase(), skipArray)) ) {
-            return Spice.failed('maps_maps');
-        }
-
         Spice.add({
-            data: response,
+            data: response.features[0],
             id: "maps_maps",
             name: "maps",
             model: "Place"
