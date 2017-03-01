aadbde85a941c1886000e94a0b77285f1a36119d
knowledgewarrior <knowledgewarrior@gmail.com>
2017-03-01 12:39:28 -0800

    Libraries.io #3101

---
+++ lib/DDG/Spice/LibrariesIO.pm
@@ -0,0 +1,18 @@
+package DDG::Spice::LibrariesIO;
+# ABSTRACT: Show a summary of the searched CPAN module.
+
+use strict;
+use DDG::Spice;
+
+spice to => 'https://libraries.io/api/search?q=$1';
+spice is_cached => 1;
+spice proxy_cache_valid => "200 7d";
+
+triggers startend => "go", "wordpress", "clojars", "atom", "homebrew", "elm", "scala", "dart", "groovy", "emacs", "pypi", "bower";
+
+handle remainder => sub {
+    my ($term) = @_;
+    return $term if $term;
+    return;
+};
+1;
