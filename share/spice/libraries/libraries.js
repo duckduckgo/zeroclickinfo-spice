2eddf9f30fc1ac576c0ac9ba440a2d0bc71cd605
knowledgewarrior <knowledgewarrior@gmail.com>
2017-03-02 19:12:08 -0800

    Libraries.io #3101

--- share/spice/libraries/libraries.js
+++ share/spice/libraries/libraries.js
@@ -21,40 +21,82 @@
 
     var libraries_query = search_items.filter(use_this);
 
+    function formatTitle(result) {
+        var formatted_title = result.name;
+        if (result.displayname && result.displayname !== '') {
+            formatted_title += ' (' + result.displayname + ')';
+        }
+        return formatted_title;
+    }
 
-    env.ddg_spice_libraries = function(api_result){
+    function formatSubtitle(result) {
+        var formatted_subtitle = null;
+        if (result.namespace && result.namespace !== '') {
+            formatted_subtitle = result.namespace;
+        }
+        return formatted_subtitle;
+    }
 
-        if (!api_result || !api_result.count) {
+
+    env.ddg_spice_libraries = function(api_result) {
+        
+        if(!api_result) {
             return Spice.failed('libraries');
         }
-    Spice.add({
-            id: "libraries",
-            name: 'Software',
-            data: api_result.results,
+
+        var data = api_result.results;
+
+        if(!data.length || data.length === 0) {
+            return Spice.failed('libraries');
+        }
+
+        var result;
+        for (var i = 0; i < data.length; i++) {
+            var checkRelevancy = [data[i].name, data[i].displayname, data[i].namespace].join(" ");
+            if (DDG.isRelevant(checkRelevancy, [], 2)) {
+                result = data[i];
+                break;
+            }
+        }
+
+        if (!result) {
+            return Spice.failed('libraries');
+        }
+
+        result.title = formatTitle(result);
+        result.subtitle = formatSubtitle(result);
+
+        Spice.add({
+            id: 'libraries',
+            name: 'Answer',
+            data: result,
             meta: {
-                search_term:libraries_query,
-                sourceName: "libraries.io",
-                sourceUrl: 'https://libraries.io/api/search?q=' + libraries_query + '&api_key={{ENV{DDG_SPICE_LIBRARIES_IO_APIKEY}}}'
+                sourceUrl: 'https://libraries.io/api/search?q=' + libraries_query + '&api_key={{ENV{DDG_SPICE_LIBRARIES_IO_APIKEY}}}',
+                sourceName: 'libraries.io'
+            },
+            normalize: function(item) {
+                var subtitleArray = [];
+                if (item.subtitle) {
+                    subtitleArray.push(item.subtitle);
+                }
+                if (item.url && item.url !== '') {
+                    subtitleArray.push({
+                        href: item.url,
+                        text: "Reference"
+                    });
+                }
+                return {
+                    title: item.title,
+                    subtitle: subtitleArray
+                };
             },
             templates: {
                 group: 'text',
-                detail: false,
-                item_detail: false,
-
-                variants: {
-                    tile: '2line',
-                    tileSnippet: 'small'
+                options: {
+                    content: Spice.search_code.content,
+                    moreAt: true
                 }
-            },
-            normalize: function(item) {
-                    return {
-                        name: item.name,
-                        platform: item.platform,
-                        description: item.description,
-                        url: item.homepage,
-                        latest: item.latest_release_number
-                    }
-                }
+            }
         });
     };
 }(this));
