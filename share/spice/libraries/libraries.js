7e600761e73ff84f6fa2ea7d24531ebd16ded35b
knowledgewarrior <knowledgewarrior@gmail.com>
2017-03-02 18:27:14 -0800

    Libraries.io #3101

--- share/spice/libraries/libraries.js
+++ share/spice/libraries/libraries.js
@@ -1,27 +1,54 @@
-(function (env) {
+(function(env) {
     "use strict";
+    var search_items = DDG.get_query().split(" ");
+
+    function use_this(search_query) {
+        return DDG.isRelevant(search_query, [
+            'go', 
+            'elm', 
+            'wordpress', 
+            'atom'
+            'homebrew'
+            'elm'
+            'scala'
+            'dart'
+            'groovy'
+            'emacs'
+            'pypi'
+            'bower'
+        ], 1, 0);
+    }
+
+    var libraries_query = search_items.filter(use_this);
+
 
     env.ddg_spice_libraries = function(api_result){
 
-        if (!api_result || api_result.error || api_result.results.length === 0) {
+        if (!api_result || !api_result.count) {
             return Spice.failed('libraries');
         }
-        
-        var script = $('[src*="/js/spice/libraries/"]')[0];
-        var source = $(script).attr("src");
-        var query = source.match(/elm\/([^\/]*)/)[1];
+    Spice.add({
+            id: "libraries",
+            name: 'Software',
+            data: api_result.results,
+            meta: {
+                search_term:libraries_query,
+                sourceName: "libraries.io",
+                sourceUrl: 'https://libraries.io/api/search \
+                &api_key={{ENV{DDG_SPICE_LIBRARIES_IO_APIKEY}}}?q=' \
+                + libraries_query
+            },
+            templates: {
+                group: 'text',
+                detail: false,
+                item_detail: false,
 
-    DDG.require('moment.js', function(moment) {
-            Spice.add({
-                id: "libraries",
-                name: "Software",
-                data: api_result,
-                meta: {
-                    sourceName: "Libraries",
-                    sourceUrl: 'https://libraries.io/api/search&api_key={{ENV{DDG_SPICE_LIBRARIES_IO_APIKEY}}}?q=' + query
-                },
-                normalize: function(item) {
-                    
+                variants: {
+                    tile: '2line',
+                    tileSnippet: 'small'
+                }
+            },
+            normalize: function(item) {
                     return {
                         name: item.name,
                         platform: item.platform,
@@ -29,19 +56,7 @@
                         url: item.homepage,
                         latest: item.latest_release_number
                     }
-                },
-                templates: {
-                    group: 'text',
-                    detail: true,
-                    item_detail: true,
-                    options: {
-                        footer: Spice.libraries.footer
-                    },
-                    variants: {
-                        tile: 'basic4'
-                    }
                 }
-            });
         });
     };
 }(this));
