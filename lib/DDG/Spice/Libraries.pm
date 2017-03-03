498a8d46581bc4d929c7b76486a9d6ed9da1867b
knowledgewarrior <knowledgewarrior@gmail.com>
2017-03-02 16:31:14 -0800

    Libraries.io #3101

--- lib/DDG/Spice/Libraries.pm
+++ lib/DDG/Spice/Libraries.pm
@@ -1,17 +1,30 @@
 package DDG::Spice::Libraries;
+# ABSTRACT: Search for code on libraries.io
 
 use strict;
 use DDG::Spice;
+
+my @triggers = share('triggers.txt')->slurp;
+triggers startend => @triggers;
+
+my $words = join "|", @triggers;
+$words =~ s/\n//g;
 
 spice to => 'https://libraries.io/api/search?q=$1';
 spice is_cached => 1;
 spice proxy_cache_valid => "200 7d";
 
-triggers start => "go", "wordpress", "clojars", "atom", "homebrew", "elm", "scala", "dart", "groovy", "emacs", "pypi", "bower";
+handle query_raw => sub {
+        return if exists $skip_queries{lc($_)};
 
-handle remainder => sub {
-    my ($term) = @_;
-    return $term if $term;
+    # make sure there is more to the
+    # query besides the trigger itself
+    if (m/$words/i){
+        my $query = $_;
+        s/$words//ig;
+        return $query if length $_ > 1;
+    }
     return;
 };
+
 1;
