8dd00adfcd8a5ea0c04406663d696e58aaf35a23
Dave Harbage <dave@duckduckgo.com>
2017-03-15 11:22:10 -0400

    map spice should fail when only low relevance results come back.

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -2,16 +2,21 @@ DDG.require('maps',function(){
     ddg_spice_maps_maps = function(response) {
 
         if (!response || !response.features || !response.features.length) { return Spice.failed('maps_maps'); }
-
         // TEMP: disable relevancy and pass all results through
         // for map module experiment:
         if (DDG.opensearch.installed.experiment === 'map_module' && DDG.opensearch.installed.variant === 'a') {
-            return Spice.add({
-                data: response.features,
-                id: 'maps_maps',
-                name: 'maps',
-                model: 'Place'
-            });
+            // if top result returned doesn't have high relevance,
+            // the rest won't either so spice should fail.
+            if (response.features[0].relevance > 0.7) {
+                return Spice.add({
+                    data: response.features,
+                    id: 'maps_maps',
+                    name: 'maps',
+                    model: 'Place'
+                });
+            } else {
+                return Spice.failed('maps_maps');
+            }
         }
 
         // Mapbox sends back a bunch of places, just want the first one for now
