d22e4fd1f561a291afb14b48ed1cc632925a9fdc
Brian Stoner <bsstoner@gmail.com>
2017-03-22 16:04:47 -0400

    Skip relevancy for maps experiment (#3112)
    * skip relevancy for maps prototype
    
    * map spice should fail when only low relevance results come back.
    
    * only return one item from response if signal is medium, in order to fall back to old IA for medium signals.
    
    * return only one result when low or medium signal, to fix low signal error.
    
    * refactor map module experiment check in maps_maps.js

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -1,7 +1,22 @@
 DDG.require('maps',function(){
     ddg_spice_maps_maps = function(response) {
-
         if (!response || !response.features || !response.features.length) { return Spice.failed('maps_maps'); }
+
+        // if user is in map module experiment, and signal is high
+        if (DDG.opensearch.installed.experiment === 'map_module' && DDG.opensearch.installed.variant === 'a' && DDG.duckbar.futureSignals[0].from === "maps_maps" && DDG.duckbar.futureSignals[0].signal === "high") {
+            // if top result returned doesn't have high relevance,
+            // the rest won't either so spice should fail.
+            if (response.features[0].relevance < 0.7) {
+                return Spice.failed('maps_maps');
+            }
+
+            return Spice.add({
+                data: response.features,
+                id: 'maps_maps',
+                name: 'maps',
+                model: 'Place'
+            });
+        }
 
         // Mapbox sends back a bunch of places, just want the first one for now
         response = response.features[0];
