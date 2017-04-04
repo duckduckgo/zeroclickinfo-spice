049c3a76b6c3457ea189e317852dc2b493b3b370
David Harbage <harbagio@gmail.com>
2017-04-04 16:19:43 -0400

    move mapbox relevance filter from .7 to .6 (#3224)

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -8,7 +8,7 @@ DDG.require('maps',function(){
         if (DDG.opensearch.installed.experiment === 'map_module' && DDG.opensearch.installed.variant === 'a') {
             // if top result returned doesn't have high relevance,
             // the rest won't either so spice should fail.
-            if (response.features[0].relevance < 0.7 && !(response.features[0].place_name && DDG.isRelevant(response.features[0].place_name.toLowerCase(), skipArray))) {
+            if (response.features[0].relevance < 0.6 && !(response.features[0].place_name && DDG.isRelevant(response.features[0].place_name.toLowerCase(), skipArray))) {
                 return Spice.failed('maps_maps');
             }
 
