7cf3d5fcb48a7739478d806ceae45c81b38e1bcd
Nalin Bhardwaj <maverick.nalin@gmail.com>
2017-04-12 00:46:40 +0000

    Improved triggering

--- lib/DDG/Spice/Twitter.pm
+++ lib/DDG/Spice/Twitter.pm
@@ -5,7 +5,8 @@ use strict;
 use DDG::Spice;
 
 spice to => 'https://duckduckgo.com/tw.js?user=$1&callback={{callback}}&current=1';
-triggers query => qr/^(?:twitter\s)?@([a-z0-9_]+)$|^twitter\s([a-z0-9_]+)$/i;
+spice proxy_cache_valid => '200 1h';
+triggers query => qr/^(?:twitter\s)?@([a-z0-9_]+)$|^twitter\s([a-z0-9_]+)$|^([a-z0-9_]+)\s(?:twitter)?$|^@([a-z0-9_]+)\s(?:twitter)?$/i;
 
 # skip words from file
 my $skip = join "|", share('skipwords.txt')->slurp(chomp => 1);
@@ -14,7 +15,11 @@ handle matches => sub {
     if ($1) {
        return $1;
     } elsif ($2) {
-       return $2 unless ($2 =~ m/^($skip)$/i)
+       return $2 unless ($2 =~ m/^($skip)$/i);
+    } elsif($3) {
+        return $3 unless ($3 =~ m/^($skip)$/i);
+    } elsif($4) {
+        return $4;
     }
     return;
 };
