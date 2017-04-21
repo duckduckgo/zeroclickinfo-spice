1acfcef2b50212eb1cc78247975349b1546990f9
Niraj Swami <fivesquared@gmail.com>
2017-04-21 16:00:03 +0000

    JSK

--- share/spice/mo/mo.js
+++ share/spice/mo/mo.js
@@ -2,7 +2,10 @@
     "use strict";
 
     env.ddg_spice_mo = function(api_result) {
-
+        var script = $('[src*="/js/spice/mo/"]')[0],
+            source = $(script).attr("src"),
+            query = source.match(/mo\/([^\/]+)/)[1];
+        
         // Validate the response (customize for your Spice)
         if (!api_result || api_result.error || !api_result.results || api_result.results.length === 0) {
             return Spice.failed('mo');
@@ -16,15 +19,14 @@
             name: 'Learn with Mo',
             data: api_result.results,
             meta: {
-                sourceName: 'Mo',
-                sourceUrl: 'https://scad.ai'
+                sourceName: 'Mo | Powered by SCAD AI',
+                sourceUrl: 'https://scad.ai',
+                itemType: 'Learning Resources',
+                searchTerm: decodeURIComponent(query)
             },
             normalize: function(item) {
                 return {
                     // customize as needed for your chosen template
-                    title: item.name,
-                    description: item.name,
-                    url: "https://scad.ai"
                 };
             },
             templates: {
