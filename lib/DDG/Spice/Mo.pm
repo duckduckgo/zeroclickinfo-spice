ef98beeed5852e79ffb84ebccdbd7b5a022f3697
Niraj Swami <fivesquared@gmail.com>
2017-04-21 15:03:26 +0000

    JSK|JM

--- lib/DDG/Spice/Mo.pm
+++ lib/DDG/Spice/Mo.pm
@@ -15,6 +15,7 @@ spice proxy_cache_valid => '200 1d'; # defaults to this automatically
 
 spice wrap_jsonp_callback => 0; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
 
+# Using the endpoint for learning searches
 # API endpoint - http://docs.duckduckhack.com/walkthroughs/forum-lookup.html#api-endpoint
 spice to => 'https://learn-with-mo.herokuapp.com/duckduckgo?query=$1';
 
