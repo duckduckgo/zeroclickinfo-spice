954a8f7b30426659ae02c010c3bbb00e3365633e
synthtech <synthtech0@gmail.com>
2017-04-26 21:17:21 -0400

    Reduce size of response

--- lib/DDG/Spice/Manga.pm
+++ lib/DDG/Spice/Manga.pm
@@ -4,7 +4,7 @@ use strict;
 use DDG::Spice;
 
 spice proxy_cache_valid => "200 30d";
-spice to => 'http://kitsu.io/api/edge/manga?filter[text]=$1&page[limit]=20';
+spice to => 'http://kitsu.io/api/edge/manga?filter[text]=$1&page[limit]=20&fields[manga]=slug,synopsis,canonicalTitle,averageRating,posterImage,volumeCount,chapterCount';
 
 triggers startend => 'manga', 'kitsu';
 spice wrap_jsonp_callback => 1;
