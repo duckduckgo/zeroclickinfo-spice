99676f9cb59e885aa572c15731ff4188cbadd2cc
Nalin Bhardwaj <maverick.nalin@gmail.com>
2017-05-15 08:47:37 +0000

    Made regex fixes

--- lib/DDG/Spice/Twitter.pm
+++ lib/DDG/Spice/Twitter.pm
@@ -7,13 +7,13 @@ use DDG::Spice;
 spice to => 'https://duckduckgo.com/tw.js?user=$1&callback={{callback}}&current=1';
 spice proxy_cache_valid => '200 1h';
 
-my $grammar = qr/(?:on)?(?:at)?/io;
-my $other = qr/(?:user)?(?:account)?(?:id)?/io;
+my $grammar = qr/on|at/;
+my $other = qr/user|account|id/;
 
 triggers query_lc => qr/^(?:twitter\s)?(?:$other\s)?@([a-z0-9_]+)$/i;
 triggers query_lc => qr/^twitter\s(?:$other\s)?([a-z0-9_]+)$/i;
 triggers query_lc => qr/^([a-z0-9_]+)\s(?:$other\s)?(?:$grammar\s)?(?:twitter)$/i;
-triggers query_lc => qr/^@([a-z0-9_]+)\s(?:$other\s)?(?:$grammar\s)?(?:twitter)?$/i;
+triggers query_lc => qr/^@([a-z0-9_]+)\s(?:$other\s)?(?:$grammar\s)?twitter?$/i;
 
 
 # skip words from file
@@ -21,7 +21,7 @@ my $skip = join "|", share('skipwords.txt')->slurp(chomp => 1);
 
 handle matches => sub {
     if ($1) {
-       return $1 unless ($1 =~ m/^($skip)$/i);
+       return $1 unless ($1 =~ m/^$skip$/i);
     }
     return;
 };
