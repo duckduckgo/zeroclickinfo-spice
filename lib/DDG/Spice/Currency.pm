75d1aaba2631dd585ec890dbdfd930982a4dc528
PJ Hampton <pjhampton@protonmail.com>
2017-05-25 15:30:03 +0100

    Tightened triggering.

--- lib/DDG/Spice/Currency.pm
+++ lib/DDG/Spice/Currency.pm
@@ -36,7 +36,7 @@ my $cardinal_re = join(' |', qw(hundred thousand k million m billion b trillion)
 my $from_qr = qr/(?<fromSymbol>\p{Currency_Symbol})|(?:(?<from>$currency_qr)s?)/;
 my $amount_qr = qr/(?<amount>$number_re*)\s?(?<cardinal>$cardinal_re)?/;
 my $keyword_qr = qr/(?:\s?(?<currencyKeyword>(?:currency|value|price))\s?)/i;
-my $lang_qr = qr/convert currency|currency conver(ter|sions?)/i;
+my $lang_qr = qr/^(?:convert currency|currency conver(ter|sions?))$/i;
 
 my $guard = qr/^$question_prefix(?:$from_qr\s?$amount_qr|$amount_qr\s?$from_qr)\s?$keyword_qr?(?:$into_qr|$vs_qr|\/|\s)?(?<to>$currency_qr)?(?<toSymbol>\p{Currency_Symbol})?s?$keyword_qr?\??$/i;
 
