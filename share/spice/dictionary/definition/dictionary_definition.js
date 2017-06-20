83a3830e9363b51d58f54cb60e116ebc6fe83feb
Brian Stoner <brian@duckduckgo.com>
2017-06-20 16:03:51 -0400

    move filter back above the guard, just add guards for the fitler

--- share/spice/dictionary/definition/dictionary_definition.js
+++ share/spice/dictionary/definition/dictionary_definition.js
@@ -102,9 +102,8 @@ var ddg_spice_dictionary = {
     definition: function(api_result) {
         "use strict";
 
+        api_result = api_result && api_result.length && api_result.filter(function(result) { return result.text; })
         if (!api_result || !api_result.length) { return Spice.failed('dictionary_definition'); }
-
-        api_result = api_result.filter(function(result) { return result.text; })
 
         // Prevent jQuery from appending "_={timestamp}" in our url when we use $.getScript.
         // If cache was set to false, it would be calling /js/spice/dictionary/definition/hello?_=12345
