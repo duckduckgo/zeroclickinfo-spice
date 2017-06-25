d2ff47397b6e6c365dd04204956cdc1dc972a511
Kcchouette <Kcchouette@users.noreply.github.com>
2017-06-25 20:32:41 +0200

    add headers for kitsu API
    See http://docs.kitsu17.apiary.io/#introduction/json-api/general-info

--- lib/DDG/Spice/Anime.pm
+++ lib/DDG/Spice/Anime.pm
@@ -6,6 +6,10 @@ use DDG::Spice;
 spice proxy_cache_valid => "200 30d";
 spice to => 'http://kitsu.io/api/edge/anime?filter[text]=$1&page[limit]=20&fields[anime]=slug,synopsis,canonicalTitle,averageRating,posterImage,episodeCount,episodeLength';
 
+spice headers => { Accept => 'application/vnd.api+json',
+                   Content-Type => 'application/vnd.api+json'
+                 };
+
 triggers startend => 'anime', 'kitsu';
 spice wrap_jsonp_callback => 1;
 
