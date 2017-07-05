f2e646cc7fb374c5cd00221582544953b23d420b
Manraj Singh <manrajsinghgrover@gmail.com>
2017-07-05 17:36:50 +0000

    GithubIssues.pm: Handles  regex errors

--- lib/DDG/Spice/GithubIssues.pm
+++ lib/DDG/Spice/GithubIssues.pm
@@ -5,6 +5,7 @@ use DDG::Spice;
 use strict;
 use warnings;
 use Text::Trim;
+use Data::Dumper;
 
 spice is_cached => 1;
 spice proxy_cache_valid => '200 1h';
@@ -13,32 +14,26 @@ spice wrap_jsonp_callback => 0;
 
 spice to => 'https://api.github.com/search/issues?q=$1&callback={{callback}}';
 
-my @languages = share("languages.txt")->slurp;
+my @languages = share("languages.txt")->slurp( chomp => 1);
 @languages = sort { length($b) <=> length($a) } @languages;
 
-chomp(@languages);
 my $langs = join("|", map(quotemeta, @languages));
 
-triggers startend => 'github issue';
+triggers end => 'github issue';
 
 # Handle statement
 handle remainder_lc => sub {
     return unless $_;
 
-    my $query = $_;
-    $query =~ s/\b($langs)\b//;
-    trim($query);
-
-    my $lang;
-
-    if (/\b($langs)\b/) {
-        $lang = $1;
+    if (m/\b($langs)$/) {
+        my $lang = $1;
+        s/\b($langs)$//;
+        trim($_);
         return unless length $_;
-
-        return qq($query language:"$lang");
-    } else {
-        return $query;
+        return qq($_ language:"$lang");
     }
+
+    return $_;
 };
 
 1;
