15da90fb307a9f2d6bea299bdc14cd256c403fa8
Khanh Tran <nkhanhtrn@users.noreply.github.com>
2017-07-10 17:39:48 -0400

    Thesaurus: Don't display IA result if searching for antonym not found (#3124)
    * skip display IA result if looking for antonym not found
    
    * lookup original query from JS function instead of document.title

--- share/spice/thesaurus/thesaurus.js
+++ share/spice/thesaurus/thesaurus.js
@@ -6,16 +6,29 @@
             return Spice.failed("thesaurus");
         }
 
+        // Check if user want to find antonym only
+        var searchAntonymOnly = (DDG.get_query().indexOf('antonym') > -1);
+
         // Retrieve the word we did a lookup on
         var script = $('[src*="/js/spice/thesaurus/"]')[0];
         var source = $(script).attr("src");
         var query = source.match(/thesaurus\/([^\/]+)/)[1];
-        query = decodeURIComponent(query);
         
+        query = decodeURIComponent(query);
         // Search the results for the word type (noun, adjective etc) with the most entries
         var wordTypeWithMostEntries;
         var highestEntryCount = 0;
+        var exactWord;
 
+        // constants for types of relationship
+        var RELATIONSHIP_TYPE = {
+            SYNONYMS : "syn",
+            SIMILAR  : "sim",
+            RELATED  : "rel",
+            ANTONYMS : "ant"
+        };
+
+        
         $.each(api_result, function(wordType, words) { // wordType = ajective, noun etc... 
             var wordTypeEntryCount = 0;
             
@@ -32,11 +45,17 @@
         if (!wordTypeWithMostEntries) {
             return Spice.failed("thesaurus");    
         }
+        exactWord = api_result[wordTypeWithMostEntries];
+        
+        // Can't find any antonym when searching for antonym
+        if (searchAntonymOnly && !exactWord.hasOwnProperty(RELATIONSHIP_TYPE.ANTONYMS)) {
+            return Spice.failed("thesaurus");
+        }
 
         Spice.add({
             id: 'thesaurus',
             name: 'Thesaurus',
-            data:  api_result[wordTypeWithMostEntries],
+            data:  exactWord,
             templates: {
                 group: 'text',
                 options:{
@@ -51,13 +70,6 @@
             },
             
             normalize: function(words) {
-                var RELATIONSHIP_TYPE = {
-                    SYNONYMS : "syn",
-                    SIMILAR  : "sim",                    
-                    RELATED  : "rel",
-                    ANTONYMS : "ant"
-                };
-                
                 var result = {
                     title: DDG.capitalize(query),
                     subtitle: DDG.capitalize(wordTypeWithMostEntries),
@@ -70,19 +82,19 @@
                     result.record_keys.push("Synonyms");
                     result.record_data["Synonyms"] = words[RELATIONSHIP_TYPE.SYNONYMS].join(', ');
                 }
-                
+
                 // Display any similar words if present
                 if (words.hasOwnProperty(RELATIONSHIP_TYPE.SIMILAR)) {
                     result.record_keys.push("Similar");
                     result.record_data["Similar"] = words[RELATIONSHIP_TYPE.SIMILAR].join(', ');
                 }
-                
+
                 // Display any related words if present
                 if (words.hasOwnProperty(RELATIONSHIP_TYPE.RELATED)) {
                     result.record_keys.push("Related");
                     result.record_data["Related"] = words[RELATIONSHIP_TYPE.RELATED].join(', ');                    
                 }
-                
+
                 // Display any antonyms if present
                 if (words.hasOwnProperty(RELATIONSHIP_TYPE.ANTONYMS)) {
                     result.record_keys.push("Antonyms");
