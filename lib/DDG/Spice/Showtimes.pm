7d22fef5c171b15f37ec7c197532117c8ce598b8
PJ Hampton <pjhampton@protonmail.com>
2017-07-21 18:24:51 +0100

    Check in work

--- lib/DDG/Spice/Showtimes.pm
+++ lib/DDG/Spice/Showtimes.pm
@@ -5,20 +5,50 @@ use DDG::Spice;
 use strict;
 use warnings;
 
-spice is_cached => 1;
-spice proxy_cache_valid => "200 10m";
-spice wrap_jsonp_callback => 0;
+use YAML::XS 'LoadFile';
 
-spice to => 'http://example.com/search/$1';
+my %cities = %{ LoadFile(share('cities.yml')) };
+
+spice is_cached => 1;
+spice proxy_cache_valid => "200 2h";
+spice wrap_jsonp_callback => 1;
+
+spice headers => { "X-API-Key" => "54TYZocmST5xtRiGhSRue7CPNfCToo3K" }; # ! YO PETE, REMOVE THIS!!
+spice to => 'https://api.internationalshowtimes.com/v4/showtimes?city_ids=$1';
+
+spice alt_to => {
+    fetch_movie_data => {
+        to => "https://api.internationalshowtimes.com/v4/movies/$1",
+        headers => { "X-API-Key" => "54TYZocmST5xtRiGhSRue7CPNfCToo3K" }, # ! YO PETE, REMOVE THIS!!
+        wrap_jsonp_callback => 1
+    },
+    fetch_cinema_data => {
+        to => "https://api.internationalshowtimes.com/v4/cinema/$1",
+        headers => { "X-API-Key" => "54TYZocmST5xtRiGhSRue7CPNfCToo3K" }, # ! YO PETE, REMOVE THIS!!
+        wrap_jsonp_callback => 1     
+    },
+    fetch_pete_data => {
+        to => "https://api.pete.com/v4/cinema/$1",
+        headers => { "X-API-Key" => "54TYZocmST5xtRiGhSRue7CPNfCToo3K" }, # ! YO PETE, REMOVE THIS!!
+        wrap_jsonp_callback => 1     
+    }
+};
+
 triggers any => 'movies in', 'movie in', 'films in', 'film in';
 
 # Handle statement
-handle query_lc => sub {
-    my ($query) = $_;
+handle remainder => sub {
+    my ($location) = $_;
+
+    $location =~ tr/A-Z/a-z/;
+
+    $location = $cities{$location}->{"id"};
+    return if $location !~ /\d+/;
 
     # Query is in $_ or @_, depending on the handle you chose...if you
     # need to do something with it before returning
-    return \$_;
+    return $location;
+    return;
 };
 
 1;
--- share/spice/showtimes/showtimes.js
+++ share/spice/showtimes/showtimes.js
@@ -1,38 +1,65 @@
 (function (env) {
     "use strict";
 
+    var movies = [
+        {"movie":{"id":"20335","slug":"war-for-the-planet-of-the-apes","title":"War for the Planet of the Apes","poster_image_thumbnail":"http://image.tmdb.org/t/p/w154/y52mjaCLoJJzxfcDDlksKDngiDx.jpg"}},
+        {"movie":{"id":"20437","slug":"une-vie","title":"A Woman's Life","poster_image_thumbnail":"http://image.tmdb.org/t/p/w154/waWWTDxKjSCmNAefMkM4uvQzdD0.jpg"}},
+        {"movie":{"id":"28162","slug":"49a38d08-365e-4847-890c-99e4eb3ec0dd","title":"Jagga Jasoos","poster_image_thumbnail":"http://image.tmdb.org/t/p/w154/syKtt4o1qk8gzlovg0edQ93bFE2.jpg"}},
+        {"movie":{"id":"20802","slug":"all-eyez-on-me","title":"All Eyez on Me","poster_image_thumbnail":"http://image.tmdb.org/t/p/w154/zmgsaKFWbmZ1Grz4SO0PLNxilv3.jpg"}},
+        {"movie":{"id":"20606","slug":"baywatch","title":"Baywatch","poster_image_thumbnail":"http://image.tmdb.org/t/p/w154/6HE4xd8zloDqmjMZuhUCCw2UcY1.jpg"}},
+        {"movie":{"id":"26272","slug":"beatriz-at-dinner","title":"Beatriz at Dinner","poster_image_thumbnail":"http://image.tmdb.org/t/p/w154/obOhjpLQ720cdJMdhprOLBMrJI1.jpg"}},
+        {"movie":{"id":"25530","slug":"everything-everything","title":"Everything, Everything","poster_image_thumbnail":"http://image.tmdb.org/t/p/w154/c8W3Go48Mw0GoNXsGK4W9hNO4Vf.jpg"}},
+    ]
+
+    function get_movie_name(id) {
+        for(var i = 0 ; i < movies.length ; i++) {
+            if(id == movies[i].movie.id) {
+                return movies[i].movie.title;
+            }
+        }
+    }
+
+    function get_movie_poster(id) {
+        for(var i = 0 ; i < movies.length ; i++) {
+            if(id == movies[i].movie.id) {
+                console.log(movies[i].movie.poster_image_thumbnail);
+                return movies[i].movie.poster_image_thumbnail;
+            }
+        }
+    }
+
     env.ddg_spice_showtimes = function(api_result) {
 
         // Validate the response (customize for your Spice)
-        if (!api_result || api_result.error) {
+        if (!api_result || api_result.error || api_result.showtimes > 5) {
             return Spice.failed('showtimes');
         }
+
+        console.log(api_result.showtimes);
+        api_result = api_result.showtimes;
 
         // Render the response
         Spice.add({
             id: 'showtimes',
+            signal: 'high',
 
             // Customize these properties
-            name: 'AnswerBar title',
+            name: 'Showtimes',
             data: api_result,
-            meta: {
-                sourceName: 'Example.com',
-                sourceUrl: 'http://example.com/url/to/details/' + api_result.name
-            },
+            meta: {},
             normalize: function(item) {
+                var title = get_movie_name(item.movie_id);
+                var image = get_movie_poster(item.movie_id);
                 return {
                     // customize as needed for your chosen template
-                    title: item.title,
-                    subtitle: item.subtitle,
-                    image: item.icon
+                    title: title || item.movie_id,
+                    image: image || "",
+                    start_at: item.start_at
                 };
             },
             templates: {
-                group: 'your-template-group',
-                options: {
-                    content: Spice.showtimes.content,
-                    moreAt: true
-                }
+                item: Spice.showtimes.item,
+                item_detail: Spice.showtimes.detail
             }
         });
     };
