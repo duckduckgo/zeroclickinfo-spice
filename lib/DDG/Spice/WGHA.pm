140469ec856928cc0333aaace4591238787356ed
PJ Hampton <pjhampton@protonmail.com>
2017-08-16 16:31:15 +0100

    Fix apparent WGHA overtriggering.

--- lib/DDG/Spice/WGHA.pm
+++ lib/DDG/Spice/WGHA.pm
@@ -5,7 +5,7 @@ use strict;
 use DDG::Spice;
 use utf8;
 
-my @triggers = ("was geht", "wasgeht", "aus gehen", "ausgehen", "nachtleben", "party", "parties", "partys", "feiern", "abfeiern", "weg gehen","weggehen", "tanz", "tanzen", "veranstaltung", "veranstaltungen", "fest", "disko", "disco" );
+my @triggers = ("was geht", "wasgeht", "aus gehen", "ausgehen", "nachtleben", "parties", "partys", "feiern", "abfeiern", "weg gehen","weggehen", "tanz", "tanzen", "veranstaltung", "veranstaltungen");
 my $cities = "Aschaffenburg|Augsburg|Berlin|Berlin-Biesdorf|Berlin-Friedrichshain|Berlin-Karow|Berlin-Köpenick|Berlin-Lichtenberg|Berlin-Marzahn|Berlin-Mitte|Berlin-Oberschöneweide|Berlin-Pankow|Berlin-Treptow|Bremen|Cologne|Darmstadt|Dieburg|Dortmund|Dreieich|Dresden|Dresden-Altstadt|Dresden-Neustadt|Düsseldorf|Essen|FFM|Frankfurt|Frankfurt am Main|Freiburg|Freiburg im Breisgau|Friedrichshain|Griesheim|Halle|Hamburg|Hamburg-Eimsbüttel|Hamburg-Mitte|Hannover|Hanover|Heidelberg|Kassel|Koeln|Germany|Kreuzberg|Köln|Leipzig|Leipzig-Connewitz|Leipzig-Leutzsch|Leipzig-Plagwitz|Leipzig-Stötteritz|Mainz|Mannheim|Munich|Mühlheim|München|München|Germany|Münster|Neu Isenburg|Neukölln|Nuremberg|Nürnberg|Offenbach|Prenzlauer Berg|Sankt Pauli|Schöneberg|Sersheim|Stuttgart|Wiesbaden";
 
 triggers any => @triggers;
