b6457515399da0c03b8da4b65caa0d7cb952b469
Thomas Denizou <javathunderman@nomik.xyz>
2017-08-24 19:09:18 +0000

    added more context to result, included no classification status

--- share/spice/terms_of_service/terms_of_service.js
+++ share/spice/terms_of_service/terms_of_service.js
@@ -5,20 +5,32 @@
         if (!api_result || api_result.error) {
             return Spice.failed('terms_of_service');
         }
-
+        var script = $('[src*="/js/spice/terms_of_service/"]')[0],
+        source = $(script).attr("src"),
+        query = source.match(/terms_of_service\/([^\/]+)/)[1],
+        decodedQuery = decodeURIComponent(query);
         Spice.add({
             id: "terms_of_service",
             name: "Terms of Service",
             data: api_result,
             meta: {
                 sourceName: "Terms of Service; Didn't Read",
-                sourceUrl: 'http://tosdr.org'
+                sourceUrl: 'https://tosdr.org#' + decodedQuery
             },
             normalize: function(item) {
+                if (item.class == false) {
+                    return {
+                        title: "No classification yet",
+                        description: "TOS;DR evaluates company terms of services with class-based ratings from A to E."
+                        
+                    }
+                }
+                else {
                 return {
                     title: "Class " + item.class,
-                    
+                    description: "TOS;DR evaluates company terms of services with class-based ratings from A to E."
                 };
+                }
             },
             templates: {
                 group: 'info'
