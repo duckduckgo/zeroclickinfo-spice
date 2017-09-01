f0d55b96877dad480464e0dcdc3d952847162e87
Thomas Denizou <javathunderman@nomik.xyz>
2017-09-01 14:10:46 +0000

    add tldr

--- share/spice/terms_of_service/terms_of_service.js
+++ share/spice/terms_of_service/terms_of_service.js
@@ -9,6 +9,7 @@
         source = $(script).attr("src"),
         query = source.match(/terms_of_service\/([^\/]+)/)[1],
         decodedQuery = decodeURIComponent(query);
+        
         Spice.add({
             id: "terms_of_service",
             name: "Terms of Service",
@@ -18,17 +19,17 @@
                 sourceUrl: 'https://tosdr.org#' + decodedQuery
             },
             normalize: function(item) {
+                var tldrresult = (item.pointsData[item.points[0]].tosdr.tldr);
                 if (item.class == false) {
                     return {
                         title: "No classification yet",
-                        description: "TOS;DR evaluates company terms of services with class-based ratings from A to E."
-                        
+                        description: tldrresult
                     }
                 }
                 else {
                 return {
                     title: "Class " + item.class,
-                    description: "TOS;DR evaluates company terms of services with class-based ratings from A to E."
+                    description: tldrresult
                 };
                 }
             },
