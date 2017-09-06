bf05b7719cf0090c03fda04b71b331aae9d39806
Zaahir Moolla <moollaza@fastmail.fm>
2017-09-06 16:35:28 -0400

    switch to HTTPS

--- lib/DDG/Spice/Anime.pm
+++ lib/DDG/Spice/Anime.pm
@@ -4,7 +4,7 @@ use strict;
 use DDG::Spice;
 
 spice proxy_cache_valid => "200 30d";
-spice to => 'http://kitsu.io/api/edge/anime?filter[text]=$1&page[limit]=20&fields[anime]=slug,synopsis,canonicalTitle,averageRating,posterImage,episodeCount,episodeLength';
+spice to => 'https://kitsu.io/api/edge/anime?filter[text]=$1&page[limit]=20&fields[anime]=slug,synopsis,canonicalTitle,averageRating,posterImage,episodeCount,episodeLength';
 
 spice headers => { Accept => 'application/vnd.api+json',
                    'Content-Type' => 'application/vnd.api+json'
