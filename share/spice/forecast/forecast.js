677fe330804fd71535c5a20a3fe2805beebd083c
PJ Hampton <pjhampton@users.noreply.github.com>
2017-09-16 00:04:36 +0100

    Forcast: Default to Celcius for non US  (#3426)
    * Default to Celcius for non US (timezone hint).
    
    * check timezone against list to determine uom

--- share/spice/forecast/forecast.js
+++ share/spice/forecast/forecast.js
@@ -1,5 +1,38 @@
 (function(env) {
     "use strict";
+
+    var usTZ = {
+        "America/New_York" : 1,
+        "America/Detroit" : 1,
+        "America/Kentucky/Louisville" : 1,
+        "America/Kentucky/Monticello" : 1,
+        "America/Indiana/Indianapolis" : 1,
+        "America/Indiana/Vincennes" : 1,
+        "America/Indiana/Winamac" : 1,
+        "America/Indiana/Marengo" : 1,
+        "America/Indiana/Petersburg" : 1,
+        "America/Indiana/Vevay" : 1,
+        "America/Chicago" : 1,
+        "America/Indiana/Tell_City" : 1,
+        "America/Indiana/Knox" : 1,
+        "America/Menominee" : 1,
+        "America/North_Dakota/Center" : 1,
+        "America/North_Dakota/New_Salem" : 1,
+        "America/North_Dakota/Beulah" : 1,
+        "America/Denver" : 1,
+        "America/Boise" : 1,
+        "America/Phoenix" : 1,
+        "America/Los_Angeles" : 1,
+        "America/Anchorage" : 1,
+        "America/Juneau" : 1,
+        "America/Sitka" : 1,
+        "America/Yakutat" : 1,
+        "America/Nome" : 1,
+        "America/Adak" : 1,
+        "America/Metlakatla" : 1,
+        "Pacific/Honolulu" : 1
+    };
+
     env.ddg_spice_forecast = function(api_result) {
 
         // Exit if we've got a bad forecast
@@ -101,12 +134,7 @@
             }
 
             var temp_str = '<span class="fe_temp_str">' + Math.round(f.currently.temperature) + '&deg;</span>'
-            /*
-            if(temp_direction > 0)
-              temp_str += ' <span class="fe_dir">and rising</span>'
-            else if(temp_direction < 0)
-              temp_str += ' <span class="fe_dir">and falling</span>'
-            */
+
             if (f.currently.apparentTemperature) {
                 feel_str = 'Feels like ' + Math.round(f.currently.apparentTemperature) + '&deg;'
             }
@@ -264,7 +292,6 @@
                     sourceName: 'Dark Sky',
                     primaryText: weatherData.header,
                     secondaryText: altMeta,
-
                     itemsWidthVaries: true
                 },
 
@@ -377,8 +404,12 @@
                     // unit of measurement. If the metric setting is enabled or the user's
                     // region is not USA and the API returned temps in F, switch to 'C':
                     var uom_in_query = DDG.get_query().match(/\b(celsius|fahrenheit)\b/);
+                    var response_timezone = api_result.timezone;
                     if (uom_in_query) {
                         uom = (uom_in_query[1] === 'celsius') ? 'C' : 'F';
+                        updateUnitOfMeasure();
+                    } else if(!usTZ[response_timezone])) {
+                        uom = 'C';
                         updateUnitOfMeasure();
                     } else if (!DDG.settings.isDefault('kaj')) {
                         uom = DDG.settings.get('kaj') === 'm' ? 'C' : 'F';
