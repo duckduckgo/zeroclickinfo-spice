09540ba52a4de781456da106086a843a9a44d619
Brian Stoner <brian@duckduckgo.com>
2017-09-28 09:57:15 -0400

    call duckbar.failed() for empty maps responses

--- share/spice/maps/maps/maps_maps.js
+++ share/spice/maps/maps/maps_maps.js
@@ -1,7 +1,12 @@
 DDG.require('maps',function(){
     ddg_spice_maps_maps = function(res) {
+        if (!res) { return DDG.duckbar.failed('maps_maps'); }
 
         var response = DDG.localAPI.getMapsResponse(res);
+
+        if (!response || !response.results || !response.results.length) {
+            return DDG.duckbar.failed('maps_maps');
+        }
         
         return Spice.add({
             data: response.results,
