d890e36ca64f165c65cc9ec37c91422b7259344f
Zaahir Moolla <moollaza@duckduckgo.com>
2017-12-08 13:51:43 -0500

    ensure ads object exists

--- share/spice/amazon/amazon.js
+++ share/spice/amazon/amazon.js
@@ -16,7 +16,7 @@
         };
 
         if (api_result.results.length > 1) {
-            var isBrandExp = DDG.page.ads.adxExperiment === 'prod_brand_v1';
+            var isBrandExp = DDG.page.ads && DDG.page.ads.adxExperiment === 'prod_brand_v1';
             templates = {
                 item: isBrandExp ? Spice.amazon.branded_products_item : 'products_item',
                 options: {
