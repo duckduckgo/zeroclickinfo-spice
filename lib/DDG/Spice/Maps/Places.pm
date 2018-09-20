2e80af98e0290af43babfbeb3bae082ac2fae683
Richard Bergmair <14801887+rbergmair@users.noreply.github.com>
2018-09-20 20:17:40 +0200

    Richard/xper places triggers (#3542)
    * put in place the necessary plumbing for an experiment with spice triggering based on categorical query triggers.
    
    * dropped in the real regex for the experimental new categorical query triggers
    
    * added even more triggers
    
    * fixed encoding problem in regex files

--- lib/DDG/Spice/Maps/Places.pm
+++ lib/DDG/Spice/Maps/Places.pm
@@ -11,8 +11,10 @@ spice proxy_cache_valid => "418 1d";
 spice is_cached => 0;
 
 my $chains_re = share('chains_re')->slurp;
+my $categories_re = share('categories_re')->slurp;
+my $categories_bl_re = share('categories_bl_re')->slurp;
 my $places_re = qr/(local|near|near me|around|around me|here|locally|nearby|close|closest|nearest|locations?|restaurants?)/;
-triggers query_lc => qr/(^$chains_re$|^$places_re|$places_re$)/s;
+triggers query_lc => qr/(^$chains_re$|^$places_re|^$categories_re\b|^$categories_bl_re\b|$places_re$)/s;
 
 my %skip_remainders = map {$_ => 0} ('current', 'time');
 
