f0b63d253a1db65ad120557825ab2c7bdd9c69c9
richard@duckduckgo.com <richard@duckduckgo.com>
2018-10-17 11:40:13 -0400

    cleaned up code wrt places triggering changes

--- lib/DDG/Spice/Maps/Places.pm
+++ lib/DDG/Spice/Maps/Places.pm
@@ -13,7 +13,7 @@ spice is_cached => 0;
 my $chains_re = share('chains_re')->slurp;
 my $categories_re = share('categories_re')->slurp;
 my $places_re = qr/(local|near|near me|around|around me|here|locally|nearby|close|closest|nearest|locations?|restaurants?)/;
-triggers query_lc => qr/(^$chains_re$|^$places_re|^$categories_re\b|$places_re$)/s;
+triggers query_lc => qr/(^$chains_re$|^$places_re\b|\b$places_re$|^$categories_re\b)/s;
 
 my %skip_remainders = map {$_ => 0} ('current', 'time');
 
@@ -26,4 +26,3 @@ handle query_lc => sub {
 };
 
 1;
-
