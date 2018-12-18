28db0132a7db2fb41a9d547af1f77848e564930f
Zaahir Moolla <moollaza@duckduckgo.com>
2018-12-18 17:06:36 -0500

    Update triggers

--- lib/DDG/Spice/DdgTraffic.pm
+++ lib/DDG/Spice/DdgTraffic.pm
@@ -11,7 +11,10 @@ spice wrap_jsonp_callback => 1;
 
 spice to => 'https://duckduckgo.com/traffic_data/direct.json';
 
-triggers start => 'duckduckgo traffic', 'ddg traffic';
+triggers start =>
+    'duckduckgo traffic', 'ddg traffic',
+    'duckduckgo traffic graph', 'ddg traffic graph',
+    'duckduckgo traffic numbers', 'ddg traffic numbers';
 
 handle remainder => sub {
     return if $_; #query should only be trigger!
